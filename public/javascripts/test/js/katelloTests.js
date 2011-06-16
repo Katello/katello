61af924402cd475974c21e2337ac712e8a64410d
shughes <shughes@scooby.rdu.redhat.com>
2011-06-16 17:03:34 -0400

    adding qunit test files for testswarm server

---
+++ public/javascripts/test/js/katelloTests.js
@@ -0,0 +1,17 @@
+test('localize_i18n', function() {
+
+  var transme = {
+    "cancel": 'Cancel',
+    "error": 'Error',
+    "complete": 'Process complete'
+  };
+
+  localize(transme);
+  equal(i18n['error'], 'Error', 'contains Error key');
+
+})
+
+test('escapeSelector', function() {
+  var val = common.escapeId("#my.val");
+  ok(val.indexOf("\\") != -1, "properly escape Jquery selectors");
+})
