74a8d83ca74ddb33004837d2574e99596167c1d6
Dmitri Dolguikh <dmitri@redhat.com>
2011-06-17 09:44:13 -0300

    added api repository controller tests for repository discovery

--- spec/controllers/api/repositories_controller_spec.rb
+++ spec/controllers/api/repositories_controller_spec.rb
@@ -9,7 +9,6 @@ describe Api::RepositoriesController do
     login_user_api
   end
 
-
   describe "create a repository" do
     it 'should call pulp and candlepin layer' do
       Product.should_receive(:find_by_cp_id).with('product_1').and_return(@product)
@@ -30,6 +29,20 @@ describe Api::RepositoriesController do
     it 'should call pulp glue layer' do
       Pulp::Repository.should_receive(:find).with('repo_1').and_return({})
       get 'show', :id => 'repo_1'
+    end
+  end
+
+  describe "repository discovery" do
+    it "should call Pulp::Proxy.post" do
+      Pulp::Proxy.should_receive(:post).with("/services/discovery/repo/", anything).once.and_return({})
+      post 'discovery'
+    end
+  end
+
+  describe "repository discovery status" do
+    it "should call Pulp::Proxy.get" do
+      Pulp::Proxy.should_receive(:get).with("/services/discovery/repo/1/").once.and_return({})
+      post 'discovery_status', :id => 1
     end
   end
 
