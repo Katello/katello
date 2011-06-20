a4cac0ab660223bf4dec3eb04217dc10f723bddd
Tomas Strachota <tstrachota@redhat.com>
2011-06-20 09:23:30 +0200

    %B

--- app/models/system_template.rb
+++ app/models/system_template.rb
@@ -17,34 +17,19 @@ class SystemTemplate < ActiveRecord::Base
 
   validates_presence_of :name
 
-  attr_reader :packages, :errata, :products, :host_group, :kickstart_attrs
+  attr_accessor :packages, :errata, :products, :host_group, :kickstart_attrs
 
+  before_validation :attrs_to_json
 
   def initialize attrs
     super(attrs)
   end
 
 
-  def packages_json= attrs
-    @packages = nil
-    @attributes['packages_json'] = attrs
-  end
-
-  def errata_json= attrs
-    @errata = nil
-    @attributes['errata_json'] = attrs
-  end
-
-  def products_json= attrs
-    @products = nil
-    @attributes['products_json'] = attrs
-  end
-
-
   def packages
     return @packages if not @packages.nil?
 
-    packages = ActiveSupport::JSON.decode(self.packages_json)
+    packages = ActiveSupport::JSON.decode((self.packages_json or "[]"))
     @packages = packages.collect do |p|
       package = self.find_package_in_env(p)
       if package == nil
@@ -58,7 +43,7 @@ class SystemTemplate < ActiveRecord::Base
   def errata
     return @errata if not @errata.nil?
 
-    errata = ActiveSupport::JSON.decode(self.errata_json)
+    errata = ActiveSupport::JSON.decode((self.errata_json or "[]"))
     @errata = errata.collect do |e|
       erratum = self.find_errata_in_env(e)
       if erratum == nil
@@ -72,7 +57,7 @@ class SystemTemplate < ActiveRecord::Base
   def products
     return @products if not @products.nil?
 
-    products = ActiveSupport::JSON.decode(self.products_json)
+    products = ActiveSupport::JSON.decode((self.products_json or "[]"))
     @products = products.collect do |p|
       product = self.environment.products.find_by_name(p)
       if product == nil
@@ -84,16 +69,18 @@ class SystemTemplate < ActiveRecord::Base
 
 
   def host_group
-    ActiveSupport::JSON.decode(self.host_group_json)
+    return @host_group if not @host_group.nil?
+    @host_group = ActiveSupport::JSON.decode((self.host_group_json or "{}"))
   end
 
 
   def kickstart_attrs
-    ActiveSupport::JSON.decode(self.kickstart_attrs_json)
+    return @kickstart_attrs if not @kickstart_attrs.nil?
+    @kickstart_attrs = ActiveSupport::JSON.decode((self.kickstart_attrs_json or "{}"))
   end
 
 
-  #TODO: comment
+  #TODO: comment, do we really need this function?
   def content_valid?
     self.packages
     self.errata
@@ -107,27 +94,37 @@ class SystemTemplate < ActiveRecord::Base
     return false
   end
 
+
   def import tpl_file_path
     Rails.logger.info "Importing into template #{name}"
 
     file = File.open(tpl_file_path,"r")
     content = file.read
-    json = ActiveSupport::JSON.decode(content)
-
-    self.revision      = json["revision"]
-    self.packages_json = (json["packages"] or []).to_json
-    self.errata_json   = (json["errata"] or []).to_json
-    self.products_json = (json["products"] or []).to_json
-    self.host_group_json      = (json["host_group"] or {}).to_json
-    self.kickstart_attrs_json = (json["kickstart_attributes"] or []).to_json
-
-    if not self.content_valid?
-      raise Errors::TemplateContentException.new("Specified template content not found in this environment.")
-    end
+    self.string_import content
 
   ensure
     file.close
   end
+
+
+  def string_import content
+    json = ActiveSupport::JSON.decode(content)
+
+    self.revision = json["revision"]
+    json["products"].collect do |p| self.add_product(p) end if not json["products"].nil?
+    json["packages"].collect do |p| self.add_package(p) end if not json["packages"].nil?
+    json["errata"].collect   do |e| self.add_erratum(e) end if not json["errata"].nil?
+
+    self.host_group = json["host_group"] if not json["host_group"].nil?
+    
+    if not json["kickstart_attributes"].nil?
+      json["kickstart_attributes"].each_pair do |k,v|
+        self.kickstart_attrs[k] = v
+      end
+    end
+
+  end
+
 
   def string_export
     tpl = {
@@ -140,6 +137,34 @@ class SystemTemplate < ActiveRecord::Base
     }
     tpl.to_json
   end
+
+
+  def add_package package_name
+      package = self.find_package_in_env(package_name)
+      if package == nil
+        raise Errors::TemplateContentException.new("Package #{package_name} not found in this environment.")
+      end
+      self.packages << package
+  end
+
+
+  def add_erratum erratum_id
+    erratum = self.find_errata_in_env(erratum_id)
+    if erratum == nil
+      raise Errors::TemplateContentException.new("Errata #{erratum_id} not found in this environment.")
+    end
+    self.errata << erratum
+  end
+
+
+  def add_product product_name
+    product = self.environment.products.find_by_name(product_name)
+    if product == nil
+      raise Errors::TemplateContentException.new("Product #{product_name} not found in this environment.")
+    end
+    self.products << product
+  end
+
 
   def to_json(options={})
      super(options.merge({
@@ -154,6 +179,30 @@ class SystemTemplate < ActiveRecord::Base
 
   protected
 
+  def packages_json= attrs
+    @attributes['packages_json'] = attrs
+  end
+
+  def errata_json= attrs
+    @attributes['errata_json'] = attrs
+  end
+
+
+  def products_json= attrs
+    @attributes['products_json'] = attrs
+  end
+
+
+  def host_groups_json= attrs
+    @attributes['host_groups_json'] = attrs
+  end
+
+
+  def kickstart_attrs_json= attrs
+    @attributes['kickstart_attrs_json'] = attrs
+  end
+
+
   def find_errata_in_env(erratum_id)
 
     self.products.each do |product|
@@ -164,9 +213,9 @@ class SystemTemplate < ActiveRecord::Base
 
       end
     end
-
     nil
   end
+
 
   def find_package_in_env(package_name)
 
@@ -178,7 +227,16 @@ class SystemTemplate < ActiveRecord::Base
 
       end
     end
-
     nil
   end
+
+
+  def attrs_to_json
+    self.products_json = self.products.map(&:name).to_json
+    self.errata_json   = self.errata.map(&:id).to_json
+    self.packages_json = self.packages.map(&:name).to_json
+    self.kickstart_attrs_json = self.kickstart_attrs.to_json
+    self.host_group_json      = self.host_group.to_json
+  end
+
 end
