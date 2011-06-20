404f6aa3c72a976585d1c45e9ab2e306bc6708dd
Tomas Strachota <tstrachota@redhat.com>
2011-06-20 18:06:38 +0200

    %B

--- config/routes.rb
+++ config/routes.rb
@@ -207,7 +207,7 @@ Src::Application.routes.draw do
 
     resources :templates do
       post :promote, :on => :member
-      put :edit_content, :on => :member
+      put :update_content, :on => :member
       post :import, :on => :collection
       get :export, :on => :member
     end
