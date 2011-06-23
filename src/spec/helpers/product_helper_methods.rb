7618118893a92d7f2d67031471589a69826b127e
Adam Price <adprice@redhat.com>
2011-06-23 16:39:21 -0400

    fixed provider url validation and added/fixed tests

--- src/spec/helpers/product_helper_methods.rb
+++ src/spec/helpers/product_helper_methods.rb
@@ -12,7 +12,7 @@ module ProductHelperMethods
 
   def new_test_product org, env
     disable_product_orchestration
-    @provider = Provider.create!({:organization => org, :name => 'provider', :repository_url => "https://url", :provider_type => Provider::REDHAT})
+    @provider = Provider.create!({:organization => org, :name => 'provider', :repository_url => "https://something.url", :provider_type => Provider::REDHAT})
     @p = Product.create!(ProductTestData::SIMPLE_PRODUCT.merge!({:environments => [env], :provider => @provider}))
   end
 
