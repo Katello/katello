9f5ca066760b70032ed34bec358b4a2cf21d4e03
Mike McCune <mmccune@redhat.com>
2011-06-28 10:03:02 -0700

    first stab at the real data

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -9,13 +9,24 @@
 # NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+require 'ostruct'
 
 class SubscriptionsController < ApplicationController
 
   def index
-    #@subscriptions = Subscription.find(:all, :params => { :owner => working_org.org_id })
-    #@subscriptions.sort! { |a,b| a.productName <=> b.productName }
+    all_subs = Candlepin::Owner.pools current_organization.cp_key
     @subscriptions = []
+    all_subs.each do |sub|
+      sub['providedProducts'].each do |cp_product|
+        product = Product.where(:cp_id =>cp_product["productId"]).first
+        # Convert to OpenStruct so we can access fields with dot notation
+        # in the haml. This reduces the code changes we pull in from headpin
+        converted_sub = OpenStruct.new(sub)
+        converted_sub.product = product
+        @subscriptions << converted_sub if !@subscriptions.include? converted_sub
+      end
+    end
+    @subscriptions
   end
 
 end
