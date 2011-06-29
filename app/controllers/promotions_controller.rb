bf1e56ea455a00a66f5366ec2c462f8f8756421c
Justin Sherrill <jsherril@redhat.com>
2011-06-29 17:11:56 -0400

    %B

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -133,7 +133,7 @@ class PromotionsController < ApplicationController
     @next_environment ||= @environment.successor
 
     @path = @next_environment.full_path if @next_environment
-    @path ||= [current_organization.locker]
+    @path ||= @environment.full_path if @environment
     @path = [current_organization.locker] + @path if !@path.first.locker?
 
     if params[:changeset_id]
