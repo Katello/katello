77d38b35cea26705f454792f6192e40e3b0e8432
Justin Sherrill <jsherril@redhat.com>
2011-06-29 10:42:03 -0400

    making promotion actually work again

--- src/public/javascripts/promotion.js
+++ src/public/javascripts/promotion.js
@@ -395,8 +395,15 @@ var changeset_obj = function(data_struct) {
         cancel_review: function(on_success, on_error) {
             change_state("new", on_success, on_error);
         },
-        promote: function() {
-
+        promote: function(on_success, on_error) {
+         $.ajax({
+            contentType:"application/json",
+            type: "POST",
+            url: "/changesets/" + id + "/promote",
+            cache: false,
+            success: function(data) {
+                on_success();
+            }});
         },
         update: function(items, on_success, on_error) {
           var data = [];
@@ -534,10 +541,13 @@ var registerEvents = function(changesetTree){
                 $("#review_cancel").show();
                 button.removeClass("disabled");
                 promotion_page.reset_page();
+                promotion_page.changeset_tree.rerender_content();
             });
         }
         else {
-            cs.promote();
+            cs.promote(function() {
+                
+            });
         }
     });
 
@@ -549,6 +559,7 @@ var registerEvents = function(changesetTree){
             $("#review_cancel").hide();
             $("#review_cancel").removeClass("disabled");
             promotion_page.reset_page();
+            promotion_page.changeset_tree.rerender_content();
         });
     });
 
