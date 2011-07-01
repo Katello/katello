3052737bec9d93c68337a99e9343ee289d9d9baf
Lukas Zapletal <lzap+git@redhat.com>
2011-07-01 11:12:47 +0200

    %B

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+AppConfig.use_cp = false if ENV['NO_CP']
+AppConfig.use_pulp = false if ENV['NO_PULP']
+
 # create basic roles
 superadmin_role = Role.find_or_create_by_name(
   :name => 'Administrator', 
