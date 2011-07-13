9ea89bd462e06b82fd9651292c2efd155936c0f0
Justin Sherrill <jsherril@redhat.com>
2011-07-13 11:20:07 -0400

    %B

--- public/javascripts/katello_client.js
+++ public/javascripts/katello_client.js
@@ -39,78 +39,6 @@ var client_common = {
     }
 };
 
-var role = {
-
-    create: function(name, on_success, on_error) {
-      $.ajax({
-        type: "POST",
-        url: "/roles/",
-        data: { "role":{"name":name}},
-        cache: false,
-        success: on_success,
-        error: on_error
-      });
-    },
-
-    remove_permission: function(role_id, perm_id, on_success, on_error) {
-      $.ajax({
-        type: "PUT",
-        url: "/roles/" + role_id,
-        data: { "role":{
-								"permissions_attributes": {"0":
-											{"id": perm_id,
-											"_destroy":1} }}},
-        cache: false,
-        success: on_success,
-        error: on_error
-      });
-	},
-
-    get_verbs_and_scopes: function(resource_type, on_success, on_error) {
-      $.ajax({
-        type: "GET",
-        url: "/roles/resource_type/" + resource_type  + "/verbs_and_scopes",
-        data: {},
-				dataType: 'json',
-        cache: false,
-        success: on_success,
-        error: on_error
-      });
-	},
-
-    create_or_update_permission: function(method, url, data, on_success, on_error) {
-      $.ajax({
-        type: method,
-        url: url,
-        data: data,
-        cache: false,
-        success: on_success,
-        error: on_error,
-				dataType: "html"
-      });
-	},
-    get_new: function(role_id, url, on_success) {
-      $.ajax({
-        type: "GET",
-        url: url,
-        data: {"role_id":role_id},
-        cache: false,
-        success: on_success,
-        dataType: "html"
-      });
-    },
-    get_existing: function(role_id, perm_id, url, on_success) {
-      $.ajax({
-        type: "GET",
-        url: url,
-        data: {"role_id":role_id, "perm_id":perm_id},
-        cache: false,
-        success: on_success,
-        dataType: "html"
-      });
-    }
-
-};
 
 var search = {
     create_favorite: function(favorite, url, on_success, on_error) {
@@ -149,20 +77,6 @@ var environment = {
     }
 };
 
-
-var change_set = {
-    update_name: function(changeset_id, name, on_success, on_error) {
-        $.ajax({
-          type: "PUT",
-          url: "/changesets/" + changeset_id + "/changesets/name",
-          data: { name: name },
-          cache: false,
-          success: on_success,
-          error: on_error
-        });
-    }
-
-};
 
 var notice = {
     details: function(notice_id, on_success, on_error, data_type) {
