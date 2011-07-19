da29f0f003f7cd78ff07cf23c80fcc988f923639
Lukas Zapletal <lzap+git@redhat.com>
2011-07-19 14:23:41 +0200

    intests - adding (disabled) system unregister test

--- scripts/test/katello-cli-simple-test.sh
+++ scripts/test/katello-cli-simple-test.sh
@@ -177,6 +177,8 @@ SYSTEM_NAME_ADMIN="admin_system_$RAND"
 SYSTEM_NAME_USER="user_system_$RAND"
 test "system register as admin" system register --name="$SYSTEM_NAME_ADMIN" --org="$FIRST_ORG" --environment="$TEST_ENV"
 skip_test "system register as $TEST_USER" -u $TEST_USER -p password system register --name="$SYSTEM_NAME_USER" --org="$FIRST_ORG" --environment="$TEST_ENV"
+test "system info" system info --name="$SYSTEM_NAME_ADMIN" --org="$FIRST_ORG"
+skip_test "system unregister" system unregister --name="$SYSTEM_NAME_ADMIN" --org="$FIRST_ORG"
 test "system list" system list --org="$FIRST_ORG"
 
 #testing distributions
