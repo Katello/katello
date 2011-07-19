0d3d23f6b212918c154a107f4ba80e225c09c5af
Eric D Helms <ehelms@redhat.com>
2011-07-19 09:28:50 -0400

    %B

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -1,14 +1,14 @@
 module LayoutHelper
   def stylesheet(*args)
-    content_for(:head) { stylesheet_link_tag(*args) }
+    args.map { |arg| content_for(:stylesheets) { include_javascripts(arg) } }
   end
 
   def javascript(*args, &block)
     if block
-      content_for(:head) { javascript_tag(block.call()) }
+      content_for(:javascripts) { javascript_tag(block.call()) }
     end
     if args
-      args.map { |arg| content_for(:head) { include_javascripts(arg) } }
+      args.map { |arg| content_for(:javascripts) { include_javascripts(arg) } }
     end
   end
 end
