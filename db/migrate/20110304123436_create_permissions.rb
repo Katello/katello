9cb5932bdb57fb7eb9d84fca52204b99df7f670c
Partha Aji <paji@redhat.com>
2011-07-21 18:03:03 -0400

    %B

--- db/migrate/20110304123436_create_permissions.rb
+++ db/migrate/20110304123436_create_permissions.rb
@@ -1,18 +1,20 @@
 class CreatePermissions < ActiveRecord::Migration
   def self.up
     create_table :permissions do |t|
-      t.belongs_to :role, :resource_type
+      t.belongs_to :role, :resource_type, :organization
       t.boolean :all_tags, :default => false
       t.boolean :all_verbs, :default => false
       t.timestamps
     end
     add_index :permissions, :role_id
-    add_index :permissions, :resource_type_id
+    add_index :permissions, :resource_type_id, :null =>true
+    add_index :permissions, :organization_id, :null =>true
   end
 
   def self.down
     remove_index :permissions, :role_id
     remove_index :permissions, :resource_type_id
+    remove_index :permissions, :organization_id, :null =>true
     drop_table :permissions
   end
 end
