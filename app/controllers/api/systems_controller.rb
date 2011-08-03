59774d514ed0509b04082b8847b2a257f5a496fe
Dmitri Dolguikh <dmitri@redhat.com>
2011-08-03 13:11:50 -0300

    %B

--- app/controllers/api/systems_controller.rb
+++ app/controllers/api/systems_controller.rb
@@ -17,7 +17,7 @@ class Api::SystemsController < Api::ApiController
   before_filter :find_organization, :only => [:create, :index]
   before_filter :find_only_environment, :only => [:create]
   before_filter :find_environment, :only => [:create, :index]
-  before_filter :find_system, :only => [:destroy, :show, :update, :regenerate_identity_certificates, :packages, :upload_package_profile]
+  before_filter :find_system, :only => [:destroy, :show, :update, :regenerate_identity_certificates, :packages, :upload_package_profile, :errata]
 
   def create
     system = System.create!(params.merge({:environment => @environment})).to_json
@@ -57,6 +57,10 @@ class Api::SystemsController < Api::ApiController
   def packages
     packages = @system.packages.sort {|a,b| a.nvrea.downcase <=> b.nvrea.downcase}
     render :partial=>"packages", :locals=>{:system=>@system, :packages => packages}
+  end
+
+  def errata
+    render :json => Pulp::Consumer.errata(@system.uuid)
   end
   
   def upload_package_profile
