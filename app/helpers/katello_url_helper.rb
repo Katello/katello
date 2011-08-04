d868ea855ac2ce8bdc952f67b51e5b3d85b50a55
Shannon Hughes <shughes@redhat.com>
2011-08-04 10:27:47 -0400

    %B

---
+++ app/helpers/katello_url_helper.rb
@@ -0,0 +1,31 @@
+#
+# Copyright 2011 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+module KatelloUrlHelper
+
+  PORT = /(([:]\d+)?)/
+  PROTOCOLS = /(http|https|ftp):\/\//
+  DOMAIN = /([a-z0-9\-]+\.?)*([a-z0-9]{2,})\.[a-z]{2,}/  
+  IPV4 = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/
+  #TODO: ipv6 support
+  #IPV6 = /(?:[0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}/
+  URLREG = /^#{PROTOCOLS}((localhost)|#{DOMAIN}|#{IPV4})#{PORT}(\/.*)?$/ix
+
+  def kipv4? url
+    IPV4.match(url) ? true : false
+  end
+
+  def kurl_valid? url
+    URLREG.match(url) ? true : false
+  end
+
+end
