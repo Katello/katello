b58ff02c30bca1e61ab89d03987975686530b7d7
Eric D Helms <ehelms@redhat.com>
2011-08-05 15:06:44 -0400

    Changesets - Moves breadcrumb creation to centralized helper and modularizes each major breadcrumb generator.

--- src/app/helpers/breadcrumb_helper.rb
+++ src/app/helpers/breadcrumb_helper.rb
@@ -19,68 +19,129 @@ module BreadcrumbHelper
     hash[id][:scrollable] = true if params[:scrollable]
     hash[id][:client_render] = true if params[:client_render]
     hash[id] = hash[id].merge(attributes)
-
-
   end
 
-  def generate_cs_breadcrumb
-    bc = {}
-    add_crumb_node!(bc, "changesets", "", _("Changesets"), [],
-                    {:client_render => true})
-
-    @changesets.each{|cs|
-      add_crumb_node!(bc, changeset_bc_id(cs), "", cs.name, ['changesets'],
-                    {:client_render => true}, {:is_new=>cs.state == Changeset::NEW})
-
-      cs.involved_products.each{|product|
-        #product details 
-        add_crumb_node!(bc, product_cs_bc_id(cs, product), "", product.name, ['changesets', changeset_bc_id(cs)],
+  module ChangesetBreadcrumbs
+    def generate_cs_breadcrumb
+      bc = {}
+      add_crumb_node!(bc, "changesets", "", _("Changesets"), [],
                       {:client_render => true})
-        #packages
-        add_crumb_node!(bc, packages_cs_bc_id(cs, product), "",  _("Packages"),
-                        ['changesets', changeset_bc_id(cs),product_cs_bc_id(cs, product)], {:client_render => true})
-
-        #errata
-        add_crumb_node!(bc, errata_cs_bc_id(cs, product), "",  _("Errata"),
-                        ['changesets', changeset_bc_id(cs), product_cs_bc_id(cs, product)], {:client_render => true})
-
-        #repos
-        add_crumb_node!(bc, repos_cs_bc_id(cs, product), "",  _("Repositories"),
-                        ['changesets', changeset_bc_id(cs), product_cs_bc_id(cs, product)], {:client_render => true})
-
-        #repos
-        add_crumb_node!(bc, deps_cs_bc_id(cs, product), "",  _("Dependencies"),
-                        ['changesets', changeset_bc_id(cs), product_cs_bc_id(cs, product)], {:client_render => true})
-
-
-      }
-    } if @changesets
-    bc.to_json
+  
+      @changesets.each{|cs|
+        add_crumb_node!(bc, changeset_bc_id(cs), "", cs.name, ['changesets'],
+                      {:client_render => true}, {:is_new=>cs.state == Changeset::NEW})
+  
+        cs.involved_products.each{|product|
+          #product details 
+          add_crumb_node!(bc, product_cs_bc_id(cs, product), "", product.name, ['changesets', changeset_bc_id(cs)],
+                        {:client_render => true})
+          #packages
+          add_crumb_node!(bc, packages_cs_bc_id(cs, product), "",  _("Packages"),
+                          ['changesets', changeset_bc_id(cs),product_cs_bc_id(cs, product)], {:client_render => true})
+  
+          #errata
+          add_crumb_node!(bc, errata_cs_bc_id(cs, product), "",  _("Errata"),
+                          ['changesets', changeset_bc_id(cs), product_cs_bc_id(cs, product)], {:client_render => true})
+  
+          #repos
+          add_crumb_node!(bc, repos_cs_bc_id(cs, product), "",  _("Repositories"),
+                          ['changesets', changeset_bc_id(cs), product_cs_bc_id(cs, product)], {:client_render => true})
+  
+          #repos
+          add_crumb_node!(bc, deps_cs_bc_id(cs, product), "",  _("Dependencies"),
+                          ['changesets', changeset_bc_id(cs), product_cs_bc_id(cs, product)], {:client_render => true})
+  
+  
+        }
+      } if @changesets
+      bc.to_json
+    end
+  
+    def changeset_bc_id cs
+      "changeset_#{cs.id}" if cs
+    end
+  
+    def product_cs_bc_id cs, product
+      "product-cs_#{cs.id}_#{product.id}" if cs
+    end
+  
+    def packages_cs_bc_id cs, product
+      "package-cs_#{cs.id}_#{product.id}" if cs
+    end
+  
+    def errata_cs_bc_id cs, product
+      "errata-cs_#{cs.id}_#{product.id}" if cs
+    end
+  
+    def repos_cs_bc_id cs, product
+      "repo-cs_#{cs.id}_#{product.id}" if cs
+    end
+  
+    def deps_cs_bc_id cs, product
+      "deps-cs_#{cs.id}_#{product.id}" if cs
+    end
   end
-
-  def changeset_bc_id cs
-    "changeset_#{cs.id}" if cs
+  
+  module ContentBreadcrumbs
+    def details_path product_id=nil
+      promotion_details_path(@organization.cp_key, @environment.name, :product_id=>product_id)
+    end
+    
+    #  Generates a json structure of the breadcrumb, consisting of a hash map of:
+    #   :id =>  {:url, :name, :trail}   where name is a human readable name, and :trail is 
+    #                                    a list of other :ids creating the trail leading up to it
+    #
+    def generate_content_breadcrumb
+     bc = {}
+     content_crumb_id = "content"
+     products_crumb_id = "products"   
+     
+     add_crumb_node!(bc, content_crumb_id, promotion_details_path(@organization.cp_key, @environment.name) ,
+         _("Content"), [], {:cache =>true, :content=>render(:partial=>"detail")})
+     add_crumb_node!(bc, "all_errata", promotion_errata_path(@organization.cp_key, @environment.name),
+         _("All Errata"), [content_crumb_id], {:scrollable=>true})
+     add_crumb_node!(bc, products_crumb_id, promotion_products_path(@organization.cp_key, @environment.name),
+         _("Products"), [content_crumb_id], {:cache=>true, :content=>render(:partial=>"products", :locals=>{:products=>@products, :changeset=>@changeset})})
+         
+     for prod in @products
+       product_id = product_bc_id(prod)
+       #top of this product
+       add_crumb_node!(bc, product_id, promotion_details_path(@organization.cp_key, @environment.name, :product_id=>prod.id),
+          prod.name, [content_crumb_id,products_crumb_id], {:cache=>true, :content=>render(:partial=>"detail", :locals=>{:product=>prod})})
+          
+       #product,packages
+       add_crumb_node!(bc, packages_bc_id(prod), promotion_packages_path(@organization.cp_key, @environment.name, :product_id=>prod.id, :changeset_id=>changeset_id(@changeset)),
+          _("Packages"), [content_crumb_id,products_crumb_id, product_id], {:scrollable=>true})
+          
+       #product_errata
+       add_crumb_node!(bc, errata_bc_id(prod), promotion_errata_path(@organization.cp_key, @environment.name, :product_id=>prod.id, :changeset_id=>changeset_id(@changeset)),
+          _("Errata"), [content_crumb_id,products_crumb_id, product_id], {:scrollable=>true})
+  
+       #product_repos
+       add_crumb_node!(bc, repo_bc_id(prod), promotion_repos_path(@organization.cp_key, @environment.name, :product_id=>prod.id, :changeset_id=>changeset_id(@changeset)),
+                       _("Repos"), [content_crumb_id,products_crumb_id, product_id], {:scrollable=>true})
+     end   
+     bc.to_json
+    end
+  
+    def product_bc_id product
+      "details_#{product.id}"
+    end
+    
+    def packages_bc_id product
+      "packages_#{product.id}"
+    end
+    
+    def errata_bc_id product
+      "errata_#{product.id}"
+    end
+  
+    def repo_bc_id product
+      "repo_#{product.id}"
+    end
+  
+    def changeset_id cs
+      return cs.id if cs
+    end
   end
-
-  def product_cs_bc_id cs, product
-    "product-cs_#{cs.id}_#{product.id}" if cs
-  end
-
-  def packages_cs_bc_id cs, product
-    "package-cs_#{cs.id}_#{product.id}" if cs
-  end
-
-  def errata_cs_bc_id cs, product
-    "errata-cs_#{cs.id}_#{product.id}" if cs
-  end
-
-  def repos_cs_bc_id cs, product
-    "repo-cs_#{cs.id}_#{product.id}" if cs
-  end
-
-  def deps_cs_bc_id cs, product
-    "deps-cs_#{cs.id}_#{product.id}" if cs
-  end
-
-
 end
