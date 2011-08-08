17ac4e4d7e4ced34d9e6931aafb3d3fe9e7b08a7
Shannon Hughes <shughes@redhat.com>
2011-08-08 17:30:24 -0400

    720442: fixing system refresh on name update

--- src/app/controllers/systems_controller.rb
+++ src/app/controllers/systems_controller.rb
@@ -116,7 +116,7 @@ class SystemsController < ApplicationController
 
   def show
     system = System.find(params[:id])
-    render :partial=>"common/list_update", :locals=>{:item=>system, :accessor=>"id", :columns=>['name', 'ip', 'kernel']}
+    render :partial=>"systems/list_system", :locals=>{:item=>system, :accessor=>"id", :columns=> COLUMNS.keys}
   end
   
   def section_id
