ac876e21b6df50ea7e6a86d25e3e632460ee620b
Adam Price <adprice@redhat.com>
2011-08-08 10:51:07 -0400

    adding 'sudo' to rhsm registration test
    if you have NOPASSWD set in visudo then you should no longer be prompted
    for password when running katello-cli-simple-test.sh

--- scripts/test/cli_tests/rhsm.sh
+++ scripts/test/cli_tests/rhsm.sh
@@ -2,6 +2,6 @@
 
 # testing registration from rhsm
 (which subscription-manager >/dev/null && \
-  test_own_cmd "rhsm registration" subscription-manager register --username=$USER --password=$PASSWORD \
+  test_own_cmd "rhsm registration" sudo subscription-manager register --username=$USER --password=$PASSWORD \
   --org=$FIRST_ORG --environment=$TEST_ENV_3 --name=$HOSTNAME --force) || \
   skip_test "rhsm registration" "subscription-manager command not found"
