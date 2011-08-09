df95c8181d3344bbed32b4156f6ffe0e311ef9b6
Brad Buckingham <bbuckingham@redhat.com>
2011-08-09 13:29:59 -0400

    Distributions - add empty distribution selection to Promotions
    This commit is just putting in place initial logic to display
    Distributions from the Promotions page.  When selected, it will
    simply indicate that none are available.

--- app/helpers/promotions_helper.rb
+++ app/helpers/promotions_helper.rb
@@ -52,15 +52,13 @@ module PromotionsHelper
      #product_repos
      add_crumb_node!(bc, repo_bc_id(prod), promotion_repos_path(@organization.cp_key, @environment.name, :product_id=>prod.id, :changeset_id=>changeset_id(@changeset)),
                      _("Repos"), [content_crumb_id,products_crumb_id, product_id], {:scrollable=>true})
-   end   
+
+     #product_distributions
+     add_crumb_node!(bc, distribution_bc_id(prod), promotion_distributions_path(@organization.cp_key, @environment.name, :product_id=>prod.id, :changeset_id=>changeset_id(@changeset)),
+                     _("Distributions"), [content_crumb_id,products_crumb_id, product_id], {:scrollable=>true})
+   end
    bc.to_json
   end
-
-
-  
-
-
-
 
   def product_bc_id product
     "details_#{product.id}"
@@ -76,6 +74,10 @@ module PromotionsHelper
 
   def repo_bc_id product
     "repo_#{product.id}"
+  end
+
+  def distribution_bc_id product
+    "distribution_#{product.id}"
   end
 
   def changeset_id cs
