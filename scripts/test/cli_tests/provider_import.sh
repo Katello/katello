56b3ffb86e79ddd057ab60c9aec74e21ed1e74ab
Lukas Zapletal <lzap+git@redhat.com>
2011-08-09 12:34:20 +0200

    fixing one more katello-cli test

--- scripts/test/cli_tests/provider_import.sh
+++ scripts/test/cli_tests/provider_import.sh
@@ -7,6 +7,6 @@ IMPORT_ORG="Import_Org_$RAND"
 IMPORT_PROV="Import_Prov_$RAND"
 test "org create" org create --name=$IMPORT_ORG
 test "provider create" provider create --org="$IMPORT_ORG" --name="$IMPORT_PROV" --type=redhat --url="https://example.com/path/"
-#test "provider import_manifest" provider import_manifest --org="$IMPORT_ORG" --name="$IMPORT_PROV" --file=$DIR/export-manifest.zip
+test "provider import_manifest" provider import_manifest --org="$IMPORT_ORG" --name="$IMPORT_PROV" --file=$DIR/export-manifest.zip
 test "provider delete" provider delete --org="$IMPORT_ORG" --name="$IMPORT_PROV"
 test "org delete" org delete --name=$IMPORT_ORG
