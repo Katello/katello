e3e941b079b1e9cdeeb019546111b7e2a897a03d
Shannon Hughes <shughes@redhat.com>
2011-08-11 09:31:49 -0400

    %B

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 require 'spec_helper'
+include OrchestrationHelper
 
 describe User do
 
@@ -34,6 +35,15 @@ describe User do
     it "have its own role" do
       #pending "implement own_role functionality"
       @user.own_role.should_not be_nil
+    end
+
+    it "have permission for two orgs" do
+      disable_org_orchestration
+      org = Organization.create!(:name => 'test_organization', :cp_key => 'test_organization')
+      moreorg = Organization.create!(:name => 'another_test_organization', :cp_key => 'another_test_organization')
+      @user.own_role.allow([:read], :providers, nil, org)
+      @user.own_role.allow([:read], :providers, nil, moreorg)
+      @user.allowed_organizations.size.should == 2
     end
 
     specify { @user.cp_oauth_header.should == {'cp-user' => @user.username}}
