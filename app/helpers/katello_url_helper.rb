c2b58a6cbb498684d37e799ccbfde7e34d240061
Shannon Hughes <shughes@redhat.com>
2011-08-22 14:29:58 -0400

    %B

--- app/helpers/katello_url_helper.rb
+++ app/helpers/katello_url_helper.rb
@@ -13,8 +13,8 @@
 module KatelloUrlHelper
 
   PORT = /(([:]\d{1,5})?)/
-  PROTOCOLS = /(https?|ftp):\/\// 
-  FILEPREFIX = /(^file:\/\/)|^\// # is this a file based url
+  PROTOCOLS = /(https?|ftp):\/\//ix 
+  FILEPREFIX = /(^file:\/\/)|^\//ix # is this a file based url
   DOMAIN = /([a-z0-9\-]+\.?)*([a-z0-9]{2,})\.[a-z]{2,}/  
   IPV4 = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/
   #TODO: ipv6 support
