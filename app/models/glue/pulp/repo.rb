75409411063cb78145ec5f89540e3ca04ee8e248
Ivan Necas <inecas@redhat.com>
2011-08-23 12:00:29 +0200

    %B

--- app/models/glue/pulp/repo.rb
+++ app/models/glue/pulp/repo.rb
@@ -93,7 +93,7 @@ class Glue::Pulp::Repo
 
 
   def get_clone env
-    Glue::Pulp::Repo.find(Glue::Pulp::Repos.clone_repo_id(self.id, env.name))
+    Glue::Pulp::Repo.find(Glue::Pulp::Repos.clone_repo_id(self, env))
   end
 
   def get_cloned_in env
@@ -197,7 +197,7 @@ class Glue::Pulp::Repo
 
   def promote(to_environment, product)
     cloned = Glue::Pulp::Repo.new
-    cloned.id = Glue::Pulp::Repos.clone_repo_id(id, to_environment.name)
+    cloned.id = Glue::Pulp::Repos.clone_repo_id(self, to_environment)
     cloned.relative_path = Glue::Pulp::Repos.clone_repo_path(self, to_environment)
     cloned.arch = arch
     cloned.name = name
