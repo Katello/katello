d32b9c8289b3cbd8a651b7852dbc32698763e133
Justin Sherrill <jsherril@redhat.com>
2011-08-25 12:10:30 -0400

    making sure sync plans page only shows readable products

--- src/app/controllers/sync_schedules_controller.rb
+++ src/app/controllers/sync_schedules_controller.rb
@@ -29,7 +29,7 @@ class SyncSchedulesController < ApplicationController
   def index
 
     @organization = current_organization
-    rproducts = @organization.locker.products.reject { |p| p.productContent.empty? }
+    rproducts = @organization.locker.products.readable(@organization).reject { |p| p.productContent.empty? }
     @products = rproducts.sort { |p1,p2| p1.name <=> p2.name }
 
     @plans = SyncPlan.where(:organization_id => current_organization.id)
