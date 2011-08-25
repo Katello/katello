c431975aff9668a90a6c3f5c38164b934da6922d
Ivan Necas <inecas@redhat.com>
2011-08-25 13:08:24 +0200

    %B

--- spec/helpers/login_helper_methods.rb
+++ spec/helpers/login_helper_methods.rb
@@ -13,9 +13,12 @@
 module LoginHelperMethods
   def login_user options={}
     if options[:mock] == false
-      @user = User.create( :username => "foo-user", :password => "password", :page_size=>25 )
-
-      @permission = Permission.create!(:role =>@user.roles.first, :all_types => true, :name => "superadmin")
+      if options[:user]
+        @user = options[:user]
+      else
+        @user = User.create( :username => "foo-user", :password => "password", :page_size=>25 )
+        @permission = Permission.create!(:role =>@user.roles.first, :all_types => true, :name => "superadmin")
+      end
 
       request.env['warden'] = mock(Warden, :user => @user, :authenticate => @user, :authenticate! => @user)
       controller.stub!(:require_org).and_return({})
