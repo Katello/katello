847e24fc2d10b22d8871c7d7e709d7b41729024a
Partha Aji <paji@redhat.com>
2011-09-02 11:13:57 -0400

    Removed 'tags' table for we could just deal with that using unique tag ids. To avoid the dreaded "explicit cast" exception when joining tags to entity ids table in postgres (example - Environments), we need tags to be integers. All our tags at the present time are integers anyway so this seems an easy enough change.

--- src/db/migrate/20110307082853_create_permission_tag.rb
+++ src/db/migrate/20110307082853_create_permission_tag.rb
@@ -1,15 +1,13 @@
 class CreatePermissionTag < ActiveRecord::Migration
   def self.up
-    create_table :permissions_tags, :id => false do |t|
-      t.belongs_to :permission, :tag
+    create_table :permission_tags do |t|
+      t.integer :permission_id
+      t.integer :tag_id
+      t.timestamps
     end
-    add_index :permissions_tags, :permission_id
-    add_index :permissions_tags, :tag_id
   end
 
   def self.down
-    remove_index :permissions_tags, :tag_id
-    remove_index :permissions_tags, :permission_id
-    drop_table :permissions_tags
+    drop_table :permission_tags
   end
 end
