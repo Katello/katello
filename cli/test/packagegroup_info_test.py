97a5d11f4000ca1f152fd9a3a46910c3dbd65b6c
Ivan Necas <inecas@redhat.com>
2011-09-05 11:07:37 +0200

    Cli unit tests for package groups

---
+++ cli/test/packagegroup_info_test.py
@@ -0,0 +1,60 @@
+import unittest
+from mock import Mock
+import urlparse
+from cli_test_utils import CLIOptionTestCase, CLIActionTestCase
+import test_data
+
+import katello.client.core.packagegroup
+from katello.client.core.packagegroup import Info
+
+class RequiredCLIOptionsTests(CLIOptionTestCase):
+    def setUp(self):
+        self.set_action(Info())
+        self.mock_options()
+
+    def test_missing_id_and_repoid_generates_error(self):
+        self.assertRaises(Exception, self.action.process_options, ['info'])
+
+    def test_missing_id_generates_error(self):
+      self.assertRaises(Exception, self.action.process_options,
+                        ['info', '--repoid=123'])
+
+    def test_missing_repoid_generates_error(self):
+      self.assertRaises(Exception, self.action.process_options,
+                        ['info', '--id=123'])
+
+    def test_no_error_if_required_options_provided(self):
+        self.action.process_options(['info', '--repoid=123','--id=123'])
+        self.assertEqual(len(self.action.optErrors), 0)
+
+
+class PackageGroupInfoTest(CLIActionTestCase):
+
+    REPO = test_data.REPOS[0]
+    PACKAGE_GROUP = test_data.PACKAGE_GROUPS["123"]
+
+    OPTIONS = {
+        'repoid': REPO['id'],
+        'id': PACKAGE_GROUP['id'],
+    }
+
+    def setUp(self):
+        self.set_action(Info())
+        self.set_module(katello.client.core.packagegroup)
+
+        self.mock_options(self.OPTIONS)
+        self.mock_printer()
+
+        self.mock(self.action.api, 'packagegroups', test_data.PACKAGE_GROUPS)
+
+    def tearDown(self):
+        self.restore_mocks()
+
+    def test_it_finds_package_groups_by_repo(self):
+        self.mock_options(self.OPTIONS)
+        self.action.run()
+        self.action.api.packagegroups.assert_called_once_with(self.REPO['id'])
+
+    def test_it_prints_package_groups(self):
+        self.action.run()
+        self.action.printer.printItem.assert_called_once_with(self.PACKAGE_GROUP)
--- cli/test/test_data.py
+++ cli/test/test_data.py
@@ -535,4 +535,25 @@ REPOS = [
     }
 ]
 
+PACKAGE_GROUPS = {
+"123":
+{"name": "katello",
+  "conditional_package_names": {},
+  "mandatory_package_names": [],
+  "default": True,
+  "_id": "123",
+  "langonly": None,
+  "id": "123",
+  "immutable": False,
+  "optional_package_names": [],
+  "default_package_names": ["pulp-test-package-0.2.1-1.fc11.x86_64.rpm"],
+  "translated_description": {},
+  "user_visible": True,
+  "display_order": 1024,
+  "repo_defined": False,
+  "description": "Katello related packages",
+  "translated_name": {}
+  }
+}
+
 
