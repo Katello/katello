f6fdd39145f0278a0f5b1a64368439d3ef3a4059
Jason E. Rist <jrist@redhat.com>
2011-09-13 06:24:39 -0600

    Subscriptions udpates, packages fix, ui spinner fix, universal KT object for applying subs.

--- src/app/controllers/systems_controller.rb
+++ src/app/controllers/systems_controller.rb
@@ -98,10 +98,10 @@ class SystemsController < ApplicationController
   def subscriptions
     consumed_pools = sys_consumed_pools
     avail_pools = sys_available_pools
-
-    render :partial=>"subscriptions", :layout => "tupane_layout", 
+    sockets = eval @system.facts['cpu.cpu_socket(s)']
+    render :partial=>"subscriptions", :layout => "tupane_layout",
                                       :locals=>{:system=>@system, :avail_subs => avail_pools,
-                                                :consumed_subs => consumed_pools, 
+                                                :consumed_subs => consumed_pools, :sockets=>sockets,
                                                 :editable=>@system.editable?}
   end
 
