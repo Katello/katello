80a140178073ab6dd49b55a1ad974679a14c1d1d
Ivan Necas <inecas@redhat.com>
2011-09-13 13:59:24 +0200

    %B

--- app/helpers/katello_url_helper.rb
+++ app/helpers/katello_url_helper.rb
@@ -12,15 +12,18 @@
 
 module KatelloUrlHelper
 
-  PORT = /(([:]\d{1,5})?)/
-  PROTOCOLS = /(https?|ftp):\/\//ix 
-  FILEPREFIX = /(^file:\/\/)|^\//ix # is this a file based url
-  DOMAIN = /([a-z0-9\-]+\.?)*([a-z0-9]{2,})\.[a-z]{2,}/  
-  IPV4 = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/
-  #TODO: ipv6 support
-  #IPV6 = /(?:[0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}/
-  URLREG = /^#{PROTOCOLS}((localhost)|#{DOMAIN}|#{IPV4})#{PORT}(\/.*)?$/ix
-  FILEREG = /#{FILEPREFIX}([\w-]*\/?)*/ix # match file based urls
+  unless defined? CONSTANTS_DEFINED
+    PORT = /(([:]\d{1,5})?)/
+    PROTOCOLS = /(https?|ftp):\/\//ix 
+    FILEPREFIX = /(^file:\/\/)|^\//ix # is this a file based url
+    DOMAIN = /([a-z0-9\-]+\.?)*([a-z0-9]{2,})\.[a-z]{2,}/  
+    IPV4 = /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/
+    #TODO: ipv6 support
+    #IPV6 = /(?:[0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}/
+    URLREG = /^#{PROTOCOLS}((localhost)|#{DOMAIN}|#{IPV4})#{PORT}(\/.*)?$/ix
+    FILEREG = /#{FILEPREFIX}([\w-]*\/?)*/ix # match file based urls
+    CONSTANTS_DEFINED = true
+  end
 
   def kipv4? url
     IPV4.match(url) ? true : false
