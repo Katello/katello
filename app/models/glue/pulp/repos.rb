fad8e45820b43f2af958bc5bfac5db8bd1e40a52
Ivan Necas <inecas@redhat.com>
2011-09-23 17:42:44 +0200

    import-stage-manifest - use pulp-valid names for repos

--- app/models/glue/pulp/repos.rb
+++ app/models/glue/pulp/repos.rb
@@ -248,7 +248,7 @@ module Glue::Pulp::Repos
             Rails.logger.error("Pulp does not support arch '#{arch}'")
             next
           end
-          repo_name = "#{pc.content.name} #{arch}"
+          repo_name = "#{pc.content.name} #{arch}".gsub(/[^a-z0-9\-_ ]/i,"")
           attrs = {:id => repo_id(repo_name),
                                       :arch => arch,
                                       :relative_path => Glue::Pulp::Repos.repo_path(self.locker, self, pc.content.name),
