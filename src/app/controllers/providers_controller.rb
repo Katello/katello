8e91fd9640103fd16060ffa0a048edde0f0f8b16
Partha Aji <paji@redhat.com>
2011-09-30 17:48:27 -0400

    Got pretty much the providers functionality done with this

--- src/app/controllers/providers_controller.rb
+++ src/app/controllers/providers_controller.rb
@@ -46,9 +46,7 @@ class ProvidersController < ApplicationController
       :products_repos => read_test,
 
       :redhat_provider =>redhat_provider_read_test,
-      :redhat_update =>redhat_provider_read_test,
-      :update_subscriptions => redhat_provider_edit_test,
-      :subscriptions => redhat_provider_read_test
+      :update_redhat_provider => redhat_provider_edit_test,
     }
   end
 
@@ -76,7 +74,8 @@ class ProvidersController < ApplicationController
         Rails.logger.error "error uploading subscriptions."
         Rails.logger.error error
         Rails.logger.error error.backtrace.join("\n")
-        render :nothing => true, :status => :bad_request and return
+        setup_subs
+        render :template =>"providers/redhat_provider", :status => :bad_request and return
       end
     end
     redhat_provider
@@ -87,26 +86,16 @@ class ProvidersController < ApplicationController
     # We default to none imported until we can properly poll Candlepin for status of the import
     @subscriptions = [{'productName' => _("None Imported"), "consumed" => "0", "available" => "0"}]
     begin
-      all_subs = Candlepin::Owner.pools @provider.organization.cp_key
-      @subscriptions = []
-      all_subs.each do |sub|
-        sub['providedProducts'].each do |cp_product|
-          product = Product.where(:cp_id =>cp_product["productId"]).first
-          if product and product.provider == @provider
-            @subscriptions << sub if !@subscriptions.include? sub
-          end
-        end
-      end
-      
+      setup_subs
     rescue Exception => error
+      errors _("Unable to retrieve your Subscription Manifest"), {:synchronous_request => false}
       Rails.logger.error "Error fetching subscriptions from Candlepin"
       Rails.logger.error error
       Rails.logger.error error.backtrace.join("\n")
-      render :nothing => true, :status => :bad_request and return
+      render :template =>"providers/redhat_provider", :status => :bad_request and return
     end
-#    render :partial => "subscriptions", :layout => "tupane_layout", :locals => {:provider => @provider}
+     render :template =>"providers/redhat_provider"
   end
-
 
   def index
     begin
@@ -142,9 +131,10 @@ class ProvidersController < ApplicationController
 
   def create
     begin
-      @provider = Provider.create! params[:provider].merge({:organization => current_organization})
+      @provider = Provider.create! params[:provider].merge({:provider_type => Provider::CUSTOM,
+                                                                    :organization => current_organization})
       notice _("Provider '#{@provider['name']}' was created.")
-      render :partial=>"common/list_item", :locals=>{:item=>@provider, :accessor=>"id", :columns=>['name', 'provider_type'], :name=>controller_display_name}
+      render :partial=>"common/list_item", :locals=>{:item=>@provider, :accessor=>"id", :columns=>['name'], :name=>controller_display_name}
 
     rescue Exception => error
       Rails.logger.error error.to_s
@@ -229,5 +219,21 @@ class ProvidersController < ApplicationController
 
   def search_filter
     @filter = {:organization_id => current_organization}
+  end
+
+  def setup_subs
+    @provider = current_organization.redhat_provider
+    # We default to none imported until we can properly poll Candlepin for status of the import
+    @subscriptions = [{'productName' => _("None Imported"), "consumed" => "0", "available" => "0"}]
+    all_subs = Candlepin::Owner.pools @provider.organization.cp_key
+    @subscriptions = []
+    all_subs.each do |sub|
+      sub['providedProducts'].each do |cp_product|
+        product = Product.where(:cp_id =>cp_product["productId"]).first
+        if product and product.provider == @provider
+          @subscriptions << sub if !@subscriptions.include? sub
+        end
+      end
+    end
   end
 end
