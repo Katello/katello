c7a55aae4b63054fb3dc34880b98a1518705f514
Tomas Strachota <tstrachota@redhat.com>
2011-09-30 16:04:27 +0200

    templates - package groups and categories identified by name -repo ids and category/group ids removed

--- src/app/models/system_template_pg_category.rb
+++ src/app/models/system_template_pg_category.rb
@@ -12,19 +12,16 @@
 
 class PgCategoryValidator < ActiveModel::Validator
   def validate(record)
-    repo = Glue::Pulp::Repo.new(:id => record.repo_id)
-    unless repo.package_group_categories(:id => record.pg_category_id).first
-      record.errors[:base] <<  (_("Package group category '%s' doesn't exist in repo '%s'") % [record.pg_category_id, record.repo_id])
-    end
+    #repo = Glue::Pulp::Repo.new(:id => record.repo_id)
+    #unless repo.package_group_categories(:id => record.pg_category_id).first
+    #  record.errors[:base] <<  (_("Package group category '%s' doesn't exist in repo '%s'") % [record.pg_category_id, record.repo_id])
+    #end
   end
 end
 
 class SystemTemplatePgCategory < ActiveRecord::Base
   belongs_to :system_template, :inverse_of => :pg_categories
   validates_with PgCategoryValidator
-  validates_uniqueness_of [:pg_category_id], :scope =>  [:system_template_id, :repo_id], :message => _("is already in the template")
+  validates_uniqueness_of [:name], :scope =>  :system_template_id, :message => _("is already in the template")
 
-  def export_hash
-    {:id => self.pg_category_id, :repo => self.repo_id}
-  end
 end
