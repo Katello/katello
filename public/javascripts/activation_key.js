04112ee7b07928cf055d5889a3c908cf331cb21a
Brad Buckingham <bbuckingham@redhat.com>
2011-09-30 18:08:19 -0400

    %B

--- public/javascripts/activation_key.js
+++ public/javascripts/activation_key.js
@@ -14,124 +14,57 @@
 $(document).ready(function() {
 
     KT.panel.set_expand_cb(function() {
-        activation_key.reset_env_select();
+        KT.activation_key.reset_env_select();
     });
 
     $('#new_activation_key').live('submit', function(e) {
         e.preventDefault();
-        activation_key.create_key($(this));
+        KT.activation_key.create_key($(this));
     });
 
     $('#remove_key').live('click', function(e) {
         e.preventDefault();
-        activation_key.delete_key($(this));
+        KT.activation_key.delete_key($(this));
     });
 
     $('#save_key').live('submit', function(e) {
         e.preventDefault();
-
-        activation_key.disable_buttons();
-
-        $(this).ajaxSubmit({
-         success: function(data) {
-             activation_key.highlight_system_templates(false);
-             activation_key.enable_buttons();
-         }, error: function(e) {
-             activation_key.highlight_system_templates(false);
-             activation_key.enable_buttons();
-         }});
+        KT.activation_key.save_key($(this));
     });
 
     $('#cancel_key').live('click', function(e) {
         e.preventDefault();
-
-        var url = $('#cancel_key').attr('data-url');
-        if (url !== undefined) {
-            activation_key.disable_buttons();
-
-            $.ajax({
-                type: "GET",
-                url: url,
-                cache: false,
-                success: function(response) {
-                    $('.panel-content').html(response);
-                    activation_key.initialize_edit();
-                },
-                error: function(data) {
-                    activation_key.initialize_edit();
-                }
-            });
-        }
+        KT.activation_key.cancel_key($(this));
     });
-
 
     //Set the callback on the environment selector
     env_select.click_callback = function(env_id) {
-        activation_key.save_selected_environment(env_id);
-        activation_key.get_system_templates();
-        activation_key.get_products();
+        KT.activation_key.save_selected_environment(env_id);
+        KT.activation_key.get_system_templates();
+        KT.activation_key.get_products();
     };
 
     $('#activation_key_system_template_id').live('change', function() {
-        activation_key.highlight_system_templates(false);
+        KT.activation_key.highlight_system_templates(false);
     });
 
     $('#go_to_available_subscriptions').live('click', function(e) {
         e.preventDefault();
-        KT.activation_key.goToAvailableSubscriptions();
+        KT.activation_key.go_to_available_subscriptions();
     });
 
     $('.clickable.product_family').live('click', function() {
-        // user clicked a product family
-        var family = $(this).closest('tr').attr('data-family_begin');
-
-        // show/hide the elements that are part of the family
-        $('tr[data-in_family="'+family+'"]').slideToggle();
-
-        // toggle the expand/collapse arrow
-        var arrow = $(this).find('img');
-        if(arrow.attr("src").indexOf("collapsed") === -1){
-            arrow.attr("src", "images/icons/expander-collapsed.png");
-        } else {
-            arrow.attr("src", "images/icons/expander-expanded.png");
-        }
-
+        KT.activation_key.toggle_family($(this));
     });
 
     // the parent (product family) was checked, so check all children
     $('.family_checkbox').live('click', function() {
-        var family = $(this).closest('tr'),
-            family_name = family.attr('data-family_begin');
-        family.siblings('tr[data-in_family="'+family_name+'"]').find('input:checkbox').attr('checked', this.checked);
+        KT.activation_key.toggle_family_checkboxes($(this), this.checked);
     });
 
     // a child was checked, so update the parent (product family), if needed
     $('#subscription_form input[type="checkbox"]').live('click', function() {
-        // check to see if the child is evan part of a product family...
-        var sub = $(this).closest('tr'),
-            in_family = sub.attr('data-in_family');
-        if (in_family !== undefined) {
-            // locate the family
-            var family = sub.prevAll('tr[data-family_begin="'+in_family+'"]');
-            if (family !== undefined) {
-                // retrieve the checkboxes for the family and siblings
-                var family_cbx = family.find('input:checkbox'),
-                    sibling_cbxs = family.siblings('tr[data-in_family="'+in_family+'"]').find('input:checkbox'),
-                    total = sibling_cbxs.length,
-                    checked = 0;
-
-                sibling_cbxs.each( function() { if (this.checked) checked++; });
-                if (total == checked) {
-                    family_cbx.attr('checked', true);
-                }
-                else if (checked > 0) {
-                    family_cbx.attr('checked', false);
-                }
-                else {
-                    family_cbx.attr('checked', false);
-                }
-            }
-        }
+        KT.activation_key.toggle_parent_checkbox($(this));
     });
 
      $('input[id^=filter]').live('change, keyup', function(){
@@ -150,128 +83,8 @@ $(document).ready(function() {
      });
 });
 
-var activation_key = (function() {
-    return {
-        initialize_edit: function() {
-            activation_key.reset_env_select();
-            activation_key.enable_buttons();
-            activation_key.highlight_system_templates(false);
-        },
-        reset_env_select: function() {
-            $('#path-expanded').hide();
-            env_select.reset_hover();
-            env_select.recalc_scroll();
-        },
-        create_key : function(data) {
-            var button = data.find('input[type|="submit"]');
-            button.attr("disabled","disabled");
-            data.ajaxSubmit({
-                success: function(data) {
-                    list.add(data);
-                    KT.panel.closePanel($('#panel'));
-                    KT.panel.select_item(list.last_child().attr("id"));
-                },
-                error: function(e) {
-                    button.removeAttr('disabled');
-                }
-            });                       
-        },
-        delete_key : function(data) {
-            var answer = confirm(data.attr('data-confirm-text'));
-            if (answer) {
-                $.ajax({
-                    type: "DELETE",
-                    url: data.attr('data-url'),
-                    cache: false,
-                    success: function() {
-                        KT.panel.closeSubPanel($('#subpanel'));
-                        KT.panel.closePanel($('#panel'));
-                        list.remove(data.attr("data-id").replace(/ /g, '_'));
-                    }
-                });
-            }
-        },
-        get_system_templates : function() {
-            // this function will retrieve the system templates associated with a given environment and
-            // update the page content, as appropriate
-            var url = $('.path_link.active').attr('data-templates_url');
-
-            activation_key.disable_buttons();
-            $.ajax({
-                type: "GET",
-                url: url,
-                cache: false,
-                success: function(response) {
-                    // update the appropriate content on the page
-                    var options = '';
-
-                    // create an html option list using the response
-                    options += '<option value="">' + i18n.noTemplate + '</option>';
-                    for (var i = 0; i < response.length; i++) {
-                        options += '<option value="' + response[i].id + '">' + response[i].name + '</option>';
-                    }
-
-                    // add the options to the system template select... this select exists on an insert form
-                    // or as part of the environment edit dialog
-                    $("#activation_key_system_template_id").html(options);
-
-                    activation_key.highlight_system_templates(true);
-                    activation_key.enable_buttons();
-                },
-                error: function(data) {
-                    activation_key.enable_buttons();
-                }
-            });
-        },
-        get_products : function() {
-            // this function will retrieve the products associated with a given environment and
-            // update the products box with the results
-            var url = $('.path_link.active').attr('data-products_url');
-            if (url !== undefined) {
-                activation_key.disable_buttons();
-                $.ajax({
-                    type: "GET",
-                    url: url,
-                    cache: false,
-                    success: function(response) {
-                        $('.productsbox').html(response);
-                        activation_key.enable_buttons();
-                    },
-                    error: function(data) {
-                        activation_key.enable_buttons();
-                    }
-                });
-            }
-        },
-        save_selected_environment : function(env_id) {
-            // save the id of the env selected
-            $("#activation_key_environment_id").attr('value', env_id);
-        },
-        disable_buttons : function() {
-            $('#cancel_key').attr("disabled","disabled");
-            $('input[id^=save_key]').attr("disabled","disabled");
-        },
-        enable_buttons : function() {
-            $('#cancel_key').removeAttr('disabled');
-            $('input[id^=save_key]').removeAttr('disabled');
-        },
-        highlight_system_templates : function(add_highlight) {
-            var select_input = $('#activation_key_system_template_id');
-            if (add_highlight) {
-                if( !select_input.next('span').hasClass('highlight_input_text')) {
-                    select_input.addClass('highlight_input');
-                    select_input.after('<span class ="highlight_input_text">' + i18n.update_template + '</span>');
-                }
-            } else {
-                select_input.removeClass('highlight_input');
-                $('.highlight_input_text').remove();
-            }
-        }
-    }
-})();
-
-KT.activation_key = function() {
-    var subscriptionSetup = function(){
+KT.activation_key = (function($) {
+    var subscription_setup = function(){
         var subbutton = $('#subscription_submit_button');
         var fakesubbutton = $('#fake_subscription_submit_button');
         var subcheckboxes = $('#subscription_form input[type="checkbox"]');
@@ -296,10 +109,7 @@ KT.activation_key = function() {
 
         subbutton.unbind('click').click(disableSubmit);
     },
-    disableSubmit = function() {
-        $('#subscription_submit_button').attr("disabled", "disabled");
-    },
-    goToAvailableSubscriptions = function() {
+    go_to_available_subscriptions = function() {
         var url = $('#go_to_available_subscriptions').attr('href');
         $.ajax({
             cache: 'false',
@@ -311,9 +121,220 @@ KT.activation_key = function() {
                 KT.panel.panelResize($('#panel_main'), false);
             }
         });
+    },
+    initialize_edit = function() {
+        reset_env_select();
+        enable_buttons();
+        highlight_system_templates(false);
+    },
+    reset_env_select = function() {
+        $('#path-expanded').hide();
+        env_select.reset_hover();
+        env_select.recalc_scroll();
+    },
+    create_key = function(data) {
+        disable_buttons();
+        data.ajaxSubmit({
+            success: function(data) {
+                list.add(data);
+                KT.panel.closePanel($('#panel'));
+                KT.panel.select_item(list.last_child().attr("id"));
+            },
+            error: function(e) {
+                enable_buttons();
+            }
+        });
+    },
+    delete_key = function(data) {
+        var answer = confirm(data.attr('data-confirm-text'));
+        if (answer) {
+            $.ajax({
+                type: "DELETE",
+                url: data.attr('data-url'),
+                cache: false,
+                success: function() {
+                    KT.panel.closeSubPanel($('#subpanel'));
+                    KT.panel.closePanel($('#panel'));
+                    list.remove(data.attr("data-id").replace(/ /g, '_'));
+                }
+            });
+        }
+    },
+    save_key = function(data) {
+        disable_buttons();
+
+        data.ajaxSubmit({
+         success: function(data) {
+             highlight_system_templates(false);
+             enable_buttons();
+         }, error: function(e) {
+             highlight_system_templates(false);
+             enable_buttons();
+         }});
+    },
+    cancel_key = function(data) {
+        var url = $('#cancel_key').attr('data-url');
+        if (url !== undefined) {
+            disable_buttons();
+
+            $.ajax({
+                type: "GET",
+                url: url,
+                cache: false,
+                success: function(response) {
+                    $('.panel-content').html(response);
+                    initialize_edit();
+                },
+                error: function(data) {
+                    initialize_edit();
+                }
+            });
+        }
+    },
+    toggle_family = function(data) {
+        // user clicked a product family
+        var family = data.closest('tr').attr('data-family_begin');
+
+        // show/hide the elements that are part of the family
+        $('tr[data-in_family="'+family+'"]').slideToggle();
+
+        // toggle the expand/collapse arrow
+        var arrow = data.find('img');
+        if(arrow.attr("src").indexOf("collapsed") === -1){
+            arrow.attr("src", "images/icons/expander-collapsed.png");
+        } else {
+            arrow.attr("src", "images/icons/expander-expanded.png");
+        }
+    },
+    toggle_family_checkboxes = function(data, checked) {
+        var family = data.closest('tr'),
+            family_name = family.attr('data-family_begin');
+        family.siblings('tr[data-in_family="'+family_name+'"]').find('input:checkbox').attr('checked', checked);
+    },
+    toggle_parent_checkbox = function(data) {
+        // if all children are checked, the parent should be checked... otherwise, it should be unchecked...
+        // so toggle the parent checkbox, if needed...
+
+        // check to see if the child is evan part of a product family...
+        var sub = data.closest('tr'),
+            in_family = sub.attr('data-in_family');
+        if (in_family !== undefined) {
+            // locate the family
+            var family = sub.prevAll('tr[data-family_begin="'+in_family+'"]');
+            if (family !== undefined) {
+                // retrieve the checkboxes for the family and siblings
+                var family_cbx = family.find('input:checkbox'),
+                    sibling_cbxs = family.siblings('tr[data-in_family="'+in_family+'"]').find('input:checkbox'),
+                    total = sibling_cbxs.length,
+                    num_checked = 0;
+
+                sibling_cbxs.each( function() { if (this.checked) num_checked++; });
+                if (total == num_checked) {
+                    family_cbx.attr('checked', true);
+                }
+                else if (num_checked > 0) {
+                    family_cbx.attr('checked', false);
+                }
+                else {
+                    family_cbx.attr('checked', false);
+                }
+            }
+        }
+    },
+    get_system_templates = function() {
+        // this function will retrieve the system templates associated with a given environment and
+        // update the page content, as appropriate
+        var url = $('.path_link.active').attr('data-templates_url');
+
+        disable_buttons();
+        $.ajax({
+            type: "GET",
+            url: url,
+            cache: false,
+            success: function(response) {
+                // update the appropriate content on the page
+                var options = '';
+
+                // create an html option list using the response
+                options += '<option value="">' + i18n.noTemplate + '</option>';
+                for (var i = 0; i < response.length; i++) {
+                    options += '<option value="' + response[i].id + '">' + response[i].name + '</option>';
+                }
+
+                // add the options to the system template select... this select exists on an insert form
+                // or as part of the environment edit dialog
+                $("#activation_key_system_template_id").html(options);
+
+                highlight_system_templates(true);
+                enable_buttons();
+            },
+            error: function(data) {
+                enable_buttons();
+            }
+        });
+    },
+    get_products = function() {
+        // this function will retrieve the products associated with a given environment and
+        // update the products box with the results
+        var url = $('.path_link.active').attr('data-products_url');
+        if (url !== undefined) {
+            disable_buttons();
+            $.ajax({
+                type: "GET",
+                url: url,
+                cache: false,
+                success: function(response) {
+                    $('.productsbox').html(response);
+                    enable_buttons();
+                },
+                error: function(data) {
+                    enable_buttons();
+                }
+            });
+        }
+    },
+    save_selected_environment = function(env_id) {
+        // save the id of the env selected
+        $("#activation_key_environment_id").attr('value', env_id);
+    },
+    disable_buttons = function() {
+        $('#cancel_key').attr("disabled","disabled");
+        $('input[id^=save_key]').attr("disabled","disabled");
+    },
+    enable_buttons = function() {
+        $('#cancel_key').removeAttr('disabled');
+        $('input[id^=save_key]').removeAttr('disabled');
+    },
+    highlight_system_templates = function(add_highlight) {
+        var select_input = $('#activation_key_system_template_id');
+        if (add_highlight) {
+            if( !select_input.next('span').hasClass('highlight_input_text')) {
+                select_input.addClass('highlight_input');
+                select_input.after('<span class ="highlight_input_text">' + i18n.update_template + '</span>');
+            }
+        } else {
+            select_input.removeClass('highlight_input');
+            $('.highlight_input_text').remove();
+        }
     };
     return {
-        subscriptionSetup: subscriptionSetup,
-        goToAvailableSubscriptions: goToAvailableSubscriptions
+        subscription_setup: subscription_setup,
+        go_to_available_subscriptions: go_to_available_subscriptions,
+        initialize_edit: initialize_edit,
+        reset_env_select: reset_env_select,
+        create_key: create_key,
+        delete_key: delete_key,
+        save_key: save_key,
+        cancel_key: cancel_key,
+        toggle_family: toggle_family,
+        toggle_family_checkboxes: toggle_family_checkboxes,
+        toggle_parent_checkbox: toggle_parent_checkbox,
+        get_system_templates: get_system_templates,
+        get_products: get_products,
+        save_selected_environment: save_selected_environment,
+        disable_buttons: disable_buttons,
+        enable_buttons: enable_buttons,
+        highlight_system_templates: highlight_system_templates
     }
-}();
+}(jQuery));
+
