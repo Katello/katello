dad5f46a00e84595c0f389bdb1a0ea9945293cce
Justin Sherrill <jsherril@redhat.com>
2011-10-04 14:15:31 -0400

    740970 - adding detection if a password contains the username

--- public/javascripts/user_edit.js
+++ public/javascripts/user_edit.js
@@ -14,6 +14,9 @@
 
 $(document).ready(function() {
 
+
+
+
    ratings =
       [{'minScore': 0,
        'className': 'meterFail',
@@ -36,7 +39,15 @@ $(document).ready(function() {
       'container': '#password_meter',
       'offset': 10,
       'showOnFocus':false,
-      'requirements': {},
+      'requirements': {
+          'noUsernameMatch': {
+              value: "#match",
+              message: i18n.usernameMatch,
+              callback: function(password, value) {
+                return password.indexOf($("#username").text().trim()) === -1;
+              }
+          }
+      },
       'defaultText':i18n.meterText,
       'ratings':ratings});
 
