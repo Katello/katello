18feb1ee9802954b7a888d03c230b9846745175f
Lukas Zapletal <lzap+git@redhat.com>
2011-10-06 16:10:07 +0200

    %B

--- config/routes.rb
+++ config/routes.rb
@@ -263,6 +263,7 @@ Src::Application.routes.draw do
         get :errata
       end
     end
+    match '/systems/:id/subscription' => 'systems#subscription', :via => :get
     match '/systems/:id/subscription' => 'systems#subscribe', :via => :post
     match '/systems/:id/subscription/:pool' => 'systems#unsubscribe', :via => :delete
 
