a12ba2dbd5e0a95a1295b6c77989d5e4fa38c9b5
Partha Aji <paji@redhat.com>
2011-10-07 14:29:08 -0400

    %B

--- lib/navigation/content_management.rb
+++ lib/navigation/content_management.rb
@@ -11,67 +11,67 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 module Navigation
   module ContentMenu
-  def self.included(base)
-    base.class_eval do
-      helper_method :custom_provider_navigation
+    def self.included(base)
+      base.class_eval do
+        helper_method :custom_provider_navigation
+      end
+    end
+
+    def custom_provider_navigation
+      [
+          { :key => :edit_custom_providers,
+            :name =>N_("Basics"),
+            :url => (@provider.nil? || @provider.new_record?) ? "" : edit_provider_path(@provider.id),
+            :if => lambda{!@provider.nil? && @provider.readable? && !@provider.new_record?},
+            :options => {:class=>"navigation_element"}
+          },
+          { :key => :products_repos,
+            :name =>N_("Products & Repositories"),
+            :url => (@provider.nil? || @provider.new_record?) ? "" : products_repos_provider_path(@provider.id),
+            :if => lambda{!@provider.nil? && @provider.readable? &&
+                          !@provider.new_record? && !@provider.has_subscriptions?},
+            :options => {:class=>"navigation_element"}
+          }
+      ]
+    end
+
+    def menu_contents
+      {:key => :content,
+       :name => N_("Content Management"),
+        :url => :sub_level,
+        :class=>'content',
+        :items=> [ menu_providers  ]
+      }
+    end
+
+    def menu_providers
+
+      {:key => :providers,
+       :name =>N_("Providers"),
+       :url => :sub_level,
+       :options => {:highlights_on => /(\/organizations\/.*\/providers)|(\/providers\/.*\/(products|repos))/},
+       :if => :sub_level,
+       :items => [menu_redhat_providers, menu_custom_providers]
+      }
+
+    end
+
+    def menu_redhat_providers
+      {:key => :redhat_providers,
+        :name =>N_("Red Hat"),
+        :url => redhat_provider_providers_path,
+        :if => lambda{current_organization && current_organization.readable?},
+        :options => {:class=>"third_level"}
+      }
+    end
+
+    def menu_custom_providers
+      {:key => :custom_providers,
+        :name =>N_("Custom"),
+        :url => lambda{organization_providers_path(current_organization())},
+        :if => lambda{current_organization && Provider.any_readable?(current_organization())},
+        :options => {:class=>"third_level"}
+      }
     end
   end
-
-  def custom_provider_navigation
-    [
-        { :key => :edit_custom_providers,
-          :name =>N_("Basics"),
-          :url => (@provider.nil? || @provider.new_record?) ? "" : edit_provider_path(@provider.id),
-          :if => lambda{!@provider.nil? && @provider.readable? && !@provider.new_record?},
-          :options => {:class=>"navigation_element"}
-        },
-        { :key => :products_repos,
-          :name =>N_("Products & Repositories"),
-          :url => (@provider.nil? || @provider.new_record?) ? "" : products_repos_provider_path(@provider.id),
-          :if => lambda{!@provider.nil? && @provider.readable? &&
-                        !@provider.new_record? && !@provider.has_subscriptions?},
-          :options => {:class=>"navigation_element"}
-        }
-    ]
-  end
-
-  def menu_contents
-    {:key => :content,
-     :name => N_("Content Management"),
-      :url => :sub_level,
-      :class=>'content',
-      :items=> [ menu_providers  ]
-    }
-  end
-
-  def menu_providers
-
-    {:key => :providers,
-     :name =>N_("Providers"),
-     :url => :sub_level,
-     :options => {:highlights_on => /(\/organizations\/.*\/providers)|(\/providers\/.*\/(products|repos))/},
-     :if => :sub_level,
-     :items => [menu_redhat_providers, menu_custom_providers]
-    }
-
-  end
-
-  def menu_redhat_providers
-    {:key => :redhat_providers,
-      :name =>N_("Red Hat"),
-      :url => redhat_provider_providers_path,
-      :if => lambda{current_organization && current_organization.readable?},
-      :options => {:class=>"third_level"}
-    }
-  end
-
-  def menu_custom_providers
-    {:key => :custom_providers,
-      :name =>N_("Custom"),
-      :url => lambda{organization_providers_path(current_organization())},
-      :if => lambda{current_organization && Provider.any_readable?(current_organization())},
-      :options => {:class=>"third_level"}
-    }
-  end
-end
 end
