476de4eb7cfb890802e33093f771268b43e16e54
Tyler Smart <tsmart@redhat.com>
2011-10-14 16:58:26 -0400

    Moving model and view tests into the common directory to further fix

--- src/spec/spec_helper.rb
+++ src/spec/spec_helper.rb
@@ -22,7 +22,7 @@ require 'helpers/organization_helper_methods'
 require 'helpers/system_helper_methods'
 require 'helpers/product_test_data.rb'
 require 'helpers/product_helper_methods'
-require 'models/model_spec_helper'
+require 'common/models/model_spec_helper'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -38,14 +38,21 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
-  #This line says add to these files all of the modules necessary to act as a controller spec
-  config.include RSpec::Rails::ControllerExampleGroup, :type => :controller, :example_group => {
-    :file_path => config.escaped_path(%w[spec common controllers])
-  }
+  [
+    [RSpec::Rails::ControllerExampleGroup, :controller],
+    [RSpec::Rails::ModelExampleGroup, :model],
+    [RSpec::Rails::ViewExampleGroup, :view]
+  ].each do |x|
+    #This line says add to these files all of the modules necessary to act as a controller spec
+    config.include x[0], :type => x[1], :example_group => {
+      :file_path => config.escaped_path(["spec","common",x[1].to_s.pluralize])
+    }
 
-  config.include RSpec::Rails::ControllerExampleGroup, :type => :controller, :example_group => {
-    :file_path => config.escaped_path(["spec", AppConfig.app_name, "controllers"])
-  }
+    config.include x[0], :type => x[1], :example_group => {
+      :file_path => config.escaped_path(["spec", AppConfig.app_name, x[1].to_s.pluralize])
+    }
+  end
+
 
   config.include Warden::Test::Helpers
 
