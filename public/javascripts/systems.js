e3af62729b0e413a66bd34c9e5fdbf3620e9493e
Tom McKay <thomasmckay@redhat.com>
2011-10-17 16:28:05 -0400

    Based on jrist feedback: + add padding to rows to account for fatter spinner + don't increment spinner value if non-zero on checkbox click + alternate row coloring (maintain color on exanding rows)

--- public/javascripts/systems.js
+++ public/javascripts/systems.js
@@ -88,7 +88,9 @@ KT.subs = function() {
                     checked++;
                     spinner = $(this).parent().parent().parent().find(".ui-spinner");
                     if(spinner.length > 0){
-                        spinner.spinner("increment");
+                        if(spinner.spinner("value") == "0") {
+                            spinner.spinner("increment");
+                        }
                     }else{
                         $(this).parent().parent().parent().find(".ui-nonspinner").val(1);
                         spinner = $(this).parent().parent().parent().find(".ui-nonspinner-label")[0];
