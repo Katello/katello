4adce6f4c5890bf0c44dd49945d32fc61d86d3de
Dmitri Dolguikh <dmitri@redhat.com>
2011-10-20 08:02:01 -0300

    added filter-related tests to cli-tests

--- cli/test-system/cli_tests/_base_cleanup.sh
+++ cli/test-system/cli_tests/_base_cleanup.sh
@@ -3,6 +3,7 @@
 header "Basic environment cleanup"
 
 #clear
+todo "filter delete" filter delete --org="$TEST_ORG" --name="$FILTER1"
 todo "repo delete" "not yet implemented" repo delete --product="$FEWUPS_PRODUCT" --org="$TEST_ORG" --name="$FEWUPS_REPO"
 todo "product delete" "not yet implemented" product delete --provider="$YUM_PROVIDER" --org="$TEST_ORG" --name="$FEWUPS_PRODUCT"
 test_success "provider delete" provider delete --name="$YUM_PROVIDER" --org="$TEST_ORG"
