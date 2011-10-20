d6183a4481b9a4be79a1f3cdd7e80fc8daa0eca4
Tomas Strachota <tstrachota@redhat.com>
2011-10-20 13:54:31 +0200

    %B

--- app/models/glue/candlepin/product_content.rb
+++ app/models/glue/candlepin/product_content.rb
@@ -11,15 +11,21 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 module Glue::Candlepin
-  
+
   class ProductContent
     attr_accessor :content, :enabled
-    def initialize(params = {})      
+    def initialize(params = {})
       @enabled = params[:enabled] || params["enabled"]
       @content = Content.new(params[:content] || params["content"])
     end
+
+    def create
+      created = Candlepin::Content.create @content
+      @content.id = created[:id]
+    end
+
   end
-  
+
   class Content
     attr_accessor :name, :id, :type, :label, :vendor, :contentUrl, :gpgUrl
     def initialize(params = {})
