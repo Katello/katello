e1a85083ce81183af58cb7825d0629c5c6f9c46b
Dmitri Dolguikh <dmitri@redhat.com>
2011-10-31 04:48:07 -0300

    updated TDL schema + corresponding changes in template export & tests

--- src/spec/models/system_template_spec.rb
+++ src/spec/models/system_template_spec.rb
@@ -506,7 +506,7 @@ describe SystemTemplate do
         subject.xpath("/template/repositories/repository/clientkey").text.should_not == nil
       end
 
-      pending #it_should_behave_like "valid tdl"
+      it_should_behave_like "valid tdl"
     end
   end
 
