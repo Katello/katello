e9d8af3a262cece7522247d7efd6f38e25a2177a
Tomas Strachota <tstrachota@redhat.com>
2011-10-31 15:24:27 +0100

    templates - fix for increased revision numbers after promotion

--- src/app/models/system_template.rb
+++ src/app/models/system_template.rb
@@ -84,8 +84,9 @@ class SystemTemplate < ActiveRecord::Base
     json["packages"].each {|p| self.add_package(p) } if json["packages"]
     json["package_groups"].each {|pg| self.add_package_group(pg) } if json["package_groups"]
     json["package_group_categories"].each {|pgc| self.add_pg_category(pgc) } if json["package_group_categories"]
-
     json["parameters"].each_pair {|k,v| self.parameters[k] = v } if json["parameters"]
+
+    self.save_content_state
   end
 
   def export_as_hash
