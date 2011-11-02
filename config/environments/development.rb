ded0436794596e04ba3d75a436b0dc55b9fba7e8
Brad Buckingham <bbuckingham@redhat.com>
2011-11-02 16:53:52 -0400

    password reset - chgs to dev env to configure sendmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,9 @@ Src::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => "localhost:3000/katello" }
 
   # Print deprecation notices to the Rails logger
