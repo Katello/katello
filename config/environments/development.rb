242e53fd39688b2c6446b6af1f3caa7ea0c32894
Brad Buckingham <bbuckingham@redhat.com>
2011-11-03 16:13:03 -0400

    %B

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Src::Application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => "localhost:3000/katello" }
+  config.action_mailer.default_url_options = { :host => "dhcp231-44.rdu.redhat.com:3000/katello" }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
