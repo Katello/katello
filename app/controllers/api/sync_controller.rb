c1ad76692a43762eadfe597977d9543aaea39662
Partha Aji <paji@redhat.com>
2011-11-03 18:29:24 -0400

    %B

--- app/controllers/api/sync_controller.rb
+++ app/controllers/api/sync_controller.rb
@@ -59,7 +59,7 @@ class Api::SyncController < Api::ApiController
   end
 
   def find_repository
-    @repository = Repository.find_by_pulp_id(params[:repository_id])
+    @repository = Repository.find(params[:repository_id])
     raise HttpErrors::NotFound, _("Couldn't find repository '#{params[:repository_id]}'") if @repository.nil?
     raise HttpErrors::NotFound, _("You can only synchronize repositories in locker environment'") if not @repository.environment.locker?
     @repository
