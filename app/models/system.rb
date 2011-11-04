9e493460e574a871960fa8f5ae86c7ea2bd44cc4
Lukas Zapletal <lzap+git@redhat.com>
2011-11-04 11:29:26 +0100

    ak cli - adding ak to the system api

--- app/models/system.rb
+++ app/models/system.rb
@@ -88,6 +88,7 @@ class System < ActiveRecord::Base
   def as_json(options)
     json = super(options)
     json['environment'] = environment.as_json unless environment.nil?
+    json['activation_key'] = activation_keys.as_json unless activation_keys.nil?
     json
   end
 
