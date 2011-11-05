860f57d0a1928e4270f5ba154e99e593613107fb
Partha Aji <paji@redhat.com>
2011-11-04 20:42:22 -0400

    Removed the ChangesetRepo table + object and made it connect to the Repository model directly

--- db/migrate/20110303032320_environments_products.rb
+++ db/migrate/20110303032320_environments_products.rb
@@ -4,9 +4,11 @@ class EnvironmentsProducts < ActiveRecord::Migration
        t.references :environment, :null =>false
        t.references :product, :null =>false
     end
+    add_index(:environment_products, [:environment_id, :product_id], :unique=>true)
   end
 
   def self.down
+    remove_index :environment_products, [:environment_id, :product_id]
     drop_table :environment_products
   end
 end
