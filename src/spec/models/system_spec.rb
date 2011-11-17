91bfde5fdadc6c29f7570b71623908eea927f6f3
Tom McKay <thomasmckay@redhat.com>
2011-11-17 10:56:55 -0500

    removed pulp test

--- src/spec/models/system_spec.rb
+++ src/spec/models/system_spec.rb
@@ -68,7 +68,7 @@ describe System do
 
   it "registers system in candlepin and pulp on create" do
     Candlepin::Consumer.should_receive(:create).once.with(@organization.name, system_name, cp_type, facts, installed_products).and_return({:uuid => uuid, :owner => {:key => uuid}})
-    Pulp::Consumer.should_receive(:create).once.with(@organization.cp_key, uuid, description).and_return({:uuid => uuid, :owner => {:key => uuid}})
+    Pulp::Consumer.should_receive(:create).once.with(@organization.cp_key, uuid, description).and_return({:uuid => uuid, :owner => {:key => uuid}}) if AppConfig.katello?
     @system.save!
   end
 
