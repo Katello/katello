2d8c5c9fc97be32286bcc4abcfd8cd8105721873
Eric D Helms <ehelms@redhat.com>
2011-11-17 10:15:59 -0500

    GPG Keys: Adds functional GPG new key view.

--- src/public/javascripts/gpg_key.js
+++ src/public/javascripts/gpg_key.js
@@ -12,3 +12,29 @@
 */
 
 KT.panel.list.registerPage('gpg_keys', { create : 'new_gpg_key' });
+
+$(document).ready(function(){
+	$('#gpg_key_content').live('input keyup paste', function(){
+		if( $(this).val() !== '' ){
+			$('#gpg_key_content_upload').attr('disabled', 'disabled');
+		} else {
+			$('#gpg_key_content_upload').removeAttr('disabled');
+		}
+	});
+	
+	$('#gpg_key_content_upload').live('change', function(){
+		if( $(this).val() !== '' ){
+			$('#gpg_key_content').attr('disabled', 'disabled');
+			$('#cancel_upload_button').removeAttr('disabled');
+		} else {
+			$('#gpg_key_content').removeAttr('disabled');
+			$('#cancel_upload_button').attr('disabled', 'disabled');
+		}
+	});
+	
+	$('#cancel_upload_button').live('click', function(){
+		$('#gpg_key_content_upload').val('');
+		$('#gpg_key_content').removeAttr('disabled');
+		$('#cancel_upload_button').attr('disabled', 'disabled');
+	});
+});
