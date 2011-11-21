cf37a78d048bbe5b319522d1d7c0c7060ea429fe
Partha Aji <paji@redhat.com>
2011-11-21 15:57:11 -0500

    added some unit tests to deal with gpg keys

--- spec/models/gpg_key_spec.rb
+++ spec/models/gpg_key_spec.rb
@@ -13,5 +13,47 @@
 require 'spec_helper'
 
 describe GpgKey do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include OrchestrationHelper
+  include AuthorizationHelperMethods
+
+  describe "permission checks" do
+    let(:organization) do
+        disable_org_orchestration
+        Organization.create!(:name => "Duh", :cp_key => "ahaha")
+    end
+
+    let(:gpg) {GpgKey.create!(:name => "hazard", :organization => organization, :content => "Barn")}
+
+    describe "check on read operations" do
+      [[:gpg, :organizations],[:read, :organizations],[:read, :providers]].each do |(perm, resource)|
+        it "user with #{perm} on #{resource} should be allowed to work with gpg" do
+          User.current = user_with_permissions{|u| u.can(perm, resource,nil, organization, :all_tags => true)}
+          GpgKey.find(gpg.id).should be_readable
+          GpgKey.readable(organization).should_not be_empty
+          GpgKey.readable(organization).should == [gpg]
+        end
+      end
+      it "user without perms should not  be allowed to work with gpg" do
+        User.current = user_without_permissions
+        GpgKey.find(gpg.id).should_not be_readable
+        GpgKey.readable(organization).should be_empty
+      end
+    end
+
+    describe "check on write operations" do
+      it "user with #{:gpg} on #{:org} should be allowed to work with gpg" do
+        User.current = user_with_permissions{|u| u.can(:gpg, :organizations,nil, organization, :all_tags => true)}
+        GpgKey.find(gpg.id).should be_manageable
+        GpgKey.manageable(organization).should == [gpg]
+      end
+
+      it "user without perms should not  be allowed to work with gpg" do
+        User.current = user_without_permissions
+        GpgKey.find(gpg.id).should_not be_manageable
+        GpgKey.manageable(organization).should be_empty
+      end
+    end
+
+  end
+
 end
