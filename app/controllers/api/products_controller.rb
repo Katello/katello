4138cdcf8a53049fe60102724901c7eee2a1389b
Tomas Strachota <tstrachota@redhat.com>
2011-11-23 12:36:20 +0100

    repo blacklist - flag for displaying disabled products via api

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -39,8 +39,14 @@ class Api::ProductsController < Api::ApiController
     query_params.delete(:organization_id)
     query_params.delete(:environment_id)
 
-    render :json => Product.readable(@organization).select("products.*, providers.name AS provider_name").joins(:provider).where(query_params) if @environment == nil
-    render :json => @environment.products.readable(@organization).select("products.*, providers.name AS provider_name").joins(:provider).where(query_params).all if @environment != nil
+
+    if @environment.nil?
+      products = query_params[:include_disabled] ? Product.all_readable(@organization) : Product.readable(@organization)
+    else
+      products = query_params[:include_disabled] ? @environment.products.all_readable(@organization) : @environment.products.readable(@organization)
+    end
+
+    render :json => products.select("products.*, providers.name AS provider_name").joins(:provider).where(query_params).all
   end
 
   def destroy
