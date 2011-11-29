24f683ee64e523c3d929c75c6108b64f7791aaa0
Bryan Kearney <bkearney@redhat.com>
2011-11-29 09:13:40 -0500

    Change the default password for thumbslug keystores

--- puppet/modules/thumbslug/manifests/params.pp
+++ puppet/modules/thumbslug/manifests/params.pp
@@ -1,5 +1,5 @@
 class thumbslug::params {
   $keystore = "/etc/candlepin/certs/keystore"
-  $keystore_pass = "secret"
+  $keystore_pass = "password"
   $oauth_secret = regsubst(generate('/usr/bin/openssl', 'rand', '-base64', '24'), '^(.{24}).*', '\1')
 }
