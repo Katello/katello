658cadf6f0d17944cb7d284d1a255e6e16e3bf7f
Shannon Hughes <shughes@redhat.com>
2011-12-05 11:15:18 -0500

    add padding for link details ellipsis

--- src/public/javascripts/system_template.js
+++ src/public/javascripts/system_template.js
@@ -359,7 +359,7 @@ KT.template_renderer = function() {
     },
     list_item = function(id, text, is_slide_link) {
         var html = '<li class="' + (is_slide_link ? 'slide_link' : '')  + '">';
-        html += '<div class="link_details simple_link" id="' + id + '">';
+        html += '<div class="link_details simple_link one-line-ellipsis" id="' + id + '">';
         html += '<span class="sort_attr">' + text + '</span>';
         html += "</div></li>";
         return html ;
