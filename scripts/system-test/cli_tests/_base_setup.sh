21dc1a3c0d48c78ed32b24c581f6f84939f02169
Lukas Zapletal <lzap+git@redhat.com>
2011-12-07 13:07:56 +0100

    cli tests - adding more debug messages to the base test

--- scripts/system-test/cli_tests/_base_setup.sh
+++ scripts/system-test/cli_tests/_base_setup.sh
@@ -17,14 +17,14 @@ FEWUPS_REPO="repo_$RAND"
 FILTER1="filter_$RAND"
 
 # BASIC RESOURCES (reused in tests)
-test_success "user create" user create --username=$TEST_USER --password=password --email=$TEST_USER@somewhere.com
-test_success "org create" org create --name=$TEST_ORG --description="org description"
+test_success "user create ($TEST_USER)" user create --username=$TEST_USER --password=password --email=$TEST_USER@somewhere.com
+test_success "org create ($TEST_ORG)" org create --name=$TEST_ORG --description="org description"
 test_success "org uebercert" org uebercert --name=$TEST_ORG
-test_success "environment create" environment create --org="$TEST_ORG" --name="$TEST_ENV" --prior="Locker"
-test_success "environment create" environment create --org="$TEST_ORG" --name="$TEST_ENV_3" --prior="$TEST_ENV"
-test_success "provider create" provider create --name="$YUM_PROVIDER" --org="$TEST_ORG" --url="$FEWUPS_REPO_URL" --description="prov description"
-test_success "product create" product create --provider="$YUM_PROVIDER" --org="$TEST_ORG" --name="$FEWUPS_PRODUCT" --url="$FEWUPS_REPO_URL" --assumeyes
-test_success "repo create" repo create --product="$FEWUPS_PRODUCT" --org="$TEST_ORG" --name="$FEWUPS_REPO" --url="$FEWUPS_REPO_URL"
-test_success "filter create" filter create --org="$TEST_ORG" --name="$FILTER1" --description="description" --packages="package1, package2"
+test_success "environment create ($TEST_ENV)" environment create --org="$TEST_ORG" --name="$TEST_ENV" --prior="Locker"
+test_success "environment create ($TEST_ENV_3)" environment create --org="$TEST_ORG" --name="$TEST_ENV_3" --prior="$TEST_ENV"
+test_success "provider create ($YUM_PROVIDER)" provider create --name="$YUM_PROVIDER" --org="$TEST_ORG" --url="$FEWUPS_REPO_URL" --description="prov description"
+test_success "product create ($FEWUPS_PRODUCT)" product create --provider="$YUM_PROVIDER" --org="$TEST_ORG" --name="$FEWUPS_PRODUCT" --url="$FEWUPS_REPO_URL" --assumeyes
+test_success "repo create ($FEWUPS_REPO)" repo create --product="$FEWUPS_PRODUCT" --org="$TEST_ORG" --name="$FEWUPS_REPO" --url="$FEWUPS_REPO_URL"
+test_success "filter create ($FILTER1)" filter create --org="$TEST_ORG" --name="$FILTER1" --description="description" --packages="package1, package2"
 REPO_NAME=`$CMD repo list --org="$TEST_ORG" | grep $FEWUPS_REPO | awk '{print $2}'`
 REPO_ID=$(get_repo_id)
