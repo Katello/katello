10c691d4f5b9e37ee753bba2d0c28f464ab92ee1
Lukas Zapletal <lzap+git@redhat.com>
2011-12-12 15:02:48 +0100

    %B

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# !!! PLEASE KEEP THIS SCRIPT IDEMPOTENT !!!
+#
+
 require 'util/password'
 
 AppConfig.use_cp = false if ENV['NO_CP']
@@ -9,12 +13,18 @@ AppConfig.use_pulp = false if ENV['NO_PULP']
 superadmin_role = Role.find_or_create_by_name(
   :name => 'Administrator', 
   :description => 'Super administrator with all access.')
-
 Permission.create!(:name=> "super-admin-perm", :role => superadmin_role, :all_types => true)
 
+# create read *everything* role and assign permissions to it
 reader_role = Role.find_or_create_by_name(
   :name => 'Read Everything',
   :description => 'Permissions to read everything.')
+Permission.find_or_create_by_name(:role => reader_role,
+                   :resource_type => ResourceType.find_by_name("all"),
+                   :all_tags => true,
+                   :all_verbs => true,
+                   :name => "Read All",
+                   :description => "Read everything permission")
 
 # create the super admin if none exist - it must be created before any statement in the seed.rb script
 User.current = user_admin = User.new(
