55a03b59457a9a51a2d2d8199a44f18097694797
Justin Sherrill <jsherril@redhat.com>
2011-12-16 14:53:20 -0500

    making user sorting be on a non-analyzed login attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,15 @@ class User < ActiveRecord::Base
 
   acts_as_reportable
 
-  index_options :json=>{:except=>[:password, :password_reset_token,
+  index_options :extended_json=>:extended_index_attrs,
+                :json=>{:except=>[:password, :password_reset_token,
                                   :password_reset_sent_at, :helptips_enabled,
                                   :disabled, :own_role_id]}
+
+  mapping do
+    indexes :login_sort, :type => 'string', :index => :not_analyzed
+  end
+
 
   has_many :roles_users
   has_many :roles, :through => :roles_users
@@ -454,6 +460,13 @@ class User < ActiveRecord::Base
     return ACTION_TO_VERB[type][verb] if ACTION_TO_VERB[type] and ACTION_TO_VERB[type][verb]
     return DEFAULT_VERBS[verb] if DEFAULT_VERBS[verb]
     verb
+  end
+
+
+  def extended_index_attrs
+    {
+        :login_sort => login.downcase
+    }
   end
 
   private
