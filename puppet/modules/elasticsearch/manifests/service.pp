64ad0d0257f741c91ca3a6defa7f76e8f883d0af
Mike McCune <mmccune@redhat.com>
2011-12-15 16:09:41 -0800

    768191 - first cut at getting elasticsearch configured

---
+++ puppet/modules/elasticsearch/manifests/service.pp
@@ -0,0 +1,7 @@
+class elasticsearch::service {
+  service {"elasticsearch":
+    ensure  => running, enable => true, hasstatus => true, hasrestart => true,
+    require => Class["elasticsearch::config"]
+  }
+}
+
