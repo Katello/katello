7a6d972a1fc8486423081bb098a9c77c3a2368ae
Partha Aji <paji@redhat.com>
2011-12-19 20:04:25 -0500

    %B

--- app/models/task_status.rb
+++ app/models/task_status.rb
@@ -11,6 +11,7 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 class TaskStatus < ActiveRecord::Base
+  serialize :result
   serialize :progress
   serialize :parameters, Hash
   class Status
