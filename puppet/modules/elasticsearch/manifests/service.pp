28f37b7253e05fff8ce5759c54b0d35933df4fa0
Mike McCune <mmccune@redhat.com>
2011-12-22 11:55:11 -0800

    768191 - ensure we have elasticsearch running before seed

--- puppet/modules/elasticsearch/manifests/service.pp
+++ puppet/modules/elasticsearch/manifests/service.pp
@@ -1,7 +1,8 @@
 class elasticsearch::service {
   service {"elasticsearch":
     ensure  => running, enable => true, hasstatus => true, hasrestart => true,
-    require => Class["elasticsearch::config"]
+    require => Class["elasticsearch::config"],
+    before => Exec["katello_seed_db"]
   }
 }
 
