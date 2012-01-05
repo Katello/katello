8508a84d119dc109b6dbd988b2717440345a5075
Ivan Necas <inecas@redhat.com>
2012-01-05 18:22:58 +0100

    771911 - keep facts on system update
    Updating facts makes only sence if they were loaded from before CP.
    
    We can not use
    
      @facts ||= {}
    
    to avoid calling nil.[] because when saving the system after that, we will
    loose othe facts for the system.

--- src/spec/controllers/api/systems_packages_controller_spec.rb
+++ src/spec/controllers/api/systems_packages_controller_spec.rb
@@ -41,7 +41,7 @@ describe Api::SystemPackagesController do
 
     @organization = Organization.create!(:name => 'test_org', :cp_key => 'test_org')
     @environment_1 = KTEnvironment.create!(:name => 'test_1', :prior => @organization.locker.id, :organization => @organization)
-    @system = System.create!(:environment => @environment_1, :uuid => "1234", :name => "system.example.com", :cp_type => 'system')
+    @system = System.create!(:environment => @environment_1, :uuid => "1234", :name => "system.example.com", :cp_type => 'system', :facts => {})
     System.stub(:first => @system)
   end
 
