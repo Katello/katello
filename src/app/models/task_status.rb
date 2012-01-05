ae5625871b499b06dc1f9f22ec4f237c8e2c1d0a
Partha Aji <paji@redhat.com>
2012-01-05 11:02:43 -0500

    Mods on the Task Status search page to include the erros or failure key words in status

--- src/app/models/task_status.rb
+++ src/app/models/task_status.rb
@@ -38,27 +38,50 @@ class TaskStatus < ActiveRecord::Base
     end
   end
 
-  index_options :json=>{:only=> [:state, :parameters, :result,
+  index_options :json=>{:only=> [:parameters, :result,
                      :organization_id, :system_ids, :start_time, :finish_time ]},
                 :extended_json=>:extended_index_attrs
 
-  mapping do
-   indexes :start_time, :type=>'date'
-   indexes :finish_time, :type=>'date'
-   indexes :task_type, :analyzer => 'snowball'
-  end
+      settings  :index => {
+                :analysis => {
+                 :analyzer => {
+                   :default => {
+                       "tokenizer"  => "lowercase",
+                      "type"   => "snowball",
+                   }
+                 }
+               }
+                } do
+        mapping do
+         indexes :start_time, :type=>'date'
+         indexes :finish_time, :type=>'date'
+        end
 
-
+      end
 
   def extended_index_attrs
     ret = {}
     ret[:username] = user.username if user
+
+    ret[:status] = state.to_s
+    ret[:status] += " pending" if pending?
+    if state.to_s == "error" || state.to_s == "timed_out"
+      ret[:status] += " fail failure"
+    end
+
+    case state.to_s
+      when "finished"
+        ret[:status] += " completed"
+      when "timed_out"
+        ret[:status] += " timed out"
+    end
+
     if task_type
       tt = task_type
       unless system_tasks.nil? ||  system_tasks.empty?
-        tt = SystemTask::TYPES[task_type][:name]
+        tt = SystemTask::TYPES[task_type][:english_name]
       end
-      ret[:task_type] = tt
+      ret[:status] +=" #{tt}"
     end
     ret
   end
