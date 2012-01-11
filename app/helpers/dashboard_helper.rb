8961154383225e586275c5fdc4962ace29b2f7f2
Brad Buckingham <bbuckingham@redhat.com>
2012-01-11 17:20:15 -0500

    %B

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -10,8 +10,6 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-require 'set'
-
 module DashboardHelper
 
   def dashboard_entry name, partial, dropbutton
