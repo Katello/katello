d0a33fb79349cf4cc6d128160ba6fd95c128be40
Eric D Helms <ehelms@redhat.com>
2012-01-16 14:12:44 -0500

    %B

--- public/javascripts/system_errata.js
+++ public/javascripts/system_errata.js
@@ -34,19 +34,7 @@ KT.system.errata = function() {
     		$('#errata_state_radio_outstanding').bind('change', fetch_errata);
             $('#run_errata_button').bind('click', add_errata);
     		load_more.bind('click', { clear_items : false }, fetch_errata);
-            $('.errata-info').tipsy({ gravity: 'e', live : true, html : true, title : generateInfoToolTip, 
-                                    hoverable : true, delayOut : 250, opacity : 1, delayIn : 300,
-                                    stickyClick : function(element, state){ 
-                                        if (state === 'on'){
-                                            $(element).addClass('details-icon-hover').removeClass('details-icon');
-                                        } else {
-                                            $(element).addClass('details-icon').removeClass('details-icon-hover');
-                                        }
-                                    },
-                                    afterShow : function(){
-                                        $('.details_container').addClass('scroll-pane');
-                                        KT.common.jscroll_init($('.scroll-pane'));
-                                    }});
+            KT.tipsy.custom.errata_tooltip();
     	},
         init_status_check = function(){
             var timeout = 8000;
@@ -203,35 +191,6 @@ KT.system.errata = function() {
             }
 
             return rows;
-        },
-        generateInfoToolTip = function(){
-            var html = '<div class="details_container">',
-                element = $(this),
-                packages_list = [],
-                generate_packages = function(){
-                    var packages = element.data('packages')[0]["packages"],
-                        i = 0, 
-                        length = packages.length,
-                        html = "";
-                    
-                    for(i; i < length; i += 1){
-                        html += "<li>" + packages[i]["filename"] + '</li>';
-                    }
-
-                    return html;
-                }; 
-           
-            packages_list = generate_packages();
-
-            html += '<div class="item-container"><label class="fl ra">ID:</label>' + '<p>' + element.data('id') + '</p></div>';
-            html += '<div class="item-container"><label class="fl ra">Title:</label>' + '<p>' + element.data('title') + '</p></div>';
-            html += '<div class="item-container"><label class="fl ra">Issued:</label>' + '<p>' + element.data('issued') + '</p></div>';
-            html += '<div class="item-container"><label class="fl ra">Reference:</label>' + '<p><a target="new" href="' +  element.data('reference_url') + '">' + element.data('reference_url') + '</a></p></div>';
-            html += '<div class="item-container"><label class="fl ra">Description:</label>' + '<p><br/><pre>' + element.data('description') + '</pre></p></div>';
-            html += '<div class="item-container"><label class="fl" style="text-align:left;">Packages:</label>' + '<ul style="margin:0 0 0 4px;" class="la"><br/>' + packages_list + '</ul></div>';            
-            html += '</div>';
-
-            return html;
         };
 	    
     return {
