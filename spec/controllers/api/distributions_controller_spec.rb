f78b87ee2cbb8884aa80ea5b14a9f6f7bc1c5c84
Lukas Zapletal <lzap+git@redhat.com>
2012-01-17 13:25:06 +0100

    %B

--- spec/controllers/api/distributions_controller_spec.rb
+++ spec/controllers/api/distributions_controller_spec.rb
@@ -14,28 +14,78 @@ require 'spec_helper.rb'
 
 describe Api::DistributionsController do
   include LoginHelperMethods
+  include AuthorizationHelperMethods
 
   before(:each) do
+    disable_org_orchestration
+    disable_product_orchestration
+    disable_user_orchestration
+    disable_repo_orchestration
+
+    @organization = new_test_org
+    @provider = Provider.create!(:name => "provider",
+                                 :provider_type => Provider::CUSTOM,
+                                 :organization => @organization,
+                                 :repository_url => "https://localhost")
+    @product = Product.create!(:name => "prod",
+                               :provider => @provider,
+                               :environments => [@organization.locker])
+    @product.stub(:repos).and_return([@repository])
+
+    ep_locker = EnvironmentProduct.find_or_create(@organization.locker, @product)
+    @repo = Repository.create!(:environment_product => ep_locker,
+                               :name=> "repo",
+                               :relative_path => "#{@organization.name}/Locker/prod/repo",
+                               :pulp_id=> "1",
+                               :enabled => true)
+    @repo.stub(:has_distribution?).and_return(true)
+    Repository.stub(:find).and_return(@repo)
+    @repo.stub(:distributions).and_return([])
+    Glue::Pulp::Distribution.stub(:find).and_return([])
+
+    @request.env["HTTP_ACCEPT"] = "application/json"
     login_user_api
-    @repo = mock(Glue::Pulp::Repo)
+  end
+  let(:authorized_user) do
+    user_with_permissions do |u|
+      u.can(:read_contents, :environments, @organization.locker.id, @organization)
+      u.can(:read, :providers, @provider.id, @organization)
+    end
+  end
+  let(:unauthorized_user) do
+    user_without_permissions
   end
 
-  describe "get a listing of distributions" do
-    it "should call pulp find repo api" do
-      Repository.should_receive(:find).once.with(1).and_return(@repo)
-      @repo.should_receive(:distributions)
-      get 'index', :repository_id => 1
+  context "rules" do
+    describe "get a listing of distributions" do
+      let(:action) { :index }
+      let(:req) {
+        get 'index', :repository_id => @repo.id
+      }
+      it_should_behave_like "protected action"
+    end
+
+    describe "show a distribution" do
+      let(:action) { :index }
+      let(:req) {
+        get 'show', :id => 1, :repository_id => @repo.id
+      }
+      it_should_behave_like "protected action"
     end
   end
 
-  describe "show a distribution" do
-    it "should call pulp find distribution api" do
-      Repository.should_receive(:find).once.with(1).and_return(@repo)
-      @repo.should_receive(:has_distribution?).once.with(1).and_return(true)
-      Glue::Pulp::Distribution.should_receive(:find).once.with(1)
-      get 'show', :id => 1, :repository_id => 1
+  context "test" do
+    before do
+      disable_authorization_rules
+    end
+
+    describe "get a listing of distributions" do
+      it "should call pulp find repo api" do
+        Repository.should_receive(:find).once.with(@repo.id).and_return(@repo)
+        @repo.should_receive(:distributions)
+        get 'index', :repository_id => @repo.id
+      end
     end
   end
-
 end
 
