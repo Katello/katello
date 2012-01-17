16d73743fe3f8bb6a0f911451f6d29f4a597292f
Ivan Necas <inecas@redhat.com>
2012-01-17 14:20:30 +0100

    %B

--- app/models/system.rb
+++ app/models/system.rb
@@ -131,6 +131,11 @@ class System < ActiveRecord::Base
     json['environment'] = environment.as_json unless environment.nil?
     json['activation_key'] = activation_keys.as_json unless activation_keys.nil?
     json['template'] = system_template.as_json unless system_template.nil?
+    if self.guest == 'true'
+      json['host'] = self.host.attributes
+    else
+      json['guests'] = self.guests.map(&:attributes)
+    end
     json
   end
 
