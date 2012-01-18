1606f54cbadd33acfbc9fb7fecb09f69af87a20c
Ivan Necas <inecas@redhat.com>
2012-01-18 16:33:55 +0100

    host-guest - fix error in the model when no host is present for guest

--- app/models/system.rb
+++ app/models/system.rb
@@ -131,7 +131,7 @@ class System < ActiveRecord::Base
     json['environment'] = environment.as_json unless environment.nil?
     json['activation_key'] = activation_keys.as_json unless activation_keys.nil?
     json['template'] = system_template.as_json unless system_template.nil?
-    if self.guest == 'true'
+    if self.guest == 'true' && self.host
       json['host'] = self.host.attributes
     else
       json['guests'] = self.guests.map(&:attributes)
