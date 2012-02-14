256e44c96666fb4c8d3e3b088fc45ba913eab24c
Partha Aji <paji@redhat.com>
2012-02-14 15:24:56 -0500

    787796 - Improvements to rh providers + repo enablement pages

--- lib/resources/pulp.rb
+++ lib/resources/pulp.rb
@@ -181,6 +181,16 @@ module Pulp
         raise e
       end
 
+      def find_all repo_ids, yell_on_404 = false
+        ids = "id=#{repo_ids.join('&id=')}"
+        response = get(repository_path  + "/?#{ids}", self.default_headers)
+        body = response.body
+        JSON.parse(body).with_indifferent_access
+      rescue RestClient::ResourceNotFound => e
+        return nil if !yell_on_404
+        raise e
+      end
+
       # Get all the Repositories known by Pulp
       # currently filtering against only one groupid is supported in PULP
       def all groupids=nil, search_params = {}
