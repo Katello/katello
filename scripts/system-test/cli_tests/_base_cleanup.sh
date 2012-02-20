5956e32efefc6173c6fc9283cca25e28d37736a4
Tomas Strachota <tstrachota@redhat.com>
2012-02-20 11:28:25 +0100

    791194 - system tests - added spaces to environment names

--- scripts/system-test/cli_tests/_base_cleanup.sh
+++ scripts/system-test/cli_tests/_base_cleanup.sh
@@ -8,6 +8,6 @@ test_success "product delete" product delete --org="$TEST_ORG" --name="$FEWUPS_P
 test_success "filter delete" filter delete --org="$TEST_ORG" --name="$FILTER1"
 test_success "provider delete" provider delete --name="$YUM_PROVIDER" --org="$TEST_ORG"
 test_success "environment delete" environment delete --name="$TEST_ENV" --org="$TEST_ORG"
-test_success "environment delete" environment delete --name="$TEST_ENV_3" --org="$TEST_ORG"
+test_success "environment delete" environment delete --name="$TEST_ENV_2" --org="$TEST_ORG"
 test_success "org delete" org delete --name="$TEST_ORG"
 test_success "user delete" user delete --username="$TEST_USER"
