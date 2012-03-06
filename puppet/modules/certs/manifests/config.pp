40d586a9e28b47acc9e0770b77d1f0a2db105ccd
Martin Bačovský <mbacovsk@redhat.com>
2012-03-06 11:43:52 +0100

    800318 - installer fails: Working directory '/var/www/html/pub' does not exist

--- puppet/modules/certs/manifests/config.pp
+++ puppet/modules/certs/manifests/config.pp
@@ -142,12 +142,19 @@ class certs::config {
 
   $candlepin_certs_dir = "/etc/candlepin/certs"
 
+  file { "/var/www/html/pub":
+    ensure => "directory",
+    owner  => "apache",
+    group  => "apache",
+    mode   => 755
+  }
+
   exec { "generate-candlepin-consumer-certificate":
     cwd       => '/var/www/html/pub',
     command   => "gen-rpm.sh --name '${candlepin_consumer_name}' --version 1.0 --release 1 --packager None --vendor None --group 'Applications/System' --summary '${candlepin_consumer_summary}' --description '${candlepin_consumer_description}' --post ${ssl_build_path}/rhsm-katello-reconfigure /etc/rhsm/ca/candlepin-local.pem:666=${ssl_build_path}/$candlepin_cert_name.crt && /sbin/restorecon ./*rpm",
     path      => "/usr/share/katello/certs:/usr/bin:/bin",
     creates   => "/var/www/html/pub/${candlepin_cert_name}-1.0-1.noarch.rpm",
-    require   => [Exec["generate-candlepin-certificate"], File["${ssl_build_path}/rhsm-katello-reconfigure"]]
+    require   => [File["/var/www/html/pub"], Exec["generate-candlepin-certificate"], File["${ssl_build_path}/rhsm-katello-reconfigure"]]
   }
 
   exec { "deploy-candlepin-certificate":
