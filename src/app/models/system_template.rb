3fa73b0a14b05c71903d2022530581b1d850e07b
Martin Bačovský <mbacovsk@redhat.com>
2012-03-07 13:23:40 +0100

    799036 - Promoting template with repo that is not already in target environment fails (cherry picked from commit ccf50f41bf20acd01affc59bf2f7d22de5e643fe)

--- src/app/models/system_template.rb
+++ src/app/models/system_template.rb
@@ -447,7 +447,7 @@ class SystemTemplate < ActiveRecord::Base
       if cloned
         async_tasks += cloned.sync
       else
-        async_tasks += repo.promote(to_env)
+        async_tasks += repo.promote(from_env, to_env)
       end
     end
     PulpTaskStatus::wait_for_tasks async_tasks
