85e341d55cda4e4b29ae9d3f72c762251e39b249
Petr Chalupa <pchalupa@redhat.com>
2012-03-15 17:18:25 +0100

    Bug 801580 - Updating sync plan does not update associated product's (repo's) sync schedule

--- spec/models/sync_plan_spec.rb
+++ spec/models/sync_plan_spec.rb
@@ -58,6 +58,22 @@ describe SyncPlan do
       @plan.sync_date = DateTime.now.yesterday()
       @plan.schedule_format.should == nil
     end
+
+    it "reassign sync_plan to its products after update" do
+      disable_product_orchestration
+
+      organization = Organization.create!(:name => ProductTestData::ORG_ID, :cp_key => 'admin-org-37070')
+      @plan.products.create! ProductTestData::SIMPLE_PRODUCT.merge(
+                                 :provider => organization.redhat_provider, :environments => [organization.library])
+      @plan.save!
+      @plan.reload
+      @plan.should have(1).products
+
+      #updating plan
+      @plan.sync_date += 1
+      @plan.products.first.should_receive(:setup_sync_schedule).and_return(true)
+      lambda { @plan.save! }.should_not raise_exception
+    end
   end
 
 end
