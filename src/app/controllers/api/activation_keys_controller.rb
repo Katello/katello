d53e1fb714c1ce9fbe22c6ae58080a126607b362
Tom McKay <thomasmckay@redhat.com>
2012-03-26 14:23:26 -0400

    806741 - added missing environment_id to api/activation_keys_controller param_rules

--- src/app/controllers/api/activation_keys_controller.rb
+++ src/app/controllers/api/activation_keys_controller.rb
@@ -36,7 +36,7 @@ class Api::ActivationKeysController < Api::ApiController
   def param_rules
     {
       :create => {:activation_key => [:name, :description, :system_template_id]},
-      :update => {:activation_key  => [:name, :description, :system_template_id]}
+      :update => {:activation_key  => [:name, :description, :environment_id, :system_template_id]}
     }
   end
 
