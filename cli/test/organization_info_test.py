a450e2af96b08109a38ac8bc2ed78f351cfc45d4
Tomas Strachota <tstrachota@redhat.com>
2012-03-27 11:19:01 +0200

    cli unit tests - test for org info

---
+++ cli/test/organization_info_test.py
@@ -0,0 +1,48 @@
+import unittest
+from mock import Mock
+import os
+
+from cli_test_utils import CLIOptionTestCase, CLIActionTestCase
+import test_data
+
+import katello.client.core.organization
+from katello.client.core.organization import Info
+
+
+class RequiredCLIOptionsTests(CLIOptionTestCase):
+    #required: name
+    def setUp(self):
+        self.set_action(Info())
+        self.mock_options()
+
+    def test_missing_name_generates_error(self):
+        self.assertRaises(Exception, self.action.process_options, ['info'])
+
+    def test_no_error_if_name_provided(self):
+        self.action.process_options(['info', '--name=role1'])
+        self.assertEqual(len(self.action.optErrors), 0)
+
+
+class OrgInfoTest(CLIActionTestCase):
+
+    ORG = test_data.ORGS[0]
+
+    OPTIONS = {
+        'name': ORG['name']
+    }
+
+    def setUp(self):
+        self.set_action(Info())
+        self.set_module(katello.client.core.organization)
+        self.mock_printer()
+
+        self.mock_options(self.OPTIONS)
+
+        self.mock(self.action.api, 'organization', self.ORG)
+
+    def test_finds_org(self):
+        self.action.run()
+        self.action.api.organization.assert_called_once_with(self.ORG['cp_key'])
+
+    def test_returns_ok(self):
+        self.assertEqual(self.action.run(), os.EX_OK)
