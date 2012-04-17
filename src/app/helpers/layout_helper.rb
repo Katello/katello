55dd7cb545e03cd9f38ddb91a98e88294a4833be
Eric D Helms <ehelms@redhat.com>
2012-04-17 16:52:04 -0400

    Changes the way inline javascript declarations are handled such that they are all injected into one universal script tag.

--- src/app/helpers/layout_helper.rb
+++ src/app/helpers/layout_helper.rb
@@ -5,7 +5,7 @@ module LayoutHelper
 
   def javascript(*args, &block)
     if block
-      content_for(:javascripts) { javascript_tag(block.call()) }
+      content_for(:inline_javascript) { block.call() }
     end
     if args
       args.map { |arg| content_for(:javascripts) { include_javascripts(arg) } }
