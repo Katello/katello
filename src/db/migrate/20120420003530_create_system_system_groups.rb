d489ced462ae1b2af9097c713153effcff3b1a6a
Brad Buckingham <bbuckingham@redhat.com>
2012-04-20 08:57:06 -0400

    system groups - adding an AR model relationship for system <-> system groups
    This commit contains the above model relation.  It may be possible
    to avoid having this model and store the information only in pulp; however
    that may not be the best in the long term.  In the future, we may also be
    performing actions on system groups outside of the context of pulp; therefore,
    storing the information locally will minimize the requests to pulp as
    well as it enables the elasticsearch indices to be defined/created/destroyed/
    updated automatically.

---
+++ src/db/migrate/20120420003530_create_system_system_groups.rb
@@ -0,0 +1,12 @@
+class CreateSystemSystemGroups < ActiveRecord::Migration
+  def self.up
+    create_table :system_system_groups do |t|
+      t.references :system
+      t.references :system_group
+    end    
+  end
+
+  def self.down
+    drop_table :system_system_groups
+  end
+end
