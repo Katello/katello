eabe31331706a8a8fb8fd7c1838f5ec71a89699e
Martin Bačovský <mbacovsk@redhat.com>
2012-04-25 12:22:21 +0200

    Addedscript for migration of Katello DB

--- puppet/upgrade-scripts/default/0001_migrate_katello_db.sh
+++ puppet/upgrade-scripts/default/0001_migrate_katello_db.sh
@@ -4,6 +4,13 @@
 #description:
 #Updates Katello database schema to the latest version
 
-# TODO
+# default configuration values (should be the same as in our sysv init script)
+KATELLO_HOME=${KATELLO_HOME:-/usr/share/katello}
+KATELLO_ENV=${KATELLO_ENV:-production}
 
-exit 0
+pushd $KATELLO_HOME >/dev/null
+RAILS_ENV=$KATELLO_ENV rake db:migrate --trace 2>&1
+ret_code=$?
+popd >/dev/null
+
+exit $ret_code
