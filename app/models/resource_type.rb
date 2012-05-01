1a36430e772fb64875545df8274ee01f6bd38d48
Justin Sherrill <jsherril@redhat.com>
2012-05-01 17:51:49 -0400

    %B

--- app/models/resource_type.rb
+++ app/models/resource_type.rb
@@ -93,6 +93,7 @@ class ResourceType < ActiveRecord::Base
       :environments => {:model => KTEnvironment, :name => _("Environments"), :global=>false},
       :activation_keys => { :model => ActivationKey, :name => _("Activation Keys"), :global=>false},
       :providers => { :model => Provider, :name => _("Providers"), :global=>false},
+      :system_group => {:model => SystemGroup, :name=>_("System Groups"), :global=>false},
       :filters => { :model => Filter, :name => _("Filters"), :global => false},
       :users => { :model => User, :name => _("Users"), :global=>true},
       :roles => { :model => Role, :name => _("Roles"), :global=>true},
