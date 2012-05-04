2af3191fbcf4ac2dc942f320c8c9af2c41aff47f
Tom McKay <thomasmckay@redhat.com>
2012-05-04 12:04:02 -0400

    818711 - use cache of release versions from CDN

--- src/app/models/provider.rb
+++ src/app/models/provider.rb
@@ -208,15 +208,17 @@ class Provider < ActiveRecord::Base
 
   def available_releases
     releases = []
-    #ca = File.read(CDN::CdnResource.ca_file)
-    self.products.engineering.each do |product|
-      #content_urls = product.productContent.map { |pc| pc.content.contentUrl }
-      cdn_var_substitutor = CDN::CdnVarSubstitutor.new(product.provider[:repository_url],
-                                                       :ssl_client_cert => OpenSSL::X509::Certificate.new(product.certificate),
-                                                       :ssl_client_key => OpenSSL::PKey::RSA.new(product.key))
-      product.productContent.each do |pc|
-        cdn_var_substitutor.substitute_vars(pc.content.contentUrl).each do |(substitutions, path)|
-          releases << CDN::Utils.parse_version(substitutions['releasever'])[:minor]
+    CDN::CdnVarSubstitutor.with_cache do
+      self.products.engineering.each do |product|
+        cdn_var_substitutor = CDN::CdnVarSubstitutor.new(product.provider[:repository_url],
+                                                         :ssl_client_cert => OpenSSL::X509::Certificate.new(product.certificate),
+                                                         :ssl_client_key => OpenSSL::PKey::RSA.new(product.key))
+        product.productContent.each do |pc|
+          if url_to_releases = pc.content.contentUrl[/^.*\$releasever/]
+            cdn_var_substitutor.substitute_vars(url_to_releases).each do |(substitutions, path)|
+              releases << CDN::Utils.parse_version(substitutions['releasever'])[:minor]
+            end
+          end
         end
       end
     end
