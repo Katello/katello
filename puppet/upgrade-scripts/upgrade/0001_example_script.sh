22129a877563692c5340de28245cbc145ce3d9cd
Tom McKay <thomasmckay@redhat.com>
2012-05-09 10:17:24 -0400

    820273 - removed change to example script

--- puppet/upgrade-scripts/upgrade/0001_example_script.sh
+++ puppet/upgrade-scripts/upgrade/0001_example_script.sh
@@ -4,14 +4,6 @@
 #apply: katello headpin
 #description: Empty bash script
 
-# default configuration values (should be the same as in our sysv init script)
-if [ -f /etc/sysconfig/katello ]; then . /etc/sysconfig/katello; fi
-KATELLO_HOME=${KATELLO_HOME:-/usr/share/katello}
-KATELLO_PREFIX=${KATELLO_PREFIX:-/katello}
-
-# Example of checking for an upgrade to apply only to katello, such as pulp upgrade
-if [ "$KATELLO_PREFIX" = "/katello" ] || [ "$KATELLO_PREFIX" = "/cfse" ]; then
-  echo "Example upgrade"
-fi
+# TODO
 
 exit 0
