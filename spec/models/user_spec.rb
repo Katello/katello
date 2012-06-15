47547889ab4e5f65244791808a07f440e3c90362
Jordan OMara <jomara@redhat.com>
2012-06-15 11:21:50 -0400

    819002 - Removing password & email validation for user creation in LDAP mode

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,9 +29,56 @@ describe User do
     }
   end
 
+  describe "Users in LDAP should" do
+    before(:each) do
+      disable_user_orchestration
+      AppConfig.warden = 'ldap'
+      @user = User.create_ldap_user!('testuser')
+    end
+
+    it 'be able to create' do
+      u = User.find_by_username('testuser')
+      u.should_not be_nil
+    end
+    
+    it 'not have an email' do
+      u = User.find_by_username('testuser')
+      u.email.should be_nil
+    end
+    
+    it "have its own role" do
+      #pending "implement own_role functionality"
+      @user.own_role.should_not be_nil
+    end
+
+    it "have permission for two orgs" do
+      disable_org_orchestration
+      org = Organization.create!(:name => 'test_organization', :cp_key => 'test_organization')
+      moreorg = Organization.create!(:name => 'another_test_organization', :cp_key => 'another_test_organization')
+      allow(@user.own_role, [:read], :providers, nil, org)
+      allow(@user.own_role,[:read], :providers, nil, moreorg)
+      @user.allowed_organizations.size.should == 2
+    end
+
+    specify { @user.cp_oauth_header.should == {'cp-user' => @user.username}}
+    specify { @user.pulp_oauth_header.should == {'pulp-user' => @user.username}}
+
+    it "be able to set default_environment" do
+      disable_org_orchestration
+      @organization = Organization.create!(:name => 'test_org', :cp_key => 'test_org')
+      @environment = KTEnvironment.create!(:name => 'test', :prior => @organization.library.id,
+                                           :organization => @organization)
+
+      @user.default_environment = @environment
+      @user.reload
+      @user.default_environment.should == @environment
+    end
+  end
+
   describe "User should" do
     before(:each) do
       disable_user_orchestration
+      AppConfig.warden = 'database'
       @user = User.create!(to_create_simple)
     end
 
