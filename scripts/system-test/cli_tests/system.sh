61f5108844ce69544c850a1c5c4dc8d232ff4311
Lukas Zapletal <lzap+git@redhat.com>
2012-06-21 12:50:49 +0200

    ulimit - system tests

--- scripts/system-test/cli_tests/system.sh
+++ scripts/system-test/cli_tests/system.sh
@@ -8,7 +8,7 @@ SYSTEM_NAME_ADMIN_2=$SYSTEM_NAME_ADMIN"_2"
 SYSTEM_NAME_USER="user_system_$RAND"
 ACTIVATION_KEY_NAME_1="activation_key_1_$RAND"
 ACTIVATION_KEY_NAME_2="activation_key_2_$RAND"
-
+ACTIVATION_KEY_NAME_3="activation_key_3_$RAND"
 
 test_success "system register as admin" system register --name="$SYSTEM_NAME_ADMIN" --org="$TEST_ORG" --environment="$TEST_ENV"
 skip_test_success "system register as $TEST_USER" "none" -u "$TEST_USER" -p password system register --name="$SYSTEM_NAME_USER" --org="$TEST_ORG" --environment="$TEST_ENV"
@@ -29,7 +29,17 @@ test_success "system update description" system update --org="$TEST_ORG" --name=
 test_success "system update location" system update --org="$TEST_ORG" --name="$SYSTEM_NAME_ADMIN" --location="The Grid"
 test_success "system update release" system update --org="$TEST_ORG" --name="$SYSTEM_NAME_ADMIN" --release="6.2"
 test_success "system unregister" system unregister --name="$SYSTEM_NAME_ADMIN" --org="$TEST_ORG"
+test_success "system unregister" system unregister --name="$SYSTEM_NAME_ADMIN_2" --org="$TEST_ORG"
 
 test_success "activation key create" activation_key create --name="$ACTIVATION_KEY_NAME_1" --description="key description" --environment="$TEST_ENV" --org="$TEST_ORG"
 test_success "activation key create" activation_key create --name="$ACTIVATION_KEY_NAME_2" --description="key description" --environment="$TEST_ENV" --org="$TEST_ORG"
-test_success "system register with activation key" system register --name="$SYSTEM_NAME_ADMIN" --org="$TEST_ORG" --activationkey="$ACTIVATION_KEY_NAME_1,$ACTIVATION_KEY_NAME_2"
+test_success "system register with activation key" system register --name="$SYSTEM_NAME_ADMIN" --org="$TEST_ORG" --activationkey="$ACTIVATION_KEY_NAME_1"
+test_success "system unregister" system unregister --name="$SYSTEM_NAME_ADMIN" --org="$TEST_ORG"
+test_success "system register with activation keys" system register --name="$SYSTEM_NAME_ADMIN" --org="$TEST_ORG" --activationkey="$ACTIVATION_KEY_NAME_1,$ACTIVATION_KEY_NAME_2"
+test_success "system unregister" system unregister --name="$SYSTEM_NAME_ADMIN" --org="$TEST_ORG"
+test_success "system unregister" system unregister --name="$SYSTEM_NAME_ADMIN_2" --org="$TEST_ORG"
+
+test_success "limited ak create" activation_key create --name="$ACTIVATION_KEY_NAME_3" --description="key description" --environment="$TEST_ENV" --org="$TEST_ORG" --limit=1
+test_success "system register with limited ak" system register --name="$SYSTEM_NAME_ADMIN" --org="$TEST_ORG" --activationkey="$ACTIVATION_KEY_NAME_3"
+test_failure "system register with limited ak" system register --name="$SYSTEM_NAME_ADMIN_2" --org="$TEST_ORG" --activationkey="$ACTIVATION_KEY_NAME_3"
+test_success "system unregister" system unregister --name="$SYSTEM_NAME_ADMIN" --org="$TEST_ORG"
