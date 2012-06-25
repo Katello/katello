2fe3c83d2e47e1c9704ee846ead7f79787955502
Lukas Zapletal <lzap+git@redhat.com>
2012-06-25 14:47:50 +0200

    installer review - introducing cpsetup_done file

--- puppet/modules/candlepin/manifests/config.pp
+++ puppet/modules/candlepin/manifests/config.pp
@@ -30,16 +30,15 @@ class candlepin::config {
 
   # this does not really work if you use a password
   require "certs::params"
-  exec {"cpsetup":
-    command => "/usr/share/candlepin/cpsetup -k ${certs::params::keystore_password} -u ${candlepin::params::db_user} -d ${candlepin::params::db_name} >> ${candlepin::params::cpsetup_log} 2>&1",
-    timeout => 300, # 5 minutes timeout (cpsetup can be really slow sometimes)
+  exec { "cpsetup":
+    command => "/usr/share/candlepin/cpsetup -k ${certs::params::keystore_password} -u ${candlepin::params::db_user} -d ${candlepin::params::db_name} >> ${candlepin::params::cpsetup_log} 2>&1 && touch /var/lib/katello/cpsetup_done",
+    timeout => 300, # 5 minutes timeout (cpsetup takes longer sometimes)
     require => [
       File["${katello::params::log_base}"],
       Postgres::Createuser[$candlepin::params::db_user],
       Class["certs::config"]
     ],
-    creates => "/etc/tomcat6/server.xml.original", # another hack not to run it again
-    #creates => "/etc/candlepin/certs/candlepin-ca.crt", # another hack not to run it again
-    before  => Class["apache2::service"]               # another hack, as we reuse cp certs by default
+    creates => "/var/lib/katello/cpsetup_done",
+    before  => Class["apache2::service"]
   }
 }
