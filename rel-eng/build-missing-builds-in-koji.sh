2761c80bd99ff27a532fe92cccae26bf19b52e8e
Miroslav Suchý <msuchy@redhat.com>
2012-06-26 16:39:33 +0200

    add utility scripts to allow building in koji

---
+++ rel-eng/build-missing-builds-in-koji.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+TAGS="katello-nightly-rhel6 katello-nightly-fedora16 katello-nightly-fedora17"
+FEDORA_UPLOAD=1
+
+pushd . >/dev/null
+pushd `dirname $0`/.. >/dev/null
+
+# say python to be nice to pipe
+export PYTHONUNBUFFERED=1
+
+echo 'Gathering data ...'
+for tag in $TAGS; do
+  rel-eng/koji-missing-builds.py $KOJI_MISSING_BUILD_BREW_ARG --no-extra $tag | \
+    perl -lne '/^\s+(.+)-.+-.+$/ and print $1' \
+    | xargs -I replacestring awk '{print $2}' rel-eng/packages/replacestring \
+    | sed "s/$/ $tag/"
+done \
+    | perl -lane '$X{$F[0]} .= " $F[1]"; END { for (sort keys %X) { print "$_$X{$_}" } }' \
+    | while read package_dir tags ; do
+      (
+      echo Building package in path $package_dir for $tags
+      cd $package_dir && \
+          ONLY_TAGS="$tags" ${TITO_PATH}tito release koji
+      )
+    if [ "0$FEDORA_UPLOAD" -eq 1 ] ; then
+      (
+      echo Uploading tgz for path $package_dir
+      cd $package_dir && LC_ALL=C ${TITO_PATH}tito build --tgz | \
+      awk '/Wrote:.*tar.gz/ {print $2}' | \
+      xargs -I packagepath scp packagepath fedorahosted.org:katello
+      )
+    fi
+    done
+
+popd >/dev/null
