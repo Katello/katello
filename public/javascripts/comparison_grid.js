d2a5e8a0ac3f68747e5c51e8a9f957adb7572444
Eric D Helms <ehelms@redhat.com>
2012-07-09 11:03:18 -0400

    %B

--- public/javascripts/comparison_grid.js
+++ public/javascripts/comparison_grid.js
@@ -350,13 +350,13 @@ KT.comparison_grid.models = function() {
 
     self.export_data = function(type) {
         if( type === "columns" ){
-            return { columns : $.extend(true, {}, self.columns) };
+            return { columns : self.columns };
         } else if( type === "rows" ){
             return { rows : $.extend(true, {}, self.rows.get()) };
         } else if( type === "mode" ){
             return { mode : self.mode };
         } else {
-            return { columns : $.extend(true, {}, self.columns), 
+            return { columns : self.columns, 
                     rows : $.extend(true, {}, self.rows.get()), 
                     mode : self.mode };
         }
