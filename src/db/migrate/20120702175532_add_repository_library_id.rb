c19f1d8da792783bae2b73a6c5076dd96738fa45
Justin Sherrill <jsherril@redhat.com>
2012-07-16 15:02:04 -0400

    content browser - fixing migration script to properly propogate

--- src/db/migrate/20120702175532_add_repository_library_id.rb
+++ src/db/migrate/20120702175532_add_repository_library_id.rb
@@ -5,18 +5,29 @@ class AddRepositoryLibraryId < ActiveRecord::Migration
       end
       Repository.reset_column_information
       User.current = User.hidden.first
-      Repository.all.each do |lib_repo|
+
+      Repository.enabled.each do |lib_repo|
         if lib_repo.environment.library?
-          KTEnvironment.where(:organization_id=>lib_repo.organization.id).each do |env|
-            if lib_repo.is_cloned_in?(env)
-              clone = lib_repo.get_clone(env)
-              clone.library_instance_id = lib_repo.id
-              clone.save!
+          Organization.all.each do |org|
+            org.promotion_paths.each  do |path|
+              process_path(lib_repo, path[0])
             end
           end
         end
       end
   end
+
+  def self.process_path lib_repo, initial_env
+    env = initial_env
+    clone = lib_repo.get_clone(initial_env)
+    while clone
+      clone.library_instance_id = lib_repo.id
+      clone.save!
+      env = env.successor
+      clone = env.nil? ? nil : clone.get_clone(env)
+    end
+  end
+
 
   def self.down
       remove_column :repositories, :library_instance_id
