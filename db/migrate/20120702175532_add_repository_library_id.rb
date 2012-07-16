69deb3c22aa2600f4644ca9f0f2465f9f42c60bb
Justin Sherrill <jsherril@redhat.com>
2012-07-16 10:48:53 -0400

    content browser - fixing migration to migrate clone.library_instance_id properly

--- db/migrate/20120702175532_add_repository_library_id.rb
+++ db/migrate/20120702175532_add_repository_library_id.rb
@@ -3,9 +3,22 @@ class AddRepositoryLibraryId < ActiveRecord::Migration
       change_table :repositories do |t|
           t.integer :library_instance_id, :null=>true
       end
+      Repository.reset_column_information
+      User.current = User.hidden.first
+      Repository.all.each do |lib_repo|
+        if lib_repo.environment.library?
+          KTEnvironment.where(:organization_id=>lib_repo.organization.id).each do |env|
+            if lib_repo.is_cloned_in?(env)
+              clone = lib_repo.get_clone(env)
+              clone.library_instance_id = lib_repo.id
+              clone.save!
+            end
+          end
+        end
+      end
   end
 
   def self.down
-      remove_column :library_instance_id
+      remove_column :repositories, :library_instance_id
   end
 end
