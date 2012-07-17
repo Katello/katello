46f93e001991cba194549e484f1700c20187ee39
Brad Buckingham <bbuckingham@redhat.com>
2012-07-17 14:21:43 -0400

    %B

--- app/models/system_group.rb
+++ app/models/system_group.rb
@@ -3,7 +3,7 @@
 #
 # This software is licensed to you under the GNU General Public
 # License as published by the Free Software Foundation; either version
-# 2 of the License (GPLv2) or (at your option)  later version.
+# 2 of the License (GPLv2) or (at your option) any later version.
 # There is NO WARRANTY for this software, express or implied,
 # including the implied warranties of MERCHANTABILITY,
 # NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
