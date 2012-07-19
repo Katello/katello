09cd3d2992a35820a5e8c3327637f46025d89d52
Partha Aji <paji@redhat.com>
2012-07-19 14:27:58 -0400

    Forgot to undo one part

--- src/db/migrate/20120718211006_adding_types_to_changeset.rb
+++ src/db/migrate/20120718211006_adding_types_to_changeset.rb
@@ -2,9 +2,6 @@ class AddingTypesToChangeset < ActiveRecord::Migration
   def self.up
     add_column :changesets, :action_type, :string, :default => Changeset::PROMOTION
     Changeset.reset_column_information
-    Changeset.all.each do  |changeset|
-      changeset.update_attributes!(:action_type => Changeset::PROMOTION)
-    end
     execute("UPDATE changesets
              SET action_type = '#{Changeset::PROMOTION}'")
 
