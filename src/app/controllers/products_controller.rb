bc6dba210e9beefbf5ed13647db2faffe7de2da3
Justin Sherrill <jsherril@redhat.com>
2012-07-20 12:38:10 -0400

    CS - properly handling search error

--- src/app/controllers/products_controller.rb
+++ src/app/controllers/products_controller.rb
@@ -121,8 +121,8 @@ class ProductsController < ApplicationController
       filter :term, {:organization_id => org.id}
     end
     render :json=>products.collect{|s| {:label=>s.name, :value=>s.name, :id=>s.id}}
-  rescue Exception => error
-    render :json=>[]
+  rescue Tire::Search::SearchRequestFailed => e
+     render :json=>Support.array_with_total
   end
 
 
