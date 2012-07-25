d7578569f861b70cc6f3be6b3aeebe7bcbf42dc3
Brad Buckingham <bbuckingham@redhat.com>
2012-07-25 16:25:04 -0400

    content-deletion - associate proper env with changeset upon creation
    When a changeset is created, it is associated with an environment.
    This commit will associate the changeset with the proper environment
    based upon the type of changeset.
    
    In the case of a deletion changeset, it needs to be associated with
    the current environment (e.g. dev, if in dev env).
    
    In the case of a promotion changeset, it needs to be associated with
    the next environment where the content is being promoted to
    (e.g. if in dev and next env is test, it would be set to test).

--- src/app/controllers/changesets_controller.rb
+++ src/app/controllers/changesets_controller.rb
@@ -78,8 +78,6 @@ class ChangesetsController < ApplicationController
     'contents'
   end
 
-
-
   ####
   # Promotion methods
   ####
@@ -105,9 +103,11 @@ class ChangesetsController < ApplicationController
 
   def create
     begin
-      env = params[:action_type] == Changeset::PROMOTION ? @environment: @environment.prior
+      env = params[:action_type] == Changeset::PROMOTION ? @next_environment: @environment
       @changeset = Changeset.create_for(params[:action_type], :name => params[:name], :description => params[:description],
                                       :environment_id => env.id)
+
+
       notify.success _("Promotion Changeset '%s' was created.") % @changeset["name"]
       bc = {}
       add_crumb_node!(bc, changeset_bc_id(@changeset), '', @changeset.name, ['changesets'],
@@ -217,7 +217,7 @@ class ChangesetsController < ApplicationController
 
   def promote
     messages = {}
-    if !params[:confirm] && @environment.prior.library?
+    if !params[:confirm] && @next_environment.prior.library?
       syncing = []
       errors = []
 
@@ -239,7 +239,7 @@ class ChangesetsController < ApplicationController
       @changeset.promote :notify => true, :async => true
       # remove user edit tracking for this changeset
       ChangesetUser.destroy_all(:changeset_id => @changeset.id)
-      notify.success _("Started promotion of '%s' to %s environment") % [@changeset.name, @environment.name]
+      notify.success _("Started promotion of '%s' to %s environment") % [@changeset.name, @next_environment.name]
     end
     render :json=>to_ret
   rescue Exception => e
@@ -254,7 +254,6 @@ class ChangesetsController < ApplicationController
     render :json=>to_ret
   end
 
-
   private
 
   def find_environment
@@ -266,6 +265,10 @@ class ChangesetsController < ApplicationController
       #didnt' find an environment, just do the first the user has access to
       list = KTEnvironment.changesets_readable(current_organization).where(:library=>false)
       @environment ||= list.first || current_organization.library
+    end
+
+    if params[:next_env_id]
+      @next_environment = KTEnvironment.find(params[:next_env_id])
     end
   end
 
