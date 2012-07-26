bb7695e1686fdd4de799de78dd780eef1020d67b
Justin Sherrill <jsherril@redhat.com>
2012-07-26 11:38:48 -0400

    CS - fixing issue where environments were not properly remembered

--- src/public/javascripts/comparison_grid.js
+++ src/public/javascripts/comparison_grid.js
@@ -350,13 +350,13 @@ KT.comparison_grid.models = function() {
 
     self.export_data = function(type) {
         if( type === "columns" ){
-            return { columns : self.columns };
+            return { columns :$.extend(true, [], self.columns) };
         } else if( type === "rows" ){
             return { rows : $.extend(true, {}, self.rows.get()) };
         } else if( type === "mode" ){
             return { mode : self.mode };
         } else {
-            return { columns : self.columns, 
+            return { columns : $.extend(true, [], self.columns),
                     rows : $.extend(true, {}, self.rows.get()), 
                     mode : self.mode };
         }
