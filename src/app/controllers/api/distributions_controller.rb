2cb79959059d3879f87171c5bc84fe5cd5c44131
Lukas Zapletal <lzap+git@redhat.com>
2012-07-27 15:38:09 +0200

    api docs - distributions

--- src/app/controllers/api/distributions_controller.rb
+++ src/app/controllers/api/distributions_controller.rb
@@ -25,14 +25,13 @@ class Api::DistributionsController < Api::ApiController
     }
   end
 
-  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
   api :GET, "/repositories/:repository_id/distributions", "List distributions"
   def index
     render :json => @repo.distributions
   end
 
-  # DOC GENERATED AUTOMATICALLY: REMOVE THIS LINE TO PREVENT REGENARATING NEXT TIME
   api :GET, "/repositories/:repository_id/distributions/:id", "Show a distribution"
+  param :repository_id, :number, :desc => "repository numeric id"
   def show
     dist = Glue::Pulp::Distribution.find(params[:id])
     render :json => dist
