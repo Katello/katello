65799ef46a10be46ff7fa0454a4daf6fc6ddd3e8
Justin Sherrill <jsherril@redhat.com>
2012-07-27 15:47:49 -0400

    CS - using newer errata icon classes

--- src/app/helpers/content_search_helper.rb
+++ src/app/helpers/content_search_helper.rb
@@ -11,7 +11,7 @@ module ContentSearchHelper
 
 
   def errata_display errata
-      types =  {'bugfix'=>'bugzilla_icon', 'enhancement'=>'enhancement_icon', 'security'=>'security_icon'}
+      types =  {'bugfix'=>'bug_icon_black', 'enhancement'=>'plus_icon_black', 'security'=>'shield_icon_black'}
       icon_class = types[errata[:type]] ||  'enhancement_icon'
       url = short_details_erratum_path(errata.id)
       return "<i class=\"errata-icon #{icon_class}\"  />" + "<span class=\"tipsify-errata\" data-url=\"#{url}\">#{errata.id}</span>"
