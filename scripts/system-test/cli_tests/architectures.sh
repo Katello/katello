32e9360bd586549bb6efdb66c9bca6d95af542d8
Tomas Strachota <tomas.str@gmail.com>
2012-08-08 10:10:19 +0200

    architectures cli - show action renamed to info to keep naming consistency

--- scripts/system-test/cli_tests/architectures.sh
+++ scripts/system-test/cli_tests/architectures.sh
@@ -7,7 +7,7 @@ NEW_ARCH_NAME="b_$RAND"
 
 #testing architectures
 test_success "architecture create" architecture create --name="$ARCH_NAME"
-test_success "architecture info" architecture show --name="$ARCH_NAME"
+test_success "architecture info" architecture info --name="$ARCH_NAME"
 test_success "architecture update" architecture update --name="$ARCH_NAME" --new_name="$NEW_ARCH_NAME"
 test_success "architecture list" architecture list
 test_failure "architecture try to delete old name" architecture delete --name="$ARCH_NAME"
