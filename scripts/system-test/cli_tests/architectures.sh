bd961561ace146c0c01121314630866465e0647e
Tomas Strachota <tomas.str@gmail.com>
2012-08-08 10:00:33 +0200

    architectures - cli smoke tests

---
+++ scripts/system-test/cli_tests/architectures.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+header "Architectures"
+
+ARCH_NAME="a_$RAND"
+NEW_ARCH_NAME="b_$RAND"
+
+#testing architectures
+test_success "architecture create" architecture create --name="$ARCH_NAME"
+test_success "architecture info" architecture show --name="$ARCH_NAME"
+test_success "architecture update" architecture update --name="$ARCH_NAME" --new_name="$NEW_ARCH_NAME"
+test_success "architecture list" architecture list
+test_failure "architecture try to delete old name" architecture delete --name="$ARCH_NAME"
+test_success "architecture delete" architecture delete --name="$NEW_ARCH_NAME"
+
