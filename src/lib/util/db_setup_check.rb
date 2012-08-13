9752bf89e103db7ed87325b0293ea137d9f0c18a
Ivan Necas <inecas@redhat.com>
2012-08-13 14:46:52 +0200

    api doc - don't require database to generate the html files

--- src/lib/util/db_setup_check.rb
+++ src/lib/util/db_setup_check.rb
@@ -3,7 +3,9 @@ module Katello
     # Ensures sqlite is not used as a database engine. It's not supported
     # by the Katello project.
     def self.check!
-      if ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'sqlite3' and not ENV['FORCE_DB_SETUP']
+      if ActiveRecord::Base.configurations[Rails.env] and
+          ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'sqlite3' and
+          not ENV['FORCE_DB_SETUP']
         raise 'SQLite3 is not supported. If you still want to use this adapeter, set FORCE_DB_SETUP=true.'
       end
     end
