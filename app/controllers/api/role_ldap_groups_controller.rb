568194d49b17321d9de5d2b99434b5e5943bc586
Martin Bačovský <mbacovsk@redhat.com>
2012-08-16 18:14:58 +0200

    apidoc - docs for role_ldap_groups_controller

--- app/controllers/api/role_ldap_groups_controller.rb
+++ app/controllers/api/role_ldap_groups_controller.rb
@@ -27,16 +27,23 @@ class Api::RoleLdapGroupsController < Api::ApiController
     }
   end
 
+  api :POST, "/roles/:role_id/ldap_groups", "Add group to list of LDAP groups associated with the role"
+  param :name, String, "name of the LDAP group"
   def create
     @role.add_ldap_group(params[:name])
     render :text => _("Added LDAP group '%s'") % params[:name], :status => 200
   end
 
+  api :DELETE, "/roles/:role_id/ldap_groups/:id", "Remove group from the list of LDAP groups associated with the role"
+  param :role_id, :identifier, :desc => "role identifier"
+  param :id, String, :desc => "ldap group (name)" 
   def destroy
     @role.remove_ldap_group(params[:id])
     render :text => _("Removed LDAP group '%s'") % params[:id], :status => 200
   end
 
+  api :GET, "/roles/:role_id/ldap_groups", "List LDAP groups associated with the role"
+  param :role_id, :identifier, :desc => "role identifier"
   def index
     render :json => @role.ldap_group_roles.where(query_params).to_json()
   end
