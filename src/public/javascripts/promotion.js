735811b6dc41b1eab3873998fcab757acc003b08
Brad Buckingham <bbuckingham@redhat.com>
2012-08-21 11:49:51 -0400

    content-deletion - update so that clicking on undefined changeset category doesnothing
    When the user is in Library, they cannot create deletion changesets.
    When the user is in the last environment, they cannot create promotion
    changesets.
    
    To address this, we made the text for the category blank; however,
    we were still binding to the category, so the code would attempt
    to switch to it.  This change basically makes the category completely
    empty in those cases so clicking will do nothing, but pg format remains
    correct.

--- src/public/javascripts/promotion.js
+++ src/public/javascripts/promotion.js
@@ -1026,7 +1026,7 @@ var changeset_obj = function(data_struct) {
 var registerEvents = function(){
     $('.tipsify').tipsy({fade : true, gravity : 's', live : true, delayIn : 500, hoverable : true, delayOut : 50 });
 
-    $('.sliding_tree_category').live('click', function() {
+    $('.sliding_tree_category[data-cs_type]').live('click', function() {
         // user clicked on a changeset category (promotion/deletion)
         var selected = $(this);
         if (selected.hasClass('selected')) {
