Katello/katello

commit 3a3086b14ea6636d8047c4b1e67db41e482fd8f0

content deletion - adding CLI actions

cli/src/katello/client/core/changeset.py
@@ -58,6 +58,7 @@ class List(ChangesetAction):
 
         self.printer.add_column('id')
         self.printer.add_column('name')
+        self.printer.add_column('type')
         self.printer.add_column('updated_at', formatter=format_date)
         self.printer.add_column('state')
         self.printer.add_column('environment_id')
@@ -111,6 +112,7 @@ class Info(ChangesetAction):
 
         self.printer.add_column('id')
         self.printer.add_column('name')
+        self.printer.add_column('type')
         self.printer.add_column('description', multiline=True, show_with=printer.VerboseStrategy)
         self.printer.add_column('updated_at', formatter=format_date)
         self.printer.add_column('state')
@@ -144,6 +146,12 @@ class Create(ChangesetAction):
                                help=_("changeset name (required)"))
         parser.add_option('--description', dest='description',
                                help=_("changeset description"))
+        parser.add_option('--promotion', dest='type_promotion', action="store_true", default=False,
+                               help=_("changeset type promotion: pushes changes to the next environment [DEFAULT]"))
+        parser.add_option('--deletion', dest='type_deletion', action="store_true", default=False,
+                               help=_("changeset type deletion: deletes items in changeset from current environment"))
+
+
 
     def check_options(self, validator):
         validator.require(('org', 'name', 'env'))
@@ -153,9 +161,18 @@ class Create(ChangesetAction):
         envName = self.get_option('env')
         csName = self.get_option('name')
         csDescription = self.get_option('description')
+        csType = 'PROMOTION'
+
+        # Check for duplicate type flags
+        if self.get_option('type_promotion') and self.get_option('type_deletion'):
+            raise OptionValueError(_("specify either --promotion or --deletion but not both"))
+        if self.get_option('type_promotion'):
+            csType = 'PROMOTION'
+        elif self.get_option('type_deletion'):
+            csType = 'DELETION'
 
         env = get_environment(orgName, envName)
-        cset = self.api.create(orgName, env["id"], csName, csDescription)
+        cset = self.api.create(orgName, env["id"], csName, csType, csDescription)
         test_record(cset,
             _("Successfully created changeset [ %s ] for environment [ %s ]") % (csName, env["name"]),
             _("Could not create changeset [ %s ] for environment [ %s ]") % (csName, env["name"])
@@ -404,8 +421,8 @@ class Delete(ChangesetAction):
 
 
 # ==============================================================================
-class Promote(ChangesetAction):
-    description = _('promotes a changeset to the next environment')
+class Apply(ChangesetAction):
+    description = _('applies a changeset based on the type (promotion, deletion)')
 
     def setup_parser(self, parser):
         parser.add_option('--name', dest='name',

cli/src/katello/client/api/changeset.py
@@ -36,11 +36,12 @@ class ChangesetAPI(KatelloAPI):
         else:
             return None
 
-    def create(self, orgName, envId, name, description=None):
+    def create(self, orgName, envId, name, type, description=None):
         data = {
             "changeset": {
                 "name": name,
-                "description": description
+                "description": description,
+                "type": type
             }
         }
         path = "/api/organizations/%s/environments/%s/changesets/" % (orgName, envId)
@@ -63,8 +64,8 @@ class ChangesetAPI(KatelloAPI):
         deps = self.server.GET(path)[1]
         return deps
 
-    def promote(self, csId):
-        path = "/api/changesets/%s/promote" % (csId)
+    def apply(self, csId):
+        path = "/api/changesets/%s/apply" % (csId)
         return self.server.POST(path)[1]
 
     def update_content(self, csId, patch):

cli/src/katello/client/api/utils.py
@@ -123,6 +123,8 @@ def get_changeset(orgName, envName, csName):
     if cset == None:
         raise ApiDataError(_("Could not find changeset [ %s ] within environment [ %s ]") %
             (csName, env["name"]))
+    # TODO: GET THIS FROM THE ACTUAL API
+    cset['type'] = 'promotion'
     return cset
 
 def get_user(userName):

cli/src/katello/client/main.py
@@ -242,7 +242,7 @@ def setup_admin(katello_cmd):
     cset_cmd.add_command('info', changeset.Info())
     cset_cmd.add_command('update', changeset.UpdateContent())
     cset_cmd.add_command('delete', changeset.Delete())
-    cset_cmd.add_command('promote', changeset.Promote())
+    cset_cmd.add_command('apply', changeset.Apply())
     katello_cmd.add_command('changeset', cset_cmd)
 
     client_cmd = client.Client()

cli/test/katello/tests/core/changeset/changeset_create_test.py
@@ -1,0 +1,53 @@
+import unittest
+from mock import Mock
+import os
+
+from katello.tests.core.action_test_utils import CLIOptionTestCase, CLIActionTestCase
+from katello.tests.core.organization.organization_data import ORGS, ENVS
+
+import katello.client.core.changeset
+from katello.client.core.changeset import Create
+
+class RequiredCLIOptionsTests(CLIOptionTestCase):
+
+    action = Create()
+
+    disallowed_options = [
+        ('--name=changeset1', ),
+        ('--org=ACME', )
+    ]
+
+    allowed_options = [
+        ('--org=ACME', '--name=changeset1', '--env=DEV', '--promotion', '--deletion')
+    ]
+
+
+class ChangesetAddTest(CLIActionTestCase):
+    ORG = 'org'
+    NAME = 'changeset1'
+    DESCRIPTION = 'description'
+    TYPE = 'promotion'
+
+    OPTIONS = {
+        'org':ORG,
+        'name':NAME,
+        'description':DESCRIPTION,
+        'type':TYPE
+    }
+
+    def setUp(self):
+        self.set_action(Create())
+        self.set_module(katello.client.core.changeset)
+        self.mock_printer()
+        self.mock_options(self.OPTIONS)
+
+        self.mock(self.action.api, 'create', [])
+        self.mock(self.module, 'get_environment', ENVS[0])
+
+    def tearDown(self):
+        self.restore_mocks()
+
+
+    def test_it_uses_filter_create_api(self):
+        self.run_action()
+        self.action.api.create.assert_called_once_with(self.ORG, ENVS[0]['id'], self.NAME, self.DESCRIPTION, self.TYPE)

commit 2596fb14ae2e3cedfc0d63e483bb764b1a91e229

fix runtime errors
addressing pylint:
E1120:266,15:Promote.run: No value passed for parameter 'type' in function call
E1101:269,19:Promote.run: Instance of 'ChangesetAPI' has no 'promote' member

This was caused by commit 3a3086b14ea6636d8047c4b1e67db41e482fd8f0

cli/src/katello/client/constants.py
@@ -16,6 +16,8 @@
 #
 #
 
+from gettext import gettext as _
+
 STATUS_DETAIL_SUCCESS = """
      Name          \t%-20s
      Result        \t%-20s
@@ -37,3 +39,13 @@ SELECTION_QUERY = """
   'c'   \t : clear selections
   'q'   \t : abort the repo creation
 """
+
+PROMOTION = 'PROMOTION'
+DELETION = 'DELETION'
+
+# Help string for optparser
+OPT_HELP_PROMOTION = _("changeset type promotion: pushes changes to the next environment [DEFAULT]")
+OPT_HELP_DELETION = _("changeset type deletion: deletes items in changeset from current environment")
+OPT_ERR_PROMOTION_OR_DELETE = _("specify either --promotion or --deletion but not both")
+
+

cli/src/katello/client/core/changeset.py
@@ -18,6 +18,7 @@ import os
 from gettext import gettext as _
 from optparse import OptionValueError
 
+from katello.client import constants
 from katello.client.api.changeset import ChangesetAPI
 from katello.client.core.base import BaseAction, Command
 from katello.client.core.utils import test_record, run_spinner_in_bg, format_date, wait_for_async_task, AsyncTask, format_task_errors
@@ -145,9 +146,9 @@ class Create(ChangesetAction):
         parser.add_option('--description', dest='description',
                                help=_("changeset description"))
         parser.add_option('--promotion', dest='type_promotion', action="store_true", default=False,
-                               help=_("changeset type promotion: pushes changes to the next environment [DEFAULT]"))
+                               help=constants.OPT_HELP_PROMOTION)
         parser.add_option('--deletion', dest='type_deletion', action="store_true", default=False,
-                               help=_("changeset type deletion: deletes items in changeset from current environment"))
+                               help=constants.OPT_ERR_PROMOTION_OR_DELETE)
 
 
 
@@ -159,15 +160,15 @@ class Create(ChangesetAction):
         envName = self.get_option('env')
         csName = self.get_option('name')
         csDescription = self.get_option('description')
-        csType = 'PROMOTION'
+        csType = constants.PROMOTION
 
         # Check for duplicate type flags
         if self.get_option('type_promotion') and self.get_option('type_deletion'):
-            raise OptionValueError(_("specify either --promotion or --deletion but not both"))
+            raise OptionValueError(constants.OPT_ERR_PROMOTION_OR_DELETE)
         if self.get_option('type_promotion'):
-            csType = 'PROMOTION'
+            csType = constants.PROMOTION
         elif self.get_option('type_deletion'):
-            csType = 'DELETION'
+            csType = constants.DELETION
 
         env = get_environment(orgName, envName)
         cset = self.api.create(orgName, env["id"], csName, csType, csDescription)
@@ -468,7 +469,7 @@ class Promote(Apply):
 
         # Block attempts to call this on deletion changesets, otherwise continue
         cset = get_changeset(orgName, envName, csName)
-        if cset['type'] == 'DELETION':
+        if cset['type'] == constants.DELETION:
             print _("This is a deletion changeset and does not support promotion")
             return os.EX_DATAERR
 

cli/src/katello/client/core/product.py
@@ -18,6 +18,7 @@ import os
 from gettext import gettext as _
 import datetime
 
+from katello.client import constants
 from katello.client.core import repo
 from katello.client.api.product import ProductAPI
 from katello.client.api.repo import RepoAPI
@@ -263,10 +264,10 @@ class Promote(SingleProductAction):
 
         returnCode = os.EX_OK
 
-        cset = self.csapi.create(orgName, env["id"], self.create_cs_name())
+        cset = self.csapi.create(orgName, env["id"], self.create_cs_name(), constants.PROMOTION)
         try:
             self.csapi.add_content(cset["id"], "products", {'product_id': prod['id']})
-            task = self.csapi.promote(cset["id"])
+            task = self.csapi.apply(cset["id"])
             task = AsyncTask(task)
 
             run_spinner_in_bg(wait_for_async_task, [task], message=_("Promoting the product, please wait... "))