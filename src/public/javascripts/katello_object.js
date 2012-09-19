b8ec125f734055159d67d65123ff5f5064c4099c
Brad Buckingham <bbuckingham@redhat.com>
2012-09-19 15:50:25 -0400

    object labels - update ui for setting label values based upon server query
    This commit adds some additional logic to the UI for handling the
    setting of object.label based on a server query.  They changes are
    primarily to improve the behavior for the user.  E.g.
    
    - when user enters name, put spinner on the 'label' so user can see
      something is happening
    - after user enters the name, if they click the name and leave,
      don't prompt the server for a new label... only do that if the
      name is changing
    - additional error handling...etc

--- src/public/javascripts/katello_object.js
+++ src/public/javascripts/katello_object.js
@@ -17,11 +17,11 @@ KT.object = KT.object || {};
 // name and upon completion, we want to retrieve a 'default' label from
 // the server generated off of that name.
 KT.object.label = (function(){
-    var label_input,
-        create_button,
+    var create_button,
+        initial_name_value,
+
         initialize = function(){
-            label_input = $(".label_input");
-            if (label_input.length === 0){
+            if ($(".label_input").length === 0){
                 return;
             }
             create_button = $(".create_button");
@@ -29,35 +29,55 @@ KT.object.label = (function(){
             disable_inputs(undefined);
 
             $('.name_input').bind('focusout', function(){
-                var name_input = $(this);
-                if (name_input.val().length > 0) {
-                    // user entered a name so go fetch the label from the server
-                    disable_inputs(name_input);
+                var name_input = $(this),
+                    name = name_input.val(),
+                    label = $(this).closest('fieldset').next('fieldset'),
+                    label_input = label.find('input.label_input');
+
+                if ((name.length > 0) && (name !== initial_name_value)) {
+                    // user changed the name so go fetch the label from the server
+                    show_label(label, false);
+
                     $.ajax({
                         type: "GET",
                         url: label_input.data("url"),
-                        data: {name:name_input.val()},
+                        data: {name:name},
                         cache: false,
                         success: function(data) {
                             if (data.length > 0) {
                                 // locate the label input and set it to the value received
-                                name_input.closest('fieldset').next('fieldset').find('input.label_input').val(data);
-                                enable_inputs(name_input);
+                                label_input.val(data);
                             }
+                            enable_inputs(name_input);
+                            show_label(label, true);
                         },
                         error: function() {
+                            enable_inputs(name_input);
+                            show_label(label, true);
                         }
                     });
+                } else {
+                    enable_inputs(name_input);
                 }
             });
-            $('.name_input').bind('click', function() {
+            $('.name_input').bind('click, focusin', function() {
                 disable_inputs($(this));
+                initial_name_value = $(this).val();
             })
         },
+    show_label = function(label, show) {
+        if (show === true) {
+            label.find('input.label_input').removeClass('hidden');
+            label.find('img.label_spinner').addClass('hidden');
+        } else {
+            label.find('input.label_input').addClass('hidden');
+            label.find('img.label_spinner').removeClass('hidden');
+        }
+    },
     disable_inputs = function(current_name_input) {
         if (current_name_input === undefined) {
             // disable all label inputs
-            label_input.attr("disabled", "disabled");
+            $(".label_input").attr("disabled", "disabled");
         } else {
             // disable the label input associated with the current name
             current_name_input.closest('fieldset').next('fieldset').find('input.label_input').attr("disabled", "disabled");
@@ -67,7 +87,7 @@ KT.object.label = (function(){
     enable_inputs = function(current_name_input) {
         if (current_name_input === undefined) {
             // enable all label inputs
-            label_input.removeAttr("disabled");
+            $(".label_input").removeAttr("disabled");
         } else {
             // enable the label input associated with the current name
             current_name_input.closest('fieldset').next('fieldset').find('input.label_input').removeAttr("disabled");
