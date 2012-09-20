5d97524ef3aa1d135aa2e583b4d66a237263875f
Eric D. Helms <ehelms@redhat.com>
2012-09-20 15:41:42 -0400

    PulpV2 - Adds glue layer unit tests for glue/pulp/user combining VCR to record Pulp data.

---
+++ test/minitest_helper.rb
@@ -0,0 +1,12 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../config/environment', __FILE__)
+
+require "minitest/autorun"
+require "vcr"
+
+#def configure_vcr(record_mode=:all)
+VCR.configure do |c|
+  c.cassette_library_dir = 'test/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.default_cassette_options = { :record => :all } #record_mode } #forcing all requests to Pulp currently
+end
