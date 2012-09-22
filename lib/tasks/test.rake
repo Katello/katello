251e82fcd04edbec69ee905e70414b8567f9b881
Eric D. Helms <ehelms@redhat.com>
2012-09-22 15:49:09 -0400

    PulpV2 - Adds a base set of Katello User model unit tests that disable inclusion of glue layer and only tests the Katello User model using Minitest Unittest and fixtures.

--- lib/tasks/test.rake
+++ lib/tasks/test.rake
@@ -33,3 +33,19 @@ end
 rescue LoadError
   # rspec not present, skipping this definition
 end
+
+Rake::TaskManager.class_eval do
+  def remove_task(task_name)
+    @tasks.delete(task_name.to_s)
+  end
+end
+
+Rake.application.remove_task 'db:test:prepare'
+
+namespace :db do
+  namespace :test do 
+    task :prepare do |t|
+      # rewrite the task to not do anything you don't want
+    end
+  end
+end
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,12 +1,20 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 
-require "minitest/autorun"
-require "vcr"
+require 'minitest/autorun'
+require 'minitest/rails'
 
-#def configure_vcr(record_mode=:all)
-VCR.configure do |c|
-  c.cassette_library_dir = 'test/fixtures/vcr_cassettes'
-  c.hook_into :webmock
-  c.default_cassette_options = { :record => :all } #record_mode } #forcing all requests to Pulp currently
+class MiniTest::Rails::ActiveSupport::TestCase
+  #fixtures :users
+end
+
+def configure_vcr
+  require "vcr"
+
+  #def configure_vcr(record_mode=:all)
+  VCR.configure do |c|
+    c.cassette_library_dir = 'test/fixtures/vcr_cassettes'
+    c.hook_into :webmock
+    c.default_cassette_options = { :record => :all } #record_mode } #forcing all requests to Pulp currently
+  end
 end
