aaeb782480cc52659966c7f97c5a19eb105365e0
Petr Chalupa <pchalupa@redhat.com>
2012-10-10 13:01:30 +0200

    reuse User foreman orchestration disablement in tests
    remove previous hackish stubbing

--- src/spec/support/foreman_user_disablement.rb
+++ src/spec/support/foreman_user_disablement.rb
@@ -10,36 +10,13 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-RSpec.configure do |config|
-  config.before do
-    User._disable_foreman_orchestration = nil
+if AppConfig.use_foreman
+  RSpec.configure do |config|
+    config.before do
+      @_original_disable_foreman_orchestration = User.foreman_orchestration_disabled?
+    end
+    config.after do
+      User.disable_foreman_orchestration! @_original_disable_foreman_orchestration
+    end
   end
-  config.after do
-    User._disable_foreman_orchestration = nil
-  end
-end
-
-User.singleton_class.send :attr_accessor, :_disable_foreman_orchestration
-User.singleton_class.send :public, :_disable_foreman_orchestration, :_disable_foreman_orchestration=
-
-disable_foreman_orchestration_methods = lambda do |user|
-  [:create_foreman_user, :update_foreman_user, :destroy_foreman_user, :foreman_consistency_check].each do |method|
-    user.stub!(method).and_return(true)
-  end
-end
-
-method_body = lambda do |original_method, *args, &block|
-  user = original_method.call(*args, &block)
-  disable_foreman_orchestration_methods.call user if User._disable_foreman_orchestration
-  user
-end
-
-original_new      = User.method :new
-User.singleton_class.send(:define_method, :new) do |*args, &block|
-  method_body.call original_new, *args, &block
-end
-
-original_allocate = User.method :allocate
-User.singleton_class.send(:define_method, :allocate) do
-  method_body.call original_allocate
 end
