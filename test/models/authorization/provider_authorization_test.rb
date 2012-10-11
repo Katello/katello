93e9969814dc5ea11ed07d587b4e24ec25836f80
Justin Sherrill <jsherril@redhat.com>
2012-10-11 16:29:00 -0400

    spliting off provider auth functions

---
+++ test/models/authorization/provider_authorization_test.rb
@@ -0,0 +1,98 @@
+#
+# Copyright 2012 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+require 'models/repository_base'
+
+
+class ProviderAuthorizationAdminTest < MiniTest::Rails::ActiveSupport::TestCase
+  include RepositoryTestBase
+
+  def setup
+    super
+    User.current = User.find(users('admin'))
+    @provider = Provider.find(providers('fedora_hosted'))
+    @org = @acme_corporation
+  end
+
+  def test_readable
+    assert !Provider.readable(@org).empty?
+  end
+
+  def test_editable
+    assert !Provider.editable(@org).empty?
+  end
+
+
+  def test_creatable?
+    assert Provider.creatable?(@org)
+  end
+
+  def test_any_readable?
+    assert Provider.any_readable?(@org)
+  end
+
+  def test_readable?
+    assert @provider.readable?
+  end
+
+  def test_editable?
+    assert @provider.editable?
+  end
+
+  def test_deletable?
+    assert @provider.deletable?
+  end
+
+end
+
+
+class ProviderAuthorizationNoPermsTest < MiniTest::Rails::ActiveSupport::TestCase
+  include RepositoryTestBase
+
+
+  def setup
+    super
+    User.current = User.find(users('no_perms_user'))
+    @provider = Provider.find(providers('fedora_hosted'))
+    @org = @acme_corporation
+  end
+
+
+  def test_readable
+    assert Provider.readable(@org).empty?
+  end
+
+  def test_editable
+    assert Provider.editable(@org).empty?
+  end
+
+
+  def test_creatable?
+    assert !Provider.creatable?(@org)
+  end
+
+  def test_any_readable?
+    assert !Provider.any_readable?(@org)
+  end
+
+  def test_readable?
+    assert !@provider.readable?
+  end
+
+  def test_editable?
+    assert !@provider.editable?
+  end
+
+  def test_deletable?
+    assert !@provider.deletable?
+  end
+end
