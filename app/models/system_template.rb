69870159ca5bbaadd6c1772fcccd37870383f78f
Justin Sherrill <jsherril@redhat.com>
2012-10-12 16:26:35 -0400

    %B

--- app/models/system_template.rb
+++ app/models/system_template.rb
@@ -24,7 +24,7 @@ class ParentTemplateValidator < ActiveModel::Validator
 end
 
 class SystemTemplate < ActiveRecord::Base
-  #include Authorization
+  include Authorization::SystemTemplate
   include LazyAccessor
   include AsyncOrchestration
 
@@ -372,40 +372,6 @@ end
     end.flatten(1)
   end
 
-
-  #### Permissions
-  def self.list_verbs global = false
-    {
-      :manage_all => _("Administer System Templates"),
-      :read_all => _("Read System Templates")
-   }.with_indifferent_access
-  end
-
-  def self.read_verbs
-    [:read_all]
-  end
-
-
-  def self.no_tag_verbs
-    SystemTemplate.list_verbs.keys
-  end
-
-  def self.any_readable? org
-    User.allowed_to?([:read_all, :manage_all], :system_templates, nil, org)
-
-  end
-
-  def self.readable? org
-    User.allowed_to?([:read_all, :manage_all], :system_templates, nil, org)
-  end
-
-  def self.manageable? org
-    User.allowed_to?([:manage_all], :system_templates, nil, org)
-  end
-
-  def readable?
-    self.class.readable?(self.environment.organization)
-  end
 
   def repos_to_be_promoted
     repos = self.repositories || []
