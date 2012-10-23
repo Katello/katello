1a2ad827d04465459fe5691ee805a013f5062cd6
Petr Chalupa <pchalupa@redhat.com>
2012-10-23 09:51:04 +0200

    update supported rspec versions in monkeypatch

--- src/spec/support/monkey_rspec_trac_creation_line_of_mocks.rb
+++ src/spec/support/monkey_rspec_trac_creation_line_of_mocks.rb
@@ -18,7 +18,7 @@
 require 'rspec/version'
 
 RSpec::Version::STRING =~ /^2\.(\d+)\.\d+$/
-unless (7..10).include? $1.to_i # change if it works for other versions
+unless (5..10).include? $1.to_i # change if it works for other versions
   warn "monkey eats only a banana! (this monkey needs rspec 2.(7-10))\n#{__FILE__}:#{__LINE__}"
 else
   module RSpec::Mocks
