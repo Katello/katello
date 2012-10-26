6db7243361806f2df2551a8239bd986788450961
David Davis <daviddavis@redhat.com>
2012-10-26 11:26:45 -0400

    Content views: Refactored composite/component
    Fixed the relationship between composite content view defs and their component
    content views. Component content views belong to definitions and not other
    content views.

--- app/models/content_view_component.rb
+++
@@ -1,4 +0,0 @@
-class ContentViewComponent < ActiveRecord::Base
-  belongs_to :composite, :class_name => "ContentView", :foreign_key => :composite_id
-  belongs_to :component, :class_name => "ContentView", :foreign_key => :component_id
-end
