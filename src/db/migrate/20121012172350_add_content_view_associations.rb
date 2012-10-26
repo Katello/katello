c1c2ae210b8bce54849c10e16a9a20b9d667493f
David Davis <daviddavis@redhat.com>
2012-10-26 15:16:02 -0400

    Content views: Added repository association to def
    Changed the existing repository relationship from content_view has_many repos
    to content_view_definition has_many repos.

--- src/db/migrate/20121012172350_add_content_view_associations.rb
+++ src/db/migrate/20121012172350_add_content_view_associations.rb
@@ -3,8 +3,8 @@ class AddContentViewAssociations < ActiveRecord::Migration
     add_column :filters, :content_view_definition_id, :integer
     add_index :filters, :content_view_definition_id
 
-    add_column :repositories, :content_view_id, :integer
-    add_index :repositories, :content_view_id
+    add_column :repositories, :content_view_definition_id, :integer
+    add_index :repositories, :content_view_definition_id
 
     add_column :environments, :default_content_view_id, :integer
     add_index :environments, :default_content_view_id
@@ -15,7 +15,7 @@ class AddContentViewAssociations < ActiveRecord::Migration
 
   def self.down
     remove_column :filters, :content_view_definition_id
-    remove_column :repositories, :content_view_id
+    remove_column :repositories, :content_view_definition_id
     remove_column :environments, :default_content_view_id
     remove_column :systems, :content_view_id
   end
