26cdee4b4f12e137a1e71880cf774b77c32e97bb
Eric D. Helms <ehelms@redhat.com>
2012-11-02 10:12:49 -0400

    %B

--- test/models/authorization/repository_authorization_test.rb
+++ test/models/authorization/repository_authorization_test.rb
@@ -10,7 +10,7 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-require 'test/models/repository_base'
+require './test/models/repository_base'
 
 
 class RepositoryAuthorizationAdminTest < MiniTest::Rails::ActiveSupport::TestCase
