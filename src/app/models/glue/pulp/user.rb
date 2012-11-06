34e1e3caa9841abcbb7a4e996d68ab5eee48df50
Partha Aji <paji@redhat.com>
2012-11-06 18:36:26 -0500

    Added changes recommended in the pull request

--- src/app/models/glue/pulp/user.rb
+++ src/app/models/glue/pulp/user.rb
@@ -19,7 +19,6 @@ module Glue::Pulp::User
       lazy_accessor :login, :name, :initializer => lambda { Runcible::Resources::User.retrieve(self.remote_id) }
       before_save :save_pulp_orchestration
       before_destroy :destroy_pulp_orchestration
-
     end
   end
 
