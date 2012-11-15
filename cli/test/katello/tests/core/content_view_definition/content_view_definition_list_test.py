25f2e9810aae1a95720f0b8cb4ccb6a523edaefa
David Davis <daviddavis@redhat.com>
2012-11-15 13:15:27 -0500

    Content views: fixing up cli tests

--- cli/test/katello/tests/core/content_view_definition/content_view_definition_list_test.py
+++ cli/test/katello/tests/core/content_view_definition/content_view_definition_list_test.py
@@ -4,8 +4,8 @@ import os
 
 from katello.tests.core.action_test_utils import CLIOptionTestCase, CLIActionTestCase
 
-import katello.client.core.content_view
-from katello.client.core.content_view import List
+import katello.client.core.content_view_definition
+from katello.client.core.content_view_definition import List
 
 
 
@@ -33,13 +33,12 @@ class ContentViewListTest(CLIActionTestCase):
 
         self.mock_options(self.OPTIONS)
 
-        self.mock(self.action.api, 'content_views_by_org', [])
+        self.mock(self.action.def_api, 'content_view_definitions_by_org', [])
 
     def tearDown(self):
         self.restore_mocks()
 
     def test_it_uses_lists_api(self):
         self.run_action()
-        self.action.api.content_views_by_org.assert_called_once_with(self.ORG,
-                None)
-
+        self.action.def_api.content_view_definitions_by_org.\
+                assert_called_once_with(self.ORG)
