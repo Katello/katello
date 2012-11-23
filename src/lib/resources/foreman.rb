a2575e0a3ec6fcaa4b74218b7db426e01354e4af
Martin Bačovský <mbacovsk@redhat.com>
2012-11-23 16:29:27 +0100

    Foreman environment orchestration

--- src/lib/resources/foreman.rb
+++ src/lib/resources/foreman.rb
@@ -25,13 +25,14 @@ module Resources
 
     Architecture    = ForemanApi::Resources::Architecture.new options
     Bookmark        = ForemanApi::Resources::Bookmark.new options
+    ConfigTemplate  = ForemanApi::Resources::ConfigTemplate.new options
+    Domain          = ForemanApi::Resources::Domain.new options
+    Environment     = ForemanApi::Resources::Environment.new options
     Home            = ForemanApi::Resources::Home.new options
     OperatingSystem = ForemanApi::Resources::OperatingSystem.new options
-    User            = ForemanApi::Resources::User.new options
-    Domain          = ForemanApi::Resources::Domain.new options
     SmartProxy      = ForemanApi::Resources::SmartProxy.new options
     Subnet          = ForemanApi::Resources::Subnet.new options
-    ConfigTemplate  = ForemanApi::Resources::ConfigTemplate.new options
+    User            = ForemanApi::Resources::User.new options
 
   end
 end
