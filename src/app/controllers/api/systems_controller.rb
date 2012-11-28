Katello/katello

commit 56de39ba17b446a2e511c4cbf57728a548581cf4

878341 - [ja_JP][SAM Web GUI] Default environment name 'Library' should not be localized.
- removing all localizations of "Library" when referring to an
  organization's default environment
- automatic whitespace removals

src/app/controllers/api/systems_controller.rb
@@ -423,7 +423,7 @@ DESC
 
   def find_only_environment
     if !@environment && @organization && !params.has_key?(:environment_id)
-      raise HttpErrors::BadRequest, _("Organization %s has 'Library' environment only. Please create an environment for system registration.") % @organization.name if @organization.environments.empty?
+      raise HttpErrors::BadRequest, _("Organization %s has the '%s' environment only. Please create an environment for system registration.") % [@organization.name, "Library"] if @organization.environments.empty?
 
       # Some subscription-managers will call /users/$user/owners to retrieve the orgs that a user belongs to.
       # Then, If there is just one org, that will be passed to the POST /api/consumers as the owner. To handle

src/app/controllers/api/environments_controller.rb
@@ -133,7 +133,7 @@ either library or an environment at the end of the chain
   see "environments#create"
   def update
     if @environment.library?
-      raise HttpErrors::BadRequest, _("Can't update Library environment")
+      raise HttpErrors::BadRequest, _("Can't update the '%s' environment") % "Library"
     else
       @environment.update_attributes!(params[:environment])
       render :json => @environment

src/app/controllers/api/filters_controller.rb
@@ -85,7 +85,7 @@ class Api::FiltersController < Api::ApiController
 
     render :json => @filter.to_json
   end
-  
+
   api :GET, "/organizations/:organization_id/filters/:id", "Show package filter details"
   param :organization_id, :identifier, :desc => "oranization identifier", :required => true
   param :id, :identifier, :desc => "filter identifier", :required => true
@@ -108,7 +108,7 @@ class Api::FiltersController < Api::ApiController
     render :json => @product.filters.to_json
   end
 
-  api :PUT, "/organizations/:organization_id/products/:product_id/filters", "Update package filters for product" 
+  api :PUT, "/organizations/:organization_id/products/:product_id/filters", "Update package filters for product"
   param :organization_id, :identifier, :desc => "oranization identifier", :required => true
   param :product_id, :identifier, :desc => "product identifier", :required => true
   param :filters, Array, :desc => "Updated list of filters", :required => true
@@ -130,7 +130,7 @@ class Api::FiltersController < Api::ApiController
 
     render :json => filters.uniq.to_json
   end
-    
+
   api :PUT, "/repositories/:repository_id/filters", "Update package filters for repository"
   param :repository_id, :identifier, :desc => "repository identifier", :required => true
   param :filters, Array, :desc => "Updated list of filters", :required => true
@@ -162,7 +162,7 @@ class Api::FiltersController < Api::ApiController
   def find_repository
     @repository = Repository.find(params[:repository_id])
     raise HttpErrors::NotFound, _("Couldn't find repository '%s'") % params[:repository_id] if @repository.nil?
-    raise HttpErrors::BadRequest, _("Filters can be stored only in Library repositories.") if not @repository.environment.library?
+    raise HttpErrors::BadRequest, _("Filters can be stored only in the '%s' repositories.") % "Library" if not @repository.environment.library?
     @repository
   end
 

src/app/controllers/api/repositories_controller.rb
@@ -90,7 +90,7 @@ class Api::RepositoriesController < Api::ApiController
     #
     # TODO: these should really be done as validations, but the orchestration engine currently converts them into OrchestrationExceptions
     #
-    raise HttpErrors::BadRequest, _("Repositories can be deleted only in Library environment.") if not @repository.environment.library?
+    raise HttpErrors::BadRequest, _("Repositories can be deleted only in the '%s' environment.") % "Library" if not @repository.environment.library?
     raise HttpErrors::BadRequest, _("Repository cannot be deleted since it has already been promoted. Using a changeset, please delete the repository from existing environments before deleting it.") if @repository.promoted?
 
     @repository.destroy
@@ -181,7 +181,7 @@ EOS
   param :id, :identifier, :required => true
   def gpg_key_content
     if @repository.gpg_key && @repository.gpg_key.content.present?
-      render(:text => @repository.gpg_key.content, :layout => false) 
+      render(:text => @repository.gpg_key.content, :layout => false)
     else
       head(404)
     end

src/app/controllers/api/templates_content_controller.rb
@@ -158,7 +158,7 @@ class Api::TemplatesContentController < Api::ApiController
   def find_template
     @template = SystemTemplate.find(params[:template_id])
     raise HttpErrors::NotFound, _("Couldn't find template '%s'") % params[:template_id] if @template.nil?
-    raise HttpErrors::BadRequest, _("Templates can be updated only in a Library environment") if not @template.environment.library?
+    raise HttpErrors::BadRequest, _("Templates can be updated only in the '%s' environment") % "Library" if not @template.environment.library?
     @template
   end
 

src/app/controllers/api/templates_controller.rb
@@ -75,7 +75,7 @@ class Api::TemplatesController < Api::ApiController
     param :parent_id, :number, :desc => "parent template numeric identifier"
   end
   def create
-    raise HttpErrors::BadRequest, _("New templates can be created only in a Library environment") if not @environment.library?
+    raise HttpErrors::BadRequest, _("New templates can be created only in the '%s' environment") % "Library" if not @environment.library?
 
     @template = SystemTemplate.new(params[:template])
     @template.environment = @environment
@@ -91,7 +91,7 @@ class Api::TemplatesController < Api::ApiController
     param :name, :identifier, :desc => "template new name"
   end
   def update
-    raise HttpErrors::BadRequest, _("Templates can be updated only in a Library environment") if not @template.environment.library?
+    raise HttpErrors::BadRequest, _("Templates can be updated only in the '%s' environment") % "Library" if not @template.environment.library?
 
     clones = @template.get_clones
     @template.attributes = params[:template].slice(:name, :parent_id, :description)
@@ -122,7 +122,7 @@ class Api::TemplatesController < Api::ApiController
     param :parent_id, :number, :desc => "parent template numeric identifier"
   end
   def import
-    raise HttpErrors::BadRequest, _("New templates can be imported only into a Library environment") if not @environment.library?
+    raise HttpErrors::BadRequest, _("New templates can be imported only into the '%s' environment") % "Library" if not @environment.library?
 
     begin
       temp_file = File.new(File.join("#{Rails.root}/tmp", "template_#{SecureRandom.hex(10)}.json"), 'w+', 0600)
@@ -142,7 +142,7 @@ class Api::TemplatesController < Api::ApiController
   api :GET, "/templates/:id/validate", "Validate a template TDL"
   param :id, :number, :desc => "template numeric identifier", :required => true
   def validate
-    raise HttpErrors::BadRequest, _("Cannot validate templates for the Library environment.") if @template.environment.library?
+    raise HttpErrors::BadRequest, _("Cannot validate templates for the '%s' environment.") % "Library" if @template.environment.library?
 
     respond_to do |format|
       format.tdl { @template.validate_tdl; render :text => 'OK' and return }
@@ -153,7 +153,7 @@ class Api::TemplatesController < Api::ApiController
   api :GET, "/templates/:id/export", "Export template as TDL or JSON"
   param :id, :number, :desc => "template numeric identifier", :required => true
   def export
-    raise HttpErrors::BadRequest, _("Cannot export templates for the Library environment.") if @template.environment.library?
+    raise HttpErrors::BadRequest, _("Cannot export templates for the '%s' environment.") % "Library" if @template.environment.library?
 
     respond_to do |format|
       format.tdl { render(:text => @template.export_as_tdl, :content_type => Mime::TDL) and return }

src/app/controllers/environments_controller.rb
@@ -61,7 +61,6 @@ class EnvironmentsController < ApplicationController
     # the edit view
     prior_envs = envs_no_successors - [@environment] - @environment.path
     env_labels = Hash[ *prior_envs.collect { |p| [ p.id, p.display_name ] }.flatten]
-    #env_labels[''] = _("Library")
     @env_labels_json = ActiveSupport::JSON.encode(env_labels)
 
     @selected = @environment.prior.nil? ? env_labels[""] : env_labels[@environment.prior.id]
@@ -100,7 +99,7 @@ class EnvironmentsController < ApplicationController
     @environment.save!
 
     if priorUpdated
-      result = @environment.prior.nil? ? _("Library") : @environment.prior.name
+      result = @environment.prior.nil? ? "Library" : @environment.prior.name
     else
       result = params[:kt_environment].values.first
     end

src/app/models/activation_key.rb
@@ -69,7 +69,7 @@ class ActivationKey < ActiveRecord::Base
   end
 
   def environment_not_library
-    errors.add(:base, _("Cannot create activation keys in Library environment ")) if environment and  environment.library?
+    errors.add(:base, _("Cannot create activation keys in the '%s' environment") % "Library") if environment and environment.library?
   end
 
   def environment_key_conflict

commit 11c37efa2f6db76452d16ac7158a42b31d29084d

fixing build issue caused by 56de39ba17b446a2e511c4cbf57728a548581cf4

src/app/controllers/api/systems_controller.rb
@@ -423,7 +423,7 @@ DESC
 
   def find_only_environment
     if !@environment && @organization && !params.has_key?(:environment_id)
-      raise HttpErrors::BadRequest, _("Organization %s has the '%s' environment only. Please create an environment for system registration.") % [@organization.name, "Library"] if @organization.environments.empty?
+      raise HttpErrors::BadRequest, _("Organization %{org} has the '%{env}' environment only. Please create an environment for system registration.") % {:org => @organization.name, :env => "Library"} if @organization.environments.empty?
 
       # Some subscription-managers will call /users/$user/owners to retrieve the orgs that a user belongs to.
       # Then, If there is just one org, that will be passed to the POST /api/consumers as the owner. To handle