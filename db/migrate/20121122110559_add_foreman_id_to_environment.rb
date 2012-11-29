04eadbaee69608d701d39443cb77fad92f2cb0f0
Martin Bačovský <mbacovsk@redhat.com>
2012-11-29 11:22:28 +0100

    %B

--- db/migrate/20121122110559_add_foreman_id_to_environment.rb
+++
@@ -1,9 +0,0 @@
-class AddForemanIdToEnvironment < ActiveRecord::Migration
-  def self.up
-    add_column :environments, :foreman_id, :integer
-  end
-
-  def self.down
-    remove_column :environments, :foreman_id
-  end
-end
