831af5820fcfd0e8aa0cbf84338dcbce1af9b87a
David Davis <daviddavis@redhat.com>
2012-12-03 17:07:53 -0500

    Content views: creating changeset during promotion shortcut

--- cli/src/katello/client/api/content_view.py
+++ cli/src/katello/client/api/content_view.py
@@ -55,9 +55,7 @@ class ContentViewAPI(KatelloAPI):
         path = "/api/organizations/%s/content_views/%s" % (org_id, cv_id)
         return self.server.DELETE(path)[1]
 
-    def promote(self, cv_id, env_id, prior_id=None):
+    def promote(self, cv_id, env_id):
         path = "/api/content_views/%s/promote" % cv_id
         params = {"environment_id": env_id}
-        if prior_id:
-            params["prior_id"] = prior_id
         return self.server.POST(path, params)[1]
--- cli/src/katello/client/core/content_view.py
+++ cli/src/katello/client/core/content_view.py
@@ -105,8 +105,6 @@ class Promote(ContentViewAction):
         parser.add_option('--label', dest='label',
                 help=_("content view label eg: foo.example.com (required)"))
         opt_parser_add_environment(parser, True)
-        parser.add_option('--prior', dest='prior',
-                help=_("prior environment name e.g.: staging"))
 
     def check_options(self, validator):
         validator.require(('org', 'label', 'environment'))
@@ -115,20 +113,13 @@ class Promote(ContentViewAction):
         org_name = self.get_option('org')
         view_label = self.get_option('label')
         env_name = self.get_option('environment')
-        prior_name = self.get_option('prior')
 
         view = get_content_view(org_name, view_label)
 
         environment = get_environment(org_name, env_name)
         env_id = environment["id"]
 
-        if prior_name:
-            prior = get_environment(org_name, prior_name)
-            prior_id = prior["id"]
-        else:
-            prior_id = None
-
-        self.api.promote(view["id"], env_id, prior_id)
+        self.api.promote(view["id"], env_id)
         print _("Successfully promoted [ %s ] to environment [ %s ]") % \
             (view["name"], environment["name"])
         return os.EX_OK
