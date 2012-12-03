622e083d9c5b29383e52c529fc118a9d6b9ecc71
Lukas Zapletal <lzap+git@redhat.com>
2012-12-03 13:33:39 +0100

    bundler.d - copying only some gems from test into dev

--- src/bundler.d/development.rb
+++ src/bundler.d/development.rb
@@ -1,8 +1,11 @@
 group :development do
+  # <test gems> that are here to make things (rake) easier
+    gem 'rspec-rails', '>= 2.0.0'
+    gem "parallel_tests"
+  # </test gems>
+
   # code documentation
   gem 'yard', '>= 0.5.3'
-  gem 'rspec-rails', '>= 2.0.0' # to be able to run rake spec form development environment
-  # for apipie generation
 
   # generates routes in javascript
   gem "js-routes", :require => 'js_routes'
