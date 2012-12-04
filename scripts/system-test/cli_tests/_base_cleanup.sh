fcf3e61e32ba14bd22d0f43151bf49bb35d9d7cc
Partha Aji <paji@redhat.com>
2012-12-04 16:53:14 -0500

    Removing filter entries in the cli-system tests

--- scripts/system-test/cli_tests/_base_cleanup.sh
+++ scripts/system-test/cli_tests/_base_cleanup.sh
@@ -4,6 +4,5 @@ header "Basic environment cleanup"
 
 #clear
 
-test_success "filter delete" filter delete --org="$TEST_ORG" --name="$FILTER1"
 test_success "org delete" org delete --name="$TEST_ORG"
 test_success "user delete" user delete --username="$TEST_USER"
