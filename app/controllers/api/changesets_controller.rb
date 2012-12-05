53f5e8518169c3e635c44df91aabe2a2ead8484a
David Davis <daviddavis@redhat.com>
2012-12-05 12:18:34 -0500

    Content views: changesets can have views in CLI

--- app/controllers/api/changesets_controller.rb
+++ app/controllers/api/changesets_controller.rb
@@ -47,7 +47,7 @@ class Api::ChangesetsController < Api::ApiController
   api :GET, "/changesets/:id", "Show a changeset"
   def show
     render :json => @changeset.to_json(:include => [:products, :packages, :errata, :repos, :system_templates,
-                                                    :distributions])
+                                                    :distributions, :content_views])
   end
 
   api :PUT, "/changesets/:id", "Update a changeset"
