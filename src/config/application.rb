627a9a1bf116350ebcc1fc9fa4045b55faca0011
Lukas Zapletal <lzap+git@redhat.com>
2012-12-06 13:00:46 +0100

    bundler.d - adding new packages to the comp files
    And one small fix - line was not needed anymore.

--- src/config/application.rb
+++ src/config/application.rb
@@ -14,9 +14,7 @@ if File.exist?(File.expand_path('../../Gemfile.in', __FILE__))
   # In bundler_ext mode we always load all groups except the testing group
   # which can cause problems mocking objects for production or development envs.
   require 'aeolus/ext/bundler_ext'
-  groups = :all
-  groups = [:defualt, :development, :test] if Rails.env == "test"
-  Aeolus::Ext::BundlerExt.system_require(File.expand_path('../../Gemfile.in', __FILE__), groups)
+  Aeolus::Ext::BundlerExt.system_require(File.expand_path('../../Gemfile.in', __FILE__), :all)
 
   # Webmock rubygem have very strong default setting - it blocks all HTTP connections
   # after it is required. Therefore we want to turn off this behavior for all environments
