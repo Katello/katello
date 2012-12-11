16d3aa3ecbdd7e0adc4adec631be6cb3c21138c4
Brad Buckingham <bbuckingham@redhat.com>
2012-12-11 17:45:04 -0500

    content views - ui - refresh - update views pane after refresh
    This commit contains modifications such that after a
    refresh request is performed, the UI view/versions are
    updated appropriately.

--- src/app/models/content_view.rb
+++ src/app/models/content_view.rb
@@ -48,7 +48,14 @@ class ContentView < ActiveRecord::Base
     result['definition']   = self.content_view_definition.try(:name)
     result['environments'] = environments.map{|e| e.try(:name)}.join(", ")
     result['versions'] = versions.map(&:version)
-    if options[:environment].present?
+    result['versions_details'] = versions.map do |v| {
+        :version => v.version,
+        :published => v.created_at.to_s,
+        :environments => v.environments.map{|e| e.name}
+      }
+    end
+
+    if options && options[:environment].present?
       result['repositories'] = repos(options[:environment]).map(&:name)
     end
 
