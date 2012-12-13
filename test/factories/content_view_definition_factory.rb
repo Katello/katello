28041ecff5fd9adb5f0c37adfaf6b564f5497681
David Davis <daviddavis@redhat.com>
2012-12-13 08:29:27 -0500

    Content views: Added tests for new arguments

--- test/factories/content_view_definition_factory.rb
+++ test/factories/content_view_definition_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :content_view_definition do
     sequence(:name) {|n| "Database_definition#{n}" }
+    sequence(:label) {|n| "Database_definition#{n}" }
     description "Database content view definition"
     organization
   end
