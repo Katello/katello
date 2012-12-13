09517009d519309cbad09cd7626783f610841c24
Tomas Strachota <tstrachota@redhat.com>
2012-12-13 14:02:04 +0100

    smart proxies - api controller

--- config/routes.rb
+++ config/routes.rb
@@ -742,6 +742,7 @@ Src::Application.routes.draw do
       scope :module => 'foreman' do
         resources :architectures, :except => [:new, :edit]
         resources :subnets, :except => [:new, :edit]
+        resources :smart_proxies, :except => [:new, :edit]
         constraints(:id => /[^\/]+/) do
           resources :domains, :except => [:new, :edit]
         end
