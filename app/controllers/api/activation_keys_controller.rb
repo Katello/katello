00a52662bd0c8d613ae8d388881f3f6e5f88c69e
Ivan Necas <inecas@redhat.com>
2012-12-13 12:54:58 +0100

    Use maruku instead of redcarpet for markdown -> html
    Redcarpet includes a C-based extension, that causes segfault
    problems on some systems. Since we don't need the efficiency of
    C for generating the documentation, switching to maruku renderer
    to do the job. The HTML output of API documentation should stay the same.

--- app/controllers/api/activation_keys_controller.rb
+++ app/controllers/api/activation_keys_controller.rb
@@ -66,7 +66,7 @@ class Api::ActivationKeysController < Api::ApiController
   api :POST, "/activation_keys", "Create an activation key"
   api :POST, "/environments/:environment_id/activation_keys", "Create an activation key"
   param :activation_key, Hash do
-    param :name, :identifier, :desc => "activation key identifier (alphanum characters, space, - and _)"
+    param :name, :identifier, :desc => "activation key identifier (alphanum characters, space, _ and -)"
     param :description, String, :allow_nil => true
   end
   def create
