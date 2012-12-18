fa4f07b8b3e2948dd7a6a0f8077d086cdafaf0f8
Justin Sherrill <jsherril@redhat.com>
2012-12-18 18:20:35 -0500

    %B

--- public/javascripts/system_template.js
+++ public/javascripts/system_template.js
@@ -201,6 +201,7 @@ KT.templates = function() {
             buttons.remove.addClass("disabled");
             buttons.save.addClass("disabled");
             buttons.download.addClass("disabled");
+            buttons.download.tipsy('hide');
             $('.package_add_remove').hide();
             $('.package_group_add_remove').hide();
             $('.repo_add_remove').hide();
@@ -209,11 +210,14 @@ KT.templates = function() {
         else {
             buttons.edit.removeClass("disabled");
             buttons.remove.removeClass("disabled");
-            buttons.download.removeClass("disabled");
             if (KT.options.current_template.modified) {
+                buttons.download.tipsy('show')
+                buttons.download.addClass("disabled");
                 buttons.save.removeClass("disabled");
             }
             else {
+                buttons.download.tipsy('hide')
+                buttons.download.removeClass("disabled");
                 buttons.save.addClass("disabled");
             }
 
@@ -1136,8 +1140,8 @@ $(document).ready(function() {
 
     $("#modified_dialog").dialog({modal: true, width: 400, autoOpen: false});
 
-
-
+    buttons.download.tipsy({gravity:'n', trigger:'manual'})
+    console.log(buttons.download.data('tipsy_text'));
     KT.options.templates = KT.template_breadcrumb["templates"].templates;
 
     KT.options.content_tree = sliding_tree("content_tree", {
