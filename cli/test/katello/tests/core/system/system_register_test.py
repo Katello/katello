46f966a3a293575139f91690d22dd3269836ab1a
David Davis <daviddavis@redhat.com>
2013-01-09 11:56:07 -0500

    Content views: added CLI for systems with content views

---
+++ cli/test/katello/tests/core/system/system_register_test.py
@@ -0,0 +1,19 @@
+from katello.tests.core.action_test_utils import CLIOptionTestCase
+from katello.client.core.system import Register
+
+
+class RequiredCLIOptionsTests(CLIOptionTestCase):
+
+    action = Register()
+
+    disallowed_options = [
+        ('--content_view=view1', '--org=ACME'),
+        ('--name=raspbi'),
+        ('--name=raspbi', '--env=Dev'),
+    ]
+
+    allowed_options = [
+        ('--org=ACME', '--name=raspbi'),
+        ('--org=ACME', '--name=raspbi', '--content_view=view1'),
+        ('--org=ACME', '--env=Dev', '--name=raspbi')
+    ]
