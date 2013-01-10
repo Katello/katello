ad9c0aabb9e7ec674f9f5c5aa180c885d8e771fd
Eric D. Helms <ehelms@redhat.com>
2013-01-10 11:25:58 -0500

    %B

--- spec/controllers/api/packages_controller_spec.rb
+++ spec/controllers/api/packages_controller_spec.rb
@@ -37,7 +37,7 @@ describe Api::PackagesController, :katello => true do
 
     @repo.stub(:packages).and_return([])
     package = { 'repository_memberships' => [ repo_id ] }
-    Runcible::Extensions::Rpm.stub(:find).and_return(package)
+    Runcible::Extensions::Rpm.stub(:find_by_unit_id).and_return(package)
 
     @request.env["HTTP_ACCEPT"] = "application/json"
     login_user_api
