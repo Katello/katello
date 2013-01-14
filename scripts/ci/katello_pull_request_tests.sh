10b9d91273d3b3f6286ed29725299f3068958816
Adam Price <komidore64@gmail.com>
2013-01-14 10:38:23 -0500

    telling travis to run new parallel rake task

--- scripts/ci/katello_pull_request_tests.sh
+++ scripts/ci/katello_pull_request_tests.sh
@@ -16,5 +16,5 @@ psql -c "CREATE USER katello WITH PASSWORD 'katello';" -U postgres
 psql -c "ALTER ROLE katello WITH CREATEDB" -U postgres
 psql -c "CREATE DATABASE katello_test OWNER katello;" -U postgres
 bundle exec rake parallel:create VERBOSE=false
-bundle exec rake parallel:migrate VERBOSE=false
-bundle exec rake parallel:spec
+bundle exec rake parallel:migrate VERBOSE=alse
+bundle exec rake "ptest:spec['', 2]"
