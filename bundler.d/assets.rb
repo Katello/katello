711762bd7fc8be958a597c40474c7804a0a9e6fa
Miroslav Suchý <msuchy@redhat.com>
2013-01-22 14:00:25 +0100

    do not fail if you use bundler
    if you do not have compass gem available, you must be using "bundler install"
    In such case make sure that you will use compass > 0.12.0.
    Otherwise determine what version of compass you have and require compass-rails if needed.

--- bundler.d/assets.rb
+++ bundler.d/assets.rb
@@ -2,9 +2,16 @@
 group :assets do
   gem 'haml', '>= 3.1.2'
   gem 'haml-rails', "= 0.3.4"
-  gem 'compass', '>= 0.11.5'
-  require 'compass'
-  if Gem.loaded_specs["compass"].version < Gem::Version.new("0.12.0")
+  begin 
+    require 'compass'
+    if Gem.loaded_specs["compass"].version < Gem::Version.new("0.12.0")
+      gem 'compass', '~> 0.11.5'
+    else
+      gem 'compass', '~> 0.12.0'
+      gem 'compass-rails', '~> 1.0.3'
+    end
+  rescue LoadError
+    gem 'compass', '~> 0.12.0'
     gem 'compass-rails', '~> 1.0.3'
   end
   gem 'compass-960-plugin', '>= 0.10.4', :require => 'ninesixty'
