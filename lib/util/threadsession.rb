0ce935075d0fa7c152269b5e2e1d10313d9a1568
Marek Hulan <ares@igloonet.cz>
2013-02-13 15:14:18 +0100

    %B

--- lib/util/threadsession.rb
+++ lib/util/threadsession.rb
@@ -59,7 +59,7 @@ module Katello
                 :user     => user_id,
                 :oauth    => {:oauth_secret => Katello.config.pulp.oauth_secret,
                               :oauth_key    => Katello.config.pulp.oauth_key },
-                :logging  => {:logger     => Rails.logger,
+                :logging  => {:logger     => ::Logging.logger['pulp_rest'],
                               :exception  => true,
                               :debug      => true }
               }
@@ -97,8 +97,10 @@ module Katello
       end
 
       def thread_locals
-        # store request uuid (for Rails 3.2+ we can use Request.uuid)
-        Thread.current[:request_uuid] = request.respond_to?(:uuid) ? request.uuid : SecureRandom.hex(16)
+        # store request uuid (for Rails 3.2+ we can use Request.uuid) and process pid
+        uuid = request.respond_to?(:uuid) ? request.uuid : SecureRandom.hex(16)
+        ::Logging.mdc['uuid'] = Thread.current[:request_uuid] = uuid
+        ::Logging.mdc['pid'] = Process.pid
 
         # store user
         u = current_user
