3d7f327c057f496b3f7159940a827cfb032c099d
Brad Buckingham <bbuckingham@redhat.com>
2013-02-15 02:53:20 -0500

    content views - fix migration that maps repos to view versions
    This is a fix for a broken migration. Without this migration,
    rake db:migrate will fail on installations that have
    existing environments/repositories.  The primary issue
    observed by the user is an error indicating an attempt
    to assign a version=view.

--- src/db/migrate/20121112144229_repository_add_content_view.rb
+++ src/db/migrate/20121112144229_repository_add_content_view.rb
@@ -3,6 +3,7 @@ class RepositoryAddContentView < ActiveRecord::Migration
     add_column :repositories, :content_view_version_id, :integer, :null=>true
     add_index :repositories, :content_view_version_id
 
+    User.current = User.hidden.first
     KTEnvironment.all.each do |env|
      view = ContentView.create!(:name=>"Default View for #{env.name}",
                          :organization=>env.organization, :default=>true)
@@ -10,11 +11,10 @@ class RepositoryAddContentView < ActiveRecord::Migration
      env.save!
      version = ContentViewVersion.create!(:version=>1, :content_view=>view)
      env.repositories.each do |repo|
-       repo.content_view_version = view
-       view.save!
+       repo.content_view_version = version
+       repo.save!
      end
     end
-
     change_column :repositories, :content_view_version_id, :integer, :null => false
   end
 
