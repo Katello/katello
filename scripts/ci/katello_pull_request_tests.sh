b7c5273d51299398362faf571cbe50d173db670d
Justin Sherrill <jsherril@redhat.com>
2013-02-21 14:12:16 -0500

    moving cli tests into same script so breakage stops travis when it occurs

--- scripts/ci/katello_pull_request_tests.sh
+++ scripts/ci/katello_pull_request_tests.sh
@@ -39,3 +39,28 @@ if [ $? -ne 0 ]
 then
   exit 1
 fi
+
+
+cd ../cli
+
+echo ""
+echo "********* Python CLI Unit Tests ***************"
+cd cli/
+echo "RUNNING: make test"
+make test
+if [ $? -ne 0 ]
+then
+  exit 1
+fi
+
+echo ""
+echo "********* Running Pylint ************************"
+echo "RUNNING: PYTHONPATH=src/ pylint --rcfile=./etc/spacewalk-pylint.rc --additional-builtins=_ katello"
+PYTHONPATH=src/ pylint --rcfile=./etc/spacewalk-pylint.rc --additional-builtins=_ katello
+if [ $? -ne 0 ]
+then
+  exit 1
+fi
+
+cd ../
+
