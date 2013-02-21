Katello/katello

commit 2cee9ef0d7ee53832d4f578dd8ce6ae4491b0d6d

Conflicts: 	src/app/controllers/roles_controller.rb

src/app/controllers/application_controller.rb
@@ -25,6 +25,7 @@ class ApplicationController < ActionController::Base
 
   helper "alchemy/translation"
   helper_method :current_organization
+  helper_method :render_correct_nav
   before_filter :set_locale
   before_filter :require_user,:require_org
   before_filter :check_deleted_org
@@ -171,7 +172,7 @@ class ApplicationController < ActionController::Base
     if current_user && current_user.default_locale
       I18n.locale = current_user.default_locale
     else
-      I18n.locale = ApplicationController.extract_locale_from_accept_language_header parse_locale
+      I18n.locale = ApplicationController.extract_locale_from_accept_language_header
     end
 
     logger.debug "Setting locale: #{I18n.locale}"
@@ -221,8 +222,8 @@ class ApplicationController < ActionController::Base
   # Look for match to list of locales specified in request. If not found, try matching just
   # first two letters. Finally, default to english if no matches at all.
   # eg. [en_US, en] would match en
-  # Expects list of locales to search as an array (use parse_locale for that)
-  def self.extract_locale_from_accept_language_header locales
+  def self.extract_locale_from_accept_language_header
+    locales = parse_locale
 
     # Look for full match
     locales.each {|locale|
@@ -243,6 +244,23 @@ class ApplicationController < ActionController::Base
     current_user.create_or_update_search_history(URI(@_request.env['HTTP_REFERER']).path, params[:search])
   rescue => error
     log_exception(error)
+  end
+
+  def render_correct_nav
+    if self.respond_to?(:menu_definition) && self.menu_definition[params[:action]] == :admin_menu
+      session[:menu_back] = true
+      #the menu definition exists, return true
+      render_admin_menu
+    elsif self.respond_to?(:menu_definition) && self.menu_definition[params[:action]] == :notices_menu
+      session[:menu_back] = true
+      session[:notifications] = true
+      render_notifications_menu
+    else
+      #the menu definition does not exist, return false
+      session[:menu_back] = false
+      session[:notifications] = false
+      render_menu(1)
+    end
   end
 
   private # why bother? methods below are not testable/tested
@@ -308,8 +326,8 @@ class ApplicationController < ActionController::Base
   end
 
   # adapted from http_accept_lang gem, return list of browser locales
-  def parse_locale
-    locale_lang = (request.env['HTTP_ACCEPT_LANGUAGE'] || '').split(/\s*,\s*/).collect do |l|
+  def self.parse_locale
+    locale_lang = env['HTTP_ACCEPT_LANGUAGE'].split(/\s*,\s*/).collect do |l|
       l += ';q=1.0' unless l =~ /;q=\d+\.\d+$/
       l.split(';q=')
     end.sort do |x,y|

src/app/controllers/application_info_controller.rb
@@ -18,6 +18,10 @@ class ApplicationInfoController < ApplicationController
     "dashboard"
   end
 
+  def menu_definition
+    {:about => :admin_menu}.with_indifferent_access
+  end
+
   def about
     @ping = Ping.ping
     @packages = Ping.packages

src/app/controllers/notices_controller.rb
@@ -14,6 +14,7 @@
 # and open the template in the editor.
 
 class NoticesController < ApplicationController
+
   skip_before_filter :authorize,:require_org
   before_filter :notices_authorize
   before_filter :readable_by, :only => [:auto_complete_search]
@@ -22,6 +23,10 @@ class NoticesController < ApplicationController
 
   def section_id
      'notifications'
+  end
+
+  def menu_definition
+    { :show => :notices_menu }.with_indifferent_access
   end
 
   def notices_authorize

src/app/controllers/organizations_controller.rb
@@ -58,9 +58,12 @@ class OrganizationsController < ApplicationController
     }
   end
 
-
   def section_id
     'orgs'
+  end
+
+  def menu_definition
+    {:index => :admin_menu}.with_indifferent_access
   end
 
   def items

src/app/controllers/roles_controller.rb
@@ -78,6 +78,10 @@ class RolesController < ApplicationController
      'operations'
   end
 
+  def menu_definition
+    {:index => :admin_menu}.with_indifferent_access
+  end
+
   def items
     render_panel_direct(Role, @panel_options,  params[:search], params[:offset], [:name_sort, :asc],
                         {:default_field => :name, :filter=>[{:self_role=>[false]}], :load=>true})

src/app/controllers/sync_management_controller.rb
@@ -35,6 +35,9 @@ class SyncManagementController < ApplicationController
   before_filter :find_providers, :only => [:sync, :sync_status]
   before_filter :authorize
 
+  def menu_definition
+    {:manage => :admin_menu}.with_indifferent_access
+  end
 
   def section_id
     'contents'

src/app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
     'operations'
   end
 
+  def menu_definition
+    {:index => :admin_menu}.with_indifferent_access
+  end
+
   before_filter :setup_options, :only => [:items, :index]
   before_filter :find_user, :only => [:items, :index, :edit, :edit_environment, :update_environment, :update_preference,
                                       :update, :update_roles, :update_locale, :clear_helptips, :setup_default_org, :destroy]

src/app/helpers/application_helper.rb
@@ -17,14 +17,9 @@ module ApplicationHelper
   include LayoutHelper
   include BrandingHelper
 
-  #require 'navigation/main'
-
-  #include Navigation
-
   def current_url(extra_params={})
     url_for params.merge(extra_params)
   end
-
 
   def link_to_authorized(*args, &block)
 
@@ -78,7 +73,6 @@ module ApplicationHelper
     list
   end
 
-
   def two_panel(collection, options)
     options[:accessor] ||= "id"
     options[:left_panel_width] ||= nil
@@ -88,8 +82,6 @@ module ApplicationHelper
     enable_sort = options[:enable_sort] ? options[:enable_sort] : false
 
     raise ":titles option not provided" unless options[:titles]
-
-
 
     render :partial => "common/panel",
            :locals => {
@@ -293,5 +285,4 @@ module ApplicationHelper
     options[:builder] = KatelloFormBuilder
     form_for(object, options, &block)
   end
-
 end

commit 2b72714253b9430b9cd579fdc7e75b953bf8da85

i18n - fixing merge issue introduced by 2cee9ef0d7ee53

src/app/controllers/api/api_controller.rb
@@ -51,7 +51,7 @@ class Api::ApiController < ActionController::Base
     if current_user && current_user.default_locale
       I18n.locale = current_user.default_locale
     else
-      I18n.locale = ApplicationController.extract_locale_from_accept_language_header
+      I18n.locale = ApplicationController.extract_locale_from_accept_language_header parse_locale
     end
 
     logger.debug "Setting locale: #{I18n.locale}"

src/app/controllers/application_controller.rb
@@ -172,7 +172,7 @@ class ApplicationController < ActionController::Base
     if current_user && current_user.default_locale
       I18n.locale = current_user.default_locale
     else
-      I18n.locale = ApplicationController.extract_locale_from_accept_language_header
+      I18n.locale = ApplicationController.extract_locale_from_accept_language_header parse_locale
     end
 
     logger.debug "Setting locale: #{I18n.locale}"
@@ -222,8 +222,11 @@ class ApplicationController < ActionController::Base
   # Look for match to list of locales specified in request. If not found, try matching just
   # first two letters. Finally, default to english if no matches at all.
   # eg. [en_US, en] would match en
-  def self.extract_locale_from_accept_language_header
-    locales = parse_locale
+  #
+  # The method accept parameter = list of locales returned by parse_locale. Since the method is used
+  # outside of the request context, we need to pass this data in as a parameter.
+  #
+  def self.extract_locale_from_accept_language_header locales
 
     # Look for full match
     locales.each {|locale|
@@ -326,8 +329,8 @@ class ApplicationController < ActionController::Base
   end
 
   # adapted from http_accept_lang gem, return list of browser locales
-  def self.parse_locale
-    locale_lang = env['HTTP_ACCEPT_LANGUAGE'].split(/\s*,\s*/).collect do |l|
+  def parse_locale
+    locale_lang = (request.env['HTTP_ACCEPT_LANGUAGE'] || '').split(/\s*,\s*/).collect do |l|
       l += ';q=1.0' unless l =~ /;q=\d+\.\d+$/
       l.split(';q=')
     end.sort do |x,y|