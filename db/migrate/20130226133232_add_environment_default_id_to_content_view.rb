97ac3f5dc33e9510b7b6ca40303c5464adba4277
Dmitri Dolguikh <dmitri@redhat.com>
2013-02-26 16:21:57 +0000

    updated db/schema.rb; removed default_content_view_id column from environments table

--- db/migrate/20130226133232_add_environment_default_id_to_content_view.rb
+++ db/migrate/20130226133232_add_environment_default_id_to_content_view.rb
@@ -1,11 +1,13 @@
 class AddEnvironmentDefaultIdToContentView < ActiveRecord::Migration
   def self.up
     add_column :content_views, :environment_default_id, :integer
+    remove_column :environments, :default_content_view_id
     add_index :content_views, [:environment_default_id]
   end
 
   def self.down
     remove_column :content_views, :environment_default_id, :integer
+    add_column :environments, :default_content_view_id, :integer
     remove_index :content_views, [:environment_default_id]
   end
 end
