69e06f70d6982226ffe1d7546e67f4b48ee6786c
David Davis <daviddavis@redhat.com>
2013-02-26 11:45:34 -0500

    Setup simplecov in katello

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -14,7 +14,7 @@ group :test do
   gem 'webmock'
   gem 'minitest', '<=4.5.0', :require => "hoe/minitest"
   gem 'minitest-rails'
-  gem 'mocha', :require=>false
+  gem 'mocha', '~> 0.13.1', :require=>false
   # make our specs go faster (also appears in development group)
   gem "parallel_tests"
 end
