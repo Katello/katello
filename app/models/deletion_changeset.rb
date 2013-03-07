dc10e8c39b5bf1066ef17c542d9ce61174540f08
Justin Sherrill <jsherril@redhat.com>
2013-03-07 15:50:09 -0500

    fixing unit tests
    also moving a couple of non-glue methods out of the glue layer

--- app/models/deletion_changeset.rb
+++ app/models/deletion_changeset.rb
@@ -13,7 +13,7 @@
 
 require 'set'
 class DeletionChangeset < Changeset
-  use_index_of Changeset
+  use_index_of Changeset if Katello.config.use_elasticsearch
   def apply(options = { })
     options = { :async => true, :notify => false }.merge options
 
