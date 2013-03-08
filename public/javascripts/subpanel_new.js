11cf60840f5329878be0bdc90dd2da0fa165f6fc
Brad Buckingham <bbuckingham@redhat.com>
2013-03-08 15:34:30 -0500

    %B

--- public/javascripts/subpanel_new.js
+++ public/javascripts/subpanel_new.js
@@ -12,7 +12,8 @@
  */
 $(document).ready(function(){
   var form_id = $('#new_subpanel'),
-      form_submit_id = form_id.find('.subpanel_create');
+      form_submit_id = form_id.find('.subpanel_create'),
+      url_after_submit = form_submit_id.data('url_after_submit');
 
-  KT.panel.registerSubPanelSubmit(form_id, form_submit_id);
+  KT.panel.registerSubPanelSubmit(form_id, form_submit_id, url_after_submit);
 });
