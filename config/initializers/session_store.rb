999acce4040578aafa3b8d70f40ba423fe9d00b4
Dmitri Dolguikh <dmitri@redhat.com>
2013-03-11 15:39:35 +0000

    %B

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Src::Application.config.session_store :cookie_store, :key => '_src_session'
+Src::Application.config.session_store :cookie_store, :key => '_src_session', :expire_after => 1.hour
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
