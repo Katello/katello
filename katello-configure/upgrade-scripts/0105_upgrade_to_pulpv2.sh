b57ae6b8bddf16eff79604685c8ddc5ca945b9d9
Justin Sherrill <jsherril@redhat.com>
2013-03-12 20:20:40 -0400

    fixing pulpv2 upgrade script

--- katello-configure/upgrade-scripts/0105_upgrade_to_pulpv2.sh
+++ katello-configure/upgrade-scripts/0105_upgrade_to_pulpv2.sh
@@ -7,8 +7,14 @@
 #This steps calls pulp-migrate tool to migrate Pulp database schema
 #to the most recent version.
 
-pulp-v1-upgrade --backup-v1-db
-/usr/bin/pulp-migrate
+if [ -f /etc/sysconfig/katello ]; then . /etc/sysconfig/katello; fi
+KATELLO_HOME=${KATELLO_HOME:-/usr/share/katello}
+KATELLO_ENV=${KATELLO_ENV:-production}
+KATELLO_PREFIX=${KATELLO_PREFIX:-/katello}
+
+
+pulp-v1-upgrade --backup-v1-db || exit 1
+/usr/bin/pulp-manage-db || exit 1
 
 /usr/sbin/service-wait httpd start
 RAILS_RELATIVE_URL_ROOT=$KATELLO_PREFIX RAILS_ENV=$KATELLO_ENV rake regenerate_repo_metadata
