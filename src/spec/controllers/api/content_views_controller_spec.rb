84df14db81724bf8fe06e036cb860f4da91eb4fa
Eric D Helms <ehelms@redhat.com>
2013-03-14 17:39:00 -0400

    Rails32 - Switching to be_json matcher for some tests.

--- src/spec/controllers/api/content_views_controller_spec.rb
+++ src/spec/controllers/api/content_views_controller_spec.rb
@@ -89,7 +89,7 @@ describe Api::ContentViewsController, :katello => true do
       @view.should_receive(:refresh_view).and_return(version)
       version.should_receive(:task_status)
       @def.should_receive(:publishable?).and_return(true)
-      post "refresh", :id => @view.id
+      post "refresh", :id => @view.id.to_s
       response.status.should eql(202)
     end
   end
