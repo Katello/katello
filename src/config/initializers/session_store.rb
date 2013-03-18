c668f232e06d2d71aced0ea1066f8bbbe278e675
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2013-03-18 14:18:51 +0000

    support for selective (based on url accessed) expiration of cookies

--- src/config/initializers/session_store.rb
+++ src/config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
-Src::Application.config.session_store :cookie_store, :key => '_src_session', :expire_after => 1.hour
+#Src::Application.config.session_store :cookie_store, :key => '_katello_session', :expire_after => 1.hour
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
