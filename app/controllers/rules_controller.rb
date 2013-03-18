c81ac91a01fc4844faad9e807e58803f73dc73c8
Brad Buckingham <bbuckingham@redhat.com>
2013-03-18 10:49:28 -0400

    %B

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -13,11 +13,15 @@
 class RulesController < ApplicationController
 
   helper FiltersHelper
+  helper ContentViewDefinitionsHelper
 
   before_filter :require_user
-  before_filter :find_content_view_definition, :only => [:new, :create, :destroy_rules]
-  before_filter :find_filter, :only => [:new, :create, :destroy_rules]
+  before_filter :find_content_view_definition
   before_filter :authorize #after find_content_view_definition, since the definition is required for authorization
+  before_filter :find_filter
+  before_filter :find_rule, :only => [:update, :edit_package, :add_package,
+                                      :edit_package_group, :add_package_group,
+                                      :edit_errata, :add_errata, :destroy_parameters]
 
   respond_to :html
 
@@ -31,6 +35,15 @@ class RulesController < ApplicationController
     {
       :new => manage_rule,
       :create => manage_rule,
+
+      :update => manage_rule,
+      :edit_package => manage_rule,
+      :add_package => manage_rule,
+      :edit_package_group => manage_rule,
+      :add_package_group => manage_rule,
+      :edit_errata => manage_rule,
+      :add_errata => manage_rule,
+      :destroy_parameters => manage_rule,
 
       :destroy_rules => manage_rule,
     }
@@ -58,6 +71,64 @@ class RulesController < ApplicationController
     render :nothing => true
   end
 
+  def update
+    @rule.update_attributes!(params[:filter_rule])
+
+    notify.success(_("Rule '%{type}' was successfully updated.") %
+                   {:type => FilterRule::CONTENT_OPTIONS.index(@rule.content_type)})
+
+    render :nothing => true
+  end
+
+  def edit_package
+    render :partial => "content_view_definitions/filters/rules/edit_package",
+           :locals => {:view_definition => @view_definition, :filter => @filter, :rule => @rule,
+                       :editable => @view_definition.editable?, :name => controller_display_name}
+  end
+
+  def add_package
+    @rule.parameters[:units] ||= []
+    @rule.parameters[:units] << {:name => params[:package]}
+    @rule.save!
+
+    notify.success(_("Package rule successfully updated for filter '%{filter}'.") % {:filter => @filter.name})
+
+    render :partial => 'content_view_definitions/filters/rules/package_item',
+           :locals => {:editable => @view_definition.editable?,
+                       :unit => {:name => params[:package]}}
+  end
+
+  def edit_package_group
+    # TODO
+  end
+
+  def add_package_group
+    # TODO
+  end
+
+  def edit_errata
+    # TODO
+  end
+
+  def add_errata
+    # TODO
+  end
+
+  def destroy_parameters
+    if params[:units] && @rule.parameters[:units]
+      params[:units].each_pair do |key, value|
+        @rule.parameters[:units].delete({"name" => key})
+      end
+    end
+    @rule.save!
+
+    notify.success(_("Rule parameters successfully deleted for rule type '%{type}'. Parameters deleted: %{parameters}.") %
+                   {:type => FilterRule::CONTENT_OPTIONS.index(@rule.content_type),
+                    :parameters => params[:units].keys.join(', ')})
+
+    render :nothing => true
+  end
+
   def destroy_rules
     FilterRule.destroy(params[:filter_rules].keys) unless params[:filter_rules].blank?
 
@@ -75,6 +146,10 @@ class RulesController < ApplicationController
 
   def find_filter
     @filter = Filter.find(params[:filter_id])
+  end
+
+  def find_rule
+    @rule = FilterRule.find(params[:id])
   end
 
   private
