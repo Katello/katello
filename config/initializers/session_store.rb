d67df847e5b41866918cfd09f43e4a5be286daef
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2013-03-19 15:46:38 +0000

    added comment in session_store initializer pointing to environment-specific initializers instead.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
-#Src::Application.config.session_store :cookie_store, :key => '_katello_session', :expire_after => 1.hour
 
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# Src::Application.config.session_store :active_record_store
+# This file is not being used to configure Katello session store
+# See environment-specific initialization in 
+# config/environments/development.rb and config/environments/production.rb
+# under "config.session_store" key
