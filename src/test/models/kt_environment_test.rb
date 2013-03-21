5627ff08c594519b1482da50c03da79cde0057d8
Justin Sherrill <jsherril@redhat.com>
2013-03-21 12:58:28 -0400

    adding addition ktenvironment tests

--- src/test/models/kt_environment_test.rb
+++ src/test/models/kt_environment_test.rb
@@ -36,7 +36,7 @@ class KTEnvironmentTestBase < MiniTest::Rails::ActiveSupport::TestCase
 end
 
 
-class KTEnvironmentCreateTest < KTEnvironmentTestBase
+class KTEnvironmentTest < KTEnvironmentTestBase
 
   def test_create_validate_view
     env = KTEnvironment.create(:organization=>@acme_corporation, :name=>"SomeEnv", :prior=>@library)
@@ -45,4 +45,19 @@ class KTEnvironmentCreateTest < KTEnvironmentTestBase
     refute_empty env.default_content_view.content_view_environments.where(:environment_id=>env.id)
   end
 
+  def test_destroy_content_view_environment
+    env = @staging
+    refute_empty env.default_content_view.content_view_environments.where(:environment_id=>env.id)
+    cve = env.default_content_view.content_view_environments.where(:environment_id=>env.id).first
+    cve_cp_id = cve.cp_id
+    env.destroy
+    assert_empty ContentViewEnvironment.where(:cp_id=>cve_cp_id)
+  end
+
+  def test_destroy_default_content_view_version
+    env = @staging
+    default_version = env.default_content_view_version
+    env.destroy
+    assert_empty ContentViewVersion.where(:id=>default_version.id)
+  end
 end
