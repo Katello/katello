bab0cd928a7600ad56ae8a0688dbe922339316d8
Justin Sherrill <jsherril@redhat.com>
2013-03-26 16:32:31 -0400

    migration fixes (cherry picked from commit 09e36fd193ce01926c505feb5fdf1d45f75c7947)

--- db/migrate/20121112144229_repository_add_content_view.rb
+++ db/migrate/20121112144229_repository_add_content_view.rb
@@ -7,7 +7,7 @@ class RepositoryAddContentView < ActiveRecord::Migration
     KTEnvironment.all.each do |env|
      view = ContentView.create!(:name=>"Default View for #{env.name}",
                          :organization=>env.organization, :default=>true)
-     env.default_content_view = view
+     env.default_content_view_id = view.id
      env.save!
      version = ContentViewVersion.create!(:version=>1, :content_view=>view)
      env.repositories.each do |repo|
