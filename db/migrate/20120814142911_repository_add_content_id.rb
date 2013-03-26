bb3de7b2c76d2cf9da13daad8884e9f2ec92840f
Justin Sherrill <jsherril@redhat.com>
2013-03-26 11:27:09 -0400

    adding content_id migratino for repositories (cherry picked from commit a797de069f9aa90c8bbe3fc78da1e92055142278)

--- db/migrate/20120814142911_repository_add_content_id.rb
+++ db/migrate/20120814142911_repository_add_content_id.rb
@@ -1,9 +1,25 @@
 class RepositoryAddContentId < ActiveRecord::Migration
   def self.up
     add_column :repositories, :content_id, :string, :null=>true
- 
+
+    User.current = User.hidden.first
+    groups = Runcible::Resources::RepositoryGroup.retrieve_all
+    groups.each do |group|
+      group_id = group['id']
+      split_id = group_id.split(':')
+      if split_id.length == 2 && split_id[0] == 'content'
+        group['repo_ids'].each do |repo_id|
+          repo = Reepository.where(:pulp_id=>repo_id)
+          repo.content_id = split_id[1]
+          repo.save!
+        end
+        Runcible::Resources::RepositoryGroup.destroy(group_id)
+      end
+    end 
+    
+    found = Repository.where(:content_id=>nil).pluck(:name)
+    puts "Found repos with nil content_id: #{found.join(', ')}\n" if !found.empty?
     change_column :repositories, :content_id, :string, :null=>false
-    #TODO Add migration for existing repos
   end
 
   def self.down
