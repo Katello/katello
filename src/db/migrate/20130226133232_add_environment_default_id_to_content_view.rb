2a46226e79d6203067b367e871a7e48d282af28f
Justin Sherrill <jsherril@redhat.com>
2013-03-27 16:10:01 -0400

    necessary upgrade changes (cherry picked from commit f7311bbde89389dccffd25d820abf21ff3f799b5)

--- src/db/migrate/20130226133232_add_environment_default_id_to_content_view.rb
+++ src/db/migrate/20130226133232_add_environment_default_id_to_content_view.rb
@@ -1,13 +1,25 @@
 class AddEnvironmentDefaultIdToContentView < ActiveRecord::Migration
   def self.up
     add_column :content_views, :environment_default_id, :integer
+    KTEnvironment.each do |env|
+      cv = ContentView.find(env.default_content_view_id)
+      cv.environment_default_id = env.id
+      cv.save!
+    end
     remove_column :environments, :default_content_view_id
     add_index :content_views, [:environment_default_id]
   end
 
   def self.down
-    remove_column :content_views, :environment_default_id, :integer
     add_column :environments, :default_content_view_id, :integer
+    ContentView.all.each do |cv|
+      if cv.environment_default_id
+        env = KTEnvironment.find(cv.environment_default_id)
+        env.default_content_view_id = cv.id
+        env.save!
+      end
+    end
+    remove_column :content_views, :environment_default_id, :integer
     remove_index :content_views, [:environment_default_id]
   end
 end
