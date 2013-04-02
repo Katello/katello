b1edc71480092e852178d6a49dd7eba16b6778fb
Adam Price <komidore64@gmail.com>
2013-04-02 17:02:14 -0400

    custom_info in the UI is now using the API

--- config/routes.rb
+++ config/routes.rb
@@ -497,13 +497,6 @@ Src::Application.routes.draw do
     end
   end
 
-  # custom information
-  match '/custom_info/:informable_type/:informable_id' => 'custom_info#create', :via => :post, :as => :create_custom_info
-  match '/custom_info/:informable_type/:informable_id' => 'custom_info#index', :via => :get, :as => :custom_info
-  match '/custom_info/:informable_type/:informable_id/:keyname' => 'custom_info#show', :via => :get, :as => :show_custom_info
-  match '/custom_info/:informable_type/:informable_id/:keyname' => 'custom_info#update', :via => :put, :as => :update_custom_info
-  match '/custom_info/:informable_type/:informable_id/:keyname' => 'custom_info#destroy', :via => :delete, :as => :destroy_custom_info
-
   namespace :api do
     class RegisterWithActivationKeyContraint
       def matches?(request)
