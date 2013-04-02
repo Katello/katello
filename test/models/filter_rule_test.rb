5591547a68e719de69580488604b458cfc390e73
Partha Aji <paji@redhat.com>
2013-04-01 20:37:47 -0400

    %B

--- test/models/filter_rule_test.rb
+++ test/models/filter_rule_test.rb
@@ -36,18 +36,72 @@ class FilterRuleTest < MiniTest::Rails::ActiveSupport::TestCase
     Repository.delete_all
   end
 
-  def test_include_filter_clause
-    actual = get_filter_clause(true, "rpm", :units => [{:name => "foo*"}, {:name => "goo*"} ])
-    expected = {"$nor"=>[{"name"=>{"$regex"=>"foo*"}}, {"name"=>{"$regex"=>"goo*"}}]}
-    assert_equal expected, actual
+  [:includes, :excludes].each do |m|
+    define_method("test_package_name_#{m}") do
+      units = {:units => [{:name => "foo*"}, {:name => "goo*"}]}
+      expected = [{"name"=>{"$regex"=>"foo*"}}, {"name"=>{"$regex"=>"goo*"}}]
+      send("exec_test_#{m}", "rpm", units, expected )
+    end
+
+    define_method("test_package_versions_#{m}") do
+      units = {:units => [{:name => "foo*", :version => "5.0"},
+                          {:name => "goo*", :min_version => "0.5", :max_version => "0.7" }]}
+      expected = [{"$and" => [{"name"=>{"$regex"=>"foo*"}}, {"version" => "5.0"}]},
+                  {"$and" => [{"name"=>{"$regex"=>"goo*"}}, {"version" => {"$gte" => "0.5", "$lte" => "0.7"}}]}]
+
+      send("exec_test_#{m}", "rpm", units, expected)
+    end
+
+
+    define_method("test_package_group_names_#{m}") do
+      units = {:units => [{:name => "foo*"}, {:name => "goo*"} ]}
+      expected = [{"name"=>{"$regex"=>"foo*"}}, {"name"=>{"$regex"=>"goo*"}}]
+      send("exec_test_#{m}", "package_group", units, expected)
+    end
+
+    define_method("test_errata_ids_#{m}") do
+      units = {:units => [{:id => "Foo1"}, {:id => "Foo2"} ]}
+      expected = [{"id"=>{"$in"=>["Foo1", "Foo2"]}}]
+      send("exec_test_#{m}", "erratum", units, expected)
+    end
+
+    define_method("test_errata_dates_#{m}") do
+      units = {:date_range => {:start => "01/23/2000", :end => "02/23/2010"}}
+      expected = [{"issued"=>{"$gte"=>"2000-01-23T00:00:00-04:00",
+                              "$lte"=>"2010-02-23T00:00:00-04:00"}}]
+      send("exec_test_#{m}", "erratum", units, expected)
+    end
+
+
+    define_method("test_errata_types_#{m}") do
+      units = {:errata_type => [:bugfix, :security]}
+      expected = [{"type"=>{"$in"=>[:bugfix, :security]}}]
+      send("exec_test_#{m}", "erratum", units, expected)
+    end
+
+    define_method("test_errata_both_#{m}") do
+      units = {:errata_type => [:enhancement, :security],
+               :date_range => {:start => "03/27/2000", :end => "05/23/2010"}}
+      expected = [{"$and"=>[{"issued"=>{"$gte"=>"2000-03-27T00:00:00-04:00",
+                    "$lte"=>"2010-05-23T00:00:00-04:00"}},
+                    {"type"=>{"$in"=>[:enhancement, :security]}}]}]
+      send("exec_test_#{m}", "erratum", units, expected)
+    end
   end
 
-  def test_exclude_filter_clause
-    actual = get_filter_clause(false, "rpm", :units => [{:name => "foo*"}, {:name => "goo*"} ])
-    expected = {"$or"=>[{"name"=>{"$regex"=>"foo*"}}, {"name"=>{"$regex"=>"goo*"}}]}
+  def exec_test_includes content_type, units, expected_fragments
+    actual = get_filter_clause(true, content_type, units)
+    expected = {"$nor"=> expected_fragments}
     assert_equal expected, actual
+
   end
 
+  def exec_test_excludes  content_type, units, expected_fragments
+    actual = get_filter_clause(false, content_type, units)
+    expected = {"$or"=> expected_fragments}
+    assert_equal expected, actual
+
+  end
 
   def get_filter_clause(inclusion, content_type, parameter)
     @filter_rule.inclusion = inclusion
