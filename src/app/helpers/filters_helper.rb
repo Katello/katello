60b17c911e4dd0c0b18432f87092b14583742015
Brad Buckingham <bbuckingham@redhat.com>
2013-04-04 12:50:49 -0400

    content views - add 'summary' info to filter rule list
    This commit contains changes so that when a user
    looks at the rule listing for a given filter, it will
    provide a summary on each of the rules.
    
    E.g.
    Include Errata: Bug Fix: 04/01/2013 - 04/04/2013
    Exclude Packages: xterm, dtterm
    Include Errata: Security: Any date
    ...etc

--- src/app/helpers/filters_helper.rb
+++ src/app/helpers/filters_helper.rb
@@ -11,6 +11,7 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 module FiltersHelper
+  include FilterRulesHelper
 
   # Objectify the record provided. This will generate a hash containing
   # the record id, list of products and list of repos. It assumes that the
@@ -44,10 +45,72 @@ module FiltersHelper
   end
 
   def filter_rule_url(rule)
-    link_to(FilterRule::CONTENT_OPTIONS.index(rule.content_type),
+    link_to(rule_summary(rule),
             edit_content_view_definition_filter_rule_path(rule.filter.content_view_definition.id,
                                                           rule.filter.id, rule.id))
   end
 
+  def rule_summary(rule)
+    if rule.content_type == FilterRule::PACKAGE || rule.content_type == FilterRule::PACKAGE_GROUP
+      summary = parameter_list_summary(rule, :name)
 
+    elsif rule.content_type == FilterRule::ERRATA
+
+      if rule.parameters.has_key?(:errata_type) || (rule.parameters.has_key?(:date_range) &&
+          (rule.parameters[:date_range].has_key?(:start) || rule.parameters[:date_range].has_key?(:end)))
+        summary = errata_type_date_summary(rule)
+      else
+        summary = parameter_list_summary(rule, :id)
+      end
+    end
+    summary
+  end
+
+  def rule_inclusion(rule)
+    rule.inclusion? ? _('Include') : _('Exclude')
+  end
+
+  def parameter_list_summary(rule, field)
+    if rule.parameters.has_key?(:units)
+      parameter_list = rule.parameters[:units].inject([]) do |result, unit|
+        result << unit[field]
+      end
+    end
+
+    parameter_list = parameter_list.blank? ? _('No details specified') : parameter_list.join(', ')
+
+    _("%{include} %{rule_type}: %{parameter_list}") %
+        {:include => rule_inclusion(rule),
+         :rule_type => FilterRule::CONTENT_OPTIONS.index(rule.content_type),
+         :parameter_list => parameter_list}
+  end
+
+  def errata_type_date_summary(rule)
+    if (errata_types = selected_errata_types(rule)).blank?
+      errata_types = _('All types')
+    end
+
+    date_summary = ""
+    if rule.parameters.has_key?(:date_range)
+      if rule.parameters[:date_range].has_key?(:start) || rule.parameters[:date_range].has_key?(:end)
+        start_date = rule.parameters[:date_range][:start]
+        end_date = rule.parameters[:date_range][:end]
+
+        if start_date.blank?
+          date_summary = _("Before %{end_date}") % {:end_date => end_date}
+        elsif end_date.blank?
+          date_summary = _("After %{start_date}") % {:start_date => start_date}
+        else
+          date_summary = _("%{start_date} - %{end_date}") % {:start_date => start_date, :end_date => end_date}
+        end
+      end
+      date_summary = _("Any date") if date_summary.blank?
+    end
+
+    _("%{include} %{rule_type}: %{errata_types}: %{date_summary}") %
+        {:include => rule_inclusion(rule),
+         :rule_type => FilterRule::CONTENT_OPTIONS.index(rule.content_type),
+         :errata_types => errata_types,
+         :date_summary => date_summary}
+  end
 end
