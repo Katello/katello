c098dd92f900a9f2f167fde3ae56e5bbc50d2ad9
Justin Sherrill <jsherril@redhat.com>
2013-04-05 15:11:00 -0400

    copyright update

--- lib/monkeys/fix_string_interpolate.rb
+++ lib/monkeys/fix_string_interpolate.rb
@@ -1,5 +1,5 @@
 #
-# Copyright 2012 Red Hat, Inc.
+# Copyright 2013 Red Hat, Inc.
 #
 # This software is licensed to you under the GNU General Public
 # License as published by the Free Software Foundation; either version
