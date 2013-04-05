5b52fe203c4b17a84fb1f79538fce71816dedfd5
Partha Aji <paji@redhat.com>
2013-04-04 20:58:45 -0400

    Added some unit test fixes to work with the new model

--- src/test/factories/filter_rule_factory.rb
+++ src/test/factories/filter_rule_factory.rb
@@ -1,19 +1,18 @@
 FactoryGirl.define do
   factory :filter_rule do
     association :filter
-    trait :package_filter do
-      content_type "rpm"
-      parameters ({:units =>[{:name =>["g*"]}]}).with_indifferent_access
-    end
-
-    trait :inclusive do
-      inclusion true
-    end
-    trait :exclusive do
-      inclusion false
-    end
-
-    factory :package_filter_rule,  :traits => [:package_filter, :inclusive]
-    factory :package_filter_rule_exclusive,  :traits => [:package_filter, :exclusive]
+    inclusion true
   end
+
+  factory :package_filter_rule, :class => PackageRule, :parent => :filter_rule do
+      parameters ({:units =>[{:name =>["g*"]}]}).with_indifferent_access
+  end
+
+  factory :erratum_filter_rule, :class => ErratumRule, :parent => :filter_rule do
+  end
+
+  factory :package_group_filter_rule, :class => PackageGroupRule, :parent => :filter_rule do
+  end
+
+
 end
