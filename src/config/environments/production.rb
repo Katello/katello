d07b7bbf62e4aef3916f30d6b3b35a389f56e6db
Eric D Helms <ehelms@redhat.com>
2013-04-07 02:34:25 -0400

    Rails32 - Removing compass compile from Travis.

--- src/config/environments/production.rb
+++ src/config/environments/production.rb
@@ -36,10 +36,10 @@ Src::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
-   
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-   
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
