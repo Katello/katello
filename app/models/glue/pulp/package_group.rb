12b20bafeccdeb6595d40f501f49d45b875356a2
Partha Aji <paji@redhat.com>
2013-04-08 14:50:37 -0400

    Removed an unnecessary requires

--- app/models/glue/pulp/package_group.rb
+++ app/models/glue/pulp/package_group.rb
@@ -9,9 +9,6 @@
 # NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-
-require 'set'
-
 module Glue::Pulp::PackageGroup
 
   def self.included(base)
