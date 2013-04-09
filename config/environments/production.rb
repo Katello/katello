4e57f15e8a283ff764a67a465dd9e8e55c2f999d
Eric D Helms <ehelms@redhat.com>
2013-04-09 14:39:27 -0400

    %B

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Src::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
