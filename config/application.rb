9befe78925da944ce57b815817df7bd450b34b47
Eric D Helms <ehelms@redhat.com>
2013-04-10 09:20:42 -0400

    %B

--- config/application.rb
+++ config/application.rb
@@ -147,6 +147,24 @@ module Src
 
     config.assets.enabled = true
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
+
+    config.assets.precompile << Proc.new { |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    }
+
   end
 end
 
