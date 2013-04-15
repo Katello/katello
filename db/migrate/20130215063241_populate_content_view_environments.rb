550349b2ce55f162e4fbbca2ec5d790ebbfb877b
Justin Sherrill <jsherril@redhat.com>
2013-04-15 12:14:19 -0400

    moving prepared statement to be reused (cherry picked from commit 668428c8221aab04dfac34846bac839652ab93a4)

--- db/migrate/20130215063241_populate_content_view_environments.rb
+++ db/migrate/20130215063241_populate_content_view_environments.rb
@@ -4,13 +4,13 @@ class PopulateContentViewEnvironments < ActiveRecord::Migration
     # default content view version.  This will trigger the creation of
     # the content view environment.
     User.current = User.hidden.first
+   ActiveRecord::Base.connection.raw_connection.prepare('insert_cvve', "insert into content_view_version_environments (content_view_version_id, environment_id, created_at, updated_at) values ($1, $2, $3, $4)")
     KTEnvironment.all.each do |env|
       unless env.content_view_environment
         # a kt_environment will only have a single version
         view = ContentView.find(env.default_content_view_id)
         version = view.versions.first
 
-        query = ActiveRecord::Base.connection.raw_connection.prepare('insert_cvve', "insert into content_view_version_environments (content_view_version_id, environment_id, created_at, updated_at) values ($1, $2, $3, $4)")
        ActiveRecord::Base.connection.raw_connection.exec_prepared('insert_cvve', [version.id, env.id, DateTime.now, DateTime.now])
 
         ContentViewEnvironment.create!(:content_view=>view,
