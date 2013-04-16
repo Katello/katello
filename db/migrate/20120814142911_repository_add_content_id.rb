f1f7bda7d8ccec52332bfb288e49feec8268beac
Justin Sherrill <jsherril@redhat.com>
2013-04-16 14:16:07 -0400

    %B

--- db/migrate/20120814142911_repository_add_content_id.rb
+++ db/migrate/20120814142911_repository_add_content_id.rb
@@ -3,20 +3,22 @@ class RepositoryAddContentId < ActiveRecord::Migration
     add_column :repositories, :content_id, :string, :null=>true
 
     User.current = User.hidden.first
-    groups = Runcible::Resources::RepositoryGroup.retrieve_all
-    groups.each do |group|
-      group_id = group['id']
-      split_id = group_id.split(':')
-      if split_id.length == 2 && split_id[0] == 'content'
-        group['repo_ids'].each do |repo_id|
-          repo = Repository.where(:pulp_id=>repo_id).first
-          repo.content_id = split_id[1]
-          repo.save!
+
+    if Repository.count > 0
+      groups = Runcible::Resources::RepositoryGroup.retrieve_all
+      groups.each do |group|
+        group_id = group['id']
+        split_id = group_id.split(':')
+        if split_id.length == 2 && split_id[0] == 'content'
+          group['repo_ids'].each do |repo_id|
+            repo = Repository.where(:pulp_id=>repo_id).first
+            repo.content_id = split_id[1]
+            repo.save!
+          end
+          Runcible::Resources::RepositoryGroup.delete(group_id)
         end
-        Runcible::Resources::RepositoryGroup.delete(group_id)
       end
     end
-
     found = Repository.where(:content_id=>nil).pluck(:name)
     puts "Found repos with nil content_id: #{found.join(', ')}\n" if !found.empty?
     change_column :repositories, :content_id, :string, :null=>false
