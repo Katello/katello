50c8bf57b42dbe1d8ed1f0570d7be0d42ce5798b
David Davis <daviddavis@redhat.com>
2013-04-18 18:27:45 -0400

    Updating tests for filter id changes (cherry picked from commit e6a1ff400ca74e5a0087dd79ddc0c95d928c78f0)

--- cli/test/katello/tests/core/filters/filter_info_test.py
+++ cli/test/katello/tests/core/filters/filter_info_test.py
@@ -30,12 +30,14 @@ class FilterInfoTest(CLIActionTestCase):
            "label": 'content_def',
            "id": 1
            }
-    FILTER = 'filter'
+    FILTER = {'name': 'filter',
+              'id': 6
+              }
 
     OPTIONS = {
         'org':ORG,
         'definition_id':DEF["id"],
-        'name':FILTER
+        'name':FILTER['name']
     }
 
     def setUp(self):
@@ -46,11 +48,12 @@ class FilterInfoTest(CLIActionTestCase):
 
         self.mock(self.action.api, 'get_filter_info', self.FILTER)
         self.mock(self.module, 'get_cv_definition', self.DEF)
+        self.mock(self.module, 'get_filter', self.FILTER)
 
     def tearDown(self):
         self.restore_mocks()
 
     def test_it_uses_content_view_show_api(self):
         self.run_action()
-        self.action.api.get_filter_info.assert_called_once_with(self.FILTER,
+        self.action.api.get_filter_info.assert_called_once_with(self.FILTER["id"],
                 self.DEF["id"], self.ORG)
