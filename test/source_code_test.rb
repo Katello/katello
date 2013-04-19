d4d2d69003d7522d948ecfdfb609e337192b138b
Petr Chalupa <pchalupa@redhat.com>
2013-04-19 14:52:17 +0200

    add gettext string test

--- test/source_code_test.rb
+++ test/source_code_test.rb
@@ -39,52 +39,83 @@ class SourceCodeTest < MiniTest::Rails::ActiveSupport::TestCase
     end
 
     def check_lines(message = nil, &condition)
-      bad_lines = each_line.
-          map { |line, line_number, file_path| [file_path, line_number] unless condition.call line }.
-          compact
+      bad_lines = each_line.map do |line, line_number, file_path|
+        [line, line_number, file_path] unless condition.call line
+      end.compact
 
       assert bad_lines.empty?,
-             "#{message + "\n" if message}check lines:\n" +
-                 bad_lines.map { |line, line_number| '    - %s:%d' % [line, line_number] }.join("\n")
+             "#{message + "\n" if message}check lines:\n" + bad_lines.
+                 map { |line, line_number, file_path| ' - %s:%d: %s' % [file_path, line_number, line.strip] }.
+                 join("\n")
     end
   end
 
-  it 'does not have trailing whitespaces' do
-    SourceCode.
-        new('**/*.{rb,js,scss,haml}').
-        check_lines { |line| line !~ / +$/ }
+  describe 'formatting' do
+    it 'does not have trailing whitespaces' do
+      SourceCode.
+          new('**/*.{rb,js,scss,haml}').
+          check_lines { |line| line !~ / +$/ }
+    end
+
+    it 'does use soft-tabs' do
+      SourceCode.
+          new('**/*.{rb,js,scss,haml}',
+              %r'public/assets/.*\.js').# tab is ok in minified files, its shorter than space
+          check_lines { |line| line !~ /\t/ }
+    end
   end
 
-  it 'does use soft-tabs' do
-    SourceCode.
-        new('**/*.{rb,js,scss,haml}',
-            %r'public/assets/.*\.js'). # tab is ok in minified files, its shorter than space
-        check_lines { |line| line !~ /\t/ }
-  end
-
-  it 'does not use rescue Exception => e' do # ok
-    SourceCode.
-        new('**/*.rb').
-        check_lines(<<-DOC) { |line| (line !~ /rescue +Exception/) ? true : line =~ /#\s?ok/ }
+  describe 'best practices' do
+    it 'does not use rescue Exception => e' do # ok
+      SourceCode.
+          new('**/*.rb').
+          check_lines(<<-DOC) { |line| (line !~ /rescue +Exception/) ? true : line =~ /#\s?ok/ }
 always rescue specific exception or at least `rescue => e` which equals to `rescue StandardError => e`
 see http://stackoverflow.com/questions/10048173/why-is-it-bad-style-to-rescue-exception-e-in-ruby
-    DOC
-  end
+      DOC
+    end
 
-  it 'does not use ENV variables' do
-    SourceCode.
-        new('**/*.rb',
-            %r'config/(application|boot)\.rb',
-            %r'test/minitest_helper\.rb', # TODO clean up minitest_helper
-            %r'lib/util/puppet\.rb').
-        check_lines(<<-DOC) { |line| (line !~ /ENV\[[^\]]+\]/) ? true : line =~ /#\s?ok/ }
+    it 'does not use ENV variables' do
+      SourceCode.
+          new('**/*.rb',
+              %r'config/(application|boot)\.rb',
+              %r'test/minitest_helper\.rb', # TODO clean up minitest_helper
+              %r'lib/util/puppet\.rb').
+          check_lines(<<-DOC) { |line| (line !~ /ENV\[[^\]]+\]/) ? true : line =~ /#\s?ok/ }
 Katello.config or Katello.early_config should be always used instead of ENV variables, Katello.config is
 the single entry point to configuration. ENV variables are processed there.
-    DOC
+      DOC
+    end
+
+    it 'does not use general rescue => e' do
+      skip 'to be enabled'
+    end
   end
 
-  it 'does not use general rescue => e' do
-    skip 'to be enabled'
-  end
+  describe 'gettext' do
+    it 'does not use interpolation or multiple anonymous placeholders' do
+      skip 'waiting for strings in src/app/lib/util/task_status.rb to be fixed'
 
+      doc = <<-DOC
+
+Interpolation example:
+  _("This is a malformed string with \#{interpolated_variable} within")
+  # should be _("This is a malformed string with %s within") % interpolated_variable
+Multiple anonymous placeholders:
+  _("This is a malformed string with %s, and another %s") % [var1, var2]
+  # should be _("This is a malformed string with %{var1}, and another %{var2}") %
+  #              {:var1 => var1, :var2 => var2}
+      DOC
+      SourceCode.
+          new('**/*.{rb,js,scss,haml}',
+              %r'script/check-gettext\.rb',
+              %r'test/source_code_test\.rb').
+          check_lines doc do |line|
+        line.scan(/_\((".*?"|'.*?')\)/).all? do |match|
+          gettext_str = match.first
+          gettext_str !~ /#\{.*?\}/ and gettext_str.scan(/%[a-z]/).size <= 1
+        end
+      end
+    end
+  end
 end
