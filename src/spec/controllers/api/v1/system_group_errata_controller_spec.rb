0a48e43ae20ef0528a26b82314de61c5d95e0d5d
Tomas Strachota <tstrachota@redhat.com>
2013-04-24 16:13:21 +0200

    api v2 - systems group errata controller fix

--- src/spec/controllers/api/v1/system_group_errata_controller_spec.rb
+++ src/spec/controllers/api/v1/system_group_errata_controller_spec.rb
@@ -74,7 +74,7 @@ describe Api::V1::SystemGroupErrataController, :katello => true do
 
   describe "install errata" do
     before do
-      @group.stub(:install_errata)
+      @group.stub(:install_errata).and_return(TaskStatus.new)
     end
 
     let(:action) { :create }
