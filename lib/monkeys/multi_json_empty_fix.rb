d0505f60623d2a2a73d132f14a5606218852b843
Justin Sherrill <jsherril@redhat.com>
2013-05-02 11:52:31 -0400

    monkey patching multi_json to parse "" as json properly

---
+++ lib/monkeys/multi_json_empty_fix.rb
@@ -0,0 +1,14 @@
+#This allows us to accept "JSON" that is malformed
+# and is empty string ("") that subscription-manager-1.1.23-1.el6.x86_64
+# will from time to time, send to katello
+module MultiJson
+  class<< self
+    alias_method :old_aliased_load, :load
+
+    def load(string, options={})
+      string = string.read if string.respond_to?(:read)
+      string = "{}" if string == '""'
+      old_aliased_load(string, options)
+    end
+  end
+end
