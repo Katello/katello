24cd8bd27b15b81931df1492e631baeace6b894d
Petr Chalupa <git@pitr.ch>
2013-05-09 10:33:03 +0200

    add help to update_doc script
    minor improvements

--- script/update_doc.rb
+++ script/update_doc.rb
@@ -2,6 +2,15 @@
 
 root = File.expand_path '..', File.dirname(__FILE__)
 
+unless (ARGV & %w[-h --help]).empty?
+  print <<-USAGE
+Usage: update_doc.rb [push]
+
+       add 'push' if you want to push changes to github Katello/katello gh-pages
+  USAGE
+  exit 0
+end
+
 push_to_github = (ARGV == %w(push))
 
 def cmd(cmd)
@@ -17,14 +26,14 @@ end
 
 message = nil
 Dir.chdir(root) do
-  p message = `git log -n 1 --oneline`
-  cmd 'yard doc'
+  puts "commit message: #{message = `git log -n 1 --oneline`.strip}"
+  cmd 'yard doc --no-cache'
 end
 
 Dir.chdir "#{root}/yardoc" do
   cmd 'git checkout gh-pages'
   if push_to_github
-    cmd "git ac -m #{message}"
+    cmd "git ac -m '#{message}'"
     cmd 'git push origin gh-pages'
   end
 end
