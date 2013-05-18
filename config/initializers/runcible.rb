f42911bfe8c1d87eda92bd7113795f564b115b2f
David Davis <daviddavis@redhat.com>
2013-05-18 13:46:38 -0400

    Runcible: Overriding the default config error message
    This goes along with Katello/runcible#64 and the goal was to show a message
    where developers would realize they have to log in a user in order to call
    Runcible methods. Previously we were just seeing '@@config undefined' when
    calling stuff like "Repository.last.packages" from rails console.

---
+++ config/initializers/runcible.rb
@@ -0,0 +1,12 @@
+if Katello.config.use_pulp
+
+  # override Runcible's default configuration error message
+  module Runcible
+    class ConfigurationUndefinedError
+      def self.message
+        "Runcible configuration not defined. Is User.current set?"
+      end
+    end
+  end
+
+end
