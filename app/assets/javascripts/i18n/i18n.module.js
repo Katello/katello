d2b68a7306e15dfe54f0805e3ed94aee1043a3b1
Walden Raines <walden@redhat.com>
2013-05-23 13:16:43 -0400

    Adding i18n angular filter for translations.

--- app/assets/javascripts/i18n/i18n.module.js
+++ app/assets/javascripts/i18n/i18n.module.js
@@ -1,4 +1,5 @@
-/* Copyright 2013 Red Hat, Inc.
+/**
+ Copyright 2013 Red Hat, Inc.
 
  This software is licensed to you under the GNU General Public
  License as published by the Free Software Foundation; either version
@@ -8,12 +9,13 @@
  NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
  have received a copy of GPLv2 along with this software; if not, see
  http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-*/
+ **/
 
-//= require "common/experimental/katello_experimental"
-//= require "menu/menu.module.js"
-//= require "i18n/i18n.module.js"
-//= require "i18n/i18n.filter.js"
-//= require "systems/systems.module.js"
-//= require "widgets/widgets.module.js"
-//= require "common/experimental/routes.service"
+/**
+ * @ngdoc module
+ * @name  Katello.i18n
+ *
+ * @description
+ *   Module for internationalization.
+ */
+angular.module('Katello.i18n', []);
