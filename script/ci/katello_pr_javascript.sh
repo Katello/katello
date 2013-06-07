330c4865e816a59aa3978fffb0759e94f343f8a4
Walden Raines <walden@redhat.com>
2013-06-07 13:04:27 -0400

    Bastion: adding Gruntfile with karma unit testing.

--- script/ci/katello_pr_javascript.sh
+++ script/ci/katello_pr_javascript.sh
@@ -11,8 +11,8 @@ sudo apt-get install nodejs
 
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
-  
-sudo npm install -g karma bower
+
+npm install -g bower grunt-cli
 npm install
 bower install --dev
-karma start --single-run
+grunt ci
