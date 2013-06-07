83852bc07c91b94b9a9837353afb58616ac4cf76
Eric D. Helms <ericdhelms@gmail.com>
2013-06-07 18:16:06 -0400

    Bastion: Removing alchemy-tables for stripped down approach to be re-factored later.
    Changing requirements and need to customize the data more reliably on nutupane
    has drove us to pulling back from a pull fledged table implementation built in
    just the JavaScript. The intent here is to establish a baseline build out of the
    Nutupane interaction and structure and re-factor into a re-usable component later
    once all design requirements have been addressed. This only supports infinite scroll,
    search, selection and providing a full page table.
    
    Adds an incubator folder to store components that could be pulled out into a re-usable
    component at a future date.

--- engines/bastion/app/assets/bastion/systems/systems.controller.js
+++ engines/bastion/app/assets/bastion/systems/systems.controller.js
@@ -17,9 +17,6 @@
  *
  * @requires $scope
  * @requires Nutupane
- * @requires $location
- * @requires $compile
- * @requires $http
  *
  * @description
  *   Provides the functionality specific to Systems for use with the Nutupane UI pattern.
@@ -27,130 +24,22 @@
  *   within the table.
  */
 angular.module('Katello').controller('SystemsController',
-    ['$scope', 'Nutupane', '$location', '$compile', '$filter', '$http',
-    function($scope, Nutupane, $location, $compile, $filter, $http) {
-
-        var columns = [{
-            id: 'name',
-            display: 'Name',
-            show: true
-        },{
-            id: 'description',
-            display: 'Description',
-            show: true
-        },{
-            id: 'environment',
-            display: 'Environment',
-            show: true
-        },{
-            id: 'content_view',
-            display: 'Content View',
-            show: true
-        },{
-            id: 'created_at',
-            display: 'Created at',
-            show: true
-        },{
-            id: 'updated_at',
-            display: 'Updated at',
-            show: true
-        }];
-
-        var transform = function(data) {
-            var rows = [];
-
-            angular.forEach(data.systems,
-                function(system) {
-                    var name_row = $compile('<a ng-click="table.select_item(\'' +
-                                    KT.routes.edit_system_path(system.id) + '\',' +
-                                    system.id + ')">' + system.name +
-                                    '</a>')($scope);
-
-                    var row = {
-                        'row_id' : system.id,
-                        'show'  : true,
-                        'cells': [{
-                            display: name_row,
-                            column_id: 'name'
-                        },{
-                            display: system.description,
-                            column_id: 'description'
-                        },{
-                            display: system.environment.name,
-                            column_id: 'environment'
-                        },{
-                            display: system.content_view ? system.content_view.name : "",
-                            column_id: 'content_view'
-                        },{
-                            display: $filter('date')(system.created_at, 'medium'),
-                            column_id: 'created_at'
-                        },{
-                            display: $filter('date')(system.updated_at, 'medium'),
-                            column_id: 'updated_at'
-                        }]
-                    };
-                    rows.push(row);
-                });
-
-            return {
-                rows    : rows,
-                total   : data.total,
-                subtotal: data.subtotal
-            };
-        };
+    ['$scope', 'Nutupane',
+    function($scope, Nutupane) {
 
         $scope.table                = Nutupane.table;
         $scope.table.url            = KT.routes.api_systems_path();
-        $scope.table.transform      = transform;
-        $scope.table.model          = 'Systems';
-        $scope.table.data.columns   = columns;
         $scope.table.active_item    = {};
+        $scope.table.modelName      = "Systems";
 
-        Nutupane.setColumns([columns[0]]);
+        Nutupane.get();
 
-        $scope.createNewSystem = function () {
-            var createSuccess = function (data) {
-                $scope.$apply(function () {
-                    Nutupane.table.setNewItemVisibility(false);
-                    $scope.table.select_item(KT.routes.edit_system_path(data.system.id));
-                });
-                notices.checkNotices();
-            };
-
-            // Temporarily get the old new systems UI
-            // TODO REPLACE ME
-            $http.get(KT.routes.new_system_path()).then(function (response) {
-                var content = $('#nutupane-new-item .nutupane-pane-content'),
-                    data = KT.common.getSearchParams() || {},
-                    button = content.find('input[type|="submit"]');
-
-                content.html(response.data);
-                Nutupane.table.setDetailsVisibility(false);
-                Nutupane.table.setNewItemVisibility(true);
-
-                content.find('#new_system').submit(function (event) {
-                    event.preventDefault();
-                    $(this).ajaxSubmit({
-                        url: KT.routes.systems_path(),
-                        data: data,
-                        success: createSuccess,
-                        error: function (e) {
-                            button.removeAttr('disabled');
-                            notices.checkNotices();
-                        }
-                    });
-                });
-            });
-        };
-
-        Nutupane.default_item_url = function(id) {
+        Nutupane.defaultItemUrl = function(id) {
             return KT.routes.edit_system_path(id);
         };
 
-        Nutupane.get(function() {
-            if ($location.search().item) {
-                $scope.table.select_item(undefined, $location.search().item);
-            }
-        });
+        $scope.selectItem = function(id) {
+            Nutupane.selectItem(KT.routes.edit_system_path(id), id);
+        }
     }]
 );
--- engines/bastion/lib/bastion/engine.rb
+++ engines/bastion/lib/bastion/engine.rb
@@ -5,10 +5,14 @@ module Bastion
     isolate_namespace Bastion
 
     initializer "bastion.assets.paths", :group => :all do |app|
-      # Slight hack due to how import loading of SCSS looks up paths
       app.config.assets.paths << Bastion::Engine.root.join('app', 'assets')
+      app.config.assets.paths << Bastion::Engine.root.join('vendor', 'assets')
+
+      # Slight hack due to how import loading of SCSS looks up paths
       app.config.assets.paths << "#{::UIAlchemy::Engine.root}/vendor/assets/ui_alchemy/alchemy-forms"
       app.config.assets.paths << "#{::UIAlchemy::Engine.root}/vendor/assets/ui_alchemy/alchemy-buttons"
+
+      app.middleware.use ::ActionDispatch::Static, "#{root}/app/assets/bastion"
     end
   end
 end
