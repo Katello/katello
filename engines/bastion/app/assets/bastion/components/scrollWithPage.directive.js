bec4cbba12753eedec683df37e19b348ccdbf1b8
Walden Raines <walden@redhat.com>
2013-06-07 18:23:48 -0400

    Bastion: adding jshint to bastion and fixing non-nutupane files.

--- engines/bastion/app/assets/bastion/components/scrollWithPage.directive.js
+++ engines/bastion/app/assets/bastion/components/scrollWithPage.directive.js
@@ -1,10 +1,10 @@
 angular.module('alchemy').directive('scrollWithPage', ['$window', function ($window) {
     return {
         link: function (scope, element){
-            var element_original_offset = $(element).offset().top;
+            var elementOriginalOffset = $(element).offset().top;
             angular.element($window).bind('scroll', function () {
-                var window_scroll_top = $($window).scrollTop();
-                if (window_scroll_top > element_original_offset) {
+                var windowScrollTop = $($window).scrollTop();
+                if (windowScrollTop > elementOriginalOffset) {
                     element.addClass('scrolling');
                 } else {
                     element.removeClass('scrolling');
