357011a4602fec968260db5c202b616484659ea4
Brad Buckingham <bbuckingham@redhat.com>
2013-06-12 09:17:03 -0400

    955706 - removed lines that were commented out

--- app/models/task_status.rb
+++ app/models/task_status.rb
@@ -197,19 +197,16 @@ class TaskStatus < ActiveRecord::Base
           return ""
         end
         msg = details[:event_messages][self.overall_status]
-        #return n_(msg[1], msg[2], p.length) % [p.first, p.length - 1]
         return n_(msg[1], msg[2], p.length) % { package: p.first, total: p.length - 1 }
       when :candlepin_event
         return self.result
       when :package_group
         p = self.parameters[:groups]
         msg = details[:event_messages][self.overall_status]
-        #return n_(msg[1], msg[2], p.length) % [p.first, p.length - 1]
         return n_(msg[1], msg[2], p.length) % { group: p.first, total: p.length - 1 }
       when :errata
         p = self.parameters[:errata_ids]
         msg = details[:event_messages][self.overall_status]
-        #return n_(msg[1], msg[2], p.length) % [p.first, p.length - 1]
         return n_(msg[1], msg[2], p.length) % { errata: p.first, total: p.length - 1 }
     end
   end
