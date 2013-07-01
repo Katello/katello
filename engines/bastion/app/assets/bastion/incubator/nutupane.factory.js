2362e57602d28a2f0a3946dff0feaae103887f69
Eric D. Helms <ericdhelms@gmail.com>
2013-07-01 14:28:37 -0400

    Bastion: Making System resource more robust to include a persistent collection.
    The logic on fetching the list of items, and deciding whether to replace
    or append to the list has been moved into the resource factory. This includes
    the logic for fetching a single item and replacing the item in the list with the
    potential more verbose single item fetched from the server. This allows two-way
    data binding between an item fetched for detailed view and the item in the left
    hand list.

--- engines/bastion/app/assets/bastion/incubator/nutupane.factory.js
+++ engines/bastion/app/assets/bastion/incubator/nutupane.factory.js
@@ -1,4 +1,3 @@
-/*global KT*/
 /**
  * Copyright 2013 Red Hat, Inc.
  *
@@ -27,166 +26,97 @@
  * @example
  *   <pre>
        angular.module('example').controller('ExampleController',
-           ['Nutupane', 'Routes', function(Nutupane, Routes) {
+           ['Nutupane', function(Nutupane)) {
                var nutupane                = new Nutupane();
                $scope.table                = nutupane.table;
-               $scope.table.url            = Routes.examplePath();
-               $scope.table.model          = 'Examples';
-               $scope.table.activeItem     = {};
-
-               nutupane.defaultItemUrl = function(id) {
-                   return Routes.defaultPath(id);
-               };
            }]
        );
     </pre>
  */
-angular.module('Bastion.widgets').factory('Nutupane', ['$location', '$http', 'CurrentOrganization', function($location, $http, CurrentOrganization) {
-    var sort = { by: 'name', order: 'ASC' };
+angular.module('Bastion.widgets').factory('Nutupane',
+    ['$location', '$timeout', 'CurrentOrganization',
+    function($location, $timeout, CurrentOrganization) {
+        var Nutupane = function(resource) {
+            var self = this;
 
-    var Nutupane = function() {
-        var self = this;
+            self.sort = {
+                by: 'name',
+                order: 'ASC'
+            };
 
-        self.sort = sort;
-        self.table = {
-            items: [],
-            offset: 0,
-            visible: true,
-            collapsed: false,
-            detailsVisible: false,
-            newPaneVisible: false,
-            total: 0,
-            searchString: $location.search().search,
-            loadingMore: false
-        };
+            self.table = {
+                resource: resource,
+                searchString: $location.search().search,
+                loadingMore: false
+            };
 
-        self.get = function(callback) {
-            self.table.working = true;
-
-            return $http.get(this.table.url, {
-                params : {
+            self.get = function(callback) {
+                var params = {
                     'organization_id':  CurrentOrganization,
-                    'search':           $location.search().search,
-                    'sort_by':          sort.by,
-                    'sort_order':       sort.order,
+                    'search':           $location.search().search || "",
+                    'sort_by':          self.sort.by,
+                    'sort_order':       self.sort.order,
                     'paged':            true,
-                    'offset':           self.table.offset
-                }
-            })
-            .then(function(response) {
-                var table = self.table,
-                    data  = response.data;
+                    'offset':           self.table.resource.offset
+                };
 
-                if( !table.loadingMore ){
-                    table.offset    = data.records.length;
-                    table.items     = data.records;
-                    table.total     = data.total;
-                    table.subtotal  = data.subtotal;
+                self.table.working = true;
+
+                return self.table.resource.get(params, function() {
+                    self.table.working = false;
+
+                    if (callback) {
+                        callback();
+                    }
+                });
+            };
+
+            self.table.search = function(searchTerm) {
+                $location.search('search', searchTerm);
+                self.table.resource.offset = 0;
+                self.table.closeItem();
+
+                if (!self.table.working) {
+                    self.get();
+                }
+            };
+
+            // Must be overridden
+            self.table.closeItem = function() {
+                throw "NotImplementedError";
+            };
+
+            self.table.nextPage = function() {
+                var table = self.table;
+
+                if (table.loadingMore || table.working || table.resource.subtotal === table.resource.offset) {
+                    return;
+                }
+
+                table.loadingMore = true;
+
+                self.get(function() {
+                    table.loadingMore = false;
+                });
+            };
+
+            self.table.sort = function(column) {
+                if (column.id === self.sort.by) {
+                    self.sort.order = (self.sort.order === 'ASC') ? 'DESC' : 'ASC';
                 } else {
-                    table.offset += data.records.length;
-                    table.items = table.items.concat(data.records);
+                    self.sort.order = 'ASC';
+                    self.sort.by = column.id;
                 }
 
-                if (callback) {
-                    callback();
-                }
+                column.active = true;
 
-                table.working = false;
-            });
+                self.table.offset = 0;
+                self.get();
+            };
+
+            $timeout(self.get, 0);
         };
 
-        self.table.search = function(searchTerm){
-            $location.search('search', searchTerm);
-            self.table.offset = 0;
-            self.table.closeItem();
-
-            self.get();
-        };
-
-        // Must be overridden
-        self.table.closeItem = function() {
-            throw "NotImplementedError";
-        };
-
-        self.table.nextPage = function() {
-            var table = self.table;
-
-            if (table.loadingMore || table.working || table.subtotal === table.offset) {
-                return;
-            }
-
-            table.loadingMore = true;
-
-            self.get(function(){
-                table.loadingMore = false;
-            });
-        };
-
-        self.table.sort = function(column) {
-            if (column.id === sort.by) {
-                sort.order = (sort.order === 'ASC') ? 'DESC' : 'ASC';
-            } else {
-                sort.order = 'ASC';
-                sort.by = column.id;
-            }
-
-            column.active = true;
-
-            self.table.offset = 0;
-            self.get();
-        };
-    };
-    return Nutupane;
-}]);
-
-/**
- * @ngdoc directive
- * @name  Bastion.widgets.directive:nutupaneDetails
- *
- * @scope
- *
- * @element ANY
- *
- * @description
- *   Turns an element into a container for holding detail pages fetched by Nutupane. By setting
- *   the html property on the bound model, the payload wll be inserted as the html into the
- *   element as inner html. Connects up a close button to remove the element form view and allows
- *   sub-menu items to call out to the selectItem() functionality of Nutupane. This is currently
- *   tied to the legacy tupane details page fetching.
- *
- * @example
- *   <span class="nutupane-details panel" id="nutupane-details" nutupane-details="table.visible" model="table" ng-class="{ 'nutupane-details-open' : !model.visible }">
- */
-angular.module('Bastion.widgets').directive('nutupaneDetails', [function(){
-    return {
-        replace: false,
-        scope: {
-            'model': '='
-        },
-        link: function(scope, elem) {
-            scope.$watch('model.active_item.html', function(item){
-                elem.html(item);
-
-                var children = angular.element(elem).find('.menu_parent');
-                angular.forEach(children, function(item) {
-                    KT.menu.hoverMenu(item, { top : '75px' });
-                });
-
-                elem.find('.panel_link > a').on('click', function() {
-                    var element = this;
-
-                    scope.$apply(function() {
-                        scope.model.selectItem(angular.element(element).attr('href'));
-                    });
-                });
-            });
-
-            elem.find('.close').on('click', function() {
-                scope.$apply(function() {
-                    scope.model.closeItem();
-                });
-            });
-
-        }
-    };
-}]);
+        return Nutupane;
+    }]
+);
