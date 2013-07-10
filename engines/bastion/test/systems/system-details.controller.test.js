1128724f01caf3f51d29414f3df24f980ba0d927
Walden Raines <walden@redhat.com>
2013-07-10 11:11:39 -0400

    Bastion: display system facts on system details page.
    Instead of having a separate page for system facts, display
    an expandable "Advanced Information" section.

--- engines/bastion/test/systems/system-details.controller.test.js
+++ engines/bastion/test/systems/system-details.controller.test.js
@@ -12,20 +12,31 @@
  **/
 
 describe('Controller: SystemDetailsController', function() {
-    var $scope, System;
+    var $scope, $controller, System, mockSystem;
 
     // load the systems module and template
     beforeEach(module('Bastion.systems', 'systems/views/systems.html'));
 
     // Initialize controller
-    beforeEach(inject(function($controller, $rootScope) {
+    beforeEach(inject(function(_$controller_, $rootScope) {
+        $controller = _$controller_;
         $scope = $rootScope.$new();
         // Mocks
+        mockSystem = {
+            facts: {
+                cpu: "Itanium",
+                "lscpu.architecture": "Intel Itanium architecture",
+                "lscpu.instructionsPerCycle": "6",
+                anotherFact: "yes"
+            }
+        };
         System = {
-            get: function() {},
+            get: function() {
+                return mockSystem;
+            },
             releaseVersions: function() {}
         };
-        spyOn(System, 'get').andReturn({value: 'yo'});
+        spyOn(System, 'get').andCallThrough();
         spyOn(System, 'releaseVersions').andReturn(['RHEL6']);
 
         $scope.$stateParams = {systemId: 2};
@@ -34,13 +45,39 @@ describe('Controller: SystemDetailsController', function() {
     }));
 
     it("gets the system using the System service and puts it on the $scope.", function() {
-        expect(System.get).toHaveBeenCalledWith({id: 2});
-        expect($scope.system.value).toBe('yo');
+        expect(System.get).toHaveBeenCalledWith({id: 2}, jasmine.any(Function));
+        expect($scope.system).toBe(mockSystem);
     });
 
     it("gets the available release versions and puts them on the $scope", function() {
         expect(System.releaseVersions).toHaveBeenCalledWith({id: 2});
         expect($scope.releaseVersions).toEqual(['RHEL6']);
+    });
+
+    describe("populates advanced system information", function () {
+        beforeEach(function() {
+            System.get = function(systemId, callback) {
+                callback.apply();
+            }
+            $controller('SystemDetailsController', {$scope: $scope, System: System});
+        });
+
+        it("creates the system facts object by converting dot notation response to an object.", function() {
+            expect(typeof $scope.systemFacts).toBe("object");
+            expect(typeof $scope.systemFacts.lscpu).toBe("object");
+            expect($scope.systemFacts.lscpu.architecture).toBe("Intel Itanium architecture");
+        });
+
+        it("populates advanced info into two groups", function() {
+            expect(Object.keys($scope.advancedInfoRight).length).toBe(1);
+            expect(Object.keys($scope.advancedInfoRight).length).toBe(1);
+        });
+    });
+
+    // TODO remove me when we upgrade to AngularJS 1.1.4, see note in system-details.controller.js
+    it("retrieves the correct template for each field based on it's type", function() {
+        expect($scope.getTemplateForType("somethingElse")).toBe("systems/views/partials/system-detail-value.html");
+        expect($scope.getTemplateForType({})).toBe("systems/views/partials/system-detail-object.html");
     });
 });
 
