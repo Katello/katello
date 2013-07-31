c908544618326f3d37eeefe971a4bc86807cef8d
David Davis <daviddavis@redhat.com>
2013-07-31 17:59:59 -0400

    Travis: Decreasing jobs from 4 to 2
    Decreasing the number of jobs in Travis from four to two (per Ruby version) in
    hopes of speeding of our test suite.

--- script/ci/katello_pr_other.sh
+++ script/ci/katello_pr_other.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 echo ""
+echo "********* Minitest Model and Glue Tests ****************"
+psql -c "CREATE USER katellouser WITH PASSWORD 'katellopw';" -U postgres
+psql -c "ALTER ROLE katellouser WITH CREATEDB" -U postgres
+psql -c "CREATE DATABASE katelloschema OWNER katellouser;" -U postgres
+
+RAILS_ENV=test bundle exec rake db:create
+
+# SCHEMA: return to schema usage after FKs fixed
+# bundle exec rake db:test:load > /dev/null
+RAILS_ENV=test bundle exec rake db:migrate > /dev/null
+
+bundle exec rake minitest
+if [ $? -ne 0 ]
+then
+  exit 1
+fi
+
+echo ""
 echo "********* Source Code Lint Tests ****************"
 RAILS_ENV='build' ruby -Itest test/source_code_test.rb
 if [ $? -ne 0 ]
