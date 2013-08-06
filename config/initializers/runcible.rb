c6f1a7654c324dd9118daddb61665c84869e69d0
Justin Sherrill <jsherril@redhat.com>
2013-08-06 12:44:35 -0400

    fixing pulp connection on Webrick
    Some web servers clear Thread.current across requests
    Webrick is one, there are probably others.  So instead of
    reusing a pulp server instance, create a new one everytime
    User.current is set.

--- config/initializers/runcible.rb
+++ config/initializers/runcible.rb
@@ -11,19 +11,5 @@ if Katello.config.use_pulp
     end
   end
 
-  uri = URI.parse(Katello.config.pulp.url)
-
-  Katello.pulp_server = Runcible::Instance.new({
-    :url      => "#{uri.scheme}://#{uri.host.downcase}",
-    :api_path => uri.path,
-    :timeout      => Katello.config.rest_client_timeout,
-    :open_timeout => Katello.config.rest_client_timeout,
-    :oauth    => {:oauth_secret => Katello.config.pulp.oauth_secret,
-                  :oauth_key    => Katello.config.pulp.oauth_key },
-    :logging  => {:logger     => ::Logging.logger['pulp_rest'],
-                  :exception  => true,
-                  :debug      => true }
-  })
-
 
 end
