27d67b6a2cd324adfa88c45be9579c13b656c7e9
Justin Sherrill <jsherril@redhat.com>
2013-08-29 08:55:10 -0400

    fixing fort style

--- engines/fort/db/migrate/20130725185514_add_node_capability.rb
+++ engines/fort/db/migrate/20130725185514_add_node_capability.rb
@@ -1,12 +1,12 @@
 class AddNodeCapability < ActiveRecord::Migration
   def up
-   create_table :node_capabilities do |t|
-     t.references :node
-     t.text :configuration
-     t.string :type
-   end
+    create_table :node_capabilities do |t|
+      t.references :node
+      t.text :configuration
+      t.string :type
+    end
 
-   add_index "node_capabilities", ["node_id", "type"], :unique=>true
+   add_index "node_capabilities", %w(node_id type), :unique=>true
    add_foreign_key :node_capabilities, :nodes, {:name=>'node_capabilities_node_id_fk'}
   end
 
