26728e5626d4f668e9015eed418bd71ee31324cd
Justin Sherrill <jsherril@redhat.com>
2013-09-17 20:38:54 -0400

    fixing fort tests

--- test/support/pulp/task_support.rb
+++ test/support/pulp/task_support.rb
@@ -10,8 +10,6 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-require 'minitest_helper'
-
 class PulpTaskStatus
   def self.any_task_running(async_tasks)
     return false
