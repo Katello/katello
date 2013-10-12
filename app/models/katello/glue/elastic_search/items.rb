461ec8589976f284cc57debea0158a296e9863ea
Eric D. Helms <ericdhelms@gmail.com>
2013-10-12 09:27:09 -0400

    Engine: Namespacing models.

---
+++ app/models/katello/glue/elastic_search/items.rb
@@ -0,0 +1,171 @@
+#
+# Copyright 2013 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+module Katello
+module Glue
+  module ElasticSearch
+    class Items
+
+      attr_accessor :obj_class, :query_string, :results, :total, :filters
+      alias_method :model=, :obj_class=
+
+      def initialize(obj_class = nil)
+        @obj_class    = obj_class
+        @query_string = query_string
+        @results      = []
+        @filters      = []
+      end
+
+      # Retrieves items from the Elasticsearch index
+      #
+      # @param [String] query_string
+      #   what the class should be searched by, e.g. name:foo*
+      # @param [Integer] start
+      #   the start position to begin searching from, used for pagination
+      # @param [Hash] search_options optional search parameters, see options
+      # @option       search_options :default_field
+      #   The field that should be used by the search engine when a user performs
+      #   a search without specifying field.
+      # @option       search_options :page_size
+      #   Specifies the number of results to return
+      # @option       search_options :sort_by
+      #   The model field on which to sort
+      # @option       search_options :sort_order
+      #   The order to sort on, one of DESC or ASC
+      # @option       search_options :filters
+      #   Filter to apply to search. Array of hashes.  Each key/value within the hash
+      #   is OR'd, whereas each HASH itself is AND'd together
+      # @option search_options [true, false] :load_records?
+      #   whether or not to load the active record object (defaults to false)
+      # TODO: break up method
+      # rubocop:disable MethodLength
+      def retrieve(query_string, start = 0, search_options = {})
+
+        @query_string = query_string
+        @filters      = search_options[:filters] || []
+        start         = start || 0
+        all_rows      = false
+        sort_by       = search_options.fetch(:sort_by, 'name_sort')
+        sort_order    = search_options[:sort_order] || 'ASC'
+        total_count   = 0
+
+        sort_by = format_sort(sort_by)
+
+        # set the query default field, if one was provided.
+        query_options = {}
+        query_options[:default_field] = search_options[:default_field] || 'name'
+
+        if @query_string.nil? || @query_string == ''
+          all_rows = true
+        elsif search_options[:simple_query] && !Katello.config.simple_search_tokens.any?{|s| search.downcase.match(s)}
+          @query_string = search_options[:simple_query]
+        end
+
+        page_size = search_options[:page_size] || total_items
+        filters = @filters
+        filters = [filters] if !filters.is_a? Array
+
+        @results = @obj_class.search :load => false do
+          query do
+            if all_rows
+              all
+            else
+              string query_string, query_options
+            end
+          end
+
+          sort {by sort_by, sort_order.to_s.downcase } if sort_by && sort_order
+
+          filter :and, filters if filters.any?
+
+          size page_size
+          from start
+        end
+
+        total_count = @results.total
+
+        if search_options[:load_records?]
+          @results = load_records
+        else
+          @results = @results.results
+        end
+
+        return @results, total_count
+      rescue Tire::Search::SearchRequestFailed => e
+        Rails.logger.error(e.class)
+
+        @results = []
+        return @results, total_count
+      end
+
+      # Loads the ActiveRecord objects from the database that match
+      # the results returned by Elasticsearch
+      #
+      # @return [Array] a list of ActiveRecord objects
+      def load_records
+        collection = @obj_class.where(:id => @results.collect{|r| r.id}).
+            order(@results.collect{|r| "id = #{r.id} DESC"})
+
+        #set total since @items will be just an array
+        @total = @results.empty? ? 0 : @results.total
+        if @total != collection.length
+          Rails.logger.error("Failed to retrieve all #{@obj_class} search results " +
+                                 "(#{collection.length}/#{@results.length} found.)")
+        end
+
+        @results = collection
+        return @results
+      end
+
+      # Retrieves the total number of items based on a set of filters
+      #
+      # @return [Integer] the total number of objects that meet the filters
+      def total_items
+        @total = 0
+        filters = @filters
+        filters = [filters] if !filters.is_a? Array
+
+        results = @obj_class.search do
+          query do
+            all
+          end
+
+          filter :and, filters if filters.any?
+
+          size 1
+          from 0
+        end
+
+        @total = results.total
+
+        return @total
+      rescue Tire::Search::SearchRequestFailed => e
+        Rails.logger.error(e.class)
+        return @total
+      rescue => e
+        puts e
+        return @total
+      end
+
+      private
+
+      def format_sort(sort_by)
+        mapping = @obj_class.mapping || {}
+        unless mapping[sort_by.to_sym] && mapping[sort_by.to_sym][:type] == 'date'
+          sort_by + '_sort' if !sort_by.include?('_sort')
+        end
+      end
+
+    end
+  end
+end
+end
---
+++ app/models/katello/glue/pulp/errata.rb
@@ -0,0 +1,146 @@
+#
+# Copyright 2013 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+require 'set'
+
+module Katello
+module Glue::Pulp::Errata
+  SECURITY = "security"
+  BUGZILLA = "bugfix"
+  ENHANCEMENT = "enhancement"
+
+  TYPES = [SECURITY, BUGZILLA, ENHANCEMENT]
+
+  # rubocop:disable MethodLength
+  def self.included(base)
+    base.send :include, InstanceMethods
+
+    base.class_eval do
+
+      attr_accessor :id, :errata_id, :title, :description, :version, :release, :type, :status, :updated,  :issued, :from_str,
+                    :reboot_suggested, :references, :pkglist, :severity, :repoids, :solution
+
+      def self.new_from_search(params)
+        params['_id'] = params['id']
+        params['id'] = params['errata_id']
+        self.new(params)
+      end
+
+      def self.errata_by_consumer(repos)
+        errata = Katello.pulp_server.extensions.consumer.applicable_errata([], repos.map(&:pulp_id), false)
+        errata[:erratum] || []
+      end
+
+      def self.find(id)
+        erratum_attrs = Katello.pulp_server.extensions.errata.find_by_unit_id(id)
+        ::Errata.new(erratum_attrs) if !erratum_attrs.nil?
+      end
+
+      def self.find_by_errata_id(id)
+        erratum_attrs = Katello.pulp_server.extensions.errata.find(id)
+        ::Errata.new(erratum_attrs) if !erratum_attrs.nil?
+      end
+
+      def self.applicable_for_consumers(uuids, type = nil)
+        id_system_hash = Hash.new { |h, k| h[k] = [] }
+        response = Katello.pulp_server.extensions.consumer.applicable_errata(uuids)
+
+        #for each set of applicability, consumer_ids
+        response.each do |item|
+          (item['applicability']['erratum'] || []).each do |errata_id|
+            id_system_hash[errata_id].concat(item['consumers'])
+          end
+        end
+
+        return [] if id_system_hash.empty?
+        filters = {:id => id_system_hash.keys}
+        filters[:type] = type unless type.blank?
+
+        found_errata = ::Errata.search("", :start => 0, :page_size => id_system_hash.size,
+                                           :filters => filters, :fields => ::Errata::SHORT_FIELDS)
+        found_errata.collect do |erratum|
+          e = ::Errata.new_from_search(erratum.as_json)
+          e.applicable_consumers = id_system_hash[e.id]
+          e
+        end
+      end
+
+      def self.list_by_filter_clauses(clauses)
+        errata = Katello.pulp_server.extensions.errata.search(::Errata::CONTENT_TYPE, :filters => clauses)
+        if errata
+          errata.collect do |attrs|
+            ::Errata.new(attrs) if attrs
+          end.compact
+        else
+          []
+        end
+      end
+    end
+
+  end
+
+  module InstanceMethods
+
+    def initialize(params = {}, options = {})
+      params['repoids'] = params.delete(:repository_memberships)
+      params['errata_id'] = params['id']
+      params['id'] = params.delete('_id')
+      params['applicable_consumers'] ||= []
+      params.each_pair {|k, v| instance_variable_set("@#{k}", v) unless v.nil? }
+    end
+
+    def package_filenames
+      self.pkglist.collect do |pkgs|
+        pkgs['packages'].collect do |pk|
+          pk["filename"]
+        end
+      end.flatten
+    end
+
+    def included_packages
+      packages = []
+
+      (self.pkglist || []).each do |pack_list|
+        packages += pack_list['packages'].collect do |err_pack|
+          ::Package.new(err_pack)
+        end
+      end
+
+      packages
+    end
+
+    def products
+      products = []
+
+      self.repoids.each do |repoid|
+        # there is a problem, that Pulp in versino <= 0.0.265-1 doesn't remove
+        # repo frmo errata when deleting repository. Therefore there might be a
+        # situation that repo is not in Pulp anymore, see BZ 790356
+        if repo = Repository.where(:pulp_id => repoid)[0]
+          products << repo.product
+        end
+      end
+
+      products.uniq
+    end
+
+    def product_ids
+      products.map(&:id)
+    end
+
+    def product_cp_ids
+      products.map(&:cp_id)
+    end
+  end
+
+end
+end
