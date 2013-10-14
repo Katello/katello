08f1de0a8516d1098066645cd160697410c126d3
Joseph Magen <joseph@isratrade.co.il>
2013-10-14 17:36:10 +0300

    Engine: Remove all existing migrations

--- db/migrate/20110921093925_rename_subscription_uuid.rb
+++
@@ -1,9 +0,0 @@
-class RenameSubscriptionUuid < ActiveRecord::Migration
-  def self.up
-    rename_column :pools, :subscription, :cp_id
-  end
-
-  def self.down
-    rename_column :pools, :cp_id, :subscription
-  end
-end
