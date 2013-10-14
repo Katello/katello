02eef66fe34961218d7e2e85675c814c17e7d503
Joseph Magen <joseph@isratrade.co.il>
2013-10-14 17:36:10 +0300

    Engine: Remove all existing migrations

--- db/migrate/20130117110946_add_repositories_synced_to_product.rb
+++
@@ -1,9 +0,0 @@
-class AddRepositoriesSyncedToProduct < ActiveRecord::Migration
-  def self.up
-    add_column :products, :cdn_import_success, :boolean, :default => true, :null => false
-  end
-
-  def self.down
-    remove_column :products, :cdn_import_success
-  end
-end
--- db/migrate/20130604124100_update_task_statuses.rb
+++
@@ -1,9 +0,0 @@
-class UpdateTaskStatuses < ActiveRecord::Migration
-  def self.up
-    change_column :task_statuses, :organization_id, :integer, :null => true
-  end
-
-  def self.down
-    change_column :task_statuses, :organization_id, :integer, :null => false
-  end
-end
