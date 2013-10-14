5b2b0cebe7a5bbde0109f425a23ebf8e73d40531
Joseph Magen <joseph@isratrade.co.il>
2013-10-14 17:36:10 +0300

    Engine: Remove all existing migrations

--- db/migrate/20110517193547_add_changeset_promotion_date.rb
+++
@@ -1,9 +0,0 @@
-class AddChangesetPromotionDate < ActiveRecord::Migration
-  def self.up
-    add_column :changesets, :promotion_date, :datetime
-  end
-
-  def self.down
-    remove_column :changesets, :promotion_date
-  end
-end
--- db/migrate/20110701090248_add_role_description.rb
+++
@@ -1,9 +0,0 @@
-class AddRoleDescription < ActiveRecord::Migration
-  def self.up
-    add_column :roles, :description, :string, :limit => 250
-  end
-
-  def self.down
-    remove_column :roles, :description
-  end
-end
