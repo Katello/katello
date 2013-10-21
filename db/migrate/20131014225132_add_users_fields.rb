6bc14533cd5661ea68cc15b785cc400d53ef27d8
Joseph Magen <joseph@isratrade.co.il>
2013-10-21 10:32:13 +0300

    Engine: fixes #3257 - Katello seeds.rb is run by rake db:seed in Foreman app

--- db/migrate/20131014225132_add_users_fields.rb
+++ db/migrate/20131014225132_add_users_fields.rb
@@ -1,14 +1,11 @@
 class AddUsersFields < ActiveRecord::Migration
 
   def change
-    add_column :users, :username, :string
     add_column :users, :helptips_enabled, :boolean, :default => true
     add_column :users, :hidden, :boolean, :default => false, :null => false
     add_column :users, :page_size, :integer, :default => 25, :null => false
     add_column :users, :disabled, :boolean, :default => false
-    add_column :users, :email, :string
     add_column :users, :preferences, :text
-    add_column :users, :foreman_id, :integer
     add_column :users, :remote_id, :string
     add_column :users, :default_environment_id, :integer
   end
