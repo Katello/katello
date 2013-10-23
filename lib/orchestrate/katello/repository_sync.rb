037bbc52599c96307eb994f743926d7a6c8a1e81
Ivan Necas <inecas@redhat.com>
2013-10-23 10:03:26 +0200

    Repo sync orch actions

---
+++ lib/orchestrate/katello/repository_sync.rb
@@ -0,0 +1,35 @@
+#
+# Copyright 2013 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+module Orchestrate
+  module Katello
+    class RepositorySync < Dynflow::Action
+
+      include Helpers::RemoteAction
+
+      input_format do
+        param :id, Integer
+      end
+
+      def plan(repo)
+        plan_action(Pulp::RepositorySync, pulp_id: repo.pulp_id)
+        plan_self(:id => repo.id)
+      end
+
+      def finalize
+        repo = Repository.find(input[:id])
+        repo.index_content
+      end
+
+    end
+  end
+end
