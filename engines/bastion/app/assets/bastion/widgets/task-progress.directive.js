b5564e6922d55ca642cafa602f99d4b5fa00aea9
Ivan Nečas <inecas@redhat.com>
2013-10-25 11:08:47 +0200

    Dummy tasks progress back-end and connection the UI

--- engines/bastion/app/assets/bastion/widgets/task-progress.directive.js
+++ engines/bastion/app/assets/bastion/widgets/task-progress.directive.js
@@ -10,13 +10,57 @@
  have received a copy of GPLv2 along with this software; if not, see
  http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
  **/
+angular.module('Bastion.widgets')
+    .factory('tasksStatusProvider', ['$timeout', '$resource', function($timeout, $resource) {
+        var callbacks = {},
+            timoutId,
+            taskResource = $resource('/katello/api/dyntasks',
+                                    {},
+                                    {query: {method:'GET', isArray: true}});
 
-angular.module('Bastion.widgets').directive('taskprogress', [function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'widgets/views/task-progress.html',
-        scope: {
-            uuid: '@',
+        function updateProgress() {
+            var uuids = []
+            angular.forEach(callbacks, function(callback, uuid) { uuids.push(uuid); })
+            if (uuids.length == 0) {
+                return;
+            }
+            taskResource.query({'uuids[]': uuids}, function(tasks) {
+                angular.forEach(tasks, function(task) {
+                    var callback = callbacks[task.uuid]
+                    if (callback) {
+                        callback(task.progress*100);
+                    }
+                });
+            });
         }
-    };
-}]);
+        function scheduleUpdate() {
+            // save the timeoutId for canceling
+            timeoutId = $timeout(function() {
+                updateProgress();
+                scheduleUpdate(); // schedule the next update
+            }, 1500);
+        }
+        scheduleUpdate();
+        return {
+            register: function(uuid, callback) { callbacks[uuid] = callback; },
+            unregister: function(uuid) { delete callbacks[uuid]; }
+        };
+    }])
+    .directive('taskprogress',
+               ['tasksStatusProvider', function(tasksStatusProvider) {
+        return {
+            restrict: 'E',
+            templateUrl: 'widgets/views/task-progress.html',
+            scope: {
+                uuid: '@',
+            },
+            link: function (scope, element, args) {
+                tasksStatusProvider.register(args.uuid, function(progress) {
+                    scope.progress = progress;
+                });
+                element.bind('$destroy', function() {
+                    tasksStatusProvider.unregister(args.uuid);
+                });
+            }
+        }
+    }]);
