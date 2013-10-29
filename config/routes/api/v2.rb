c270e3d59b1b3e91cb7e818c4ebec271333c12ab
Ivan Nečas <inecas@redhat.com>
2013-10-29 16:18:05 +0100

    DyntasksController#search for handling multiple tasks conditions.

--- config/routes/api/v2.rb
+++ config/routes/api/v2.rb
@@ -22,7 +22,7 @@ Src::Application.routes.draw do
         onlies = [:show, :destroy, :index, :update]
       end
 
-      api_resources :dyntasks, :only => [:index]
+      post '/dyntasks' => 'dyntasks#search'
 
       api_resources :organizations do
         member do
