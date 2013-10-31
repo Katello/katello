576cb7f7c634f200b1817d4585c2aa5434c24f10
Ivan Nečas <inecas@redhat.com>
2013-10-31 12:34:35 +0100

    Extract pulp polling service into module

--- lib/orchestrate/katello/pulp/repository_sync.rb
+++ lib/orchestrate/katello/pulp/repository_sync.rb
@@ -13,87 +13,10 @@
 module Orchestrate
   module Katello
     module Pulp
-
-    class PollingServiceImpl < Dynflow::MicroActor
-
-      Task = Algebrick.type do
-        fields(action:    Dynflow::Action::Suspended,
-               pulp_user: String,
-               task_id:   String)
-      end
-
-      Tick = Algebrick.type
-
-      def initialize(logger)
-        super(logger)
-        @tasks    = Set.new
-        @progress = Hash.new { |h, k| h[k] = 0 }
-
-        @start_ticker = Queue.new
-        @ticker       = Thread.new do
-          loop do
-            sleep interval
-            self << Tick
-            @start_ticker.pop
-          end
-        end
-      end
-
-      def wait_for_task(action, pulp_user, task_id)
-        # simulate polling for the state of the external task
-        self << Task[action,
-                     pulp_user,
-                     task_id]
-      end
-
-      private
-
-      def interval
-        0.5
-      end
-
-      def on_message(message)
-        match(message,
-              ~Task >>-> task do
-                @tasks << task
-              end,
-              Tick >>-> do
-                poll
-              end)
-      end
-
-      def tick
-        @start_ticker << true
-      end
-
-      def as_pulp_user(pulp_user)
-        ret = nil
-        User.set_pulp_config(pulp_user) do
-          ret = yield
-        end
-        return ret
-      end
-
-      def poll
-        @tasks.delete_if do |task|
-          as_pulp_user(task[:pulp_user]) do
-            pulp_task = ::Katello.pulp_server.resources.task.poll(task[:task_id])
-            done = !! pulp_task[:finish_time]
-            task[:action].update_progress(done, pulp_task)
-            done
-          end
-        end
-      ensure
-        tick
-      end
-    end
-
-    PollingService = PollingServiceImpl.new(Logging.logger['glue'])
-
-
       class RepositorySync < Dynflow::Action
 
         include Helpers::RemoteAction
+        include Helpers::PulpTask
 
         input_format do
           param :repo_id, Integer
@@ -108,25 +31,14 @@ module Orchestrate
           output[:pulp_tasks] = pulp_tasks
 
           # TODO: would be better polling for the whole task group to make sure we're really finished at the end
-          sync_task = pulp_tasks.find do |task|
+          output[:pulp_task] = pulp_tasks.find do |task|
             task['tags'].include?("pulp:action:sync")
           end
-          output[:sync_task] = sync_task
-          output[:task_id] = sync_task['task_id']
-          suspend
-        end
-
-
-        def setup_suspend(suspended_action)
-          PollingService.wait_for_task(suspended_action, input[:remote_user], output[:task_id])
-        end
-
-        def update_progress(done, pulp_task)
-          output.update sync_task: pulp_task, done: done
+          wait_for_pulp
         end
 
         def run_progress
-          sync_task = output[:sync_task]
+          sync_task = output[:pulp_task]
           if sync_task &&
                 sync_task[:progress] &&
                 sync_task[:progress][:yum_importer] &&
