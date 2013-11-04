0021bdccc84f964e7b1e7e21a729ad6ae61bbe2f
Ivan Nečas <inecas@redhat.com>
2013-11-04 18:16:40 +0100

    Show current tasks for the user

---
+++ engines/bastion/app/assets/bastion/widgets/current-tasks.widget.js
@@ -0,0 +1,52 @@
+/**
+ Copyright 2013 Red Hat, Inc.
+
+ This software is licensed to you under the GNU General Public
+ License as published by the Free Software Foundation; either version
+ 2 of the License (GPLv2) or (at your option) any later version.
+ There is NO WARRANTY for this software, express or implied,
+ including the implied warranties of MERCHANTABILITY,
+ NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+ have received a copy of GPLv2 along with this software; if not, see
+ http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ **/
+
+angular.module('Bastion.widgets').directive('currentTasks',
+    ['$document', 'CurrentUser', 'taskListProvider',
+    function($document, CurrentUser, taskListProvider) {
+
+        return {
+            restrict: 'A',
+            scope: true,
+            templateUrl: 'widgets/views/current-tasks.html',
+
+            controller: ['$scope', function($scope) {
+                $scope.visible = false;
+                $scope.currentUser = CurrentUser;
+                $scope.count = 0;
+
+                $scope.toggleVisibility = function() {
+                    $scope.visible = !$scope.visible;
+                };
+
+                $scope.updateTasks = function(tasks) {
+                    $scope.count = tasks.length;
+                }
+
+                // Hide the current tasks list if the user clicks outside of it
+                var currentTasksMenu = angular.element('#currentTasks');
+                $document.bind('click', function (event) {
+                    var target = angular.element(event.target);
+                    if (!currentTasksMenu.find(target).length) {
+                        $scope.visible = false;
+                        if (!$scope.$$phase) {
+                            $scope.$apply();
+                        }
+                    }
+                });
+            }],
+            link: function(scope) {
+                taskListProvider.registerUser(scope, { activeOnly: true }, CurrentUser);
+            }
+        };
+    }]);
