b234924b2fe0a74139730ab23b2cfe2f8cf56ea4
Petr Chalupa <git@pitr.ch>
2013-11-11 09:56:51 +0100

    Add Katello specific world

--- lib/orchestrate.rb
+++ lib/orchestrate.rb
@@ -1,4 +1,6 @@
 module Orchestrate
+  require 'orchestrate/world'
+
   files = Dir.chdir(File.join(Rails.root, 'lib')) do
     Dir.glob('orchestrate/{helpers,elastic_search,katello,headpin}/**/*.rb') +
         Dir.glob('{katello,headpin}/actions/*.rb')
@@ -9,25 +11,20 @@ module Orchestrate
   def self.world
     return @world if @world
 
-    db_config = ActiveRecord::Base.configurations[Rails.env]
+    db_config            = ActiveRecord::Base.configurations[Rails.env]
     db_config['adapter'] = 'postgres' if db_config['adapter'] == 'postgresql'
-    world_options =
-        { executor_class:      Dynflow::Executors::Parallel,
-          pool_size:           5,
-          persistence_adapter: Dynflow::PersistenceAdapters::Sequel.new(db_config),
-          transaction_adapter: Dynflow::TransactionAdapters::ActiveRecord.new }
+    world_options        = {
+        executor_class:      Dynflow::Executors::Parallel, # TODO configurable Parallel or Remote
+        pool_size:           5,
+        persistence_adapter: Dynflow::PersistenceAdapters::Sequel.new(db_config),
+        transaction_adapter: Dynflow::TransactionAdapters::ActiveRecord.new }
 
     @world = Orchestrate::World.new(world_options).tap do
       at_exit { @world.terminate!.wait }
     end
   end
 
-  def self.trigger(action, *args)
-    uuid, f = world.trigger(action, *args)
-    DynflowTask.create!(uuid: uuid, action: action.name, user_id: User.current.id) do |task|
-      # to set additional task meta-data
-      yield task if block_given?
-    end
-    return uuid, f
+  def self.trigger(action, *args, &block)
+    world.trigger action, *args, &block
   end
 end
