5eed711671d7e310fa6cd398efa91d3871137f03
Petr Chalupa <git@pitr.ch>
2013-11-11 14:46:41 +0100

    reloading of dynflow actions
    for better development

--- lib/orchestrate.rb
+++ lib/orchestrate.rb
@@ -1,13 +1,6 @@
 module Orchestrate
   require 'orchestrate/world'
 
-  files = Dir.chdir(File.join(Rails.root, 'lib')) do
-    Dir.glob('orchestrate/{helpers,elastic_search,katello,headpin}/**/*.rb') +
-        Dir.glob('{katello,headpin}/actions/*.rb')
-  end
-
-  files.each { |f| require f }
-
   def self.world
     return @world if @world
 
@@ -21,12 +14,29 @@ module Orchestrate
         persistence_adapter: Dynflow::PersistenceAdapters::Sequel.new(db_config),
         transaction_adapter: Dynflow::TransactionAdapters::ActiveRecord.new }
 
-    @world = Orchestrate::World.new(world_options).tap do
+    @world = Orchestrate::World.new(world_options).tap do |world|
+      ActionDispatch::Reloader.to_prepare { world.reload! }
       at_exit { @world.terminate!.wait }
     end
   end
 
   def self.trigger(action, *args, &block)
     world.trigger action, *args, &block
+  end
+
+  def self.eager_load_paths
+    %W( #{Rails.root}/app/lib/orchestrate
+        #{Rails.root}/app/lib/headpin/actions
+        #{Rails.root}/app/lib/katello/actions )
+
+  end
+
+  def self.eager_load!
+    eager_load_paths.each do |load_path|
+      matcher = /\A#{Regexp.escape(load_path)}\/(.*)\.rb\Z/
+      Dir.glob("#{load_path}/**/*.rb").sort.each do |file|
+        require_dependency file.sub(matcher, '\1')
+      end
+    end
   end
 end
