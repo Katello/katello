f1da708e7ad1c1fa0d208dd34d62b0a83a2fe20d
Eric D. Helms <ericdhelms@gmail.com>
2013-11-12 07:49:23 -0500

    Engine: Fixes #3632: Cleans up spec/helpers.

--- spec/helpers/application_info_helper_spec.rb
+++ spec/helpers/application_info_helper_spec.rb
@@ -10,47 +10,47 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-require 'spec_helper'
+require 'katello_test_helper'
 
 def current_user
   User.current
 end
 
+module Katello
 describe ApplicationInfoHelper do
   include AuthorizationHelperMethods
   helper ApplicationInfoHelper
 
   context '.can_read_system_info?' do
     before(:each) do
-      Resources::Candlepin::Owner.stub!(:create_user).and_return(true)
-      Resources::Candlepin::Owner.should_receive(:create).at_least(:once).
-        and_return({})
+      Resources::Candlepin::Owner.stubs(:create_user).returns(true)
+      Resources::Candlepin::Owner.expects(:create).once.returns({})
       disable_env_orchestration
       disable_user_orchestration
       Katello.config[:warden] = 'ldap'
       Katello.config[:validate_ldap] = false
-      User.stub!(:cp_oauth_header).and_return("abc123")
+      User.stubs(:cp_oauth_header).returns("abc123")
       @org = Organization.create!(:name => "Haskell_Curry_Inc",
                                   :label => "haskell_curry_inc"
                                  )
-      @user = User.create!(:login => "haskell",
-                           :mail => "hcurry@lambda.com"
-                          )
+      @user = users(:one)
     end
 
     it 'should return false if there is no current user' do
-      can_read_system_info?.should eql(false)
+      User.current = nil
+      can_read_system_info?.must_equal(false)
     end
 
     it 'should return false for a user without org read access' do
       User.current = @user
-      can_read_system_info?.should be_false
+      can_read_system_info?.must_equal(false)
     end
 
     it 'should return true for a user with org read access' do
       User.current = @user
       allow(@user.own_role, [:read], :organizations, nil, @org)
-      can_read_system_info?.should be_true
+      can_read_system_info?.must_equal(true)
     end
   end
+end
 end
