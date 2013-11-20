068e53612e1d4c48027f5899ec7ec4df61e5a4a4
Adam Price <komidore64@gmail.com>
2013-11-20 17:20:02 -0500

    API v2 - ping controller and /api/ping route

--- app/controllers/katello/api/v2/ping_controller.rb
+++ app/controllers/katello/api/v2/ping_controller.rb
@@ -11,9 +11,15 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 module Katello
-class Api::V2::PingController < Api::V1::PingController
+  class Api::V2::PingController < Api::V1::PingController
 
-  include Api::V2::Rendering
+    include Api::V2::Rendering
 
-end
+    api :GET, "/ping", "Shows status of system and it's subcomponents"
+    description "This service is only available for authenticated users"
+    def index
+      respond_for_show :resource => Ping.ping
+    end
+
+  end
 end
