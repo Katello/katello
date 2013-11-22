2cde0c1d77c50a69356b8dfd0924feb55846ca88
Eric D. Helms <ericdhelms@gmail.com>
2013-11-22 11:49:37 -0500

    Bastion: Updating forms to Bootstrap3 styling and encapsulating the basic structure into directives.

---
+++ engines/bastion/app/assets/bastion/incubator/alch-form-buttons.directive.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2013 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public
+ * License as published by the Free Software Foundation; either version
+ * 2 of the License (GPLv2) or (at your option) any later version.
+ * There is NO WARRANTY for this software, express or implied,
+ * including the implied warranties of MERCHANTABILITY,
+ * NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+ * have received a copy of GPLv2 along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ */
+
+/**
+ * @ngdoc directive
+ * @name alchemy.directive:alchFormButtons
+ *
+ * @description
+ *   Encapsulates the standard structure and styling for create and cancel buttons
+ *   when used with a form.
+ *
+ * @example
+ *      <div alch-form-buttons
+             on-cancel="transitionTo('product.index')"
+             on-save="save(product)"
+             working="working">
+        </div>
+ */
+angular.module('alchemy').directive('alchFormButtons', function() {
+    return {
+        replace: true,
+        require: '^form',
+        templateUrl: 'incubator/views/alch-form-buttons.html',
+        scope: {
+            handleCancel: '&onCancel',
+            handleSave: '&onSave',
+            working: '='
+        },
+        link: function(scope, iElement, iAttrs, controller) {
+
+            scope.invalid = function() {
+                var invalid = controller.$invalid;
+
+                angular.forEach(controller, function(value) {
+                    if (value.$error) {
+                        if (value.$error.server) {
+                            invalid = false;
+                        }
+                    }
+                });
+
+                return invalid;
+            };
+        }
+    };
+});
