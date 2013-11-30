bb797c27e35d393b49b13fd99f88d93a4bcd99af
Adam Price <komidore64@gmail.com>
2013-11-30 11:14:52 -0500

    attempting to fix busted test: Katello::Api::V1::UsersControllerTest.test_list_owners_username

--- app/lib/katello/api/constraints/api_version_constraint.rb
+++ app/lib/katello/api/constraints/api_version_constraint.rb
@@ -11,24 +11,24 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 module Katello
-class ApiVersionConstraint
-  def initialize(options)
-    @version = options[:version]
-    @default = options.key?(:default) ? options[:default] : false
-  end
+  class ApiVersionConstraint
+    def initialize(options)
+      @version = options[:version]
+      @default = options.key?(:default) ? options[:default] : false
+    end
 
-  def matches?(req)
-    header_match = req.accept.match(/version=([\d\.]+)/)
-    route_match = req.fullpath.match(%r{api/v\d/})
-    version_match = req.fullpath.match(%r{api/v#{@version}/})
+    def matches?(req)
+      header_match = req.accept.try(:match, /version=([\d\.]+)/)
+      route_match = req.fullpath.match(%r{api/v\d/})
+      version_match = req.fullpath.match(%r{api/v#{@version}/})
 
-    if route_match
-      version_match
-    elsif header_match && (version = header_match[1]) # version is specified in header
-      version == @version.to_s # are the versions same
-    else
-      @default # version is not specified, match if it's default version of api
+      if route_match
+        version_match
+      elsif header_match && (version = header_match[1]) # version is specified in header
+        version == @version.to_s # are the versions same
+      else
+        @default # version is not specified, match if it's default version of api
+      end
     end
   end
-end
 end
