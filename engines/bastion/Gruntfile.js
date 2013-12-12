877eae9971d01135148f04e5211ff315a7f75057
Justin Sherrill <jsherril@redhat.com>
2013-12-12 09:00:13 -0500

    Fixing error @FontAwesomePath undefined

--- engines/bastion/Gruntfile.js
+++ engines/bastion/Gruntfile.js
@@ -39,7 +39,17 @@ module.exports = function (grunt) {
                 options: {
                     targetDir: 'vendor/assets',
                     copy: true,
-                    layout: 'byType',
+                    layout: function(type, component) {
+                        // We provide a bit of customization here by allowing
+                        // explicit path declarations if the component is included
+                        // in the type. This is handy for sub-nesting within folders
+                        // for a component. Fallback is 'byType'.
+                        if (type.indexOf(component) !== -1) {
+                            return require('path').join(type);
+                        } else {
+                            return require('path').join(type, component);
+                        }
+                    },
                     clearBowerDir: true
                 }
             },
