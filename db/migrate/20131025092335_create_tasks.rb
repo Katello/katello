b4e73e8381358e1cc02379a5c8159f8b5ca0561c
Ivan Nečas <inecas@redhat.com>
2014-01-10 09:00:40 +0100

    Remove unused migrations

--- db/migrate/20131025092335_create_tasks.rb
+++
@@ -1,8 +0,0 @@
-class CreateTasks < ActiveRecord::Migration
-  def change
-    create_table :katello_tasks, :id => false do |t|
-      t.string :uuid, index: true
-      t.string :action, index: true
-    end
-  end
-end
