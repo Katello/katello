3a81191cdc58c408f2aa4faa11366caf7824ed2a
Eric D. Helms <ericdhelms@gmail.com>
2014-01-28 17:38:38 -0500

    Bastion: Converts to less based stylesheets completely and removes SCSS dependencies.

--- engines/bastion/lib/bastion/engine.rb
+++ engines/bastion/lib/bastion/engine.rb
@@ -1,15 +1,11 @@
 module Bastion
   class Engine < ::Rails::Engine
+
     isolate_namespace Bastion
 
     initializer "bastion.assets.paths", :group => :all do |app|
-      app.config.assets.paths << Bastion::Engine.root.join('vendor', 'assets', 'stylesheets', 'bastion',
-                                                           'font-awesome', 'scss')
-      app.config.assets.paths << Bastion::Engine.root.join('vendor', 'assets', 'fonts')
-
-      app.config.less.paths << "#{Bastion::Engine.root}/app/assets/stylesheets/bastion/less"
+      app.config.less.paths << "#{Bastion::Engine.root}/app/assets/stylesheets/bastion"
       app.config.less.paths << "#{Bastion::Engine.root}/vendor/assets/stylesheets/bastion"
-
       app.middleware.use ::ActionDispatch::Static, "#{Bastion::Engine.root}/app/assets/javascripts/bastion"
     end
   end
