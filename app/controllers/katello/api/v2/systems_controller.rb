1dadba7c2a1bb2c06530e879b75e855338071c8c
Ivan Nečas <inecas@redhat.com>
2014-02-17 16:20:40 +0100

    Reload system after orchestration is finished

--- app/controllers/katello/api/v2/systems_controller.rb
+++ app/controllers/katello/api/v2/systems_controller.rb
@@ -158,6 +158,7 @@ class Api::V2::SystemsController < Api::V2::ApiController
     @system = System.new(system_params(params).merge(:environment  => @environment,
                                                      :content_view => @content_view))
     sync_task(::Actions::Headpin::System::Create, @system)
+    @system.reload
     respond_for_create
   end
 
