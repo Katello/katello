c0391a38d539047229153d35d38841c4f295d40b
Ivan Nečas <inecas@redhat.com>
2014-03-05 22:49:29 +0100

    Update the Candlepin environment in run phase of create repo

--- app/lib/actions/katello/content_view/update_environment.rb
+++ app/lib/actions/katello/content_view/update_environment.rb
@@ -15,10 +15,14 @@ module Actions
     module ContentView
       class UpdateEnvironment < Dynflow::Action
 
-        def plan(content_view, environment)
+        def plan(content_view, environment, new_content_id = nil)
           view_env = content_view.content_view_environment(environment)
           content_ids = content_view.repos(environment).select(&:enabled).
-              map(&:content_id).uniq
+              map(&:content_id).uniq.compact
+          # in case we create new custom repository that doesn't have the
+          # content_id set yet in the plan phase, we allow to pass it as
+          # additional argument
+          content_ids << new_content_id if new_content_id
           plan_action(Candlepin::Environment::SetContent,
                       cp_environment_id: view_env.cp_id,
                       content_ids:       content_ids)
--- app/lib/actions/katello/repository/create.rb
+++ app/lib/actions/katello/repository/create.rb
@@ -15,14 +15,13 @@ module Actions
     module Repository
       class Create < Actions::EntryAction
 
-        middleware.use Actions::Middleware::RemoteAction
-
         def plan(repository, clone = false)
           repository.disable_auto_reindex!
           repository.save!
           action_subject(repository)
           plan_self
 
+          org = repository.organization
           #check the instance variable, as we do not want to go to pulp
           checksum_type = repository.checksum_type if self.instance_variable_get('@checksum_type')
           if repository.puppet?
@@ -35,7 +34,6 @@ module Actions
             path = repository.relative_path
           end
           sequence do
-            plan_action(Katello::Product::ContentCreate, repository) unless clone
             plan_action(Pulp::Repository::Create,
                         content_type: repository.content_type,
                         pulp_id: repository.pulp_id,
@@ -49,22 +47,16 @@ module Actions
                         path: path,
                         with_importer: true)
             unless clone
+              content_create = plan_action(Katello::Product::ContentCreate, repository)
               plan_action(Katello::Repository::MetadataGenerate, repository)
+              plan_action(ContentView::UpdateEnvironment, org.default_content_view, org.library, content_create.input[:content_id])
               plan_action(ElasticSearch::Reindex, repository)
             end
           end
-
         end
 
         def humanized_name
           _("Create")
-        end
-
-        def finalize
-          repository = ::Katello::Repository.find(input[:repository][:id])
-          org = repository.product.organization
-          # TODO: should be done in Katello::ContentView::UpdateEnvironment
-          org.default_content_view.update_cp_content(org.library)
         end
 
       end
