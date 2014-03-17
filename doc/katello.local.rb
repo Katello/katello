bd1d2f38a41e417f46a61233da2a088905c8c26e
Justin Sherrill <jsherril@redhat.com>
2014-03-17 16:45:44 -0400

    removing symlink to local dynflow

--- doc/katello.local.rb
+++ doc/katello.local.rb
@@ -1,5 +1,4 @@
 # This is the gemfile for foreman that will load the Katello engine.
 # It belongs in foreman/bundler.d/
 
-gem 'dynflow', :path => '../dynflow'
 gemspec :path => '../katello', :development_group => :katello_dev
