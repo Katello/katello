29f27a117d1e8ea4414b3521effc13d7271eaf4c
Michael Stead <mstead@redhat.com>
2014-03-21 12:48:20 -0300

    Fixes #4878: Proxy requests to guestid and content_overrides to candlepin
    - Add support for proxing PUT requests to candlepin
    
    - Allow proxying DELETE request with body to candlepin
    
    - Proxy calls to /api/consumer/:id/guestids/* directly to candlepin
      * Add fake root path for guestids allowing client tooling to check support
      * Proxy GET /consumers/:id/guestids to candlepin
      * Proxy GET /consumers/:id/guestids/:guest_id to candlepin
      * Proxy PUT /consumers/:id/guestids to candlepin
      * Proxy PUT /consumers/:id/guestids/:guest_id to candlepin
      * Proxy DELETE /consumers/:id/guestids/:guest_id to candlepin
    
    - Proxy requests to candlepin's content_overrides
      * Added fake root path that is checked by rhsm (unlocks adding extra
        guest data support in python-rhsm)
      * GET /api/consumers/:id/content_overrides
      * PUT /api/consumers/:id/content_overrides
      * DELETE /api/consumers/:id/content_overrides

--- app/lib/katello/resources/candlepin.rb
+++ app/lib/katello/resources/candlepin.rb
@@ -29,9 +29,11 @@ module Resources
         client.post body, {:accept => :json, :content_type => :json}.merge(User.cp_oauth_header)
       end
 
-      def self.delete(path)
+      def self.delete(path, body = nil)
         logger.debug "Sending DELETE request to Candlepin: #{path}"
         client = CandlepinResource.rest_client(Net::HTTP::Delete, :delete, path_with_cp_prefix(path))
+        # Some candlepin calls will set the body in DELETE requests.
+        client.options[:payload] = body unless body.nil?
         client.delete({:accept => :json, :content_type => :json}.merge(User.cp_oauth_header))
       end
 
@@ -39,6 +41,12 @@ module Resources
         logger.debug "Sending GET request to Candlepin: #{path}"
         client = CandlepinResource.rest_client(Net::HTTP::Get, :get, path_with_cp_prefix(path))
         client.get({:accept => :json}.merge(User.cp_oauth_header))
+      end
+
+      def self.put(path, body)
+        logger.debug "Sending PUT request to Candlepin: #{path}"
+        client = CandlepinResource.rest_client(Net::HTTP::Put, :put, path_with_cp_prefix(path))
+        client.put body, {:accept => :json, :content_type => :json}.merge(User.cp_oauth_header)
       end
 
       def self.path_with_cp_prefix(path)
