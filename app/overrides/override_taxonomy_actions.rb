760ce8560ca5c6e636b5d0d9a84f61e9f76ca343
Brad Buckingham <bbuckingham@redhat.com>
2014-06-28 13:02:08 -0400

    fixes #6427 / BZ 1109737 - remove Nest option from taxonomy actions
    This commit removes Nest as a valid option for taxonomies (e.g Organization
    & Location)

--- app/overrides/override_taxonomy_actions.rb
+++ app/overrides/override_taxonomy_actions.rb
@@ -1,6 +1,7 @@
 # TODO: ORG_DESTROY - temporarily remove the org destroy link
+# Remove support for organization nesting
 Deface::Override.new(:virtual_path => "taxonomies/index",
-                     :name => "remove_organization_delete",
+                     :name => "override_taxonomy_actions",
                      :replace => 'code[erb-loud]:contains("action_buttons")',
                      :partial => '../overrides/foreman/taxonomies/action_buttons'
                     )
