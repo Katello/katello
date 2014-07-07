bdc09c73b8c33ef37e222a1ffca7fc739626f849
Justin Sherrill <jsherril@redhat.com>
2014-07-07 09:36:13 -0400

    fixes #6508 - fixing package group search
    With the removal of the lowercase filter previously
    in the kt_name_analyzer, filters would work properly
    but search were still being downcased by default, thus
    anything with an uppercase letter could not be searched for
    
    also the controller logic had some incorrect code

--- app/controllers/katello/api/v2/content_view_filters_controller.rb
+++ app/controllers/katello/api/v2/content_view_filters_controller.rb
@@ -117,9 +117,9 @@ class Api::V2::ContentViewFiltersController < Api::V2::ApiController
   param :id, :identifier, :desc => N_("filter identifier"), :required => true
   def available_package_groups
     current_ids = @filter.package_group_rules.map(&:uuid)
-    repo_ids = @filter.applicable_repos.select([:pulp_id, "#{Katello::Repository.table_name}.name"])
-    search_filters = [{ :terms => { :repo_id => repo_ids } },
-                      { :not => { :terms => { :id => current_ids } } }]
+    repo_ids = @filter.applicable_repos.readable.pluck("#{Repository.table_name}.pulp_id")
+    search_filters = [{ :terms => { :repo_id => repo_ids } }]
+    search_filters << { :not => { :terms => { :id => current_ids } } } unless current_ids.blank?
 
     options = sort_params
     options[:filters] = search_filters
