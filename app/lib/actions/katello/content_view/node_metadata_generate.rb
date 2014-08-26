b50dc0e4e85bd0f46d65775c35f0050c4a62c988
Justin Sherrill <jsherril@redhat.com>
2014-08-26 17:51:51 -0400

    fixes #7273 - sync puppet and rpm content together
    previously there was a task spawned to sync rpm content to the capsule
    when puppet content sync to the capsule was added, it was not added in
    this 2nd task.  This commit moves puppet syncing to that 2nd, spanwed task.

--- app/lib/actions/katello/content_view/node_metadata_generate.rb
+++ app/lib/actions/katello/content_view/node_metadata_generate.rb
@@ -26,12 +26,16 @@ module Actions
         def plan(content_view, environment)
           action_subject(content_view)
 
-          sequence do
+          concurrence do
             ::Katello::Repository.in_content_views([content_view]).in_environment(environment).each do |repo|
               plan_action(Katello::Repository::NodeMetadataGenerate, repo)
-              plan_self(:environment_name => environment.name)
             end
+
+            cv_puppet_env = ::Katello::ContentViewPuppetEnvironment.in_environment(environment).
+                in_content_view(content_view).first
+            plan_action(Katello::Repository::NodeMetadataGenerate, cv_puppet_env)
           end
+          plan_self(:environment_name => environment.name)
         end
 
       end
