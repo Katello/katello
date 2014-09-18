02487db9278bd8080dd8e91716213a47b36bd6a0
Ivan Nečas <inecas@redhat.com>
2014-09-18 16:49:39 +0200

    Fixes #7527 - don't require less in production unless from rake

--- engines/bastion/lib/bastion/engine.rb
+++ engines/bastion/lib/bastion/engine.rb
@@ -12,8 +12,11 @@ module Bastion
     end
 
     initializer "bastion.assets.paths", :group => :all do |app|
-      app.config.less.paths << "#{Bastion::Engine.root}/app/assets/stylesheets/bastion"
-      app.config.less.paths << "#{Bastion::Engine.root}/vendor/assets/stylesheets/bastion"
+      if !Rails.env.production? || Foreman.in_rake?
+        require 'less-rails'
+        app.config.less.paths << "#{Bastion::Engine.root}/app/assets/stylesheets/bastion"
+        app.config.less.paths << "#{Bastion::Engine.root}/vendor/assets/stylesheets/bastion"
+      end
       app.middleware.use ::ActionDispatch::Static, "#{Bastion::Engine.root}/app/assets/javascripts/bastion"
     end
 
