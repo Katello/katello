2ccb516995053895d565642c0242fed62dd77b58
Brad Buckingham <bbuckingham@redhat.com>
2014-09-24 15:20:51 -0400

    fixes #7124 - Docker - initial backend/api changes to support repository CRUD actions
    This commit contains the changes to support create/review/update/delete
    of docker repositories.

--- app/controllers/katello/api/v2/repositories_controller.rb
+++ app/controllers/katello/api/v2/repositories_controller.rb
@@ -36,7 +36,7 @@ class Api::V2::RepositoriesController < Api::V2::ApiController
     param :url, String, :required => true, :desc => N_("repository source url")
     param :gpg_key_id, :number, :desc => N_("id of the gpg key that will be assigned to the new repository")
     param :unprotected, :bool, :desc => N_("true if this repository can be published via HTTP")
-    param :content_type, String, :desc => N_("type of repo (either 'yum' or 'puppet', defaults to 'yum')")
+    param :content_type, String, :desc => N_("type of repo ('yum', 'puppet' or 'docker', defaults to 'yum')")
   end
 
   api :GET, "/repositories", N_("List of enabled repositories")
