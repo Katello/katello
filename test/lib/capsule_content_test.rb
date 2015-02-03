a9ee482323cfdc9f2bfbbfd1b09a3dba85dabc92
Partha Aji <paji@redhat.com>
2015-02-03 14:48:08 -0500

    Fixes #9209 - Default capsule now checked using Pulp feature
    Previously default capsules would be checked via matching hostnames of
    the satellite and the hostname given to the capsule. This caused all
    sorts of issues when customers did things like renaming hosts etc.
    
    This is now fixed by using the fact that only the default capsule  has
    "Pulp" feature, as opposed to the other capsules that typically have the 'Pulp Node'
    feature.

--- test/lib/capsule_content_test.rb
+++ test/lib/capsule_content_test.rb
@@ -31,5 +31,29 @@ module Katello
       capsule_content.lifecycle_environments.must_include(environment)
       capsule_content.lifecycle_environments(organization.id).wont_include(environment)
     end
+
+    specify "listing capsule content in environment" do
+      pulp_node_feature = Feature.create(:name => SmartProxy::PULP_NODE_FEATURE)
+      pulp_default_feature = Feature.create(:name => SmartProxy::PULP_FEATURE)
+
+      with_pulp_node = smart_proxies(:four).tap do |proxy|
+        proxy.features << pulp_node_feature
+      end
+      with_pulp = smart_proxies(:three).tap do |proxy|
+        proxy.features << pulp_default_feature
+      end
+      pulp_node_capsule_content = Katello::CapsuleContent.new(with_pulp_node)
+      pulp_node_capsule_content.add_lifecycle_environment(environment)
+
+      pulp_capsule_content = Katello::CapsuleContent.new(with_pulp)
+      pulp_capsule_content.add_lifecycle_environment(environment)
+
+      refute_includes CapsuleContent.with_environment(environment, false).map(&:capsule), with_pulp
+      refute_includes CapsuleContent.with_environment(environment).map(&:capsule), with_pulp
+      assert_includes CapsuleContent.with_environment(environment).map(&:capsule), with_pulp_node
+
+      assert_includes CapsuleContent.with_environment(environment, true).map(&:capsule), with_pulp
+      assert_includes CapsuleContent.with_environment(environment, true).map(&:capsule), with_pulp_node
+    end
   end
 end
