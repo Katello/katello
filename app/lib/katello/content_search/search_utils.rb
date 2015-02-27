5ec8425138e5f7cd0a978da4025b7909c6b4f099
Eric D. Helms <ericdhelms@gmail.com>
2015-02-27 16:34:26 -0500

    Fixes #9586: Content view comparison will provide results again. (cherry picked from commit 973918eeb03fe3db1c1b6ab643977d57c1bdab97)

--- app/lib/katello/content_search/search_utils.rb
+++ app/lib/katello/content_search/search_utils.rb
@@ -14,10 +14,12 @@ module Katello
   class ContentSearch::SearchUtils
     cattr_accessor :current_organization, :env_ids, :offset, :current_user
 
-    delegate :page_size, :to => :current_user
+    class << self
+      delegate :page_size, :to => :current_user
+    end
 
     def self.search_envs(mode)
-      if mode != :all
+      if mode != 'all'
         KTEnvironment.readable.where(:id => self.env_ids)
       else
         KTEnvironment.readable
