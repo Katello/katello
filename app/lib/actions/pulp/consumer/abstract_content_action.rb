c0e5ea074b4e171653c4abcef02ff8ce612e1419
Justin Sherrill <jsherril@redhat.com>
2015-03-12 09:44:14 -0400

    fixes #9722 - recognize and show errors on host content actions (cherry picked from commit 19d6db33a74a38368ef0563043fa6ff523da01ca)

--- app/lib/actions/pulp/consumer/abstract_content_action.rb
+++ app/lib/actions/pulp/consumer/abstract_content_action.rb
@@ -17,6 +17,28 @@ module Actions
         include Helpers::Presenter
         include Actions::Pulp::ExpectOneTask
 
+        def external_task=(external_task_data)
+          super(external_task_data)
+          task_errors = find_errors(output[:pulp_tasks])
+          if task_errors.any?
+            fail ::Katello::Errors::PulpError, task_errors.join("\n")
+          end
+        end
+
+        def find_errors(tasks)
+          messages = []
+          tasks.each do |pulp_task|
+            if pulp_task[:result] && pulp_task[:result][:details]
+              pulp_task[:result][:details].each do |_content_type, result|
+                unless result[:succeeded]
+                  messages << result[:details][:message]
+                end
+              end
+            end
+          end
+          messages
+        end
+
         def presenter
           Consumer::ContentPresenter.new(self)
         end
