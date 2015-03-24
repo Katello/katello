b969597f0b96261b3a051ac47491fb7fc67a8285
Justin Sherrill <jsherril@redhat.com>
2015-03-24 09:22:46 -0400

    fixes #9883 - making auto attach run after consumer update
    dynflow uses concurrent by default, which is not really what we want here
    (cherry picked from commit a6ca904fe2fd057e5606683e1354318cbc4f5da1)

--- app/lib/actions/katello/system/update.rb
+++ app/lib/actions/katello/system/update.rb
@@ -20,12 +20,17 @@ module Actions
           system.disable_auto_reindex!
           action_subject system
           system.update_attributes!(sys_params)
-          plan_action(::Actions::Pulp::Consumer::Update, system) if ::Katello.config.use_pulp
-          plan_action(::Actions::Candlepin::Consumer::Update, system) if ::Katello.config.use_cp
-          if sys_params[:autoheal] && ::Katello.config.use_cp
-            plan_action(::Actions::Candlepin::Consumer::AutoAttachSubscriptions, system)
+          sequence do
+            concurrence do
+              plan_action(::Actions::Pulp::Consumer::Update, system) if ::Katello.config.use_pulp
+              plan_action(::Actions::Candlepin::Consumer::Update, system) if ::Katello.config.use_cp
+            end
+
+            if sys_params[:autoheal] && ::Katello.config.use_cp
+              plan_action(::Actions::Candlepin::Consumer::AutoAttachSubscriptions, system)
+            end
+            plan_action(ElasticSearch::Reindex, system) if ::Katello.config.use_elasticsearch
           end
-          plan_action(ElasticSearch::Reindex, system) if ::Katello.config.use_elasticsearch
         end
       end
     end
