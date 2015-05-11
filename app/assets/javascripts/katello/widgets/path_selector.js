605f0ebb0c76202382636645cc3f02954fae5ecf
David Davis <daviddavis@redhat.com>
2015-05-11 19:11:43 -0400

    Refs #10388 - Remove license from javascript non-test files

--- app/assets/javascripts/katello/widgets/path_selector.js
+++ app/assets/javascripts/katello/widgets/path_selector.js
@@ -1,18 +1,4 @@
 /**
- Copyright 2014 Red Hat, Inc.
-
- This software is licensed to you under the GNU General Public
- License as published by the Free Software Foundation; either version
- 2 of the License (GPLv2) or (at your option) any later version.
- There is NO WARRANTY for this software, express or implied,
- including the implied warranties of MERCHANTABILITY,
- NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
- have received a copy of GPLv2 along with this software; if not, see
- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-*/
-
-
-/**
  * @param div_id   -  id of div to house env_selector
  * @param name    -  the name of this env selector ( in case there is more than one)
  * @param environments - array of environment paths
