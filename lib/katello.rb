83637bb8183fdbe40fcae38618aad69b11e806c9
Eric D. Helms <ericdhelms@gmail.com>
2015-08-13 13:35:22 -0400

    Fixes #11348: Remove justified rubygem as a dependency.

--- lib/katello.rb
+++ lib/katello.rb
@@ -11,7 +11,6 @@ require "hooks"
 require "foreigner"
 require "foreman-tasks"
 require "rest_client"
-require "justified/standard_error"
 require "anemone"
 require "securerandom"
 require 'foreman_docker'
