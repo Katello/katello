9305dffa91cfa8e6e4b4226e22cb9492fe563ba6
Ondrej Prazak <oprazak@redhat.com>
2015-09-24 14:19:19 +0200

    Fixes #11947 - Whitelist for organization parameters route

--- config/routes/overrides.rb
+++ config/routes/overrides.rb
@@ -1,6 +1,21 @@
+module Katello
+  class WhitelistConstraint
+    PATHS = [/^\/api\/v2\/organizations\/\S+\/parameters/]
+
+    def matches?(request)
+      PATHS.map { |path| request.env["REQUEST_PATH"].match(path) }.any? ? false : true
+    end
+  end
+end
+
 Foreman::Application.routes.draw do
-  match "/api/v2/organizations/*all", to: proc { [404, {}, ['']] }, :via => :get
-  match "/api/v1/organizations/:id", via: :delete, to: proc { [404, {}, ['']] }
+  override_message = '{"message": "Route forbidden by Katello, check katello/config/routes/overrides"}'
+
+  match "/api/v2/organizations/*all", :to => proc { [404, {}, [override_message]] },
+                                      :via => :get,
+                                      :constraints => Katello::WhitelistConstraint.new
+
+  match "/api/v1/organizations/:id", via: :delete, to: proc { [404, {}, [override_message]] }
 
   resources :operatingsystems, :only => [] do
     get 'available_kickstart_repo', :on => :member
