e1cbfbd622c120e738eaa6cd9d1000329215d977
Adam Price <komidore64@gmail.com>
2015-10-05 15:41:07 -0400

    refs #10970 - fixing AR methods after master->rails4 merge

--- test/actions/import_pool_test.rb
+++ test/actions/import_pool_test.rb
@@ -6,6 +6,8 @@ module Katello
     let(:pool) { OpenStruct.new(:id => pool_id) }
 
     before do
+      ::Actions::Candlepin::ImportPoolHandler.any_instance.stubs(:attempt_find_pool).returns('id' => pool_id)
+      ::Katello::Pool.expects(:find_by).with(has_key(:cp_id)).returns(pool)
       ::Katello::Pool.stubs(:search).returns([pool])
       ::Katello::Pool.any_instance.stubs(:import_data).returns(true)
     end
