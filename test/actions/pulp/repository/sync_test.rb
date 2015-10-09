0270c27a1e05cbbf0f85e5993a9a05e6ec03aea0
Adam Price <komidore64@gmail.com>
2015-10-09 10:49:08 -0400

    refs #10970 - renaming resource_ids to pulp_resource_ids
    At an unknown point in time between Rails 3 and 4, ActiveRecord defined
    some new bits that generated a method for collecting database IDs [1].
    We don't want a collision to occuring between the ActiveRecord
    <resource>_ids method and our defined pulp <resource>_ids methods.
    
    [1] http://edgeguides.rubyonrails.org/association_basics.html#has-many-association-reference

--- test/actions/pulp/repository/sync_test.rb
+++ test/actions/pulp/repository/sync_test.rb
@@ -29,7 +29,7 @@ module ::Actions::Pulp::Repository
     let(:action_class) { ::Actions::Pulp::Repository::Sync }
     def test_sync
       run_action(action_class, pulp_id: repo.pulp_id)
-      assert_equal 8, repo.rpm_ids.length
+      assert_equal 8, repo.pulp_rpm_ids.length
     end
   end
 end
