82d6d8fd8153a5f8bd0262e4eb0b6eb1e04a6dfc
John Mitsch <jomitsch@redhat.com>
2015-11-13 16:42:30 -0500

    Fixes #12476 - Refactor max content host validator for rails 4

--- test/controllers/api/v2/host_collections_controller_test.rb
+++ test/controllers/api/v2/host_collections_controller_test.rb
@@ -6,6 +6,7 @@ module Katello
   class Api::V2::HostCollectionsControllerTest < ActionController::TestCase
     def models
       @system = katello_systems(:simple_server)
+      @system_two = katello_systems(:simple_server2)
       @host_collection = katello_host_collections(:simple_host_collection)
       @organization = get_organization
 
@@ -64,6 +65,59 @@ module Katello
       assert_equal results['system_ids'], [@system.id]
 
       assert_template 'api/v2/host_collections/create'
+    end
+
+    def test_validate_systems
+      max_content_hosts = 1
+      content_host_name = 'Collection A'
+      post :create, :organization_id => @organization,
+                    :host_collection => {:name => content_host_name, :description => 'Collection A, World Cup 2014',
+                                         :max_content_hosts => max_content_hosts, :unlimited_content_hosts => false,
+                                         :system_ids => [@system.id, @system_two.id]}
+
+      results = JSON.parse(response.body)
+      error_message = "You cannot have more than #{max_content_hosts} content host(s) associated with host collection #{content_host_name}."
+
+      assert_response 422
+      assert results["errors"]["base"].include?(error_message)
+    end
+
+    def test_max_content_host_validator
+      put :update, :id => @host_collection.id, :organization_id => @organization.id,
+                   :max_content_hosts => 2, :unlimited_content_hosts => false, :system_ids => [@system.id, @system_two.id]
+
+      assert_response :success
+
+      put :update, :id => @host_collection.id, :organization_id => @organization.id,
+                   :max_content_hosts => 1
+
+      results = JSON.parse(response.body)
+      error_message = "may not be less than the number of content hosts associated with the host collection."
+
+      assert_response 422
+      assert results["errors"]["max_content_host"].include?(error_message)
+    end
+
+    def test_max_content_host_zero
+      put :update, :id => @host_collection.id, :organization_id => @organization.id,
+                   :max_content_hosts => 0, :unlimited_content_hosts => false
+
+      results = JSON.parse(response.body)
+      error_message = "must be a positive integer value."
+
+      assert_response 422
+      assert results["displayMessage"].include?(error_message)
+    end
+
+    def test_nil_max_content_hosts
+      put :update, :id => @host_collection.id, :organization_id => @organization.id,
+                   :unlimited_content_hosts => false
+
+      results = JSON.parse(response.body)
+      error_message = "must be given a value if this host collection is not unlimited."
+
+      assert_response 422
+      assert results["displayMessage"].include?(error_message)
     end
 
     def test_create_with_system_uuid
