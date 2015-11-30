052633607e71fa01a4ad81a03397d286e5ad2222
John Mitsch <jomitsch@redhat.com>
2015-11-30 09:42:04 -0500

    Refs #10970 - Rails 4 fixes viewing list/remove subscription tab

--- app/presenters/katello/activation_key_subscription_presenter.rb
+++ app/presenters/katello/activation_key_subscription_presenter.rb
@@ -4,7 +4,7 @@ module Katello
 
     def initialize(subscription)
       @subscription = Katello::Pool.find_by(:cp_id => subscription["id"])
-      @subscription["quantity_attached"] = subscription.try(:[], :amount)
+      @subscription.quantity_attached = subscription.try(:[], :amount)
     end
   end
 end
