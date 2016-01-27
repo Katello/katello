35817179e14cebe0bb59139982a1ad5d21ad9bab
Eric D. Helms <ericdhelms@gmail.com>
2016-01-26 23:18:50 -0500

    Fixes #5959: Remove unused content readable permissions

--- app/models/katello/authorization/repository.rb
+++ app/models/katello/authorization/repository.rb
@@ -35,28 +35,6 @@ module Katello
       def syncable
         where(:product_id => Katello::Product.authorized(:sync_products))
       end
-
-      def libraries_content_readable(_org)
-        repos = Repository.readable
-        lib_ids = []
-        repos.each { |r|  lib_ids << (r.library_instance_id || r.id) }
-        where(:id => lib_ids)
-      end
-
-      def content_readable(org)
-        prod_ids = Katello::Product.readable.collect { |p| p.id }
-        env_ids = KTEnvironment.content_readable(org)
-        where(environment_id: env_ids, product_id: prod_ids)
-      end
-
-      def readable_in_org(org, *skip_library)
-        if (skip_library.empty? || skip_library.first.nil?)
-          # 'skip library' not included, so retrieve repos in library in the result
-          where(environment_id: KTEnvironment.content_readable(org))
-        else
-          where(environment_id: KTEnvironment.content_readable(org).non_library)
-        end
-      end
     end
   end
 end
