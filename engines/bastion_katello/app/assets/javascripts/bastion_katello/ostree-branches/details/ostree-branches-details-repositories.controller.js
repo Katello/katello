67d0bd279e88783cd141bde4f00bf9619a6d06cf
Partha Aji <paji@redhat.com>
2016-03-08 18:29:33 -0500

    Fixes #13953 - Can search Ostree Repos
    Added an entry in the content menu to show ostree units

---
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/ostree-branches/details/ostree-branches-details-repositories.controller.js
@@ -0,0 +1,60 @@
+/**
+ * @ngdoc object
+ * @name  Bastion.ostree-branches.controller:OstreeBranchesDetailsRepositoriesController
+ *
+ * @requires $scope
+ * @requires $q
+ * @requires Nutupane
+ * @requires Repository
+ * @requires Environment
+ * @requires ContentView
+ * @requires CurrentOrganization
+ *
+ * @description
+ *   Provides the functionality for the OSTree branch repositories page.
+ */
+angular.module('Bastion.ostree-branches').controller('OstreeBranchesDetailsRepositoriesController',
+    ['$scope', '$q', 'Nutupane', 'Repository', 'Environment', 'ContentView', 'CurrentOrganization',
+    function ($scope, $q, Nutupane, Repository, Environment, ContentView, CurrentOrganization) {
+        var repositoriesNutupane, environment, contentView, params;
+        params = {
+            'ostree_branch_id': $scope.$stateParams.branchId,
+            'organization_id': CurrentOrganization,
+            'paged': false
+        };
+
+        repositoriesNutupane = new Nutupane(Repository, params);
+        $scope.detailsTable = repositoriesNutupane.table;
+        $scope.detailsTable.initialLoad = false;
+        repositoriesNutupane.masterOnly = true;
+        repositoriesNutupane.setSearchKey('repositoriesSearch');
+
+        environment = Environment.queryUnpaged(function (response) {
+            $scope.environments = response.results;
+            $scope.environmentFilter = _.findWhere($scope.environments, {library: true}).id;
+        });
+
+        contentView = ContentView.queryUnpaged(function (response) {
+            $scope.contentViews = response.results;
+            $scope.contentViewFilter = _.findWhere($scope.contentViews, {'default': true});
+        });
+
+        $scope.detailsTable.working = true;
+        $q.all([contentView.$promise, environment.$promise]).then(function () {
+            $scope.filterBranches();
+            $scope.detailsTable.working = false;
+        });
+
+        $scope.filterBranches = function () {
+            params['environment_id'] = $scope.environmentFilter;
+            params['content_view_version_id'] = $scope.contentViewFilter;
+
+            if ($scope.contentViewFilter) {
+                params['content_view_version_id'] = _.pluck($scope.contentViewFilter.versions, 'id');
+            }
+
+            repositoriesNutupane.setParams(params);
+            repositoriesNutupane.refresh();
+        };
+    }]
+);
