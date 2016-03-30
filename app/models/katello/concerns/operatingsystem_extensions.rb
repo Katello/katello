713a9e2d4278444980aa666c7b92c9c5a952b85d
Partha Aji <paji@redhat.com>
2016-03-30 17:47:08 -0400

    Fixes #14388 - Creates RH Atomic OS in seed
    When subscription manager sends facts about a content host then the
    Operating system of the host is inferred from its distribution name
    and version. That inferred OS is attached to the host
    However the host validation requires install media/partition tables to
    be of the same OS
    The facts parser however does not understand what an Atomic OS is.
    Thus it ties Atomic host to Redhat 7.2 even for atomic.
    This causes host.save! to fail because the right media is NOT attached
    to OS.
    
    This commit creates a dedicated  "Redhat Atomic 7.2"  OS in the seed
    script  and tell the user if you want to use Atomic use this exact OS.
    
    The facts parser then checks if "distro name has atomic" tie it to this OS
    
    (cherry picked from commit 5926f92b1a19397dd3f57f0d932221279c4d4180)

--- app/models/katello/concerns/operatingsystem_extensions.rb
+++ app/models/katello/concerns/operatingsystem_extensions.rb
@@ -3,15 +3,25 @@ module Katello
     module OperatingsystemExtensions
       extend ActiveSupport::Concern
 
+      REDHAT_ATOMIC_HOST_DISTRO_NAME = "Red Hat Enterprise Linux Atomic Host"
+      REDHAT_ATOMIC_HOST_OS = "RedHat_Enterprise_Linux_Atomic_Host"
+
       included do
         after_create :assign_templates!
+        before_create :set_atomic_attributes, :if => proc { |os| os.name == ::Operatingsystem::REDHAT_ATOMIC_HOST_OS }
       end
 
       def assign_templates!
         # Automatically assign default templates
         if self.family == 'Redhat'
           TemplateKind.all.each do |kind|
-            if (template = ProvisioningTemplate.find_by_name(Setting["katello_default_#{kind.name}"]))
+            if name == ::Operatingsystem::REDHAT_ATOMIC_HOST_OS && kind.name == "provision"
+              provisioning_template_name = Setting["katello_default_atomic_provision"]
+            else
+              provisioning_template_name = Setting["katello_default_#{kind.name}"]
+            end
+
+            if (template = ProvisioningTemplate.find_by_name(provisioning_template_name))
               provisioning_templates << template unless provisioning_templates.include?(template)
               if OsDefaultTemplate.where(:template_kind_id => kind.id, :operatingsystem_id => id).empty?
                 OsDefaultTemplate.create(:template_kind_id => kind.id, :provisioning_template_id => template.id, :operatingsystem_id => id)
@@ -23,6 +33,12 @@ module Katello
             ptables << ptable unless ptables.include?(ptable)
           end
         end
+      end
+
+      def set_atomic_attributes
+        self.description = "#{::Operatingsystem::REDHAT_ATOMIC_HOST_DISTRO_NAME} #{release}"
+        self.architectures << Architecture.where(:name => "x86_64").first_or_create
+        self.family = "Redhat"
       end
     end
   end
