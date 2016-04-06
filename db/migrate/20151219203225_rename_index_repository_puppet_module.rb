56dd28b969d64812fdbf1bb9b201726926b23f54
xprazak2 <xprazak2@users.noreply.github.com>
2016-04-06 17:25:11 +0200

    Fixes #14433 - Migrations no longer fail on mysql (cherry picked from commit 8b9306ecd9dcef41a0dc196cea8101ffab4cb102)

--- db/migrate/20151219203225_rename_index_repository_puppet_module.rb
+++ db/migrate/20151219203225_rename_index_repository_puppet_module.rb
@@ -3,7 +3,7 @@ class RenameIndexRepositoryPuppetModule < ActiveRecord::Migration
     original_name = 'index_katello_repository_puppet_module_on_module_id_and_repo_id'
     shorter_name = 'index_katello_repo_puppet_module_on_module_id_and_repo_id'
 
-    if index_exists?(:katello_repository_puppet_module, [:module_id, :repo_id], :name => original_name)
+    if index_exists?(:katello_repository_puppet_modules, [:module_id, :repo_id], :name => original_name)
       rename_index :katello_repository_puppet_modules, original_name, shorter_name
     end
   end
