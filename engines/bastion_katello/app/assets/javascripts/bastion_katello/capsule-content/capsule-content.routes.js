5e646d60413ae498e8c15d741b7f89f3425953a0
Walden Raines <walden@redhat.com>
2016-04-18 16:41:44 -0400

    Fixes #13718: call foreman setTab() on non-content pages.
    Fix the dropdown menu not working by calling the foreman setTab()
    function when a route does not match the content routes.
    
    http://projects.theforeman.org/issues/13718

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/capsule-content/capsule-content.routes.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/capsule-content/capsule-content.routes.js
@@ -1,15 +1,15 @@
 /**
- * @ngdoc object
- * @name Bastion.capsule-content.config
- *
- * @requires $stateProvider
- *
- * @description
- *   Used for systems level configuration such as setting up the ui state machine.
- */
+* @ngdoc object
+* @name Bastion.capsule-content.config
+*
+* @requires $stateProvider
+* @requires $urlRouterProvider
+*
+* @description
+*   Used for systems level configuration such as setting up the ui state machine.
+*/
 angular.module('Bastion.capsule-content').config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-
-    // Catch the url to prevent the router to perform redirect.
+    //Catch the url to prevent the router to perform redirect.
     $urlRouterProvider.when('/smart_proxies/:proxyId', ['$match', '$stateParams', function ($match, $stateParams) {
         $stateParams.pageName = 'smart_proxies/detail';
         return true;
@@ -25,5 +25,26 @@ angular.module('Bastion.capsule-content').config(['$stateProvider', '$urlRouterP
             $window.location.href = $location.path();
         }
     });
+}]);
 
+/**
+ * @ngdoc run
+ * @name Bastion.capsule-content.run
+ *
+ * @requires $rootScope
+ * @requires $window
+ * @requires $timeout
+ *
+ * @description
+ *   Ensure foreman's setTab() function is called on capsule content pages.
+ */
+angular.module('Bastion.capsule-content').run(['$rootScope', '$window', '$timeout', function ($rootScope, $window, $timeout) {
+    var smartProxiesRegex = new RegExp("/smart_proxies/.+#.+");
+    $rootScope.$on('$locationChangeStart', function (event, newUrl) {
+        if (newUrl.match(smartProxiesRegex)) {
+            $timeout(function () {
+                $window.setTab();
+            });
+        }
+    });
 }]);
