5de5cf3a64808de2027d3fbfbffa7b655d25a610
Justin Sherrill <jlsherrill@gmail.com>
2016-05-26 16:19:16 -0400

    Fixes #14766 - prevent concurrent content update (#6074)
    during repository destroy

--- app/lib/actions/katello/repository/destroy.rb
+++ app/lib/actions/katello/repository/destroy.rb
@@ -22,15 +22,16 @@ module Actions
 
           plan_action(ContentViewPuppetModule::Destroy, repository) if repository.puppet?
           plan_action(Pulp::Repository::Destroy, pulp_id: repository.pulp_id)
-          plan_action(Product::ContentDestroy, repository)
-          plan_action(Repository::DestroyMedium, repository)
+          sequence do
+            plan_action(Product::ContentDestroy, repository)
+            plan_action(Repository::DestroyMedium, repository)
 
-          view_env = repository.content_view.content_view_environment(repository.environment)
+            view_env = repository.content_view.content_view_environment(repository.environment)
+            repository.destroy! if planned_destroy
 
-          repository.destroy! if planned_destroy
-
-          if !skip_environment_update && ::SETTINGS[:katello][:use_cp] && view_env
-            plan_action(ContentView::UpdateEnvironment, repository.content_view, repository.environment)
+            if !skip_environment_update && ::SETTINGS[:katello][:use_cp] && view_env
+              plan_action(ContentView::UpdateEnvironment, repository.content_view, repository.environment)
+            end
           end
 
           plan_self(:user_id => ::User.current.id, :planned_destroy => planned_destroy)
