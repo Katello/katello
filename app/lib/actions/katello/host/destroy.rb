55c5b4eb18dde8e08da4ea3f5305a023c068ac11
Partha Aji <paji@redhat.com>
2016-06-07 19:45:04 -0400

    Fixes #15340 - Org destroy unassociates hostgroups
    Org destroy was not properly unassociating environments and content
    views from hosts + hostgroups.
    
    Updated the plan to handle that
    
    (cherry picked from commit c6a4c5bf14b009651ba9b90f8cd0c604f27cc4f6)

--- app/lib/actions/katello/host/destroy.rb
+++ app/lib/actions/katello/host/destroy.rb
@@ -5,13 +5,13 @@ module Actions
         middleware.use ::Actions::Middleware::RemoteAction
 
         def plan(host, options = {})
-          skip_candlepin = options.fetch(:skip_candlepin, false)
+          organization_destroy = options.fetch(:organization_destroy, false)
           unregistering = options.fetch(:unregistering, false)
 
           action_subject(host)
 
           concurrence do
-            if !skip_candlepin && host.subscription_facet.try(:uuid)
+            if !organization_destroy && host.subscription_facet.try(:uuid)
               plan_action(Candlepin::Consumer::Destroy, uuid: host.subscription_facet.uuid)
             end
             plan_action(Pulp::Consumer::Destroy, uuid: host.content_facet.uuid) if host.content_facet.try(:uuid)
@@ -20,20 +20,30 @@ module Actions
           host.subscription_facet.try(:destroy!)
 
           if unregistering
-            if host.content_facet
-              host.content_facet.uuid = nil
-              host.content_facet.save!
-            end
-
-            host.get_status(::Katello::ErrataStatus).destroy
-            host.get_status(::Katello::SubscriptionStatus).destroy
-            host.installed_packages.destroy_all
+            unregister(host)
+          elsif organization_destroy
+            host.content_facet.try(:destroy!)
+            destroy_host_artifacts(host)
           else
             host.content_facet.try(:destroy!)
             unless host.destroy
               fail host.errors.full_messages.join('; ')
             end
           end
+        end
+
+        def unregister(host)
+          if host.content_facet
+            host.content_facet.uuid = nil
+            host.content_facet.save!
+          end
+          destroy_host_artifacts(host)
+        end
+
+        def destroy_host_artifacts(host)
+          host.get_status(::Katello::ErrataStatus).destroy
+          host.get_status(::Katello::SubscriptionStatus).destroy
+          host.installed_packages.destroy_all
         end
 
         def humanized_name
