11f606251d0b90710c81e477482edadfaf93c5a8
John Mitsch <jomitsch@redhat.com>
2016-06-15 09:57:07 -0700

    Fixes #15394 - quantity updates after subs are removed from host (#6119)
    When removing subscriptions from a host, the subscription quanities
    now update so that the quantity for that subscription/pool will be
    correct.
    (cherry picked from commit e28756464517ca7ad971d6669010cd69dfdaf575)

--- app/lib/actions/candlepin/consumer/remove_subscription.rb
+++ app/lib/actions/candlepin/consumer/remove_subscription.rb
@@ -7,10 +7,16 @@ module Actions
         input_format do
           param :uuid, String
           param :entitlement_id, String
+          param :pool_id, String
         end
 
         def run
           ::Katello::Resources::Candlepin::Consumer.remove_entitlement(input[:uuid], input[:entitlement_id])
+        end
+
+        def finalize
+          pool = ::Katello::Pool.where(:cp_id => input[:pool_id]).first
+          pool.import_data if pool
         end
       end
     end
