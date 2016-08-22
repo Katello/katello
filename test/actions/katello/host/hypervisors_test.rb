506988470373ee33c4c0a86bbe6e270afb4df6bd
Justin Sherrill <jsherril@redhat.com>
2016-08-22 10:18:47 -0400

    Fixes #16228 - improve virt-who checkin actions * reduce amount of data kept * skip failed steps * simplify logic to consolidate hypervisor list

--- test/actions/katello/host/hypervisors_test.rb
+++ test/actions/katello/host/hypervisors_test.rb
@@ -11,20 +11,36 @@ module Katello::Host
       @content_view = katello_content_views(:library_dev_view)
       @content_view_environment = katello_content_view_environments(:library_dev_view_library)
       @hypervisor_params = { 'hypervisor' => ['guest-1', 'guest-2'] }
+      @hypervisor_response = {
+        'created' => [{'uuid' => 1, 'name' => 'foo', 'extra' => 'stranger', 'owner' => {'key' => 'org-label'}}]
+      }
       Dynflow::Testing::DummyPlannedAction.any_instance.stubs(:error).returns(nil)
     end
 
     describe 'Hypervisors' do
       it 'plans' do
         action = create_action(::Actions::Katello::Host::Hypervisors)
-        action.execution_plan.stub_planned_action(::Actions::Candlepin::Consumer::Hypervisors) do |candlepin_action|
-          candlepin_action.stubs(output: { :results => 'candlepin results' })
-        end
+        ::Katello::Resources::Candlepin::Consumer.expects(:register_hypervisors).with(@hypervisor_params).returns(@hypervisor_response)
+
         plan_action(action, @hypervisor_params)
-        assert_action_planed_with(action, ::Actions::Candlepin::Consumer::Hypervisors, @hypervisor_params)
         assert_action_planed_with(action, ::Actions::Katello::Host::HypervisorsUpdate) do |results, *_|
-          results.must_equal 'candlepin results'
+          results.must_equal [{:uuid => 1, :name => 'foo', :organization_label => 'org-label'}]
         end
+      end
+
+      it 'properly parses hypervisor response' do
+        action = create_action(::Actions::Katello::Host::Hypervisors)
+        owner = {'key' => 'org-label'}
+        json = {
+          'created' => [{'uuid' => 1, 'name' => 'foo1', 'extra' => 'stranger', 'owner' => owner}],
+          'updated' => [{'uuid' => 2, 'name' => 'foo2', 'owner' => owner}],
+          'unchanged' => [{'uuid' => 3, 'name' => 'foo3', 'owner' => owner}]
+        }
+        expected = [{:uuid => 1, :name => 'foo1', :organization_label => 'org-label'},
+                    {:uuid => 2, :name => 'foo2', :organization_label => 'org-label'},
+                    {:uuid => 3, :name => 'foo3', :organization_label => 'org-label'}]
+        assert_equal expected, action.parse_hypervisors(json)
+        assert_equal [], action.parse_hypervisors({})
       end
     end
   end
