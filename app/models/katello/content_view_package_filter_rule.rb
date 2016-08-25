ba47edbcbe2a780230e80c1ec16ad7461919e002
John Mitsch <jomitsch@redhat.com>
2016-08-25 11:36:36 -0400

    Fixes #16186 - Validate uniqueness on package filter rules
    To test - create a content view package filter and try to add
    the same exact rule twice. You should get an error message explaining
    that this rule has already been added on the second rule.
    
    It is also worth testing that this won't block any normal usage of
    filter rule creation.

--- app/models/katello/content_view_package_filter_rule.rb
+++ app/models/katello/content_view_package_filter_rule.rb
@@ -9,6 +9,19 @@ module Katello
 
     validates_lengths_from_database
     validates :name, :presence => true
+    validate :ensure_unique_attributes
     validates_with Validators::ContentViewFilterVersionValidator
+
+    def ensure_unique_attributes
+      other = self.class.where(:name => self.name,
+                               :version => self.version,
+                               :content_view_filter_id => self.content_view_filter_id,
+                               :min_version => self.min_version,
+                               :max_version => self.max_version)
+      other = other.where.not(:id => self.id) if self.id
+      if other.exists?
+        errors.add(:base, "This package filter rule already exists.")
+      end
+    end
   end
 end
