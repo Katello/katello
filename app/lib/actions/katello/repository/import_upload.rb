68c5d7d13d2337ed03658bd96742af3e1241a166
Eric D Helms <eric.d.helms@gmail.com>
2016-09-07 13:00:24 -0400

    Fixes #16429: Support large file uploads (#6297)

--- app/lib/actions/katello/repository/import_upload.rb
+++ app/lib/actions/katello/repository/import_upload.rb
@@ -2,12 +2,12 @@ module Actions
   module Katello
     module Repository
       class ImportUpload < Actions::EntryAction
-        def plan(repository, upload_id)
+        def plan(repository, upload_id, unit_key = {})
           action_subject(repository)
           import_upload = plan_action(Pulp::Repository::ImportUpload,
                                       pulp_id: repository.pulp_id,
                                       unit_type_id: repository.unit_type_id,
-                                      unit_key: {},
+                                      unit_key: unit_key,
                                       upload_id: upload_id)
 
           plan_action(FinishUpload, repository, import_upload.output)
