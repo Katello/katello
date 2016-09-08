b461c9adacee2adba5cd4b7b1d84f802d5e7be68
Justin Sherrill <jlsherrill@gmail.com>
2016-09-08 12:09:10 -0400

    Fixes #16458 - Add link to hosts for pkg (#6299)
    adds a link and count to hosts with this package
    installed

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/packages/details/package-details.controller.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/packages/details/package-details.controller.js
@@ -9,8 +9,9 @@
  * @description
  *   Provides the functionality for the errata details action pane.
  */
-angular.module('Bastion.packages').controller('PackageDetailsController', ['$scope', 'Package', 'ApiErrorHandler',
-    function ($scope, Package, ApiErrorHandler) {
+angular.module('Bastion.packages').controller('PackageDetailsController',
+    ['$scope', 'Package', 'Host', 'CurrentOrganization', 'ApiErrorHandler',
+    function ($scope, Package, Host, CurrentOrganization, ApiErrorHandler) {
         $scope.panel = {
             error: false,
             loading: true
@@ -20,8 +21,22 @@ angular.module('Bastion.packages').controller('PackageDetailsController', ['$sco
             $scope.panel.loading = false;
         }
 
+        $scope.installedPackageCount = undefined;
+
+        $scope.fetchHostCount = function() {
+            Host.get({'per_page': 0, 'search': $scope.createSearchString(), 'organization_id': CurrentOrganization}, function (data) {
+                $scope.installedPackageCount = data.subtotal;
+            });
+        };
+
+        $scope.createSearchString = function() {
+            return 'installed_package=' + $scope.package.name + '-' + $scope.package.version + '-' + $scope.package.release + '.' +
+                            $scope.package.arch;
+        };
+
         $scope.package = Package.get({id: $scope.$stateParams.packageId}, function () {
             $scope.panel.loading = false;
+            $scope.fetchHostCount();
         }, function(response) {
             $scope.panel.loading = false;
             ApiErrorHandler.handleGETRequestErrors(response, $scope);
--- engines/bastion_katello/test/packages/details/package-details.controller.test.js
+++ engines/bastion_katello/test/packages/details/package-details.controller.test.js
@@ -1,23 +1,50 @@
 describe('Controller: PackageDetailsController', function() {
-    var $scope, Package;
+    var $scope, Package, Host, fakePackage, currentOrganization;
 
     beforeEach(module('Bastion.packages', 'Bastion.test-mocks'));
 
     beforeEach(inject(function($injector) {
         var $controller = $injector.get('$controller');
 
+        currentOrganization = 'myOrg';
+        fakePackage = {name: 'foo', version: '1.0', release: '2', arch: 'greatestArch'};
         Package = $injector.get('MockResource').$new();
+        Host = $injector.get('MockResource').$new();
         $scope = $injector.get('$rootScope').$new();
 
         $scope.$stateParams = {packageId: 1};
+        $scope.package = fakePackage;
+        Host.get = function(params, success, failure) {
+          success({subtotal: 5})
+        };
+
 
         $controller('PackageDetailsController', {
             $scope: $scope,
-            Package: Package
+            Package: Package,
+            Host: Host,
+            CurrentOrganization: currentOrganization
         });
     }));
 
     it("gets the content host using the host collection service and puts it on the $scope.", function() {
         expect($scope.package).toBeDefined();
     });
+
+    it("can generate an installed package search string", function() {
+        $scope.package = fakePackage;
+        expect($scope.createSearchString()).toBe("installed_package=foo-1.0-2.greatestArch")
+    })
+
+    it("fetches hosts counts", function() {
+        $scope.package = fakePackage;
+
+        spyOn(Host, 'get').and.callThrough();
+
+        $scope.fetchHostCount();
+        expect(Host.get).toHaveBeenCalledWith({per_page: 0, search: $scope.createSearchString(), 'organization_id': currentOrganization},
+            jasmine.any(Function));
+        expect($scope.installedPackageCount).toBe(5)
+    })
+
 });
