2b5cd85d045cfacf5a2785a48b527e0042adc264
Partha Aji <paji@redhat.com>
2016-10-05 16:50:57 -0400

    Fixes #18361 - Better progrs for ostree repo sync
    Improved the ostree repo presenter to show the progress
    of an ostree repo more accurately. YOu should now be
    able see status information like
    "Pulling remote branches. Downloaded 333/3012 units."

--- app/lib/actions/pulp/repository/presenters/ostree_presenter.rb
+++ app/lib/actions/pulp/repository/presenters/ostree_presenter.rb
@@ -7,13 +7,26 @@ module Actions
             return 0.01 unless task_progress_details
 
             completion = 0.0
-            completion += 0.4 if content_completed?(details("import_create_repository"))
-            completion += 0.3 if content_completed?(details("import_pull"))
+            completion += 0.2 if content_completed?(details("import_create_repository"))
+
+            if content_completed?(details("import_pull"))
+              completion += 0.5
+            end
             completion += 0.3 if content_completed?(details("import_add_unit"))
             completion
           end
 
           private
+
+          def fetch_pull_ratio(content_details)
+            # looks like "details": "fetching 24980/46836 53%",
+            fetch_details_line = content_details["details"]
+            ret = "0"
+            if fetch_details_line[-1] == "%"
+              ret = fetch_details_line.split[-2] # "24980/46836"
+            end
+            ret
+          end
 
           def humanized_details
             ret = []
@@ -24,7 +37,7 @@ module Actions
                      when "import_create_repository"
                        _("Creating local repository")
                      when "import_pull"
-                       _("Pulling remote branches")
+                       _("Pulling remote branches. Downloaded %s units." % fetch_pull_ratio(details("import_pull")))
                      when "import_add_unit"
                        _("Adding content units")
                      end
