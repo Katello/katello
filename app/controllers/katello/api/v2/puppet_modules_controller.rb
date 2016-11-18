13f1896ccec51492593394cf137b5070ad28974b
Brad Buckingham <bbuckingham@redhat.com>
2016-11-18 17:23:20 -0500

    fixes #17402 - lifecycle environment - fix puppet modules filter
    This commit will fix the filtering to obtain the list of
    puppet modules in a non-library (e.g. dev, test,...)
    lifecycle environment.  (Ref: Content -> Lifecycle Environments)

--- app/controllers/katello/api/v2/puppet_modules_controller.rb
+++ app/controllers/katello/api/v2/puppet_modules_controller.rb
@@ -2,5 +2,13 @@ module Katello
   class Api::V2::PuppetModulesController < Api::V2::ApiController
     apipie_concern_subst(:a_resource => N_("a puppet module"), :resource => "puppet_modules")
     include Katello::Concerns::Api::V2::RepositoryContentController
+
+    def custom_index_relation(collection)
+      if @environment && !@environment.library?
+        collection = collection.joins(:content_view_puppet_environments).
+            where("#{Katello::ContentViewPuppetEnvironment.table_name}.environment_id" => @environment.id)
+      end
+      collection
+    end
   end
 end
