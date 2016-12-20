Katello/katello

commit e5586b7e6bfd07455997fdb1d7651650f9a83154

fixes #6757 - composite view to allow duplicate repos
This commit removes the restriction that component content views
added to a composite content view are not allowed to contain
the same source repository.  This is to handle the case where
the user is managing multiple instances of the repos with different
views/purposes and want to combine them through composites.

With this, when publishing a composite content view, if there are
N instances of the same repository, the version published for
the composite will only have 1 repository that combines them
all.

This change in behavior also affects incremental update as it
needs to ensure that that single repository is still a combination
of all versions on the composite (e.g. from new incremental versions
as well as the prior published versions).

app/lib/actions/katello/content_view/publish.rb
@@ -18,9 +18,9 @@ module Actions
           sequence do
             plan_action(ContentView::AddToEnvironment, version, library)
             concurrence do
-              content_view.repositories_to_publish.each do |repository|
+              content_view.repositories_to_publish_by_library_instance.each do |_library_instance, repositories|
                 sequence do
-                  clone_to_version = plan_action(Repository::CloneToVersion, repository, version)
+                  clone_to_version = plan_action(Repository::CloneToVersion, repositories, version)
                   plan_action(Repository::CloneToEnvironment, clone_to_version.new_repository, library)
                 end
               end

app/models/katello/content_view.rb
@@ -50,7 +50,6 @@ module Katello
     validates :name, :presence => true, :uniqueness => {:scope => :organization_id}
     validates :organization_id, :presence => true
     validate :check_non_composite_components
-    validate :check_repo_conflicts
     validate :check_puppet_conflicts
     validates :composite, :inclusion => [true, false]
 
@@ -227,6 +226,17 @@ module Katello
       composite? ? repositories_to_publish.pluck(&:id) : repository_ids
     end
 
+    def repositories_to_publish_by_library_instance
+      # retrieve the list of repositories in a hash, where the key
+      # is the library instance id, and the value is an array
+      # of the repositories for that instance.
+      repositories_to_publish.inject({}) do |result, repo|
+        result[repo.library_instance_id] ||= []
+        result[repo.library_instance_id] << repo
+        result
+      end
+    end
+
     # Returns actual puppet modules associated with all components
     def component_modules_to_publish
       composite? ? components.flat_map { |version| version.puppet_modules } : nil
@@ -323,14 +333,6 @@ module Katello
     def check_non_composite_components
       if !composite? && components.present?
         errors.add(:base, _("Cannot add component versions to a non-composite content view"))
-      end
-    end
-
-    def check_repo_conflicts
-      duplicate_repositories.each do |repo|
-        versions = components.with_library_repo(repo).uniq.map(&:name).join(", ")
-        msg = _("Repository conflict: '%{repo}' is in %{versions}.") % {repo: repo.name, versions: versions}
-        errors.add(:base, msg)
       end
     end
 

app/lib/actions/katello/content_view_version/incremental_update.rb
@@ -35,8 +35,8 @@ module Actions
 
           sequence do
             concurrence do
-              repos_to_copy(old_version, new_components).each do |source_repo|
-                copy_action_outputs += copy_repo(source_repo, new_content_view_version, content, dep_solve)
+              repos_to_copy(old_version, new_components).each do |source_repos|
+                copy_action_outputs += copy_repos(source_repos, new_content_view_version, content, dep_solve)
               end
 
               sequence do
@@ -54,16 +54,16 @@ module Actions
           end
         end
 
-        def repos_to_copy(old_version, components)
+        def repos_to_copy(old_version, new_components)
           old_version.archived_repos.map do |source_repo|
-            components_repo_instance(source_repo, components) || source_repo
+            components_repo_instances(source_repo, new_components)
           end
         end
 
-        def copy_repo(source_repo, new_version, content, dep_solve)
+        def copy_repos(source_repos, new_version, content, dep_solve)
           copy_output = []
           sequence do
-            new_repo = plan_action(Repository::CloneToVersion, source_repo, new_version, true).new_repository
+            new_repo = plan_action(Repository::CloneToVersion, source_repos, new_version, true).new_repository
             copy_output = copy_yum_content(new_repo, dep_solve, content[:package_ids], content[:errata_ids])
 
             plan_action(Katello::Repository::MetadataGenerate, new_repo, nil)
@@ -72,15 +72,43 @@ module Actions
           copy_output
         end
 
-        # for a given repo, find its instance out of a list of content view versions.  Since these versions
-        #  are all part of a composite, there should only be one
-        def components_repo_instance(repo, component_versions)
-          possible_repos = component_versions.map do |cvv|
-            cvv.repositories.select do |component_repo|
-              component_repo.library_instance_id == repo.library_instance_id
+        # For a given repo, find it's instances in both the new and old component versions.
+        # This is necessary, since a composite content view may have components containing
+        # the same repository within multiple component views and all of the source repos
+        # will be needed to publish the new repo.
+        def components_repo_instances(old_version_repo, new_component_versions)
+          # Attempt to locate the repo instance in the new component versions
+          new_repos = nil
+          new_component_versions.map do |cvv|
+            cvv.repositories.each do |component_repo|
+              if component_repo.library_instance_id == old_version_repo.library_instance_id
+                new_repos ||= []
+                new_repos << component_repo
+                break # each CVV can only have 1 repo with this instance id, so go to next CVV
+              end
             end
           end
-          possible_repos.flatten.first
+
+          # If we found it, we need to also locate the repo instance in the old component
+          # versions, but omit the one changed by the new component version.
+          if new_repos
+            old_repos = nil
+            old_version_repo.content_view_version.components.each do |component|
+              component.archived_repos.each do |component_repo|
+                # if the archived repo is not the same source as one of the new repos, include it
+                new_repos.each do |new_repo|
+                  if (new_repo.library_instance_id == component_repo.library_instance_id) &&
+                      (new_repo.content_view.id != component_repo.content_view.id)
+                    old_repos ||= []
+                    old_repos << component_repo
+                  end
+                end
+              end
+            end
+            new_repos.concat(old_repos)
+          else
+            [old_version_repo]
+          end
         end
 
         def run

app/lib/actions/katello/repository/clone_to_version.rb
@@ -5,23 +5,27 @@ module Actions
         # allows accessing the build object from the superior action
         attr_accessor :new_repository
 
-        def plan(repository, content_view_version, incremental = false)
+        def plan(repositories, content_view_version, incremental = false)
           content_view = content_view_version.content_view
-          filters = incremental ? [] : content_view.filters.applicable(repository)
-          self.new_repository = repository.build_clone(content_view: content_view,
-                                                       version: content_view_version)
+          filters = incremental ? [] : content_view.filters.applicable(repositories.first)
+
+          self.new_repository = repositories.first.build_clone(content_view: content_view,
+                                                               version: content_view_version)
+
           sequence do
             plan_action(Repository::Create, new_repository, true, false)
 
-            if new_repository.yum?
-              plan_action(Repository::CloneYumContent, repository, new_repository, filters, !incremental,
-                          :generate_metadata => !incremental, :index_content => !incremental, :simple_clone => incremental)
-            elsif new_repository.docker?
-              plan_action(Repository::CloneDockerContent, repository, new_repository)
-            elsif new_repository.ostree?
-              plan_action(Repository::CloneOstreeContent, repository, new_repository)
-            elsif new_repository.file?
-              plan_action(Repository::CloneFileContent, repository, new_repository)
+            repositories.each do |repository|
+              if new_repository.yum?
+                plan_action(Repository::CloneYumContent, repository, new_repository, filters, !incremental,
+                            :generate_metadata => !incremental, :index_content => !incremental, :simple_clone => incremental)
+              elsif new_repository.docker?
+                plan_action(Repository::CloneDockerContent, repository, new_repository)
+              elsif new_repository.ostree?
+                plan_action(Repository::CloneOstreeContent, repository, new_repository)
+              elsif new_repository.file?
+                plan_action(Repository::CloneFileContent, repository, new_repository)
+              end
             end
           end
         end

test/models/content_view_test.rb
@@ -227,13 +227,8 @@ module Katello
       v1 = ContentViewVersion.find(katello_content_view_versions(:library_view_version_1).id)
       v2 = ContentViewVersion.find(katello_content_view_versions(:library_view_version_2).id)
 
-      refute composite.update_attributes(component_ids: [v1.id, v2.id])
-      assert_equal 2, composite.errors.count # docker and yum repos
-      assert composite.errors.full_messages.first =~ /^Repository conflict/
-
-      assert_raises(RuntimeError) do
-        composite.components << v1
-      end
+      assert composite.update_attributes(component_ids: [v1.id, v2.id])
+      assert_equal 0, composite.errors.count # docker and yum repos
     end
 
     def test_puppet_module_conflicts

commit 5a48a495cde59745188fa5f8085daa7a9aac383e

Fixes #17610 - Corrects multi repo cv publishes
Commit e5586b7e6bfd07455997fdb1d7651650f9a83154 broke publishing of
non-composite content views with multiple repositories. This is because
the changes were meant more for composite cv's and assumed library
instances of repositories to be published to be non nil.
In short you could not add a yum repo and repo of another type and
publish em together.
This commit fixes that issue by treating repos with no library
instances differently from  ones with library instances (which tend to
be the component cv repos.)

app/lib/actions/katello/content_view/publish.rb
@@ -23,7 +23,7 @@ module Actions
           sequence do
             plan_action(ContentView::AddToEnvironment, version, library)
             concurrence do
-              content_view.repositories_to_publish_by_library_instance.each do |_library_instance, repositories|
+              content_view.publish_repositories do |repositories|
                 sequence do
                   clone_to_version = plan_action(Repository::CloneToVersion, repositories, version)
                   plan_action(Repository::CloneToEnvironment, clone_to_version.new_repository, library)

app/models/katello/content_view.rb
@@ -291,6 +291,17 @@ module Katello
       end
     end
 
+    def publish_repositories
+      repositories = composite? ? repositories_to_publish_by_library_instance.values : repositories_to_publish
+      repositories.each do |repos|
+        if repos.is_a? Array
+          yield repos
+        else
+          yield [repos]
+        end
+      end
+    end
+
     # Returns actual puppet modules associated with all components
     def component_modules_to_publish
       composite? ? components.flat_map { |version| version.puppet_modules } : nil