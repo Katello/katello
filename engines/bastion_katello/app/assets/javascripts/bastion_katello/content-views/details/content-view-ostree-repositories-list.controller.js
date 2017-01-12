51870d8d3e3cdda5d996d7e5018cd32321e018b0
Tom McKay <thomasmckay@redhat.com>
2017-01-12 09:28:12 -0500

    fixes #18048 - fix ostree and docker cv pages

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/content-views/details/content-view-ostree-repositories-list.controller.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/content-views/details/content-view-ostree-repositories-list.controller.js
@@ -1,19 +1,19 @@
-/**
- * @ngdoc object
- * @name  Bastion.content-views.controller:ContentViewOstreeRepositoriesListController
- *
- * @requires $scope
- * @requires Repository
- * @requires Nutupane
- * @requires CurrentOrganization
- * @requires ContentViewRepositoriesUtil
- *
- * @description
- *    Provides UI functionality list/remove ostree repositories from a content view
- */
-angular.module('Bastion.content-views').controller('ContentViewOstreeRepositoriesListController',
-    ['$scope', 'Repository', 'Nutupane', 'CurrentOrganization', 'ContentViewRepositoriesUtil',
-    function ($scope, Repository, Nutupane, CurrentOrganization, ContentViewRepositoriesUtil) {
+(function () {
+
+    /**
+     * @ngdoc object
+     * @name  Bastion.content-views.controller:ContentViewOstreeRepositoriesListController
+     *
+     * @requires $scope
+     * @requires Repository
+     * @requires Nutupane
+     * @requires CurrentOrganization
+     * @requires ContentViewRepositoriesUtil
+     *
+     * @description
+     *    Provides UI functionality list/remove ostree repositories from a content view
+     */
+    function ContentViewOstreeRepositoriesListController($scope, Repository, Nutupane, CurrentOrganization, ContentViewRepositoriesUtil) {
         var nutupane;
 
         ContentViewRepositoriesUtil($scope);
@@ -22,16 +22,25 @@ angular.module('Bastion.content-views').controller('ContentViewOstreeRepositorie
             'organization_id': CurrentOrganization,
             'content_view_id': $scope.$stateParams.contentViewId,
             'content_type': 'ostree'
-
         },
         'queryUnpaged');
 
         nutupane.masterOnly = true;
+        nutupane.load();
 
         $scope.table = nutupane.table;
 
         $scope.removeRepositories = function () {
             $scope.removeSelectedRepositoriesFromContentView(nutupane, $scope.contentView);
         };
-    }]
-);
+    }
+
+    angular
+        .module('Bastion.content-views')
+        .controller('ContentViewOstreeRepositoriesListController', ContentViewOstreeRepositoriesListController);
+
+    ContentViewOstreeRepositoriesListController.$inject = [
+        '$scope', 'Repository', 'Nutupane', 'CurrentOrganization', 'ContentViewRepositoriesUtil'
+    ];
+
+})();
