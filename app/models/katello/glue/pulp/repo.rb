14fe54dc83a5228db3e53211db92a6e561b4becc
Justin Sherrill <jsherril@redhat.com>
2017-01-17 13:53:40 -0500

    Fixes #18110 - fully import docker tags
    (cherry picked from commit 70a2d0dae5e1c14081fa9af5f6413652daf140b7)

--- app/models/katello/glue/pulp/repo.rb
+++ app/models/katello/glue/pulp/repo.rb
@@ -710,7 +710,7 @@ module Katello
         self.import_distribution_data
       elsif self.docker?
         Katello::DockerManifest.import_for_repository(self)
-        Katello::DockerTag.import_for_repository(self)
+        Katello::DockerTag.import_for_repository(self, true)
       elsif self.puppet?
         Katello::PuppetModule.import_for_repository(self)
       elsif self.ostree?
