b4373db9e2ee047c51a4457679cca77d315257c8
Daniel Lobato Garcia <me@daniellobato.me>
2017-01-18 20:23:35 +0000

    Fixes #18141 - Fix Host form endpoints for Katello props
    Properties like Content Source and friends did no longer show up in the
    Host UI after #15405 was merged. The endpoints changed, this commit
    hooks now the Katello properties to show up below "Deploy on".

--- app/overrides/add_activation_keys_input.rb
+++ app/overrides/add_activation_keys_input.rb
@@ -15,7 +15,7 @@ Deface::Override.new(:virtual_path => "hostgroups/_form",
 
 Deface::Override.new(:virtual_path => "hosts/_form",
                      :name => "hosts_update_environments_select",
-                     :insert_before => 'erb[loud]:contains("host_puppet_environment_field")',
+                     :insert_after => 'erb[loud]:contains("Deploy on")',
                      :partial => 'overrides/activation_keys/host_environment_select')
 
 Deface::Override.new(:virtual_path => "common/os_selection/_operatingsystem",
