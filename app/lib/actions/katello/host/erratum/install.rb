34eee5e67e856d92ca71b423b3a9dae2de497ad5
Andrew Kofink <akofink@redhat.com>
2017-01-26 09:07:35 -0500

    Fixes #18258 - allow installing errata in batches

--- app/lib/actions/katello/host/erratum/install.rb
+++ app/lib/actions/katello/host/erratum/install.rb
@@ -9,10 +9,19 @@ module Actions
             Type! host, ::Host::Managed
 
             action_subject(host, :errata => errata_ids)
-            plan_action(Pulp::Consumer::ContentInstall,
-                        consumer_uuid: host.content_facet.uuid,
-                        type:          'erratum',
-                        args:          errata_ids)
+            if Setting['erratum_install_batch_size'] && Setting['erratum_install_batch_size'] > 0
+              errata_ids.each_slice(Setting['erratum_install_batch_size']) do |errata_ids_batch|
+                plan_action(Pulp::Consumer::ContentInstall,
+                            consumer_uuid: host.content_facet.uuid,
+                            type:          'erratum',
+                            args:          errata_ids_batch)
+              end
+            else
+              plan_action(Pulp::Consumer::ContentInstall,
+                          consumer_uuid: host.content_facet.uuid,
+                          type:          'erratum',
+                          args:          errata_ids)
+            end
           end
 
           def humanized_name
