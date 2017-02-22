351439ed15b0389a874362a51bb9583517f7fed7
Tom McKay <thomasmckay@redhat.com>
2017-02-22 14:22:41 -0500

    fixes #18631 - display future dated subscriptions

--- db/seeds.d/108-subcription-bookmarks.rb
+++ db/seeds.d/108-subcription-bookmarks.rb
@@ -6,7 +6,9 @@
 
 Bookmark.without_auditing do
   bookmarks = [
-    {:name => "list hypervisors", :query => 'hypervisor = true', :controller => "hosts"}
+    {:name => "list hypervisors", :query => 'hypervisor = true', :controller => "hosts"},
+    {:name => "future", :query => 'starts > Today', :controller => "katello_subscriptions"},
+    {:name => "expiring soon", :query => 'expires 30 days from now', :controller => "katello_subscriptions"}
   ]
 
   bookmarks.each do |input|
