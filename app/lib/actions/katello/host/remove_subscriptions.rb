3df3dfece19352bc84caa4478936076c2f72ff6a
Tom McKay <thomasmckay@redhat.com>
2017-04-04 07:45:49 -0400

    fixes #19158 - sequence around sub removal

--- app/lib/actions/katello/host/remove_subscriptions.rb
+++ app/lib/actions/katello/host/remove_subscriptions.rb
@@ -6,19 +6,21 @@ module Actions
 
         def plan(host, pools_with_quantities_params)
           action_subject(host)
-          pools_with_quantities = pools_with_quantities_params.map do |pool_with_quantity|
-            ::Katello::PoolWithQuantities.fetch(pool_with_quantity)
-          end
-          cp_consumer = host.subscription_facet.candlepin_consumer
-          entitlements = pools_with_quantities.map do |pool_with_quantities|
-            cp_consumer.filter_entitlements(pool_with_quantities.pool.cp_id, pool_with_quantities.quantities)
-          end
-
           pool_ids = []
-          entitlements.flatten.each do |entitlement|
-            pool_ids << entitlement['pool']['id']
-            plan_action(::Actions::Candlepin::Consumer::RemoveSubscription, :uuid => host.subscription_facet.uuid,
-                        :entitlement_id => entitlement['id'], :pool_id => entitlement['pool']['id'])
+          sequence do
+            pools_with_quantities = pools_with_quantities_params.map do |pool_with_quantity|
+              ::Katello::PoolWithQuantities.fetch(pool_with_quantity)
+            end
+            cp_consumer = host.subscription_facet.candlepin_consumer
+            entitlements = pools_with_quantities.map do |pool_with_quantities|
+              cp_consumer.filter_entitlements(pool_with_quantities.pool.cp_id, pool_with_quantities.quantities)
+            end
+
+            entitlements.flatten.each do |entitlement|
+              pool_ids << entitlement['pool']['id']
+              plan_action(::Actions::Candlepin::Consumer::RemoveSubscription, :uuid => host.subscription_facet.uuid,
+                          :entitlement_id => entitlement['id'], :pool_id => entitlement['pool']['id'])
+            end
           end
           plan_self(:host_id => host.id, :host_name => host.name, :pool_ids => pool_ids)
         end
