ac2936bd3d716a3c67b15b6b93ed8888664781d2
John Mitsch <jomitsch@redhat.com>
2017-04-04 18:43:52 +0000

    Fixes #18987 - Check ueber certs on each proxy sync
    (cherry picked from commit a0541934a469879d381d0cb4d4437bd0020979f9)

--- app/services/cert/certs.rb
+++ app/services/cert/certs.rb
@@ -15,5 +15,12 @@ module Cert
     def self.ssl_client_key
       @ssl_client_key ||= OpenSSL::PKey::RSA.new(File.open(Setting['pulp_client_key'], 'r').read)
     end
+
+    def self.verify_ueber_cert(organization)
+      ueber_cert = OpenSSL::X509::Certificate.new(self.ueber_cert(organization)[:cert])
+      cert_store = OpenSSL::X509::Store.new
+      cert_store.add_file Setting[:ssl_ca_file]
+      organization.regenerate_ueber_cert unless cert_store.verify ueber_cert
+    end
   end
 end
