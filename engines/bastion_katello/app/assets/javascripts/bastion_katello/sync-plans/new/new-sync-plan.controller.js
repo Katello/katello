936bb78652c557c5ee42b2fcf02598fb107a31fc
Walden Raines <walden@redhat.com>
2017-04-07 10:39:52 -0400

    Fixes #19194: show new sync plan on products form in modal.
    Instead of redirecting to another page for the new sync plan when
    creating a new product we should show the new sync plan form in a modal.
    
    http://projects.theforeman.org/issues/19194
    (cherry picked from commit 532672308e8a5e3387a17ccf3ae06441dbcbb4db)

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/sync-plans/new/new-sync-plan.controller.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/sync-plans/new/new-sync-plan.controller.js
@@ -5,51 +5,43 @@
  * @requires $scope
  * @requires translate
  * @requires SyncPlan
+ * @requires SyncPlanHelper
+ * @requires GlobalNotification
  *
  * @description
  *   Controls the creation of an empty SyncPlan object for use by sub-controllers.
  */
 angular.module('Bastion.sync-plans').controller('NewSyncPlanController',
-    ['$scope', 'translate', 'SyncPlan', 'GlobalNotification',
-        function ($scope, translate, SyncPlan, GlobalNotification) {
-            $scope.intervals = [
-                {id: 'hourly', value: translate('hourly')},
-                {id: 'daily', value: translate('daily')},
-                {id: 'weekly', value: translate('weekly')}
-            ];
+    ['$scope', '$rootScope', 'translate', 'SyncPlan', 'SyncPlanHelper', 'GlobalNotification',
+        function ($scope, $rootScope, translate, SyncPlan, SyncPlanHelper, GlobalNotification) {
+            $scope.intervals = SyncPlanHelper.getIntervals();
 
             $scope.syncPlan = new SyncPlan();
             $scope.syncPlan.interval = $scope.intervals[0].id;
 
             function success(syncPlan) {
                 $scope.working = false;
+                $scope.$state.go('sync-plan.info', {syncPlanId: syncPlan.id});
                 GlobalNotification.setSuccessMessage(translate('New sync plan successfully created.'));
-                if ($scope.product) {
-                    $scope.product['sync_plan_id'] = syncPlan.id;
-                    $scope.$state.go('product.info', {productId: $scope.product.id});
-                } else {
-                    $scope.$state.go('sync-plan.info', {syncPlanId: syncPlan.id});
-                }
             }
 
             function error(response) {
-                $scope.working = false;
+                var form = SyncPlanHelper.getForm();
+
                 angular.forEach(response.data.errors, function (errors, field) {
-                    $scope.syncPlanForm[field].$setValidity('server', false);
-                    $scope.syncPlanForm[field].$error.messages = errors;
+                    form[field].$setValidity('server', false);
+                    form[field].$error.messages = errors;
                 });
+
+                $scope.working = false;
             }
 
             $scope.createSyncPlan = function (syncPlan) {
-                var GMT_OFFSET_MILLISECONDS = syncPlan.startDate.getTimezoneOffset() * 60000,
-                    syncDate = new Date(syncPlan.startDate.getTime() + GMT_OFFSET_MILLISECONDS),
-                    syncTime = new Date(syncPlan.startTime || new Date());
-                syncDate.setHours(syncTime.getHours());
-                syncDate.setMinutes(syncTime.getMinutes());
-                syncDate.setSeconds(0);
+                SyncPlanHelper.createSyncPlan(syncPlan, success, error);
+            };
 
-                syncPlan['sync_date'] = syncDate.toString();
-                syncPlan.$save(success, error);
+            $scope.setForm = function (form) {
+                SyncPlanHelper.setForm(form);
             };
         }]
 );
