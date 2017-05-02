51094c0efff9e6d71bbb22c7ccfd6c3979792ba0
Justin Sherrill <jsherril@redhat.com>
2017-05-02 15:12:40 -0400

    Fixes #19434 - remove duplicate test code
    and properly require needed base file

--- test/actions/pulp/repository/sync_test.rb
+++ test/actions/pulp/repository/sync_test.rb
@@ -1,26 +1,7 @@
 require 'katello_test_helper'
+require_relative 'test_base.rb'
 
 module ::Actions::Pulp::Repository
-  class TestBase < ActiveSupport::TestCase
-    include Dynflow::Testing
-    include Support::Actions::PulpTask
-    include Support::Actions::RemoteAction
-  end
-
-  class VCRTestBase < TestBase
-    include VCR::TestCase
-    let(:repo) { katello_repositories(:fedora_17_x86_64) }
-
-    def setup
-      set_user
-      ::Katello::RepositorySupport.create_repo(repo.id)
-    end
-
-    def teardown
-      ::Katello::RepositorySupport.destroy_repo
-    end
-  end
-
   class SyncTest < VCRTestBase
     def test_sync
       ForemanTasks.sync_task(::Actions::Pulp::Repository::Sync, :pulp_id => repo.pulp_id).main_action
