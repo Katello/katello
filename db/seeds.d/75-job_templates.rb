ebe3daf726a3c0f6208549743179384d82ef381b
Marek Hulan <mhulan@redhat.com>
2017-06-02 09:03:37 +0200

    Fixes #19739 - update job templates seeding
    (cherry picked from commit e406121794cda65ac350433be984abb680731806)

--- db/seeds.d/75-job_templates.rb
+++ db/seeds.d/75-job_templates.rb
@@ -3,7 +3,12 @@ if Katello.with_remote_execution?
     JobTemplate.without_auditing do
       Dir[File.join("#{Katello::Engine.root}/app/views/foreman/job_templates/**/*.erb")].each do |template|
         sync = !Rails.env.test? && Setting[:remote_execution_sync_templates]
-        JobTemplate.import!(File.read(template), :default => true, :locked => true, :update => sync)
+        # import! was renamed to import_raw! around 1.3.1
+        if JobTemplate.respond_to?('import_raw!')
+          JobTemplate.import_raw!(File.read(template), :default => true, :locked => true, :update => sync)
+        else
+          JobTemplate.import!(File.read(template), :default => true, :locked => true, :update => sync)
+        end
       end
     end
   end
