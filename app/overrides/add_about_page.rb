e470a2277fa952a8454da5ca7a8dfd9aede92927
Bryan Kearney <bkearney@redhat.com>
2017-07-11 14:09:04 -0400

    Fixes #20278 - Remove Katello specific support information.
    This is remnant of katello being not well integrated. I
    would suggest that this be removed since katello behaves
    like the other plugins now.
    
    (cherry picked from commit a45d8c439620ae5a0e971ba17deb8d880bffe09f)

--- app/overrides/add_about_page.rb
+++ app/overrides/add_about_page.rb
@@ -11,10 +11,3 @@ Deface::Override.new(:virtual_path => "about/index",
                      :insert_bottom => '#about',
                      :partial => 'overrides/about/installed_packages'
                     )
-
-# Add Katello Support information to About page
-Deface::Override.new(:virtual_path => "about/index",
-                     :name => "add_support_documentation",
-                     :insert_bottom => '.col-md-5 .stats-well:first-child',
-                     :partial => 'overrides/about/support_documentation'
-                    )
