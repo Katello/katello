70c8be756dda2b3c92fd32b6cee81136af93eb5a
Klaas Demter <Klaas-@users.noreply.github.com>
2017-08-24 12:55:01 +0200

    Fixes #20727 - create hypervisors with enabled => false

--- app/lib/actions/katello/host/hypervisors_update.rb
+++ app/lib/actions/katello/host/hypervisors_update.rb
@@ -49,7 +49,7 @@ module Actions
           location ||= Location.unscoped.find_by_title(
             Setting[:default_location_subscribed_hosts])
           host = ::Host::Managed.new(:name => name, :organization => organization,
-                                     :location => location, :managed => false)
+                                     :location => location, :managed => false, :enabled => false)
           host.save!
           host
         end
