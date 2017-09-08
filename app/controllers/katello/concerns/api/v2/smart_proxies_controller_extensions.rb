50cb01d71fcf94d6f02c37c31d79af73317ad799
Brad Buckingham <bbuckingham@redhat.com>
2017-09-08 20:12:28 +0000

    fixes #20863 - correctly extend smart proxies API

--- app/controllers/katello/concerns/api/v2/smart_proxies_controller_extensions.rb
+++ app/controllers/katello/concerns/api/v2/smart_proxies_controller_extensions.rb
@@ -1,30 +1,26 @@
 module Katello
   module Concerns
     module Api::V2::SmartProxiesControllerExtensions
+      module ApiPieExtensions
+        extend ::Apipie::DSL::Concern
+
+        update_api(:create, :update) do
+          param :smart_proxy, Hash do
+            param :download_policy, String, :required => false, :desc => N_('Download Policy of the capsule, must be one of %s') %
+                SmartProxy::DOWNLOAD_POLICIES.join(', ')
+          end
+        end
+      end
+
       extend ActiveSupport::Concern
 
       included do
-        def_param_group :smart_proxy do
-          param :smart_proxy, Hash, :required => true, :action_aware => true do
-            param :name, String, :required => true
-            param :url, String, :required => true
-            param :download_policy, String, :required => false, :desc => N_('Download Policy of the capsule, must be one of %s') %
-                                      SmartProxy::DOWNLOAD_POLICIES.join(', ')
-            param_group :taxonomies, ::Api::V2::BaseController
-          end
-        end
-
-        api :POST, "/smart_proxies/", N_("Create a smart proxy")
-        param_group :smart_proxy, :as => :create
+        include ApiPieExtensions
 
         def create
           @smart_proxy = SmartProxy.new(smart_proxy_params)
           process_response @smart_proxy.save
         end
-
-        api :PUT, "/smart_proxies/:id/", N_("Update a smart proxy")
-        param :id, String, :required => true
-        param_group :smart_proxy
 
         def update
           process_response @smart_proxy.update_attributes(smart_proxy_params)
