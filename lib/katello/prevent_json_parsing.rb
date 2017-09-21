ce3122499fef374ec4add0e0af8ae56f2194dfee
Justin Sherrill <jsherril@redhat.com>
2017-09-21 13:09:36 -0400

    Fixes #21019 - change content_type for specific actions
    this commit removes the Params parser wrapper, replacing
    it with middleware that modifies the content type for
    certain requests to prevent json parsing of the content.
    
    Rails 5 no longer uses the params parser middleware and
    instead recommends you inherit from a different base
    controller.  However this would require massive refactoring
    of Foreman controllers, and seems less than ideal

---
+++ lib/katello/prevent_json_parsing.rb
@@ -0,0 +1,15 @@
+module Katello
+  class PreventJsonParsing
+    def initialize(app, exclude)
+      @app = app
+      @exclude = exclude
+    end
+
+    def call(env)
+      if @exclude && @exclude.call(env)
+        env['CONTENT_TYPE'] = 'text/plain'
+      end
+      @app.call(env)
+    end
+  end
+end
