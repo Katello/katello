48d9140028dfde2d7efd1c5d17e5f11af231d9ed
Jonathon Turel <jturel@redhat.com>
2017-10-17 10:58:56 -0400

    Fixes #20785 - Validate hostgroup kickstart repos
    (cherry picked from commit 81410e9e377d463ec717d113608692c572f93b3c)

---
+++ test/lib/validators/hostgroup_kickstart_repository_validator_test.rb
@@ -0,0 +1,94 @@
+# encoding: utf-8
+
+require 'katello_test_helper'
+
+module Katello
+  class HostgroupKickstartRepositoryValidatorTest < ActiveSupport::TestCase
+    def setup
+      @validator = Validators::HostgroupKickstartRepositoryValidator.new({})
+      @os = ::Redhat.create_operating_system('RedHat', '9', '0')
+      content_source = FactoryGirl.create(:smart_proxy)
+      library_environment = katello_environments(:library)
+      content_source.lifecycle_environments = [library_environment]
+
+      @repos = [{:name => "foo", :id => 4}]
+      @error_messages = {
+        :mismatched_cs_lce => 'The selected content source and lifecycle environment do not match',
+        :missing_os => 'Please select an operating system before assigning a kickstart repository',
+        :missing_arch => 'Please select an architecture before assigning a kickstart repository',
+        :invalid_os => 'Kickstart repositories can only be assigned to hosts in the Red Hat family',
+        :missing_content_source => 'Please select a content source before assigning a kickstart repository',
+        :mismatched_ks_repo => 'The selected kickstart repository is not part of the assigned content view, lifecycle environment,
+                  content source, operating system, and architecture'
+      }
+      @hostgroup = ::Hostgroup.new(:operatingsystem => @os, :kickstart_repository_id => 4, :lifecycle_environment_id => library_environment.id,
+                                   :architecture => ::Architecture.new, :content_source_id => content_source.id)
+    end
+
+    test 'it validates a hostgroup' do
+      @os.expects(:kickstart_repos).returns(@repos)
+
+      @validator.validate(@hostgroup)
+
+      assert_empty @hostgroup.errors[:base]
+    end
+
+    test 'it invalidates on content source and environment mismatch' do
+      @hostgroup.expects(:content_source).twice.returns(FactoryGirl.create(:smart_proxy))
+
+      @validator.validate(@hostgroup)
+
+      assert_equal @error_messages[:mismatched_cs_lce], @hostgroup.errors[:base].first
+    end
+
+    test 'it invalidates on missing OS' do
+      @hostgroup.operatingsystem = nil
+
+      @validator.validate(@hostgroup)
+
+      assert_equal @error_messages[:missing_os], @hostgroup.errors[:base].first
+    end
+
+    test 'it invalidates on missing arch' do
+      @hostgroup.architecture = nil
+
+      @validator.validate(@hostgroup)
+
+      assert_equal @error_messages[:missing_arch], @hostgroup.errors[:base].first
+    end
+
+    test 'it short-circuits on nil kickstart repo id' do
+      @hostgroup.kickstart_repository_id = nil
+      @hostgroup.expects(:operatingsystem).never
+
+      @validator.validate(@hostgroup)
+
+      assert_empty @hostgroup.errors.values
+    end
+
+    test 'it invalidates missing content source on a hostgroup' do
+      @hostgroup.content_source_id = nil
+
+      @validator.validate(@hostgroup)
+
+      assert_equal @error_messages[:missing_content_source], @hostgroup.errors[:base].first
+    end
+
+    test 'it invalidates non-RedHat OS on a hostgroup' do
+      @hostgroup.operatingsystem = ::Operatingsystem.new
+
+      @validator.validate(@hostgroup)
+
+      assert_equal @error_messages[:invalid_os], @hostgroup.errors[:base].first
+    end
+
+    test 'it invalidates mismatched selected ks repo on a hostgroup' do
+      @hostgroup.kickstart_repository_id = 5
+      @os.expects(:kickstart_repos).returns(@repos)
+
+      @validator.validate(@hostgroup)
+
+      assert_equal @error_messages[:mismatched_ks_repo], @hostgroup.errors[:base].first
+    end
+  end
+end
