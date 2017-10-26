135d224ad6eb1ae4ae450e28b3d3d0db9fab5784
Shira Maximov <shiramaximov@gmail.com>
2017-10-26 13:34:51 +0300

    Fixes #21475 - Change the inline help

--- app/helpers/katello/concerns/hosts_and_hostgroups_helper_extensions.rb
+++ app/helpers/katello/concerns/hosts_and_hostgroups_helper_extensions.rb
@@ -11,7 +11,7 @@ module Katello
         html_options.merge!(
           :label => _("Puppet Environment"),
           'data-content_puppet_match' => (@host || @hostgroup).new_record? || (@host || @hostgroup).content_and_puppet_match?,
-          :help_inline => link_to(_("Reset Puppet Environment to match selected Content View"), '#', :id => 'reset_puppet_environment'))
+          :help_inline => link_to(_("Reset Puppet Environment"), '#', :id => 'reset_puppet_environment'))
         puppet_environment_field_without_katello(form, environments_choice, select_options, html_options)
       end
     end
