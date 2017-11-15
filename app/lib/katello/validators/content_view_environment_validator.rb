06dfbdf26cda3267872a0e95913d68165eb6f607
Daniel Lobato Garcia <me@daniellobato.me>
2017-11-15 09:49:45 +0000

    Fixes #21670 - Allow non RH hosts to NOT have content views
    Currently the Content View and Lifecycle Environment are fields of a
    Content Facet. These fields have a NOT-NULL constraint, so they are
    validated on every host. This means that if I'm trying to create a
    CentOS, or Debian host, it will ask for a Content View and Lifecycle -
    which I may not want for hosts that don't use subscription-manager.
    
    In fact for the CentOS case, it's harmful: having a Content View for a
    CentOS host changes the @mediapath variable (the parameter sent to --url
    in the kickstart) to point to Pulp, which means you're pointing to
    Red Hat and installing Red Hat instead of CentOS (unless you've synced
    CentOS).

--- app/lib/katello/validators/content_view_environment_validator.rb
+++ app/lib/katello/validators/content_view_environment_validator.rb
@@ -6,9 +6,9 @@ module Katello
         environment_id = record.respond_to?(:lifecycle_environment_id) ? record.lifecycle_environment_id : record.environment_id
 
         if record.content_view_id && environment_id
-          view = ContentView.find(record.content_view_id)
-          env = KTEnvironment.find(environment_id)
-          unless view.in_environment?(env)
+          view = ContentView.where(:id => record.content_view_id).first
+          env = KTEnvironment.where(:id => environment_id).first
+          unless view.blank? || env.blank? || view.in_environment?(env)
             record.errors[:base] << _("Content view '%{view}' is not in environment '%{env}'") %
                                       {:view => view.name, :env => env.name}
           end
