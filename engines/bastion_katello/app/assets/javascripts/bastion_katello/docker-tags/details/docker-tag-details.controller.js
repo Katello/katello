Katello/katello

commit 1fc82810ba25036abae9d7d2ce0caf5eb3b81956

Fixes #21439 - fix js error on docker tags page

engines/bastion_katello/app/assets/javascripts/bastion_katello/docker-tags/details/docker-tag-details.controller.js
@@ -3,7 +3,6 @@
  * @name  Bastion.docker-tags.controller:DockerTagDetailsController
  *
  * @requires $scope
- * @requires translate
  * @requires $location
  * @requires DockerTag
  * @requires CurrentOrganization
@@ -13,14 +12,12 @@
  *   Provides the functionality for the docker tags details action pane.
  */
 angular.module('Bastion.docker-tags').controller('DockerTagDetailsController',
-    ['$scope', 'translate', '$location', 'Nutupane', 'DockerTag', 'CurrentOrganization', 'ApiErrorHandler',
-    function ($scope, translate, $location, Nutupane, DockerTag, CurrentOrganization, ApiErrorHandler) {
+    ['$scope', '$location', 'DockerTag', 'CurrentOrganization', 'ApiErrorHandler',
+    function ($scope, $location, DockerTag, CurrentOrganization, ApiErrorHandler) {
         $scope.panel = {
             error: false,
             loading: true
         };
-
-        $scope.table = {};
 
         if ($scope.tag) {
             $scope.panel.loading = false;
@@ -32,35 +29,5 @@ angular.module('Bastion.docker-tags').controller('DockerTagDetailsController',
             $scope.panel.loading = false;
             ApiErrorHandler.handleGETRequestErrors(response, $scope);
         });
-
-        $scope.tag.$promise.then(function () {
-            var ids = _.map($scope.tag.related_tags, 'id');
-            var params = {
-                'organization_id': CurrentOrganization,
-                'search': $location.search().search || "",
-                'sort_by': 'name',
-                'sort_order': 'ASC',
-                'paged': false,
-                'ids[]': ids
-            };
-            var nutupane = new Nutupane(DockerTag, params, null, {disableAutoLoad: true});
-
-            $scope.controllerName = 'katello_docker_tags';
-            $scope.table = nutupane.table;
-            $scope.panel.loading = false;
-
-            if (!_.isEmpty(ids)) {
-                nutupane.refresh();
-            }
-        });
-
-        $scope.getManifestType = function (schema) {
-            if (schema['manifest_type'] === 'image') {
-                return translate("Image");
-            }
-            return translate("List");
-
-        };
-
     }
 ]);

engines/bastion_katello/app/assets/javascripts/bastion_katello/docker-tags/details/docker-tag-environments.controller.js
@@ -1,0 +1,64 @@
+/**
+ * @ngdoc object
+ * @name  Bastion.docker-tags.controller:DockerTagDetailsController
+ *
+ * @requires $scope
+ * @requires $location
+ * @requires DockerTag
+ * @requires CurrentOrganization
+ * @requires translate
+ *
+ * @description
+ *   Provides the functionality for the docker tags details environments list.
+ */
+angular.module('Bastion.docker-tags').controller('DockerTagEnvironmentsController',
+    ['$scope', '$location', 'Nutupane', 'DockerTag', 'CurrentOrganization', 'translate',
+    function ($scope, $location, Nutupane, DockerTag, CurrentOrganization, translate) {
+        var params = {
+            'organization_id': CurrentOrganization,
+            'search': $location.search().search || "",
+            'sort_by': 'name',
+            'sort_order': 'ASC',
+            'paged': false
+        };
+        var nutupane = new Nutupane(DockerTag, params, null, {disableAutoLoad: true});
+
+        var renderTable = function () {
+            var ids = _.map($scope.tag.related_tags, 'id');
+            var newParams = {
+                'organization_id': CurrentOrganization,
+                'search': $location.search().search || "",
+                'sort_by': 'name',
+                'sort_order': 'ASC',
+                'paged': false,
+                'ids[]': ids
+            };
+            $scope.table = nutupane.table;
+            nutupane.setParams(newParams);
+            $scope.panel.loading = false;
+            if (!_.isEmpty(ids)) {
+                nutupane.refresh();
+            }
+        };
+
+        $scope.controllerName = 'katello_docker_tags';
+
+        if ($scope.tag) {
+            $scope.panel.loading = false;
+        }
+
+        if ($scope.tag && $scope.tag.related_tags) {
+            renderTable();
+        } else {
+            $scope.tag.$promise.then(renderTable);
+        }
+
+        $scope.getManifestType = function (schema) {
+            if (schema['manifest_type'] === 'image') {
+                return translate("Image");
+            }
+            return translate("List");
+        };
+
+    }
+]);

engines/bastion_katello/app/assets/javascripts/bastion_katello/docker-tags/docker-tags.routes.js
@@ -46,6 +46,7 @@ angular.module('Bastion.docker-tags').config(['$stateProvider', function ($state
         url: '/environments',
         permission: 'view_environments',
         templateUrl: 'docker-tags/details/views/docker-tag-environments.html',
+        controller: 'DockerTagEnvironmentsController',
         ncyBreadcrumb: {
             label: "{{ 'Lifecycle Environments' | translate }}",
             parent: 'docker-tag.info'

engines/bastion_katello/test/docker-tags/details/docker-tag-details.controller.test.js
@@ -1,31 +1,17 @@
 describe('Controller: DockerTagDetailsController', function() {
     var $scope,
         DockerTag,
-        Nutupane,
-        dockerTag,
-        translate;
+        dockerTag;
 
     beforeEach(module('Bastion.docker-tags', 'Bastion.test-mocks', 'Bastion.common'));
 
     beforeEach(inject(function(MockResource, $injector) {
-        Nutupane = function() {
-            this.table = {
-                showColumns: function() {}
-            };
-            this.get = function() {};
-            this.setParams = function (params) {};
-            this.getParams = function (params) { return {}; };
-            this.refresh = function () {};
-        };
         DockerTag = $injector.get('MockResource').$new();
         spyOn(DockerTag, 'get').and.callThrough();
         dockerTag = DockerTag.get({id: 1});
-        translate = function(message) {
-            return message;
-        };
     }));
 
-    beforeEach(inject(function($controller, $rootScope, $location, MockResource, translateMock, $injector) {
+    beforeEach(inject(function($controller, $rootScope, $location, MockResource, $injector) {
         var ApiErrorHandler = $injector.get('ApiErrorHandler');
 
         $scope = $rootScope.$new();
@@ -36,9 +22,7 @@ describe('Controller: DockerTagDetailsController', function() {
 
         $controller('DockerTagDetailsController', {
             $scope: $scope,
-            translate: translate,
             $location: $location,
-            Nutupane: Nutupane,
             DockerTag: DockerTag,
             CurrentOrganization: 'CurrentOrganization',
             ApiErrorHandler: ApiErrorHandler

engines/bastion_katello/test/docker-tags/details/docker-tag-environments.controller.test.js
@@ -1,0 +1,48 @@
+describe('Controller: DockerTagEnvironmentsController', function() {
+    var $scope,
+        dockerTag,
+        DockerTag,
+        Nutupane;
+
+    beforeEach(module('Bastion.docker-tags', 'Bastion.test-mocks', 'Bastion.common'));
+
+    beforeEach(inject(function(MockResource, $injector) {
+        Nutupane = function() {
+            this.table = {
+                showColumns: function() {}
+            };
+            this.get = function() {};
+            this.setParams = function (params) {};
+            this.getParams = function (params) { return {}; };
+            this.refresh = function () {};
+        };
+        dockerTag = {id: 1, related_tags: [{id: 1}]};
+        DockerTag = MockResource.$new();
+    }));
+
+    beforeEach(inject(function($controller, $rootScope, $location, MockResource, $injector) {
+        var ApiErrorHandler = $injector.get('ApiErrorHandler');
+        var translate = function(){};
+
+        $scope = $rootScope.$new();
+
+        $scope.tag = dockerTag;
+        $scope.panel = {};
+        $scope.$stateParams = {tagId: 1};
+
+        $controller('DockerTagEnvironmentsController', {
+            $scope: $scope,
+            $location: $location,
+            translate: translate,
+            Nutupane: Nutupane,
+            DockerTag: DockerTag,
+            CurrentOrganization: 'CurrentOrganization',
+            ApiErrorHandler: ApiErrorHandler
+        });
+    }));
+
+
+    it('attaches the nutupane table to the scope', function() {
+        expect($scope.table).toBeDefined();
+    });
+});

commit ca267f7a2d26022887e4b3a85a8020e33ef20890

Fixes #21692 - DockerTag Info shows Manifest Type
Commit 1fc82810ba25036abae9d7d2ce0caf5eb3b81956 introduced a minor error
in the DockerTag info screen. The Manifest Type (List/Image) were not
getting displayed and the logic that said 'do not show schema version
information if its not present' wasn't getting handled correctly. This
commit fixes that issue.

engines/bastion_katello/app/assets/javascripts/bastion_katello/docker-tags/details/docker-tag-details.controller.js
@@ -7,13 +7,14 @@
  * @requires DockerTag
  * @requires CurrentOrganization
  * @requires ApiErrorHandler
+ * @requires translate
  *
  * @description
  *   Provides the functionality for the docker tags details action pane.
  */
 angular.module('Bastion.docker-tags').controller('DockerTagDetailsController',
-    ['$scope', '$location', 'DockerTag', 'CurrentOrganization', 'ApiErrorHandler',
-    function ($scope, $location, DockerTag, CurrentOrganization, ApiErrorHandler) {
+    ['$scope', '$location', 'DockerTag', 'CurrentOrganization', 'ApiErrorHandler', 'translate',
+    function ($scope, $location, DockerTag, CurrentOrganization, ApiErrorHandler, translate) {
         $scope.panel = {
             error: false,
             loading: true
@@ -23,11 +24,24 @@ angular.module('Bastion.docker-tags').controller('DockerTagDetailsController',
             $scope.panel.loading = false;
         }
 
-        $scope.tag = DockerTag.get({id: $scope.$stateParams.tagId}, function () {
+        $scope.tag = DockerTag.get({id: $scope.$stateParams.tagId}, function (data) {
+            if (data.manifest_schema1) {
+                data.manifest_schema1["manifest_type_display"] = $scope.getManifestDisplayType(data.manifest_schema1);
+            }
+            if (data.manifest_schema2) {
+                data.manifest_schema2["manifest_type_display"] = $scope.getManifestDisplayType(data.manifest_schema2);
+            }
             $scope.panel.loading = false;
         }, function (response) {
             $scope.panel.loading = false;
             ApiErrorHandler.handleGETRequestErrors(response, $scope);
         });
+
+        $scope.getManifestDisplayType = function (schema) {
+            if (schema['manifest_type'] === 'image') {
+                return translate("Image");
+            }
+            return translate("List");
+        };
     }
 ]);

engines/bastion_katello/app/assets/javascripts/bastion_katello/docker-tags/details/docker-tag-environments.controller.js
@@ -6,14 +6,13 @@
  * @requires $location
  * @requires DockerTag
  * @requires CurrentOrganization
- * @requires translate
  *
  * @description
  *   Provides the functionality for the docker tags details environments list.
  */
 angular.module('Bastion.docker-tags').controller('DockerTagEnvironmentsController',
-    ['$scope', '$location', 'Nutupane', 'DockerTag', 'CurrentOrganization', 'translate',
-    function ($scope, $location, Nutupane, DockerTag, CurrentOrganization, translate) {
+    ['$scope', '$location', 'Nutupane', 'DockerTag', 'CurrentOrganization',
+    function ($scope, $location, Nutupane, DockerTag, CurrentOrganization) {
         var params = {
             'organization_id': CurrentOrganization,
             'search': $location.search().search || "",
@@ -52,13 +51,5 @@ angular.module('Bastion.docker-tags').controller('DockerTagEnvironmentsControlle
         } else {
             $scope.tag.$promise.then(renderTable);
         }
-
-        $scope.getManifestType = function (schema) {
-            if (schema['manifest_type'] === 'image') {
-                return translate("Image");
-            }
-            return translate("List");
-        };
-
     }
 ]);

engines/bastion_katello/app/assets/javascripts/bastion_katello/docker-tags/details/views/docker-tag-info.html
@@ -21,13 +21,13 @@
 
   <div data-block="right-column">
     <h4 translate>Docker Manifest</h4>
-    <div ng-if="!_.isEmpty(tag.manifest_schema1)">
+    <div ng-if="tag.manifest_schema1">
       <h5 translate>Schema Version 1</h5>
       <dl class="dl-horizontal dl-horizontal-left">
 
         <dt translate>Manifest Type</dt>
         <dd>
-          {{ getManifestType(tag.manifest_schema1) }}
+          {{ tag.manifest_schema1.manifest_type_display }}
         </dd>
         <dt translate>Digest</dt>
         <dd>
@@ -35,12 +35,12 @@
         </dd>
       </dl>
     </div>
-    <div ng-if="!_.isEmpty(tag.manifest_schema2)">
+    <div ng-if="tag.manifest_schema2">
       <h5 translate>Schema Version 2</h5>
       <dl class="dl-horizontal dl-horizontal-left">
         <dt translate>Manifest Type</dt>
         <dd>
-          {{ getManifestType(tag.manifest_schema2) }}
+          {{ tag.manifest_schema2.manifest_type_display }}
         </dd>
         <dt translate>Digest</dt>
         <dd>
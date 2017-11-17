ca267f7a2d26022887e4b3a85a8020e33ef20890
Partha Aji <paji@redhat.com>
2017-11-17 17:38:50 -0500

    Fixes #21692 - DockerTag Info shows Manifest Type
    Commit 1fc82810ba25036abae9d7d2ce0caf5eb3b81956 introduced a minor error
    in the DockerTag info screen. The Manifest Type (List/Image) were not
    getting displayed and the logic that said 'do not show schema version
    information if its not present' wasn't getting handled correctly. This
    commit fixes that issue.

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/docker-tags/details/docker-tag-details.controller.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/docker-tags/details/docker-tag-details.controller.js
@@ -7,13 +7,14 @@
  * @requires DockerTag
  * @requires CurrentOrganization
  * @requires ApiErrorHandler
+ * @requires translate
  *
  * @description
  *   Provides the functionality for the docker tags details action pane.
  */
 angular.module('Bastion.docker-tags').controller('DockerTagDetailsController',
-    ['$scope', '$location', 'DockerTag', 'CurrentOrganization', 'ApiErrorHandler',
-    function ($scope, $location, DockerTag, CurrentOrganization, ApiErrorHandler) {
+    ['$scope', '$location', 'DockerTag', 'CurrentOrganization', 'ApiErrorHandler', 'translate',
+    function ($scope, $location, DockerTag, CurrentOrganization, ApiErrorHandler, translate) {
         $scope.panel = {
             error: false,
             loading: true
@@ -23,11 +24,24 @@ angular.module('Bastion.docker-tags').controller('DockerTagDetailsController',
             $scope.panel.loading = false;
         }
 
-        $scope.tag = DockerTag.get({id: $scope.$stateParams.tagId}, function () {
+        $scope.tag = DockerTag.get({id: $scope.$stateParams.tagId}, function (data) {
+            if (data.manifest_schema1) {
+                data.manifest_schema1["manifest_type_display"] = $scope.getManifestDisplayType(data.manifest_schema1);
+            }
+            if (data.manifest_schema2) {
+                data.manifest_schema2["manifest_type_display"] = $scope.getManifestDisplayType(data.manifest_schema2);
+            }
             $scope.panel.loading = false;
         }, function (response) {
             $scope.panel.loading = false;
             ApiErrorHandler.handleGETRequestErrors(response, $scope);
         });
+
+        $scope.getManifestDisplayType = function (schema) {
+            if (schema['manifest_type'] === 'image') {
+                return translate("Image");
+            }
+            return translate("List");
+        };
     }
 ]);
