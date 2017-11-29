87e45d8be8438e78349bcc2d31815d3a8126a749
Walden Raines <walden@redhat.com>
2017-11-29 15:19:29 -0500

    Fixes #21712: fix double search on CH and AK pages.
    This commit fixes an issue where a search was executed
    twice for pages that included an unnecessary query
    string parameter for search.
    
    http://projects.theforeman.org/issues/21712
    (cherry picked from commit a406fbe95cf7b41b2de7bc772d2fe3195fb69e75)

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/subscriptions/subscriptions.routes.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/subscriptions/subscriptions.routes.js
@@ -60,7 +60,7 @@ angular.module('Bastion.subscriptions').config(['$stateProvider', function ($sta
         }
     })
     .state('subscription.content-hosts', {
-        url: '/content-hosts?search',
+        url: '/content-hosts',
         permission: 'view_subscriptions',
         controller: 'SubscriptionContentHostsController',
         templateUrl: 'subscriptions/details/views/subscription-content-hosts.html',
