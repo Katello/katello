13b438b13726cfdb81e9f130fc289d8b09e5905d
Dan Seethaler <dan@seethalers.com>
2017-12-11 10:56:34 -0500

    Fixes #21931 - Add react-storybook to Katello
    Adds dependency for react-storybook as well as the deployer.
    I've included one story as an example.
    
    http://projects.theforeman.org/issues/21931

---
+++ webpack/scenes/RedHatRepositories/components/EnabledRepository.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import EnabledRepository from './EnabledRepository.js';
+
+storiesOf('RedHat Repositories Page', module).add('Enabled Repository', () => (
+  <EnabledRepository
+    id={638}
+    name="Red Hat Enterprise Linux 6 Server Kickstart x86_64 6.8"
+    releasever="6.8"
+    arch="x86_64"
+    type="rpm"
+  />
+));
