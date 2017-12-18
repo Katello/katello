b6c386051ee54a94fc8f7449400fc25b427403b6
Justin Sherrill <jsherril@redhat.com>
2017-12-18 12:10:27 -0500

    Fixes #22015 - write log to correct location
    and reduce code duplication

--- test/lib/tasks/update_subscription_facet_backend_data_test.rb
+++ test/lib/tasks/update_subscription_facet_backend_data_test.rb
@@ -4,9 +4,9 @@ require 'rake'
 module Katello
   class UpdateSubscriptionFacetBackendDataTest < ActiveSupport::TestCase
     def setup
-      Rake.application.rake_require 'katello/tasks/upgrades/3.0/update_subscription_facet_backend_data'
+      Rake.application.rake_require 'katello/tasks/update_subscription_facet_backend_data'
 
-      Rake::Task['katello:upgrades:3.0:update_subscription_facet_backend_data'].reenable
+      Rake::Task['katello:update_subscription_facet_backend_data'].reenable
       Rake::Task.define_task(:environment)
       @host = hosts(:one)
       @host.organization = get_organization
@@ -19,7 +19,7 @@ module Katello
 
       Katello::Host::SubscriptionFacet.expects(:update_facts).with(@host, :foo => :bar).once
 
-      Rake.application.invoke_task('katello:upgrades:3.0:update_subscription_facet_backend_data')
+      Rake.application.invoke_task('katello:update_subscription_facet_backend_data')
     end
   end
 end
