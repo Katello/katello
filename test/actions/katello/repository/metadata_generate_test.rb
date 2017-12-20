2ef7126e5212c761b01dfceff8ba2272182933f7
Justin Sherrill <jsherril@redhat.com>
2017-12-20 11:18:52 -0500

    Fixes #22040 - fix promotion of puppet content

--- test/actions/katello/repository/metadata_generate_test.rb
+++ test/actions/katello/repository/metadata_generate_test.rb
@@ -11,6 +11,7 @@ module Actions
     let(:yum_repo) { katello_repositories(:fedora_17_x86_64) }
     let(:yum_repo2) { katello_repositories(:fedora_17_x86_64_dev) }
     let(:puppet_repo) { katello_repositories(:p_forge) }
+    let(:content_view_puppet_env) { katello_content_view_puppet_environments(:library_view_puppet_environment) }
 
     it 'plans a yum refresh' do
       action = create_action(action_class)
@@ -20,6 +21,27 @@ module Actions
           :distributor_type_id => Runcible::Models::YumDistributor.type_id,
           :source_pulp_id => nil,
           :override_config => {:force_full => false},
+          :dependency => nil,
+          :matching_content => nil)
+    end
+
+    it 'plans with a content view puppet env' do
+      action = create_action(action_class)
+      content_view_puppet_env.puppet_environment = ::Environment.create(:name => 'foobar')
+
+      plan_action(action, content_view_puppet_env)
+
+      assert_action_planed_with(action, pulp_publish_class, :pulp_id => content_view_puppet_env.pulp_id,
+          :distributor_type_id => Runcible::Models::PuppetInstallDistributor.type_id,
+          :source_pulp_id => nil,
+          :override_config => {},
+          :dependency => nil,
+          :matching_content => nil)
+
+      assert_action_planed_with(action, pulp_publish_class, :pulp_id => content_view_puppet_env.pulp_id,
+          :distributor_type_id => Runcible::Models::PuppetDistributor.type_id,
+          :source_pulp_id => nil,
+          :override_config => {},
           :dependency => nil,
           :matching_content => nil)
     end
