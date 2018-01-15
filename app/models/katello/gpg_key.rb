1bbff19f06cacd68cf2d6335852523e4d20cbc4f
Bernhard Suttner <suttner@atix.de>
2018-01-15 18:29:56 +0100

    refs #15068 - Fix issues found at review

--- app/models/katello/gpg_key.rb
+++ app/models/katello/gpg_key.rb
@@ -4,6 +4,9 @@ module Katello
     MAX_CONTENT_LENGTH = 100_000
     MAX_CONTENT_LINE_LENGTH = 65
 
+    GPG_KEY_TYPE = 'gpg_key'.freeze
+    CERT_TYPE = 'cert'.freeze
+
     has_many :repositories, :class_name => "Katello::Repository", :inverse_of => :gpg_key, :dependent => :nullify
     has_many :products, :class_name => "Katello::Product", :inverse_of => :gpg_key, :dependent => :nullify
     has_many :ssl_ca_products, :class_name => "Katello::Product", :foreign_key => "ssl_ca_cert_id",
@@ -23,8 +26,8 @@ module Katello
     validates_lengths_from_database
     validates :name, :presence => true, :uniqueness => {:scope => :organization_id,
                                                         :message => N_("has already been taken")}
-    validates :content_type, :presence => true, :inclusion => { :in => %w(gpg_key cert),
-                                                                :message => N_("must be gpg_key or cert")}
+    validates :content_type, :presence => true, :inclusion => { :in => [GPG_KEY_TYPE, CERT_TYPE],
+                                                                :message => N_("must be %{gpg_key} or %{cert}") % { :gpg_key => GPG_KEY_TYPE, :cert => CERT_TYPE} }
     validates :content, :presence => true, :length => {:maximum => MAX_CONTENT_LENGTH}
     validates :organization, :presence => true
     validates_with Validators::KatelloNameFormatValidator, :attributes => :name
@@ -35,7 +38,7 @@ module Katello
     scoped_search :on => :organization_id, :complete_value => true, :only_explicit => true, :validator => ScopedSearch::Validators::INTEGER
 
     def use_gpg_content_validator?
-      content_type == "gpg_key" && SETTINGS[:katello][:gpg_strict_validation]
+      content_type == GPG_KEY_TYPE && SETTINGS[:katello][:gpg_strict_validation]
     end
 
     def as_json(options = {})
