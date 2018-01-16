b43fe119ab475845c105ecb7bc7ba58c92af9702
Partha Aji <paji@redhat.com>
2018-01-16 12:04:14 -0500

    Fixes #22288 - correct json value for Component_id
    Commit 062a73e039eb62fb60e1c9e5c140ba4ebefc0c80 incorrectly associated
    content_view_versions to component_ids. This commit fixes that

--- app/models/katello/content_view.rb
+++ app/models/katello/content_view.rb
@@ -81,6 +81,10 @@ module Katello
       hosts.count
     end
 
+    def component_ids
+      components.map(&:id)
+    end
+
     def components
       content_view_components.map(&:latest_version).compact.freeze
     end
