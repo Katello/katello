66a8b16f10b60cad4712062b1ed3a1ad7c75427e
Justin Sherrill <jsherril@redhat.com>
2018-01-22 16:01:21 -0500

    Fixes #22360 - fix non-admin cv publish
    (cherry picked from commit a98594ac41d5d914f99a79ac4547bd775388a705)

--- app/models/katello/authorization/pool.rb
+++ app/models/katello/authorization/pool.rb
@@ -2,8 +2,6 @@ module Katello
   module Authorization::Pool
     extend ActiveSupport::Concern
 
-    include Authorizable
-
     module ClassMethods
       def readable
         where(:subscription_id => Katello::Subscription.authorized(:view_subscriptions))
