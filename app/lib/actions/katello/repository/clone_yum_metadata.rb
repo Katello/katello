4774ebb65022e61a583855b11a9f1c746f0f2407
Partha Aji <paji@redhat.com>
2018-01-23 15:41:45 -0500

    Fixes #21612 - Promote op performs distro publish
    Previously commits added smarts on running the metadata operations on a
    CV publish only if necessary. The code would match content and force a
    distribution publish only if content changed. However that code does not
    look for changes related to the "publish via http"/"unprotected" on a
    repo. So if the "publish-via-http" option on a repo is changed, the
    promote operation would not perform a distributor publish even though it
    should.
    
    This commit fixes that by making sure that "matching content" operation
    happens only if the "publish-via-http"/"unprotected" attribute has not
    changed.
    
    (cherry picked from commit 66ef3f91b66e75ea73342395e44757beddb3cfbb)

---
+++ app/lib/actions/katello/repository/clone_yum_metadata.rb
@@ -0,0 +1,28 @@
+module Actions
+  module Katello
+    module Repository
+      class CloneYumMetadata < Actions::Base
+        def plan(source_repo, target_repo, options = {})
+          sequence do
+            # Check for matching content before indexing happens, the content in pulp is
+            # actually updated, but it is not reflected in the database yet.
+            output = {}
+            if !target_repo.previous_changes.include?(:unprotected) &&
+                target_repo.environment && !options[:force_yum_metadata_regeneration]
+              output = plan_action(Katello::Repository::CheckMatchingContent,
+                                   :source_repo_id => source_repo.id,
+                                   :target_repo_id => target_repo.id).output
+            end
+
+            plan_action(Katello::Repository::IndexContent, id: target_repo.id)
+
+            plan_action(Katello::Repository::MetadataGenerate,
+                        target_repo,
+                        :source_repository => source_repo,
+                        :matching_content => output[:matching_content])
+          end
+        end
+      end
+    end
+  end
+end
