37057931b490dc56a259c8b2ea7f5519d5c6568e
Justin Sherrill <jsherril@redhat.com>
2018-03-12 21:26:29 -0400

    Fixes #22873 - handle race condition on host unregister
    soon after checkin

--- app/models/katello/content_facet_applicable_rpm.rb
+++ app/models/katello/content_facet_applicable_rpm.rb
@@ -1,5 +1,6 @@
 module Katello
   class ContentFacetApplicableRpm < Katello::Model
+    # Do not use active record callbacks in this join model.  Direct INSERTs and DELETEs are done
     belongs_to :content_facet, :inverse_of => :content_facet_applicable_rpms, :class_name => 'Katello::Host::ContentFacet'
     belongs_to :rpm, :inverse_of => :content_facet_applicable_rpms, :class_name => 'Katello::Rpm'
   end
