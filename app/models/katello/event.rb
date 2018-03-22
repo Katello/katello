4c934a7dd83c567ba06eb86ea8c74894ed888b03
Partha Aji <paji@redhat.com>
2018-03-21 22:27:28 -0400

    Fixes #22978 - Improve Event Queue db queries
    The delete and update operations in the event queue were not quick
    enough when there was a lot of data in the event queue (20K entries
    with many duplicates etc.)
    This commit addresses that issue by speeding up the delete and update
    operations

--- app/models/katello/event.rb
+++ app/models/katello/event.rb
@@ -1,6 +1,8 @@
 module Katello
   class Event < Katello::Model
     validate :validate_event_type
+    # Note: Do not use active record call backs or dependent references on this class
+    # Direct deletes are made in EventQueue#clear_events (instead of destroys).
 
     def validate_event_type
       unless EventQueue.supported_event_types.include?(self.event_type)
