cbb800d8415f806aedb849c53091070fcbeea45f
Evgeni Golov <evgeni@golov.de>
2018-04-17 14:41:37 +0200

    Fixes #23296 - short index name for content_facet_applicable_rpms

--- db/migrate/20180413153126_content_facet_applicable_rpms_index.rb
+++ db/migrate/20180413153126_content_facet_applicable_rpms_index.rb
@@ -1,5 +1,5 @@
 class ContentFacetApplicableRpmsIndex < ActiveRecord::Migration[5.1]
   def change
-    add_index :katello_content_facet_applicable_rpms, :content_facet_id
+    add_index :katello_content_facet_applicable_rpms, :content_facet_id, :name => "index_k_content_facet_applicable_rpms_on_content_facet_id"
   end
 end
