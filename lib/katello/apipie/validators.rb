c9e262ff52e3e0c0b069213b05cd55111db12588
Andrew Kofink <akofink@redhat.com>
2018-04-30 12:41:09 -0400

    Fixes #23460: Use apipie-rails built in Validators
    (cherry picked from commit cf076c66a1ea2ab029eff11520ac3b1c794e967f)

--- lib/katello/apipie/validators.rb
+++ lib/katello/apipie/validators.rb
@@ -13,42 +13,6 @@ end
 module Katello
   module Apipie
     module Validators
-      class UndefValidator < ::Apipie::Validator::BaseValidator
-        def validate(_value)
-          true
-        end
-
-        def self.build(param_description, argument, _options, _block)
-          if argument == :undef
-            self.new(param_description)
-          end
-        end
-
-        def description
-          nil
-        end
-      end
-
-      class NumberValidator < ::Apipie::Validator::BaseValidator
-        def validate(value)
-          value.to_s =~ /^(0|[1-9]\d*)$/
-        end
-
-        def self.build(param_description, argument, _options, _block)
-          if argument == :number
-            self.new(param_description)
-          end
-        end
-
-        def error
-          "Parameter #{param_name} expecting to be a number, got: #{@error_value}"
-        end
-
-        def description
-          "number."
-        end
-      end
-
       class IdentifierValidator < ::Apipie::Validator::BaseValidator
         def validate(value)
           value = value.to_s
@@ -67,26 +31,6 @@ module Katello
 
         def description
           "string from 2 to 128 characters containing only alphanumeric characters, space, '_', '-' with no leading or trailing space.."
-        end
-      end
-
-      class BooleanValidator < ::Apipie::Validator::BaseValidator
-        def validate(value)
-          %w(true false True False).include?(value.to_s)
-        end
-
-        def self.build(param_description, argument, _options, _block)
-          if argument == :bool
-            self.new(param_description)
-          end
-        end
-
-        def error
-          "Parameter #{param_name} expecting to be a boolean value, got: #{@error_value}"
-        end
-
-        def description
-          "boolean"
         end
       end
     end
