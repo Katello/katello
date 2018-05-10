8ab296a1f5d33443f83ab5b5cbe531b33e41c8b9
John Mitsch <jomitsch@redhat.com>
2018-05-10 20:56:54 +0000

    Fixes #23286 - Add confirmation dialog for deleting manifest

---
+++ webpack/scenes/Subscriptions/Manifest/DeleteManifestModalText.js
@@ -0,0 +1,22 @@
+const question = __('Are you sure you want to delete the manifest?');
+const note = __(`Note: Deleting a subscription manifest is STRONGLY discouraged.
+                 Deleting a manifest will:`);
+const l1 = __('Delete all subscriptions that are attached to running hosts.');
+const l2 = __('Delete all subscriptions attached to activation keys.');
+const l3 = __('Disable Red Hat Insights.');
+const l4 = __(`Require you to upload the subscription-manifest and re-attach
+               subscriptions to hosts and activation keys.`);
+const debug = __(`This action should only be taken in extreme circumstances or
+                  for debugging purposes.`);
+
+const DeleteManifestModalText = `<p>${question}</p>
+<p>${note}</p>
+<ul class="list-aligned">
+  <li>${l1}</li>
+  <li>${l2}</li>
+  <li>${l3}</li>
+  <li>${l4}</li>
+</ul>
+<p>${debug}</p>`;
+
+export default DeleteManifestModalText;
