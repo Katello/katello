302033155de1038950001cd6e8c23140b9edd7e6
Adam Ruzicka <aruzicka@redhat.com>
2018-05-17 10:54:31 +0200

    Fixes #23326 - Create active jobs properly

--- lib/katello/scheduled_jobs.rb
+++ lib/katello/scheduled_jobs.rb
@@ -1,18 +1,6 @@
 # First, we check if there's a job already enqueued for any notifications
 ::Foreman::Application.dynflow.config.on_init do |world|
-  pending_jobs = world.persistence.find_execution_plans(filters: { :state => 'scheduled' })
-  scheduled_pulp_job = pending_jobs.select do |job|
-    delayed_plan = world.persistence.load_delayed_plan job.id
-    next if delayed_plan.blank?
-    delayed_plan.to_hash[:serialized_args].first.try(:[], 'job_class') == 'CreatePulpDiskSpaceNotifications'
+  [CreatePulpDiskSpaceNotifications, SendExpireSoonNotifications].each do |job_class|
+    job_class.spawn_if_missing(world)
   end
-  scheduled_subs_expiration_job = pending_jobs.select do |job|
-    delayed_plan = world.persistence.load_delayed_plan job.id
-    next if delayed_plan.blank?
-    delayed_plan.to_hash[:serialized_args].first.try(:[], 'job_class') == 'SendExpireSoonNotifications'
-  end
-
-  # Only create notifications if there isn't a scheduled job
-  CreatePulpDiskSpaceNotifications.perform_later if scheduled_pulp_job.blank?
-  SendExpireSoonNotifications.perform_later if scheduled_subs_expiration_job.blank?
 end
