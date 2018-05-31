7b6668549b71e7a7cb649d539401f116ce0e02a6
Tom McKay <thomasmckay@redhat.com>
2018-05-31 15:39:17 -0400

    fixes #23390 - set data for breadcrumbs
    (cherry picked from commit cd85395bf0f786b5c88235bee33076fef8662c54)

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/environments/environments.controller.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/environments/environments.controller.js
@@ -3,16 +3,30 @@
  * @name  Bastion.environments.controller:EnvironmentsController
  *
  * @requires $scope
+ * @requires PathsService
+ * @requires $location
  * @requires Organization
  * @requires CurrentOrganization
+ * @requires Nutupane
+ * @requires Environment
  *
  * @description
  *   Provides the functionality for the environments path page.
  */
 angular.module('Bastion.environments').controller('EnvironmentsController',
-    ['$scope', 'PathsService',
-        function ($scope, PathsService) {
-            $scope.loading = true;
+    ['$scope', 'PathsService', '$location', 'Organization', 'CurrentOrganization', 'Nutupane', 'Environment',
+        function ($scope, PathsService, $location, Organization, CurrentOrganization, Nutupane, Environment) {
+
+            var params = {
+                'organization_id': CurrentOrganization,
+                'search': $location.search().search || "",
+                'sort_by': 'name',
+                'sort_order': 'ASC',
+                'paged': true
+            };
+
+            var nutupane = new Nutupane(Environment, params);
+            $scope.table = nutupane.table;
 
             PathsService.getActualPaths().then(function (data) {
                 $scope.library = data.library;
