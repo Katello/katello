9fdeec41ef6def25ccd1148077029aed89fe4ca4
Avi Sharvit <asharvit@redhat.com>
2018-06-07 19:32:18 +0300

    Fixes #21648 - Red Hat Recommended Repositories
    Adds recommended-repository-sets-toggler
    to the available-repository-sets-list
    
    (cherry picked from commit 09cecfa093b7755c7deac4c7459a3690c865ec8d)

--- webpack/redux/actions/RedHatRepositories/sets.js
+++ webpack/redux/actions/RedHatRepositories/sets.js
@@ -1,21 +1,30 @@
 import api, { orgId } from '../../../services/api';
-import { normalizeRepositorySets, repoTypeFilterToSearchQuery, joinSearchQueries } from './helpers';
+import {
+  normalizeRepositorySets,
+  repoTypeFilterToSearchQuery,
+  joinSearchQueries,
+  recommendedRepositorySetsQuery,
+} from './helpers';
 
 import {
   REPOSITORY_SETS_REQUEST,
   REPOSITORY_SETS_SUCCESS,
   REPOSITORY_SETS_FAILURE,
+  REPOSITORY_SETS_UPDATE_RECOMMENDED,
 } from '../../consts';
 import { propsToSnakeCase } from '../../../services/index';
 
 // eslint-disable-next-line import/prefer-default-export
-export const loadRepositorySets = (extendedParams = {}) => (dispatch) => {
+export const loadRepositorySets = (extendedParams = {}) => (dispatch, getState) => {
+  const { recommended } = getState().katello.redHatRepositories.sets;
+
   dispatch({ type: REPOSITORY_SETS_REQUEST, params: extendedParams });
 
   const searchParams = extendedParams.search || {};
   const search = joinSearchQueries([
     repoTypeFilterToSearchQuery(searchParams.filters || []),
     searchParams.query,
+    recommended ? recommendedRepositorySetsQuery : '',
   ]);
 
   const params = {
@@ -29,14 +38,27 @@ export const loadRepositorySets = (extendedParams = {}) => (dispatch) => {
     .then(({ data }) => {
       dispatch({
         type: REPOSITORY_SETS_SUCCESS,
-        response: normalizeRepositorySets(data),
-        search: searchParams,
+        payload: {
+          response: normalizeRepositorySets(data),
+          search: searchParams,
+        },
       });
     })
     .catch((result) => {
       dispatch({
         type: REPOSITORY_SETS_FAILURE,
-        result,
+        payload: result,
       });
     });
+};
+
+export const updateRecommendedRepositorySets = value => (dispatch, getState) => {
+  const { search } = getState().katello.redHatRepositories.sets;
+
+  dispatch({
+    type: REPOSITORY_SETS_UPDATE_RECOMMENDED,
+    payload: value,
+  });
+
+  dispatch(loadRepositorySets({ search }));
 };
