cc9ebd2b7ee9d5d16ef215164b3b58b6abfe6131
Tomas Strachota <tstrachota@redhat.com>
2018-06-11 17:03:38 +0200

    Fixes #23475 - stop polling tasks on unauthorized request
    (cherry picked from commit fd5efcdaaf472c3d8d759fae2b7fa27c5f530c4b)

--- webpack/scenes/Tasks/__tests__/TaskActions.test.js
+++ webpack/scenes/Tasks/__tests__/TaskActions.test.js
@@ -6,13 +6,15 @@ import configureMockStore from 'redux-mock-store';
 import {
   bulkSearchSuccessResponse,
   bulkSearchSuccessActions,
-  bulkSearchFailureActions,
+  buildBulkSearchFailureActions,
   getTaskSuccessResponse,
   getTaskSuccessActions,
-  getTaskFailureActions,
+  buildTaskFailureActions,
+  getTaskPendingResponse,
+  getTaskPendingActions,
 } from './task.fixtures';
 
-import { bulkSearch, loadTask, pollTaskUntilDone } from '../TaskActions';
+import { bulkSearch, loadTask, pollTaskUntilDone, pollBulkSearch } from '../TaskActions';
 
 const mockStore = configureMockStore([thunk]);
 const store = mockStore({ tasks: Immutable({}) });
@@ -24,13 +26,19 @@ beforeEach(() => {
 });
 
 let originalTimeout;
+let setTimeoutSpy;
 
 beforeEach(() => {
+  setTimeoutSpy = jest.spyOn(window, 'setTimeout');
+
   originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 });
 
 afterEach(() => {
+  setTimeoutSpy.mockReset();
+  setTimeoutSpy.mockRestore();
+
   jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
 });
 
@@ -42,7 +50,7 @@ describe('task actions', () => {
       mockApi.onPost(url).reply(422);
 
       return store.dispatch(bulkSearch())
-        .then(() => expect(store.getActions()).toEqual(bulkSearchFailureActions));
+        .then(() => expect(store.getActions()).toEqual(buildBulkSearchFailureActions()));
     });
 
     it('and ends with success', () => {
@@ -61,7 +69,7 @@ describe('task actions', () => {
       mockApi.onGet(url).reply(422);
 
       return store.dispatch(loadTask(taskId))
-        .then(() => expect(store.getActions()).toEqual(getTaskFailureActions));
+        .then(() => expect(store.getActions()).toEqual(buildTaskFailureActions()));
     });
 
     it('and ends with success', () => {
@@ -70,12 +78,87 @@ describe('task actions', () => {
       return store.dispatch(loadTask(taskId))
         .then(() => expect(store.getActions()).toEqual(getTaskSuccessActions));
     });
+  });
 
-    it('and can poll', () => {
-      mockApi.onGet(url).reply(200, getTaskSuccessResponse);
+  describe('pollTaskUntilDone', () => {
+    const taskId = 'eb1b6271-8a69-4d98-84fc-bea06ddcc166';
+    const url = `/foreman_tasks/api/tasks/${taskId}`;
 
-      return store.dispatch(pollTaskUntilDone(taskId))
-        .then(() => expect(store.getActions()).toEqual(getTaskSuccessActions));
+    it("doesn't start polling when the task has finished", () => {
+      mockApi.onGet(url).replyOnce(200, getTaskSuccessResponse);
+
+      return store
+        .dispatch(pollTaskUntilDone(taskId, {}, 1))
+        .then(() => {
+          expect(store.getActions()).toEqual(getTaskSuccessActions);
+          expect(setTimeoutSpy).toHaveBeenCalledTimes(0);
+        });
+    });
+
+    it('polls until a task is done', () => {
+      mockApi
+        .onGet(url)
+        .replyOnce(200, getTaskPendingResponse)
+        .onGet(url)
+        .replyOnce(200, getTaskPendingResponse)
+        .onGet(url)
+        .replyOnce(200, getTaskSuccessResponse);
+
+      const expectedActions = getTaskPendingActions
+        .concat(getTaskPendingActions)
+        .concat(getTaskSuccessActions);
+
+      return store
+        .dispatch(pollTaskUntilDone(taskId, {}, 1))
+        .then(() => {
+          expect(store.getActions()).toEqual(expectedActions);
+          expect(setTimeoutSpy).toHaveBeenCalledTimes(2);
+        });
+    });
+
+    it('stops polling on unauthorized response', () => {
+      mockApi
+        .onGet(url).replyOnce(200, getTaskPendingResponse)
+        .onGet(url).replyOnce(401);
+
+      const expectedActions = getTaskPendingActions
+        .concat(buildTaskFailureActions(401));
+
+      return store
+        .dispatch(pollTaskUntilDone(taskId, {}, 1))
+        .catch(() => {
+          expect(store.getActions()).toEqual(expectedActions);
+          expect(setTimeoutSpy).toHaveBeenCalledTimes(1);
+        });
+    });
+  });
+
+  describe('pollBulkSearch', () => {
+    const url = '/foreman_tasks/api/tasks/bulk_search';
+
+    it('polls', () => {
+      mockApi
+        .onPost(url)
+        .replyOnce(200, bulkSearchSuccessResponse);
+
+      return store
+        .dispatch(pollBulkSearch({}, 1))
+        .then(() => {
+          expect(store.getActions()).toEqual(bulkSearchSuccessActions);
+          expect(setTimeoutSpy).toHaveBeenCalledTimes(1);
+        });
+    });
+
+    it('stops polling on unauthorized response', () => {
+      mockApi
+        .onPost(url).replyOnce(401);
+
+      return store
+        .dispatch(pollBulkSearch({}, 1))
+        .then(() => {
+          expect(store.getActions()).toEqual(buildBulkSearchFailureActions(401));
+          expect(setTimeoutSpy).toHaveBeenCalledTimes(0);
+        });
     });
   });
 });
