f58aa6cd14a148419181e22745a10ea9fb25d07d
Adam Ruzicka <aruzicka@redhat.com>
2018-06-12 09:05:02 +0200

    Fixes #23326 - Version bumping
    (cherry picked from commit 9ab0d774aba6a0f6f2050f5d9b5905f2b34cb433)

--- lib/katello/plugin.rb
+++ lib/katello/plugin.rb
@@ -3,7 +3,7 @@ require 'katello/repository_types.rb'
 
 # rubocop:disable Metrics/BlockLength
 Foreman::Plugin.register :katello do
-  requires_foreman '>= 1.17'
+  requires_foreman '>= 1.18'
 
   sub_menu :top_menu, :content_menu, :caption => N_('Content'),
            :icon => 'fa fa-book', :after => :monitor_menu do
