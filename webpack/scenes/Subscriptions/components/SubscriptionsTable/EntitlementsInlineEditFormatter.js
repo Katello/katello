c093b5886e067e9943b8f4edbf18077333a180c0
Tomas Strachota <tstrachota@redhat.com>
2018-07-03 14:14:42 +0200

    Fixes #24145 - correct message of max available entitlements

--- webpack/scenes/Subscriptions/components/SubscriptionsTable/EntitlementsInlineEditFormatter.js
+++ webpack/scenes/Subscriptions/components/SubscriptionsTable/EntitlementsInlineEditFormatter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { sprintf } from 'jed';
 import { Table, FormControl, FormGroup, HelpBlock, Spinner } from 'patternfly-react';
 import { validateQuantity } from '../../SubscriptionValidations';
 import { KEY_CODES } from '../../../../move_to_foreman/common/helpers';
@@ -39,10 +40,9 @@ export const entitlementsInlineEditFormatter =
         ? 'editable editing changed'
         : 'editable editing';
 
-      const maxMessage = [
-        __('Max'),
-        availableQuantity,
-      ].join(' ');
+      const maxMessage = (availableQuantity < 1)
+        ? __('Unlimited')
+        : sprintf(__('Max %(availableQuantity)s'), { availableQuantity });
 
       const validation = validateQuantity(value, availableQuantity);
 
@@ -55,7 +55,7 @@ export const entitlementsInlineEditFormatter =
         // The same issue prevents from correct switching inputs on TAB.
         // See the reactabular code for details:
         // https://github.com/reactabular/reactabular/blob/master/packages/reactabular-table/src/body-row.js#L58
-        <Spinner loading={!availableQuantity} size="xs">
+        <Spinner loading={availableQuantity === undefined} size="xs">
           <FormGroup
             validationState={validation.state}
           >
