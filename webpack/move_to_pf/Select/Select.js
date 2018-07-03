6ef62d1f497f905848f6185a279ca12d0c170eb8
amirfefer <amirfefer@gmail.com>
2018-07-03 15:32:00 +0000

    Fixes #22568, #24143 - add select org page

---
+++ webpack/move_to_pf/Select/Select.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Select = ({
+  placeholder, onChange, options, disabled, value,
+}) => {
+  const renderOptions = arr =>
+    arr.map(({ name, id }) => (
+      <option key={id} value={id}>
+        {name}
+      </option>
+    ));
+
+  return (
+    <select
+      disabled={disabled}
+      className="form-control"
+      value={value}
+      onChange={onChange}
+    >
+      <option value="" disabled >{placeholder}</option>
+      {renderOptions(options)}
+    </select>
+  );
+};
+
+export default Select;
+
+Select.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.object).isRequired,
+  disabled: PropTypes.bool,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string,
+};
+
+Select.defaultProps = {
+  disabled: false,
+  value: '',
+};
