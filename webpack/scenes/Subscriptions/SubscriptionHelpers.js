b8f9d900f47cfc0c1daddd279858807b5313b079
Walden Raines <walden@redhat.com>
2018-07-10 14:26:08 -0400

    Fixes #24129: fix subs. display when no manifest.
    When there is no manifest:
    
    - Disable "Add subscription" button in the top right toolbar
      with an appropriate tooltip message.
    
    - Show different empty view on the subscriptions page saying
      "Import a Manifest to manage your Entitlements" or something
      similar and show "Manage Manifest" button.
    
    - Change the text in the add subscriptions empty view to the above.
    
    https://projects.theforeman.org/issues/24129
    (cherry picked from commit d04bf684dad7949ec72b8cc463eabdede1ac3b6f)

--- webpack/scenes/Subscriptions/SubscriptionHelpers.js
+++ webpack/scenes/Subscriptions/SubscriptionHelpers.js
@@ -1,3 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const filterRHSubscriptions = subscriptions =>
   subscriptions.filter(sub => sub.available >= 0);
+
+export const manifestExists = organization =>
+  organization.owner_details && organization.owner_details.upstreamConsumer;
