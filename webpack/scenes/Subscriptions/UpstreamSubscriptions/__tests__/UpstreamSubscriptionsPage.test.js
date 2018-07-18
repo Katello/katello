2c760d96327c5b1cd2f9addc80c58b6e23c45682
John Mitsch <jomitsch@redhat.com>
2018-07-18 01:07:30 +0000

    Fixes #24215 - Add validation for upstream sub quantities
    This commit adds validation for the text fields in the
    "Add Subscriptions" page. The validation is handled by:
    - The submit button being grey when no subscriptions
      are selected, so the user can't submit a request
      with no subscriptions checked
    - The text box turning green or red depending on if the
      quantity is valid
    - The quantity values being checked on submission, and
      showing an error notification if its invalid. This
      prevents a bad request from being sent to the API
    
    In addition, the "Available Entitlements" column on the table
    has changed to reflect what is available, not the total.
    This is because the available subscriptionsis the max
    amount that can be added.
    
    Since this quantity is
    shown and visual feedback is given to the user if they
    exceed this, there is no need for the "MAX" text under the
    text box. This closely mirrors the functionality of the Red
    Hat Portal.
    
    (cherry picked from commit b5af2f1bbd42e02b1fbe0454e25153229750c34c)

--- webpack/scenes/Subscriptions/UpstreamSubscriptions/__tests__/UpstreamSubscriptionsPage.test.js
+++ webpack/scenes/Subscriptions/UpstreamSubscriptions/__tests__/UpstreamSubscriptionsPage.test.js
@@ -8,15 +8,59 @@ import { loadUpstreamSubscriptions, saveUpstreamSubscriptions } from '../Upstrea
 jest.mock('../../../../move_to_foreman/foreman_toast_notifications');
 
 describe('upstream subscriptions page', () => {
-  const mockHistory = { push: () => {} };
-
-  it('should render', async () => {
-    const page = shallow(<UpstreamSubscriptionsPage
+  let shallowWrapper;
+  beforeEach(() => {
+    shallowWrapper = shallow(<UpstreamSubscriptionsPage
       upstreamSubscriptions={successState}
       loadUpstreamSubscriptions={loadUpstreamSubscriptions}
       saveUpstreamSubscriptions={saveUpstreamSubscriptions}
       history={mockHistory}
     />);
-    expect(toJson(page)).toMatchSnapshot();
+  });
+  const mockHistory = { push: () => {} };
+
+  it('should render', async () => {
+    expect(toJson(shallowWrapper)).toMatchSnapshot();
+  });
+
+  it('should validate correct subscription quantities', async () => {
+    const validPools = [
+      { available: 10, updatedQuantity: 5 },
+      { available: 10, updatedQuantity: '5' },
+      { available: 10, updatedQuantity: '10' },
+      { available: 10, updatedQuantity: '1' },
+      { available: -1, updatedQuantity: '1000' },
+    ];
+    validPools.forEach((pool, i) => {
+      // using object with index attribute to print out index on failure,
+      // jest doesn't support messages on failure :(
+      const result = shallowWrapper.instance().quantityValidation(pool)[0];
+      expect({ index: i, result }).toEqual({ index: i, result: true });
+    });
+  });
+
+  it('should invalidate incorrect subscription quantities', async () => {
+    const invalidPools = [
+      { available: 10, updatedQuantity: 11 },
+      { available: 10, updatedQuantity: 'foo' },
+      { available: 10, updatedQuantity: 0 },
+      { available: 10, updatedQuantity: '0' },
+      { available: 10, updatedQuantity: '11' },
+      { available: 10, updatedQuantity: '2.0' },
+      { available: 10, updatedQuantity: '2/3' },
+      { available: -1, updatedQuantity: '-1' },
+      { available: -1, updatedQuantity: '0' },
+      { available: -1, updatedQuantity: 'foo' },
+      { available: -1, updatedQuantity: '2/3' },
+      { available: -1, updatedQuantity: '2.0' },
+      { available: -1, updatedQuantity: '99999999999' },
+    ];
+
+    invalidPools.forEach((pool, i) => {
+      // using object with index attribute to print out index on failure,
+      // jest doesn't support messages on failure :(
+      const result = shallowWrapper.instance().quantityValidation(pool)[0];
+      expect({ index: i, result }).toEqual({ index: i, result: false });
+    });
   });
 });
