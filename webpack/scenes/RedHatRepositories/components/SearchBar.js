a1585a1359003072ddc57b99d23ccd9ace6b6fb4
John Mitsch <jomitsch@redhat.com>
2018-07-20 15:30:44 +0000

    Fixes #24324 - Update eslint commands and fix issues
    This is to prepare our codebase for eslint being added
    to jenkins. Currently we do not run linting tasks for
    code in webpack/ on PRs using jenkins. I've changed
    the tasks to `npm run lint`, which runs eslint normally
    and `npm run lint:fix`, which runs `eslint` with `--fix`.
    I also corrected any eslint issues.
    
    There will be an accompanying PR to foreman-infra to add
    `npm run lint` to the jenkins job, so it runs on PRs.
    
    To test, run `npm run lint` and make sure there are no errors.

--- webpack/scenes/RedHatRepositories/components/SearchBar.js
+++ webpack/scenes/RedHatRepositories/components/SearchBar.js
@@ -97,7 +97,7 @@ class SearchBar extends Component {
         <MultiSelect
           value={this.state.filters}
           options={filterOptions}
-          noneSelectedText={__("Filter by type")}
+          noneSelectedText={__('Filter by type')}
           onChange={(e) => {
             const values = [...e.target.options]
               .filter(({ selected }) => selected)
