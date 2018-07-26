cdf391c1eaec4ef7ba16bedc54b3e40e3ff1d17d
Andrew Kofink <akofink@redhat.com>
2018-07-26 06:18:24 -0400

    Fixes #23886: Add product content to subs detail
    (cherry picked from commit 3fdd5848139237dec6f8b5605984e878500c1d32)

--- webpack/scenes/Subscriptions/Details/SubscriptionDetailReducer.js
+++ webpack/scenes/Subscriptions/Details/SubscriptionDetailReducer.js
@@ -4,14 +4,27 @@ import {
   SUBSCRIPTION_DETAILS_SUCCESS,
   SUBSCRIPTION_DETAILS_FAILURE,
 } from './SubscriptionDetailConstants';
+import {
+  PRODUCTS_REQUEST,
+  PRODUCTS_SUCCESS,
+  PRODUCTS_FAILURE,
+} from '../../Products/ProductConstants';
 
 const initialState = Immutable({
   loading: false,
+  enabledProducts: {
+    results: [],
+    total: 0,
+  },
 });
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case SUBSCRIPTION_DETAILS_REQUEST: {
+      return state.set('loading', true);
+    }
+
+    case PRODUCTS_REQUEST: {
       return state.set('loading', true);
     }
 
@@ -24,7 +37,23 @@ export default (state = initialState, action) => {
       });
     }
 
+    case PRODUCTS_SUCCESS: {
+      const enabledProducts = { enabledProducts: action.response };
+
+      return state.merge({
+        ...enabledProducts,
+        loading: false,
+      });
+    }
+
     case SUBSCRIPTION_DETAILS_FAILURE: {
+      return state.merge({
+        error: action.payload.message,
+        loading: false,
+      });
+    }
+
+    case PRODUCTS_FAILURE: {
       return state.merge({
         error: action.payload.message,
         loading: false,
--- webpack/scenes/Subscriptions/Details/__tests__/SubscriptionDetails.test.js
+++ webpack/scenes/Subscriptions/Details/__tests__/SubscriptionDetails.test.js
@@ -5,7 +5,9 @@ import SubscriptionDetails from '../SubscriptionDetails';
 import SubscriptionDetailAssociations from '../SubscriptionDetailAssociations';
 import SubscriptionDetailInfo from '../SubscriptionDetailInfo';
 import SubscriptionDetailProducts from '../SubscriptionDetailProducts';
+import SubscriptionDetailEnabledProducts from '../SubscriptionDetailEnabledProducts';
 import { loadSubscriptionDetails } from '../SubscriptionDetailActions';
+import { loadProducts } from '../../../Products/ProductActions';
 import { successState } from './subscriptionDetails.fixtures';
 
 jest.mock('../../../../move_to_foreman/foreman_toast_notifications');
@@ -16,12 +18,14 @@ describe('subscriptions details page', () => {
 
     const wrapper = shallow(<SubscriptionDetails
       loadSubscriptionDetails={loadSubscriptionDetails}
+      loadProducts={loadProducts}
       subscriptionDetails={successState}
       match={match}
     />);
     expect(wrapper.find(SubscriptionDetailAssociations)).toHaveLength(1);
     expect(wrapper.find(SubscriptionDetailInfo)).toHaveLength(1);
     expect(wrapper.find(SubscriptionDetailProducts)).toHaveLength(1);
+    expect(wrapper.find(SubscriptionDetailEnabledProducts)).toHaveLength(1);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
