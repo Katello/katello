667ffe10bb525fcd9dc33a2b5f5ae05feea9715c
Andrew Kofink <akofink@redhat.com>
2018-08-01 10:39:18 -0400

    Fixes #24513: Include url_helpers in ReactCtrler

--- app/controllers/katello/react_controller.rb
+++ app/controllers/katello/react_controller.rb
@@ -2,6 +2,8 @@ module Katello
   class ReactController < ::ApplicationController
     skip_before_action :authorize
 
+    include Rails.application.routes.url_helpers
+
     def index
       render 'katello/layouts/react', :layout => false
     end
