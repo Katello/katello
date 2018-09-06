edbf27607f8b35f39a4d7dff62e5d609887fc872
Matthias Dellweg <dellweg@atix.de>
2018-09-06 13:20:14 +0200

    Fixes #24775 - Use REX for errata installation

--- app/lib/actions/katello/host/erratum/install.rb
+++ app/lib/actions/katello/host/erratum/install.rb
@@ -11,16 +11,10 @@ module Actions
             action_subject(host, :hostname => host.name, :errata => errata_ids)
             if Setting['erratum_install_batch_size'] && Setting['erratum_install_batch_size'] > 0
               errata_ids.each_slice(Setting['erratum_install_batch_size']) do |errata_ids_batch|
-                plan_action(Pulp::Consumer::ContentInstall,
-                            consumer_uuid: host.content_facet.uuid,
-                            type:          'erratum',
-                            args:          errata_ids_batch)
+                plan_errata_install(host, errata_ids_batch)
               end
             else
-              plan_action(Pulp::Consumer::ContentInstall,
-                          consumer_uuid: host.content_facet.uuid,
-                          type:          'erratum',
-                          args:          errata_ids)
+              plan_errata_install(host, errata_ids)
             end
             plan_self(:host_id => host.id)
           end
@@ -48,6 +42,24 @@ module Actions
           def finalize
             host = ::Host.find_by(:id => input[:host_id])
             host.update(audit_comment: (_("Installation of errata requested: %{errata}") % {errata: input[:errata].join(", ")}).truncate(255))
+          end
+
+          private
+
+          def plan_errata_install(host, errata_ids)
+            if Setting['remote_execution_by_default']
+              composer = JobInvocationComposer.for_feature(:katello_errata_install,
+                                                           [host.id],
+                                                           :errata => errata_ids.join(','))
+              job_invocation = composer.job_invocation
+              job_invocation.save!
+              plan_action(RemoteExecution::RunHostsJob, job_invocation)
+            else
+              plan_action(Pulp::Consumer::ContentInstall,
+                          consumer_uuid: host.content_facet.uuid,
+                          type:          'erratum',
+                          args:          errata_ids)
+            end
           end
         end
       end
