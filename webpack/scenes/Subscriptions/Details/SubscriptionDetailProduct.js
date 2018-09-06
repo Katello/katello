75e26d47f35a4dec8ba92124e36f8a2969c36cfd
Amir Fefer <amirfefer@gmail.com>
2018-09-06 16:43:33 +0300

    Fixes #24810 - allign text in subscriptions details page

--- webpack/scenes/Subscriptions/Details/SubscriptionDetailProduct.js
+++ webpack/scenes/Subscriptions/Details/SubscriptionDetailProduct.js
@@ -5,20 +5,26 @@ import { Row, Col } from 'patternfly-react';
 const SubscriptionDetailProduct = ({ content }) => (
   <Row key={content.id}>
     <Col sm={12}>
-      <Row><u>{content.name}</u></Row>
+      <Row>
+        <u>{content.name}</u>
+      </Row>
     </Col>
-    <Col sm={3}>
-      <Row>{ __('Content Download URL') }</Row>
-      <Row>{ __('GPG Key URL') }</Row>
-      <Row>{ __('Repo Type') }</Row>
-      <Row>{ __('Enabled?') }</Row>
-    </Col>
-    <Col sm={9}>
-      <Row>{content.content_url}</Row>
-      <Row>{content.gpg_url}</Row>
-      <Row>{content.type}</Row>
-      <Row>{content.enabled ? __('yes') : __('no')}</Row>
-    </Col>
+    <Row>
+      <Col sm={3}>{__('Content Download URL')}</Col>
+      <Col sm={9}>{content.content_url} </Col>
+    </Row>
+    <Row>
+      <Col sm={3}>{__('GPG Key URL')}</Col>
+      <Col sm={9}>{content.gpg_url} </Col>
+    </Row>
+    <Row>
+      <Col sm={3}>{__('Repo Type')}</Col>
+      <Col sm={9}>{content.type} </Col>
+    </Row>
+    <Row>
+      <Col sm={3}>{__('Enabled')}</Col>
+      <Col sm={9}>{content.enabled ? __('yes') : __('no')} </Col>
+    </Row>
   </Row>
 );
 
