e1d924f0b8894fb9539aa3e284a0d510e12a6185
Michael Johnson <micjohns@redhat.com>
2018-09-18 17:41:26 +0000

    Refs #22498 - Improve error messages for content views

--- test/factories/content_view_version_factory.rb
+++ test/factories/content_view_version_factory.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :katello_content_view_version, :class => Katello::ContentViewVersion do
     sequence(:major)
+    sequence(:minor)
     association :content_view, :factory => :katello_content_view
   end
 end
