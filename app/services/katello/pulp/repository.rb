62eeb306c7de10579e9f4b12fc127c77d1d52fd4
Samir Jha <sjha4@ncsu.edu>
2018-10-26 19:14:13 +0000

    Fixes #25318 - Repo sync doesn't pass source url

--- app/services/katello/pulp/repository.rb
+++ app/services/katello/pulp/repository.rb
@@ -12,7 +12,7 @@ module Katello
         sync_options = {}
         sync_options[:max_speed] = SETTINGS.dig(:katello, :pulp, :sync_KBlimit)
         sync_options[:num_threads] = SETTINGS.dig(:katello, :pulp, :sync_threads)
-        sync_options[:feed] = overrides[:source_url] if overrides[:source_url]
+        sync_options[:feed] = overrides[:feed] if overrides[:feed]
         sync_options[:validate] = !SETTINGS.dig(:katello, :pulp, :skip_checksum_validation)
         sync_options.merge!(overrides[:options]) if overrides[:options]
         [::Katello::CapsuleContent.new(@smart_proxy).pulp_server.resources.repository.sync(@repo.pulp_id, override_config: sync_options.compact!)]
