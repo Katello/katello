f803a98863814e4a8d423e0498d40901fcff8b8e
Partha Aji <paji@redhat.com>
2018-10-30 11:40:54 -0400

    Refs #25086 - Fixed  test

--- test/actions/katello/host/upload_profiles_test.rb
+++ test/actions/katello/host/upload_profiles_test.rb
@@ -38,13 +38,15 @@ module Katello::Host
         action = create_action action_class
         action.stubs(:action_subject).with(@host)
 
-        ::Host.expects(:find_by).returns(@host)
+        ::Host.expects(:find_by).returns(@host).at_least_once
         mock_consumer = mock
         mock_consumer.expects(:upload_package_profile)
         ::Katello::Pulp::Consumer.expects(:new).returns(mock_consumer)
-        @host.expects(:import_package_profile).with(rpm_profiles)
+        @host.expects(:import_package_profile).with do |packages|
+          expected_packages = rpm_profiles.map { |prof| ::Katello::Pulp::SimplePackage.new(prof) }
+          packages.map(&:nvra).must_equal(expected_packages.map(&:nvra))
+        end
         @host.expects(:import_enabled_repositories).with(enabled_repos)
-        @host.expects(:import_module_streams).with(modumd_inventory)
 
         plan_action action, @host, profile.to_json
         run_action action
@@ -54,13 +56,12 @@ module Katello::Host
         action = create_action action_class
         action.stubs(:action_subject).with(@host)
 
-        ::Host.expects(:find_by).returns(@host)
+        ::Host.expects(:find_by).returns(@host).at_least_once
         mock_consumer = mock
         mock_consumer.expects(:upload_package_profile).raises(RestClient::ResourceNotFound)
         ::Katello::Pulp::Consumer.expects(:new).returns(mock_consumer)
         @host.expects(:import_package_profile).with(any_parameters).never
         @host.expects(:import_enabled_repositories).with(enabled_repos)
-        @host.expects(:import_module_streams).with(modumd_inventory)
 
         plan_action action, @host, profile.to_json
         run_action action
@@ -81,7 +82,7 @@ module Katello::Host
         action = create_action action_class
         action.stubs(:action_subject).with(@host)
 
-        ::Host.expects(:find_by).returns(@host)
+        ::Host.expects(:find_by).returns(@host).at_least_once
         @host.expects(:content_facet).returns(nil)
         ::Katello::Pulp::Consumer.expects(:new).never
 
@@ -93,13 +94,12 @@ module Katello::Host
         action = create_action action_class
         action.stubs(:action_subject).with(@host)
 
-        ::Host.expects(:find_by).returns(@host)
+        ::Host.expects(:find_by).returns(@host).at_least_once
         mock_consumer = mock
         mock_consumer.expects(:upload_package_profile)
         ::Katello::Pulp::Consumer.expects(:new).returns(mock_consumer)
         @host.expects(:import_package_profile).with(any_parameters).raises(ActiveRecord::InvalidForeignKey)
         @host.expects(:import_enabled_repositories).with(enabled_repos)
-        @host.expects(:import_module_streams).with(modumd_inventory)
 
         plan_action action, @host, profile.to_json
         run_action action
