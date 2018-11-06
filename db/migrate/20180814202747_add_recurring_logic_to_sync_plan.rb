cf08cf38454ec6357f1ab2cf67db11d87760286b
Samir Jha <sjha4@ncsu.edu>
2018-11-06 15:46:10 +0000

    Fixes #25399 - Upgrade fails at db:migrate
    (cherry picked from commit d303f34f699905a8236fe7e0c7e57793e20bb04f)

--- db/migrate/20180814202747_add_recurring_logic_to_sync_plan.rb
+++ db/migrate/20180814202747_add_recurring_logic_to_sync_plan.rb
@@ -1,6 +1,7 @@
 class AddRecurringLogicToSyncPlan < ActiveRecord::Migration[5.1]
   def change
     add_column :katello_sync_plans, :foreman_tasks_recurring_logic_id, :integer
+    add_column :katello_sync_plans, :cron_expression, :string
     add_foreign_key :katello_sync_plans, :foreman_tasks_recurring_logics, :name => "katello_sync_plan_foreman_tasks_recurring_logic_fk", :column => "foreman_tasks_recurring_logic_id"
     Katello::SyncPlan.find_each do |sync_plan|
       User.as_anonymous_admin do
