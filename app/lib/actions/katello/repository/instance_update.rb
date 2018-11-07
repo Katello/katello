cff302e43a05562053337ea95dfd0052a8b4cee3
Justin Sherrill <jsherril@redhat.com>
2018-11-07 16:26:14 -0500

    Fixes #25410 - fix env update with docker repositories

---
+++ app/lib/actions/katello/repository/instance_update.rb
@@ -0,0 +1,21 @@
+module Actions
+  module Katello
+    module Repository
+      class InstanceUpdate < Actions::EntryAction
+        middleware.use Actions::Middleware::KeepCurrentUser
+
+        def plan(repository)
+          action_subject repository
+          repository.save!
+          plan_action(::Actions::Pulp::Repository::Refresh, repository)
+          plan_self(:repository_id => repository.id)
+        end
+
+        def run
+          repository = ::Katello::Repository.find(input[:repository_id])
+          ForemanTasks.async_task(Katello::Repository::MetadataGenerate, repository)
+        end
+      end
+    end
+  end
+end
