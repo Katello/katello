ac0620f2d614d6ad690008f3f5a2d7848972ce6e
Andrew Kofink <akofink@redhat.com>
2018-11-14 09:21:05 -0500

    Fixes #25464: Handle non-unlimited guest subs
    Handle non-unlimited guest subscriptions in the subscriptions page. Do
    not allow editing said rows, and prevent the UI from creating a text box
    when the user hovers.

--- webpack/scenes/Subscriptions/SubscriptionHelpers.js
+++ webpack/scenes/Subscriptions/SubscriptionHelpers.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const filterRHSubscriptions = subscriptions =>
-  subscriptions.filter(sub => sub.available >= 0);
+  subscriptions.filter(sub =>
+    sub.available >= 0 && sub.upstream_pool_id);
 
 export const manifestExists = organization =>
   organization.owner_details && organization.owner_details.upstreamConsumer;
