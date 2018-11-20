09ab65d5d80cd0e0be936f2fea3a0f9154939d29
Samir Jha <sjha4@ncsu.edu>
2018-11-20 19:37:12 +0000

    Fixes #25519 - Enable save button post error

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/products/new/new-sync-plan-modal.controller.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/products/new/new-sync-plan-modal.controller.js
@@ -11,36 +11,40 @@
  *   A controller for creating a new sync plan in a modal.
  */
 angular.module('Bastion.products').controller('NewSyncPlanModalController',
-    ['$scope', '$uibModalInstance', 'SyncPlan', 'SyncPlanHelper',
-        function ($scope, $uibModalInstance, SyncPlan, SyncPlanHelper) {
+    ['$scope', '$uibModalInstance', 'SyncPlan', 'SyncPlanHelper', 'Notification', 'translate',
+        function ($scope, $uibModalInstance, SyncPlan, SyncPlanHelper, Notification, translate) {
             function success(syncPlan) {
+                Notification.setSuccessMessage(translate("Sync Plan saved"));
+                $scope.isWorking = false;
                 $uibModalInstance.close(syncPlan);
             }
 
             function error(response) {
                 var form = SyncPlanHelper.getForm();
-
+                $scope.isWorking = false;
                 angular.forEach(response.data.errors, function (errors, field) {
                     if (form[field]) {
                         form[field].$setValidity('server', false);
                         form[field].$error.messages = errors;
                     } else {
-                        Notification.setErrorMessage("Error saving the Sync Plan: " + " " + errors);
+                        Notification.setErrorMessage(translate("Error saving the Sync Plan: " + " " + errors));
                     }
                 });
             }
 
             $scope.ok = function (syncPlan) {
+                $scope.isWorking = true;
                 SyncPlanHelper.createSyncPlan(syncPlan, success, error);
             };
 
             $scope.cancel = function () {
+                $scope.isWorking = true;
                 $uibModalInstance.dismiss('cancel');
             };
 
             $scope.isFormDisabled = function () {
                 var form = SyncPlanHelper.getForm();
-                return form && !form.$valid;
+                return form && $scope.isWorking;
             };
 
             $scope.intervals = SyncPlanHelper.getIntervals();
