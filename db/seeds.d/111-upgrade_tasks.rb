9a1741ba5c8ef1e19bc6b25fde4a725dc0c3f116
Stephen Benjamin <stephen@redhat.com>
2018-11-30 09:05:58 -0500

    fixes #25518 - fix invalid repos in 3.10 upgrade
    (cherry picked from commit 34d3d132752f7c7c53795b0ed7e85337dcad4e42)

--- db/seeds.d/111-upgrade_tasks.rb
+++ db/seeds.d/111-upgrade_tasks.rb
@@ -4,6 +4,7 @@ UpgradeTask.define_tasks(:katello) do
     {:name => 'katello:correct_puppet_environments', :long_running => true, :skip_failure => true, :always_run => true},
     {:name => 'katello:clean_backend_objects', :long_running => true, :skip_failure => true, :always_run => true},
     {:name => 'katello:upgrades:3.8:clear_checksum_type'},
-    {:name => 'katello:upgrades:3.9:migrate_sync_plans'}
+    {:name => 'katello:upgrades:3.9:migrate_sync_plans'},
+    {:name => 'katello:upgrades:3.10:clear_invalid_repo_credentials'}
   ]
 end
