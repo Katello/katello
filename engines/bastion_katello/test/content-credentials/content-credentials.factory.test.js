6ec5c4eab03420382de21ad2319a15a5c82da190
John Mitsch <jomitsch@redhat.com>
2018-12-11 16:26:07 -0500

    Fixes #25660 - Content credential page search
    * Fixes #25660 - Content credential page search
    
    This fix does many things:
    - Update the rabl template for content credentials to use the
      correct renamed methods.
    - Update the rabl template for content credentials to use root
      repositories. This way the user isn't returned all repositories
      from content views, rather just the library instance.
    - Update the javascript functions to build the product and repo
      list to give to display on the page.
    - Since we don't use the 'repositories' endpoint for gpg keys,
      I added a simple UI filter for the repo and product pages in
      content credential pages.
    
    To test, create gpg keys and add to repos and products. It helps
    if those repos are in content views to ensure that the CV repos
    aren't showing up in the results. Check the content credential
    detail page and ensure the product and repo associations listed
    are correct.
    
    * Refs #25660 - changed to use mapping and added tests
    
    (cherry picked from commit 6eee848da3774cd12cfb0441bab77bd307094efb)

--- engines/bastion_katello/test/content-credentials/content-credentials.factory.test.js
+++ engines/bastion_katello/test/content-credentials/content-credentials.factory.test.js
@@ -1,38 +1,116 @@
-describe('Factory: ContentCredential', function() {
-    var $httpBackend,
-        contentCredentials;
+describe("Factory: ContentCredential", function() {
+  var $httpBackend, contentCredentials, contentCredential;
 
-    beforeEach(module('Bastion.content-credentials', 'Bastion.test-mocks'));
+  beforeEach(module("Bastion.content-credentials", "Bastion.test-mocks"));
 
-    beforeEach(module(function($provide) {
-        contentCredentials = {
-            records: [
-                { name: 'ContentCredential1', id: 1 }
-            ],
-            total: 2,
-            subtotal: 1
-        };
+  beforeEach(
+    module(function($provide) {
+      contentCredential = {
+        name: "fakegpg",
+        content_type: "gpg_key",
+        content: "hello",
+        id: 2,
+        organization_id: 4,
+        organization: { name: "org-4351", label: "org-4351", id: 4 },
+        created_at: "2018-12-04 16:05:47 -0500",
+        updated_at: "2018-12-04 16:05:47 -0500",
+        gpg_key_products: [
+          {
+            id: 1,
+            cp_id: "644602083972",
+            name: "test_product",
+            provider: { name: "Anonymous", id: 17 },
+            repository_count: 5
+          }
+        ],
+        gpg_key_repos: [
+          {
+            id: 5,
+            name: "fakegpg",
+            content_type: "yum",
+            product: { id: 1, cp_id: "644602083972", name: "test_product" }
+          },
+          {
+            id: 1,
+            name: "yum",
+            content_type: "yum",
+            product: { id: 1, cp_id: "644602083972", name: "test_product" }
+          }
+        ],
+        ssl_ca_products: [],
+        ssl_ca_root_repos: [],
+        ssl_client_products: [],
+        ssl_client_root_repos: [],
+        ssl_key_products: [],
+        ssl_key_root_repos: [],
+        permissions: {
+          view_content_credenials: true,
+          edit_content_credenials: true,
+          destroy_content_credenials: true
+        }
+      };
 
-        $provide.value('CurrentOrganization', 'ACME');
-    }));
+      contentCredentials = {
+        records: [
+          contentCredential          
+        ],
+        total: 2,
+        subtotal: 1
+      };
 
-    beforeEach(inject(function($injector) {
-        $httpBackend = $injector.get('$httpBackend');
-        ContentCredential = $injector.get('ContentCredential');
-    }));
+      $provide.value("CurrentOrganization", "ACME");
+    })
+  );
 
-    afterEach(function() {
-        $httpBackend.flush();
-        $httpBackend.verifyNoOutstandingExpectation();
-        $httpBackend.verifyNoOutstandingRequest();
+  beforeEach(inject(function($injector) {
+    $httpBackend = $injector.get("$httpBackend");
+    ContentCredential = $injector.get("ContentCredential");
+  }));
+
+  afterEach(function() {
+    $httpBackend.flush();
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it("provides a way to get a list of repositories", function() {
+    $httpBackend
+      .expectGET("katello/api/v2/content_credentials?organization_id=ACME")
+      .respond(contentCredentials);
+
+    ContentCredential.queryPaged(function(contentCredentials) {
+      expect(contentCredentials.records.length).toBe(1);
     });
+  });
 
-    it('provides a way to get a list of repositorys', function() {
-        $httpBackend.expectGET('katello/api/v2/content_credentials?organization_id=ACME').respond(contentCredentials);
+  it("provide a way to get a single content credential's repositories", function() {
+    $httpBackend
+      .expectGET("katello/api/v2/content_credentials/1/repositories?organization_id=ACME")
+      .respond(contentCredential);
 
-        ContentCredential.queryPaged(function(contentCredentials) {
-            expect(contentCredentials.records.length).toBe(1);
-        });
+    ContentCredential.repositories({id: 1, action: 'repositories'}, function(response) {
+      var results = response.results;
+      expect(results.length).toBe(2);
+      angular.forEach(results, function(result) {
+        expect(result.name).toBeDefined();
+        expect(result.content_type).toBe('yum');
+        expect(result.usedAs).toBe('GPG Key');
+      })
     });
+  });
 
+  it("provide a way to get a single content credential's products", function() {
+    $httpBackend
+      .expectGET("katello/api/v2/content_credentials/1/products?organization_id=ACME")
+      .respond(contentCredential);
+
+    ContentCredential.products({id: 1, action: 'products'}, function(response) {
+      var results = response.results;
+      expect(results.length).toBe(1);
+      angular.forEach(results, function(result) {
+        expect(result.name).toBeDefined();
+        expect(result.usedAs).toBe('GPG Key');
+      })
+    });
+  });
 });
