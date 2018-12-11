9430d1696b411507bbb1c2bed3181692836303d1
John Mitsch <jomitsch@redhat.com>
2018-12-11 16:26:07 -0500

    Fixes #25660 - Content credential page search
    * Fixes #25660 - Content credential page search
    
    This fix does many things:
    - Update the rabl template for content credentials to use the
      correct renamed methods.
    - Update the rabl template for content credentials to use root
      repositories. This way the user isn't returned all repositories
      from content views, rather just the library instance.
    - Update the javascript functions to build the product and repo
      list to give to display on the page.
    - Since we don't use the 'repositories' endpoint for gpg keys,
      I added a simple UI filter for the repo and product pages in
      content credential pages.
    
    To test, create gpg keys and add to repos and products. It helps
    if those repos are in content views to ensure that the CV repos
    aren't showing up in the results. Check the content credential
    detail page and ensure the product and repo associations listed
    are correct.
    
    * Refs #25660 - changed to use mapping and added tests
    
    (cherry picked from commit 6eee848da3774cd12cfb0441bab77bd307094efb)

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/content-credentials/content-credential.factory.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/content-credentials/content-credential.factory.js
@@ -7,77 +7,75 @@
  * @description
  *   Provides a BastionResource for Content Credentials.
  */
-angular.module('Bastion.content-credentials').factory('ContentCredential',
-    ['BastionResource', 'CurrentOrganization',
-    function (BastionResource, CurrentOrganization) {
+angular.module("Bastion.content-credentials").factory("ContentCredential", [
+    "BastionResource",
+    "CurrentOrganization",
+    function(BastionResource, CurrentOrganization) {
+        var repoTypeMap = {
+            "gpg_key_repos": "GPG Key",
+            "ssl_ca_root_repos": "SSL CA Cert",
+            "ssl_client_root_repos": "SSL Client Cert",
+            "ssl_key_root_repos": "SSL Client Key"
+        };
 
-        return BastionResource('katello/api/v2/content_credentials/:id/:action',
-            {id: '@id', 'organization_id': CurrentOrganization},
+        var productTypeMap = {
+            "gpg_key_products": "GPG Key",
+            "ssl_ca_products": "SSL CA Cert",
+            "ssl_client_products": "SSL Client Cert",
+            "ssl_key_products": "SSL Client Key"
+        };
+
+        function appendData(allData, fullCredential, usedAs) {
+            angular.forEach(fullCredential, function(data) {
+                data.usedAs = usedAs;
+                allData.push(data);
+            });
+        }
+
+        function parseContentCredentialData(data, typeMap) {
+            var contentCredential = angular.fromJson(data);
+            var allData = [];
+            _.forOwn(contentCredential, function(value, key) {
+                if (typeMap.hasOwnProperty(key)) {
+                    appendData(allData, value, typeMap[key]);
+                }
+            });
+            return allData;
+        }
+
+        return BastionResource(
+            "katello/api/v2/content_credentials/:id/:action",
+            { "id": "@id", "organization_id": CurrentOrganization },
             {
-                autocomplete: {method: 'GET', isArray: true, params: {id: 'auto_complete_search'}},
-                update: {method: 'PUT'},
-                products: {method: 'GET', transformResponse:
-                    function (data) {
-                        var contentCredential = angular.fromJson(data);
-                        var allProducts = {};
-                        allProducts.length = 0;
-                        allProducts.products = {};
-
-                        function collectProducts(allP, thisP, usedAs) {
-                            var key;
-                            for (key in thisP) {
-                                if (thisP.hasOwnProperty(key)) {
-                                    thisP[key]["used_as"] = usedAs;
-                                    allP.products[allP.length] = thisP[key];
-                                    allP.length++;
-                                }
-                            }
-                        }
-
-                        collectProducts(allProducts, contentCredential.gpg_key_products, "GPG Key");
-                        collectProducts(allProducts, contentCredential.ssl_ca_products, "SSL CA Cert");
-                        collectProducts(allProducts, contentCredential.ssl_client_products, "SSL Client Cert");
-                        collectProducts(allProducts, contentCredential.ssl_key_products, "SSL Client Key");
-
+                autocomplete: {
+                    method: "GET",
+                    isArray: true,
+                    params: { id: "auto_complete_search" }
+                },
+                update: { method: "PUT" },
+                products: {
+                    method: "GET",
+                    transformResponse: function(data) {
+                        var allProducts = parseContentCredentialData(data, productTypeMap);
                         return {
                             total: allProducts.length,
                             subtotal: allProducts.length,
-                            results: allProducts.products
+                            results: allProducts
                         };
                     }
                 },
-                repositories: {method: 'GET', transformResponse:
-                    function (data) {
-                        var contentCredential = angular.fromJson(data);
-                        var allRepos = {};
-                        allRepos.length = 0;
-                        allRepos.repositories = {};
-
-                        function collectRepos(allR, thisR, usedAs) {
-                            var key;
-                            for (key in thisR) {
-                                if (thisR.hasOwnProperty(key)) {
-                                    thisR[key]["used_as"] = usedAs;
-                                    allR.repositories[allR.length] = thisR[key];
-                                    allR.length++;
-                                }
-                            }
-                        }
-
-                        collectRepos(allRepos, contentCredential.gpg_key_repos, "GPG Key");
-                        collectRepos(allRepos, contentCredential.ssl_ca_repos, "SSL CA Cert");
-                        collectRepos(allRepos, contentCredential.ssl_client_repos, "SSL Client Cert");
-                        collectRepos(allRepos, contentCredential.ssl_key_repos, "SSL Client Key");
-
+                repositories: {
+                    method: "GET",
+                    transformResponse: function(data) {
+                        var allRepos = parseContentCredentialData(data, repoTypeMap);
                         return {
                             total: allRepos.length,
                             subtotal: allRepos.length,
-                            results: allRepos.repositories
+                            results: allRepos
                         };
                     }
                 }
             }
         );
-
-    }]
-);
+    }
+]);
