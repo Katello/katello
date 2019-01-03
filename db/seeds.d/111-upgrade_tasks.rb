498b1cfd32381b5a8ea623e81a2833b67d7658d5
Partha Aji <paji@redhat.com>
2019-01-03 18:20:08 -0500

    Refs #25718 - Moved to upgrades

--- db/seeds.d/111-upgrade_tasks.rb
+++ db/seeds.d/111-upgrade_tasks.rb
@@ -5,6 +5,7 @@ UpgradeTask.define_tasks(:katello) do
     {:name => 'katello:clean_backend_objects', :long_running => true, :skip_failure => true, :always_run => true},
     {:name => 'katello:upgrades:3.8:clear_checksum_type'},
     {:name => 'katello:upgrades:3.9:migrate_sync_plans'},
-    {:name => 'katello:upgrades:3.10:clear_invalid_repo_credentials'}
+    {:name => 'katello:upgrades:3.10:clear_invalid_repo_credentials'},
+    {:name => 'katello:upgrades:3.11:import_yum_metadata'}
   ]
 end
