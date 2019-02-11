0f752247afe4c00be788390c0fc78a5a50da190a
Ivan Nečas <inecas@redhat.com>
2019-02-11 17:33:08 +0100

    Fixes #26028 - set foreign key for tasks sync plan explicitly
    Otherwise, destroying fails with `katello_sync_plans.recurring_logic_id
    does not exist`

--- app/models/katello/concerns/recurring_logic_extensions.rb
+++ app/models/katello/concerns/recurring_logic_extensions.rb
@@ -4,7 +4,9 @@ module Katello
       extend ActiveSupport::Concern
 
       included do
-        has_one :sync_plan, :inverse_of => :foreman_tasks_recurring_logic, :class_name => "Katello::SyncPlan", :dependent => :destroy
+        has_one :sync_plan, :inverse_of => :foreman_tasks_recurring_logic,
+                :class_name => "Katello::SyncPlan", :dependent => :destroy,
+                :foreign_key => :foreman_tasks_recurring_logic_id
       end
     end
   end
