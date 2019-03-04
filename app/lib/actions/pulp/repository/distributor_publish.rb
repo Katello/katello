9f90895561db916890dca0252b9803ec2489c938
Justin Sherrill <jsherril@redhat.com>
2019-03-04 13:55:55 -0500

    Fixes #26166 - properly check for cv puppet env

--- app/lib/actions/pulp/repository/distributor_publish.rb
+++ app/lib/actions/pulp/repository/distributor_publish.rb
@@ -5,16 +5,23 @@ module Actions
         middleware.use Actions::Middleware::SkipIfMatchingContent
 
         def plan(repository, smart_proxy, options)
-          plan_self(:repository_id => repository.id, :smart_proxy_id => smart_proxy.id, :options => options, :dependency => options[:dependency])
+          options = {:smart_proxy_id => smart_proxy.id, :options => options, :dependency => options[:dependency]}
+          if repository.is_a?(Katello::ContentViewPuppetEnvironment)
+            options[:content_view_puppet_environment_id] = repository.id
+          else
+            options[:repository_id] = repository.id
+          end
+          plan_self(options)
         end
 
         def invoke_external_task
-          repo = ::Katello::Repository.find_by(:id => input[:repository_id])
-          if repo.nil?
+          if input[:repository_id]
+            repo = ::Katello::Repository.find_by(:id => input[:repository_id])
+          else
             repo = ::Katello::ContentViewPuppetEnvironment.find_by(:id => input[:repository_id]).nonpersisted_repository
           end
-          smart_proxy = ::SmartProxy.find(input[:smart_proxy_id])
-          repo.backend_service(smart_proxy).distributor_publish(input[:options])
+
+          repo.backend_service(smart_proxy(input[:smart_proxy_id])).distributor_publish(input[:options])
         end
 
         def humanized_name
