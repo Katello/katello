67c0117aaa76bf2e19de5911fc6bd440e79df727
Justin Sherrill <jsherril@redhat.com>
2019-03-18 14:24:32 -0400

    Fixes #26395 - track repostiory clear tasks

---
+++ app/lib/actions/pulp/repository/clear.rb
@@ -0,0 +1,18 @@
+module Actions
+  module Pulp
+    module Repository
+      class Clear < Actions::Pulp::AbstractAsyncTask
+        def plan(repo, smart_proxy)
+          plan_self(:repo_id => repo.id, :smart_proxy_id => smart_proxy.id)
+        end
+
+        def invoke_external_task
+          repo = ::Katello::Repository.find(input[:repo_id])
+          ::Katello::RepositoryTypeManager.find(repo.content_type).content_types.map do |type|
+            ::SmartProxy.find(input[:smart_proxy_id]).content_service(type).remove(repo)
+          end
+        end
+      end
+    end
+  end
+end
