a583224d943b7286d26262705a56c931ea7752d3
Evgeni Golov <evgeni@golov.de>
2019-03-19 16:29:17 +0100

    Fixes #26410 - use the right input id when publishing puppet CVs

--- app/lib/actions/pulp/repository/distributor_publish.rb
+++ app/lib/actions/pulp/repository/distributor_publish.rb
@@ -6,7 +6,7 @@ module Actions
 
         def plan(repository, smart_proxy, options)
           options = {:smart_proxy_id => smart_proxy.id, :options => options, :dependency => options[:dependency]}
-          if repository.is_a?(Katello::ContentViewPuppetEnvironment)
+          if repository.is_a?(::Katello::ContentViewPuppetEnvironment)
             options[:content_view_puppet_environment_id] = repository.id
           else
             options[:repository_id] = repository.id
@@ -18,7 +18,7 @@ module Actions
           if input[:repository_id]
             repo = ::Katello::Repository.find_by(:id => input[:repository_id])
           else
-            repo = ::Katello::ContentViewPuppetEnvironment.find_by(:id => input[:repository_id]).nonpersisted_repository
+            repo = ::Katello::ContentViewPuppetEnvironment.find_by(:id => input[:content_view_puppet_environment_id]).nonpersisted_repository
           end
 
           repo.backend_service(smart_proxy(input[:smart_proxy_id])).distributor_publish(input[:options])
