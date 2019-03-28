1c3ecceed7710693aa2629ab1e6b246a24c207fb
James Jeffers <jdjeffers@gmail.com>
2019-03-28 10:39:09 -0400

    Fixes #26241 - pulp3 file repository delete action steps.

---
+++ app/lib/actions/pulp3/orchestration/repository/delete.rb
@@ -0,0 +1,17 @@
+module Actions
+  module Pulp3
+    module Orchestration
+      module Repository
+        class Delete < Pulp3::Abstract
+          def plan(repository, smart_proxy)
+            sequence do
+              plan_action(Actions::Pulp3::Repository::DeleteRemote, repository, smart_proxy)
+              plan_action(Actions::Pulp3::Repository::DeletePublisher, repository, smart_proxy)
+              plan_action(Actions::Pulp3::Repository::Delete, repository, smart_proxy)
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/actions/pulp3/orchestration/file_delete_test.rb
@@ -0,0 +1,28 @@
+require 'katello_test_helper'
+
+class FileDeleteTest < ActiveSupport::TestCase
+  include ::Katello::Pulp3Support
+
+  def setup
+    @master = FactoryBot.create(:smart_proxy, :default_smart_proxy, :with_pulp3)
+    @repo = katello_repositories(:generic_file)
+    @repo.root.update_attributes(:url => 'http://test/test/')
+    ensure_creatable(@repo, @master)
+    ForemanTasks.sync_task(
+      ::Actions::Pulp3::Orchestration::Repository::Create, @repo, @master)
+  end
+
+  def test_delete
+    ForemanTasks.sync_task(
+      ::Actions::Pulp3::Orchestration::Repository::Delete, @repo, @master)
+    @repo.reload
+
+    repo_reference = Katello::Pulp3::RepositoryReference.find_by(
+      :root_repository_id => @repo.root.id,
+      :content_view_id => @repo.content_view.id)
+
+    assert_nil repo_reference
+
+  end
+
+end
