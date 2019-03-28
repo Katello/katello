2a62bfc089ae46ce2f575ca5a1e48c4da0a02907
James Jeffers <jdjeffers@gmail.com>
2019-03-28 14:14:24 -0400

    Fixes #26241 - refactor actions to async tasks

--- app/lib/actions/pulp3/repository/delete_publisher.rb
+++ app/lib/actions/pulp3/repository/delete_publisher.rb
@@ -1,12 +1,12 @@
 module Actions
   module Pulp3
     module Repository
-      class DeletePublisher < Pulp3::Abstract
+      class DeletePublisher < Pulp3::AbstractAsyncTask
         def plan(repository, smart_proxy)
           plan_self(:repository_id => repository.id, :smart_proxy_id => smart_proxy.id)
         end
 
-        def run
+        def invoke_external_task
           repo = ::Katello::Repository.find(input[:repository_id])
           output[:response] = repo.backend_service(smart_proxy).delete_publisher
         end
