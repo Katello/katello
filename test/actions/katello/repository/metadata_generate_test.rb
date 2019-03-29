c3474687c6c41feb7a1e4ad061db22634015dc8c
James Jeffers <jdjeffers@gmail.com>
2019-03-29 13:51:05 -0400

    Fixes #26241 - repairs metadata planned action expectations

--- test/actions/katello/repository/metadata_generate_test.rb
+++ test/actions/katello/repository/metadata_generate_test.rb
@@ -13,6 +13,13 @@ module Actions
     let(:yum_repo2) { katello_repositories(:fedora_17_x86_64_dev) }
     let(:puppet_repo) { katello_repositories(:p_forge) }
     let(:content_view_puppet_env) { katello_content_view_puppet_environments(:library_view_puppet_environment) }
+    let(:action_options) {{
+      :force => false,
+      :matching_content => nil,
+      :source_repository => nil,
+      :dependency => nil,
+      :repository_creation => false
+    }}
 
     before do
       FactoryBot.create(:smart_proxy, :default_smart_proxy)
@@ -23,7 +30,7 @@ module Actions
       plan_action(action, yum_repo)
 
       assert_action_planed_with(action, select_class, pulp_publish_classes, yum_repo, SmartProxy.pulp_master,
-            :force => false, :matching_content => nil, :source_repository => nil, :dependency => nil, :repository_creation => false)
+            action_options)
     end
 
     it 'plans with a content view puppet env' do
@@ -33,23 +40,27 @@ module Actions
       plan_action(action, content_view_puppet_env)
 
       assert_action_planed_with(action, select_class, pulp_publish_classes, content_view_puppet_env, SmartProxy.pulp_master,
-              :force => false, :matching_content => nil, :source_repository => nil, :dependency => nil, :repository_creation => false)
+              action_options)
     end
 
     it 'plans a yum refresh with force true' do
       action = create_action(action_class)
       plan_action(action, yum_repo, :force => true)
 
+      yum_action_options = action_options.clone
+      yum_action_options[:force] = true
       assert_action_planed_with(action, select_class, pulp_publish_classes, yum_repo, SmartProxy.pulp_master,
-            :force => true, :matching_content => nil, :source_repository => nil, :dependency => nil, :repository_creation => false)
+            yum_action_options)
     end
 
     it 'plans a yum refresh with source repo' do
       action = create_action(action_class)
       plan_action(action, yum_repo, :source_repository => yum_repo2)
 
+      yum_action_options = action_options.clone
+      yum_action_options[:source_repository] = yum_repo2
       assert_action_planed_with(action, select_class, pulp_publish_classes, yum_repo, SmartProxy.pulp_master,
-            :force => false, :matching_content => nil, :source_repository => yum_repo2, :dependency => nil, :repository_creation => false)
+            yum_action_options)
     end
   end
 end
