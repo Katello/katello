24da45ab0f4741659d2f1be89b314171feca9c6f
James Jeffers <jdjeffers@gmail.com>
2019-04-02 15:58:58 -0400

    Fixes #26241 - corrects param handling for destroy

--- app/lib/actions/pulp3/orchestration/repository/delete.rb
+++ app/lib/actions/pulp3/orchestration/repository/delete.rb
@@ -3,12 +3,12 @@ module Actions
     module Orchestration
       module Repository
         class Delete < Pulp3::Abstract
-          def plan(repository, smart_proxy)
+          def plan(repository_id, smart_proxy)
             sequence do
-              plan_action(Actions::Pulp3::Repository::DeleteRemote, repository, smart_proxy)
-              plan_action(Actions::Pulp3::Repository::DeletePublisher, repository, smart_proxy)
-              plan_action(Actions::Pulp3::Repository::DeleteDistributions, repository, smart_proxy)
-              plan_action(Actions::Pulp3::Repository::Delete, repository, smart_proxy)
+              plan_action(Actions::Pulp3::Repository::DeleteRemote, repository_id, smart_proxy)
+              plan_action(Actions::Pulp3::Repository::DeletePublisher, repository_id, smart_proxy)
+              plan_action(Actions::Pulp3::Repository::DeleteDistributions, repository_id, smart_proxy)
+              plan_action(Actions::Pulp3::Repository::Delete, repository_id, smart_proxy)
             end
           end
         end
--- app/lib/actions/pulp3/repository/delete_publisher.rb
+++ app/lib/actions/pulp3/repository/delete_publisher.rb
@@ -2,8 +2,8 @@ module Actions
   module Pulp3
     module Repository
       class DeletePublisher < Pulp3::AbstractAsyncTask
-        def plan(repository, smart_proxy)
-          plan_self(:repository_id => repository.id, :smart_proxy_id => smart_proxy.id)
+        def plan(repository_id, smart_proxy)
+          plan_self(:repository_id => repository_id, :smart_proxy_id => smart_proxy.id)
         end
 
         def invoke_external_task
