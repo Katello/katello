240bfb9037b86f11f6be59d8ac15d5eae7196009
sjha4 <sjha4@ncsu.edu>
2019-04-04 03:40:56 +0000

    Fixes #26219 - state control from Rec Logic

--- db/seeds.d/111-upgrade_tasks.rb
+++ db/seeds.d/111-upgrade_tasks.rb
@@ -4,7 +4,6 @@ UpgradeTask.define_tasks(:katello) do
     {:name => 'katello:correct_puppet_environments', :long_running => true, :skip_failure => true, :always_run => true},
     {:name => 'katello:clean_backend_objects', :long_running => true, :skip_failure => true, :always_run => true},
     {:name => 'katello:upgrades:3.8:clear_checksum_type'},
-    {:name => 'katello:upgrades:3.9:migrate_sync_plans'},
     {:name => 'katello:upgrades:3.10:clear_invalid_repo_credentials'},
     {:name => 'katello:upgrades:3.11:import_yum_metadata'},
     {:name => 'katello:upgrades:3.11:update_puppet_repos'},
