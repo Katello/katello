4e0080951376b5f7da58f152d55e427161aeba26
Partha Aji <paji@redhat.com>
2019-04-26 17:37:49 -0400

    Refs #26694 - Update repo URL

--- app/lib/actions/katello/repository/update_redhat_repository.rb
+++ app/lib/actions/katello/repository/update_redhat_repository.rb
@@ -4,6 +4,7 @@ module Actions
       class UpdateRedhatRepository < Actions::EntryAction
         def plan(repo)
           action_subject repo
+          repo.root.update_attributes!(:url => upstream_url(repo)) if repo.library_instance?
           repo.update_attributes!(relative_path: relative_path(repo))
           plan_action(::Actions::Pulp::Repository::Refresh, repo)
           plan_self(:repository_id => repo.id)
@@ -16,12 +17,20 @@ module Actions
 
         private
 
-        def relative_path(repo)
+        def path(repo)
           path = repo.content.content_url
           repo.root.substitutions.each do |key, value|
             path = path.gsub("$#{key}", value) if value
           end
-          ::Katello::Glue::Pulp::Repos.repo_path_from_content_path(repo.environment, path)
+          path
+        end
+
+        def relative_path(repo)
+          ::Katello::Glue::Pulp::Repos.repo_path_from_content_path(repo.environment, path(repo))
+        end
+
+        def upstream_url(repo)
+          repo.product.repo_url(path(repo))
         end
       end
     end
