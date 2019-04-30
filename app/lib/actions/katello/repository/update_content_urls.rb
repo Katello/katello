fb945572350004afd190e2af3a8b8af4bda77d72
Partha Aji <paji@redhat.com>
2019-04-30 11:58:38 -0400

    Refs #26694 - update pr

--- app/lib/actions/katello/repository/update_content_urls.rb
+++ app/lib/actions/katello/repository/update_content_urls.rb
@@ -4,8 +4,10 @@ module Actions
       class UpdateContentUrls < Actions::EntryAction
         def plan(content_to_update)
           concurrence do
-            content_to_update.flat_map(&:repositories).each do |repo|
-              plan_action(Katello::Repository::UpdateRedhatRepository, repo)
+            content_to_update.each do |content|
+              content.repositories.each do |repo|
+                plan_action(Katello::Repository::UpdateRedhatRepository, repo)
+              end
             end
           end
         end
