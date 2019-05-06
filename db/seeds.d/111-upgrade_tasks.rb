6da1946cee182c40776eea8ed1980bd320e847b0
Jonathon Turel <jturel@gmail.com>
2019-05-06 17:40:05 +0000

    Fixes #25823 - Unable to retrieve GPG keys through smart proxy
    (cherry picked from commit 81f1a08b907fa4b2ae6e743cadb7d459b0813b42)
    
    Conflicts:
    	db/seeds.d/111-upgrade_tasks.rb

--- db/seeds.d/111-upgrade_tasks.rb
+++ db/seeds.d/111-upgrade_tasks.rb
@@ -5,6 +5,7 @@ UpgradeTask.define_tasks(:katello) do
     {:name => 'katello:clean_backend_objects', :long_running => true, :skip_failure => true, :always_run => true},
     {:name => 'katello:upgrades:3.8:clear_checksum_type'},
     {:name => 'katello:upgrades:3.10:clear_invalid_repo_credentials'},
+    {:name => 'katello:upgrades:3.10:update_gpg_key_urls'},
     {:name => 'katello:upgrades:3.11:import_yum_metadata'},
     {:name => 'katello:upgrades:3.11:update_puppet_repos'},
     {:name => 'katello:upgrades:3.11:clear_checksum_type', :task_name => 'katello:upgrades:3.8:clear_checksum_type'},
