5181e54a6e53fa5f66a894e9cb5089fde654deec
Jonathon Turel <jturel@gmail.com>
2019-05-14 20:36:09 +0000

    Fixes #26685 - Show syspurpose info on sub. details

---
+++ webpack/scenes/Subscriptions/Details/SubscriptionPurposeAttributes.js
@@ -0,0 +1,8 @@
+import { translate as __ } from 'foremanReact/common/I18n';
+
+export default {
+  support_level: __('Service Level (SLA)'),
+  usage: __('Usage Type'),
+  role: __('Role'),
+  addons: __('Add ons'),
+};
