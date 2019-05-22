9bdb24c8fad11e1d9e88590eff926e64f9752287
James Jeffers <jdjeffers@gmail.com>
2019-05-22 09:23:59 -0400

    Fixes #26243 - adjust order of action options

--- test/actions/katello/repository/metadata_generate_test.rb
+++ test/actions/katello/repository/metadata_generate_test.rb
@@ -15,8 +15,8 @@ module Actions
     let(:content_view_puppet_env) { katello_content_view_puppet_environments(:library_view_puppet_environment) }
     let(:action_options) do
       { :force => false,
-        :matching_content => nil,
         :source_repository => nil,
+        :matching_content => false,
         :dependency => nil,
         :repository_creation => false
       }
