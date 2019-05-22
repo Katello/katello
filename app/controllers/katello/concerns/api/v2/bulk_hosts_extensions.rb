f92d4fa6dcc87b6bb13d50d87e067dd479197194
Partha Aji <paji@redhat.com>
2019-05-22 15:18:51 -0400

    Refs #26811 - Change sql to ar

--- app/controllers/katello/concerns/api/v2/bulk_hosts_extensions.rb
+++ app/controllers/katello/concerns/api/v2/bulk_hosts_extensions.rb
@@ -27,7 +27,7 @@ module Katello
         end
 
         @hosts = restrict_to.call(@hosts) if restrict_to
-        @hosts = @hosts.where('id not in (?)', bulk_params[:excluded][:ids]) unless bulk_params[:excluded][:ids].blank?
+        @hosts = @hosts.where.not(id: bulk_params[:excluded][:ids]) unless bulk_params[:excluded][:ids].blank?
 
         if bulk_params[:included][:ids].blank? && bulk_params[:included][:search].nil?
           fail HttpErrors::BadRequest, _("No hosts have been specified.")
