6217b0f54be4c39c55c5ff83979693d9a27858da
Michael Moll <kvedulv@kvedulv.de>
2019-05-25 02:16:19 +0200

    wip

--- test/factories/content_view_factory.rb
+++ test/factories/content_view_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :katello_content_view, :class => Katello::ContentView do
     sequence(:name) { |n| "Database#{n}" }
-    description "This content view is for database content"
+    description { "This content view is for database content" }
     association :organization, :factory => :katello_organization
 
     trait :composite do
