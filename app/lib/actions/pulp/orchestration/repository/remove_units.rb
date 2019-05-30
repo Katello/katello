34dd45b6f0268a644ed92ebb9f153ad7eed51981
James Jeffers <jdjeffers@gmail.com>
2019-05-30 13:57:53 -0400

    Fixes #26243 - fixes namespace of pulp action

--- app/lib/actions/pulp/orchestration/repository/remove_units.rb
+++ app/lib/actions/pulp/orchestration/repository/remove_units.rb
@@ -4,9 +4,7 @@ module Actions
       module Repository
         class RemoveUnits < Pulp::Abstract
           def plan(_repository, _smart_proxy, options)
-            sequence do
-              plan_action(Actions::Repository::RemoveUnits, options)
-            end
+            plan_action(Actions::Pulp::Repository::RemoveUnits, options)
           end
         end
       end
