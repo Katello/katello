f1c05c99065f9e44d0a47800a367d6ab435d15d6
Justin Sherrill <jsherril@redhat.com>
2019-06-04 06:36:17 -0400

    Fixes #26947 - correct metadata gen logic
    a logic check was introduced, but during the
    planning phase, the value is actually unresolved
    output from a previous step which is always truthy
    during the plan phase

--- app/lib/actions/katello/repository/metadata_generate.rb
+++ app/lib/actions/katello/repository/metadata_generate.rb
@@ -8,9 +8,13 @@ module Actions
           dependency = options.fetch(:dependency, nil)
           force = options.fetch(:force, false)
           repository_creation = options.fetch(:repository_creation, false)
-          matching_content = options.fetch(:matching_content, false) && !repository_creation
           source_repository = options.fetch(:source_repository, nil)
           source_repository ||= repository.target_repository if repository.link?
+          if repository_creation
+            matching_content = false
+          else
+            matching_content = options.fetch(:matching_content, false)
+          end
 
           plan_pulp_action([Pulp::Repository::DistributorPublish, Pulp3::Orchestration::Repository::GenerateMetadata],
                         repository, SmartProxy.pulp_master,
