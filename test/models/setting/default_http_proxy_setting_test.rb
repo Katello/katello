0dcc6534f372736ee0e3e378c8ff7da3a52cd5e2
James Jeffers <jdjeffers@gmail.com>
2019-06-11 15:44:24 -0400

    Fixes #27009 - adds a default http proxy setting for content

---
+++ test/models/setting/default_http_proxy_setting_test.rb
@@ -0,0 +1,30 @@
+require 'katello_test_helper'
+
+module Katello
+  class DefaultHTTPProxySettingTest < ActiveSupport::TestCase
+
+    class TestAppController < ApplicationController
+    end
+
+    def setup
+      @name = 'content_default_http_proxy'
+    end
+
+    def test_default_setting_accepts_proxy_url
+      setting = Setting.where(name: @name).first
+      FactoryBot.create(:http_proxy)
+      assert setting.valid?
+    end
+
+    def test_collection_children_empty_when_no_proxies_defined
+      children = TestAppController.helpers.send("#{@name}_collection").last[:children]
+      assert_empty children
+    end
+
+    def test_collection_includes_defined_proxy
+      proxy = FactoryBot.create(:http_proxy)
+      children = TestAppController.helpers.send("#{@name}_collection").last[:children]
+      assert_includes children, proxy.url
+    end
+  end
+end
