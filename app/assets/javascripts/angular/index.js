3b04f151eed02eddbc3cc9b85f6e93052a4e10c9
John Mitsch <jomitsch@redhat.com>
2019-06-12 17:43:44 +0000

    Removing Angular blocks from rails side

--- app/assets/javascripts/angular/index.js
+++ app/assets/javascripts/angular/index.js
@@ -1,2 +1,3 @@
+// We load angular-patternfly here instead of webpack as it requires less modification
+// of the wepback config, html includes, and complicated angular-patternfly dependencies.
 //= require angular/angular-patternfly/angular-patternfly
-//= require angular/angular-blocks/angular-blocks
