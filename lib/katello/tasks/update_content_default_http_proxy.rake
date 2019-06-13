54e66d2a3bcb6c79a3963311cc107d81acb0a4d1
James Jeffers <jdjeffers@gmail.com>
2019-06-13 09:57:51 -0400

    Fixes #23037 - rake tasks and tests for http proxy settings

---
+++ lib/katello/tasks/update_content_default_http_proxy.rake
@@ -0,0 +1,22 @@
+require File.expand_path("../engine", File.dirname(__FILE__))
+
+namespace :katello do
+  desc "Sets the content default http proxy to an existing http proxy based on supplied URL."
+  task :update_default_http_proxy, [:proxy_name] => [ :environment] do |task, args|
+    setting = ::Setting::Content.where(name: 'content_default_http_proxy').first
+
+    http_proxy = HttpProxy.where(name: args[:proxy_name]).first
+
+    if http_proxy
+      setting.update_attribute(:value, http_proxy.name)
+      puts "Content default http proxy set to #{http_proxy.name_and_url}."
+    else
+      $stderr.print "No http proxy found with name \"#{args[:proxy_name]}\"."
+    end
+  end
+
+  task http_proxy_list: [:environment] do
+    ::HttpProxy.all.each { |proxy| puts "#{proxy.name_and_url}" }
+  end
+end
+
---
+++ test/lib/tasks/http_proxy_tasks_test.rb
@@ -0,0 +1,46 @@
+require 'katello_test_helper'
+require 'rake'
+
+module Katello
+  class UpdateContentHttpProxyTest < ActiveSupport::TestCase
+    def setup
+      Rake.application.rake_require 'katello/tasks/update_content_default_http_proxy'
+      Rake::Task['katello:update_default_http_proxy'].reenable
+      Rake::Task.define_task(:environment)
+      @setting = Setting::Content.where(name: 'content_default_http_proxy').first
+      assert @setting
+    end
+
+    def test_update_proxy_with_missing_proxy
+      result = Rake.application.invoke_task('katello:update_default_http_proxy[some proxy]')
+      refute_equal "some proxy", @setting.reload.value
+    end
+
+    def test_update_proxy_by_proxy_url_fails
+      current_default_proxy = FactoryBot.create(:http_proxy)
+      @setting.update_attribute(:value, current_default_proxy.name)
+      proxy = FactoryBot.create(:http_proxy)
+      result = Rake.application.invoke_task("katello:update_default_http_proxy[#{proxy.url}]")
+      assert_equal current_default_proxy.name, @setting.reload.value
+    end
+
+    def test_update_proxy_by_proxy_name_sets_default
+      current_default_proxy = FactoryBot.create(:http_proxy)
+      @setting.update_attribute(:value, current_default_proxy.name)
+      proxy = FactoryBot.create(:http_proxy)
+      result = Rake.application.invoke_task("katello:update_default_http_proxy[#{proxy.name}]")
+      assert_equal proxy.name, @setting.reload.value
+    end
+
+    def test_proxy_list_when_no_proxies
+      assert_empty HttpProxy.all.to_a
+      Rake.application.invoke_task("katello:http_proxy_list")
+    end
+
+    def test_proxy_list_with_defined_proxies
+      3.times { FactoryBot.create(:http_proxy) }
+      Rake.application.invoke_task("katello:http_proxy_list")
+    end
+  end
+end
+
