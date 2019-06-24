20c220ada8da20c0a33613c4581515353acf5e15
Nagoor Shaik <nagoor.s@gmail.com>
2019-06-24 13:32:20 +0530

    Fixes #27108 - Show hostname in Tasks for Host actions
    (cherry picked from commit 9a82bb9ee1b89af8b7faef9289c809916dcd8f5f)

--- app/lib/actions/katello/host/package/install.rb
+++ app/lib/actions/katello/host/package/install.rb
@@ -8,7 +8,7 @@ module Actions
           def plan(host, packages)
             Type! host, ::Host::Managed
 
-            action_subject(host, :packages => packages)
+            action_subject(host, :hostname => host.name, :packages => packages)
             plan_action(Pulp::Consumer::ContentInstall,
                         consumer_uuid: host.content_facet.uuid,
                         type:          'rpm',
@@ -17,7 +17,11 @@ module Actions
           end
 
           def humanized_name
-            _("Install package")
+            if input.try(:[], :hostname)
+              _("Install package for %s") % input[:hostname]
+            else
+              _("Install package")
+            end
           end
 
           def humanized_input
