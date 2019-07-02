c6fa1ec1d8b784e23f4e827a9e1b8043e84e8108
ianballou <ianballou67@gmail.com>
2019-07-02 20:17:33 +0000

    Fixes #26996 - pulp 3 docker CRUD

---
+++ app/services/katello/pulp3/repository/docker.rb
@@ -0,0 +1,55 @@
+require 'pulp_docker_client'
+
+module Katello
+  module Pulp3
+    class Repository
+      class Docker < ::Katello::Pulp3::Repository
+        def api_client
+          PulpDockerClient::ApiClient.new(smart_proxy.pulp3_configuration(PulpDockerClient::Configuration))
+        end
+
+        def api_exception_class
+          PulpDockerClient::ApiError
+        end
+
+        def remote_class
+          PulpDockerClient::DockerRemote
+        end
+
+        def remotes_api
+          PulpDockerClient::RemotesDockerApi.new(api_client)
+        end
+
+        def distribution_class
+          PulpDockerClient::DockerDistribution
+        end
+
+        def distributions_api
+          PulpDockerClient::DistributionsDockerApi.new(api_client)
+        end
+
+        def remote_options
+          if root.url.blank?
+            super
+          else
+            options = {url: root.url, upstream_name: root.docker_upstream_name}
+            if root.docker_tags_whitelist && root.docker_tags_whitelist.any?
+              options[:whitelist_tags] = root.docker_tags_whitelist.join(",")
+            else
+              options[:whitelist_tags] = nil
+            end
+            common_remote_options.merge(options)
+          end
+        end
+
+        def distribution_options(path)
+          {
+            base_path: path,
+            repository_version: repo.version_href,
+            name: "#{backend_object_name}"
+          }
+        end
+      end
+    end
+  end
+end
--- lib/katello/repository_types/docker.rb
+++ lib/katello/repository_types/docker.rb
@@ -1,5 +1,8 @@
 Katello::RepositoryTypeManager.register(::Katello::Repository::DOCKER_TYPE) do
   service_class Katello::Pulp::Repository::Docker
+  pulp3_service_class Katello::Pulp3::Repository::Docker
+  pulp3_skip_publication true
+  pulp3_plugin 'pulp_docker'
 
   content_type Katello::DockerManifest, :priority => 1, :pulp2_service_class => ::Katello::Pulp::DockerManifest, :user_removable => true
   content_type Katello::DockerManifestList, :priority => 2, :pulp2_service_class => ::Katello::Pulp::DockerManifestList
