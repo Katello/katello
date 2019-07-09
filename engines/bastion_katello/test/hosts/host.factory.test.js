bc46bff284be3e4bf482a1d941a4b61fb1d7818a
Justin Sherrill <jsherril@redhat.com>
2019-07-09 15:56:07 -0400

    Fixes #27257 - properly check os name for debian

--- engines/bastion_katello/test/hosts/host.factory.test.js
+++ engines/bastion_katello/test/hosts/host.factory.test.js
@@ -15,9 +15,50 @@ describe('Factory: Host', function() {
     }));
 
     afterEach(function() {
-        $httpBackend.flush();
         $httpBackend.verifyNoOutstandingExpectation();
         $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it("determines if its debian with debian os", function() {
+        host = new Host({operatingsystem_name: "Debian"})
+
+        expect(host.isDebEnabled()).toEqual(true)
+    });
+
+    it("determines if its debian with debian os", function() {
+        host = new Host({operatingsystem_name: "Best Debian"})
+
+        expect(host.isDebEnabled()).toEqual(true)
+    });
+
+    it("determines if its debian with debian os", function() {
+        host = new Host({operatingsystem_name: "Best Ubuntu"})
+
+        expect(host.isDebEnabled()).toEqual(true)
+    });
+
+    it("determines if its debian with debian os", function() {
+        host = new Host({operatingsystem_name: "Best Debian"})
+
+        expect(host.isDebEnabled()).toEqual(true)
+    });
+
+    it("determines if its debian with debian os", function() {
+        host = new Host({operatingsystem_name: "Redhat"})
+
+        expect(host.isDebEnabled()).toEqual(false)
+    });
+
+    it("determines if its debian with null os", function() {
+        host = new Host({operatingsystem_name: null})
+
+        expect(host.isDebEnabled()).toEqual(false)
+    });
+
+    it("determines if its debian with undefined os", function() {
+        host = new Host({})
+
+        expect(host.isDebEnabled()).toEqual(false)
     });
 
     it("provides a way to update the host's host collections", function() {
@@ -26,10 +67,12 @@ describe('Factory: Host', function() {
         $httpBackend.expectPUT('/api/v2/hosts/1/host_collections').respond(host);
 
         Host.updateHostCollections({id: 1}, {"host_collection_ids": [1,2]});
+        $httpBackend.flush();
     });
 
     it("provides a way to get the host index via a post", function() {
         $httpBackend.expectPOST('/api/v2/hosts/post_index').respond({});
         Host.postIndex();
+        $httpBackend.flush();
     });
 });
