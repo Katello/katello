d41f8c65066439e4643949b781ffbab8968349a8
sjha4 <sjha4@ncsu.edu>
2019-07-18 17:41:50 +0000

    Fixes #27277 - Fix pulp2 file indexing

--- test/services/katello/pulp/repository/file_test.rb
+++ test/services/katello/pulp/repository/file_test.rb
@@ -10,10 +10,30 @@ module Katello
         def setup
           @master = FactoryBot.create(:smart_proxy, :default_smart_proxy)
           @custom = katello_repositories(:generic_file)
+          @repo = katello_repositories(:pulp3_file_1)
+          RepositorySupport.create_and_sync_repo(@repo)
+        end
+
+        def teardown
+          RepositorySupport.destroy_repo(@repo)
         end
       end
 
-      class FileTest < FileBaseTest
+      class FileVCRTest < FileBaseTest
+        def test_sync_index_content
+          @repo.index_content
+          assert_equal @repo.files.count, 3
+        ensure
+          teardown
+        end
+      end
+
+      class FileNonVCRTest < ::ActiveSupport::TestCase
+        def setup
+          @master = FactoryBot.create(:smart_proxy, :default_smart_proxy)
+          @custom = katello_repositories(:generic_file)
+        end
+
         def test_unit_keys
           upload = {'id' => '1', 'size' => '12333', 'checksum' => 'asf23421324', 'name' => 'test'}
           assert_equal [upload.except('id')], @custom.backend_service(@master).unit_keys([upload])
