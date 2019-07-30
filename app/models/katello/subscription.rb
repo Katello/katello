b1a1cb18d58b791c5f4daa1f92e2153f5bfb1eb8
Jeremy Lenz <jlenz@redhat.com>
2019-07-30 18:28:54 -0400

    Fixes #25010: Permissions on Subscriptions page

--- app/models/katello/subscription.rb
+++ app/models/katello/subscription.rb
@@ -3,6 +3,7 @@ module Katello
     include Glue::Candlepin::CandlepinObject
     include Glue::Candlepin::Subscription
     include Katello::Authorization::Subscription
+    include AuthorizeHelper
 
     has_many :pools, :class_name => "Katello::Pool", :inverse_of => :subscription, :dependent => :destroy
 
--- app/views/katello/api/v2/subscriptions/index.json.rabl
+++ app/views/katello/api/v2/subscriptions/index.json.rabl
@@ -2,6 +2,7 @@ object false
 
 extends 'katello/api/v2/common/org_reference'
 extends "katello/api/v2/common/metadata"
+extends 'katello/api/v2/subscriptions/permissions'
 
 child @collection[:results] => :results do
   attributes :quantity_attached
