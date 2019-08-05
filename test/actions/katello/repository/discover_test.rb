332484232b66b7907a8104a19ea97eb697b75c79
Chris Roberts <chrobert@redhat.com>
2019-08-05 16:02:13 -0400

    Refs #27485 - Clear proxy settings for tests

--- test/actions/katello/repository/discover_test.rb
+++ test/actions/katello/repository/discover_test.rb
@@ -13,6 +13,7 @@ module Actions
     let(:action_class) { ::Actions::Katello::Repository::Discover }
 
     def setup
+      SETTINGS[:katello][:cdn_proxy] = {}
       get_organization #ensure we have an org label
     end
 
