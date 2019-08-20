ed8d8cc0c9af1983d7eca8abbec7567c86e5deb6
swetha <sseelaml@redhat.com>
2019-08-20 14:50:11 +0000

    Refs #27643 - Revert registry URL change

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/products/discovery/container-registries.service.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/products/discovery/container-registries.service.js
@@ -11,7 +11,7 @@ angular.module('Bastion.products').service('ContainerRegistries',
     ['translate', function () {
 
         this.registries = {
-            'redhat': { name: 'Red Hat Registry (registry.access.redhat.com)', url: "http://registry.access.redhat.com/" },
+            'redhat': { name: 'Red Hat Registry (registry.redhat.io)', url: "https://registry.redhat.io" },
             'dockerhub': { name: 'Docker Hub', url: "https://index.docker.io",
                                                 createUrl: "https://registry-1.docker.io" },
             'quay': { name: 'Quay', url: "https://quay.io" },
