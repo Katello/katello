c505aca5ea3fdfd7f79ca0f8a9bee7afed720471
Ondrej Prazak <oprazak@redhat.com>
2019-08-28 10:08:31 +0200

    Fixes #27720 - Use DSL to add content host link

--- app/overrides/add_subscription_status.rb
+++
@@ -1,6 +0,0 @@
-# Add link to content-host on the host show page
-Deface::Override.new(:virtual_path => "hosts/show",
-                     :name => "add_system_link_to_host",
-                     :insert_bottom => 'td:first',
-                     :partial => 'overrides/hosts/subscription_link'
-                    )
