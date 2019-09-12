fe05109a68dd3bbed5ae9d23062ad0c4095e9a8a
Oleh Fedorenko <ofedoren@redhat.com>
2019-09-12 11:13:19 +0000

    Fixes #27843 - V2 API ping call

--- app/controllers/katello/api/v2/katello_ping_controller.rb
+++ app/controllers/katello/api/v2/katello_ping_controller.rb
@@ -1,24 +1,23 @@
 module Katello
-  class Api::V2::PingController < Api::V2::ApiController
+  class Api::V2::KatelloPingController < Api::V2::ApiController
     resource_description do
       api_version "v2"
+      api_base_url "/katello/api"
     end
 
     skip_before_action :authorize
     before_action :require_login, :only => [:index]
 
-    api :GET, "/ping", N_("Shows status of system and it's subcomponents")
+    api :GET, "/ping", N_("Shows status of Katello system and it's subcomponents")
     description N_("This service is only available for authenticated users")
     def index
-      respond_for_show :resource => Ping.ping
+      respond_for_show :resource => Katello::Ping.ping
     end
 
     api :GET, "/status", N_("Shows version information")
     description N_("This service is available for unauthenticated users")
     def server_status
-      status = { :version => Katello::VERSION,
-                 :timeUTC => Time.now.getutc }
-      respond_for_show :resource => status, :template => "server_status"
+      respond_for_show :resource => Katello::Ping.status, :template => "server_status"
     end
   end
 end
