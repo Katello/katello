b9c38bf5d9c8bc4a5444760723132c49daf0b541
Nagoor Shaik <nagoor.s@gmail.com>
2019-10-08 22:36:16 +0530

    Fixes #28019 - Allow SafeMode for Content Host Description (#8374)

--- app/models/katello/concerns/host_managed_extensions.rb
+++ app/models/katello/concerns/host_managed_extensions.rb
@@ -279,5 +279,5 @@ end
 
 class ::Host::Managed::Jail < Safemode::Jail
   allow :content_source, :subscription_manager_configuration_url, :rhsm_organization_label,
-        :host_collections
+        :host_collections, :comment
 end
