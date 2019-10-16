7b94366329614bf856b436da9cade1cfef2a0bd5
Oleh Fedorenko <ofedoren@redhat.com>
2019-10-16 17:12:50 +0000

    Fixes #28076 - Ping endpoints apidoc should be explicit

--- app/controllers/katello/api/v2/katello_ping_controller.rb
+++ app/controllers/katello/api/v2/katello_ping_controller.rb
@@ -2,19 +2,20 @@ module Katello
   class Api::V2::KatelloPingController < Api::V2::ApiController
     resource_description do
       api_version "v2"
-      api_base_url "/katello/api"
+      resource_id "ping"
+      api_base_url ""
     end
 
     skip_before_action :authorize
     before_action :require_login, :only => [:index]
 
-    api :GET, "/ping", N_("Shows status of Katello system and it's subcomponents")
+    api :GET, "/katello/api/ping", N_("Shows status of Katello system and it's subcomponents")
     description N_("This service is only available for authenticated users")
     def index
       respond_for_show :resource => Katello::Ping.ping
     end
 
-    api :GET, "/status", N_("Shows version information")
+    api :GET, "/katello/api/status", N_("Shows version information")
     description N_("This service is available for unauthenticated users")
     def server_status
       respond_for_show :resource => Katello::Ping.status, :template => "server_status"
