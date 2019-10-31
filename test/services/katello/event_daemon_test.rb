bcf035770aa8477f01bb55e3dfdea11952b3aad6
Jonathon Turel <jturel@gmail.com>
2019-10-31 13:05:09 +0000

    Refs #27674 - start daemon threads in single worker

--- test/services/katello/event_daemon_test.rb
+++ test/services/katello/event_daemon_test.rb
@@ -11,8 +11,10 @@ module Katello
     end
 
     def setup
+      @lockfile = Tempfile.new
       Katello::EventDaemon.stubs(:services).returns(mock_service: MockService)
       Katello::EventDaemon.stubs(:runnable?).returns(true)
+      Katello::EventDaemon.stubs(:settings).returns(enabled: true, multiprocess: false, lock_file: @lockfile.path)
     end
 
     def test_start_runs_services
@@ -20,7 +22,16 @@ module Katello
 
       Katello::EventDaemon.start
 
+      assert Katello::EventDaemon.started?
       Katello::EventDaemon.stop
+    end
+
+    def test_start_multiprocess_non_worker
+      Katello::EventDaemon.stubs(:settings).returns(enabled: true, multiprocess: true, lock_file: @lockfile.path)
+
+      Katello::EventDaemon.start
+
+      refute Katello::EventDaemon.started?
     end
 
     def test_stop_close_services
