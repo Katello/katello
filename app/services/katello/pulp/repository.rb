1ab835d2c1851085c048202d5c506e355d2e97d5
Partha Aji <paji@redhat.com>
2019-11-12 11:14:17 -0500

    Fixes #28189 - Refresh repo when needed (#8415)
    * Fixes #28189 - Refresh repo when needed
    
    When a manifest refresh is triggered all the RH repos update their
    distributors and importers, even if those repos did not change. This
    commit makes a repo update its importers and distributors only if
    needed.
    
    (cherry picked from commit 1237135e38137300fcefa9990402181e6cf6f013)

--- app/services/katello/pulp/repository.rb
+++ app/services/katello/pulp/repository.rb
@@ -11,8 +11,10 @@ module Katello
       end
 
       def backend_data(force = false)
-        return smart_proxy.pulp_api.extensions.repository.retrieve_with_details(repo.pulp_id) if (repo.pulp_id && force)
-        @backend_data ||= smart_proxy.pulp_api.extensions.repository.retrieve_with_details(repo.pulp_id) if repo.pulp_id
+        if (repo.pulp_id && (force || @backend_data.nil?))
+          @backend_data = smart_proxy.pulp_api.extensions.repository.retrieve_with_details(repo.pulp_id)
+        end
+        @backend_data
       rescue RestClient::ResourceNotFound
         nil
       end
@@ -84,6 +86,10 @@ module Katello
       end
 
       def mirror_needs_updates?
+        needs_update?
+      end
+
+      def needs_update?
         needs_importer_updates? || needs_distributor_updates?
       end
 
@@ -197,6 +203,10 @@ module Katello
 
       def proxy_host_importer_value
         root.ignore_global_proxy ? "" : nil
+      end
+
+      def refresh_if_needed
+        refresh if needs_update?
       end
 
       def refresh
