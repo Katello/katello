2fa20775c94f6f0a417a8f04c06b6097057b40c6
Avi Sharvit <sharvita@gmail.com>
2020-01-09 19:08:54 +0200

    Fixes #28708 - fix enabled repo test

--- webpack/redux/actions/RedHatRepositories/__tests__/enabled.test.js
+++ webpack/redux/actions/RedHatRepositories/__tests__/enabled.test.js
@@ -2,6 +2,7 @@ import thunk from 'redux-thunk';
 import Immutable from 'seamless-immutable';
 import configureMockStore from 'redux-mock-store';
 import { disableRepository, loadEnabledRepos } from '../enabled.js';
+import { mock as mockApi } from '../../../../mockRequest';
 import {
   ENABLED_REPOSITORIES_REQUEST,
   DISABLE_REPOSITORY_REQUEST,
@@ -14,11 +15,13 @@ describe('RedHatRepositories enabled actions', () => {
   describe('disableRepository', () => {
     it('dispatches DISABLE_REPOSITORY_REQUEST', async () => {
       const mockRepo = { // don't need actual values because just checking that the action matches
-        productId: null,
-        contentId: null,
-        basearch: null,
-        releasever: null,
+        productId: 'some-product-id',
+        contentId: 'some-content-id',
+        basearch: 'some-base-search',
+        releasever: 'some-releasever',
       };
+      mockApi.onPut(`/products/${mockRepo.productId}/repository_sets/${mockRepo.contentId}/disable`).reply(200, []);
+
       await store.dispatch(disableRepository(mockRepo));
       expect(store.getActions()).toContainEqual({
         type: DISABLE_REPOSITORY_REQUEST,
@@ -28,6 +31,8 @@ describe('RedHatRepositories enabled actions', () => {
   });
   describe('loadEnabledRepos', () => {
     it('dispatches ENABLED_REPOSITORIES_REQUEST', async () => {
+      mockApi.onGet('/repositories').reply(200, []);
+
       await store.dispatch(loadEnabledRepos());
       expect(store.getActions()).toContainEqual({
         type: ENABLED_REPOSITORIES_REQUEST,
