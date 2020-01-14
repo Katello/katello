1d9f0d0121f2f663153fff8f2b1e6dc2310b7ca8
Chris Roberts <chrobert@redhat.com>
2020-01-14 16:12:05 -0500

    Fixes #28425 - Change service name to Foreman URL
    (cherry picked from commit 11f05ed4805d556028c8ec1e13a8bf37627475a6)

--- app/models/katello/glue/candlepin/repository.rb
+++ app/models/katello/glue/candlepin/repository.rb
@@ -13,14 +13,6 @@ module Katello
       def content
         Katello::Content.find_by(:cp_content_id => self.content_id, :organization_id => self.product.organization_id)
       end
-
-      def yum_gpg_key_url
-        # if the repo has a gpg key return a url to access it
-        if (gpg_key && gpg_key.content.present?)
-          host = SETTINGS[:fqdn]
-          gpg_key_content_api_repository_url(self, :host => host + "/katello", :protocol => 'https')
-        end
-      end
     end
   end
 end
