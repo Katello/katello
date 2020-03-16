6aca5045fb408e41d79b5e768ff1f0cda736d9cc
Marek Hulan <mhulan@redhat.com>
2020-03-16 13:02:27 +0100

    Fixes #29355 - allow CVE id in safe mode

--- app/models/katello/erratum_cve.rb
+++ app/models/katello/erratum_cve.rb
@@ -1,5 +1,9 @@
 module Katello
   class ErratumCve < Katello::Model
     belongs_to :erratum, :inverse_of => :cves, :class_name => 'Katello::Erratum'
+
+    class Jail < ::Safemode::Jail
+      allow :erratum, :cve_id, :href
+    end
   end
 end
