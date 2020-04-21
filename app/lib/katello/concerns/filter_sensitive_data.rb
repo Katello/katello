6c6ae58d303a201fd0df12607f04096fd193a8f1
Jeremy Lenz <jlenz@redhat.com>
2020-04-21 09:31:32 -0400

    Refs #27501 - use [\s\S] in regex so newlines still match

--- app/lib/katello/concerns/filter_sensitive_data.rb
+++ app/lib/katello/concerns/filter_sensitive_data.rb
@@ -10,7 +10,7 @@ module Katello
 
       class_methods do
         def filter_sensitive_data(payload)
-          payload.gsub(/-----BEGIN RSA PRIVATE KEY-----.*-----END RSA PRIVATE KEY-----/, '[private key filtered]')
+          payload.gsub(/-----BEGIN RSA PRIVATE KEY-----[\s\S]*-----END RSA PRIVATE KEY-----/, '[private key filtered]')
         end
       end
     end
