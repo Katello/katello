f92d000fe909ad8f2363caa7e07357258a526861
Jeremy Lenz <jlenz@redhat.com>
2020-05-01 16:00:57 -0400

    Refs #29540 - fix some other tests

--- test/actions/pulp/repository/sync_test.rb
+++ test/actions/pulp/repository/sync_test.rb
@@ -14,21 +14,4 @@ module ::Actions::Pulp::Repository
       assert_equal 18, ::Katello::Pulp::Rpm.ids_for_repository(repo.pulp_id).length
     end
   end
-
-  class BackgroundSyncTest < VCRTestBase
-    def setup
-      FactoryBot.create(:smart_proxy, :default_smart_proxy)
-      super
-    end
-
-    def test_sync
-      repo.root.update!(download_policy: 'background', url: 'http://foo.com/') if repo.yum?
-      output = ForemanTasks.sync_task(::Actions::Pulp::Repository::Sync, :repo_id => repo.id).output
-      download_tasks = output["pulp_tasks"].select { |task| task["tags"].include?("pulp:action:download") }
-
-      assert_equal "background", repo.download_policy
-      assert_empty download_tasks
-      assert_equal 2, output['pulp_tasks'].length
-    end
-  end
 end
