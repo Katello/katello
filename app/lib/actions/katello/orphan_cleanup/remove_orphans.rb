b2e4107b986e67a3fa6cada7b0567b9aa5ed3cb7
Samir Jha <sjha4@ncsu.edu>
2020-05-19 16:01:15 -0400

    Fixes #29595 - Orphaned dangling content in katello fails migration (#8715)

--- app/lib/actions/katello/orphan_cleanup/remove_orphans.rb
+++ app/lib/actions/katello/orphan_cleanup/remove_orphans.rb
@@ -13,6 +13,19 @@ module Actions
                 Actions::Pulp3::Orchestration::OrphanCleanup::RemoveOrphans,
                 proxy)
             end
+            plan_self
+          end
+        end
+
+        def run
+          models = []
+          ::Katello::RepositoryTypeManager.repository_types.each_value do |repo_type|
+            indexable_types = repo_type.content_types_to_index
+            models += indexable_types&.map(&:model_class)
+            models.select! { |model| model.many_repository_associations }
+          end
+          models.each do |model|
+            model.joins("left join katello_#{model.repository_association} on #{model.table_name}.id = katello_#{model.repository_association}.#{model.unit_id_field}").where("katello_#{model.repository_association}.#{model.unit_id_field} IS NULL").destroy_all
           end
         end
       end
