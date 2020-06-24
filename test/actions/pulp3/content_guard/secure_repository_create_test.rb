eedd7d026deb88b0be5a0a61adb9b4c899853df2
Samir Jha <sjha4@ncsu.edu>
2020-06-24 13:26:17 -0400

    Fixes #29753 - Support skipping srpms (#8749)

--- test/actions/pulp3/content_guard/secure_repository_create_test.rb
+++ test/actions/pulp3/content_guard/secure_repository_create_test.rb
@@ -8,14 +8,12 @@ module ::Actions::Pulp3
       @master = FactoryBot.create(:smart_proxy, :default_smart_proxy, :with_pulp3)
       @repo = katello_repositories(:generic_file)
       @repo.root.update(:url => 'http://test/test/', :unprotected => false)
-      cert = File.read(CERT_FIXTURE)
-      Cert::Certs.stubs(:ca_cert).returns(cert)
-      create_repo(@repo, @master)
     end
 
     def test_create
       content_guard = ::Katello::Pulp3::ContentGuard.first
       assert_nil content_guard
+      create_repo(@repo, @master)
       ForemanTasks.sync_task(
           ::Actions::Katello::Repository::MetadataGenerate, @repo)
       @repo.reload
