e5708b4ee8950669060ae2789b26246f085b125c
Jonathon Turel <jturel@gmail.com>
2020-06-30 14:20:17 +0000

    Refs #30129 - remove reference to settings prop

--- webpack/scenes/Subscriptions/SubscriptionsPage.js
+++ webpack/scenes/Subscriptions/SubscriptionsPage.js
@@ -48,7 +48,6 @@ class SubscriptionsPage extends Component {
       loadAvailableQuantities,
       organization,
       pingUpstreamSubscriptions,
-      settings,
       subscriptions,
       task,
     } = this.props;
