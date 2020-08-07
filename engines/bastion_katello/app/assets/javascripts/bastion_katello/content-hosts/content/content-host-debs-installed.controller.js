8b75ade79f78f2f12ff41326d1d67c65d7c7db78
Bernhard Suttner <suttner@atix.de>
2020-08-07 11:53:37 +0200

    Fixes #30811 - Fix package removal
    Use similar functionality like:
    content-host-packages.controller.js
    content-host-packages-installed.controller.js

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/content-hosts/content/content-host-debs-installed.controller.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/content-hosts/content/content-host-debs-installed.controller.js
@@ -8,39 +8,14 @@
  * @requires HostDeb
  * @requires translate
  * @requires Nutupane
- * @requires Notification
  *
  * @description
  *   Provides the functionality for the content host deb packages list and actions.
  */
 angular.module('Bastion.content-hosts').controller('ContentHostDebsInstalledController',
-    ['$scope', '$timeout', '$window', 'HostDeb', 'translate', 'Nutupane', 'BastionConfig', 'Notification',
-    function ($scope, $timeout, $window, HostDeb, translate, Nutupane, BastionConfig, Notification) {
+    ['$scope', '$timeout', '$window', 'HostDeb', 'translate', 'Nutupane',
+    function ($scope, $timeout, $window, HostDeb, translate, Nutupane) {
         var debsNutupane;
-
-        $scope.openEventInfo = function (event) {
-            // when the event has label defined, it means it comes
-            // from foreman-tasks
-            if (event.label) {
-                $scope.transitionTo('content-host.tasks.details', {taskId: event.id});
-            } else {
-                $scope.transitionTo('content-host.events.details', {eventId: event.id});
-            }
-            $scope.working = false;
-        };
-
-        $scope.errorHandler = function (response) {
-            angular.forEach(response.data.errors, function (responseError) {
-                Notification.setErrorMessage(responseError);
-            });
-            $scope.working = false;
-        };
-
-        $scope.working = false;
-        $scope.remoteExecutionPresent = BastionConfig.remoteExecutionPresent;
-        $scope.packageActionFormValues = {
-            authenticityToken: $window.AUTH_TOKEN.replace(/&quot;/g, '')
-        };
 
         $scope.removeSelectedDebs = function () {
             var selected;
@@ -52,21 +27,6 @@ angular.module('Bastion.content-hosts').controller('ContentHostDebsInstalledCont
                 }).join(' ');
                 $scope.performPackageAction('packageRemove', selected);
             }
-        };
-
-        $scope.performPackageAction = function (actionType, term) {
-            $scope.performViaRemoteExecution(actionType, term, false);
-        };
-
-        $scope.performViaRemoteExecution = function (actionType, term, customize) {
-            $scope.packageActionFormValues.package = term;
-            $scope.packageActionFormValues.remoteAction = actionType;
-            $scope.packageActionFormValues.hostIds = $scope.host.id;
-            $scope.packageActionFormValues.customize = customize;
-
-            $timeout(function () {
-                angular.element('#packageActionForm').submit();
-            }, 0);
         };
 
         debsNutupane = new Nutupane(HostDeb, {id: $scope.$stateParams.hostId});
