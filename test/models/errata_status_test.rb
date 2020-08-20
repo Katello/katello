6ed299988785be68f043ffbc52c03ab9c72634fa
Jonathon Turel <jturel@gmail.com>
2020-08-20 18:48:40 +0000

    Fixes #30452 - Improve errata status reporting
    (cherry picked from commit 43074f2f9e8cfe940520c5dde9fa22487bc64a23)

--- test/models/errata_status_test.rb
+++ test/models/errata_status_test.rb
@@ -5,6 +5,7 @@ module Katello
     let(:security_errata) { katello_errata(:security) }
     let(:bugfix_errata) { katello_errata(:bugfix) }
     let(:repo) { katello_repositories(:rhel_6_x86_64) }
+    let(:installed_package) { Katello::InstalledPackage.create(name: 'test-package', nvrea: 'test-package-1.0.x86_64', nvra: 'test-package-1.0.x86_64') }
 
     let(:host) do
       FactoryBot.create(:host, :with_content, :content_view => katello_content_views(:library_dev_view),
@@ -29,20 +30,46 @@ module Katello
 
     def test_to_status_non_security
       host.content_facet.applicable_errata << bugfix_errata
+
       assert_equal Katello::ErrataStatus::NEEDED_ERRATA, status.to_status
+
+      status.refresh!
+      assert_equal "Non-security errata applicable", status.to_label
     end
 
     def test_to_status_no_repos
+      host.installed_packages << installed_package
+
+      status.refresh!
+
+      assert_empty host.content_facet.bound_repositories
       assert_equal Katello::ErrataStatus::UNKNOWN, status.to_status
+      assert_match(/enabled repositories/, status.to_label)
+    end
+
+    def test_to_status_no_packages
+      host.content_facet.bound_repositories << repo
+
+      status.refresh!
+
+      assert_empty host.installed_packages
+      assert_equal Katello::ErrataStatus::UNKNOWN, status.to_status
+      assert_match(/installed packages/, status.to_label)
     end
 
     def test_to_status_repos_no_errata
       host.content_facet.bound_repositories << repo
+      host.installed_packages << installed_package
+
       assert_equal Katello::ErrataStatus::UP_TO_DATE, status.to_status
+      assert_equal "All errata applied", status.to_label
     end
 
     def test_no_content_facet
-      assert_equal Katello::ErrataStatus::UNKNOWN, FactoryBot.build(:host).get_status(Katello::ErrataStatus).to_status
+      host.content_facet.destroy
+      host.reload
+
+      refute status.relevant?
     end
 
     def test_to_global
@@ -54,9 +81,11 @@ module Katello
       Setting['errata_status_installable'] = true
       host.content_facet.bound_repositories << repo
       host.content_facet.applicable_errata << bugfix_errata
-      ::Katello::Host::ContentFacet.any_instance.expects(:installable_errata).returns(Erratum.where("0=1"))
+      host.installed_packages << installed_package
+      host.content_facet.expects(:installable_errata).returns(Erratum.none)
 
       assert_equal Katello::ErrataStatus::UP_TO_DATE, status.to_status
+      assert_equal "All errata applied", status.to_label
     end
   end
 end
