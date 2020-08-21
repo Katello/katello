0dae7f443131d06ec7e28c755c410cab7e625e43
Jeremy Lenz <jlenz@redhat.com>
2020-08-21 14:27:33 -0400

    Refs #29926 - add tests

--- test/controllers/foreman/organizations_controller_test.rb
+++ test/controllers/foreman/organizations_controller_test.rb
@@ -29,4 +29,30 @@ class OrganizationsControllerTest < ActionController::TestCase
     assert_redirected_to step2_organization_path(:id => org.to_param)
     assert_equal session[:organization_id], org.id
   end
+
+  def test_simple_content_access_toggle
+    # use org with SCA enabled
+    org = Organization.first
+    Organization.any_instance.stubs(:simple_content_access?).returns true
+    TaxHost.any_instance.stubs(:check_for_orphans).returns false
+    # assert correct task will be started
+    @controller.expects(:async_task).with do |action_class, _org|
+      action_class == ::Actions::Katello::Organization::SimpleContentAccess::Disable
+    end
+    # send request to disable SCA
+    put :update, params: { id: org.id, simple_content_access: false }
+  end
+
+  def test_simple_content_access_unchanged
+    # use org with SCA enabled
+    org = Organization.first
+    Organization.any_instance.stubs(:simple_content_access?).returns true
+    TaxHost.any_instance.stubs(:check_for_orphans).returns false
+    # assert SCA task is not initiated
+    @controller.expects(:async_task).with do |action_class, _org|
+      action_class == ::Actions::Katello::Organization::SimpleContentAccess::Disable
+    end.never
+    # update org and don't change SCA
+    put :update, params: { id: org.id, simple_content_access: true }
+  end
 end
