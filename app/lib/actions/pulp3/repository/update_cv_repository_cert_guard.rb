e88b411e70ce5bf88eae7479f5e0834371e61ec9
Justin Sherrill <jsherril@redhat.com>
2020-08-28 16:24:58 -0400

    Fixes #30736 - refactor cert guard updating
    previously when updating the CDN url, a task was triggered
    to update each repository and as part of that to update cert guard
    data.  Due to a small logic bug, this was happening even when it should
    not have been.  But in addition to this, the place that it was triggered
    could lead to tasks being left in pending state.
    
    This refactor moves UpdateCvRepoCertGuard into a orchestration action,
    which will optionally get called if the repo is pulp3.  This means that under
    pulp2, a repo will not have this action triggered for it at all saving some
    computation.
    
    (cherry picked from commit d62fe7d219c482ebfa8837debc6a5e20bbfeaa78)

--- app/lib/actions/pulp3/repository/update_cv_repository_cert_guard.rb
+++ app/lib/actions/pulp3/repository/update_cv_repository_cert_guard.rb
@@ -2,12 +2,16 @@ module Actions
   module Pulp3
     module Repository
       class UpdateCVRepositoryCertGuard < Pulp3::Abstract
-        def plan(repository, _smart_proxy)
+        def plan(repository, smart_proxy)
           root = repository.root
           cv_repositories = root.repositories - [root.library_instance]
           cv_repositories.each do |repo|
-            plan_action(::Actions::Pulp3::Repository::RefreshDistribution, repo, SmartProxy.pulp_master)
+            plan_action(::Actions::Pulp3::Repository::RefreshDistribution, repo, smart_proxy)
           end
+        end
+
+        def humanized_name
+          _("Updating repository authentication configuration")
         end
       end
     end
