f084eaa4e4b6d230b240febd8305e14fc2c81ce5
Justin Sherrill <jsherril@redhat.com>
2020-08-31 11:24:49 -0400

    Fixes #30746 - fix pulp3 cert auth with excon
    faraday adapter, in case the azure plugin is installed
    
    (cherry picked from commit d2b75839705a72a7970f732c7d7159a48125f3fe)

---
+++ test/services/katello/pulp3/api/core_test.rb
@@ -0,0 +1,36 @@
+require 'katello_test_helper'
+require 'support/pulp3_support'
+
+module Katello
+  module Service
+    module Pulp3
+      module Api
+        class CoreTest < ActiveSupport::TestCase
+          include Katello::Pulp3Support
+
+          def setup
+            @master = FactoryBot.create(:smart_proxy, :default_smart_proxy, :with_pulp3)
+          end
+
+          def test_with_excon
+            default = Faraday.default_adapter
+            Faraday.default_adapter = :excon
+
+            assert Katello::Pulp3::Api::Core.new(@master).tasks_api.list(limit: 1)
+          ensure
+            Faraday.default_adapter = default
+          end
+
+          def test_with_net_http
+            default = Faraday.default_adapter
+            Faraday.default_adapter = :net_http
+
+            assert Katello::Pulp3::Api::Core.new(@master).tasks_api.list(limit: 1)
+          ensure
+            Faraday.default_adapter = default
+          end
+        end
+      end
+    end
+  end
+end
