24f5fd100639cbed6d1bd13a1d88bde5f116ea15
Amir Fefer <amirfefer@gmail.com>
2020-09-09 02:21:30 +0300

    Fixes #30804 - Add a default status to selectAPIStatus selector

--- webpack/scenes/ContentViews/ContentViewSelectors.js
+++ webpack/scenes/ContentViews/ContentViewSelectors.js
@@ -3,10 +3,14 @@ import {
   selectAPIError,
   selectAPIResponse,
 } from 'foremanReact/redux/API/APISelectors';
+import { STATUS } from 'foremanReact/constants';
 import CONTENT_VIEWS_KEY from './ContentViewsConstants';
 
-export const selectContentViews = state => selectAPIResponse(state, CONTENT_VIEWS_KEY) || {};
+export const selectContentViews = state =>
+  selectAPIResponse(state, CONTENT_VIEWS_KEY) || {};
 
-export const selectContentViewStatus = state => selectAPIStatus(state, CONTENT_VIEWS_KEY);
+export const selectContentViewStatus = state =>
+  selectAPIStatus(state, CONTENT_VIEWS_KEY) || STATUS.PENDING;
 
-export const selectContentViewError = state => selectAPIError(state, CONTENT_VIEWS_KEY);
+export const selectContentViewError = state =>
+  selectAPIError(state, CONTENT_VIEWS_KEY);
--- webpack/scenes/ContentViews/Details/ContentViewDetailSelectors.js
+++ webpack/scenes/ContentViews/Details/ContentViewDetailSelectors.js
@@ -3,13 +3,14 @@ import {
   selectAPIError,
   selectAPIResponse,
 } from 'foremanReact/redux/API/APISelectors';
+import { STATUS } from 'foremanReact/constants';
 import CONTENT_VIEWS_KEY from '../ContentViewsConstants';
 
 export const selectCVDetails = (state, cvId) =>
   selectAPIResponse(state, `${CONTENT_VIEWS_KEY}_${cvId}`) || {};
 
 export const selectCVDetailStatus =
-  (state, cvId) => selectAPIStatus(state, `${CONTENT_VIEWS_KEY}_${cvId}`);
+  (state, cvId) => selectAPIStatus(state, `${CONTENT_VIEWS_KEY}_${cvId}`) || STATUS.PENDING;
 
 export const selectCVDetailError =
   (state, cvId) => selectAPIError(state, `${CONTENT_VIEWS_KEY}_${cvId}`);
