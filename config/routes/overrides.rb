227ac68c1de20e9a184190251ab9102d1ae734ad
vagrant <vagrant@centos7-katello-devel-stable.example.com>
2020-09-11 17:48:21 +0000

    Fixes #30735 - refactor tracer resolve actions
    (cherry picked from commit 84034dc79b019cff2d0db22162a173adda27f83f)

--- config/routes/overrides.rb
+++ config/routes/overrides.rb
@@ -77,6 +77,7 @@ Foreman::Application.routes.draw do
             match '/bulk/environment_content_view' => 'hosts_bulk_actions#environment_content_view', :via => :put
             match '/bulk/release_version' => 'hosts_bulk_actions#release_version', :via => :put
             match '/bulk/traces' => 'hosts_bulk_actions#traces', :via => :post
+            match '/bulk/resolve_traces' => 'hosts_bulk_actions#resolve_traces', :via => :put
             match '/bulk/available_incremental_updates' => 'hosts_bulk_actions#available_incremental_updates', :via => :post
             match '/bulk/module_streams' => 'hosts_bulk_actions#module_streams', :via => :post
             match '/subscriptions/' => 'host_subscriptions#create', :via => :post
@@ -114,6 +115,9 @@ Foreman::Application.routes.draw do
           end
 
           resources :traces, :only => [:index], :controller => :host_tracer do
+            collection do
+              put :resolve
+            end
           end
         end
       end
