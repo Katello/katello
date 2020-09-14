cf67714c531c7cec166ad4157f18b1170215a218
Amir Fefer <amirfefer@gmail.com>
2020-09-15 02:28:31 +0300

    Refs #30791 - demonstrate tabs addition to the new host details page

---
+++ webpack/components/extensions/HostDetails/Tabs/ContentTab.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmptyPage from 'foremanReact/components/common/EmptyState/EmptyStatePattern';
+
+const contentTab = () => (
+  <EmptyPage
+    icon="cloud-security"
+    header="WIP Content"
+    description="This is a demonstration for adding content to the new host details page"
+  />
+);
+
+export default contentTab;
--- webpack/fills_index.js
+++ webpack/fills_index.js
@@ -3,8 +3,12 @@ import { addGlobalFill } from 'foremanReact/components/common/Fill/GlobalFill';
 import { registerReducer } from 'foremanReact/common/MountingService';
 
 import SystemStatuses from './components/extensions/about';
+import ContentTab from './components/extensions/HostDetails/Tabs/ContentTab';
+import SubscriptionTab from './components/extensions/HostDetails/Tabs/SubscriptionTab';
 import extendReducer from './components/extensions/reducers';
 
 registerReducer('katelloExtends', extendReducer);
 
 addGlobalFill('aboutFooterSlot', '[katello]AboutSystemStatuses', <SystemStatuses key="katello-system-statuses" />, 100);
+addGlobalFill('host-details-page-tabs', 'Content', <ContentTab key="content-tab" />, 100);
+addGlobalFill('host-details-page-tabs', 'Subscription', <SubscriptionTab key="subscription-tab" />, 2000);
