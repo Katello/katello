4aeba80e295dfacc6aea6c86ea1ba8a0496d534a
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-10-10 20:35:51 +0200

    Fixes #31377 - Drop turbolinks override on smart proxy
    Foreman has removed Turbolinks so this override serves no purpose.

--- app/overrides/disable_turbolinks_on_proxies_index.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(
-  :virtual_path => 'smart_proxies/index',
-  :name => 'disable_turbolinks_on_proxies_index',
-  :set_attributes => '.proxy-show',
-  :attributes => {'data-no-turbolink' => 'true'})
