86943a90e0152e42a8c205d0849ae91d1550fa49
Jonathon Turel <jturel@gmail.com>
2020-10-12 22:23:23 +0000

    Refs #31053 - add upgrade rake task to fill metadata

---
+++ lib/katello/tasks/upgrades/3.18/add_cvv_export_history_metadata.rb
@@ -0,0 +1,18 @@
+namespace :katello do
+  namespace :upgrades do
+    namespace '3.18' do
+      task :add_cvv_export_history_metadata => ['environment', 'check_ping'] do
+        smart_proxy = SmartProxy.pulp_primary!
+
+        Katello::ContentViewVersionExportHistory.includes(:content_view_version).find_each do |export_history|
+          export = ::Katello::Pulp3::ContentViewVersion::Export.new(
+            content_view_version: export_history.content_view_version,
+            smart_proxy: smart_proxy
+          )
+
+          export_history.update(metadata: export.generate_metadata)
+        end
+      end
+    end
+  end
+end
