6588c7d3213a3be5c8007214508351be5f1c10f5
Jeremy Lenz <jlenz@redhat.com>
2020-10-20 16:48:39 -0400

    Fixes #24166 - change to text datatype

--- db/migrate/20201008204114_add_required_tags_to_katello_root_repositories.rb
+++ db/migrate/20201008204114_add_required_tags_to_katello_root_repositories.rb
@@ -1,5 +1,5 @@
 class AddRequiredTagsToKatelloRootRepositories < ActiveRecord::Migration[6.0]
   def change
-    add_column :katello_root_repositories, :required_tags, :string
+    add_column :katello_root_repositories, :required_tags, :text
   end
 end
