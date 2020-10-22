8e2e23708c8ed7945d3795800013c6d1aeb908cf
Jonathon Turel <jturel@gmail.com>
2020-10-22 17:58:02 +0000

    Refs #31053 - review feedback + only update nil metadata on upgrade

--- lib/katello/tasks/upgrades/3.18/add_cvv_export_history_metadata.rb
+++ lib/katello/tasks/upgrades/3.18/add_cvv_export_history_metadata.rb
@@ -4,7 +4,7 @@ namespace :katello do
       task :add_cvv_export_history_metadata => ['environment', 'check_ping'] do
         smart_proxy = SmartProxy.pulp_primary!
 
-        Katello::ContentViewVersionExportHistory.includes(:content_view_version).find_each do |export_history|
+        Katello::ContentViewVersionExportHistory.includes(:content_view_version).where(metadata: nil).find_each do |export_history|
           export = ::Katello::Pulp3::ContentViewVersion::Export.new(
             content_view_version: export_history.content_view_version,
             smart_proxy: smart_proxy
