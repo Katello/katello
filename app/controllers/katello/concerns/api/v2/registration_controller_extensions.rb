6f671bb7871b1cb40db9b7eef2a8595d11825e07
Leos Stejskal <lstejska@redhat.com>
2020-11-03 18:03:32 +0100

    Fixes #31050 - Global Registration fails when host not found (#8993)
    Fix for case, when the host is already registered by
    subscription-manager, but does not exists in Foreman.

--- app/controllers/katello/concerns/api/v2/registration_controller_extensions.rb
+++ app/controllers/katello/concerns/api/v2/registration_controller_extensions.rb
@@ -5,7 +5,11 @@ module Katello
 
       def prepare_host
         if params['uuid']
-          @host = Katello::Host::SubscriptionFacet.find_by(uuid: params['uuid']).host
+          @host = Katello::Host::SubscriptionFacet.find_by(uuid: params['uuid'])&.host
+          if @host.nil?
+            msg = N_("Host was not found by the subscription UUID: '%s', this can happen if the host is registered already, but not to this Foreman") % params['uuid']
+            fail ActiveRecord::RecordNotFound, msg
+          end
           @host.assign_attributes(host_params('host'))
           @host.save!
         else
