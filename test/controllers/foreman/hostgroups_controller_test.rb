2b20ec1fa2b9f8ae4a1b0adcf4f41003f7731c75
Justin Sherrill <jsherril@redhat.com>
2020-12-22 11:07:59 -0500

    Fixes #31544 - properly check for content source on new hostgroup
    (cherry picked from commit 1a4e20ce4cfb4e8118c86d57bd93080aecc9f731)

--- test/controllers/foreman/hostgroups_controller_test.rb
+++ test/controllers/foreman/hostgroups_controller_test.rb
@@ -32,4 +32,29 @@ class HostgroupsControllerTest < ActionController::TestCase
     assert_equal 1, ::Hostgroup.unscoped.where(:name => "foobar").count
     assert_response 302
   end
+
+  def test_create_with_ks_repo
+    repo = katello_repositories(:fedora_17_x86_64)
+    smart_proxy = FactoryBot.create(:smart_proxy, :default_smart_proxy, :with_pulp3)
+
+    os = Redhat.find_or_create_operating_system(repo)
+    arch = Architecture.where(:name => repo.distribution_arch).first_or_create!
+    os.architectures << arch unless os.architectures.include?(arch)
+
+    post :create, params: {
+      :hostgroup => {
+        :name => "foobar",
+        :architecture_id => arch.id,
+        :operatingsystem_id => os.id,
+        :content_view_id => repo.content_view.id,
+        :lifecycle_environment_id => repo.environment.id,
+        :content_source_id => smart_proxy.id,
+        :kickstart_repository_id => repo.id
+      }
+
+    }
+
+    assert_equal 1, ::Hostgroup.unscoped.where(:name => "foobar").count
+    assert_response 302
+  end
 end
