8621f2cd1d71a220632b30c65caf52b3ff6c7299
Jeremy Lenz <jlenz@redhat.com>
2020-12-23 15:05:39 -0500

    Refs #31550 - Add Timer class to Katello::Logging

--- app/lib/katello/logging.rb
+++ app/lib/katello/logging.rb
@@ -9,6 +9,33 @@ module Katello
       data_string = data.map { |k, v| "#{k}=#{v}" }.join(' ')
 
       logger.send(level, "#{message} #{data_string}")
+      "#{message} #{data_string}"
+    end
+
+    class Timer
+      def initialize(key = "default")
+        @key = key
+        @start_time = Time.now
+        @@timers ||= {}
+        @@timers[key] = self
+      end
+
+      def start
+        Rails.logger.info "Timer #{@key} starting at #{Time.now}"
+        @start_time = Time.now
+        self
+      end
+
+      def stop
+        duration = (Time.now - @start_time).truncate(2)
+        @start_time = nil
+        Rails.logger.info "Timer #{@key} stopping at #{Time.now}: #{duration} sec"
+        "Timer #{@key} stopping at #{Time.now}: #{duration} sec"
+      end
+
+      def self.find_by_key(key)
+        @@timers[key]
+      end
     end
   end
 end
