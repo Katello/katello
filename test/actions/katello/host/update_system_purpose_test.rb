904aa13f69d103435cb94f08c9a6287a7201b2d1
Jonathon Turel <jturel@gmail.com>
2021-01-07 15:02:56 +0000

    Fixes #31591 - Ensure Candlepin is updated when updating system purpose in bulk
    (cherry picked from commit f15e30213887249ba25b413abd6e71f1a27a325d)

--- test/actions/katello/host/update_system_purpose_test.rb
+++ test/actions/katello/host/update_system_purpose_test.rb
@@ -19,17 +19,19 @@ module Katello::Host
     end
 
     def test_update_system_purpose
+      @host.expects(:update_candlepin_associations)
+
       plan_action @action, @host, @service_level, @purpose_role, @purpose_usage, @purpose_addons
 
       assert_equal(@host.subscription_facet.service_level, @service_level)
       assert_equal(@host.subscription_facet.purpose_role, @purpose_role)
       assert_equal(@host.subscription_facet.purpose_usage, @purpose_usage)
-      assert_equal(@host.subscription_facet.purpose_addons.count, 2)
-      assert_equal(@host.subscription_facet.purpose_addons.first.name, 'Addon One')
-      assert_equal(@host.subscription_facet.purpose_addons.second.name, 'Addon Two')
+      assert_equal(['Addon One', 'Addon Two'], @host.subscription_facet.purpose_addons.pluck(:name))
     end
 
     def test_dont_clear_host_values_on_nil_params
+      @host.expects(:update_candlepin_associations)
+
       @host.subscription_facet.purpose_addons << katello_purpose_addons(:addon)
       @host.subscription_facet.service_level = @service_level
       @host.subscription_facet.purpose_role = @purpose_role
@@ -44,6 +46,8 @@ module Katello::Host
     end
 
     def test_unset_value_with_empty_string_array
+      @host.expects(:update_candlepin_associations)
+
       @host.subscription_facet.purpose_addons << katello_purpose_addons(:addon)
       @host.subscription_facet.service_level = @service_level
       @host.subscription_facet.purpose_role = @purpose_role
