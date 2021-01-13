240998f2f28ad96ad909a93cbb6a6010812835ad
Eric D. Helms <ericdhelms@gmail.com>
2021-01-13 15:11:16 -0500

    Fixes #31624: host object may not always have a content facet
    (cherry picked from commit c1c9043fd67e3b0798d20c8dbf189435039acfe8)

--- app/models/katello/concerns/redhat_extensions.rb
+++ app/models/katello/concerns/redhat_extensions.rb
@@ -44,7 +44,7 @@ module Katello
 
       def kickstart_repos(host)
         distros = distribution_repositories(host).where(distribution_bootable: true)
-        if distros && host.content_facet.content_source
+        if distros && host&.content_facet&.content_source
           distros.map { |distro| distro.to_hash(host.content_facet.content_source) }
         else
           []
