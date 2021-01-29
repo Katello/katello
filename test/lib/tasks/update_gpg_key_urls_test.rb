6633fbae367808462a3c30f73f72272d4ce17495
Justin Sherrill <jsherril@redhat.com>
2021-01-29 15:32:27 -0500

    Update GPG URL test

--- test/lib/tasks/update_gpg_key_urls_test.rb
+++ test/lib/tasks/update_gpg_key_urls_test.rb
@@ -9,6 +9,7 @@ module Katello
       Rake::Task['katello:upgrades:3.10:update_gpg_key_urls'].reenable
       Rake::Task['katello:check_ping'].reenable
       Rake::Task.define_task(:environment)
+      Rake::Task.define_task('dynflow:client')
     end
 
     def test_update_gpg_key_urls
