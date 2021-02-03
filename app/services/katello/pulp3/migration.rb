ddbd8eeeacccc0b24e869a232039509e73b5a2f6
Ian Ballou <ianballou67@gmail.com>
2021-02-03 22:56:26 +0000

    Fixes #31654 - Support content migration reset
    (cherry picked from commit e3b7ca51ee0725a16220413700f1edc1838affdb)

--- app/services/katello/pulp3/migration.rb
+++ app/services/katello/pulp3/migration.rb
@@ -114,6 +114,44 @@ module Katello
         Katello::Pulp3::MigrationPlan.new(@repository_types).generate.as_json
       end
 
+      def reset
+        if @repository_types.empty?
+          fail ::Katello::Errors::Pulp3MigrationError, 'There are no Pulp 3 content types to reset'
+        end
+
+        plugins = @repository_types.sort.map do |repository_type|
+          {
+            type: ::Katello::Pulp3::MigrationPlan.pulp2_repository_type(repository_type)
+          }
+        end
+        plan = { plugins: plugins }
+
+        # TODO: Don't provide the plan as a string once this is resolved: https://pulp.plan.io/issues/8211
+        migration_plan_api.reset(migration_plan_api.create(plan: plan).pulp_href, plan.to_json)
+
+        content_types_for_migration.each do |content_type|
+          if content_type.model_class == ::Katello::Erratum
+            ::Katello::RepositoryErratum.update_all(erratum_pulp3_href: nil)
+          else
+            content_type.model_class.update_all(migrated_pulp3_href: nil)
+          end
+        end
+
+        @repository_types.each do |repo_type|
+          if repo_type == "file"
+            ::Katello::Repository.file_type.update(remote_href: nil, publication_href: nil, version_href: nil)
+          elsif repo_type == "docker"
+            ::Katello::Repository.docker_type.update(remote_href: nil, publication_href: nil, version_href: nil)
+          elsif repo_type == "yum"
+            ::Katello::Repository.yum_type.update(remote_href: nil, publication_href: nil, version_href: nil)
+          end
+        end
+
+        ::Katello::Pulp3::RepositoryReference.destroy_all
+        ::Katello::Pulp3::DistributionReference.destroy_all
+        ::Katello::Pulp3::ContentGuard.destroy_all
+      end
+
       def create_migrations
         plan = migration_plan
         Rails.logger.info("Migration Plan: #{plan}")
