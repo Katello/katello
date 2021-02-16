86e06f92d4c2af090463bfaef9018e8277dabc54
James Jeffers <jjeffers@redhat.com>
2021-02-16 15:06:41 -0500

    Refs #31785 - rubocop warning

--- app/services/katello/pulp3/erratum.rb
+++ app/services/katello/pulp3/erratum.rb
@@ -36,7 +36,7 @@ module Katello
         custom_json["updated"] = convert_date_if_epoch(custom_json["updated"]) unless custom_json["updated"].blank?
 
         if model.updated.blank? ||
-	    (custom_json['updated'] && (custom_json['updated'].to_datetime != model.updated.to_datetime))
+            (custom_json['updated'] && (custom_json['updated'].to_datetime != model.updated.to_datetime))
           custom_json['errata_id'] = custom_json.delete('id')
           custom_json['errata_type'] = custom_json.delete('type')
           custom_json['updated'] = custom_json['updated'].blank? ? custom_json['issued'] : custom_json['updated']
