05618e42ef313b0dbbb861a77f9014af713e5241
Jonathon Turel <jturel@gmail.com>
2021-02-17 19:03:54 -0500

    Fixes #31880 - correctly use SSL for qpid connections (#9166)
    * Fixes #31880 - correctly use SSL for qpid connections
    
    * Fixes #31880 - fixup

--- app/lib/katello/agent/connection.rb
+++ app/lib/katello/agent/connection.rb
@@ -1,23 +1,37 @@
 module Katello
   module Agent
     class Connection
+      def initialize
+        @connection = ::Katello::Qpid::Connection.new(
+          url: settings[:broker_url],
+          ssl_cert_file: settings[:broker_ssl_cert_file],
+          ssl_key_file: settings[:broker_ssl_key_file],
+          ssl_ca_file: settings[:broker_ssl_ca_file]
+        )
+      end
+
       def send_messages(messages)
-        connection = ::Katello::Qpid::Connection.new(settings[:broker_url])
-        connection.send_messages(messages)
+        @connection.send_messages(messages)
       end
 
       def fetch_agent_messages(handler = ClientMessageHandler)
-        connection = ::Katello::Qpid::Connection.new(settings[:broker_url])
-        connection.receive_messages(address: settings[:event_queue_name], handler: handler)
+        @connection.receive_messages(address: settings[:event_queue_name], handler: handler)
       end
 
       def delete_client_queue(queue_name)
-        connection = ::Katello::Qpid::Connection.new(settings[:broker_url])
-        connection.delete_queue(queue_name)
+        @connection.delete_queue(queue_name)
       end
 
       def settings
         SETTINGS[:katello][:agent]
+      end
+
+      def open?
+        @connection.open?
+      end
+
+      def close
+        @connection.close
       end
     end
   end
