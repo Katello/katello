995216c778afaea9717829fd8ad50a4e291b0a6e
Jeremy Lenz <jlenz@redhat.com>
2021-02-19 12:17:44 -0500

    Refs #31929 - update angular

--- engines/bastion/vendor/assets/javascripts/bastion/angular-route/angular-route.js
+++ engines/bastion/vendor/assets/javascripts/bastion/angular-route/angular-route.js
@@ -1,6 +1,6 @@
 /**
- * @license AngularJS v1.7.9
- * (c) 2010-2018 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.8.2
+ * (c) 2010-2020 Google LLC. http://angularjs.org
  * License: MIT
  */
 (function(window, angular) {'use strict';
@@ -101,7 +101,7 @@ var noop;
 /* global -ngRouteModule */
 var ngRouteModule = angular.
   module('ngRoute', []).
-  info({ angularVersion: '1.7.9' }).
+  info({ angularVersion: '1.8.2' }).
   provider('$route', $RouteProvider).
   // Ensure `$route` will be instantiated in time to capture the initial `$locationChangeSuccess`
   // event (unless explicitly disabled). This is necessary in case `ngView` is included in an
