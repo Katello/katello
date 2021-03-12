ba5de2bd308b2bd265ad03a4957c36997feb6316
Chris Roberts <chrobert@redhat.com>
2021-03-12 11:11:30 -0500

    Fixes #32007 - remove old pulp2-based upgrade tasks
    (cherry picked from commit d415fe0a7c25ff2b682a351450cd4e8d757e150d)

--- db/seeds.d/111-upgrade_tasks.rb
+++ db/seeds.d/111-upgrade_tasks.rb
@@ -3,18 +3,6 @@ UpgradeTask.define_tasks(:katello) do
     {:name => 'katello:correct_repositories', :long_running => true, :skip_failure => true, :always_run => true},
     {:name => 'katello:correct_puppet_environments', :long_running => true, :skip_failure => true, :always_run => true},
     {:name => 'katello:clean_backend_objects', :long_running => true, :skip_failure => true, :always_run => true},
-    {:name => 'katello:upgrades:3.8:clear_checksum_type'},
-    {:name => 'katello:upgrades:3.10:clear_invalid_repo_credentials'},
-    {:name => 'katello:upgrades:3.10:update_gpg_key_urls'},
-    {:name => 'katello:upgrades:3.11:import_yum_metadata'},
-    {:name => 'katello:upgrades:3.11:update_puppet_repos'},
-    {:name => 'katello:upgrades:3.11:clear_checksum_type', :task_name => 'katello:upgrades:3.8:clear_checksum_type'},
-    {:name => 'katello:upgrades:3.12:remove_pulp2_notifier'},
-    {:name => 'katello:upgrades:3.13:republish_deb_metadata'},
-    {:name => 'katello:upgrades:3.15:set_sub_facet_dmi_uuid'},
-    {:name => 'katello:upgrades:3.15:reindex_rpm_modular'},
-    {:name => 'katello:upgrades:3.16:update_applicable_el8_hosts'},
-    {:name => 'katello:upgrades:3.18:add_cvv_export_history_metadata'},
     {:name => 'katello:upgrades:4.0:remove_ostree_puppet_content'}
   ]
 end
