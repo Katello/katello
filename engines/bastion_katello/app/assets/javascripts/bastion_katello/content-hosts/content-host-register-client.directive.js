a304c88315514a4912c3a01ceea337e5a8910aff
Stejskal Leos <github@stejskalleos.cz>
2021-03-16 08:57:48 +0000

    Fixes #32106 - Default registration method
    Make 'Global Registration' default method for host registration and
    remove old Content host registration page
    'Register content host' button redirects to Global Registration page

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/content-hosts/content-host-register-client.directive.js
+++
@@ -1,6 +0,0 @@
-angular.module('Bastion.content-hosts').directive('registerClient', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'content-hosts/views/register-client.html'
-    };
-});
