da2416251a1ed8ffa09099be577e1a2eeb2d1763
Jeremy Lenz <jlenz@redhat.com>
2021-03-24 17:03:40 -0400

    Refs #32039 - fix truncated migration name

--- db/migrate/20210319123300_add_foreign_keys_to_import_export_histories.rb
+++ db/migrate/20210319123300_add_foreign_keys_to_import_export_histories.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToImportExportHistories < ActiveRecord::Migration[6.0]
   def change
-    add_foreign_key "katello_content_view_version_export_histories", "katello_content_view_versions", column: "content_view_version_id", name: "content_view_version_export_histories_content_view_version_id_fk"
-    add_foreign_key "katello_content_view_version_import_histories", "katello_content_view_versions", column: "content_view_version_id", name: "content_view_version_import_histories_content_view_version_id_fk"
+    add_foreign_key "katello_content_view_version_export_histories", "katello_content_view_versions", column: "content_view_version_id", name: "cvv_export_histories_cvv_id_fk"
+    add_foreign_key "katello_content_view_version_import_histories", "katello_content_view_versions", column: "content_view_version_id", name: "cvv_import_histories_cvv_id_fk"
   end
 end
