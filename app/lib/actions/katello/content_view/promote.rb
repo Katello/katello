ce0ff7b1a261e6937061ecd053fce0b65d6cbf1d
Oleh Fedorenko <ofedoren@redhat.com>
2021-03-29 16:15:49 +0000

    Fixes #32224 - Expose Katello events for Webhooks

--- app/lib/actions/katello/content_view/promote.rb
+++ app/lib/actions/katello/content_view/promote.rb
@@ -2,6 +2,9 @@ module Actions
   module Katello
     module ContentView
       class Promote < Actions::EntryAction
+        extend ApipieDSL::Class
+        include ::Actions::ObservableAction
+
         def plan(version, environments, is_force = false, description = nil, incremental_update = false)
           action_subject(version.content_view)
           version.check_ready_to_promote!(environments)
@@ -17,6 +20,28 @@ module Actions
               plan_action(Katello::ContentViewVersion::AfterPromoteHook, :id => version.id)
             end
           end
+        end
+
+        def environments
+          input['environments']
+        end
+
+        apipie :class, "A class representing #{self} object" do
+          desc 'This object is available as **@object** variable in
+                webhook templates when a corresponding event occures.
+                The following properties can be used to retrieve the needed information.'
+          name "#{class_scope}"
+          refs "#{class_scope}"
+          sections only: %w[all webhooks]
+          property :task, object_of: 'Task', desc: 'Returns the task to which this action belongs'
+          property :environments, array_of: String, desc: 'Returns the list of environments the content view was promoted to'
+        end
+        include Actions::Katello::JailConcern::Organization
+        include Actions::Katello::JailConcern::ContentView
+        class Jail < ::Actions::ObservableAction::Jail
+          allow :organization_id, :organization_name, :organization_label,
+                :content_view_id, :content_view_name, :content_view_label,
+                :environments
         end
       end
     end
