6d70fab5d062010335e04eee8e9a995b824256f4
Lucy Fu <lufu@redhat.com>
2021-04-16 09:42:00 -0400

    Refs #32008 - make sure upgrade task runs

--- db/seeds.d/111-upgrade_tasks.rb
+++ db/seeds.d/111-upgrade_tasks.rb
@@ -2,6 +2,7 @@ UpgradeTask.define_tasks(:katello) do
   [
     {:name => 'katello:correct_repositories', :long_running => true, :skip_failure => true, :always_run => true},
     {:name => 'katello:clean_backend_objects', :long_running => true, :skip_failure => true, :always_run => true},
-    {:name => 'katello:upgrades:4.0:remove_ostree_puppet_content'}
+    {:name => 'katello:upgrades:4.0:remove_ostree_puppet_content'},
+    {:name => 'katello:upgrades:4.1:sync_noarch_content'}
   ]
 end
