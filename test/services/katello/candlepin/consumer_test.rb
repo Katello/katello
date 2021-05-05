789b54806b222c36fa07117c72e4c853d70a3ddc
lumarel <lumarel@users.noreply.github.com>
2021-05-05 10:37:23 +0200

    Parse Rocky Linux distribution properly

--- test/services/katello/candlepin/consumer_test.rb
+++ test/services/katello/candlepin/consumer_test.rb
@@ -62,6 +62,8 @@ module Katello
 
         assert_equal 'OracleLinux', Candlepin::Consumer.distribution_to_puppet_os('Oracle Linux Server')
 
+        assert_equal 'RockyLinux', Candlepin::Consumer.distribution_to_puppet_os('Rocky Linux')
+
         assert_equal 'Unknown', Candlepin::Consumer.distribution_to_puppet_os('RedHot')
       end
 
