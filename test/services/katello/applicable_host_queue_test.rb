d168ae45bedbd0516c03a0718819f8e6f1f58fec
Justin Sherrill <jsherril@redhat.com>
2021-05-12 17:40:27 -0400

    Fixes #31411 - bulk insert into applicable host queue
    (cherry picked from commit 9e06c62b37c568f03466a155f3a801b101e25f21)

--- test/services/katello/applicable_host_queue_test.rb
+++ test/services/katello/applicable_host_queue_test.rb
@@ -11,14 +11,14 @@ module Katello
     end
 
     def test_pop_1_host
-      ApplicableHostQueue.push_host(999)
+      ApplicableHostQueue.push_hosts([999])
       popped_hosts = ApplicableHostQueue.pop_hosts
 
       assert_equal [999], popped_hosts.map(&:host_id).sort
     end
 
     def test_pop_5_hosts
-      5.times { |i| ApplicableHostQueue.push_host(i) }
+      5.times { |i| ApplicableHostQueue.push_hosts([i]) }
       popped_hosts = ApplicableHostQueue.pop_hosts
 
       assert_equal [0, 1, 2, 3, 4], popped_hosts.map(&:host_id).sort
@@ -26,22 +26,22 @@ module Katello
 
     def test_pop_batch_size_only
       Setting['applicability_batch_size'] = 3
-      5.times { |i| ApplicableHostQueue.push_host(i) }
+      5.times { |i| ApplicableHostQueue.push_hosts([i]) }
       popped_hosts = ApplicableHostQueue.pop_hosts
 
       assert_equal 3, popped_hosts.to_a.size
     end
 
     def test_pop_duplicate_hosts
-      5.times { |i| ApplicableHostQueue.push_host(i) }
-      5.times { |i| ApplicableHostQueue.push_host(i) }
+      5.times { |i| ApplicableHostQueue.push_hosts([i]) }
+      5.times { |i| ApplicableHostQueue.push_hosts([i]) }
       popped_hosts = ApplicableHostQueue.pop_hosts
 
       assert_equal [0, 1, 2, 3, 4], popped_hosts.map(&:host_id).sort
     end
 
     def test_queue_depth
-      3.times { |i| ApplicableHostQueue.push_host(i) }
+      3.times { |i| ApplicableHostQueue.push_hosts([i]) }
 
       assert_equal 3, ApplicableHostQueue.queue_depth
     end
