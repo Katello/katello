f659fc160da226aa0b9ca0222588a034fe74916b
Chris Roberts <chrobert@redhat.com>
2021-05-17 17:46:32 +0000

    Fixes #31996 - Remove and update deprecated API params for Katello 4.1
    (cherry picked from commit 1984006ea57a497a7619b5c0c1f61b32b52e86be)

--- app/controllers/katello/api/v2/host_errata_controller.rb
+++ app/controllers/katello/api/v2/host_errata_controller.rb
@@ -53,7 +53,7 @@ module Katello
 
     api :PUT, "/hosts/:host_id/errata/apply", N_("Schedule errata for installation using katello-agent. %s") % katello_agent_deprecation_text, deprecated: true
     param :host_id, :number, :desc => N_("Host ID"), :required => true
-    param :errata_ids, Array, :desc => N_("List of Errata ids to install. Will be removed in Katello 4.1."), :required => false, :deprecated => true
+    param :errata_ids, Array, :desc => N_("List of Errata ids to install. Will be removed in %s") % katello_agent_removal_release, required: false, deprecated: true
 
     param_group :bulk_errata_ids
     def apply
