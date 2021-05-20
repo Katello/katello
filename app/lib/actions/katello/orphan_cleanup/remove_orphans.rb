9f7c6a3941188a453da833071cb5502f32e554cd
Ian Ballou <ianballou67@gmail.com>
2021-05-20 10:34:57 -0400

    Fixes #32623 - delete_orphaned_content fails if no Pulp 3 (#9373)
    (cherry picked from commit 63a1d5bae152290cc0a31e9f1eea491749f8a009)

--- app/lib/actions/katello/orphan_cleanup/remove_orphans.rb
+++ app/lib/actions/katello/orphan_cleanup/remove_orphans.rb
@@ -8,7 +8,7 @@ module Actions
         def plan(proxy)
           sequence do
             plan_action(Actions::Pulp::Orchestration::OrphanCleanup::RemoveOrphans, proxy)
-            if proxy.pulp3_enabled?
+            if proxy.pulp3_enabled? && ::Katello::Ping.pulpcore_enabled
               plan_action(
                 Actions::Pulp3::Orchestration::OrphanCleanup::RemoveOrphans,
                 proxy)
