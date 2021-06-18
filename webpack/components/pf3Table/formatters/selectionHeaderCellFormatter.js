0aa5729f7cddebff752b5e37fa5154cd0baabc11
Lucy Fu <lufu@redhat.com>
2021-06-18 14:43:05 +0000

    Fixes #32815 - Display checkbox/select column for grouped subscriptions

--- webpack/components/pf3Table/formatters/selectionHeaderCellFormatter.js
+++ webpack/components/pf3Table/formatters/selectionHeaderCellFormatter.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TableSelectionHeaderCell from '../components/TableSelectionHeaderCell';
 
-export default (selectionController, label) => (
+export default (selectionController, label, selectionEnabled) => (
   <TableSelectionHeaderCell
     label={label}
     checked={selectionController.allRowsSelected()}
     onChange={() => selectionController.selectAllRows()}
+    disabled={!selectionEnabled}
   />
 );
