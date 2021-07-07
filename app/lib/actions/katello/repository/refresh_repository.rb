d01536439d8904e1716b95852632cf6eb696aa54
Jonathon Turel <jturel@gmail.com>
2021-07-07 16:24:10 +0000

    Refs #32580 - use plan_action instead of plan_pulp_action

--- app/lib/actions/katello/repository/refresh_repository.rb
+++ app/lib/actions/katello/repository/refresh_repository.rb
@@ -5,7 +5,7 @@ module Actions
         def plan(repo, options = {})
           User.as_anonymous_admin do
             repo = ::Katello::Repository.find(repo.id)
-            plan_pulp_action(Actions::Pulp3::Orchestration::Repository::RefreshIfNeeded,
+            plan_action(Actions::Pulp3::Orchestration::Repository::RefreshIfNeeded,
                              repo, SmartProxy.default_capsule!, :dependency => options[:dependency])
             plan_self(:name => repo.name, :dependency => options[:dependency])
           end
