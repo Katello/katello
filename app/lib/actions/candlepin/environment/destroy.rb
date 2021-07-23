049f47302ed896c51cbae4c2a518fc5c8da0f346
Jonathon Turel <jturel@gmail.com>
2021-07-23 14:57:29 +0000

    Fixes #33019 - Don't raise errors when deleting missing Candlepin environment

--- app/lib/actions/candlepin/environment/destroy.rb
+++ app/lib/actions/candlepin/environment/destroy.rb
@@ -8,6 +8,8 @@ module Actions
 
         def run
           ::Katello::Resources::Candlepin::Environment.destroy(input['cp_id'])
+        rescue ::Katello::Errors::CandlepinEnvironmentGone
+          Rails.logger.info("Candlepin environment cp_id=#{input['cp_id']} was not found, continuing")
         end
       end
     end
