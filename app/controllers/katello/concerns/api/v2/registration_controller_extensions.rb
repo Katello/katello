da73b7824661e58062e2803d930a3ceb07f8bef4
Leos Stejskal <github@stejskalleos.cz>
2021-07-28 16:35:12 +0200

    Fixes #33031 - Registration - Remove 'Foreman' from error msg (#9488)

--- app/controllers/katello/concerns/api/v2/registration_controller_extensions.rb
+++ app/controllers/katello/concerns/api/v2/registration_controller_extensions.rb
@@ -7,7 +7,7 @@ module Katello
         if params['uuid']
           @host = Katello::Host::SubscriptionFacet.find_by(uuid: params['uuid'])&.host
           if @host.nil?
-            msg = N_("Host was not found by the subscription UUID: '%s', this can happen if the host is registered already, but not to this Foreman") % params['uuid']
+            msg = N_("Host was not found by the subscription UUID: '%s', this can happen if the host is registered already, but not to this instance") % params['uuid']
             fail ActiveRecord::RecordNotFound, msg
           end
           @host.assign_attributes(host_params('host'))
