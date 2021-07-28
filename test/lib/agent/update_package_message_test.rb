0580278a2d40ee9aef57debd31ea846de045826b
Partha Aji <paji@redhat.com>
2021-07-28 11:11:37 -0400

    Refs #33040 - Readable upgrade-all tests (#9506)
    Added a couple of suggestions from a prior pr that got missed.
    Pr #9474

--- test/lib/agent/update_package_message_test.rb
+++ test/lib/agent/update_package_message_test.rb
@@ -5,14 +5,19 @@ module Katello
     class UpdatePackageMessageTest < ActiveSupport::TestCase
       def test_all_package_update
         json = UpdatePackageMessage.new(content: [], consumer_id: "100000").json
-        assert json[:request][:args][1][:all]
-        assert_empty json[:request][:args][0][0][:unit_key]
+        options = json[:request][:args][1]
+        units = json[:request][:args][0]
+        assert options[:all]
+        assert_empty units[0][:unit_key]
       end
 
       def test_content_package_update
         json = UpdatePackageMessage.new(content: ["foo.rpm"], consumer_id: "100000").json
-        assert_nil json[:request][:args][1][:all]
-        assert_equal "foo.rpm", json[:request][:args][0][0][:unit_key][:name]
+        options = json[:request][:args][1]
+        units = json[:request][:args][0]
+
+        assert_nil options[:all]
+        assert_equal "foo.rpm", units[0][:unit_key][:name]
       end
     end
   end
