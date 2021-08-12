734dada012e5f9c1439a2ca79ddbf6f9c0e274ff
Samir Jha <sjha4@ncsu.edu>
2021-08-12 13:28:45 -0400

    Fixes #33221 - Hide upload section for Repository collections (#9540)
    (cherry picked from commit 5ad117c114e45d4b93c803412ae27c23405ad932)

--- app/controllers/katello/api/v2/content_uploads_controller.rb
+++ app/controllers/katello/api/v2/content_uploads_controller.rb
@@ -12,7 +12,9 @@ module Katello
     param :checksum, String, :required => false, :desc => N_("Checksum of file to upload")
     param :content_type, RepositoryTypeManager.uploadable_content_types(false).map(&:label), :required => false, :desc => N_("content type ('deb', 'docker_manifest', 'file', 'ostree', 'rpm', 'srpm')")
     def create
-      content_type = params[:content_type] || ::Katello::RepositoryTypeManager.find(@repository.content_type).default_managed_content_type.label
+      fail Katello::Errors::InvalidRepositoryContent, _("Cannot upload Ansible collections.") if @repository.ansible_collection?
+      content_type = params[:content_type] || ::Katello::RepositoryTypeManager.find(@repository.content_type)&.default_managed_content_type&.label
+
       RepositoryTypeManager.check_content_matches_repo_type!(@repository, content_type)
 
       unit_type_id = SmartProxy.pulp_primary.content_service(content_type).content_type
