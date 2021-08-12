821d49fac7f54b17fcfd4a77eafc86e65b92dda5
Justin Sherrill <jsherril@redhat.com>
2021-08-12 15:32:18 -0400

    Fixes #33251 - improve repository update for ansible coll
    * adds more validations to catch errors sooner
    * tracks remote update as an async task
    * does a test remote creation in the plan phase of repository update
       and reformats the error to be more user friendly

--- app/lib/actions/pulp3/repository/update_remote.rb
+++ app/lib/actions/pulp3/repository/update_remote.rb
@@ -1,14 +1,17 @@
 module Actions
   module Pulp3
     module Repository
-      class UpdateRemote < Pulp3::Abstract
+      class UpdateRemote < Pulp3::AbstractAsyncTask
         def plan(repository, smart_proxy)
-          plan_self(:repository_id => repository.id, :smart_proxy_id => smart_proxy.id)
+          if repository.root.url
+            repository.backend_service(smart_proxy).create_test_remote if smart_proxy.pulp_primary?
+            plan_self(:repository_id => repository.id, :smart_proxy_id => smart_proxy.id)
+          end
         end
 
-        def run
+        def invoke_external_task
           repo = ::Katello::Repository.find(input[:repository_id])
-          output[:response] = repo.backend_service(smart_proxy).update_remote
+          repo.backend_service(smart_proxy).update_remote
         end
       end
     end
---
+++ test/actions/pulp3/orchestration/ansible_collection_update_test.rb
@@ -0,0 +1,30 @@
+require 'katello_test_helper'
+
+module ::Actions::Pulp3
+  class AnsibleCollectionUpdateTest < ActiveSupport::TestCase
+    include Katello::Pulp3Support
+
+    def setup
+      @primary = SmartProxy.pulp_primary
+      @repo = katello_repositories(:pulp3_ansible_collection_1)
+      create_repo(@repo, @primary)
+    end
+
+    def test_invalid_update
+      ::Katello::Pulp3::Repository::AnsibleCollection.any_instance.stubs(:test_remote_name).returns(:test_remote_name)
+
+      bad_requirements = "---
+collections:
+  # Install a collection from Ansible Galaxy.
+  - name: geerlingguy.php_roles
+    version: 0.9.3
+    source: ftp://foobar/path/tobaz"
+      @repo.root.update(ansible_collection_requirements: bad_requirements)
+
+      message = "Invalid URL ftp://foobar/path/tobaz. Ensure the URL ends '/'."
+      assert_raises_with_message(::Katello::Errors::Pulp3Error, message) do
+        ForemanTasks.sync_task(::Actions::Pulp3::Orchestration::Repository::Update, @repo, @primary)
+      end
+    end
+  end
+end
