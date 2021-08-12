0dc09a5be87b0f0ed160f54229bb1aacde97d3fb
Justin Sherrill <jsherril@redhat.com>
2021-08-12 15:32:18 -0400

    Fixes #33251 - improve repository update for ansible coll
    * adds more validations to catch errors sooner
    * tracks remote update as an async task
    * does a test remote creation in the plan phase of repository update
       and reformats the error to be more user friendly

--- app/services/katello/pulp3/repository.rb
+++ app/services/katello/pulp3/repository.rb
@@ -75,8 +75,39 @@ module Katello
 
       def create_remote
         remote_file_data = api.class.remote_class.new(remote_options)
-        response = api.remotes_api.create(remote_file_data)
-        repo.update!(:remote_href => response.pulp_href)
+        reformat_api_exception do
+          response = api.remotes_api.create(remote_file_data)
+          repo.update!(:remote_href => response.pulp_href)
+        end
+      end
+
+      # When updating a repository, we need to update the remote, but this is
+      # an async task.  If some validation occurs, we won't know about it until
+      # the task runs.  Errors during a repository update task are very difficult to
+      # handle once the task is in its run phase, so this creates a test remote
+      # with a random name in order to validate the remote's configuration
+      def create_test_remote
+        test_remote_options = remote_options
+        test_remote_options[:name] = test_remote_name
+        remote_file_data = api.class.remote_class.new(test_remote_options)
+
+        reformat_api_exception do
+          response = api.remotes_api.create(remote_file_data)
+          #delete is async, but if its not properly deleted, orphan cleanup will take care of it later
+          delete_remote(response.pulp_href)
+        end
+      end
+
+      def test_remote_name
+        "test_remote_#{SecureRandom.uuid}"
+      end
+
+      def reformat_api_exception
+        yield
+      rescue api.class.client_module::ApiError => exception
+        body = JSON.parse(exception.response_body) rescue body
+        body = body.values.join(',') if body.respond_to?(:values)
+        raise ::Katello::Errors::Pulp3Error, body
       end
 
       def update_remote
