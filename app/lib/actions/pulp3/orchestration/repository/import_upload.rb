c09510fca8378797ba766272fd8e09caf4105e44
Samir Jha <sjha4@ncsu.edu>
2021-08-18 08:51:41 -0400

    Fixes #33235 - Allow duplicate uploads to katello (#9547)

--- app/lib/actions/pulp3/orchestration/repository/import_upload.rb
+++ app/lib/actions/pulp3/orchestration/repository/import_upload.rb
@@ -4,15 +4,36 @@ module Actions
     module Orchestration
       module Repository
         class ImportUpload < Pulp3::Abstract
+          # rubocop:disable Metrics/AbcSize
           def plan(repository, smart_proxy, args)
-            file = {:filename => args.dig(:unit_key, :name)}
+            file = {:filename => args.dig(:unit_key, :name), :sha256 => args.dig(:unit_key, :checksum) }
             content_unit_href = args.dig(:unit_key, :content_unit_id)
             docker_tag = (args.dig(:unit_type_id) == "docker_tag")
             sequence do
               if content_unit_href
-                plan_self(:commit_output => [], :content_unit_href => content_unit_href)
-                action_output = plan_action(Pulp3::Repository::ImportUpload, content_unit_href, repository, smart_proxy).output
-                plan_action(Pulp3::Repository::SaveVersion, repository, tasks: action_output[:pulp_tasks]).output
+                content_backend_service = SmartProxy.pulp_primary.content_service(args.dig(:unit_type_id))
+                duplicate_sha_path_content_list = content_backend_service.content_api.list(
+                    "sha256": file[:sha256],
+                    "relative_path": file[:filename])
+                duplicate_content_href = duplicate_sha_path_content_list&.results&.first&.pulp_href
+                if duplicate_content_href
+                  plan_self(:commit_output => [], :content_unit_href => duplicate_content_href)
+                  action_output = plan_action(Pulp3::Repository::ImportUpload, duplicate_content_href, repository, smart_proxy).output
+                  plan_action(Pulp3::Repository::SaveVersion, repository, tasks: action_output[:pulp_tasks]).output
+                else
+                  duplicate_sha_artifact_list = ::Katello::Pulp3::Api::Core.new(smart_proxy).artifacts_api.list("sha256": file[:sha256])
+                  duplicate_sha_artifact_href = duplicate_sha_artifact_list&.results&.first&.pulp_href
+                  if duplicate_sha_artifact_href
+                    artifact_output = plan_action(Pulp3::Repository::SaveArtifact,
+                                                         file, repository, smart_proxy,
+                                                         nil, args.dig(:unit_type_id),
+                                                         artifact_href: duplicate_sha_artifact_href).output
+                    content_unit_href = artifact_output[:pulp_tasks]
+                    plan_self(:commit_output => nil, :artifact_output => artifact_output[:pulp_tasks])
+                    action_output = plan_action(Pulp3::Repository::ImportUpload, content_unit_href, repository, smart_proxy).output
+                    plan_action(Pulp3::Repository::SaveVersion, repository, tasks: action_output[:pulp_tasks]).output
+                  end
+                end
               elsif docker_tag
                 tag_manifest_output = plan_action(Pulp3::Repository::UploadTag,
                             repository,
