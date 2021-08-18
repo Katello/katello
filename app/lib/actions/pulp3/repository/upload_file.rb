87df0bf7596ba796253d6e35eb2a45a6332a347c
Samir Jha <sjha4@ncsu.edu>
2021-08-18 08:51:41 -0400

    Fixes #33235 - Allow duplicate uploads to katello (#9547)
    (cherry picked from commit c09510fca8378797ba766272fd8e09caf4105e44)

--- app/lib/actions/pulp3/repository/upload_file.rb
+++ app/lib/actions/pulp3/repository/upload_file.rb
@@ -6,6 +6,7 @@ module Actions
           plan_self(:repository_id => repository.id, :smart_proxy_id => smart_proxy.id, :file => file)
         end
 
+        # rubocop:disable Metrics/MethodLength
         def invoke_external_task
           repo = ::Katello::Repository.find(input[:repository_id])
           repo_backend_service = repo.backend_service(smart_proxy)
@@ -34,10 +35,21 @@ module Actions
 
             if response
               upload_href = response.pulp_href
-              output[:pulp_tasks] = [uploads_api.commit(upload_href, sha256: sha256)]
+              #Check for any duplicate artifacts created in parallel subtasks
+              duplicate_sha_artifact_list = ::Katello::Pulp3::Api::Core.new(smart_proxy).artifacts_api.list("sha256": sha256)
+              duplicate_sha_artifact_href = duplicate_sha_artifact_list&.results&.first&.pulp_href
+              if duplicate_sha_artifact_href
+                uploads_api.delete(upload_href)
+                output[:artifact_href] = duplicate_sha_artifact_href
+                output[:pulp_tasks] = nil
+              else
+                output[:artifact_href] = nil
+                output[:pulp_tasks] = [uploads_api.commit(upload_href, sha256: sha256)]
+              end
             end
           end
         end
+        # rubocop:enable Metrics/MethodLength
 
         def external_task=(tasks)
           super
@@ -50,8 +62,18 @@ module Actions
         def check_error_details
           output[:pulp_tasks].each do |pulp_task|
             error_details = pulp_task.try(:[], "error")
-            if error_details && !error_details.nil?
-              fail _("An error occurred during the sync \n%{error_message}") % {:error_message => error_details}
+            if error_details && !error_details.nil? && !unique_error(error_details)
+              fail _("An error occurred during upload \n%{error_message}") % {:error_message => error_details}
+            end
+          end
+        end
+
+        def check_for_errors
+          combined_tasks.each do |task|
+            if unique_error task.error
+              warn _("Duplicate artifact detected")
+            else
+              super
             end
           end
         end
@@ -69,6 +91,10 @@ module Actions
         def content_range(start, finish, total)
           finish = finish > total ? total : finish
           "bytes #{start}-#{finish}/#{total}"
+        end
+
+        def unique_error(message)
+          message&.include?("code='unique'")
         end
       end
     end
