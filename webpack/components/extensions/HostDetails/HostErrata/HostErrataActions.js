7dcfc74aa662537a4ab71b1df58217dc8a70ca2c
Partha Aji <paji@redhat.com>
2021-08-25 15:29:50 -0400

    Fixes #33361 - Initial stab at the host errata page
    Update webpack/components/extensions/HostDetails/Tabs/ContentTab.js
    
    Co-authored-by: Jeremy Lenz <jlenz@redhat.com>

---
+++ webpack/components/extensions/HostDetails/HostErrata/HostErrataActions.js
@@ -0,0 +1,11 @@
+import { API_OPERATIONS, get } from 'foremanReact/redux/API';
+import { foremanApi } from '../../../../services/api';
+import { HOST_ERRATA_KEY } from './HostErrataConstants';
+
+export const getInstallableErrata = (hostId, params) => get({
+  type: API_OPERATIONS.GET,
+  key: HOST_ERRATA_KEY,
+  url: foremanApi.getApiUrl(`/hosts/${hostId}/errata`),
+  params,
+});
+export default getInstallableErrata;
