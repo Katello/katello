f5cd356ccb5e36bfbe1f0f7eeda11218f7af8e30
Justin Sherrill <jlsherrill@gmail.com>
2021-08-30 15:50:37 -0400

    Fixes #33372 - fix missing prop type (#9591)
    (cherry picked from commit c0c1b6e4cbd2ae7ea6fa11415cfd87b38b6feb78)

--- webpack/components/AddedStatusLabel.js
+++ webpack/components/AddedStatusLabel.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Label } from '@patternfly/react-core';
 import { CheckCircleIcon } from '@patternfly/react-icons';
 import { translate as __ } from 'foremanReact/common/I18n';
+import PropTypes from 'prop-types';
 
 const AddedStatusLabel = ({ added }) => {
   if (added) {
@@ -12,6 +13,10 @@ const AddedStatusLabel = ({ added }) => {
     );
   }
   return __('Not added');
+};
+
+AddedStatusLabel.propTypes = {
+  added: PropTypes.bool.isRequired,
 };
 
 export default AddedStatusLabel;
