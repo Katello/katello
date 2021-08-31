bd85cf4ca2de1ba308e5b8023c02f08d38b6fe3b
Partha Aji <paji@redhat.com>
2021-08-31 10:35:54 -0400

    Fixes #33385,#33054,#33144 - Fixes host group inheritance
    Before the PR -
    
    On create hostgroups page the inheritance hierachy behavior was different
    between katello attribute like content_view/lce/content source etc vs
    things like os and arch
    For example if you create a child hostgroup and ppointed to a parent
    with contnet view, you would see the cv automatically selected as the
    default, instead of 'Inherit Content View' as os/arch fields behave.
    
    More over if you had cv, lce, cs, arch filled in the parent hostgroup
    and enabled operating system in the child the media section would not
    get automatically populated (because the notion of parent_id was
     missing)
    
    After:
    
    Both the above cases behave appropriately.

--- app/lib/katello/validators/hostgroup_kickstart_repository_validator.rb
+++ app/lib/katello/validators/hostgroup_kickstart_repository_validator.rb
@@ -3,21 +3,34 @@ module Katello
     class HostgroupKickstartRepositoryValidator < ActiveModel::Validator
       def validate(facet)
         return unless facet.kickstart_repository_id
+        if facet.content_source.blank? && facet.hostgroup.content_source.blank?
+          prop = :content_source
+          msg = _("Please select a content source before assigning a kickstart repository")
+        elsif facet.hostgroup.operatingsystem.blank?
+          prop = :base
+          msg = _("Please select an operating system before assigning a kickstart repository")
+        elsif !facet.hostgroup.operatingsystem.is_a?(Redhat)
+          prop = :base
+          msg = _("Kickstart repositories can only be assigned to hosts in the Red Hat family")
+        elsif facet.hostgroup.architecture.blank?
+          prop = :base
+          msg = _("Please select an architecture before assigning a kickstart repository")
+        elsif !content_view_in_env?(facet)
+          prop = :lifecycle_environment
+          msg = _("The selected/Inherited Content View is not available for this Lifecycle Environment")
+        elsif !facet.hostgroup.matching_kickstart_repository?(facet)
+          prop = :kickstart_repository
+          msg = _("The selected kickstart repository is not part of the assigned content view, " \
+                  "lifecycle environment, content source, operating system, and architecture")
+        end
+        facet.hostgroup.errors.add(prop, msg) if msg
+      end
 
-        msg = if facet.content_source.blank?
-                _("Please select a content source before assigning a kickstart repository")
-              elsif facet.hostgroup.operatingsystem.blank?
-                _("Please select an operating system before assigning a kickstart repository")
-              elsif !facet.hostgroup.operatingsystem.is_a?(Redhat)
-                _("Kickstart repositories can only be assigned to hosts in the Red Hat family")
-              elsif facet.hostgroup.architecture.blank?
-                _("Please select an architecture before assigning a kickstart repository")
-              elsif !facet.hostgroup.matching_kickstart_repository?(facet)
-                _("The selected kickstart repository is not part of the assigned content view, lifecycle environment,
-                  content source, operating system, and architecture")
-              end
-
-        facet.errors.add(:base, msg) if msg
+      def content_view_in_env?(facet)
+        env = facet.lifecycle_environment || facet.hostgroup.lifecycle_environment
+        cv = facet.content_view || facet.hostgroup.content_view
+        return true if env.blank? || cv.blank?
+        env.content_views.include?(cv)
       end
     end
   end
