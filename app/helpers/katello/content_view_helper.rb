47bc0e3bad80d39378c1c587a692ea9d4bb488fd
Ian Ballou <ianballou67@gmail.com>
2021-09-02 22:34:25 +0000

    Fixes #32460 - improve speed of cv publish with filters

--- app/helpers/katello/content_view_helper.rb
+++ app/helpers/katello/content_view_helper.rb
@@ -1,10 +1,10 @@
 module Katello
   module ContentViewHelper
-    def separated_repo_mapping(repo_mapping)
-      separated_mapping = { :pulp3_yum => {}, :other => {} }
+    def separated_repo_mapping(repo_mapping, use_multicopy_actions)
+      separated_mapping = { :pulp3_yum_multicopy => {}, :other => {} }
       repo_mapping.each do |source_repos, dest_repo|
-        if dest_repo.content_type == "yum" && SmartProxy.pulp_primary.pulp3_support?(dest_repo)
-          separated_mapping[:pulp3_yum][source_repos] = dest_repo
+        if dest_repo.content_type == "yum" && SmartProxy.pulp_primary.pulp3_support?(dest_repo) && use_multicopy_actions
+          separated_mapping[:pulp3_yum_multicopy][source_repos] = dest_repo
         else
           separated_mapping[:other][source_repos] = dest_repo
         end
