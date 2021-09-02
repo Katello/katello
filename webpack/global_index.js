b303a63ddd4530f33a71ee43f6f81ef7e08f4c5a
Amir Fefer <amirfefer@gmail.com>
2021-09-02 15:04:30 +0300

    Fixes #33398 - translate host details tabs

--- webpack/global_index.js
+++ webpack/global_index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { addGlobalFill } from 'foremanReact/components/common/Fill/GlobalFill';
 import { registerReducer } from 'foremanReact/common/MountingService';
+import { translate as __ } from 'foremanReact/common/I18n';
 
 import SystemStatuses from './components/extensions/about';
 import RegistrationCommands from './components/extensions/RegistrationCommands';
@@ -18,10 +19,14 @@ registerReducer('katello', rootReducer);
 
 addGlobalFill('aboutFooterSlot', '[katello]AboutSystemStatuses', <SystemStatuses key="katello-system-statuses" />, 100);
 addGlobalFill('registrationAdvanced', '[katello]RegistrationCommands', <RegistrationCommands key="katello-reg" />, 100);
-addGlobalFill('host-details-page-tabs', 'Content', <ContentTab key="content" />, 900);
+addGlobalFill('host-details-page-tabs', 'Content', <ContentTab key="content" />, 900, { title: __('Content') });
 /* eslint-disable max-len */
-// addGlobalFill('host-details-page-tabs', 'Subscription', <SubscriptionTab key="subscription" />, 100);
-addGlobalFill('host-details-page-tabs', 'Traces', <TracesTab key="traces" />, 800);
-addGlobalFill('host-details-page-tabs', 'Repository sets', <RepositorySetsTab key="repository-sets" />, 700);
+// addGlobalFill('host-details-page-tabs', 'Subscription', <SubscriptionTab key="subscription" />, 100, { title: __('Subscription') });
+addGlobalFill('host-details-page-tabs', 'Traces', <TracesTab key="traces" />, 800, { title: __('Traces') });
+addGlobalFill('host-details-page-tabs', 'Repository sets', <RepositorySetsTab key="repository-sets" />, 700, { title: __('Repository sets') });
 
-addGlobalFill('details-cards', 'Content View Details', <ContentViewDetailsCard key="content-view-details" />);
+addGlobalFill(
+  'details-cards',
+  'Content View Details',
+  <ContentViewDetailsCard key="content-view-details" />,
+);
