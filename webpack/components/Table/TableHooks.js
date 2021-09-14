5a755505f17a5dee46a89b88f34312f101cc532c
Jeremy Lenz <jlenz@redhat.com>
2021-09-14 13:16:06 -0400

    Refs #33078 - Move forceRender inside hook (#9635)

--- webpack/components/Table/TableHooks.js
+++ webpack/components/Table/TableHooks.js
@@ -1,11 +1,46 @@
 import { useState, useRef } from 'react';
 
+class ReactConnectedSet extends Set {
+  constructor(initialValue, forceRender) {
+    super();
+    this.forceRender = forceRender;
+    // The constructor would normally call add() with the initial value, but since we
+    // must call super() at the top, this.forceRender() isn't defined yet.
+    // So, we call super() above with no argument, then call add() manually below
+    // after forceRender is defined.
+    if (initialValue) {
+      if (initialValue.constructor.name === 'Array') {
+        initialValue.forEach(val => this.add(val));
+      } else {
+        this.add(initialValue);
+      }
+    }
+  }
+  add(value) {
+    const result = super.add(value); // ensuring these methods have the same API as the superclass
+    this.forceRender();
+    return result;
+  }
+
+  clear() {
+    const result = super.clear();
+    this.forceRender();
+    return result;
+  }
+
+  delete(value) {
+    const result = super.delete(value);
+    this.forceRender();
+    return result;
+  }
+}
+
 const useSet = (initialArry) => {
-  const set = useRef(new Set(initialArry));
   const [, setToggle] = useState(false);
   // needed because mutating a Ref won't cause React to rerender
   const forceRender = () => setToggle(prev => !prev);
-  return [set.current, forceRender];
+  const set = useRef(new ReactConnectedSet(initialArry, forceRender));
+  return set.current;
 };
 
 export default useSet;
