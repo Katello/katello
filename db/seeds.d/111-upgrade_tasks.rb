6c7accee3e49561ea630c8d68333eb63b81345e2
Partha Aji <paji@redhat.com>
2021-09-20 16:47:55 -0400

    Fixes #33527 - Add upgrade task to seeds

--- db/seeds.d/111-upgrade_tasks.rb
+++ db/seeds.d/111-upgrade_tasks.rb
@@ -4,6 +4,7 @@ UpgradeTask.define_tasks(:katello) do
     {:name => 'katello:clean_backend_objects', :long_running => true, :skip_failure => true, :always_run => true},
     {:name => 'katello:upgrades:4.0:remove_ostree_puppet_content'},
     {:name => 'katello:upgrades:4.1:sync_noarch_content'},
-    {:name => 'katello:upgrades:4.1:fix_invalid_pools'}
+    {:name => 'katello:upgrades:4.1:fix_invalid_pools'},
+    {:name => 'katello:upgrades:4.1:update_content_import_export_perms'}
   ]
 end
