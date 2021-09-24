9152000fa03e36c298064854d1ece8032280bd3a
Ondřej Ezr <oezr@redhat.com>
2021-09-24 11:03:30 +0200

    Fixes #33561 - drop Puppet Environment migration
    Foreman core wants to remove the Environments migrations and Katello useless migration is blocking it.
    We can safely remove the migration and let ForemanPuppet remove the column.
    
    ForemanPuppet is dealing with this in https://github.com/theforeman/foreman_puppet/pull/200

--- db/migrate/20140110000001_update_environments_add_katello_id.rb
+++ db/migrate/20140110000001_update_environments_add_katello_id.rb
@@ -1,10 +1,8 @@
 class UpdateEnvironmentsAddKatelloId < ActiveRecord::Migration[4.2]
   def up
-    add_column :environments, :katello_id, :string, :limit => 255
-    add_index :environments, :katello_id
+    # not doing anything, it is not needed anymore in supported versions
   end
 
   def down
-    remove_column :environments, :katello_id
   end
 end
