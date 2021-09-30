8dccc61ffc32b1bf598dd36de69c79352a5972ad
Samir Jha <sjha4@ncsu.edu>
2021-09-30 23:01:56 +0000

    Fixes #33399 - Add errata by date filter rule

---
+++ webpack/scenes/ContentViews/Details/Filters/__tests__/cvErrataDateFilterContent.test.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { renderWithRedux, patientlyWaitFor, fireEvent } from 'react-testing-lib-wrapper';
+import * as reactRedux from 'react-redux';
+import {
+  nockInstance,
+  assertNockRequest,
+} from '../../../../../test-utils/nockWrapper';
+import api from '../../../../../services/api';
+import CVErrataDateFilterContent from '../CVErrataDateFilterContent';
+
+const cvFilterDetails = require('./contentViewErrataByDateDetails.fixtures.json');
+
+const cvFilterDetailsPath = api.getApiUrl('/content_view_filters/36');
+
+const cvErrataDateRuleEditPath = api.getApiUrl('/content_view_filters/36/rules/35');
+
+test('Can display errata-date filter rule and edit', async (done) => {
+  const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
+  useSelectorMock.mockReturnValue(cvFilterDetails);
+
+  const cvFilterScope = nockInstance
+    .get(cvFilterDetailsPath)
+    .query(true)
+    .reply(200, cvFilterDetails);
+
+  const editDetails = {
+    id: 35,
+    content_view_filter_id: '36',
+    start_date: null,
+    end_date: '2020-08-15T12:00:00.000Z',
+    types: ['enhancement', 'security'],
+    date_type: 'issued',
+  };
+
+  const ruleEditScope = nockInstance
+    .put(cvErrataDateRuleEditPath, editDetails)
+    .reply(200, cvFilterDetails);
+
+  const {
+    getByText, queryByText, getByLabelText, getAllByText, queryAllByText,
+  } = renderWithRedux(<CVErrataDateFilterContent
+    cvId={1}
+    filterId="36"
+    showAffectedRepos={false}
+    setShowAffectedRepos={() => {}}
+  />);
+
+  await patientlyWaitFor(() => {
+    expect(getAllByText('ANY')).toHaveLength(2);
+    expect(getByLabelText('save_filter_rule')).toHaveAttribute('aria-disabled', 'true');
+  });
+  fireEvent.change(getByLabelText('start_date_input'), { target: { value: '08/15/1990' } });
+  fireEvent.change(getByLabelText('end_date_input'), { target: { value: '08/15/2020' } });
+  await patientlyWaitFor(() => {
+    expect(queryAllByText('ANY')).toHaveLength(0);
+    expect(getByText('08/15/1990')).toBeInTheDocument();
+    expect(getByText('08/15/2020')).toBeInTheDocument();
+  });
+  // Can clear date with chip
+  getByLabelText('08/15/1990').click();
+  await patientlyWaitFor(() => {
+    expect(getByText('ANY')).toBeInTheDocument();
+    expect(queryByText('08/15/1990')).not.toBeInTheDocument();
+    expect(getByText('08/15/2020')).toBeInTheDocument();
+    // Enabled Edit rule button
+    expect(getByLabelText('save_filter_rule')).toHaveAttribute('aria-disabled', 'false');
+  });
+  getByLabelText('save_filter_rule').click();
+
+  useSelectorMock.mockClear();
+  assertNockRequest(ruleEditScope, done);
+  assertNockRequest(cvFilterScope, done);
+});
