a1a1789ee511d3f12a79de7e8faf69d47e829868
Justin Sherrill <jsherril@redhat.com>
2021-10-06 17:03:01 -0400

    Fixes #33652 - Pulp 3.15 support

--- test/services/katello/pulp3/content_test.rb
+++ test/services/katello/pulp3/content_test.rb
@@ -16,7 +16,15 @@ module Katello
           @file1 = {path: tmp_file1, filename: "test_other_type.json"}
           tmp_file = File.join(Katello::Engine.root, "test/fixtures/files/test_erratum.json")
           @file = {path: tmp_file, filename: "test_erratum.json"}
+
           create_repo(@repo, @primary)
+        end
+
+        def teardown
+          ForemanTasks.sync_task(
+              ::Actions::Pulp3::Orchestration::Repository::Delete, @repo, @primary) rescue "skipped repo delete"
+          ForemanTasks.sync_task(
+              ::Actions::Pulp3::Orchestration::OrphanCleanup::RemoveOrphans, @primary) rescue "skipped remove orphans"
         end
 
         def test_create_upload
@@ -25,18 +33,22 @@ module Katello
           sha256 = Digest::SHA256.hexdigest(File.read(@file1[:path]))
           unit_type_id = "file"
           upload_hash = @repo.backend_content_service(@primary).create_upload(size, sha256, unit_type_id)
-          refute_nil upload_hash.with_indifferent_access[:upload_id]
+          upload_id = upload_hash.with_indifferent_access[:upload_id]
+
+          refute_nil upload_id
         end
 
         def test_chunk_upload
-          chunk_size = 5
+          chunk_size = 100
           @repo.reload
           size = File.size(@file[:path])
           sha256 = Digest::SHA256.hexdigest(File.read(@file[:path]))
           unit_type_id = "file"
           upload_hash = @repo.backend_content_service(@primary).create_upload(size, sha256, unit_type_id)
           upload_href = upload_hash.with_indifferent_access[:upload_id]
-          refute_nil upload_hash.with_indifferent_access[:upload_id]
+
+          refute_nil upload_hash.with_indifferent_access[:upload_id], "You likely need to set 'ORPHAN_PROTECTION_TIME = 0' in pulp's settings.py"
+
           offset = 0
           VCR.use_cassette(cassette_name + '_binary', :match_requests_on => [:method, :path, :params]) do
             File.open(@file[:path], "rb") do |file|
@@ -47,11 +59,13 @@ module Katello
               end
             end
 
-            assert_equal offset, size
             uploads = [{'id' => upload_href, 'size' => size, 'checksum' => sha256, 'name' => @file[:filename]}]
-            ::ForemanTasks.sync_task(Actions::Katello::Repository::ImportUpload, @repo, uploads)
+            task = ::ForemanTasks.sync_task(Actions::Katello::Repository::ImportUpload, @repo, uploads)
+            task
           end
+          @repo.reload
           @repo.index_content
+          assert @repo.file_units.any?
         end
       end
     end
