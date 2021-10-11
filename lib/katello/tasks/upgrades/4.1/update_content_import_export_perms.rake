7d46f80ffe412ac4d46baa6247acae1855be4f0b
Partha Aji <paji@redhat.com>
2021-10-11 12:22:58 -0400

    Fixes #33678 - Fixes import/export permissions on an upgrade task

--- lib/katello/tasks/upgrades/4.1/update_content_import_export_perms.rake
+++ lib/katello/tasks/upgrades/4.1/update_content_import_export_perms.rake
@@ -5,17 +5,49 @@ namespace :katello do
       task :update_content_import_export_perms => ['environment'] do
         User.current = User.anonymous_api_admin
 
+        def deduplicate_filters(perm)
+          # This method makes sure that the perms [import_content, export_content]
+          # are not duplicated in this role
+          # This case used to occur if this upgrade script  did not run
+          # and the user created a role with
+          # import_library_content, export_library_content, import_content, export_content
+          # After running this task the output used to be
+          # [import_content, import_content, export_content, export_content]
+          # This method just ensures that those get cleaned up.
+
+          filtering_map = Filtering.where(permission_id: Permission.where(name: [perm]))
+                           .order(:id)
+                           .group_by(&:filter_id)
+                           .select { |_, value| value.length > 1 }
+          filtering_map.values.each do |filterings|
+            sliced = filterings[0...-1] # keep the last filtering and destroy the rest
+            sliced.each(&:destroy)
+          end
+        end
+
         permission_map = {
           Permission.find_by(name: :export_library_content) => Permission.find_by(name: :export_content),
           Permission.find_by(name: :import_library_content) => Permission.find_by(name: :import_content)
         }
 
         permission_map.each do |old_perm, new_perm|
-          Filtering.where(permission_id: old_perm.id).update_all(:permission_id => new_perm.id) if old_perm
+          if old_perm
+            # if there was already an export_content/import_content permission
+            if Filtering.where(permission_id: new_perm.id).exists?
+              # then just remove export_library_content, import_library_content
+              Filtering.where(permission_id: old_perm).delete_all
+            else
+              # then just remove export_library_content, import_library_content
+              Filtering.where(permission_id: old_perm.id).update_all(:permission_id => new_perm.id)
+            end
+          end
         end
 
         names = permission_map.keys.compact.map(&:name)
         Permission.where(:name => names).destroy_all if names.any?
+
+        deduplicate_filters('import_content')
+        deduplicate_filters('export_content')
 
         export_content_views = Permission.find_by_name(:export_content_views)
         next if export_content_views.blank?
--- test/lib/tasks/upgrades/4.1/update_content_import_export_perms_test.rb
+++ test/lib/tasks/upgrades/4.1/update_content_import_export_perms_test.rb
@@ -27,6 +27,76 @@ module Katello
       assert_equal Filtering.where(permission_id: old_perms.collect(&:id)).size, 0
     end
 
+    def test_update_content_import_export_perms_both_library_and_regular
+      # This tests the case were the user has a role with perms
+      # export_library_content, export_content, import_library_content, import_content
+      # The execution of upgrade task must eliminate  import_library_content and
+      # export_library_content (instead of moving it)
+      # So post upgrade  you should expect to see -> [import_content, export_content]
+      #
+      old_perm_names = %w[export_library_content import_library_content].freeze
+      new_perm_names = %w[export_content import_content].freeze
+
+      old_perms = old_perm_names.collect do |perm_name|
+        FactoryBot.create(:permission, name: perm_name)
+      end
+      new_perms = new_perm_names.map do |perm_name|
+        Permission.find_by(name: perm_name)
+      end
+
+      role = FactoryBot.create(:role, name: 'old content tester', permissions: old_perms + new_perms)
+      assert_equal 2, Permission.where(name: new_perm_names).count
+      assert_equal 2, Filtering.where(filter_id: role.filters.select(:id), permission_id: new_perms.map(&:id)).count
+
+      assert_equal Permission.where(name: old_perm_names).count, 2
+      assert_equal Filtering.where(permission_id: old_perms.map(&:id)).count, 2
+
+      Rake.application.invoke_task('katello:upgrades:4.1:update_content_import_export_perms')
+
+      assert_equal Permission.where(name: old_perm_names).count, 0
+      assert_equal Filtering.where(permission_id: old_perms.map(&:id)).count, 0
+
+      assert_equal Permission.where(name: new_perm_names).count, 2
+      assert_equal 2, Filtering.where(filter_id: role.filters.select(:id), permission_id: new_perms.map(&:id)).count
+    end
+
+    def test_update_content_import_export_perms_duplicated
+      # This tests the case were the user has a role with duplicate perms
+      # export_content, export_content, import_content, import_content
+      # The execution of upgrade task must eliminate  one import_content and
+      # and one export_content
+      # So post upgrade  you should expect to see -> [import_content, export_content]
+      #
+      old_perm_names = %w[export_library_content import_library_content].freeze
+      old_perms = old_perm_names.map do |perm_name|
+        FactoryBot.create(:permission, name: perm_name, resource_type: 'Organization')
+      end
+
+      new_perm_names = %w[export_content import_content].freeze
+      new_perms = new_perm_names.map do |perm_name|
+        Permission.find_by(name: perm_name)
+      end
+
+      role = FactoryBot.create(:role, name: 'old content tester', permissions: old_perms + new_perms)
+
+      assert_equal 4, Permission.where(name: old_perm_names + new_perm_names).count
+
+      # Now just rename import_libray_content to import_content
+      permission_map = {
+        Permission.find_by(name: :export_library_content) => Permission.find_by(name: :export_content),
+        Permission.find_by(name: :import_library_content) => Permission.find_by(name: :import_content)
+      }
+      permission_map.each do |old_perm, new_perm|
+        Filtering.where(permission_id: old_perm.id).update_all(:permission_id => new_perm.id)
+      end
+      Permission.where(:name => old_perm_names).destroy_all
+      assert_equal 4, Filtering.where(filter_id: role.filters.select(:id), permission_id: new_perms.map(&:id)).count
+
+      Rake.application.invoke_task('katello:upgrades:4.1:update_content_import_export_perms')
+
+      assert_equal 2, Filtering.where(filter_id: role.filters.select(:id), permission_id: new_perms.map(&:id)).count
+    end
+
     def test_update_export_content_views
       old_perm_names = %w[export_content_views view_content_views].freeze
 
