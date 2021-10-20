45e7ecf3da1f81dee60f15ccc12cc75f2a356334
Chris Roberts <chrobert@redhat.com>
2021-10-20 17:18:19 +0000

    Refs #33628 - Hide packages and modulestreams tab from content page

--- webpack/components/extensions/HostDetails/Tabs/ContentTab/SecondaryTabsRoutes.js
+++ webpack/components/extensions/HostDetails/Tabs/ContentTab/SecondaryTabsRoutes.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { ErrataTab } from '../ErrataTab';
-import EmptyPage from './EmptyPage';
 import { route } from './helpers';
 
 const SecondaryTabRoutes = () => (
   <Switch>
     <Route exact path="/Content">
-      <Redirect to={route('packages')} />
-    </Route>
-    <Route path={route('packages')}>
-      <EmptyPage header="WIP Packages" />
+      <Redirect to={route('errata')} />
     </Route>
     <Route path={route('errata')}>
       <ErrataTab />
-    </Route>
-    <Route path={route('modulestreams')}>
-      <EmptyPage header="WIP Module streams" />
     </Route>
   </Switch>
 );
