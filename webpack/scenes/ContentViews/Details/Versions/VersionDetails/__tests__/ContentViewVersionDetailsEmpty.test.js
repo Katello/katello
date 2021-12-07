635132df79514fc293c1daf984f2c34181addb75
Samir Jha <sjha4@ncsu.edu>
2021-12-07 14:26:39 -0500

    Fixes #34043 - UX Review - Actions on Version details page (#9821)

--- webpack/scenes/ContentViews/Details/Versions/VersionDetails/__tests__/ContentViewVersionDetailsEmpty.test.js
+++ webpack/scenes/ContentViews/Details/Versions/VersionDetails/__tests__/ContentViewVersionDetailsEmpty.test.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { renderWithRedux, patientlyWaitFor } from 'react-testing-lib-wrapper';
 import { Route } from 'react-router-dom';
 
-import { nockInstance, assertNockRequest } from '../../../../../../test-utils/nockWrapper';
+import nock, { nockInstance, assertNockRequest } from '../../../../../../test-utils/nockWrapper';
 import api from '../../../../../../services/api';
 import { cvVersionDetailsKey } from '../../../../ContentViewsConstants';
 import ContentViewVersionDetails from '../ContentViewVersionDetails';
 import ContentViewVersionDetailsEmptyData from './ContentViewVersionDetails.fixtures.json';
 import cvDetailData from '../../../../__tests__/mockDetails.fixtures.json';
+import environmentPathsData from '../../Delete/__tests__/versionRemoveEnvPaths.fixtures';
 
 const withCVRoute = component =>
   <Route path="/versions/:versionId([0-9]+)">{component}</Route>;
 const cvVersions = api.getApiUrl('/content_view_versions/73');
+let envScope;
+let versionScope;
+const environmentPathsPath = api.getApiUrl('/organizations/1/environments/paths');
 
 const renderOptions = {
   apiNamespace: cvVersionDetailsKey(3, 73),
@@ -20,6 +24,23 @@ const renderOptions = {
     initialIndex: 1,
   },
 };
+
+beforeEach(() => {
+  envScope = nockInstance
+    .get(environmentPathsPath)
+    .query(true)
+    .reply(200, environmentPathsData);
+  versionScope = nockInstance
+    .get(cvVersions)
+    .query(true)
+    .reply(200, ContentViewVersionDetailsEmptyData);
+});
+
+afterEach(() => {
+  assertNockRequest(envScope);
+  assertNockRequest(versionScope);
+  nock.cleanAll();
+});
 
 test('Can show versions detail header', async (done) => {
   const { version } = ContentViewVersionDetailsEmptyData;
