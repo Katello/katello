a6213a67f7ee2cb25a1a66e5529a60243dddfa32
Evgeni Golov <evgeni@golov.de>
2022-01-04 08:36:02 +0100

    Fixes #34208 - include mirroring_policy in export metadata
    Without it, import doesn't work as you can't create a repository without
    a mirroring_policy:
    
        Validation failed: Mirroring policy Invalid mirroring policy for repository type yum, only additive, mirror_complete, mirror_content_only are valid.

--- app/services/katello/pulp3/content_view_version/metadata_generator.rb
+++ app/services/katello/pulp3/content_view_version/metadata_generator.rb
@@ -43,7 +43,7 @@ module Katello
 
         def generate_repository_metadata(repo)
           repo.slice(:name, :label, :description, :arch, :content_type, :unprotected,
-                     :checksum_type, :os_versions, :major, :minor).
+                     :checksum_type, :os_versions, :major, :minor, :mirroring_policy).
             merge(product: generate_product_metadata(repo.product),
                   gpg_key: generate_gpg_metadata(repo.gpg_key),
                   content: generate_content_metadata(repo.content),
