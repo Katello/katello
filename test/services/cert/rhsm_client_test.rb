d2578c25cc4b733d721c9f35e2737a4f8b5edf5d
Chris Roberts <chrobert@redhat.com>
2022-01-24 20:01:38 +0000

    Fixes #34306 - Update how we parse CN from subman identity certs
    (cherry picked from commit bc1eef93a9299a8d127f0febeb4d4a2e111f964f)

--- test/services/cert/rhsm_client_test.rb
+++ test/services/cert/rhsm_client_test.rb
@@ -4,7 +4,9 @@ require File.expand_path("#{Katello::Engine.root}/app/services/cert/rhsm_client.
 
 module Katello
   class RhsmClientTest < Minitest::Test
-    CERT = '
+    # Subject of old cert is
+    # Subject: CN = 14e98155-731a-4cae-b151-5c504cc30e1a
+    OLD_CERT = '
       -----BEGIN CERTIFICATE-----
       MIIEaTCCA1GgAwIBAgIIMAikOB+/HpowDQYJKoZIhvcNAQEFBQAwezELMAkGA1UE
       BhMCVVMxFzAVBgNVBAgTDk5vcnRoIENhcm9saW5hMRAwDgYDVQQHEwdSYWxlaWdo
@@ -32,9 +34,52 @@ module Katello
       okjCBjwYlp5cNyAJSQscLF7rj/iOJYhRdetWMZg=
       -----END CERTIFICATE-----'.freeze
 
+    # Subject of new cert is
+    # Subject: O=Default_Organization, CN=eb48d5a8-b759-417c-97f7-93dc2369de29
+    NEW_CERT = '
+      -----BEGIN CERTIFICATE-----
+    MIIGjDCCBHSgAwIBAgIIabvFmF/tIwkwDQYJKoZIhvcNAQELBQAwgZcxCzAJBgNV
+    BAYTAlVTMRcwFQYDVQQIDA5Ob3J0aCBDYXJvbGluYTEQMA4GA1UEBwwHUmFsZWln
+    aDEQMA4GA1UECgwHS2F0ZWxsbzEUMBIGA1UECwwLU29tZU9yZ1VuaXQxNTAzBgNV
+    BAMMLGNlbnRvczcta2F0ZWxsby1jYW5kbGVwaW4uYXJlYTUyLmV4YW1wbGUuY29t
+    MB4XDTIyMDEyMDIwMTIwNVoXDTM4MDEyMDIxMTIwNVowTjEdMBsGA1UECgwURGVm
+    YXVsdF9Pcmdhbml6YXRpb24xLTArBgNVBAMMJDU3YjI2Y2UzLWU0ZmUtNGY3OS1h
+    Mzc3LTJjNGQ5ZGVhNTIwNDCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIB
+    ALikZKO/50yG6qcWkSIr6IzPT8ohENEro6TERhWMCAuwikHE+MKdhPpyUIOkdt56
+    GXRU2zGhuxP4o+uG4B6r1EtG6Apvy5J6asL+pXHIUEvLG4HQmNQp8AqztAmS/Fh+
+    5j6n2PyD7/iF5Moozf7Ew+HJJvjy+RPHin3EEm3QvWcG65/5HGQHF2F9vUfo2kAE
+    ENhum2RLJ0EYZqBz7FkkBzS+onkY/uwmuRkxm0v4sHdQk1Fe5tLnBBm6pFDZzswB
+    v134Gi/j6I865i9FvvPvxrOE+MsBW/x1JAwj54IMa2HDINQmAY8jWWyiPGUY5yLl
+    kwnJ/CUBs+6qqKoUt+8H4TpC9dHStEdf2u1+nm+MgjRLm07piWo/Qp/bDh1OwfBT
+    bsRl2riE/wSofjex6X8ZjRxImhWNjxvzJAn/VD6p5+rpxFDu5RjCoKsGF9Tbn5Vr
+    /QRsfuiy9kwrsQYfp3tBAgyKsblTUncCT4KMTtVDbIm8MxlTLIDCjPRxLwhB1yan
+    05FuYHMHAMKkH6NCypuGA4YF1XSPMgO6SfBLz61yoAqMEXT9ugkj7gFt8bFzMmVn
+    b2BXNBXnC13oWG+EPd3THTeFEIZpbljZvGciqXTCKBnWD3tNcQx+ZQEbmhfNiwMg
+    hVv6lonLbPat4zK3JkW920/Vc+rfdoPRbxImsjMKh24BAgMBAAGjggEiMIIBHjAO
+    BgNVHQ8BAf8EBAMCBLAwEwYDVR0lBAwwCgYIKwYBBQUHAwIwCQYDVR0TBAIwADAR
+    BglghkgBhvhCAQEEBAMCBaAwHQYDVR0OBBYEFOx+heEao9Xx+vY/e3ozUT2FeM3i
+    MB8GA1UdIwQYMBaAFBnHeIYJrfaf8/3O44D+GvLA2Pc3MIGYBgNVHREEgZAwgY2k
+    UDBOMR0wGwYDVQQKDBREZWZhdWx0X09yZ2FuaXphdGlvbjEtMCsGA1UEAwwkNTdi
+    MjZjZTMtZTRmZS00Zjc5LWEzNzctMmM0ZDlkZWE1MjA0pDkwNzE1MDMGA1UEAwws
+    Y2VudG9zNy1rYXRlbGxvLWNhbmRsZXBpbi5hcmVhNTIuZXhhbXBsZS5jb20wDQYJ
+    KoZIhvcNAQELBQADggIBAAirOKFelb8FqTa6aaFb7JA7A3bQw7gjCDgaLiWF2KrT
+    RUa1ayHQJ+Tm5jJPo9RNLzDnpKNjVFDK/E9zB8tcOTg8dZoeCvQ6FvjbRTi/8MWP
+    Be5m3ef64a8tQxhdJPeMkRAjKnUzsGZbBjttZue3B89XisKytssk1tkXnY62V42Z
+    m5aAiUfkTr/jeZkuZyLI9uF7Np4rws44/XmoEJp/LNWKmCTtvgIR6NP1W+J2LA/g
+    LOIDLNTyy8Ju95ZDdNQ8KJC+Hrb+OiL86SvwHK9gTAqNqbKmFmQkAtpsgmcObRZF
+    tQRzaHPwR+Pj1wmIoi6UMbeUKuPbVYRf2qVPtvtqFAIGhVBobmIHSE1+G2as0zqZ
+    4LkssrjsyRR02K2aoIOl6IB0s1U2vZ8SRUI4HabSYX/rw9iEdWO1f/gD76YBxSxF
+    5d/rtZUAUQEgS1hdrPlzeNsG5H8Mf05lJMghH8GFJfjkqm6OHkGYcJGCns1Mpai0
+    4RS8QEA83rtEmSdY/oubjNGUMeXk0mjkOrSIblSUYixJJYdi+66IjePyCGMRhtd4
+    0TV5WB3ONylUv777yuHRICFZyny63C1vtUIZMGtu67qYjKk4uMKnHC5lWMaSmepG
+    ifSe+u12fpxjD0UirYSMj38vDtCbcFSPcAAu/1YKw4uvN5zcv+YI91tjOkMJuVah
+    -----END CERTIFICATE-----'.freeze
+
     def test_uuid
-      rhsm_cert = ::Cert::RhsmClient.new(CERT)
-      assert_equal rhsm_cert.uuid, '14e98155-731a-4cae-b151-5c504cc30e1a'
+      rhsm_cert_old = ::Cert::RhsmClient.new(OLD_CERT)
+      rhsm_cert_new = ::Cert::RhsmClient.new(NEW_CERT)
+      assert_equal rhsm_cert_old.uuid, '14e98155-731a-4cae-b151-5c504cc30e1a'
+      assert_equal rhsm_cert_new.uuid, '57b26ce3-e4fe-4f79-a377-2c4d9dea5204'
     end
 
     def test_empty_cert
