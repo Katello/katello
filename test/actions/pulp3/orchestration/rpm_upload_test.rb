17e3f53a55d9b93647047ebec0cb504547c826e1
rverdile <51457421+rverdile@users.noreply.github.com>
2022-01-31 15:20:45 -0500

    Fixes #34202 - Incorrect rpm is uploaded (#9903)
    (cherry picked from commit 9803f40e203e49e369378bae6fae0adbf8cb8372)

--- test/actions/pulp3/orchestration/rpm_upload_test.rb
+++ test/actions/pulp3/orchestration/rpm_upload_test.rb
@@ -7,8 +7,10 @@ module ::Actions::Pulp3
     def setup
       @primary = SmartProxy.pulp_primary
       @repo = katello_repositories(:fedora_17_x86_64)
-      tmp_file = File.join(Katello::Engine.root, 'test/fixtures/test_repos/zoo/duck-0.7-1.noarch.rpm')
-      @file = {path: tmp_file, filename: 'duck-0.7.1.noarch.rpm'}
+      file1 = File.join(Katello::Engine.root, 'test/fixtures/test_repos/zoo/duck-0.7-1.noarch.rpm')
+      file2 = File.join(Katello::Engine.root, 'test/fixtures/test_repos/zoo/kangaroo-0.2-1.noarch.rpm')
+      @file1 = {path: file1, filename: 'duck-0.7.1.noarch.rpm'}
+      @file2 = {path: file2, filename: 'kangaroo-0.2-1.noarch.rpm'}
       create_repo(@repo, @primary)
     end
 
@@ -20,19 +22,26 @@ module ::Actions::Pulp3
     end
 
     def test_upload
-      action_result = ""
       @repo.reload
       assert @repo.remote_href
 
       VCR.use_cassette(cassette_name + '_binary', :match_requests_on => [:method, :path, :params]) do
-        action_result = ForemanTasks.sync_task(::Actions::Pulp3::Orchestration::Repository::UploadContent, @repo, @primary, @file, 'rpm')
-      end
-      assert_equal "success", action_result.result
-      @repo.reload
-      repository_reference = Katello::Pulp3::RepositoryReference.find_by(
+        action_result = ForemanTasks.sync_task(::Actions::Pulp3::Orchestration::Repository::UploadContent, @repo, @primary, @file1, 'rpm')
+        assert_equal "success", action_result.result
+        @repo.reload
+        repository_reference = Katello::Pulp3::RepositoryReference.find_by(
+            :root_repository_id => @repo.root.id,
+            :content_view_id => @repo.content_view.id)
+        assert_equal repository_reference.repository_href + "versions/1/", @repo.version_href
+
+        action_result = ForemanTasks.sync_task(::Actions::Pulp3::Orchestration::Repository::UploadContent, @repo, @primary, @file2, 'rpm')
+        assert_equal "success", action_result.result
+        @repo.reload
+        repository_reference = Katello::Pulp3::RepositoryReference.find_by(
           :root_repository_id => @repo.root.id,
           :content_view_id => @repo.content_view.id)
-      assert_equal repository_reference.repository_href + "versions/1/", @repo.version_href
+        assert_equal repository_reference.repository_href + "versions/2/", @repo.version_href
+      end
     end
 
     def test_duplicate_upload
@@ -41,7 +50,7 @@ module ::Actions::Pulp3
       assert @repo.remote_href
 
       VCR.use_cassette(cassette_name + '_binary', :match_requests_on => [:method, :path, :params]) do
-        action_result = ForemanTasks.sync_task(::Actions::Pulp3::Orchestration::Repository::UploadContent, @repo, @primary, @file, 'rpm')
+        action_result = ForemanTasks.sync_task(::Actions::Pulp3::Orchestration::Repository::UploadContent, @repo, @primary, @file1, 'rpm')
       end
       assert_equal "success", action_result.result
       @repo.reload
@@ -51,7 +60,7 @@ module ::Actions::Pulp3
       assert_equal repository_reference.repository_href + "versions/1/", @repo.version_href
 
       VCR.use_cassette(cassette_name + '_binary_duplicate', :match_requests_on => [:method, :path, :params]) do
-        action_result = ForemanTasks.sync_task(::Actions::Pulp3::Orchestration::Repository::UploadContent, @repo, @primary, @file, 'rpm')
+        action_result = ForemanTasks.sync_task(::Actions::Pulp3::Orchestration::Repository::UploadContent, @repo, @primary, @file1, 'rpm')
       end
       assert_equal "success", action_result.result
       @repo.reload
