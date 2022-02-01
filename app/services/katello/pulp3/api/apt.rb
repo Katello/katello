4a363fd945d56ad48f6a30446d9c16320819d4a5
Manisha Singhal <singhal@atix.de>
2022-02-01 18:01:26 +0100

    Fixes #34279 - Use APT verbatim publisher (#9891)

--- app/services/katello/pulp3/api/apt.rb
+++ app/services/katello/pulp3/api/apt.rb
@@ -4,6 +4,13 @@ module Katello
   module Pulp3
     module Api
       class Apt < Core
+        def publication_verbatim_class
+          PulpDebClient::DebVerbatimPublication
+        end
+
+        def publications_verbatim_api
+          PulpDebClient::PublicationsVerbatimApi.new(api_client)
+        end
       end
     end
   end
