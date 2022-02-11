f9a0969983427b25c7612961036d34dc71a55483
andrewgdewar <andrewgdewar@gmail.com>
2022-02-11 18:07:49 -0500

    Fixes #34418 - Update CVV Repositories tab redirects

--- webpack/scenes/ContentViews/Details/Versions/VersionDetails/ContentViewVersionRepositoryCell.js
+++ webpack/scenes/ContentViews/Details/Versions/VersionDetails/ContentViewVersionRepositoryCell.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { translate as __ } from 'foremanReact/common/I18n';
 import { urlBuilder } from 'foremanReact/common/urlHelpers';
-import { isEmpty } from 'lodash';
+import {
+  camelCase,
+  isEmpty,
+} from 'lodash';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import {
   Grid,
   GridItem,
 } from '@patternfly/react-core';
-import InactiveText from '../../../components/InactiveText';
 import ContentConfig from '../../../../Content/ContentConfig';
+import InactiveText from '../../../components/InactiveText';
 
 const ContentViewVersionRepositoryCell = ({
   data: {
@@ -20,11 +24,11 @@ const ContentViewVersionRepositoryCell = ({
   const CONTENT_COUNTS = {
     ansible_collection: {
       name: __('Ansible collections'),
-      url: `ansible_collections?repositoryId=${libraryInstanceId}`,
+      to: `ansibleCollections?library_instance_id=${libraryInstanceId}`,
     },
     deb: {
       name: __('Deb packages'),
-      url: `debs?repositoryId=${libraryInstanceId}`,
+      to: `debPackages?library_instance_id=${libraryInstanceId}`,
     },
     docker_manifest: {
       name: __('Container manifests'),
@@ -36,31 +40,27 @@ const ContentViewVersionRepositoryCell = ({
     },
     docker_tag: {
       name: __('Container image tags'),
-      url: `docker_tags?repositoryId=${libraryInstanceId}`,
+      to: `dockerTags?library_instance_id=${libraryInstanceId}`,
     },
     erratum: {
       name: __('Errata'),
-      url: `errata?repositoryId=${libraryInstanceId}`,
+      to: `errata?library_instance_id=${libraryInstanceId}`,
     },
     file: {
       name: __('Files'),
-      url: `files?repositoryId=${libraryInstanceId}`,
+      to: `files?library_instance_id=${libraryInstanceId}`,
     },
     module_stream: {
       name: __('Module streams'),
-      url: `products/${id}/repositories/${libraryInstanceId}/content/module_streams`,
-    },
-    package: {
-      name: __('Packages'),
-      url: `products/${id}/repositories/${libraryInstanceId}/content/packages`,
+      to: `moduleStreams?library_instance_id=${libraryInstanceId}`,
     },
     package_group: {
       name: __('Package groups'),
-      url: `products/${id}/repositories/${libraryInstanceId}/content/package_groups`,
+      to: `rpmPackageGroups?library_instance_id=${libraryInstanceId}`,
     },
     rpm: {
-      name: __('Rpm packages'),
-      url: `packages?repositoryId=${libraryInstanceId}`,
+      name: __('RPM packages'),
+      to: `rpmPackages?library_instance_id=${libraryInstanceId}`,
     },
     srpm: {
       name: __('Source RPMs'),
@@ -70,7 +70,7 @@ const ContentViewVersionRepositoryCell = ({
   ContentConfig.forEach((type) => {
     CONTENT_COUNTS[type.names.singularLabel] = {
       name: type.names.pluralLowercase,
-      url: `products/${id}/repositories/${libraryInstanceId}/content/${type.names.pluralLabel}`,
+      to: `${camelCase(type.names.pluralLabel)}?library_instance_id=${libraryInstanceId}`,
     };
   });
 
@@ -87,23 +87,40 @@ const ContentViewVersionRepositoryCell = ({
     }
   };
 
+  const CountComponent = ({ countKey }) => {
+    const { to, url, name } = CONTENT_COUNTS[countKey];
+    const count = ContentCounts[countKey];
+    switch (true) {
+      case !!url:
+        return (
+          <a href={urlBuilder(url, '')}>
+            {count} {name}
+          </a>);
+      case !!to:
+        return (
+          <Link to={to}>
+            {count} {name}
+          </Link>);
+      default:
+        return `${count} ${name} `;
+    }
+  };
+
+  CountComponent.propTypes = {
+    countKey: PropTypes.string.isRequired,
+  };
+
   const contentCountArray = Object.keys(CONTENT_COUNTS);
   const contentCountToShow = contentCountArray.filter(key => !!ContentCounts[key]);
   const contentSpan = getContentSpan(contentCountToShow.length);
+
   return (
     <Grid>
       {!isEmpty(contentCountToShow) ?
-        contentCountToShow.map((countKey) => {
-          const { url = undefined, name } = CONTENT_COUNTS[countKey];
-          const count = ContentCounts[countKey];
-          return (
-            <GridItem key={countKey} span={contentSpan}>
-              {url ?
-                <a href={urlBuilder(url, '')}>
-                  {count} {name}
-                </a> : `${count} ${name} `}
-            </GridItem>);
-        }) : <InactiveText text={__('N/A')} />
+        contentCountToShow.map(countKey => (
+          <GridItem key={countKey} span={contentSpan}>
+            <CountComponent countKey={countKey} />
+          </GridItem>)) : <InactiveText text={__('N/A')} />
       }
     </Grid >
   );
