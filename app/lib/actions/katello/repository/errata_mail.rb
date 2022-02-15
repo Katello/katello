7d3c04e841d3b0b217fa388b6e570e2561226ef6
Samir Jha <sjha4@ncsu.edu>
2022-02-15 16:35:59 -0500

    Fixes #34414 - Errata mail should only list new errata (#9931)
    (cherry picked from commit c89e5680bf4c65e5203f9ed0ab1f6e85b313b40c)

--- app/lib/actions/katello/repository/errata_mail.rb
+++ app/lib/actions/katello/repository/errata_mail.rb
@@ -4,17 +4,16 @@ module Actions
       class ErrataMail < Actions::EntryAction
         middleware.use Actions::Middleware::ExecuteIfContentsChanged
 
-        def plan(repo, last_updated = nil, contents_changed = nil)
-          last_updated ||= repo.repository_errata.order('updated_at ASC').last.try(:updated_at) || Time.now
-          plan_self(:repo => repo.id, :last_updated => last_updated.to_s, :contents_changed => contents_changed)
+        def plan(repo, contents_changed = nil)
+          plan_self(:repo => repo.id, :contents_changed => contents_changed)
         end
 
         def run
           ::User.current = ::User.anonymous_admin
-
           repo = ::Katello::Repository.find(input[:repo])
+          last_updated = repo.repository_errata.order('updated_at ASC').last.try(:updated_at) || Time.now
           users = ::User.select { |user| user.receives?(:sync_errata) && user.organization_ids.include?(repo.organization.id) && user.can?(:view_products, repo.product) }.compact
-          errata = ::Katello::Erratum.where(:id => repo.repository_errata.where('katello_repository_errata.updated_at > ?', input[:last_updated].to_datetime).pluck(:erratum_id))
+          errata = ::Katello::Erratum.where(:id => repo.repository_errata.where('katello_repository_errata.updated_at > ?', last_updated).pluck(:erratum_id))
 
           begin
             MailNotification[:sync_errata].deliver(:users => users, :repo => repo, :errata => errata) unless (users.blank? || errata.blank?)
