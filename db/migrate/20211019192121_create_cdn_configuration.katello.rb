3e5514b54b435742297266038039226c558372ea
Partha Aji <paji@redhat.com>
2022-02-16 19:16:18 -0500

    Fixes #34455 - Migration error related to cdn configuration (#9953)
    (cherry picked from commit 274acc476bc65225e98d7725e8ad0216a06fa99c)

--- db/migrate/20211019192121_create_cdn_configuration.katello.rb
+++ db/migrate/20211019192121_create_cdn_configuration.katello.rb
@@ -1,4 +1,8 @@
 class CreateCdnConfiguration < ActiveRecord::Migration[6.0]
+  class FakeCdnConfiguration < Katello::Model
+    self.table_name = 'katello_cdn_configurations'
+  end
+
   def up
     create_table :katello_cdn_configurations do |t|
       t.integer :organization_id
@@ -14,17 +18,22 @@ class CreateCdnConfiguration < ActiveRecord::Migration[6.0]
     add_foreign_key :katello_cdn_configurations, :taxonomies, name: 'katello_cdn_configurations_organization_id', column: :organization_id
     add_foreign_key :katello_cdn_configurations, :katello_content_credentials, name: 'katello_cdn_configurations_ssl_ca_credential_id', column: :ssl_ca_credential_id
 
+    FakeCdnConfiguration.reset_column_information
+
     ::Organization.all.each do |org|
-      Katello::CdnConfiguration.where(
-        organization: org,
+      FakeCdnConfiguration.where(
+        organization_id: org.id,
         url: org.redhat_provider.repository_url || ::Katello::Resources::CDN::CdnResource.redhat_cdn_url
       ).first_or_create!
     end
 
     remove_column :katello_providers, :repository_url
+    ::Katello::Provider.reset_column_information
   end
 
   def down
+    add_column :katello_providers, :repository_url, :string
+    ::Katello::Provider.reset_column_information
     drop_table :katello_cdn_configurations
   end
 end
