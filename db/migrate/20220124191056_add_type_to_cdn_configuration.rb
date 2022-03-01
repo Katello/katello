26afdff1d4917f7b12316c616b28c572bd79c47e
Partha Aji <paji@redhat.com>
2022-03-01 13:54:10 -0500

    fixes #34531 - Resolving a bad migration for CdnConfiguration (#9988)

--- db/migrate/20220124191056_add_type_to_cdn_configuration.rb
+++ db/migrate/20220124191056_add_type_to_cdn_configuration.rb
@@ -1,16 +1,10 @@
 class AddTypeToCdnConfiguration < ActiveRecord::Migration[6.0]
   def change
+    add_index :katello_cdn_configurations, :organization_id, unique: true
     add_column :katello_cdn_configurations, :type, :string, default: ::Katello::CdnConfiguration::CDN_TYPE
 
     ::Katello::CdnConfiguration.reset_column_information
     ::Katello::CdnConfiguration.all.each do |config|
-      unless Setting[:subscription_connection_enabled]
-        # if subscription connection is not enabled
-        # the user most likely wants the type to be airgapped
-        config.update!(type: ::Katello::CdnConfiguration::AIRGAPPED_TYPE)
-        next
-      end
-
       unless config.username.blank? ||
              config.password.blank? ||
              config.upstream_organization_label.blank? ||
