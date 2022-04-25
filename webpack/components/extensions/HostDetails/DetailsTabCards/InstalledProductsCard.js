f839ae522a852fdbbed5959e5f14e3892cc7bb61
Jeremy Lenz <jlenz@redhat.com>
2022-04-25 17:43:40 -0400

    Fixes #34816 - Add Installed Products card

---
+++ webpack/components/extensions/HostDetails/DetailsTabCards/InstalledProductsCard.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { translate as __ } from 'foremanReact/common/I18n';
+import { List, ListItem } from '@patternfly/react-core';
+import CardTemplate from 'foremanReact/components/HostDetails/Templates/CardItem/CardTemplate';
+
+const InstalledProductsCard = ({ isExpandedGlobal, hostDetails }) => {
+  const installedProducts = hostDetails?.subscription_facet_attributes?.installed_products;
+  if (!installedProducts?.length) return null;
+  return (
+    <CardTemplate
+      overrideGridProps={{ rowSpan: 2 }}
+      header={__('Installed products')}
+      expandable
+      isExpandedGlobal={isExpandedGlobal}
+    >
+      <List isPlain>
+        {installedProducts.map(product => (
+          <ListItem key={product.productId}>
+            {product.productName}
+          </ListItem>
+        ))}
+      </List>
+    </CardTemplate>
+  );
+};
+
+InstalledProductsCard.propTypes = {
+  isExpandedGlobal: PropTypes.bool,
+  hostDetails: PropTypes.shape({
+    subscription_facet_attributes: PropTypes.shape({
+      installed_products: PropTypes.arrayOf(PropTypes.shape({
+        productId: PropTypes.string,
+        productName: PropTypes.string,
+      })),
+    }),
+  }),
+};
+
+InstalledProductsCard.defaultProps = {
+  isExpandedGlobal: false,
+  hostDetails: {},
+};
+
+export default InstalledProductsCard;
