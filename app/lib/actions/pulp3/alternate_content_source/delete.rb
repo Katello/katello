8a2f60fef9cef42d47232580fe18949d6635be43
Ian Ballou <ianballou67@gmail.com>
2022-05-25 16:34:55 -0400

    Fixes #34926 - Upgrade to Pulpcore 3.18 (#10119)
    * Fixes #34926 - Upgrade to Pulpcore 3.18
    
    * Refs #34926 - Upgrade to Pulpcore 3.18

--- app/lib/actions/pulp3/alternate_content_source/delete.rb
+++ app/lib/actions/pulp3/alternate_content_source/delete.rb
@@ -1,12 +1,12 @@
 module Actions
   module Pulp3
     module AlternateContentSource
-      class Delete < Pulp3::Abstract
+      class Delete < Pulp3::AbstractAsyncTask
         def plan(acs, smart_proxy)
           plan_self(:acs_id => acs.id, :smart_proxy_id => smart_proxy.id)
         end
 
-        def run
+        def invoke_external_task
           acs = ::Katello::AlternateContentSource.find(input[:acs_id])
           output[:response] = acs.backend_service(smart_proxy).delete_alternate_content_source
         end
--- app/lib/actions/pulp3/alternate_content_source/delete_remote.rb
+++ app/lib/actions/pulp3/alternate_content_source/delete_remote.rb
@@ -1,12 +1,12 @@
 module Actions
   module Pulp3
     module AlternateContentSource
-      class DeleteRemote < Pulp3::Abstract
+      class DeleteRemote < Pulp3::AbstractAsyncTask
         def plan(acs, smart_proxy)
           plan_self(:acs_id => acs.id, :smart_proxy_id => smart_proxy.id)
         end
 
-        def run
+        def invoke_external_task
           acs = ::Katello::AlternateContentSource.find(input[:acs_id])
           output[:response] = acs.backend_service(smart_proxy).delete_remote
         end
