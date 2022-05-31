a5fa4a7f6113564c973f5056b0ad602c231453de
Chris Roberts <chrobert@redhat.com>
2022-05-31 14:43:47 -0400

    Fixes #34974 - Remove enable_dynflow seed to fix nightlies (#10133)

--- db/seeds.d/107-enable_dynflow.rb
+++
@@ -1,8 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# !!! PLEASE KEEP THIS SCRIPT IDEMPOTENT !!!
-#
-User.as(::User.anonymous_api_admin.login) do
-  Setting.find_by(:name => "dynflow_enable_console").update!(:value => true) if Rails.env.development?
-end
