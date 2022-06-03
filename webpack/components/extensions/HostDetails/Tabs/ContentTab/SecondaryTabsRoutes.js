57fac5e4405f84bc6140a96362588ccca997664b
Aliya Tazhibayeva <atazhiba@redhat.com>
2022-06-03 17:06:29 +0000

    Refs #34890 - fix eslint

--- webpack/components/extensions/HostDetails/Tabs/ContentTab/SecondaryTabsRoutes.js
+++ webpack/components/extensions/HostDetails/Tabs/ContentTab/SecondaryTabsRoutes.js
@@ -1,37 +1,27 @@
 import React from 'react';
-import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { PackagesTab } from '../PackagesTab/PackagesTab.js';
 import { ErrataTab } from '../ErrataTab/ErrataTab.js';
 import { ModuleStreamsTab } from '../ModuleStreamsTab/ModuleStreamsTab';
-import  RepositorySetsTab  from '../RepositorySetsTab/RepositorySetsTab';
+import RepositorySetsTab from '../RepositorySetsTab/RepositorySetsTab';
 import { route } from './helpers';
 
-const SecondaryTabRoutes = () => {
-  console.log(route('Repository%20sets'));
-  console.log(useLocation());
-  return (
-    <Switch>
-      <Route path={route('packages')}>
-        <PackagesTab />
-      </Route>
-
-      <Route path={route('errata')}>
-        <ErrataTab />
-      </Route>
-
-      <Route path={route('module-streams')}>
-        <ModuleStreamsTab />
-      </Route>
-
-      <Route path={route('Repository sets')}>
-
-        <RepositorySetsTab />
-      </Route>
-
-      <Redirect to={route('errata')} />
-
-    </Switch>
-  );
-};
+const SecondaryTabRoutes = () => (
+  <Switch>
+    <Route path={route('packages')}>
+      <PackagesTab />
+    </Route>
+    <Route path={route('errata')}>
+      <ErrataTab />
+    </Route>
+    <Route path={route('module-streams')}>
+      <ModuleStreamsTab />
+    </Route>
+    <Route path={route('Repository sets')}>
+      <RepositorySetsTab />
+    </Route>
+    <Redirect to={route('errata')} />
+  </Switch>
+);
 
 export default SecondaryTabRoutes;
--- webpack/components/extensions/HostDetails/Tabs/ContentTab/constants.js
+++ webpack/components/extensions/HostDetails/Tabs/ContentTab/constants.js
@@ -4,7 +4,7 @@ const SECONDARY_TABS = [
   { key: 'packages', title: __('Packages') },
   { key: 'errata', title: __('Errata') },
   { key: 'module-streams', title: __('Module streams') },
-  { key: 'Repository sets', title: __ ('Repository sets')},
+  { key: 'Repository sets', title: __('Repository sets') },
 ];
 
 export default SECONDARY_TABS;
