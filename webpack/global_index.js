8824c7254a9dc457a9859292b31d3ce1bba3c859
Jeremy Lenz <jlenz@redhat.com>
2022-06-09 10:55:58 -0400

    Fixes #35039 - use correct slot for recent communication card item (#10156)

--- webpack/global_index.js
+++ webpack/global_index.js
@@ -44,7 +44,7 @@ addGlobalFill(
   700,
 );
 addGlobalFill('host-overview-cards', 'Installable errata', <ErrataOverviewCard key="errata-overview" />, 1900);
-addGlobalFill('host-overview-cards', 'Recent communication', <RecentCommunicationCardExtensions key="recent-communication" />, 3000);
+addGlobalFill('recent-communication-card-item', 'Recent communication', <RecentCommunicationCardExtensions key="recent-communication" />, 3000);
 
 // Details tab cards & card extensions
 addGlobalFill('host-tab-details-cards', 'Installed products', <InstalledProductsCard key="installed-products" />, 100);
