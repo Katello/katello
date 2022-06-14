5a6f8fe5d0b641e540a79f3fa141df3d765fd78a
Hao Yu <hyu@redhat.com>
2022-06-14 06:04:55 +0000

    Fixes #35053 - User no longer receive errata email notification
    Use "repository_errata.updated_at" before the repository sync is
    started to retrieve all new errata created in the repository
    sync.

--- app/lib/actions/katello/repository/errata_mail.rb
+++ app/lib/actions/katello/repository/errata_mail.rb
@@ -5,15 +5,15 @@ module Actions
         middleware.use Actions::Middleware::ExecuteIfContentsChanged
 
         def plan(repo, contents_changed = nil)
-          plan_self(:repo => repo.id, :contents_changed => contents_changed)
+          last_updated = repo.repository_errata.order('updated_at ASC').last.try(:updated_at) || Time.now
+          plan_self(:repo => repo.id, :contents_changed => contents_changed, :last_updated => last_updated.to_s)
         end
 
         def run
           ::User.current = ::User.anonymous_admin
           repo = ::Katello::Repository.find(input[:repo])
-          last_updated = repo.repository_errata.order('updated_at ASC').last.try(:updated_at) || Time.now
           users = ::User.select { |user| user.receives?(:sync_errata) && user.organization_ids.include?(repo.organization.id) && user.can?(:view_products, repo.product) }.compact
-          errata = ::Katello::Erratum.where(:id => repo.repository_errata.where('katello_repository_errata.updated_at > ?', last_updated).pluck(:erratum_id))
+          errata = ::Katello::Erratum.where(:id => repo.repository_errata.where('katello_repository_errata.updated_at > ?', input['last_updated'].to_datetime).pluck(:erratum_id))
 
           begin
             MailNotification[:sync_errata].deliver(:users => users, :repo => repo, :errata => errata) unless (users.blank? || errata.blank?)
