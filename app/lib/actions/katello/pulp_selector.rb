59de33acc62aa05844b5ed12128bbd9fb1ab44ec
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-06-27 16:58:50 +0200

    Refs #35006 - Remove unused plan_pulp_action method
    Fixes: 901236e5fe931206600af10cd1212cfaf16bcda8

--- app/lib/actions/katello/pulp_selector.rb
+++ app/lib/actions/katello/pulp_selector.rb
@@ -1,14 +1,6 @@
 module Actions
   module Katello
     module PulpSelector
-      def plan_pulp_action(backend_actions, repository, smart_proxy, *args)
-        fail "nil smart_proxy passed to PulpSelector" if smart_proxy.nil?
-        smart_proxy.fix_pulp3_capabilities(repository.content_type)
-        planned = plan_optional_pulp_action(backend_actions, repository, smart_proxy, *args)
-        fail "Could not locate an action for type #{smart_proxy.backend_service_type(repository)}" unless planned
-        planned
-      end
-
       def plan_optional_pulp_action(backend_actions, repository, smart_proxy, *args)
         found_action = select_pulp_action(backend_actions, repository, smart_proxy)
         plan_action(found_action, repository, smart_proxy, *args) if found_action
