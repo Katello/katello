Katello/katello

commit 1545b9ecaa9b5c99c5aa4802bc52e182f49f3980

Fixes #6297 - remove unused and unreachable code that used delayed jobs

app/models/katello/glue/pulp/repo.rb
@@ -609,60 +609,6 @@ module Glue::Pulp::Repo
       sync_history_item['state'] == PulpTaskStatus::Status::FINISHED.to_s
     end
 
-    def generate_metadata(options = {})
-      force_regeneration = options.fetch(:force_regeneration, false)
-      cloned_repo_override = options.fetch(:cloned_repo_override, nil)
-
-      unless force_regeneration
-        clone = cloned_repo_override ||
-            self.content_view_version.repositories.where(:library_instance_id => self.library_instance_id).where("id != #{self.id}").first
-      end
-
-      tasks = []
-      if force_regeneration || self.content_view.default? || clone.nil?
-        tasks << self.publish_distributor
-      else
-        tasks << self.publish_clone_distributor(clone)
-      end
-
-      # If this repository is for an 'archive', it doesn't need to be
-      # published using the node distributor.
-      if !self.archive? && self.find_node_distributor
-        if options[:node_publish_async]
-          self.async(:organization => self.organization,
-                     :task_type => TaskStatus::TYPES[:content_view_node_publish][:type]).publish_node_distributor
-        else
-          tasks << self.publish_node_distributor
-        end
-      end
-
-      tasks
-    end
-
-    def publish_distributor
-      dist = find_distributor
-      dist.nil? ? nil :  Katello.pulp_server.extensions.repository.publish(self.pulp_id, dist['id'])
-    end
-
-    def publish_node_distributor
-      dist = self.find_node_distributor
-      task = Katello.pulp_server.extensions.repository.publish(self.pulp_id, dist['id'])
-      PulpTaskStatus.wait_for_tasks([task])
-      # TODO: is this code still reachable?
-      ::ForemanTasks.sync_task(::Actions::Katello::Repository::NodeMetadataGenerate, self)
-    end
-
-    def publish_clone_distributor(source_repo)
-      dist = find_distributor(true)
-      source_dist = source_repo.find_distributor
-
-      fail "Could not find #{self.content_type} clone distributor for #{self.pulp_id}" if dist.nil?
-      fail "Could not find #{self.content_type} distributor for #{source_repo.pulp_id}" if source_dist.nil?
-      Katello.pulp_server.extensions.repository.publish(self.pulp_id, dist['id'],
-                               :override_config => {:source_repo_id => source_repo.pulp_id,
-                                                    :source_distributor_id => source_dist['id']})
-    end
-
     def find_distributor(use_clone_distributor = false)
       dist_type_id = if use_clone_distributor
                        case self.content_type

app/lib/actions/katello/provider/manifest_delete.rb
@@ -34,7 +34,7 @@ module Actions
 
         def run
           provider = ::Katello::Provider.find(input[:provider][:id])
-          provider.delete_manifest(:async => false, :notify => false)
+          provider.delete_manifest
         end
 
       end

app/lib/actions/katello/provider/manifest_import.rb
@@ -40,9 +40,7 @@ module Actions
         def run
           provider = ::Katello::Provider.find(input[:provider][:id])
           provider.import_manifest(input[:path],
-                                   force:  input[:force],
-                                   async:  false,
-                                   notify: false)
+                                   force:  input[:force])
         end
 
       end

app/lib/actions/katello/provider/manifest_refresh.rb
@@ -35,7 +35,7 @@ module Actions
 
         def run
           provider = ::Katello::Provider.find(input[:provider][:id])
-          provider.refresh_manifest(input[:upstream], :async => false, :notify => false)
+          provider.refresh_manifest(input[:upstream])
         end
 
       end

app/models/katello/concerns/organization_extensions.rb
@@ -146,16 +146,6 @@ module Katello
           end
         end
 
-        def discover_repos(url, notify = false)
-          fail _("Repository Discovery already in progress") if self.repo_discovery_task && !self.repo_discovery_task.finished?
-          fail _("Discovery URL not set.") if url.blank?
-          task = self.async(:organization => self, :task_type => :repo_discovery).start_discovery_task(url, notify)
-          task.parameters = {:url => url}
-          self.task_statuses << task
-          self.save!
-          task
-        end
-
         def redhat_repository_url
           redhat_provider.repository_url
         end

app/models/katello/content_view.rb
@@ -289,167 +289,6 @@ class ContentView < Katello::Model
     environments.where(:library => false).length > 0
   end
 
-  # rubocop:disable MethodLength
-  def publish(options = { })
-    fail "Cannot publish content view without a logged in user." if ::User.current.nil?
-    options = { :async => true, :notify => false }.merge(options)
-
-    version = create_new_version
-    if cve = self.content_view_environment(organization.library)
-      version.content_view_environments << cve
-    else
-      self.add_environment(organization.library, version)
-    end
-
-    if options[:async]
-      task = self.async(:organization => self.organization,
-                         :task_type => TaskStatus::TYPES[:content_view_publish][:type]).
-        publish_content(version, options[:notify])
-
-      version.task_status = task
-      version.save!
-    else
-      version.create_task_status!(
-        :uuid => ::UUIDTools::UUID.random_create.to_s,
-        :user_id => ::User.current.try(:id),
-        :organization => self.organization,
-        :state => Katello::TaskStatus::Status::WAITING,
-        :task_type => TaskStatus::TYPES[:content_view_publish][:type]
-      )
-
-      begin
-        publish_content(version, options[:notify])
-        version.task_status.update_attributes!(:state => Katello::TaskStatus::Status::FINISHED)
-      rescue => e
-        version.task_status.update_attributes!(:state => Katello::TaskStatus::Status::ERROR)
-        raise e
-      end
-    end
-    version
-  end
-
-  def publish_content(version, notify = false)
-    # 1. generate the version repositories
-    publish_version_content(version)
-
-    # 2. generate the library repositories
-    publish_library_yum_content(version)
-    publish_library_puppet_content(version)
-
-    # 3. update candlepin, etc
-    update_cp_content(self.organization.library)
-
-    clone_overrides = self.repositories.select{|r| self.filters.applicable(r).empty?}
-    version.trigger_contents_changed(:cloned_repo_overrides => clone_overrides, :non_archive => true)
-
-    if notify
-      message = _("Successfully published content view '%s'.") % name
-      Notify.success(message, :request_type => "content_view___publish",
-                              :organization => self.organization)
-    end
-  rescue => e
-    Rails.logger.error(e)
-    Rails.logger.error(e.backtrace.join("\n"))
-
-    if notify
-      message = _("Failed to publish content view '%s'.") % self.name
-      Notify.exception(message, e, :request_type => "content_view___publish",
-                                   :organization => self.organization)
-    end
-
-    raise e
-  end
-
-  def publish_library_yum_content(version)
-
-    # prepare the yum repos currently in the library for the publish
-    async_tasks = []
-    repos(organization.library).each do |repo|
-      if repositories_to_publish_ids.include?(repo.library_instance_id)
-        repo.content_view_version_id = version.id
-        repo.save!
-
-        # this repo is in both the content view and in the library,
-        # so clear it and later we'll regenerate the content... this is more
-        # efficient than deleting the repo and recreating it...
-        async_tasks += repo.clear_contents
-      else
-        # this repo no longer exists in the view, so destroy it
-        repo.destroy
-      end
-    end
-    PulpTaskStatus.wait_for_tasks async_tasks unless async_tasks.blank?
-
-    async_tasks = []
-    repos_to_filter = []
-    repositories_to_publish.each do |repo|
-      # the repos from the content view are based upon initial synced repos, we need to
-      # determine if each of those repos has been cloned in library
-      library_clone = get_repo_clone(organization.library, repo).first
-      if library_clone.nil?
-        # this repo doesn't currently exist in the library
-        clone = repo.create_clone(:environment => organization.library, :content_view => self)
-        repos_to_filter << clone
-      else
-        # this repo already exists in the library, so update it
-        library_clone = Repository.find(library_clone) # reload readonly obj
-        repos_to_filter << library_clone
-      end
-    end
-
-    repos_to_filter.each do |repo|
-      associate_yum_content(repo) unless repo.puppet?
-    end
-
-    PulpTaskStatus.wait_for_tasks async_tasks unless async_tasks.blank?
-  end
-
-  def publish_library_puppet_content(version)
-    # prepare the puppet environment currently in the library for the publish
-    async_tasks = []
-    if puppet_env = puppet_env(organization.library)
-      if !content_view_puppet_modules.empty?
-        puppet_env.content_view_version_id = version.id
-        puppet_env.save!
-
-        # this puppet environment has been previously published and the version
-        # being published has puppet modules, so clear it and later we'll
-        # regenerate the content... this is more efficient than deleting the
-        # env/repo and recreating it...
-        async_tasks += puppet_env.clear_contents
-      else
-        # this content view doesn't contain any puppet modules, so destroy
-        # the environment
-        puppet_env.destroy
-      end
-    end
-    PulpTaskStatus.wait_for_tasks async_tasks unless async_tasks.blank?
-
-    unless content_view_puppet_modules.empty?
-      unless puppet_env
-        puppet_env = create_puppet_env(:environment => organization.library, :content_view => self)
-      end
-      associate_puppet_content(puppet_env)
-    end
-
-    PulpTaskStatus.wait_for_tasks async_tasks unless async_tasks.blank?
-  end
-
-  def publish_version_content(version)
-    repositories_to_publish.each do |repo|
-      clone = repo.create_clone(:content_view => self, :version => version)
-      associate_yum_content(clone)
-    end
-
-    unless content_view_puppet_modules.empty?
-      puppet_env = create_puppet_env(:content_view => self, :version => version)
-      associate_puppet_content(puppet_env)
-    end
-
-    clone_overrides = repositories_to_publish.select{|r| self.filters.applicable(r).empty?}
-    version.trigger_contents_changed(:cloned_repo_overrides => clone_overrides, :wait => true)
-  end
-
   def duplicate_repositories
     counts = repositories_to_publish.each_with_object(Hash.new(0)) do |repo, h|
       h[repo.library_instance_id] += 1

app/models/katello/glue/candlepin/environment.rb
@@ -28,46 +28,6 @@ module Glue::Candlepin::Environment
     def content_ids
       self.candlepin_info['environmentContent'].collect{ |c| c['id'] }
     end
-
-    def del_environment
-      Rails.logger.info _("Deleting environment in candlepin: %s") % self.label
-      Resources::Candlepin::Environment.destroy(self.cp_id)
-      true
-    rescue RestClient::ResourceNotFound
-      Rails.logger.info _("Candlepin environment doesn't exist: %s") % self.label
-      true
-    rescue => e
-      Rails.logger.error _("Failed to delete candlepin environment %s") %
-                           "#{self.label}: #{e}, #{e.backtrace.join("\n")}"
-      fail e
-    end
-
-    def update_cp_content
-      all_env_ids = all_env_content_ids
-      saved_cp_ids = saved_env_content_ids
-
-      add_ids = all_env_ids - saved_cp_ids
-      Resources::Candlepin::Environment.add_content(self.cp_id, add_ids) unless add_ids.empty?
-
-      delete_ids = saved_cp_ids - all_env_ids.to_a
-      Resources::Candlepin::Environment.delete_content(self.cp_id, delete_ids) unless delete_ids.empty?
-    end
-
-    protected
-
-    def all_env_content_ids
-      self.content_view.repos(self.owner).select{|r| r.yum?}.reduce(Set.new) do |env_content_ids, repo|
-        env_content_ids << repo.content_id
-      end
-    end
-
-    def saved_env_content_ids
-      Resources::Candlepin::Environment.find(self.cp_id)[:environmentContent].map do |content|
-        content[:contentId]
-      end
-    end
-
   end
-
 end
 end

app/models/katello/glue/provider.rb
@@ -24,39 +24,22 @@ module Glue::Provider
   module InstanceMethods
 
     def import_manifest(zip_file_path, options = {})
-      options = { :async => false, :notify => false , :zip_file_path => zip_file_path}.merge(options)
-      options.assert_valid_keys(:force, :async, :notify, :zip_file_path)
+      options = {:zip_file_path => zip_file_path}.merge(options)
+      options.assert_valid_keys(:force, :zip_file_path)
 
-      if options[:async]
-        self.task_status = async(:organization => self.organization, :task_type => "import manifest").queue_import_manifest(options)
-        self.save!
-      else
-        queue_import_manifest options
-      end
+      queue_import_manifest options
     end
 
     def delete_manifest(options = {})
-      options = { :async => false, :notify => false }.merge(options)
-      options.assert_valid_keys(:async, :notify)
-
-      if options[:async]
-        self.task_status = async(:organization => self.organization, :task_type => "delete manifest").queue_delete_manifest(options)
-        self.save!
-      else
-        queue_delete_manifest(options)
-      end
+      options = options.dup
+      queue_delete_manifest(options)
     end
 
     def refresh_manifest(upstream, options = {})
-      options = { :async => true, :notify => false, :upstream => upstream }.merge(options)
-      options.assert_valid_keys(:async, :notify, :upstream)
+      options = { :upstream => upstream }.merge(options)
+      options.assert_valid_keys(:upstream)
 
-      if options[:async]
-        self.task_status = async(:organization => self.organization, :task_type => "refresh manifest").queue_import_manifest(options)
-        self.save!
-      else
-        queue_import_manifest(options)
-      end
+      queue_import_manifest(options)
     end
 
     def sync
@@ -280,21 +263,6 @@ module Glue::Provider
                          :priority => 6, :action => [self, :refresh_existing_products]) if manifest_update && Katello.config.use_pulp
 
         self.save!
-
-        if options[:notify]
-          message = if Katello.config.katello?
-                      _("Subscription manifest uploaded successfully for provider '%s'. " +
-                            "Please enable the repositories you want to sync by selecting 'Enable Repositories' and " +
-                            "selecting individual repositories to be enabled.")
-                    else
-                      _("Subscription manifest uploaded successfully for provider '%s'.")
-                    end
-          values = [self.name]
-          Notify.success message % values,
-                         :request_type => 'providers__update_redhat_provider',
-                         :organization => self.organization,
-                         :details      => output.read.dup
-        end
       rescue => error
         display_manifest_message(manifest_refresh ? 'refresh' : 'import', error, options)
         raise error

commit b16cd4310d36da586fc7f192ae9755162eb17b74

Remove unused clear_contents method
Fixes: 1545b9ecaa9b5c99c5aa4802bc52e182f49f3980

app/models/katello/glue/pulp/repo.rb
@@ -92,14 +92,6 @@ module Katello
         content_unit_counts
       end
 
-      def clear_contents
-        tasks = content_types.flat_map { |type| type.unassociate_from_repo(self.pulp_id, {}) }
-
-        tasks << Katello.pulp_server.extensions.repository.unassociate_units(self.pulp_id,
-                   :type_ids => ['rpm'], :filters => {}, :fields => { :unit => Pulp::Rpm::PULP_SELECT_FIELDS})
-        tasks
-      end
-
       def content_types
         [Katello.pulp_server.extensions.errata,
          Katello.pulp_server.extensions.package_group,