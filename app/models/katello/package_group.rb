cc9ea870153f37d9b2faa6d3c2521c3d9353ec83
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-06-27 15:13:05 +0200

    Refs #35006 - Remove unused class PackageClauseGenerator
    Fixes: 7c7910e830e158c087602f1ce35d1acfc9d3ac64

--- app/models/katello/package_group.rb
+++ app/models/katello/package_group.rb
@@ -12,18 +12,6 @@ module Katello
       self.repositories.first
     end
 
-    def self.list_by_filter_clauses(clauses)
-      package_names = []
-      pulp_package_groups = Katello.pulp_server.extensions.package_group.search(Katello::PackageGroup::CONTENT_TYPE, :filters => clauses)
-      groupings = [:default_package_names, :conditional_package_names, :optional_package_names, :mandatory_package_names]
-      if pulp_package_groups.any?
-        pulp_package_groups.flat_map { |group| groupings.each { |grouping| package_names << group[grouping] } }
-        package_names.flatten!
-      else
-        []
-      end
-    end
-
     def package_names
       service_class = SmartProxy.pulp_primary!.content_service(CONTENT_TYPE)
       group = service_class.new(self.pulp_id)
