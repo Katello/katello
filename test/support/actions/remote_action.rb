cb2ee1b518982323212441b1c48ef88d3503577d
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-06-27 15:01:44 +0200

    Remove unused runcible_expects method

--- test/support/actions/remote_action.rb
+++ test/support/actions/remote_action.rb
@@ -8,23 +8,6 @@ module Support
           User.current = users(:one)
         end
       end
-
-      # runcible_expects(action, :extensions, :consumer, :create).with('uuid')
-      # will check that pulp_extensions.consumer.create('uuid') was called in the
-      # action.
-      def runcible_expects(action, entry_method, *method_chain)
-        method_chain = method_chain.dup
-        if [:resources, :extensions].include?(entry_method)
-          method_chain.unshift "pulp_#{entry_method}"
-        else
-          fail "Unexpected entry method: #{entry_method}"
-        end
-        last_method = method_chain.pop
-        last_stub = method_chain.reduce(action) do |target, method|
-          stub(method).tap { |next_stub| target.expects(method).returns(next_stub) }
-        end
-        last_stub.expects(last_method)
-      end
     end
   end
 end
