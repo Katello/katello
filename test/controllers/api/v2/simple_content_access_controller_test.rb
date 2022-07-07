42bbf892bb5e64c2765deb983e1b1725f5e31108
Partha Aji <parthaa@gmail.com>
2022-07-07 15:29:41 -0400

    Update test/controllers/api/v2/simple_content_access_controller_test.rb

--- test/controllers/api/v2/simple_content_access_controller_test.rb
+++ test/controllers/api/v2/simple_content_access_controller_test.rb
@@ -93,6 +93,7 @@ module Katello
       assert_response :success
       refute(body['simple_content_access'])
     end
+
     def test_status_protected
       allowed_perms = [permission]
       denied_perms = []
