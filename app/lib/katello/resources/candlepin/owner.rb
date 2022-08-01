5ee2cf3ef2753587856e12179e3ec6aaa918558f
Jeremy Lenz <jlenz@redhat.com>
2022-08-01 14:01:24 -0400

    Refs #35312 - write contentAccessModeList on org update

--- app/lib/katello/resources/candlepin/owner.rb
+++ app/lib/katello/resources/candlepin/owner.rb
@@ -18,7 +18,7 @@ module Katello
               :displayName => description,
               :contentPrefix => "/#{key}/$env",
               :contentAccessMode => content_access_mode,
-              :contentAccessModeList => ['org_environment', 'entitlement'].join(',')
+              :contentAccessModeList => ['entitlement', 'org_environment'].join(',')
             }
             owner_json = self.post(path, attrs.to_json, self.default_headers).body
             JSON.parse(owner_json).with_indifferent_access
@@ -42,6 +42,7 @@ module Katello
           def update(key, attrs)
             owner = find(key)
             owner.merge!(attrs)
+            owner.merge!(:contentAccessModeList => ['entitlement', 'org_environment'].join(','))
             self.put(path(key), JSON.generate(owner), self.default_headers).body
           end
 
