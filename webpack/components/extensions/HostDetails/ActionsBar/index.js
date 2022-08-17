dc10b82fe5af4bc9141a7f6d20fd1fb7e24b99c4
Chris Roberts <chrobert@redhat.com>
2022-08-17 09:37:18 -0400

    Fixes #35367 - Add legacy ch host ui button to new host page (#10235)

--- webpack/components/extensions/HostDetails/ActionsBar/index.js
+++ webpack/components/extensions/HostDetails/ActionsBar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { DropdownItem } from '@patternfly/react-core';
-import { CubeIcon } from '@patternfly/react-icons';
+import { CubeIcon, UndoIcon } from '@patternfly/react-icons';
 
 import { translate as __ } from 'foremanReact/common/I18n';
 import { foremanUrl } from 'foremanReact/common/helpers';
@@ -13,6 +13,13 @@ const HostActionsBar = () => {
 
   return (
     <>
+      <DropdownItem
+        key="katello-legacy-contenthost-ui"
+        href={foremanUrl(`/content_hosts/${hostDetails?.id}`)}
+        icon={<UndoIcon />}
+      >
+        {__('Legacy content host UI')}
+      </DropdownItem>
       <DropdownItem
         key="katello-change-host-content-source"
         href={foremanUrl(`/change_host_content_source?host_id=${hostDetails?.id}`)}
