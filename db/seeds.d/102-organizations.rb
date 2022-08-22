72b86f71c2d2eafd68eb9fd1a6d2aded40ad2e65
Samir Jha <samirjha1525@gmail.com>
2022-08-22 13:26:19 -0400

    Refs #35246 - Update hash of seed file with minor comment change to ensure db:seed runs (#10240)
    (cherry picked from commit 06e4ebe0aa1f38b86f2233d1a67438f8eb6b0553)

--- db/seeds.d/102-organizations.rb
+++ db/seeds.d/102-organizations.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# !!! PLEASE KEEP THIS SCRIPT IDEMPOTENT !!!
+# !! PLEASE KEEP THIS SCRIPT IDEMPOTENT !!
 #
 
 unless Rails.env.test?
