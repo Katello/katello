bc66aca4d91bd77385ba9bf697b99ebe32edd1fc
Ian Ballou <ianballou67@gmail.com>
2022-09-22 15:22:22 -0400

    Fixes #35504 - Simplified ACSs are being created during content view publishing (#10284)
    (cherry picked from commit 4b5be48625b3cbfa3f5ceaa5506f9b62e3ddbdca)

--- app/lib/actions/katello/repository/create.rb
+++ app/lib/actions/katello/repository/create.rb
@@ -34,7 +34,7 @@ module Actions
 
             concurrence do
               plan_self(:repository_id => repository.id, :clone => clone)
-              if repository.url.present?
+              if !clone && repository.url.present?
                 repository.product.alternate_content_sources.with_type(repository.content_type).each do |acs|
                   acs.smart_proxies.each do |smart_proxy|
                     smart_proxy_acs = ::Katello::SmartProxyAlternateContentSource.create(alternate_content_source_id: acs.id, smart_proxy_id: smart_proxy.id, repository_id: repository.id)
