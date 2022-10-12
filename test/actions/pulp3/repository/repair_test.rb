68d0eb915524c80b529136136eb3542d5000e182
Samir Jha <sjha4@ncsu.edu>
2022-10-12 16:29:46 -0400

    Fixes #35600 - Fix repository repair API call (#10306)
    (cherry picked from commit e8a883b2a64cdc2d40e59faca28f7b66280baa37)

---
+++ test/actions/pulp3/repository/repair_test.rb
@@ -0,0 +1,23 @@
+require 'katello_test_helper'
+
+module ::Actions::Pulp3::Repository
+  class RepairTest < ActiveSupport::TestCase
+    include Katello::Pulp3Support
+
+    def setup
+      User.current = users(:admin)
+      @primary = SmartProxy.pulp_primary
+      @repo = katello_repositories(:fedora_17_x86_64_duplicate)
+      @repo.root.update!(url: 'https://jlsherrill.fedorapeople.org/fake-repos/needed-errata/')
+      create_repo(@repo, @primary)
+      ForemanTasks.sync_task(
+        ::Actions::Katello::Repository::MetadataGenerate, @repo)
+    end
+
+    def test_repair_task_succeeds
+      task = ForemanTasks.sync_task(::Actions::Pulp3::Repository::Repair,
+                                     @repo.id, @primary)
+      assert_equal task.result, "success"
+    end
+  end
+end
