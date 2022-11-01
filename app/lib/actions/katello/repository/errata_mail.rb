e626c4ce9078ddfd36bfe3216a85bacc96427212
William Bradford Clark <wclark@redhat.com>
2022-11-01 11:49:45 -0400

    Fixes #35503 - Notify user only when new errata are added to repo (#10328)
    Co-authored-by: William Bradford Clark <wclark@redhat.com>
    Co-authored-by: Pavel Moravec <pmoravec@redhat.com>
    
    Co-authored-by: Hao Yu <hyu@redhat.com>
    Co-authored-by: Pavel Moravec <pmoravec@redhat.com>

--- app/lib/actions/katello/repository/errata_mail.rb
+++ app/lib/actions/katello/repository/errata_mail.rb
@@ -2,18 +2,21 @@ module Actions
   module Katello
     module Repository
       class ErrataMail < Actions::EntryAction
-        middleware.use Actions::Middleware::ExecuteIfContentsChanged
-
-        def plan(repo, contents_changed = nil)
-          last_updated = repo.repository_errata.order('updated_at ASC').last.try(:updated_at) || Time.now
-          plan_self(:repo => repo.id, :contents_changed => contents_changed, :last_updated => last_updated.to_s)
+        def plan(repo)
+          plan_self(:repo => repo.id, :associated_errata_before_syncing => repo.repository_errata.pluck(:erratum_id).uniq.sort.reverse, :new_associated_errata => [])
         end
 
         def run
           ::User.current = ::User.anonymous_admin
           repo = ::Katello::Repository.find(input[:repo])
+          input[:new_associated_errata] = repo.repository_errata.pluck(:erratum_id).uniq.sort.reverse - input[:associated_errata_before_syncing]
+
           users = ::User.select { |user| user.receives?(:sync_errata) && user.organization_ids.include?(repo.organization.id) && user.can?(:view_products, repo.product) }.compact
-          errata = ::Katello::Erratum.where(:id => repo.repository_errata.where('katello_repository_errata.updated_at > ?', input['last_updated'].to_datetime).pluck(:erratum_id))
+          errata = ::Katello::Erratum.where(:id => input[:new_associated_errata])
+
+          [:associated_errata_before_syncing, :new_associated_errata].each do |key|
+            input[key] = "Trimmed list... (#{input[key].length} #{key.to_s.gsub('_', ' ')})" if input[key].length > 3
+          end
 
           begin
             MailNotification[:sync_errata].deliver(:users => users, :repo => repo, :errata => errata) unless (users.blank? || errata.blank?)
