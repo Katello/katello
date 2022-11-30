28e0136ae8c9820a1cd179eb0ab02781bfbf21dc
Samir Jha <sjha4@ncsu.edu>
2022-11-30 16:40:01 -0500

    Fixes #35776 - Add pulp_deb monkeypatch for pulp 3.21 - 3.18 sync (#10369)

--- config/initializers/monkeys.rb
+++ config/initializers/monkeys.rb
@@ -2,4 +2,5 @@
 require 'monkeys/anemone'
 require 'monkeys/ar_postgres_evr_t'
 require 'monkeys/fx_sqlite_skip'
-require 'monkeys/try_pulp_container_path'
+require 'monkeys/fix_pulp_container_path'
+require 'monkeys/fix_deb_optimized_sync'
