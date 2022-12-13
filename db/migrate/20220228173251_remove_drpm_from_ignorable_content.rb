6297a9f18cf1ab7e3c044c7dcd6661dca26daade
Samir Jha <sjha4@ncsu.edu>
2022-12-13 19:13:59 +0000

    Fixes #35864 - Remove all non-srpm content types from ignorable content

--- db/migrate/20220228173251_remove_drpm_from_ignorable_content.rb
+++ db/migrate/20220228173251_remove_drpm_from_ignorable_content.rb
@@ -1,12 +1,16 @@
 class RemoveDrpmFromIgnorableContent < ActiveRecord::Migration[6.0]
   def up
-    Katello::RootRepository.select { |r| r&.ignorable_content&.include? "drpm" }.each do |root|
-      root.ignorable_content = root.ignorable_content - ["drpm"]
+    Katello::RootRepository.select { |r| !r&.ignorable_content&.blank? }.each do |root|
+      if root&.ignorable_content&.include?("srpm")
+        root.ignorable_content = ["srpm"]
+      else
+        root.ignorable_content = []
+      end
       root.save!
     end
   end
 
   def down
-    fail ActiveRecord::IrreversibleMigration
+    #noop
   end
 end
