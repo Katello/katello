ea835a6adaf4e044dcdbf179aa75fad7f9da2888
Stejskal Leos <github@stejskalleos.cz>
2023-01-11 09:39:39 +0000

    Fixes #35919 - Change content source UX improvements
    (cherry picked from commit 0c258941b12848d3932f6b24923585c3cc7b1fd6)

--- webpack/scenes/Hosts/ChangeContentSource/index.js
+++ webpack/scenes/Hosts/ChangeContentSource/index.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { Alert, Grid, GridItem } from '@patternfly/react-core';
+import { Alert, Grid, GridItem, PageSection, Title } from '@patternfly/react-core';
 
 import { translate as __ } from 'foremanReact/common/I18n';
+import { foremanUrl } from 'foremanReact/common/helpers';
 import { STATUS } from 'foremanReact/constants';
+import BreadcrumbBar from 'foremanReact/components/BreadcrumbBar';
+import Head from 'foremanReact/components/Head';
 
 import { selectApiDataStatus,
   selectApiContentViewStatus,
@@ -53,6 +56,8 @@ const ChangeContentSourcePage = () => {
   const [environmentId, setEnvironmentId] = useState('');
   const [contentViewId, setContentViewId] = useState('');
 
+  const noHostSpecified = getHostIds(urlParams.host_id).length === 0 && urlParams.searchParam === '';
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -61,6 +66,7 @@ const ChangeContentSourcePage = () => {
       contentViewId,
       contentSourceId,
       contentHosts.map(h => h.id),
+      jobInvocationPath,
     ));
   };
 
@@ -72,6 +78,19 @@ const ChangeContentSourcePage = () => {
     if (id) {
       dispatch(getProxy(id));
     }
+  };
+
+  const breadcrumbItems = () => {
+    const linkHosts = { caption: __('Hosts'), url: foremanUrl('/hosts') };
+    const linkContent = { caption: __('Change host content source') };
+
+    if (urlParams.host_id) {
+      const hostName = contentHosts.concat(hostsWithoutContent)
+        .find(h => `${h.id}` === urlParams.host_id)?.name;
+
+      return ([linkHosts, { caption: hostName, url: foremanUrl(`/new/hosts/${hostName}`) }, linkContent]);
+    }
+    return ([linkHosts, linkContent]);
   };
 
   const handleEnvironment = (envId) => {
@@ -86,45 +105,66 @@ const ChangeContentSourcePage = () => {
     dispatch(getFormData(getHostIds(urlParams.host_id), urlParams.searchParam));
   }, [dispatch, urlParams.host_id, urlParams.searchParam]);
 
-  if (getHostIds(urlParams.host_id).length === 0 && urlParams.searchParam === '') {
-    return (
-      <Grid className="margin-40">
-        <GridItem span={7}>
-          <Alert
-            variant="danger"
-            title={__('No hosts with content source found!')}
-          />
-        </GridItem>
-      </Grid>);
-  }
-
   return (
-    <Grid className="margin-40">
-      <GridItem span={7}>
-        <h1>{__('Change host content source')}</h1>
-      </GridItem>
-      <Hosts
-        contentHosts={contentHosts}
-        hostsWithoutContent={hostsWithoutContent}
-      />
+    <>
+      <Head>
+        <title>{__('Change host content source')}</title>
+      </Head>
+      <PageSection
+        isFilled
+        variant="light"
+      >
+        <div className="margin-left-20">
+          <BreadcrumbBar
+            breadcrumbItems={breadcrumbItems()}
+          />
+        </div>
+        <Grid className="margin-left-20">
+          <GridItem span={7}>
+            <Title
+              headingLevel="h5"
+              size="2xl"
+            >
+              {__('Change host content source')}
+            </Title>
+          </GridItem>
+          {noHostSpecified &&
+            <GridItem span={7}>
+              <Alert
+                variant="danger"
+                className="margin-top-20"
+                title={__('No hosts were specified')}
+              />
+            </GridItem>
+        }
+          { !noHostSpecified &&
+          <>
+            <Hosts
+              contentHosts={contentHosts}
+              hostsWithoutContent={hostsWithoutContent}
+            />
 
-      <ContentSourceForm
-        handleSubmit={handleSubmit}
-        environments={environments}
-        handleEnvironment={handleEnvironment}
-        environmentId={environmentId}
-        contentViews={contentViews}
-        handleContentView={setContentViewId}
-        contentViewId={contentViewId}
-        contentSources={contentSources}
-        contentSourceId={contentSourceId}
-        handleContentSource={handleContentSource}
-        contentHosts={contentHosts}
-        isLoading={isLoading}
-      />
-      { apiChangeStatus === STATUS.RESOLVED &&
-      <ContentSourceTemplate template={template} jobInvocationPath={jobInvocationPath} /> }
-    </Grid>
+            <ContentSourceForm
+              handleSubmit={handleSubmit}
+              environments={environments}
+              handleEnvironment={handleEnvironment}
+              environmentId={environmentId}
+              contentViews={contentViews}
+              handleContentView={setContentViewId}
+              contentViewId={contentViewId}
+              contentSources={contentSources}
+              contentSourceId={contentSourceId}
+              handleContentSource={handleContentSource}
+              contentHosts={contentHosts}
+              isLoading={isLoading}
+              hostsUpdated={apiChangeStatus === STATUS.RESOLVED}
+            />
+          </> }
+          { apiChangeStatus === STATUS.RESOLVED &&
+          <ContentSourceTemplate template={template} jobInvocationPath={jobInvocationPath} /> }
+        </Grid>
+      </PageSection>
+    </>
   );
 };
 
