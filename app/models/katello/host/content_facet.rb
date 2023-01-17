2011fa5903625da263e270b9d1f27ee0565153eb
Samir Jha <samirjha1525@gmail.com>
2023-01-17 15:05:24 +0000

    Refs #35861 - Do not allow blank for content facet host

--- app/models/katello/host/content_facet.rb
+++ app/models/katello/host/content_facet.rb
@@ -38,7 +38,7 @@ module Katello
 
       validates_with ::AssociationExistsValidator, attributes: [:content_source]
       validates_with Katello::Validators::GeneratedContentViewValidator
-      validates :host, :presence => true, :allow_blank => true
+      validates :host, :presence => true, :allow_blank => false
 
       attr_accessor :cves_changed
 
