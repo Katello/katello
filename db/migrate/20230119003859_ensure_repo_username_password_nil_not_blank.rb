6657ac51c5c978beceb5333657fe3a64e0d8081d
Ian Ballou <ianballou67@gmail.com>
2023-01-26 12:37:28 -0500

    Fixes #35927 - ensure upstream auth is nil if empty string (#10415)
    (cherry picked from commit b1cce6066630e14e3c7ad930d63a3af14dca3bce)

---
+++ db/migrate/20230119003859_ensure_repo_username_password_nil_not_blank.rb
@@ -0,0 +1,9 @@
+class EnsureRepoUsernamePasswordNilNotBlank < ActiveRecord::Migration[6.1]
+  def change
+    ::Katello::Repository.library.each do |repo|
+      if repo.upstream_username == '' && repo.upstream_password == ''
+        repo.update(upstream_username: nil, upstream_password: nil)
+      end
+    end
+  end
+end
