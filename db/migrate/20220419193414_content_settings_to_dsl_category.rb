87ddaf179f9598bf11b2bce319d0310fc23aee68
Leos Stejskal <github@stejskalleos.cz>
2023-01-26 14:46:57 +0100

    Fixes #36007 - Migration error 'column settings.category does not exist'

--- db/migrate/20220419193414_content_settings_to_dsl_category.rb
+++ db/migrate/20220419193414_content_settings_to_dsl_category.rb
@@ -1,5 +1,5 @@
 class ContentSettingsToDslCategory < ActiveRecord::Migration[6.0]
   def up
-    Setting.where(category: 'Setting::Content').update_all(category: 'Setting')
+    Setting.where(category: 'Setting::Content').update_all(category: 'Setting') if column_exists?(:settings, :category)
   end
 end
