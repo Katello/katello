96c23ce7ffee083be13bb0cfcfa444fb28b5c934
Jeremy Lenz <jlenz@redhat.com>
2023-02-06 15:36:15 -0500

    Fixes #36043 - assign content view correctly when reassigning hosts (#10437)

--- app/lib/actions/katello/host/reassign.rb
+++ app/lib/actions/katello/host/reassign.rb
@@ -3,8 +3,10 @@ module Actions
     module Host
       class Reassign < Actions::Base
         def plan(host, content_view_id, environment_id)
-          host.content_facet.content_view = ::Katello::ContentView.find(content_view_id)
-          host.content_facet.lifecycle_environment = ::Katello::KTEnvironment.find(environment_id)
+          host.content_facet.assign_single_environment(
+            content_view: ::Katello::ContentView.find(content_view_id),
+            lifecycle_environment: ::Katello::KTEnvironment.find(environment_id)
+          )
           host.update_candlepin_associations
         end
       end
