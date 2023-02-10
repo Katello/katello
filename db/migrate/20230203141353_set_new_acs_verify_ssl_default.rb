e8226e287df3af2438dbe3f7dcb594fcef658b9a
Quinn James <qjames@redhat.com>
2023-02-10 13:13:06 +0000

    Refs #36038 - Fixed Rubocop styling

--- db/migrate/20230203141353_set_new_acs_verify_ssl_default.rb
+++ db/migrate/20230203141353_set_new_acs_verify_ssl_default.rb
@@ -1,5 +1,5 @@
 class SetNewAcsVerifySslDefault < ActiveRecord::Migration[6.1]
-    def change
-      change_column_default(:katello_alternate_content_sources, :verify_ssl, nil)
-    end
+  def change
+    change_column_default(:katello_alternate_content_sources, :verify_ssl, nil)
   end
+end
