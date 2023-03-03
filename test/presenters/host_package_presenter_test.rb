449e319762f962c33dafd8b123f3f7f1d9feb1f5
Lucy Fu <lufu@redhat.com>
2023-03-03 17:54:52 +0000

    Fixes #36100 - check arch when looking for package upgrades

--- test/presenters/host_package_presenter_test.rb
+++ test/presenters/host_package_presenter_test.rb
@@ -7,7 +7,7 @@ module Katello
       @rpm = katello_rpms(:one)
     end
 
-    let(:installed_package) { Katello::InstalledPackage.create(name: @rpm.name, nvra: @rpm.nvra, version: @rpm.version, release: @rpm.release, nvrea: @rpm.nvrea) }
+    let(:installed_package) { Katello::InstalledPackage.create(name: @rpm.name, nvra: @rpm.nvra, version: @rpm.version, release: @rpm.release, nvrea: @rpm.nvrea, arch: @rpm.arch) }
     let(:new_version) { 'one-1.2-5.el7.x86_64' }
 
     test "with set upgradable_version" do
@@ -29,10 +29,23 @@ module Katello
     test "with_latest" do
       host = katello_content_facets(:content_facet_one).host
       host.content_facet.bound_repositories << @repo
-      @rpm.update(version: '1.2', nvra: new_version, release: '5')
+      update = Katello::Rpm.create(name: 'one', pulp_id: 'one-new-uuid', version: '1.2', nvra: new_version, release: '5', arch: 'x86_64')
+      ::Katello::Rpm.stubs(:installable_for_hosts).returns(Katello::Rpm.where(id: update.id))
       presenter = HostPackagePresenter.with_latest([installed_package], host).first
 
       assert_equal presenter.upgradable_versions, [new_version]
+      assert_equal presenter.name, installed_package.name
+      assert_equal presenter.rpm_id, @rpm.id
+    end
+
+    test "with arch" do
+      host = katello_content_facets(:content_facet_one).host
+      host.content_facet.bound_repositories << @repo
+      update = Katello::Rpm.create(name: 'one', pulp_id: 'one-new-uuid', version: '1.2', nvra: 'one-1.2-5.el7.noarch', release: '5', arch: 'noarch')
+      ::Katello::Rpm.stubs(:installable_for_hosts).returns(Katello::Rpm.where(id: update.id))
+      presenter = HostPackagePresenter.with_latest([installed_package], host).first
+
+      assert_nil presenter.upgradable_versions
       assert_equal presenter.name, installed_package.name
       assert_equal presenter.rpm_id, @rpm.id
     end
