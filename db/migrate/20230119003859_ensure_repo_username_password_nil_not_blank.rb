33450d34117aea6b434e9d32db1fafb02bbe57c1
Ian Ballou <ianballou67@gmail.com>
2023-03-14 15:37:00 -0400

    Fixes #36196 - upstream auth migration should refer to root (#10490)
    (cherry picked from commit 623da34ea7b1b29656096058ee4841c46ddd21fc)

--- db/migrate/20230119003859_ensure_repo_username_password_nil_not_blank.rb
+++ db/migrate/20230119003859_ensure_repo_username_password_nil_not_blank.rb
@@ -2,7 +2,7 @@ class EnsureRepoUsernamePasswordNilNotBlank < ActiveRecord::Migration[6.1]
   def change
     ::Katello::Repository.library.each do |repo|
       if repo.upstream_username == '' && repo.upstream_password == ''
-        repo.update(upstream_username: nil, upstream_password: nil)
+        repo.root.update(upstream_username: nil, upstream_password: nil)
       end
     end
   end
