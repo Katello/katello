449922b3637236f234b1d6f960ab4565f089c938
Lucy Fu <lufu@redhat.com>
2023-04-10 15:42:21 +0000

    Fixes #36287 - Manifest import uses async candlepin API

--- test/support/scenario_support.rb
+++ test/support/scenario_support.rb
@@ -21,7 +21,7 @@ class ScenarioSupport
   def import_manifest(owner_label, path_to_file)
     filename = path_to_file.split('/').last
     record("import_manifest_#{filename}", match_requests_on: [:method, :path, :params]) do
-      path = "/candlepin/owners/#{owner_label}/imports?force=SIGNATURE_CONFLICT&force=MANIFEST_SAME"
+      path = "/candlepin/owners/#{owner_label}/imports/async?force=SIGNATURE_CONFLICT&force=MANIFEST_SAME"
       client = ::Katello::Resources::Candlepin::CandlepinResource.rest_client(Net::HTTP::Post, :post, path)
       body = {:import => File.new(path_to_file, 'rb')}
       client.post body, {:accept => :json}.merge(User.cp_oauth_header)
