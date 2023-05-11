06c0d3ae15cb849fe451eead0e3ab81e2fe3285a
Samir Jha <sjha4@ncsu.edu>
2023-05-11 14:49:25 +0000

    Fixes #36390 - Orphan cleanup on capsules

--- app/lib/actions/pulp3/orchestration/orphan_cleanup/remove_orphans.rb
+++ app/lib/actions/pulp3/orchestration/orphan_cleanup/remove_orphans.rb
@@ -7,13 +7,13 @@ module Actions
             if proxy.pulp3_enabled?
               sequence do
                 plan_action(Actions::Pulp3::OrphanCleanup::DeleteOrphanRepositoryVersions, proxy)
-                plan_action(Actions::Pulp3::OrphanCleanup::RemoveOrphans, proxy)
                 if proxy.pulp_mirror?
                   plan_action(Actions::Pulp3::OrphanCleanup::RemoveUnneededRepos, proxy)
                   plan_action(Actions::Pulp3::OrphanCleanup::DeleteOrphanDistributions, proxy)
                   plan_action(Actions::Pulp3::OrphanCleanup::DeleteOrphanAlternateContentSources, proxy)
                   plan_action(Actions::Pulp3::OrphanCleanup::DeleteOrphanRemotes, proxy)
                 end
+                plan_action(Actions::Pulp3::OrphanCleanup::RemoveOrphans, proxy)
               end
             end
           end
