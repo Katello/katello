5c11c02530cb5f6e9adcb67c694c0a479ff96ca1
William Bradford Clark <wclark@redhat.com>
2023-06-16 11:51:24 -0400

    Fixes #36482 - Use string keys for accessing 'arch' and 'minor' in repo hash

--- test/lib/resources/katello_cdn_test.rb
+++ test/lib/resources/katello_cdn_test.rb
@@ -55,4 +55,38 @@ class KatelloCdnResourceTest < ActiveSupport::TestCase
     end
     assert_equal(exception.message, "Repository with content label: 'test', arch: 'noarch', version: '8server' was not found in upstream organization 'test', content view 'test' and lifecycle environment 'test'")
   end
+
+  def test_fetch_paths
+    katello_cdn = ::Katello::Resources::CDN::KatelloCdn.new('https://test.com', {
+                                                              organization_label: 'test',
+                                                              content_view_label: 'test',
+                                                              lifecycle_environment_label: 'test'
+                                                            })
+
+    content_path = 'rhel-6-server-els-rpms'
+    repo_set = {
+      'label' => content_path
+    }
+
+    response_body = {
+      'results' => [
+        {
+          'arch' => 'i386',
+          'minor' => '6Server'
+        }
+      ]
+    }
+
+    expected_url = "/katello/api/v2/repositories?full_result=true&organization_id=#{@organization['id']}&content_view_id=2&environment_id=2&search=#{CGI.escape("content_label = #{content_path}")}"
+
+    ::Katello::Resources::CDN::KatelloCdn.any_instance.expects(:fetch_repo_set).with(content_path).returns(repo_set)
+    ::Katello::Resources::CDN::KatelloCdn.any_instance.expects(:get).with(expected_url).returns(response_body.to_json)
+    ::Katello::Resources::CDN::KatelloCdn.any_instance.expects(:organization).returns(@organization)
+    ::Katello::Resources::CDN::KatelloCdn.any_instance.expects(:content_view_id).returns(2)
+    ::Katello::Resources::CDN::KatelloCdn.any_instance.expects(:lifecycle_environment_id).returns(2)
+    Katello::Content.expects(:substitute_content_path).with(arch: 'i386', releasever: '6Server', content_path: content_path).returns("/test/path")
+
+    paths = katello_cdn.fetch_paths(content_path)
+    assert_equal paths, ["/test/path"]
+  end
 end
