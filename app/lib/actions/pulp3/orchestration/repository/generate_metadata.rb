2e75f497a6c94cb10e17633c6e7df962c76c04c4
Samir Jha <sjha4@ncsu.edu>
2023-06-26 19:38:19 +0000

    Fixes #36523 - Optimized capsule sync doesn't sync recently published/promoted docker repositories

--- app/lib/actions/pulp3/orchestration/repository/generate_metadata.rb
+++ app/lib/actions/pulp3/orchestration/repository/generate_metadata.rb
@@ -14,6 +14,8 @@ module Actions
                 plan_self(source_repository_id: options[:source_repository].id, target_repository_id: repository.id, smart_proxy_id: smart_proxy.id)
               elsif publication_content_type && (force_publication || repository.publication_href.nil? || !repository.using_mirrored_metadata?)
                 plan_action(Actions::Pulp3::Repository::CreatePublication, repository, smart_proxy, options)
+              elsif !publication_content_type
+                plan_self(target_repository_id: repository.id, contents_changed: options[:contents_changed], skip_publication: true)
               end
               plan_action(Actions::Pulp3::ContentGuard::Refresh, smart_proxy) unless repository.unprotected
               plan_action(Actions::Pulp3::Repository::RefreshDistribution, repository, smart_proxy, :contents_changed => options[:contents_changed]) if Setting[:distribute_archived_cvv] || repository.environment
@@ -21,13 +23,18 @@ module Actions
           end
 
           def run
-            #we don't have to actually generate a publication, we can reuse the old one
             target_repo = ::Katello::Repository.find(input[:target_repository_id])
-            source_repo = ::Katello::Repository.find(input[:source_repository_id])
-            if (target_repo.publication_href != source_repo.publication_href && smart_proxy.pulp_primary?)
-              target_repo.clear_smart_proxy_sync_histories
+            if input[:skip_publication]
+              #Need to clear smart proxy sync histories for non-publication content_types: docker, ansible collection
+              target_repo.clear_smart_proxy_sync_histories if input[:contents_changed]
+            else
+              #we don't have to actually generate a publication, we can reuse the old one
+              source_repo = ::Katello::Repository.find(input[:source_repository_id])
+              if (target_repo.publication_href != source_repo.publication_href && smart_proxy.pulp_primary?)
+                target_repo.clear_smart_proxy_sync_histories
+              end
+              target_repo.update!(publication_href: source_repo.publication_href)
             end
-            target_repo.update!(publication_href: source_repo.publication_href)
           end
         end
       end
