f9bf7d0065c55e202275de84b9c69b5ebbca9ed0
Jeremy Lenz <jlenz@redhat.com>
2023-06-30 17:36:23 -0400

    Fixes #36528 - handle integer hash keys in katello-agent actions (#10627)

--- app/lib/actions/katello/bulk_agent_action.rb
+++ app/lib/actions/katello/bulk_agent_action.rb
@@ -21,7 +21,10 @@ module Actions
 
       def spawn_plans
         args = input[:args].first
-        histories = ::Katello::Agent::DispatchHistory.where(id: args[:dispatch_histories].slice(*current_batch.map(&:to_s)).values)
+        # args[:dispatch_histories] keys are numeric host ids; they may be integer or string
+        # Hash#slice will return a filtered hash only with the specified keys, and ignore keys that don't exist
+        possible_keys = [*current_batch.map(&:to_i), *current_batch.map(&:to_s)]
+        histories = ::Katello::Agent::DispatchHistory.where(id: args[:dispatch_histories].slice(*possible_keys).values)
         ::Katello::Agent::Dispatcher.dispatch(
           args[:type].to_sym,
           histories,
