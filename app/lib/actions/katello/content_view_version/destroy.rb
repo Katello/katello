6077a1ecf4221470fe3eaadbf6822477bf6ee707
Samir Jha <sjha4@ncsu.edu>
2023-07-20 18:27:05 -0400

    Fixes #36500 - Optimize DockerMetaTag query and CV version deletion to run a single invocation of the method (#10599)

--- app/lib/actions/katello/content_view_version/destroy.rb
+++ app/lib/actions/katello/content_view_version/destroy.rb
@@ -7,22 +7,26 @@ module Actions
 
           destroy_env_content = !options.fetch(:skip_destroy_env_content, false)
           repos = destroy_env_content ? version.repositories : version.archived_repos
+          docker_cleanup = false
 
           sequence do
             concurrence do
               repos.each do |repo|
                 repo_options = options.clone
+                repo_options[:docker_cleanup] = false
                 plan_action(Repository::Destroy, repo, repo_options)
+                docker_cleanup ||= repo.docker?
               end
             end
           end
 
-          plan_self(:id => version.id)
+          plan_self(:id => version.id, :docker_cleanup => docker_cleanup)
         end
 
         def finalize
           version = ::Katello::ContentViewVersion.find_by(id: input[:id])
           version&.destroy!
+          ::Katello::DockerMetaTag.cleanup_tags if input[:docker_cleanup]
         end
       end
     end
