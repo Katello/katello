8bbb1ff0df34cce029946ea70aa0647fdb93325a
Jeremy Lenz <jlenz@redhat.com>
2023-07-21 13:16:18 -0400

    Fixes #36597 - Rename 'Change content source' job template (#10652)

---
+++ db/migrate/20230717150442_change_change_content_source_job_template_name.rb
@@ -0,0 +1,20 @@
+class ChangeChangeContentSourceJobTemplateName < ActiveRecord::Migration[6.1]
+  TEMPLATE_NAMES = {
+    "Change content source" => "Configure host for new content source"
+  }.freeze
+
+  def up
+    TEMPLATE_NAMES.each do |from, to|
+      token = SecureRandom.base64(5)
+      ::Template.unscoped.find_by(name: to)&.update_columns(:name => "#{to} Backup #{token}")
+      ::Template.unscoped.find_by(name: from)&.update_columns(:name => to)
+    end
+  end
+
+  def down
+    TEMPLATE_NAMES.each do |from, to|
+      ::Template.unscoped.find_by(name: from)&.delete
+      ::Template.unscoped.find_by(name: to)&.update_columns(:name => from)
+    end
+  end
+end
