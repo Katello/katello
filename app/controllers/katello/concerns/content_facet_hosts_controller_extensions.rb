974078042a894aff75910ba93f6005c67f24ad37
Lucy Fu <lufu@redhat.com>
2023-08-10 13:54:20 +0000

    Fixes #36667 - Hammer should fail non-existent LCE in host update
    (cherry picked from commit 05b73f708fbebf6a5118da91dd75f888d9432605)

--- app/controllers/katello/concerns/content_facet_hosts_controller_extensions.rb
+++ app/controllers/katello/concerns/content_facet_hosts_controller_extensions.rb
@@ -9,14 +9,12 @@ module Katello
           return unless @host&.content_facet.present? && params[:host]&.[](:content_facet_attributes)&.present?
           cv_id = params[:host][:content_facet_attributes].delete(:content_view_id)
           env_id = params[:host][:content_facet_attributes].delete(:lifecycle_environment_id)
-          Rails.logger.info "set_up_content_view_environment: cv_id=#{cv_id}, env_id=#{env_id}"
-          if (cv_id.present? && env_id.present?)
-            @host.content_facet.assign_single_environment(
-              lifecycle_environment_id: env_id,
-              content_view_id: cv_id
-            )
-            Rails.logger.info "set_up_content_view_environment: done"
-          end
+          Rails.logger.info "#{__method__}: cv_id=#{cv_id}, env_id=#{env_id}"
+          @host.content_facet.assign_single_environment(
+            lifecycle_environment_id: env_id,
+            content_view_id: cv_id
+          )
+          Rails.logger.info "#{__method__}: done"
         end
       end
     end
