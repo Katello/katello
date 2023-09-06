Katello/katello

commit ea06f3d103587765413c3427b776a836cab6520e

Refs #36706 - Remove remaining katello-agent code including Agent Events & Dynflow actions (#10710)
* Refs #36706 - Remove agent events and actions

* Refs #36706 - Remove dispatch histories via migration

* Refs #36706 - fix tests

* Refs #36706 - remove katello-agent-notice

* Refs #36706 - use errata presence

* Refs #36706 - add back agent_input

* Refs #36706 - remove bastion reference and more stragglers

app/models/katello/ping.rb
@@ -2,7 +2,6 @@ module Katello
   class Ping
     OK_RETURN_CODE = 'ok'.freeze
     FAIL_RETURN_CODE = 'FAIL'.freeze
-    PACKAGES = %w(katello candlepin pulp qpid foreman tfm hammer).freeze
 
     class << self
       def services(capsule_id = nil)
@@ -141,13 +140,6 @@ module Katello
         result[:status] = FAIL_RETURN_CODE
         result[:message] = e.message
         result
-      end
-
-      # get package information for katello and its components
-      def packages
-        names = PACKAGES.join("|")
-        packages = `rpm -qa | egrep "#{names}"`
-        packages.split("\n").sort
       end
 
       def pulp_url(capsule_id)

app/controllers/katello/api/rhsm/candlepin_proxies_controller.rb
@@ -188,7 +188,7 @@ module Katello
 
     api :PUT, "/systems/:id/enabled_repos", N_("Update the information about enabled repositories")
     desc <<-DESC
-      Used by katello-agent to keep the information about enabled repositories up to date.
+      Used to keep the information about enabled repositories up to date.
       This information is then used for computing the errata available for the system.
     DESC
     param :enabled_repos, Hash, :required => true do

app/controllers/katello/api/v2/hosts_bulk_actions_controller.rb
@@ -1,5 +1,4 @@
 module Katello
-  # rubocop:disable Metrics/ClassLength
   class Api::V2::HostsBulkActionsController < Api::V2::ApiController
     include Concerns::Api::V2::BulkHostsExtensions
     include Katello::Concerns::Api::V2::ContentOverridesController
@@ -16,7 +15,6 @@ module Katello
     before_action :find_organization, only: [:add_subscriptions]
     before_action :find_traces, only: [:resolve_traces]
 
-    before_action :validate_content_action, only: [:install_content, :update_content, :remove_content]
     before_action :validate_organization, only: [:add_subscriptions]
 
     # disable *_count fields on erratum rabl, since they perform N+1 queries
@@ -26,22 +24,6 @@ module Katello
       api_version 'v2'
       api_base_url "/api"
     end
-
-    PARAM_ACTIONS = {
-      :install_content => {
-        :package => ::Actions::Katello::Host::Package::Install,
-        :package_group => ::Actions::Katello::Host::PackageGroup::Install,
-        :errata => :install_errata
-      },
-      :update_content => {
-        :package => ::Actions::Katello::Host::Package::Update,
-        :package_group => ::Actions::Katello::Host::PackageGroup::Install
-      },
-      :remove_content => {
-        :package => ::Actions::Katello::Host::Package::Remove,
-        :package_group => ::Actions::Katello::Host::PackageGroup::Remove
-      }
-    }.with_indifferent_access
 
     def_param_group :bulk_params do
       param :organization_id, :number, :required => true, :desc => N_("ID of the organization")
@@ -349,33 +331,6 @@ module Katello
 
       unless max_hosts_exceeded.empty?
         fail HttpErrors::BadRequest, _("Maximum number of content hosts exceeded for host collection(s): %s") % max_hosts_exceeded.join(', ')
-      end
-    end
-
-    def content_action
-      if params[:content_type] == 'errata'
-        options = {}
-        options[:update_all] = true if ::Foreman::Cast.to_bool(params[:install_all])
-        options[:errata_ids] = params[:content]
-
-        task = async_task(::Actions::BulkAction, ::Actions::Katello::Host::Erratum::ApplicableErrataInstall, @hosts, options)
-        respond_for_async :resource => task
-      else
-        content = params[:content]
-        if params[:action] == :update_content && params[:update_all]
-          content = []
-        end
-        task = async_task(Actions::Katello::BulkAgentAction, PARAM_ACTIONS[params[:action]][params[:content_type]], @hosts, content: content)
-        respond_for_async :resource => task
-      end
-    end
-
-    def validate_content_action
-      fail HttpErrors::BadRequest, _("A content_type must be provided.") if params[:content_type].blank?
-      fail HttpErrors::BadRequest, _("No content has been provided.") if params[:content].blank? && !params[:update_all]
-
-      if PARAM_ACTIONS[params[:action]][params[:content_type]].nil?
-        fail HttpErrors::BadRequest, _("Invalid content type %s") % params[:content_type]
       end
     end
 

app/lib/actions/katello/agent/dispatch_history_presenter.rb
@@ -1,64 +1,0 @@
-module Actions
-  module Katello
-    module Agent
-      class DispatchHistoryPresenter
-        def initialize(dispatch_history, action_type)
-          @result = dispatch_history&.result&.with_indifferent_access
-          @action_type = action_type
-        end
-
-        def humanized_output
-          return if @result.empty?
-
-          result = package_result
-
-          if result[:message]
-            result[:message]
-          elsif result[:packages].any?
-            packages = result[:packages].map { |package| package[:qname] }
-            packages.sort.join("\n")
-          else
-            humanized_no_package
-          end
-        end
-
-        def error_messages
-          messages = []
-          @result.each_value do |result|
-            if !result[:succeeded] && result.dig(:details, :message)
-              messages << result[:details][:message]
-            end
-          end
-          messages
-        end
-
-        private
-
-        def package_result
-          result = { packages: [] }
-
-          @result.each_value do |v|
-            if v[:succeeded]
-              result[:packages].concat(v[:details][:resolved] + v[:details][:deps])
-              break
-            else
-              result[:message] = v[:details][:message]
-              break
-            end
-          end
-
-          result
-        end
-
-        def humanized_no_package
-          case @action_type
-          when :content_install
-            _("No new packages installed")
-          when :content_uninstall
-            _("No packages removed")
-          end
-        end
-      end
-    end
-  end
-end

app/lib/actions/katello/agent_action.rb
@@ -1,120 +1,0 @@
-module Actions
-  module Katello
-    class AgentAction < Actions::EntryAction
-      include Dynflow::Action::Timeouts
-      include Helpers::Presenter
-
-      def self.agent_message
-        fail NotImplementedError
-      end
-
-      def agent_action_type
-        nil
-      end
-
-      def plan(host, options)
-        action_subject(host, :hostname => host.name, :content => options[:content])
-        # options[:dispatch_histories] keys might be strings or integers
-        dispatch_history_id = options.dig(:dispatch_histories, host.id.to_s) ||
-        options.dig(:dispatch_histories, host.id.to_i) ||
-          ::Katello::Agent::Dispatcher.create_histories(
-          host_ids: [host.id]
-        ).first.id
-
-        plan_self(
-          host_id: host.id,
-          dispatch_history_id: dispatch_history_id,
-          content: options[:content],
-          bulk: options[:bulk]
-        )
-      end
-
-      def run(event = nil)
-        case event
-        when nil
-          history = dispatch_history
-          timeout = accept_timeout
-
-          if history.finished?
-            fail_on_errors
-            return
-          elsif history.accepted?
-            timeout = finish_timeout
-          end
-
-          suspend do |suspended_action|
-            history.dynflow_execution_plan_id = suspended_action.execution_plan_id
-            history.dynflow_step_id = suspended_action.step_id
-            history.save!
-            dispatch_message(history) unless input[:bulk]
-
-            schedule_timeout(timeout, optional: true)
-          end
-        when Dynflow::Action::Skip
-          # Do not fail and goto a paused state, instead skip and send the state to warning so we do not block other host actions
-        when Dynflow::Action::Timeouts::Timeout
-          process_timeout
-        when 'accepted'
-          schedule_timeout(finish_timeout, optional: true)
-          suspend
-        else
-          fail_on_errors
-        end
-      end
-
-      def dispatch_message(history)
-        ::Katello::Agent::Dispatcher.dispatch(
-          self.class.agent_message,
-          [history],
-          content: input[:content]
-        )
-      end
-
-      def accept_timeout
-        Setting['content_action_accept_timeout']
-      end
-
-      def finish_timeout
-        Setting['content_action_finish_timeout']
-      end
-
-      def process_timeout
-        history = dispatch_history
-
-        unless history.accepted?
-          fail _("Host did not respond within %s seconds. The task has been cancelled. Is katello-agent installed and goferd running on the Host?") % accept_timeout
-        end
-
-        unless history.finished?
-          # we could be processing the accept_timeout here
-          # only fail for finish_timeout unless the actual duration has elapsed
-          finish_limit = history.accepted_at + finish_timeout
-          if finish_limit < DateTime.now
-            fail _("Host did not finish content action in %s seconds.  The task has been cancelled.") % finish_timeout
-          end
-          suspend
-        end
-      end
-
-      def fail_on_errors
-        errors = presenter.error_messages
-
-        if errors.any?
-          fail errors.join("\n")
-        end
-      end
-
-      def presenter
-        Actions::Katello::Agent::DispatchHistoryPresenter.new(dispatch_history, agent_action_type)
-      end
-
-      def rescue_strategy
-        Dynflow::Action::Rescue::Skip
-      end
-
-      def dispatch_history
-        ::Katello::Agent::DispatchHistory.find_by_id(input[:dispatch_history_id])
-      end
-    end
-  end
-end

app/lib/actions/katello/bulk_agent_action.rb
@@ -1,37 +1,0 @@
-module Actions
-  module Katello
-    class BulkAgentAction < Actions::BulkAction
-      def plan(agent_action, hosts, args)
-        host_ids = hosts.map(&:id)
-
-        histories = ::Katello::Agent::Dispatcher.create_histories(
-          host_ids: host_ids
-        )
-
-        grouped_histories = {}
-        histories.each { |h| grouped_histories[h.host_id] = h.id }
-        options = {
-          dispatch_histories: grouped_histories,
-          type: agent_action.agent_message,
-          content: args[:content],
-          bulk: true
-        }
-        super(agent_action, hosts, options)
-      end
-
-      def spawn_plans
-        args = input[:args].first
-        # args[:dispatch_histories] keys are numeric host ids; they may be integer or string
-        # Hash#slice will return a filtered hash only with the specified keys, and ignore keys that don't exist
-        possible_keys = [*current_batch.map(&:to_i), *current_batch.map(&:to_s)]
-        histories = ::Katello::Agent::DispatchHistory.where(id: args[:dispatch_histories].slice(*possible_keys).values)
-        ::Katello::Agent::Dispatcher.dispatch(
-          args[:type].to_sym,
-          histories,
-          content: args[:content]
-        )
-        super
-      end
-    end
-  end
-end

app/lib/actions/katello/content_view/incremental_updates.rb
@@ -4,8 +4,7 @@ module Actions
       class IncrementalUpdates < Actions::EntryAction
         include Helpers::Presenter
 
-        def plan(version_environments, composite_version_environments, content, dep_solve, hosts, description) # rubocop:disable Metrics/MethodLength
-          use_remote_execution = true # TODO: remove this when we remove katello-agent dynflow actions
+        def plan(version_environments, composite_version_environments, content, dep_solve, hosts, description)
           old_new_version_map = {}
           output_for_version_ids = []
 
@@ -34,13 +33,8 @@ module Actions
               handle_composites(old_new_version_map, composite_version_environments, output_for_version_ids, description)
             end
 
-            if hosts.any? && !content[:errata_ids].blank? && !use_remote_execution
-              errata = ::Katello::Erratum.with_identifiers(content[:errata_ids])
-              hosts = hosts.where(:id => ::Katello::Host::ContentFacet.with_applicable_errata(errata).pluck(:host_id))
-              plan_action(::Actions::BulkAction, ::Actions::Katello::Host::Erratum::ApplicableErrataInstall, hosts, :errata_ids => content[:errata_ids])
-            end
             plan_self(:version_outputs => output_for_version_ids, :host_ids => hosts.pluck(:id),
-                      :errata_ids => content[:errata_ids], :use_remote_execution => use_remote_execution)
+                      :errata_ids => content[:errata_ids])
           end
         end
 
@@ -65,7 +59,7 @@ module Actions
         end
 
         def run
-          if input[:errata_ids].present? && input[:host_ids].present? && input[:use_remote_execution]
+          if input[:errata_ids].present? && input[:host_ids].present?
             errata_ids = input[:errata_ids].join(',')
             errata = ::Katello::Erratum.with_identifiers(input[:errata_ids])
             hosts = ::Host.where(:id => input[:host_ids] &

app/lib/actions/katello/host/erratum/applicable_errata_install.rb
@@ -1,44 +1,0 @@
-module Actions
-  module Katello
-    module Host
-      module Erratum
-        class ApplicableErrataInstall < Actions::EntryAction
-          include Helpers::Presenter
-
-          #takes a list of errata and schedules the installation of those that are applicable
-          def plan(host, options = {})
-            fail _("errata_ids or update_all must be provided") if !options[:errata_ids] && !options[:update_all]
-            errata_ids = options.fetch(:errata_ids, nil)
-
-            applicable_errata = host.content_facet.applicable_errata
-            applicable_errata = applicable_errata.with_identifiers(errata_ids) unless options[:update_all]
-            if applicable_errata.empty?
-              plan_self(:hostname => host.name, :skip => true)
-            else
-              plan_action(Actions::Katello::Host::Erratum::Install, host, content: applicable_errata.pluck(:errata_id))
-              plan_self(:hostname => host.name, :skip => false)
-            end
-          end
-
-          def run
-            if input[:skip]
-              output[:response] = _("No applicable errata for %s, skipping" % input[:hostname])
-            end
-          end
-
-          def humanized_name
-            if input.try(:[], :hostname).nil?
-              _("Install Applicable Errata")
-            else
-              _("Install Applicable Errata on %s") % input[:hostname]
-            end
-          end
-
-          def presenter
-            Helpers::Presenter::Delegated.new(self, planned_actions(Katello::Host::Erratum::Install))
-          end
-        end
-      end
-    end
-  end
-end

commit 88c5380185a9953294a20f38b2df72885d74de5d

Fixes #36722 - restore Katello::Ping.packages
This was wrongly removed during katello-agent removal.

Instead of removing this, update the package list to not list qpid.

Fixes: ea06f3d103587765413c3427b776a836cab6520e

app/models/katello/ping.rb
@@ -2,6 +2,7 @@ module Katello
   class Ping
     OK_RETURN_CODE = 'ok'.freeze
     FAIL_RETURN_CODE = 'FAIL'.freeze
+    PACKAGES = %w(katello candlepin pulp foreman hammer).freeze
 
     class << self
       def services(capsule_id = nil)
@@ -140,6 +141,13 @@ module Katello
         result[:status] = FAIL_RETURN_CODE
         result[:message] = e.message
         result
+      end
+
+      # get package information for katello and its components
+      def packages
+        names = PACKAGES.join("|")
+        packages = `rpm -qa | egrep "#{names}"`
+        packages.split("\n").sort
       end
 
       def pulp_url(capsule_id)