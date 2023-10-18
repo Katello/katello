337b5d6163d324ad601a4165e90dae7de4779392
Jeremy Lenz <jlenz@redhat.com>
2023-10-18 16:12:12 -0400

    Fixes #36753 - Only allow coherent default CVE (#10764)
    in global registration form
      - remove 'Lifecycle Environment' field from global registration form
      - add Rails validator preventing content view environments from using a
        non-default lifecycle environment with the Default Organization View

--- webpack/components/extensions/RegistrationCommands/index.js
+++ webpack/components/extensions/RegistrationCommands/index.js
@@ -3,62 +3,38 @@ import PropTypes from 'prop-types';
 import { noop } from 'foremanReact/common/helpers';
 
 import ActivationKeys from './fields/ActivationKeys';
-import LifecycleEnvironment from './fields/LifecycleEnvironment';
 import IgnoreSubmanErrors from './fields/IgnoreSubmanErrors';
 import Force from './fields/Force';
 
 export const RegistrationCommands = ({
-  organizationId,
-  hostGroupId,
   pluginValues,
-  pluginData,
   onChange,
   isLoading,
-}) => {
-  useEffect(() => {
-    onChange({ lifecycleEnvironmentId: '' });
-  }, [onChange, organizationId, hostGroupId]);
-
-  return (
-    <>
-      <LifecycleEnvironment
-        pluginValues={pluginValues}
-        lifecycleEnvironments={pluginData?.lifecycleEnvironments}
-        hostGroupEnvironment={pluginData?.hostGroupEnvironment}
-        organizationId={organizationId}
-        onChange={onChange}
-        isLoading={isLoading}
-      />
-      <IgnoreSubmanErrors
-        value={pluginValues?.ignoreSubmanErrors}
-        pluginValues={pluginValues}
-        onChange={onChange}
-        isLoading={isLoading}
-      />
-      <Force
-        value={pluginValues?.force}
-        pluginValues={pluginValues}
-        onChange={onChange}
-        isLoading={isLoading}
-      />
-    </>
-  );
-};
+}) => (
+  <>
+    <IgnoreSubmanErrors
+      value={pluginValues?.ignoreSubmanErrors}
+      pluginValues={pluginValues}
+      onChange={onChange}
+      isLoading={isLoading}
+    />
+    <Force
+      value={pluginValues?.force}
+      pluginValues={pluginValues}
+      onChange={onChange}
+      isLoading={isLoading}
+    />
+  </>
+);
 
 RegistrationCommands.propTypes = {
-  organizationId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  hostGroupId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   pluginValues: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-  pluginData: PropTypes.object, // eslint-disable-line react/forbid-prop-types
   onChange: PropTypes.func,
   isLoading: PropTypes.bool,
 };
 
 RegistrationCommands.defaultProps = {
-  organizationId: undefined,
-  hostGroupId: undefined,
   pluginValues: {},
-  pluginData: {},
   isLoading: false,
   onChange: noop,
 };
