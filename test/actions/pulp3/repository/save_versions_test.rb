67326225936e9c6a3e048177570626d79814e4ad
Markus Bucher <bucher@atix.de>
2023-12-04 18:33:05 +0100

    Fixes #36859 - Fix inconsistent repo publication
    (cherry picked from commit df80f7bc568d90d81a8ed4397630b457897b933e)

--- test/actions/pulp3/repository/save_versions_test.rb
+++ test/actions/pulp3/repository/save_versions_test.rb
@@ -81,5 +81,24 @@ module ::Actions::Pulp3::Repository
       assert_equal task.output[:contents_changed], true
       assert_empty task.output[:updated_repositories] - [@repo1.id, @repo2.id]
     end
+
+    def test_save_version_with_outdated_publication
+      @repo1.update(version_href: "test_repo_1/1/", publication_href: "test_publ_1/")
+      ::Katello::Pulp3::RepositoryReference.new(repository_href: "test_repo_1/", root_repository_id: @repo1.root_id, content_view_id: @repo1.content_view.id).save
+
+      tasks_map = [{ created_resources: [] }]
+
+      ::PulpFileClient::PublicationsFileApi.any_instance.expects(:read).with("test_publ_1/").
+        returns(::PulpFileClient::FileFilePublicationResponse.new(repository_version: "test_repo_1/0/"))
+      ::Katello::Repository.any_instance.stubs(:index_content).returns(true)
+
+      task = ForemanTasks.sync_task(::Actions::Pulp3::Repository::SaveVersion, @repo1, tasks: tasks_map)
+
+      @repo1.reload
+
+      assert_equal "test_repo_1/1/", @repo1.version_href
+      assert task.output[:contents_changed]
+      assert_nil task.output[:updated_repositories]
+    end
   end
 end
