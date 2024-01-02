3773276af1e5489641132b60f455047b2d7f347f
Evgeni Golov <evgeni@golov.de>
2024-01-02 12:17:33 +0100

    Fixes #37073 - only rename Katello settings if category column exists

--- db/migrate/20160505181337_rename_katello_settings.rb
+++ db/migrate/20160505181337_rename_katello_settings.rb
@@ -1,9 +1,9 @@
 class RenameKatelloSettings < ActiveRecord::Migration[4.2]
   def up
-    Setting.where(category: 'Setting::Katello').update_all(:category => 'Setting::Content')
+    Setting.where(category: 'Setting::Katello').update_all(:category => 'Setting::Content') if column_exists?(:settings, :category)
   end
 
   def down
-    Setting.where(category: 'Setting::Content').update_all(:category => 'Setting::Katello')
+    Setting.where(category: 'Setting::Content').update_all(:category => 'Setting::Katello') if column_exists?(:settings, :category)
   end
 end
