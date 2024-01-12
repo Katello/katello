a02bb2ed0b4cafbf93c5582527bdf84973080fc5
Ian Ballou <ianballou67@gmail.com>
2024-01-12 20:01:39 +0000

    Fixes #37050 - restrict module copying to source repository
    (cherry picked from commit edd534121b047e470bde4bcd250acbf478bbbe5d)

--- app/models/katello/content_view_module_stream_filter.rb
+++ app/models/katello/content_view_module_stream_filter.rb
@@ -29,7 +29,7 @@ module Katello
       if self.original_module_streams
         module_ids.concat(repo.module_streams_without_errata.map(&:id))
       end
-      modules_streams = ModuleStream.where(id: module_ids).includes(:rpms)
+      modules_streams = ModuleStream.in_repositories(repo).where(id: module_ids).includes(:rpms)
       content_unit_ids += modules_streams.pluck(:pulp_id).flatten.uniq
       if dependents && !modules_streams.empty?
         rpms = modules_streams.map(&:rpms).flatten
