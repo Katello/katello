3ca8e77a9ad03c71e1f4f91455c6a410541c3399
Bernhard Suttner <suttner@atix.de>
2024-02-29 11:53:52 +0100

    Fixes #37198 - Allow installedDeb package attributes in safemode
    (cherry picked from commit b5d785fee9552acc0c17c8e73f397db5dbeb0a7d)

--- app/models/katello/installed_deb.rb
+++ app/models/katello/installed_deb.rb
@@ -1,5 +1,17 @@
 module Katello
   class InstalledDeb < Katello::Model
+    apipie :class, desc: "A class representing #{model_name.human} object" do
+      name 'Installed Deb'
+      refs 'InstalledDeb'
+      sections only: %w[all additional]
+      property :name, String, desc: 'Returns name of the package'
+      property :version, String, desc: 'Returns package version'
+      property :architecture, String, desc: 'Returns package architecture'
+    end
+    class Jail < Safemode::Jail
+      allow :name, :version, :architecture
+    end
+
     has_many :host_installed_debs, :class_name => "Katello::HostInstalledDeb", :dependent => :destroy, :inverse_of => :installed_deb
     has_many :hosts, :through => :host_installed_debs, :class_name => "::Host"
 
