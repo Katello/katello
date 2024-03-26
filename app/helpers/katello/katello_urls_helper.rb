566f5acc20452f92571649d0a26c10f5b65262ef
Ian Ballou <ianballou67@gmail.com>
2024-03-26 09:39:48 -0400

    Fixes #37268 - add repository_url back & address the multi cv issue (#10938)
    (cherry picked from commit 2c4f736bff93d9d8205535faed1e2040ae263232)

--- app/helpers/katello/katello_urls_helper.rb
+++ app/helpers/katello/katello_urls_helper.rb
@@ -37,11 +37,36 @@ module Katello
       "#{prefix}/pub/#{config}"
     end
 
-    apipie :method, 'Generates an absolute path to the file' do
+    apipie :method, 'Generates an absolute path to the file for liveimg kickstart templates' do
       required :content_path, String, desc: "Relative path to the file or it's name"
       optional :schema, String, desc: 'Optional URL schema for the content source', default: 'http'
       optional :content_type, String, desc: 'Content type', default: 'repos'
       returns String, desc: 'Absolute path to a file'
+    end
+    def repository_url(content_path, _content_type = nil, schema = 'http')
+      return content_path if content_path =~ %r|^([\w\-\+]+)://|
+      url = if @host.content_source
+              "#{schema}://#{@host.content_source.hostname}"
+            else
+              foreman_settings_url(schema)
+            end
+      content_path = content_path.sub(%r|^/|, '')
+
+      lifecycle_environment = nil
+      if @host.default_environment?
+        # Don't update the content_path if the host is using the default org view / library
+        lifecycle_environment = ::Katello::KTEnvironment.library.find_by(organization_id: @host.organization_id)
+      elsif !@host.single_content_view_environment?
+        fail ::Katello::Errors::MultiEnvironmentNotSupportedError,
+          "Host #{@host.name} must be subscribed to only a single content view & environment or subscribe to the default organization content view for liveimg provisioning."
+      else
+        lifecycle_environment = @host.single_lifecycle_environment
+        content_path = [@host.single_content_view.label, content_path].join('/')
+      end
+
+      path = ::Katello::Repository.repo_path_from_content_path(
+        lifecycle_environment, content_path)
+      "#{url}/pulp/content/#{path}"
     end
   end
 end
