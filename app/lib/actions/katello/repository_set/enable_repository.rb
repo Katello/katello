ea3b8296a38d353d733e1d283e8a13a855ab5505
Chris Roberts <chrobert@redhat.com>
2024-06-26 11:47:07 -0400

    Fixes #37598 - Fix missing kwargs on repo enable for ISS

--- app/lib/actions/katello/repository_set/enable_repository.rb
+++ app/lib/actions/katello/repository_set/enable_repository.rb
@@ -6,8 +6,7 @@ module Actions
           _("Enable")
         end
 
-        def plan(product, content, substitutions, override_url: nil,
-                 override_arch: nil)
+        def plan(product, content, substitutions, opts = {})
           mapper = ::Katello::Candlepin::RepositoryMapper.new(product,
                                                                content,
                                                                substitutions)
@@ -16,10 +15,10 @@ module Actions
             fail ::Katello::Errors::ConflictException, _("The repository is already enabled")
           end
           repository = mapper.build_repository
-          repository.root.arch = override_arch if override_arch.present?
-          if override_url
-            repository.root.url = override_url
-            repository.root.download_policy = ::Katello::RootRepository::DOWNLOAD_IMMEDIATE if URI(override_url).scheme == 'file'
+          repository.root.arch = opts[:override_arch] if opts[:override_arch].present?
+          if opts[:override_url]
+            repository.root.url = opts[:override_url]
+            repository.root.download_policy = ::Katello::RootRepository::DOWNLOAD_IMMEDIATE if URI(opts[:override_url]).scheme == 'file'
           end
           plan_action(Repository::Create, repository, clone: false)
           action_subject(repository)
