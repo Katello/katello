8a29c881350131a6f343c5a5562caa22c56d5166
Nikos Moumoulidis <nmoumoul@redhat.com>
2024-07-05 16:36:07 +0200

    Remove leftover reference to distributor_versions Candlepin API
    - It seems all code related to distributor versions was removed in
      this PR: https://github.com/Katello/katello/pull/5458
      and this was a leftover method that is not used any more.

--- app/lib/katello/resources/candlepin/candlepin_ping.rb
+++ app/lib/katello/resources/candlepin/candlepin_ping.rb
@@ -7,11 +7,6 @@ module Katello
             response = get('/candlepin/status').body
             JSON.parse(response).with_indifferent_access
           end
-
-          def distributor_versions
-            response = get("/candlepin/distributor_versions").body
-            JSON.parse(response)
-          end
         end
       end
     end
