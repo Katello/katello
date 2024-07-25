e3666cc59c23ad41dec35cbc4be57c753adc0b66
Jeremy Lenz <jlenz@redhat.com>
2024-07-25 13:45:21 -0400

    Refs #37669 - add tests

--- app/controllers/katello/concerns/api/v2/hosts_controller_extensions.rb
+++ app/controllers/katello/concerns/api/v2/hosts_controller_extensions.rb
@@ -42,7 +42,7 @@ module Katello
         end
 
         def set_content_view_environments
-          return if @host.content_facet.blank? ||
+          return if @host&.content_facet.blank? ||
             (cve_params[:content_view_id].present? && cve_params[:lifecycle_environment_id].present?)
           new_cves = nil
           if cve_params[:content_view_environments].present? && cve_params[:content_view_environment_ids].blank?
