460af387e6b93a9ccfb8e898f050c7f54b53ac78
akumari <akumari@redhat.com>
2024-09-19 01:32:01 +0530

    Fix Style/CaseLikeIf cop

--- app/models/katello/content_view_environment.rb
+++ app/models/katello/content_view_environment.rb
@@ -68,9 +68,10 @@ module Katello
     end
 
     def priority(content_object)
-      if content_object.is_a? Katello::ActivationKey
+      case content_object
+      when Katello::ActivationKey
         content_view_environment_activation_keys.find_by(:activation_key_id => content_object.id).try(:priority)
-      elsif content_object.is_a? Katello::Host::ContentFacet
+      when Katello::Host::ContentFacet
         content_view_environment_content_facets.find_by(:content_facet_id => content_object.id).try(:priority)
       end
     end
