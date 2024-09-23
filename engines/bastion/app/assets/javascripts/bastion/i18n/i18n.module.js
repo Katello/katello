5ee842984f2f8251599d8cae5c35737ddd653d64
Maria <mariaaga@redhat.com>
2024-09-23 23:30:56 +0100

    Fixes #37858 - Send events on angular translation load (#11158)
    (cherry picked from commit f5510e9a91ba0ced64f95aad7937f158437303e9)

--- engines/bastion/app/assets/javascripts/bastion/i18n/i18n.module.js
+++ engines/bastion/app/assets/javascripts/bastion/i18n/i18n.module.js
@@ -5,6 +5,10 @@
  * @description
  *   Module for internationalization.
  */
+var loadAngularJSi18n = new Event('loadAngularJSi18n');
+
 angular.module('Bastion.i18n', [
     'gettext'
 ]);
+
+document.dispatchEvent(loadAngularJSi18n);
