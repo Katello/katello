6abdfb587052284476cde2a3714b8097703c5caf
Quirin Pamp <pamp@atix.de>
2024-10-08 17:49:00 +0200

    Fixes #38741 - Make structured APT mandatory for deb content

--- app/models/katello/root_repository.rb
+++ app/models/katello/root_repository.rb
@@ -389,10 +389,6 @@ module Katello
       self.content_type == Repository::DEB_TYPE
     end
 
-    def deb_using_structured_apt?
-      self.deb? && !self.content_id
-    end
-
     def ansible_collection?
       self.content_type == Repository::ANSIBLE_COLLECTION_TYPE
     end
@@ -446,7 +442,8 @@ module Katello
     end
 
     def format_arches
-      if content_type == ::Katello::Repository::DEB_TYPE
+      if self.deb?
+        # Pulp 2 needed a "," separated string-list, but Pulp 3 needs " " separated!
         self.deb_architectures&.gsub(" ", ",")
       else
         self.arch == "noarch" ? nil : self.arch
