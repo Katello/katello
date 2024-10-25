32b1d8c1521e44a5cc1849ad4fada1ed2e5f2512
Samir Jha <samirjha1525@gmail.com>
2024-10-25 16:43:41 +0000

    Fixes #36874 - Drop migrated_pulp3_href from content tables

---
+++ db/migrate/20241025151105_remove_pulp3_migrated_hrefs_from_content_tables.rb
@@ -0,0 +1,10 @@
+class RemovePulp3MigratedHrefsFromContentTables < ActiveRecord::Migration[6.1]
+  def change
+    content_models = [Katello::Rpm, Katello::ModuleStream, Katello::Erratum, Katello::PackageGroup, Katello::YumMetadataFile,
+                      Katello::Srpm, Katello::FileUnit, Katello::DockerManifestList, Katello::DockerManifest, Katello::DockerTag]
+
+    content_models.each do |model|
+      remove_column model.table_name, :migrated_pulp3_href, :string
+    end
+  end
+end
