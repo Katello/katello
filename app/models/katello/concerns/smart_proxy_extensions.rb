Katello/katello

commit d6adef8e4ca5ba135277ec6c5c1721870e9e8776

Fixes #34070 - fix container registry with azure plugin
a previous fix switched to using filenames for certs
if the Faraday default adapter was set to :excon,
however this broke the container registry proxy, as its
using rest_client, but is still using this same piece of code

app/models/katello/concerns/smart_proxy_extensions.rb
@@ -185,17 +185,17 @@ module Katello
         end
       end
 
-      def pulp3_ssl_configuration(config)
+      def pulp3_ssl_configuration(config, connection_adapter = Faraday.default_adapter)
         legacy_pulp_cert = !self.setting(PULP3_FEATURE, 'client_authentication')&.include?('client_certificate')
 
-        if Faraday.default_adapter == :excon
+        if connection_adapter == :excon
           config.ssl_client_cert = ::Cert::Certs.ssl_client_cert_filename(use_admin_as_cn_cert: legacy_pulp_cert)
           config.ssl_client_key = ::Cert::Certs.ssl_client_key_filename(use_admin_as_cn_cert: legacy_pulp_cert)
-        elsif Faraday.default_adapter == :net_http
+        elsif connection_adapter == :net_http
           config.ssl_client_cert = ::Cert::Certs.ssl_client_cert(use_admin_as_cn_cert: legacy_pulp_cert)
           config.ssl_client_key = ::Cert::Certs.ssl_client_key(use_admin_as_cn_cert: legacy_pulp_cert)
         else
-          fail "Unexpected faraday default_adapter #{Faraday.default_adapter}!  Cannot continue, this is likely a bug."
+          fail "Unexpected connection_adapter #{Faraday.default_adapter}!  Cannot continue, this is likely a bug."
         end
       end
 

app/lib/katello/resources/registry.rb
@@ -32,7 +32,7 @@ module Katello
             self.prefix = "/pulpcore_registry/"
             self.site = "#{uri.scheme}://#{uri.host}:#{uri.port}"
             self.ca_cert_file = Setting[:ssl_ca_file]
-            pulp_primary.pulp3_ssl_configuration(self)
+            pulp_primary.pulp3_ssl_configuration(self, :net_http)
 
             self
           end

commit d184feece6db5655a7c6dcd8fc8f0642594c6e9d

Refs #30368 - Use correct connection_adapter variable
Rather than using the default value, this uses the actual value that was
passed in.

Fixes: d6adef8e4ca5 ("Fixes #34070 - fix container registry with azure plugin")

app/models/katello/concerns/smart_proxy_extensions.rb
@@ -260,7 +260,7 @@ module Katello
           config.ssl_client_cert = ::Cert::Certs.ssl_client_cert
           config.ssl_client_key = ::Cert::Certs.ssl_client_key
         else
-          fail "Unexpected connection_adapter #{Faraday.default_adapter}!  Cannot continue, this is likely a bug."
+          fail "Unexpected connection_adapter #{connection_adapter}!  Cannot continue, this is likely a bug."
         end
       end
 