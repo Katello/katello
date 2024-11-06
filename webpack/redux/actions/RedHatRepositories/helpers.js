f5bbaac3594ad7b985393c99941a104088377998
Ashish Humbe <ahumbe@redhat.com>
2024-11-06 15:58:05 +0530

    Refs #37984 - Add 6.16 Capsule, Maintenance and Utils repos

--- webpack/redux/actions/RedHatRepositories/helpers.js
+++ webpack/redux/actions/RedHatRepositories/helpers.js
@@ -37,6 +37,9 @@ const recommendedRepositoriesMisc = [
   'satellite-capsule-6.16-for-rhel-8-x86_64-rpms',
   'satellite-maintenance-6.16-for-rhel-8-x86_64-rpms',
   'satellite-utils-6.16-for-rhel-8-x86_64-rpms',
+  'satellite-utils-6.16-for-rhel-9-x86_64-rpms',
+  'satellite-maintenance-6.16-for-rhel-9-x86_64-rpms',
+  'satellite-capsule-6.16-for-rhel-9-x86_64-rpms',
 ];
 
 const recommendedRepositorySetLables = recommendedRepositoriesRHEL
