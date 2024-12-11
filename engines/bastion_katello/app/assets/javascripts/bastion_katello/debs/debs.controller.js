a4fb7ac6fde8d0b83e0ea392a1130ed0b0cc44e8
Nadja Heitmann <nadjah@atix.de>
2024-12-11 12:09:59 +0000

    Fixes #38083 - Add repository filter to Debian packages page

--- engines/bastion_katello/app/assets/javascripts/bastion_katello/debs/debs.controller.js
+++ engines/bastion_katello/app/assets/javascripts/bastion_katello/debs/debs.controller.js
@@ -1,37 +1,51 @@
-(function () {
-    'use strict';
-
-    /**
-     * @ngdoc controller
-     * @name  Bastion.debs.controller:DebsController
-     *
-     * @description
-     *   Handles fetching deb packages and populating Nutupane based on the current
-     *   ui-router state.
-     *
-     * @requires translate
-     *
-     */
-    function DebsController($scope, $location, translate, Nutupane, Deb, CurrentOrganization) {
+/**
+ * @ngdoc controller
+ * @name  Bastion.debs.controller:DebsController
+ *
+ * @description
+ *   Handles fetching deb packages and populating Nutupane based on the current
+ *   ui-router state.
+ *
+ * @requires translate
+ *
+ */
+angular.module('Bastion.debs').controller('DebsController',
+    ['$scope', '$location', 'translate', 'Nutupane', 'Deb', 'Repository', 'CurrentOrganization',
+    function DebsController($scope, $location, translate, Nutupane, Deb, Repository, CurrentOrganization) {
         var nutupane;
 
         var params = {
             'organization_id': CurrentOrganization,
             'search': $location.search().search || "",
+            'repository_id': $location.search().repositoryId || null,
             'paged': true,
             'sort_by': 'name',
             'sort_order': 'ASC'
         };
 
-        nutupane = new Nutupane(Deb, params);
+        nutupane = $scope.nutupane = new Nutupane(Deb, params);
         nutupane.primaryOnly = true;
 
         $scope.table = nutupane.table;
 
         // Labels so breadcrumb strings can be translated
         $scope.label = translate('Debs');
+        $scope.repositoriesLabel = translate('Repositories');
 
         $scope.controllerName = 'katello_debs';
+
+        $scope.repository = {name: translate('All Repositories'), id: 'all'};
+
+        Repository.queryUnpaged({'organization_id': CurrentOrganization, 'content_type': 'deb', 'with_content': 'deb'}, function (response) {
+            $scope.repositories = [$scope.repository];
+            $scope.repositories = $scope.repositories.concat(response.results);
+
+            if ($location.search().repositoryId) {
+                $scope.repository = _.find($scope.repositories, function (repository) {
+                    return repository.id === parseInt($location.search().repositoryId, 10);
+                });
+            }
+        });
 
         Deb.queryPaged({'organization_id': CurrentOrganization}, function (result) {
             $scope.packageCount = result.total;
@@ -50,12 +64,22 @@
             nutupane.refresh();
         };
 
-    }
+        $scope.$watch('repository', function (repository) {
+            var nutupaneParams = nutupane.getParams();
 
-    angular
-        .module('Bastion.debs')
-        .controller('DebsController', DebsController);
+            if (repository.id === 'all') {
+                nutupaneParams['repository_id'] = null;
+                nutupane.setParams(nutupaneParams);
+            } else {
+                $location.search('repositoryId', repository.id);
+                nutupaneParams['repository_id'] = repository.id;
+                nutupane.setParams(nutupaneParams);
+            }
 
-    DebsController.$inject = ['$scope', '$location', 'translate', 'Nutupane', 'Deb', 'CurrentOrganization'];
+            if (!nutupane.table.initialLoad) {
+                nutupane.refresh();
+            }
+        });
 
-})();
+    }]
+);
