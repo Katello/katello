3c7ae1f983e8f5e4bfbdfd9b6fb8c4bfa78e0d8b
Lucy Fu <lufu@redhat.com>
2024-12-11 16:47:54 -0500

    Fixes #38084 - Add new REX job templates for bootc upgrade/switch/rollback

--- db/seeds.d/75-job_templates.rb
+++ db/seeds.d/75-job_templates.rb
@@ -1,8 +1,12 @@
 User.as_anonymous_admin do
   JobTemplate.without_auditing do
+    # let's name root template as *_action.erb
+    root_action_files = Dir[File.join("#{Katello::Engine.root}/app/views/foreman/job_templates/**/*_action.erb")]
     template_files = Dir[File.join("#{Katello::Engine.root}/app/views/foreman/job_templates/**/*.erb")]
     template_files.reject! { |file| file.end_with?('_ansible_default.erb') } unless Katello.with_ansible?
-    template_files.each do |template|
+
+    # root templates need to be imported first
+    (root_action_files + (template_files - root_action_files)).each do |template|
       sync = !Rails.env.test? && Setting[:remote_execution_sync_templates]
       template = JobTemplate.import_raw!(File.read(template), :default => true, :lock => true, :update => sync)
 
