a153d9423d6f4555dc55575c3e5b859b9aecf2fd
Samir Jha <samirjha1525@gmail.com>
2024-12-12 13:39:00 -0500

    Fixes #37859 - Re-drop evr extension in katello (#11159)

--- db/migrate/20240924161240_katello_recreate_evr_constructs.rb
+++ db/migrate/20240924161240_katello_recreate_evr_constructs.rb
@@ -1,8 +1,11 @@
-require 'fx'
-
-class CreateEvrType < ActiveRecord::Migration[5.2]
+class KatelloRecreateEvrConstructs < ActiveRecord::Migration[6.1]
   def up
-    unless connection.adapter_name.downcase.include?('sqlite')
+    if !extension_enabled?('evr')
+      return
+    else
+      execute <<~SQL
+        DROP EXTENSION evr CASCADE;
+      SQL
 
       execute <<~SQL
               create type evr_array_item as (
@@ -134,11 +137,6 @@ class CreateEvrType < ActiveRecord::Migration[5.2]
       add_column :katello_rpms, :evr, :evr_t
       add_column :katello_installed_packages, :evr, :evr_t
 
-      create_trigger :evr_insert_trigger_katello_rpms, on: :katello_rpms
-      create_trigger :evr_update_trigger_katello_rpms, on: :katello_rpms
-      create_trigger :evr_insert_trigger_katello_installed_packages, on: :katello_installed_packages
-      create_trigger :evr_update_trigger_katello_installed_packages, on: :katello_installed_packages
-
       execute <<-SQL
         update katello_rpms SET evr = (ROW(coalesce(epoch::numeric,0),
                                            rpmver_array(coalesce(version,'empty'))::evr_array_item[],
@@ -149,26 +147,14 @@ class CreateEvrType < ActiveRecord::Migration[5.2]
                                                          rpmver_array(coalesce(release,'empty'))::evr_array_item[])::evr_t);
       SQL
 
-      add_index :katello_rpms, [:name, :arch, :evr]
-      add_index :katello_erratum_packages, [:erratum_id, :nvrea]
+      create_trigger :evr_insert_trigger_katello_rpms, on: :katello_rpms
+      create_trigger :evr_update_trigger_katello_rpms, on: :katello_rpms
+      create_trigger :evr_insert_trigger_katello_installed_packages, on: :katello_installed_packages
+      create_trigger :evr_update_trigger_katello_installed_packages, on: :katello_installed_packages
     end
   end
 
   def down
-    # fx doesn't seem to have support for dropping functions with parameters
-    unless connection.adapter_name.downcase.include?('sqlite')
-      remove_index :katello_rpms, column: [:name, :arch, :evr]
-      remove_index :katello_erratum_packages, column: [:erratum_id, :nvrea]
-
-      drop_trigger :evr_insert_trigger_katello_rpms, on: :katello_rpms
-      drop_trigger :evr_update_trigger_katello_rpms, on: :katello_rpms
-      drop_trigger :evr_insert_trigger_katello_installed_packages, on: :katello_installed_packages
-      drop_trigger :evr_update_trigger_katello_installed_packages, on: :katello_installed_packages
-
-      remove_column :katello_rpms, :evr
-      remove_column :katello_installed_packages, :evr
-
-      disable_extension "evr"
-    end
+    fail ActiveRecord::IrreversibleMigration
   end
 end
