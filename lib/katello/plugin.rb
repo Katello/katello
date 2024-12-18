55c376d35ca1d4ca0e6156fe2e439bee9b632e25
Stejskal Leos <github@stejskalleos.cz>
2024-12-18 09:00:57 +0000

    refs #37923 - Bug fixes & improvements for the Default HTTP proxy
    Follow up on PR#11183 with:
    * Unified naming of the parameter ('content_default_http_proxy')
    * On edit form showing information if the capsule is default
    * Fixed API + extended show with 'content_default_http_proxy'

--- lib/katello/plugin.rb
+++ lib/katello/plugin.rb
@@ -298,6 +298,7 @@ Foreman::Plugin.register :katello do
   extend_rabl_template 'api/v2/smart_proxies/main', 'katello/api/v2/smart_proxies/pulp_info'
   extend_rabl_template 'api/v2/hosts/show', 'katello/api/v2/hosts/host_collections'
   extend_rabl_template 'api/v2/hosts/show', 'katello/api/v2/hosts/show'
+  extend_rabl_template 'api/v2/http_proxies/show', 'katello/api/v2/http_proxies/show'
 
   # Katello variables for Host Registration
   extend_allowed_registration_vars :activation_keys
