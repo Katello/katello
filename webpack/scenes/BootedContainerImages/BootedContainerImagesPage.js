ade4874763b91e50e6a8b375d7c4b7f621eec407
Ian Ballou <ianballou67@gmail.com>
2024-12-18 21:37:33 +0000

    Refs #38107 - use Table on bootc images page

--- webpack/scenes/BootedContainerImages/BootedContainerImagesPage.js
+++ webpack/scenes/BootedContainerImages/BootedContainerImagesPage.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import TableIndexPage from 'foremanReact/components/PF4/TableIndexPage/TableIndexPage';
+import { Table } from 'foremanReact/components/PF4/TableIndexPage/Table/Table';
+import {
+  useSetParamsAndApiAndSearch,
+  useTableIndexAPIResponse,
+} from 'foremanReact/components/PF4/TableIndexPage/Table/TableIndexHooks';
+import {
+  useUrlParams,
+} from 'foremanReact/components/PF4/TableIndexPage/Table/TableHooks';
 import { translate as __ } from 'foremanReact/common/I18n';
 import BOOTED_CONTAINER_IMAGES_KEY, { BOOTED_CONTAINER_IMAGES_API_PATH } from './BootedContainerImagesConstants';
 
@@ -9,6 +17,46 @@ const BootedContainerImagesPage = () => {
       title: __('Image name'),
     },
   };
+
+  const STATUS = {
+    PENDING: 'PENDING',
+    RESOLVED: 'RESOLVED',
+    ERROR: 'ERROR',
+  };
+
+  const {
+    searchParam: urlSearchQuery = '',
+    page: urlPage,
+    per_page: urlPerPage,
+  } = useUrlParams();
+  const defaultParams = { search: urlSearchQuery };
+  if (urlPage) defaultParams.page = Number(urlPage);
+  if (urlPerPage) defaultParams.per_page = Number(urlPerPage);
+  const apiOptions = { key: BOOTED_CONTAINER_IMAGES_KEY };
+  const response = useTableIndexAPIResponse({
+    apiUrl: BOOTED_CONTAINER_IMAGES_API_PATH,
+    apiOptions,
+    defaultParams,
+  });
+
+  const {
+    response: {
+      results,
+      per_page: perPage,
+      page,
+      subtotal,
+      message: errorMessage,
+    },
+    status = STATUS.PENDING,
+    setAPIOptions,
+  } = response;
+
+  const { setParamsAndAPI, params } = useSetParamsAndApiAndSearch({
+    defaultParams,
+    apiOptions,
+    setAPIOptions: response.setAPIOptions,
+  });
+
   return (
     <TableIndexPage
       apiUrl={BOOTED_CONTAINER_IMAGES_API_PATH}
@@ -17,8 +65,33 @@ const BootedContainerImagesPage = () => {
       createable={false}
       isDeleteable={false}
       controller="/katello/api/v2/host_bootc_images"
-      columns={columns}
-    />
+    >
+      <Table
+        ouiaId="booted-container-images-table"
+        isEmbedded={false}
+        params={{
+          ...params,
+          page,
+          perPage,
+        }}
+        setParams={setParamsAndAPI}
+        itemCount={subtotal}
+        results={results}
+        url={BOOTED_CONTAINER_IMAGES_API_PATH}
+        isDeleteable={false}
+        refreshData={() =>
+          setAPIOptions({
+            ...apiOptions,
+            params: { urlSearchQuery },
+          })
+        }
+        columns={columns}
+        errorMessage={
+          status === STATUS.ERROR && errorMessage ? errorMessage : null
+        }
+        isPending={status === STATUS.PENDING}
+      />
+    </TableIndexPage>
   );
 };
 
