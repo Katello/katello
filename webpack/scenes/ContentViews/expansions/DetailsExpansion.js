8307210a95430053fa243e66d2529a9dc5ce2bb3
Pavan Soma Shekar <shekarpavan97@gmail.com>
2025-01-30 19:14:42 +0000

    Fixes #38160 - Update hosts link to new hosts URL

--- webpack/scenes/ContentViews/expansions/DetailsExpansion.js
+++ webpack/scenes/ContentViews/expansions/DetailsExpansion.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { translate as __ } from 'foremanReact/common/I18n';
+import { useForemanHostsPageUrl } from 'foremanReact/Root/Context/ForemanContext';
 import RelatedCompositeContentViewsModal from './RelatedCompositeContentViewsModal';
 import RelatedContentViewComponentsModal from './RelatedContentViewComponentsModal';
 
@@ -9,6 +10,9 @@ const DetailsExpansion = ({
 }) => {
   const activationKeyCount = activationKeys.length;
   const hostCount = hosts.length;
+
+  const baseHostsPageUrl = useForemanHostsPageUrl();
+  const hostsPageUrl = `${baseHostsPageUrl}?search=${encodeURIComponent(`content_view_id=${cvId}`)}`;
 
   const relatedContentViewModal = () => {
     if (cvComposite) {
@@ -36,7 +40,7 @@ const DetailsExpansion = ({
     <div id={`cv-details-expansion-${cvId}`}>
       {__('Activation keys: ')}<a aria-label={`activation_keys_link_${cvId}`} href={`/activation_keys?search=content_view_id+%3D+${cvId}`}>{activationKeyCount}</a>
       <br />
-      {__('Hosts: ')}<a aria-label={`host_link_${cvId}`} href={`/hosts?search=content_view_id+%3D+${cvId}`}>{hostCount}</a>
+      {__('Hosts: ')}<a aria-label={`host_link_${cvId}`} href={hostsPageUrl}>{hostCount}</a>
       <br />
       {relatedContentViewModal()}
     </div>
