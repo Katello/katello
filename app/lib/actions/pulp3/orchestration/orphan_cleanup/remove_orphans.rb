6a25570d6f662d685052c71c678ccbf2392bfdf7
Ian Ballou <ianballou67@gmail.com>
2025-03-26 19:24:41 +0000

    Refs #38205 - delete orphan distributions before repository versions

--- app/lib/actions/pulp3/orchestration/orphan_cleanup/remove_orphans.rb
+++ app/lib/actions/pulp3/orchestration/orphan_cleanup/remove_orphans.rb
@@ -6,10 +6,10 @@ module Actions
           def plan(proxy)
             if proxy.pulp3_enabled?
               sequence do
+                plan_action(Actions::Pulp3::OrphanCleanup::DeleteOrphanDistributions, proxy)
                 plan_action(Actions::Pulp3::OrphanCleanup::DeleteOrphanRepositoryVersions, proxy)
                 if proxy.pulp_mirror?
                   plan_action(Actions::Pulp3::OrphanCleanup::RemoveUnneededRepos, proxy)
-                  plan_action(Actions::Pulp3::OrphanCleanup::DeleteOrphanDistributions, proxy)
                   plan_action(Actions::Pulp3::OrphanCleanup::DeleteOrphanAlternateContentSources, proxy)
                   plan_action(Actions::Pulp3::OrphanCleanup::DeleteOrphanRemotes, proxy)
                 end
