871b862a1daf09bde35cebaf34228ea3084e57ac
Vladimir Sedmik <46570670+vsedmik@users.noreply.github.com>
2025-03-28 14:28:22 +0100

    Fixes #38308 - Remove recommended RH repo duplicates (#11350)

--- webpack/redux/actions/RedHatRepositories/helpers.js
+++ webpack/redux/actions/RedHatRepositories/helpers.js
@@ -12,8 +12,6 @@ const repoTypeSearchQueryMap = {
 const recommendedRepositoriesRHEL = [
   'rhel-10-for-x86_64-baseos-rpms',
   'rhel-10-for-x86_64-baseos-kickstart',
-  'rhel-10-for-x86_64-baseos-rpms',
-  'rhel-10-for-x86_64-baseos-kickstart',
   'rhel-10-for-x86_64-appstream-rpms',
   'rhel-10-for-x86_64-appstream-kickstart',
   'rhel-10-for-x86_64-baseos-eus-rpms',
