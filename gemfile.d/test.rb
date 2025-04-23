2b4033443f6fb2ea70eaec69734854200b85668b
Ian Ballou <ianballou67@gmail.com>
2025-04-23 17:21:18 +0000

    Refs #38271 - Test without excon

--- gemfile.d/test.rb
+++ gemfile.d/test.rb
@@ -1,5 +1,6 @@
 group :test do
   gem "vcr", "~> 6.1"
-  gem "faraday-excon", ">= 1.1.0", "< 2.0.0"
-  gem "faraday-net_http", ">= 1.0.1", "< 2.0.0"
+  # Test: dont include these test requirements
+  #gem "faraday-excon", ">= 1.1.0", "< 2.0.0"
+  #gem "faraday-net_http", ">= 1.0.1", "< 2.0.0"
 end
