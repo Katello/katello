2f5a8e19750ffaae2b3b4509f5fe857291bc3fa7
Pavan Soma Shekar <32700193+pavanshekar@users.noreply.github.com>
2025-07-21 10:21:25 -0400

    Fixes #38537 - Add mirror action (#11442)

--- webpack/scenes/FlatpakRemotes/Details/FlatpakRemoteDetailActions.js
+++ webpack/scenes/FlatpakRemotes/Details/FlatpakRemoteDetailActions.js
@@ -1,6 +1,7 @@
-import { API_OPERATIONS, get, put } from 'foremanReact/redux/API';
+import { API_OPERATIONS, get, post, put } from 'foremanReact/redux/API';
 import { translate as __ } from 'foremanReact/common/I18n';
 import { flatpakRemoteDetailsKey,
+  flatpakRemoteRepositoriesKey,
   UPDATE_FLATPAK_REMOTE,
   UPDATE_FLATPAK_REMOTE_SUCCESS,
   UPDATE_FLATPAK_REMOTE_FAILURE } from '../FlatpakRemotesConstants';
@@ -13,6 +14,14 @@ const getFlatpakRemoteDetails = (id, extraParams = {}) => get({
   params: { organization_id: orgId(), include_permissions: true, ...extraParams },
   url: api.getApiUrl(`/flatpak_remotes/${id}`),
 });
+
+export const getRemoteRepositories = frId => () =>
+  get({
+    type: API_OPERATIONS.GET,
+    key: flatpakRemoteRepositoriesKey(frId),
+    url: api.getApiUrl(`/flatpak_remotes/${frId}/flatpak_remote_repositories`),
+    params: { organization_id: orgId() },
+  });
 
 export const updateFlatpakRemote = (frId, params, handleSuccess) => put({
   type: API_OPERATIONS.PUT,
@@ -29,5 +38,22 @@ export const updateFlatpakRemote = (frId, params, handleSuccess) => put({
     FAILURE: UPDATE_FLATPAK_REMOTE_FAILURE,
   },
 });
+
+export const mirrorFlatpakRepository = (
+  flatpakRepoId,
+  productName,
+  handleSuccess,
+  handleError,
+) =>
+  post({
+    type: API_OPERATIONS.POST,
+    key: flatpakRemoteRepositoriesKey(flatpakRepoId),
+    url: api.getApiUrl(`/flatpak_remote_repositories/${flatpakRepoId}/mirror`),
+    params: { product_name: productName, organization_id: orgId() },
+    handleSuccess,
+    handleError,
+    successToast: () => __('Repository mirroring task started in the background'),
+    errorToast: error => getResponseErrorMsgs(error.response),
+  });
 
 export default getFlatpakRemoteDetails;
