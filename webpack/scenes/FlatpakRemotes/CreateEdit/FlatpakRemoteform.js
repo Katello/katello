7b75c70e1076e9603ee8963f477d3ab2965e4314
Samir Jha <samirjha1525@gmail.com>
2025-07-22 14:46:30 -0400

    Fixes #38596 - Add remote detail actions to details page (#11446)

--- webpack/scenes/FlatpakRemotes/CreateEdit/FlatpakRemoteform.js
+++ webpack/scenes/FlatpakRemotes/CreateEdit/FlatpakRemoteform.js
@@ -77,7 +77,7 @@ const FlatpakRemotesForm = ({ setModalOpen, remoteData }) => {
           url,
           username,
           token: password,
-        }, () => window.location.assign('/flatpak_remotes/'),
+        }, () => window.location.assign(`/flatpak_remotes/${editingId}`),
         () => setModalOpen(false),
       ) :
       createFlatpakRemote({
