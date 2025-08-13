e60f3f93e6505f3bf053b263eaa368c33c9b5e8f
Jeremy Lenz <jlenz@redhat.com>
2025-08-13 09:20:34 -0400

    Fixes #38443 - Improve rabl and scoped search on content view environments (#11405)
    - add id field to rabl in a few places
      - split validation and saving of content view environments params
    
    (cherry picked from commit 53942dfd1e31bd7539a2bee759363f482d39a878)

--- app/controllers/katello/concerns/api/v2/hosts_controller_extensions.rb
+++ app/controllers/katello/concerns/api/v2/hosts_controller_extensions.rb
@@ -18,7 +18,8 @@ module Katello
 
       included do
         prepend Overrides
-        before_action :set_content_view_environments, only: [:create, :update]
+        around_action :handle_content_view_environments_for_create, only: [:create]
+        before_action :handle_content_view_environments_for_update, only: [:update]
 
         def destroy
           Katello::RegistrationManager.unregister_host(@host, :unregistering => false)
@@ -34,21 +35,48 @@ module Katello
           render(:locals => { :resource => @host }, :template => 'katello/api/v2/hosts/show', :status => :ok)
         end
 
-        def set_content_view_environments
+        def handle_content_view_environments_for_create
+          # validations should occur before the action so that the request can fail and not render multiple responses
+          cves = validate_content_view_environment_params
+          yield
+          # the actual assigning needs to wait until the host is created
+          set_content_view_environments(cves)
+        end
+
+        def handle_content_view_environments_for_update
+          cves = validate_content_view_environment_params
+          set_content_view_environments(cves)
+        end
+
+        def validate_content_view_environment_params
           content_facet_attributes = params.dig(:host, :content_facet_attributes)
-          return if content_facet_attributes.blank? || @host&.content_facet.blank? ||
-            (cve_params[:content_view_id].present? && cve_params[:lifecycle_environment_id].present?)
+          return if content_facet_attributes.blank? ||
+          (cve_params[:content_view_id].present? && cve_params[:lifecycle_environment_id].present?)
+
           cves = ::Katello::ContentViewEnvironment.fetch_content_view_environments(
             labels: cve_params[:content_view_environments],
             ids: cve_params[:content_view_environment_ids],
-            organization: @organization || @host&.organization)
-          if cves.present?
-            @host.content_facet.content_view_environments = cves
-          else
+            organization: find_organization || @host&.organization)
+          if cves.blank?
             handle_errors(labels: cve_params[:content_view_environments],
               ids: cve_params[:content_view_environment_ids])
           end
+          cves
         end
+
+        # rubocop:disable Naming/AccessorMethodName
+        def set_content_view_environments(cves)
+          return if cves.blank?
+          if @host.blank?
+            Rails.logger.debug "No host; not assigning content view environments"
+            return
+          elsif @host&.content_facet.blank?
+            content_facet = Katello::Host::ContentFacet.new(host: @host)
+            @host.content_facet = content_facet
+          end
+          @host.content_facet.content_view_environments = cves
+        end
+        # rubocop:enable Naming/AccessorMethodName
 
         def cve_params
           params.require(:host).require(:content_facet_attributes).permit(:content_view_id, :lifecycle_environment_id, content_view_environments: [], content_view_environment_ids: [])
