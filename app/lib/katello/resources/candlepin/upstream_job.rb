8dc78dd030b51729faf5eb81b30278dee2d9ced7
Jeremy Lenz <jlenz@redhat.com>
2025-09-05 17:42:44 -0400

    Fixes #38724 - Handle /consumers/ path in manifest apiUrl

--- app/lib/katello/resources/candlepin/upstream_job.rb
+++ app/lib/katello/resources/candlepin/upstream_job.rb
@@ -5,13 +5,14 @@ module Katello
         class << self
           NOT_FINISHED_STATES = %w(CREATED WAITING PENDING RUNNING).freeze unless defined? NOT_FINISHED_STATES
           API_URL = 'https://subscription.rhsm.redhat.com/subscription'.freeze
+          SUBSCRIPTION_PATH = '/subscription'.freeze
 
           def not_finished?(job)
             NOT_FINISHED_STATES.include?(job[:state])
           end
 
           def get(id, upstream)
-            url = ENV['REDHAT_RHSM_API_URL'] || API_URL
+            url = ENV['REDHAT_RHSM_API_URL'] || subscription_path(upstream['apiUrl']) || API_URL
             response = Resources::Candlepin::UpstreamConsumer.start_upstream_export("#{url}#{path(id)}", upstream['idCert']['cert'],
               upstream['idCert']['key'], nil)
             job = JSON.parse(response)
@@ -20,6 +21,13 @@ module Katello
 
           def path(id = nil)
             "/jobs/#{id}"
+          end
+
+          def subscription_path(upstream_api_url)
+            return if upstream_api_url.blank?
+            uri = URI.parse(upstream_api_url) # https://subscription.rhsm.redhat.com/subscription/consumers/
+            uri.path = SUBSCRIPTION_PATH # https://subscription.rhsm.redhat.com/subscription
+            uri.to_s
           end
         end
       end
