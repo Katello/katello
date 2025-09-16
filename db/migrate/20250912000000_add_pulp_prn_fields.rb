37b0cb3da5bec52338e0bcb5b7b4076ecaf3f48d
Samir Jha <samirjha1525@gmail.com>
2025-09-16 18:22:52 +0000

    Fixes #38751 - Populate PRN fields for DB records in a migration

--- db/migrate/20250912000000_add_pulp_prn_fields.rb
+++ db/migrate/20250912000000_add_pulp_prn_fields.rb
@@ -5,14 +5,12 @@ class AddPulpPrnFields < ActiveRecord::Migration[7.0]
     add_column :katello_content_guards, :pulp_prn, :string
 
     # Tables with pulp_id fields - add pulp_prn field
-    add_column :katello_repositories, :pulp_prn, :string
     add_column :katello_ansible_collections, :pulp_prn, :string
     add_column :katello_generic_content_units, :pulp_prn, :string
     add_column :katello_module_streams, :pulp_prn, :string
     add_column :katello_docker_manifest_lists, :pulp_prn, :string
     add_column :katello_docker_manifests, :pulp_prn, :string
     add_column :katello_docker_tags, :pulp_prn, :string
-    add_column :katello_errata, :pulp_prn, :string
     add_column :katello_rpms, :pulp_prn, :string
     add_column :katello_srpms, :pulp_prn, :string
     add_column :katello_files, :pulp_prn, :string # FileUnit uses table_name 'katello_files'
@@ -32,56 +30,28 @@ class AddPulpPrnFields < ActiveRecord::Migration[7.0]
 
     # Add indexes for performance on commonly queried fields
     add_index :katello_content_guards, :pulp_prn
-    add_index :katello_repositories, :pulp_prn
     add_index :katello_repositories, :remote_prn
     add_index :katello_repositories, :publication_prn
     add_index :katello_repositories, :version_prn
-    add_index :katello_ansible_collections, :pulp_prn
-    add_index :katello_generic_content_units, :pulp_prn
-    add_index :katello_module_streams, :pulp_prn
-    add_index :katello_docker_manifest_lists, :pulp_prn
-    add_index :katello_docker_manifests, :pulp_prn
-    add_index :katello_docker_tags, :pulp_prn
-    add_index :katello_errata, :pulp_prn
-    add_index :katello_rpms, :pulp_prn
-    add_index :katello_srpms, :pulp_prn
-    add_index :katello_files, :pulp_prn
-    add_index :katello_package_groups, :pulp_prn
-    add_index :katello_debs, :pulp_prn
     add_index :katello_repository_errata, :erratum_prn
   end
 
   def down
     # Remove indexes
     remove_index :katello_content_guards, :pulp_prn
-    remove_index :katello_repositories, :pulp_prn
     remove_index :katello_repositories, :remote_prn
     remove_index :katello_repositories, :publication_prn
     remove_index :katello_repositories, :version_prn
-    remove_index :katello_ansible_collections, :pulp_prn
-    remove_index :katello_generic_content_units, :pulp_prn
-    remove_index :katello_module_streams, :pulp_prn
-    remove_index :katello_docker_manifest_lists, :pulp_prn
-    remove_index :katello_docker_manifests, :pulp_prn
-    remove_index :katello_docker_tags, :pulp_prn
-    remove_index :katello_errata, :pulp_prn
-    remove_index :katello_rpms, :pulp_prn
-    remove_index :katello_srpms, :pulp_prn
-    remove_index :katello_files, :pulp_prn
-    remove_index :katello_package_groups, :pulp_prn
-    remove_index :katello_debs, :pulp_prn
     remove_index :katello_repository_errata, :erratum_prn
 
     # Remove columns - pulp_prn fields
     remove_column :katello_content_guards, :pulp_prn
-    remove_column :katello_repositories, :pulp_prn
     remove_column :katello_ansible_collections, :pulp_prn
     remove_column :katello_generic_content_units, :pulp_prn
     remove_column :katello_module_streams, :pulp_prn
     remove_column :katello_docker_manifest_lists, :pulp_prn
     remove_column :katello_docker_manifests, :pulp_prn
     remove_column :katello_docker_tags, :pulp_prn
-    remove_column :katello_errata, :pulp_prn
     remove_column :katello_rpms, :pulp_prn
     remove_column :katello_srpms, :pulp_prn
     remove_column :katello_files, :pulp_prn
