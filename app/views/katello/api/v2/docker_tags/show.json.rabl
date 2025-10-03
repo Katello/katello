160beb5a395db14cbf9434712f0c17881ad5c947
Samir Jha <samirjha1525@gmail.com>
2025-10-03 19:37:10 +0000

    Fixes #38801 - Add manifest list children to the tags response json

--- app/views/katello/api/v2/docker_tags/show.json.rabl
+++ app/views/katello/api/v2/docker_tags/show.json.rabl
@@ -2,11 +2,6 @@ object @resource
 
 extends 'katello/api/v2/docker_tags/base'
 
-child :docker_manifest => :manifest do
-  attributes :uuid => :id
-  attributes :schema_version, :digest, :manifest_type
-end
-
 child :related_tags => :related_tags do
   attributes :id, :name
 end
--- test/controllers/api/v2/docker_tags_controller_test.rb
+++ test/controllers/api/v2/docker_tags_controller_test.rb
@@ -100,5 +100,85 @@ module Katello
       assert_response :success
       assert_template "katello/api/v2/docker_tags/compare"
     end
+
+    def test_show_manifest_list_children
+      # Create a manifest list with child manifests
+      manifest_list = FactoryBot.create(:docker_manifest_list)
+      child_manifest1 = FactoryBot.create(:docker_manifest, :digest => "sha256:child1")
+      child_manifest2 = FactoryBot.create(:docker_manifest, :digest => "sha256:child2")
+
+      manifest_list.docker_manifests << child_manifest1
+      manifest_list.docker_manifests << child_manifest2
+
+      # Create a tag for the manifest list
+      tag = @repo.docker_tags.create!(:name => "multiarch", :docker_taggable => manifest_list, :pulp_id => 'listpulpid')
+      meta_tag = DockerMetaTag.create!(:name => tag.name, :schema2 => tag, :repositories => [@repo])
+
+      get :show, params: { :repository_id => @repo.id, :id => meta_tag.id }
+
+      assert_response :success
+      body = JSON.parse(response.body)
+
+      # Verify manifest type is 'list'
+      assert_equal 'list', body['manifest']['manifest_type']
+
+      # Verify children are included (factory creates 1 child + 2 we added = 3 total)
+      assert_not_nil body['manifest']['manifests']
+      assert_equal 3, body['manifest']['manifests'].length
+
+      # Verify child manifest details
+      child_digests = body['manifest']['manifests'].map { |m| m['digest'] }
+      assert_includes child_digests, 'sha256:child1'
+      assert_includes child_digests, 'sha256:child2'
+
+      # Verify each child has required fields
+      body['manifest']['manifests'].each do |child|
+        assert_not_nil child['id']
+        assert_not_nil child['digest']
+        assert_not_nil child['schema_version']
+        assert_equal 'image', child['manifest_type']
+      end
+    end
+
+    def test_index_manifest_list_children
+      # Create a manifest list with child manifests
+      manifest_list = FactoryBot.create(:docker_manifest_list)
+      child_manifest = FactoryBot.create(:docker_manifest, :digest => "sha256:indexchild1")
+      manifest_list.docker_manifests << child_manifest
+
+      # Create a tag for the manifest list
+      tag = @repo.docker_tags.create!(:name => "multiarch-index", :docker_taggable => manifest_list, :pulp_id => 'listindexpulpid')
+      DockerMetaTag.create!(:name => tag.name, :schema2 => tag, :repositories => [@repo])
+
+      get :index, params: { :repository_id => @repo.id }
+
+      assert_response :success
+      body = JSON.parse(response.body)
+
+      # Find the manifest list tag in results
+      list_tag = body['results'].find { |t| t['name'] == 'multiarch-index' }
+      assert_not_nil list_tag
+
+      # Verify manifest children are present (factory creates 1 child + 1 we added = 2 total)
+      assert_equal 'list', list_tag['manifest']['manifest_type']
+      assert_not_nil list_tag['manifest']['manifests']
+      assert_equal 2, list_tag['manifest']['manifests'].length
+      child_digests = list_tag['manifest']['manifests'].map { |m| m['digest'] }
+      assert_includes child_digests, 'sha256:indexchild1'
+    end
+
+    def test_show_non_list_manifest_has_no_children
+      # Regular image manifests should not have children node
+      get :show, params: { :repository_id => @repo.id, :id => @meta_tag.id }
+
+      assert_response :success
+      body = JSON.parse(response.body)
+
+      # Verify it's an image type
+      assert_equal 'image', body['manifest']['manifest_type']
+
+      # Verify no manifests array for non-list types
+      assert_nil body['manifest']['manifests']
+    end
   end
 end
