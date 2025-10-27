47c747749e8649cacdcaacedd9084521d482a193
Ian Ballou <ianballou67@gmail.com>
2025-10-27 18:07:27 +0000

    Refs #38856 - address rubocop concerns.

--- config/initializers/foreman_tasks_chaining.rb
+++ config/initializers/foreman_tasks_chaining.rb
@@ -6,43 +6,45 @@
 #
 # See: https://github.com/Dynflow/dynflow/pull/446
 
+module ForemanTasksChaining
+  # Chain execution plans so that a new plan waits until prerequisite plans finish before executing.
+  # This is useful for coordinating dependent tasks where one task should only run after
+  # other tasks have completed successfully.
+  #
+  # The chained plan will remain in 'scheduled' state until all prerequisite plans
+  # reach 'stopped' state (regardless of success/failure).
+  #
+  # @param plan_uuids [String, Array<String>] UUID(s) of prerequisite execution plan(s)
+  # @param action [Class] Action class to execute
+  # @param args Arguments to pass to the action
+  # @return [ForemanTasks::Task::DynflowTask] The chained task that will wait for prerequisites
+  #
+  # @example Chain a task to wait for another task
+  #   task1 = ForemanTasks.async_task(SomeAction)
+  #   task2 = ForemanTasks.chain(task1.external_id, AnotherAction, arg1, arg2)
+  #   # task2 will only execute after task1 completes
+  #
+  # @example Chain a task to wait for multiple tasks
+  #   task1 = ForemanTasks.async_task(Action1)
+  #   task2 = ForemanTasks.async_task(Action2)
+  #   task3 = ForemanTasks.chain([task1.external_id, task2.external_id], Action3)
+  #   # task3 will only execute after both task1 and task2 complete
+  def chain(plan_uuids, action, *args)
+    result = dynflow.world.chain(plan_uuids, action, *args)
+    # The ForemanTasks record may not exist yet for delayed plans,
+    # so we need to find or create it and properly initialize it
+    ForemanTasks::Task.find_by(:external_id => result.id) ||
+      begin
+        delayed_plan = dynflow.world.persistence.load_delayed_plan(result.id)
+        execution_plan = dynflow.world.persistence.load_execution_plan(result.id)
+        ForemanTasks::Task::DynflowTask.new_for_execution_plan(execution_plan).tap do |task|
+          task.update_from_dynflow(execution_plan, delayed_plan)
+        end
+      end
+  end
+end
+
 # Defer extension until after ForemanTasks module is loaded
 Rails.application.config.to_prepare do
-  module ForemanTasks
-    # Chain execution plans so that a new plan waits until prerequisite plans finish before executing.
-    # This is useful for coordinating dependent tasks where one task should only run after
-    # other tasks have completed successfully.
-    #
-    # The chained plan will remain in 'scheduled' state until all prerequisite plans
-    # reach 'stopped' state (regardless of success/failure).
-    #
-    # @param plan_uuids [String, Array<String>] UUID(s) of prerequisite execution plan(s)
-    # @param action [Class] Action class to execute
-    # @param args Arguments to pass to the action
-    # @return [ForemanTasks::Task::DynflowTask] The chained task that will wait for prerequisites
-    #
-    # @example Chain a task to wait for another task
-    #   task1 = ForemanTasks.async_task(SomeAction)
-    #   task2 = ForemanTasks.chain(task1.external_id, AnotherAction, arg1, arg2)
-    #   # task2 will only execute after task1 completes
-    #
-    # @example Chain a task to wait for multiple tasks
-    #   task1 = ForemanTasks.async_task(Action1)
-    #   task2 = ForemanTasks.async_task(Action2)
-    #   task3 = ForemanTasks.chain([task1.external_id, task2.external_id], Action3)
-    #   # task3 will only execute after both task1 and task2 complete
-    def self.chain(plan_uuids, action, *args)
-      result = dynflow.world.chain(plan_uuids, action, *args)
-      # The ForemanTasks record may not exist yet for delayed plans,
-      # so we need to find or create it and properly initialize it
-      ForemanTasks::Task.find_by(:external_id => result.id) ||
-        begin
-          delayed_plan = dynflow.world.persistence.load_delayed_plan(result.id)
-          execution_plan = dynflow.world.persistence.load_execution_plan(result.id)
-          ForemanTasks::Task::DynflowTask.new_for_execution_plan(execution_plan).tap do |task|
-            task.update_from_dynflow(execution_plan, delayed_plan)
-          end
-        end
-    end
-  end
+  ForemanTasks.singleton_class.prepend(ForemanTasksChaining)
 end
