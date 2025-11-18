65a4a55d1bb4169916ef17664cb707113c45020f
Quirin Pamp <pamp@atix.de>
2025-11-18 15:47:59 +0100

    Fixes #38912 - Update deb content URL options via metadata generate
    (cherry picked from commit c5beb0a5f404ef8538de855f5ed767c4c296070d)

--- app/lib/actions/katello/repository/metadata_generate.rb
+++ app/lib/actions/katello/repository/metadata_generate.rb
@@ -3,7 +3,8 @@ module Actions
     module Repository
       class MetadataGenerate < Actions::EntryAction
         def plan(repository, options = {})
-          return if repository.root.is_container_push && repository.library_instance?
+          root = repository.root
+          return if root.is_container_push && repository.library_instance?
           action_subject(repository)
           repository.check_ready_to_act!
           source_repository = options.fetch(:source_repository, nil)
@@ -17,6 +18,20 @@ module Actions
                         :force_publication => force_publication,
                         :source_repository => source_repository,
                         :matching_content => matching_content)
+
+          if repository.deb? && repository.content && !repository.content.content_url.end_with?(repository.deb_content_url_options)
+            plan_action(::Actions::Candlepin::Product::ContentUpdate,
+                        owner:           repository.organization.label,
+                        repository_id:   repository.id,
+                        name:            root.name,
+                        type:            root.content_type,
+                        arches:          root.format_arches,
+                        label:           repository.content.label,
+                        content_url:     root.custom_content_path,
+                        gpg_key_url:     repository.yum_gpg_key_url,
+                        os_versions:     root.os_versions&.join(','),
+                        metadata_expire: root.metadata_expire)
+          end
         end
 
         def resource_locks
