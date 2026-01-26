c95da0aa77f5a11b1d71d74a7768bd8b26ba6737
Ian Ballou <ianballou67@gmail.com>
2026-01-26 23:12:39 +0000

    Fixes #38856 - chain composite CV publishes to wait on children

--- test/services/katello/content_view_manager_test.rb
+++ test/services/katello/content_view_manager_test.rb
@@ -29,9 +29,50 @@ module Katello
     test 'trigger_auto_publish!' do
       request = build_stubbed(:katello_content_view_auto_publish_request)
 
-      request.expects(:reload).with(lock: true)
+      request.expects(:with_lock).yields
       request.expects(:destroy!)
-      ForemanTasks.expects(:async_task).with(::Actions::Katello::ContentView::Publish, request.content_view, anything, auto_published: true, triggered_by_id: request.content_view_version_id)
+      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(false)
+      Katello::ContentViewManager.expects(:running_component_publish_task_ids).returns([])
+      ForemanTasks.expects(:async_task).with(
+        ::Actions::Katello::ContentView::Publish,
+        request.content_view,
+        anything,
+        auto_published: true,
+        triggered_by_id: request.content_view_version_id
+      )
+
+      Katello::ContentViewManager.trigger_auto_publish!(request: request)
+    end
+
+    test 'trigger_auto_publish with chaining' do
+      request = build_stubbed(:katello_content_view_auto_publish_request)
+      sibling_task_ids = ['task-1', 'task-2']
+      mock_world = mock('world')
+
+      request.expects(:with_lock).yields
+      request.expects(:destroy!)
+      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(false)
+      Katello::ContentViewManager.expects(:running_component_publish_task_ids).returns(sibling_task_ids)
+      ForemanTasks.dynflow.expects(:world).returns(mock_world)
+      mock_world.expects(:chain).with(
+        sibling_task_ids,
+        Actions::Katello::ContentView::Publish,
+        request.content_view,
+        anything,
+        auto_published: true,
+        triggered_by_id: request.content_view_version_id
+      )
+
+      Katello::ContentViewManager.trigger_auto_publish!(request: request)
+    end
+
+    test 'trigger_auto_publish skips when composite already scheduled' do
+      request = build_stubbed(:katello_content_view_auto_publish_request)
+
+      request.expects(:with_lock).yields
+      request.expects(:destroy!)
+      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(true)
+      ForemanTasks.expects(:async_task).never
 
       Katello::ContentViewManager.trigger_auto_publish!(request: request)
     end
@@ -39,8 +80,9 @@ module Katello
     test 'trigger_auto_publish locks found' do
       request = build_stubbed(:katello_content_view_auto_publish_request)
 
-      request.expects(:reload).with(lock: true)
+      request.expects(:with_lock).yields
       request.expects(:destroy!).never
+      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(false)
       Katello::ContentViewManager.expects(:content_view_locks).with(content_view: request.content_view).returns([1])
       ForemanTasks.expects(:async_task).never
 
@@ -50,8 +92,10 @@ module Katello
     test 'trigger_auto_publish lock conflict' do
       request = build_stubbed(:katello_content_view_auto_publish_request)
 
-      request.expects(:reload).with(lock: true)
+      request.expects(:with_lock).yields
       request.expects(:destroy!).never
+      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(false)
+      Katello::ContentViewManager.expects(:running_component_publish_task_ids).returns([])
       ForemanTasks.expects(:async_task).raises(ForemanTasks::Lock::LockConflict.new(mock, []))
 
       Katello::ContentViewManager.trigger_auto_publish!(request: request)
@@ -60,7 +104,9 @@ module Katello
     test 'trigger_auto_publish unhandled error' do
       request = build_stubbed(:katello_content_view_auto_publish_request)
 
-      request.expects(:reload).with(lock: true)
+      request.expects(:with_lock).yields
+      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(false)
+      Katello::ContentViewManager.expects(:running_component_publish_task_ids).returns([])
       ForemanTasks.expects(:async_task).raises(StandardError)
 
       assert_raises(StandardError) do
