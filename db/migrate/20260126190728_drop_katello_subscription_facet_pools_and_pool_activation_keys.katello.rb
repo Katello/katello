693ddc93764607e4047ec4d830c8bf376de1be0a
Jonathon Turel <jturel@gmail.com>
2026-01-28 21:08:45 +0000

    Migration can be reversed

--- db/migrate/20260126190728_drop_katello_subscription_facet_pools_and_pool_activation_keys.katello.rb
+++ db/migrate/20260126190728_drop_katello_subscription_facet_pools_and_pool_activation_keys.katello.rb
@@ -1,7 +1,7 @@
 class DropKatelloSubscriptionFacetPoolsAndPoolActivationKeys < ActiveRecord::Migration[7.0]
   def change
-    drop_table :katello_pool_activation_keys
-    drop_table :katello_subscription_facet_pools
+    drop_table :katello_pool_activation_keys, if_exists: true
+    drop_table :katello_subscription_facet_pools, if_exists: true
 
     NotificationBlueprint.where(name: ['sca_disable_error', 'sca_disable_success', 'sca_enable_error', 'sca_enable_success']).destroy_all
   end
