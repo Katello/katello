6648380391fed6c12a1620955e3400cbb810126b
Ian Ballou <ianballou67@gmail.com>
2026-02-10 23:25:29 +0000

    Refs #38856 - skip auto publish request if already scheduled

--- test/services/katello/content_view_manager_test.rb
+++ test/services/katello/content_view_manager_test.rb
@@ -26,12 +26,35 @@ module Katello
       assert_nil result
     end
 
+    test 'request_auto_publish skips when composite already scheduled' do
+      composite_cv = katello_content_views(:composite_view)
+      cvv = katello_content_view_versions(:composite_view_version_1)
+
+      # Create a scheduled publish task for the composite CV
+      task = ForemanTasks::Task.create!(
+        type: 'ForemanTasks::Task::DynflowTask',
+        label: 'Actions::Katello::ContentView::Publish',
+        state: 'scheduled',
+        result: 'pending',
+        external_id: 'test-scheduled-publish-123'
+      )
+
+      # Mock the delayed plan to return our composite CV as first arg
+      delayed_plan = mock('delayed_plan')
+      delayed_plan.stubs(:args).returns([composite_cv])
+      ForemanTasks.dynflow.world.persistence.stubs(:load_delayed_plan).with(task.external_id).returns(delayed_plan)
+
+      result = Katello::ContentViewManager.request_auto_publish(content_view: composite_cv, content_view_version: cvv)
+
+      assert_nil result
+      assert_nil composite_cv.auto_publish_request
+    end
+
     test 'trigger_auto_publish!' do
       request = build_stubbed(:katello_content_view_auto_publish_request)
 
       request.expects(:with_lock).yields
       request.expects(:destroy!)
-      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(false)
       Katello::ContentViewManager.expects(:running_component_publish_task_ids).returns([])
       ForemanTasks.expects(:async_task).with(
         ::Actions::Katello::ContentView::Publish,
@@ -51,7 +74,6 @@ module Katello
 
       request.expects(:with_lock).yields
       request.expects(:destroy!)
-      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(false)
       Katello::ContentViewManager.expects(:running_component_publish_task_ids).returns(sibling_task_ids)
       ForemanTasks.dynflow.expects(:world).returns(mock_world)
       mock_world.expects(:chain).with(
@@ -66,23 +88,11 @@ module Katello
       Katello::ContentViewManager.trigger_auto_publish!(request: request)
     end
 
-    test 'trigger_auto_publish skips when composite already scheduled' do
-      request = build_stubbed(:katello_content_view_auto_publish_request)
-
-      request.expects(:with_lock).yields
-      request.expects(:destroy!)
-      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(true)
-      ForemanTasks.expects(:async_task).never
-
-      Katello::ContentViewManager.trigger_auto_publish!(request: request)
-    end
-
     test 'trigger_auto_publish locks found' do
       request = build_stubbed(:katello_content_view_auto_publish_request)
 
       request.expects(:with_lock).yields
       request.expects(:destroy!).never
-      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(false)
       Katello::ContentViewManager.expects(:content_view_locks).with(content_view: request.content_view).returns([1])
       ForemanTasks.expects(:async_task).never
 
@@ -94,7 +104,6 @@ module Katello
 
       request.expects(:with_lock).yields
       request.expects(:destroy!).never
-      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(false)
       Katello::ContentViewManager.expects(:running_component_publish_task_ids).returns([])
       ForemanTasks.expects(:async_task).raises(ForemanTasks::Lock::LockConflict.new(mock, []))
 
@@ -105,7 +114,6 @@ module Katello
       request = build_stubbed(:katello_content_view_auto_publish_request)
 
       request.expects(:with_lock).yields
-      Katello::ContentViewManager.expects(:scheduled_composite_publish?).returns(false)
       Katello::ContentViewManager.expects(:running_component_publish_task_ids).returns([])
       ForemanTasks.expects(:async_task).raises(StandardError)
 
