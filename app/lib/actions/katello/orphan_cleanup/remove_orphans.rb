fcfe4c61bde4ea6336089fb6eda1b9d727c6304a
Pavel Moravec <pmoravec@redhat.com>
2026-02-27 14:38:54 +0100

    Fixes #39112 - run Caps UpdateContentCounts from RemoveOrphans conditionally
    Signed-off-by: Pavel Moravec <pmoravec@redhat.com>

--- app/lib/actions/katello/orphan_cleanup/remove_orphans.rb
+++ app/lib/actions/katello/orphan_cleanup/remove_orphans.rb
@@ -21,9 +21,11 @@ module Actions
         end
 
         def finalize
-          smart_proxy = ::SmartProxy.unscoped.find(input[:smart_proxy_id])
-          if smart_proxy.pulp_mirror?
-            ::ForemanTasks.async_task(::Actions::Katello::CapsuleContent::UpdateContentCounts, smart_proxy)
+          if Setting[:automatic_content_count_updates]
+            smart_proxy = ::SmartProxy.unscoped.find(input[:smart_proxy_id])
+            if smart_proxy.pulp_mirror?
+              ::ForemanTasks.async_task(::Actions::Katello::CapsuleContent::UpdateContentCounts, smart_proxy)
+            end
           end
         end
       end
