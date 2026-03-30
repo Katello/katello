821d05d7b5761b6874da7e1eee0f242728384490
Quirin Pamp <pamp@atix.de>
2026-03-30 16:00:46 +0200

    Fixes #39054 - Rely on pulp_deb for empty deb metadata
    Replaces https://github.com/Katello/katello/pull/11631
    
    Requires https://github.com/pulp/pulp_deb/pull/1424
    
    This reworks how empty deb repos are handling. Instead of "initializing"
    all deb type repos with "empty metadata content", we now rely on a
    change in pulp_deb to automatically publish minimal empty metadata for
    empty repository versions.
    
    This approach greatly reduces the complexity on the Katello side, since
    there are no special workflows for adding or removing "empty metadata"
    to repositories. Empty repository versions are simply empty and pulp_deb
    handles them in a standard way that Katello knows how to configure  on
    hosts and sync to smart proxy.
    
    As part of this change we reduced the number of duplicate API calls
    Katello sends to Pulp.
    
    We also became stricter in throwing a hard error if we encounter a Pulp
    repository that does not include any distributions after accounting for
    empty repository versions as well as the fallback mechanism for missing
    structure content.

--- app/lib/actions/pulp3/orchestration/repository/create.rb
+++ app/lib/actions/pulp3/orchestration/repository/create.rb
@@ -6,12 +6,7 @@ module Actions
           def plan(repository, smart_proxy, force = false)
             sequence do
               create_action = plan_action(Actions::Pulp3::Repository::Create, repository, smart_proxy, force)
-              if repository.deb? && repository.library_instance?
-                response = plan_action(Pulp3::Repository::Initialize, repository, smart_proxy)
-                plan_action(Actions::Pulp3::Repository::SaveVersion, repository, tasks: response.output[:pulp_tasks])
-              else
-                plan_action(Actions::Pulp3::Repository::SaveVersion, repository, repository_details: create_action.output[:response])
-              end
+              plan_action(Actions::Pulp3::Repository::SaveVersion, repository, repository_details: create_action.output[:response])
 
               if repository.content_view.default? || !smart_proxy.pulp_primary?
                 plan_action(Actions::Pulp3::Repository::CreateRemote, repository, smart_proxy)
