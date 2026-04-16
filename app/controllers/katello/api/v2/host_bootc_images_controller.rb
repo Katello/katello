97a2b7c7d27dee01b582d13fd1ba91c244b53b9c
tomaioo <hongsaygio.com@gmail.com>
2026-04-16 11:17:54 -0700

    fix(security): 2 improvements across 2 files
    - Security: Potential SQL injection via unsanitized `order` parameter
    - Security: Missing object-level authorization check when loading repository for uploads
    
    Signed-off-by: tomaioo <203048277+tomaioo@users.noreply.github.com>

--- app/controllers/katello/api/v2/host_bootc_images_controller.rb
+++ app/controllers/katello/api/v2/host_bootc_images_controller.rb
@@ -11,12 +11,8 @@ module Katello
     param_group :search, Api::V2::ApiController
     def bootc_images
       params[:sort_by] = sanitize_sort_column(params[:sort_by])
-      params[:sort_order] ||= 'asc'
-      if params[:order]
-        params[:order] = "#{params[:order].split(' ')[0]} #{sanitize_sort_order(params[:order].split(' ')[1])}"
-      else
-        params[:order] = "#{params[:sort_by]} #{sanitize_sort_order(params[:sort_order])}"
-      end
+      params[:sort_order] = sanitize_sort_order(params[:sort_order])
+      params[:order] = "#{params[:sort_by]} #{params[:sort_order]}"
       per_page = params[:per_page].present? ? params[:per_page].to_i : Setting[:entries_per_page]
       page = params[:page].present? ? params[:page].to_i : 1
 
