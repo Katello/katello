d3a1c690fb6e1ceddc674f9f7212c267cc6878f2
Samir Jha <samirjha1525@gmail.com>
2026-05-13 19:45:19 +0000

    Fixes #39319 - Generic content pages are not scoped by organization

--- webpack/scenes/Content/ContentActions.js
+++ webpack/scenes/Content/ContentActions.js
@@ -1,11 +1,14 @@
 import { API_OPERATIONS, get } from 'foremanReact/redux/API';
-import api from '../../services/api';
+import api, { orgId } from '../../services/api';
 import { CONTENT_KEY, CONTENT_TYPES_KEY, CONTENT_ID_KEY, REPOSITORY_CONTENT_ID_KEY } from './ContentConstants';
 
 export const getContent = (contentType, params) => get({
   type: API_OPERATIONS.GET,
   key: CONTENT_KEY,
-  params,
+  params: {
+    ...params,
+    organization_id: orgId(),
+  },
   url: api.getApiUrl(`/${contentType}`),
 });
 
@@ -18,13 +21,19 @@ export const getContentTypes = () => get({
 export const getContentDetails = (contentType, id) => get({
   type: API_OPERATIONS.GET,
   key: CONTENT_ID_KEY,
+  params: {
+    organization_id: orgId(),
+  },
   url: api.getApiUrl(`/${contentType}/${id}`),
 });
 
 export const getRepositoryContentDetails = (contentType, id, params) => get({
   type: API_OPERATIONS.GET,
   key: REPOSITORY_CONTENT_ID_KEY,
-  params,
+  params: {
+    ...params,
+    organization_id: orgId(),
+  },
   url: api.getApiUrl(`/repositories?${contentType}_id=${id}`),
 });
 
