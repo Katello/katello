cd5ac2340a86efe23ac101345fcc5b904ff72a43
Samir Jha <samirjha1525@gmail.com>
2026-05-21 19:18:58 +0000

    Fixes #39346 - Possible race condition due to synchronous distribution delete

--- app/lib/actions/pulp3/repository/delete_distributions.rb
+++ app/lib/actions/pulp3/repository/delete_distributions.rb
@@ -1,14 +1,21 @@
 module Actions
   module Pulp3
     module Repository
-      class DeleteDistributions < Pulp3::Abstract
+      class DeleteDistributions < Pulp3::AbstractAsyncTask
         def plan(repository_id, smart_proxy)
           plan_self(:repository_id => repository_id, :smart_proxy_id => smart_proxy.id)
         end
 
-        def run
+        def invoke_external_task
           repo = ::Katello::Repository.find(input[:repository_id])
-          repo.backend_service(smart_proxy).delete_distributions
+          output[:response] = repo.backend_service(smart_proxy).delete_distributions
+        end
+
+        def finalize
+          # Only destroy the distribution reference after Pulp task succeeds
+          repo = ::Katello::Repository.find(input[:repository_id])
+          dist_ref = repo.backend_service(smart_proxy).distribution_reference
+          dist_ref&.destroy!
         end
       end
     end
