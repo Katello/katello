a25be3fc0105743db6ffcb3181e03d2a01055f2a
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-08-11 09:51:43 -0400

    Fixes #39556 - migrate UpstreamSubscription table to TableIndexPage and switch to useApi instead of redux stores

--- webpack/scenes/Subscriptions/UpstreamSubscriptions/UpstreamSubscriptionsActions.js
+++ webpack/scenes/Subscriptions/UpstreamSubscriptions/UpstreamSubscriptionsActions.js
@@ -1,67 +1,45 @@
-import { propsToSnakeCase } from 'foremanReact/common/helpers';
-
+import React from 'react';
+import { APIActions } from 'foremanReact/redux/API';
+import { translate as __ } from 'foremanReact/common/I18n';
+import { urlBuilder } from 'foremanReact/common/urlHelpers';
 import api, { orgId } from '../../../services/api';
-import { apiError } from '../../../utils/helpers.js';
-
+import { apiError, getResponseErrorMsgs } from '../../../utils/helpers';
 import {
-  UPSTREAM_SUBSCRIPTIONS_REQUEST,
-  UPSTREAM_SUBSCRIPTIONS_SUCCESS,
-  UPSTREAM_SUBSCRIPTIONS_FAILURE,
-  SAVE_UPSTREAM_SUBSCRIPTIONS_REQUEST,
-  SAVE_UPSTREAM_SUBSCRIPTIONS_SUCCESS,
-  SAVE_UPSTREAM_SUBSCRIPTIONS_FAILURE,
+  SAVE_UPSTREAM_SUBSCRIPTIONS_KEY,
+  PING_UPSTREAM_SUBSCRIPTIONS_KEY,
   PING_UPSTREAM_SUBSCRIPTIONS_SUCCESS,
   PING_UPSTREAM_SUBSCRIPTIONS_FAILURE,
 } from './UpstreamSubscriptionsConstants';
 
-export const pingUpstreamSubscriptions = () => async (dispatch) => {
-  try {
-    const { data } = await api.get(`/organizations/${orgId()}/upstream_subscriptions/ping`);
-    return dispatch({
-      type: PING_UPSTREAM_SUBSCRIPTIONS_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    return dispatch(apiError(PING_UPSTREAM_SUBSCRIPTIONS_FAILURE, error));
-  }
+const saveSuccessToast = (response) => {
+  const task = response.data;
+
+  return (
+    <span>
+      <span>{__('Subscriptions have been saved and are being updated. ')}</span>
+      <a href={urlBuilder('foreman_tasks/tasks', '', task.id)}>
+        {__('Click here to go to the tasks page for the task.')}
+      </a>
+    </span>
+  );
 };
 
-export const loadUpstreamSubscriptions = (extendedParams = {}) => async (dispatch) => {
-  dispatch({ type: UPSTREAM_SUBSCRIPTIONS_REQUEST });
+export const saveUpstreamSubscriptions = (params, handleSuccess) =>
+  APIActions.post({
+    key: SAVE_UPSTREAM_SUBSCRIPTIONS_KEY,
+    url: api.getApiUrl(`/organizations/${orgId()}/upstream_subscriptions`),
+    params,
+    successToast: saveSuccessToast,
+    errorToast: error => getResponseErrorMsgs(error.response),
+    handleSuccess,
+  });
 
-  const params = {
-    ...{ organization_id: orgId(), attachable: true },
-    ...propsToSnakeCase(extendedParams),
-  };
+const pingUpstreamSubscriptions = () => dispatch =>
+  dispatch(APIActions.get({
+    key: PING_UPSTREAM_SUBSCRIPTIONS_KEY,
+    url: api.getApiUrl(`/organizations/${orgId()}/upstream_subscriptions/ping`),
+    handleSuccess: () => dispatch({ type: PING_UPSTREAM_SUBSCRIPTIONS_SUCCESS }),
+    handleError: error => dispatch(apiError(PING_UPSTREAM_SUBSCRIPTIONS_FAILURE, error)),
+  }));
 
-  try {
-    const { data } = await api.get(`/organizations/${orgId()}/upstream_subscriptions`, {}, params);
-    return dispatch({
-      type: UPSTREAM_SUBSCRIPTIONS_SUCCESS,
-      response: data,
-      search: extendedParams.search,
-    });
-  } catch (error) {
-    return dispatch(apiError(UPSTREAM_SUBSCRIPTIONS_FAILURE, error));
-  }
-};
-
-export const saveUpstreamSubscriptions = upstreamSubscriptions => async (dispatch) => {
-  dispatch({ type: SAVE_UPSTREAM_SUBSCRIPTIONS_REQUEST });
-
-  const params = {
-    ...propsToSnakeCase(upstreamSubscriptions),
-  };
-
-  try {
-    const { data } = await api.post(`/organizations/${orgId()}/upstream_subscriptions`, params);
-    return dispatch({
-      type: SAVE_UPSTREAM_SUBSCRIPTIONS_SUCCESS,
-      response: data,
-    });
-  } catch (error) {
-    return dispatch(apiError(SAVE_UPSTREAM_SUBSCRIPTIONS_FAILURE, error));
-  }
-};
-
-export default loadUpstreamSubscriptions;
+export default pingUpstreamSubscriptions;
